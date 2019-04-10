b42dd5b774f4d60638dfcb34837e596ee0c7c920
Steven Danna <steve@chef.io>
2019-04-10 15:24:13 +0100

    Fix a2 references in concurrency group config
    Signed-off-by: Steven Danna <steve@chef.io>

--- .expeditor/publish-release-notes.sh
+++ .expeditor/publish-release-notes.sh
@@ -11,7 +11,7 @@ build_version=$(jq -r -c ".build"  manifest.json)
 
 git clone https://github.com/chef/automate.wiki.git
 
-pushd ./a2.wiki
+pushd ./automate.wiki
   # Publish release notes to S3
   aws s3 cp Current-Release-Notes.md "s3://chef-automate-artifacts/release-notes/automate/${build_version}.md" --acl public-read --content-type "text/plain" --profile chef-cd
   aws s3 cp Current-Release-Notes.md "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/automate/release-notes.md" --acl public-read --content-type "text/plain" --profile chef-cd
