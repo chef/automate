754fd2a306f0f622f9b1bf6de9b647dc93bb3935
Pete Higgins <pete@peterhiggins.org>
2019-04-15 16:47:13 -0700

    GetReports should handle users with access to unassigned resources.
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>

--- components/compliance-service/integration_test/reporting_server_list_reports_test.go
+++ components/compliance-service/integration_test/reporting_server_list_reports_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	authzConstants "github.com/chef/automate/components/authz-service/constants/v2"
 	"github.com/chef/automate/components/compliance-service/api/reporting"
 	reportingServer "github.com/chef/automate/components/compliance-service/api/reporting/server"
 	"github.com/chef/automate/components/compliance-service/reporting/relaxting"
@@ -14,6 +15,9 @@ import (
 func TestListReports(t *testing.T) {
 	server := reportingServer.New(&relaxting.ES2Backend{ESUrl: elasticsearchUrl})
 	reports := []*relaxting.ESInSpecReport{
+		{
+			Projects: []string{},
+		},
 		{
 			Projects: []string{"project1"},
 		},
@@ -50,17 +54,37 @@ func TestListReports(t *testing.T) {
 		{
 			description:     "Projects: user has access to one project with reports",
 			allowedProjects: []string{"project1"},
-			expectedIds:     reportIds[:2],
+			expectedIds:     reportIds[1:3],
 		},
 		{
 			description:     "Projects: user has access to some projects with reports",
 			allowedProjects: []string{"project1", "project2"},
-			expectedIds:     reportIds[:4],
+			expectedIds:     reportIds[1:5],
 		},
 		{
 			description:     "Projects: user has access to projects without reports",
 			allowedProjects: []string{"project4", "project5"},
 			expectedIds:     []string{},
+		},
+		{
+			description:     "Projects: user has access to one project with reports and unassigned reports",
+			allowedProjects: []string{"project1", authzConstants.UnassignedProjectID},
+			expectedIds:     reportIds[:3],
+		},
+		{
+			description:     "Projects: user has access to some projects with reports and unassigned reports",
+			allowedProjects: []string{"project1", "project2", authzConstants.UnassignedProjectID},
+			expectedIds:     reportIds[:5],
+		},
+		{
+			description:     "Projects: user has access to projects without reports and unassigned reports",
+			allowedProjects: []string{"project4", "project5", authzConstants.UnassignedProjectID},
+			expectedIds:     reportIds[:1],
+		},
+		{
+			description:     "Projects: user has access to unassigned reports",
+			allowedProjects: []string{authzConstants.UnassignedProjectID},
+			expectedIds:     reportIds[:1],
 		},
 	}
 
