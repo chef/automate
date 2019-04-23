45377d0223196b6c8826bb4aa710baf309454e5b
Salim Afiune <afiune@chef.io>
2019-04-23 12:23:49 -0500

    Add Feature Flag to applications-service (#148)
    * Add Feature Flag to applications-service
    * Update automate-cli enable/disable apps cmd
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- api/config/applications/config_request.go
+++ api/config/applications/config_request.go
@@ -23,6 +23,7 @@ func DefaultConfigRequest() *ConfigRequest {
 	c := NewConfigRequest()
 	c.V1.Sys.Service.Host = w.String("0.0.0.0")
 	c.V1.Sys.Service.Port = w.Int32(10133)
+	c.V1.Sys.Service.EnableNatsFeature = w.Bool(false)
 	c.V1.Sys.Log.Level = w.String("info")
 	c.V1.Sys.Log.Format = w.String("text")
 	return c
