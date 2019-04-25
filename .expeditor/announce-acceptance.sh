a836205a0fd6e14641379b71d65d615cfe63045a
Stephen Delano <stephen@chef.io>
2019-04-24 21:26:14 -0700

    [promotion] automate weekly promotion announcement
    Signed-off-by: Stephen Delano <stephen@chef.io>

--- .expeditor/announce-acceptance.sh
+++ .expeditor/announce-acceptance.sh
@@ -3,10 +3,12 @@
 current=$(curl https://packages.chef.io/manifests/current/automate/latest.json 2>/dev/null | jq -r '.git_sha');
 acceptance=$(curl https://packages.chef.io/manifests/acceptance/automate/latest.json 2>/dev/null | jq -r '.git_sha');
 
-cat <<EOF
+read -r -d '' message <<EOF
 @here :success: A2 has been promoted from \`dev\` to \`acceptance\` :success:
 
 The list of changes can be found here: https://github.com/chef/automate/compare/${current}...${acceptance}
 
 Please take your time to fill out the release notes by *EOD WEDNESDAY*: https://github.com/chef/automate/wiki/Current-Release-Notes
 EOF
+
+post_slack_message "a2-team" "$message"
