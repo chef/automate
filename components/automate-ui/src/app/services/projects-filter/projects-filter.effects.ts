28456fb259f920852f64b691bcebfff706a3575c
Brenna Hewer-Darroch <brenna@chef.io>
2019-05-02 11:47:37 -0700

    ui: fetch authorized projects from backend
    Signed-off-by: Blake Johnson <bstier@chef.io>
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- components/automate-ui/src/app/services/projects-filter/projects-filter.effects.ts
+++ components/automate-ui/src/app/services/projects-filter/projects-filter.effects.ts
@@ -5,6 +5,7 @@ import { switchMap, catchError, map, tap } from 'rxjs/operators';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { ProjectsFilterOption, ProjectsFilterOptionTuple } from './projects-filter.reducer';
 import { ProjectsFilterService } from './projects-filter.service';
+import { ProjectsFilterRequests, AuthorizedProjectsResponse } from './projects-filter.requests';
 import {
   ProjectsFilterActionTypes,
   LoadOptions,
@@ -17,18 +18,21 @@ import {
 export class ProjectsFilterEffects {
   constructor(
     private actions$: Actions,
-    private projectsFilter: ProjectsFilterService
+    private projectsFilter: ProjectsFilterService,
+    private requests: ProjectsFilterRequests
+
   ) { }
 
   @Effect()
   loadOptions$ = this.actions$.pipe(
     ofType<LoadOptions>(ProjectsFilterActionTypes.LOAD_OPTIONS),
     switchMap(() => {
-      return this.projectsFilter.fetchOptions().pipe(
-        map((fetched: ProjectsFilterOption[]) => {
+      return this.requests.fetchOptions().pipe(
+        map((fetched: AuthorizedProjectsResponse) => {
+          const converted = convertResponse(fetched.projects);
           const restored = this.projectsFilter.restoreOptions() || [];
           return new LoadOptionsSuccess(<ProjectsFilterOptionTuple>{
-            fetched: fetched,
+            fetched: converted,
             restored: restored
           });
         }),
@@ -40,4 +44,17 @@ export class ProjectsFilterEffects {
     ofType<SaveOptions>(ProjectsFilterActionTypes.SAVE_OPTIONS),
     tap(({ payload }) => this.projectsFilter.storeOptions(payload))
   );
+}
+
+export function convertResponse(authorizedProjects: string[]): ProjectsFilterOption[] {
+  let convertedProjects: ProjectsFilterOption[];
+  authorizedProjects.forEach(project => {
+      let option = <ProjectsFilterOption>{
+          label: project,
+          value: project,
+          checked: false,
+      }
+      convertedProjects.push(option)
+  });
+  return convertedProjects
 }
--- components/automate-ui/src/app/services/projects-filter/projects-filter.service.ts
+++ components/automate-ui/src/app/services/projects-filter/projects-filter.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable, of as observableOf } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { NgrxStateAtom } from 'app/ngrx.reducers';
 import { ProjectConstants } from 'app/entities/projects/project.model';
@@ -41,39 +40,4 @@ export class ProjectsFilterService {
     return JSON.parse(localStorage.getItem(STORE_OPTIONS_KEY));
   }
 
-  fetchOptions(): Observable<ProjectsFilterOption[]> {
-    // Request some options available to the user.
-    return observableOf([
-      {
-        value: 'project-2',
-        label: 'Project 2',
-        checked: false
-      },
-      {
-        value: 'project-4',
-        label: 'Project 4',
-        checked: false
-      },
-      {
-        value: ProjectConstants.UNASSIGNED_PROJECT_ID,
-        label: ProjectConstants.UNASSIGNED_PROJECT_LABEL,
-        checked: false
-      },
-      {
-        value: 'ze end of the alpha project',
-        label: 'ZETA PROJ',
-        checked: false
-      },
-      {
-        value: 'project-that-has-a-really-long-name',
-        label: 'Project that has a really long name',
-        checked: false
-      },
-      {
-        value: 'super-project',
-        label: 'Super Duper Project',
-        checked: false
-      }
-    ]);
-  }
 }
