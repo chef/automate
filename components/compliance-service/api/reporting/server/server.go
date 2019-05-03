50b176c4cd89264e80c2506a11f0d0c470f6fdaa
pete higgins <pete@peterhiggins.org>
2019-05-03 12:20:23 -0700

    Reporting code cleanup (#246)
    * Remove unnecessary variable declaration.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Fix function names in error messages.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Remove repeated filter guard.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Combine functions only ever used together.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Delete commented code.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Remove unused struct.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Run `gofmt -s -w -l .` across the project.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Combine redundant functions.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Remove dead functions.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Flip condition to have an early return with no data returned.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>
    
    * Simplify return value of function to how it is used.
    
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>

--- components/compliance-service/api/reporting/server/server.go
+++ components/compliance-service/api/reporting/server/server.go
@@ -65,7 +65,6 @@ func (srv *Server) ListReports(ctx context.Context, in *reporting.Query) (*repor
 
 // ReadReport returns a reports based on id
 func (srv *Server) ReadReport(ctx context.Context, in *reporting.Query) (*reporting.Report, error) {
-	var report *reporting.Report
 	formattedFilters := formatFilters(in.Filters)
 	//todo - deep filtering - should we open this up to more than just one?  only for ReadReport?
 	if len(formattedFilters["profile_id"]) > 1 {
@@ -76,7 +75,7 @@ func (srv *Server) ReadReport(ctx context.Context, in *reporting.Query) (*report
 		return nil, utils.FormatErrorMsg(err, in.Id)
 	}
 	// Using ComplianceTwenty as the report might not be in the latest index
-	report, err = srv.es.GetReport(relaxting.ComplianceDailyRepTwenty, in.Id, formattedFilters)
+	report, err := srv.es.GetReport(relaxting.ComplianceDailyRepTwenty, in.Id, formattedFilters)
 	if err != nil {
 		return nil, utils.FormatErrorMsg(err, in.Id)
 	}
@@ -153,24 +152,13 @@ func (srv *Server) Export(in *reporting.Query, stream reporting.ReportingService
 		return err
 	}
 
-	err = exportReports(formattedFilters, srv.es, exporter)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func exportReports(filters map[string][]string, esr *relaxting.ES2Backend, sendResult exportHandler) error {
 	// Step 1: Retrieving the latest report ID for each node based on the provided filters
-	esIndex, err := relaxting.GetEsIndex(filters, false, false)
+	esIndex, err := relaxting.GetEsIndex(formattedFilters, false, false)
 	if err != nil {
 		return status.Error(codes.Internal, fmt.Sprintf("Failed to determine how many reports exist: %s", err))
 	}
 
-	if len(filters["profile_name"]) > 1 {
-		return status.Error(codes.InvalidArgument, "Only one 'profile_name' filter is allowed")
-	}
-	reportIDs, err := esr.GetReportIds(esIndex, filters)
+	reportIDs, err := srv.es.GetReportIds(esIndex, formattedFilters)
 	if err != nil {
 		return status.Error(codes.Internal, fmt.Sprintf("Failed to determine how many reports exist: %s", err))
 	}
@@ -178,11 +166,11 @@ func exportReports(filters map[string][]string, esr *relaxting.ES2Backend, sendR
 	total := len(reportIDs)
 	// Step 2: get all reports one by one in reverse order to be sorted asc by end_time
 	for idx := total - 1; idx >= 0; idx-- {
-		cur, err := esr.GetReport(esIndex, reportIDs[idx], filters)
+		cur, err := srv.es.GetReport(esIndex, reportIDs[idx], formattedFilters)
 		if err != nil {
 			return status.Error(codes.NotFound, fmt.Sprintf("Failed to retrieve report %d/%d with ID %s . Error: %s", idx, total, reportIDs[idx], err))
 		}
-		err = sendResult(cur)
+		err = exporter(cur)
 		if err != nil {
 			return status.Error(codes.Internal, fmt.Sprintf("Failed to stream report %d/%d with ID %s . Error: %s", idx, total, reportIDs[idx], err))
 		}
--- components/compliance-service/reporting/relaxting/migrations.go
+++ components/compliance-service/reporting/relaxting/migrations.go
@@ -333,7 +333,7 @@ func (backend ES2Backend) getScanDateRange(indexPrefix string) (*time.Time, *tim
 		return nil, nil, errors.Wrap(err, fmt.Sprintf("%s unable to Unmarshal min_date", myName))
 	}
 
-	logrus.Debugf("GetMinScanDate: earliest scan date for range = %s", minDateAsString)
+	logrus.Debugf("earliest scan date for range = %s", minDateAsString)
 
 	formatOfDate := "2006-01-02"
 	earliestScanDate, err := time.Parse(formatOfDate, minDateAsString)
@@ -347,7 +347,7 @@ func (backend ES2Backend) getScanDateRange(indexPrefix string) (*time.Time, *tim
 		return nil, nil, errors.Wrap(err, fmt.Sprintf("%s unable to Unmarshal max_date", myName))
 	}
 
-	logrus.Debugf("GetMinScanDate: most recent scan date for range = %s", maxDateAsString)
+	logrus.Debugf(" most recent scan date for range = %s", maxDateAsString)
 
 	mostRecentScanDate, err := time.Parse(formatOfDate, maxDateAsString)
 	if err != nil {
@@ -407,16 +407,11 @@ func (backend ES2Backend) reindex(src, dest, reindexScript, srcDocType string) (
 
 func (backend ES2Backend) getLatestReportIds(sumDailyToday string) ([]string, error) {
 	myName := "getLatestReportIds"
-	var nodeReports map[string]string
-	var reportIds []string
-	nodeReports, err = backend.getNodeReportIdsFromTimeseries(sumDailyToday, make(map[string][]string, 0), false)
+	reportIds, err := backend.getNodeReportIdsFromTimeseries(sumDailyToday, make(map[string][]string, 0), false)
 	if err != nil {
 		logrus.Errorf("%s no report ids for this day: %s. Will still reindex yesterday's latest though", myName, sumDailyToday)
 	}
 
-	if len(nodeReports) > 0 {
-		reportIds = MapValues(nodeReports)
-	}
 	return reportIds, nil
 }
 
