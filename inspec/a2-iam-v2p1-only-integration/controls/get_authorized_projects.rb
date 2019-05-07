21ed461e0cf2ed88d719ce43f7aeb2d3036b89b7
Blake Johnson <bstier@chef.io>
2019-05-07 11:59:52 -0700

    inspec: non admin user authorized projects test
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- inspec/a2-iam-v2p1-only-integration/controls/get_authorized_projects.rb
+++ inspec/a2-iam-v2p1-only-integration/controls/get_authorized_projects.rb
@@ -25,9 +25,8 @@ control 'iam-v2-global-projects-filter-1' do
   }
 
   PROJECTS = [ PROJECT_1, PROJECT_2, PROJECT_3 ]
-
-  describe 'allowed projects for admin' do
-
+  
+  describe 'allowed projects' do
     before(:all) do
       PROJECTS.each do|project|
         resp = automate_api_request("/apis/iam/v2beta/projects",
@@ -37,56 +36,85 @@ control 'iam-v2-global-projects-filter-1' do
         expect(resp.http_status).to eq 200
       end
     end
-
+    
     after(:all) do
       PROJECTS.each do|project|
         resp = automate_api_request("/apis/iam/v2beta/projects/#{project[:id]}", http_method: 'delete')
         expect(resp.http_status).to eq 200
       end
     end
- 
-    it 'returns list of all projects and unassigned' do
-      resp = automate_api_request("/api/v0/auth/introspect_projects", http_method: 'get')
 
-      expect(resp.http_status).to eq 200
-      # always returns complete list of projects + (unassigned)
-      expect(resp.parsed_response_body[:projects].length).to eq PROJECTS.length + 1
-      expected_projects = PROJECTS.map { |p| p[:id] }.push('(unassigned)')
-      expect(resp.parsed_response_body[:projects]).to match_array(expected_projects)
+    describe 'allowed projects for admin' do
+      it 'returns list of all projects and unassigned' do
+        resp = automate_api_request("/api/v0/auth/introspect_projects", http_method: 'get')
+
+        expect(resp.http_status).to eq 200
+        # always returns complete list of projects + (unassigned)
+        expected_projects = PROJECTS.map { |p| p[:id] }.push('(unassigned)')
+        expect(resp.parsed_response_body[:projects]).to match_array(expected_projects)
+      end
+    end
+
+    describe 'allowed projects for non-admin' do
+      non_admin_username = 'inspec-test-non-admin'
+      non_admin_policy_id = 'non-admin-policy'
+
+      before(:all) do
+        create_user_response = automate_api_request(
+          '/apis/iam/v2beta/users',
+          http_method: 'POST',
+          request_body: {
+            'name': non_admin_username,
+            'id': non_admin_username,
+            'password': ENV['AUTOMATE_API_DEFAULT_PASSWORD'] || 'chefautomate',
+          }.to_json
+        )
+
+        expect(create_user_response.http_status.to_s).to match(/200|409/)
+
+        create_policy_response = automate_api_request("/apis/iam/v2beta/policies",
+          http_method: 'POST',
+          request_body: {
+            id: non_admin_policy_id,
+            name: 'gotta catch em all',
+            members: ["user:local:#{non_admin_username}"],
+            statements: [
+              {
+                effect: "ALLOW",
+                # TODO this test will fail if there is only one parameterized action (i.e. just iam:teams:get)
+                actions: ["iam:teams:list", "iam:teams:get"],
+                projects: [PROJECT_ID_1, PROJECT_ID_2]
+              }
+            ]
+          }.to_json()
+        )
+        expect(create_policy_response.http_status.to_s).to match (/200|409/)
+      end
+
+      after(:all) do
+        delete_user_response = automate_api_request(
+          "/apis/iam/v2beta/users/#{non_admin_username}",
+          http_method: 'DELETE',
+        )
+
+        expect(delete_user_response.http_status.to_s).to match(/200|404/)
+
+        delete_policy_response = automate_api_request(
+          "/apis/iam/v2beta/policies/#{non_admin_policy_id}",
+          http_method: 'DELETE',
+        )
+
+        expect(delete_policy_response.http_status.to_s).to match(/200|404/)
+      end
+
+      it 'returns list of allowed projects' do
+        resp = automate_api_request("/api/v0/auth/introspect_projects", http_method: 'get', user: non_admin_username)
+
+        expect(resp.http_status).to eq 200
+        expected_projects = [PROJECT_ID_1, PROJECT_ID_2]
+        expect(resp.parsed_response_body[:projects]).to match_array(expected_projects)
+      end
     end
   end
 end
-
-  # describe 'all allowed projects for non-admin user' do
-
-  #   before(:all) do
-  #     #   // before: 
-  #     #   // create projects
-  #     #   // create policy
-  #     #   // create user
-  #     #   // log in as user
-  #     Projects.each do|project|
-  #       resp = automate_api_request("/apis/iam/v2beta/projects",
-  #         http_method: 'POST',
-  #         request_body: project.to_json
-  #       )
-  #       expect(resp.http_status).to eq 200
-  #     end
-  #     end
-  #   end
-
-  #   after(:all) do
-  #     Projects.each do|project|
-  #       resp = automate_api_request("/apis/iam/v2beta/projects/#{project[:id]}", http_method: 'delete')
-  #       expect(resp.http_status).to eq 200
-  #     end
-  #   end
- 
-  #   # // for non-admin with access to project 1 and 2, only displays those 2 projects
-  #   it "returns only projects specified in user's policy" do
-  #   #   // assert: 
-  #   #   // check global projects filter contents
-  #   #   expect(resp.parsed_response_body[:roles].find { |item| item[:id] == CUSTOM_ROLE_ID_1 }).to_not be_nil
-  #   end
-  # end
 
