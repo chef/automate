9ba37d962534ff5e0d3a77935ea2fa9778c2ce92
michael sorens <msorens@users.noreply.github.com>
2019-05-08 12:45:58 -0700

    Basic tests and functionality for interceptor in place
    Signed-off-by: michael sorens <msorens@users.noreply.github.com>

--- components/automate-ui/src/app/services/http/http-client-auth.interceptor.spec.ts
+++ components/automate-ui/src/app/services/http/http-client-auth.interceptor.spec.ts
@@ -1,17 +1,30 @@
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClientAuthInterceptor } from './http-client-auth.interceptor';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+
 import { ChefSessionService } from '../chef-session/chef-session.service';
+import {
+  ProjectsFilterState, ProjectsFilterOption
+} from '../projects-filter/projects-filter.reducer';
+import { HttpClientAuthInterceptor } from './http-client-auth.interceptor';
 
 describe('HttpClientAuthInterceptor', () => {
   let httpClient: HttpClient;
   let httpMock: HttpTestingController;
   let chefSession: ChefSessionService;
+  const projects = ['proj1', 'proj2', 'proj3'];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        StoreModule.forRoot({
+          projectsFilter: () => <ProjectsFilterState>{
+            options: projects.map(p => <ProjectsFilterOption>{ value: p })
+          }
+        })
+      ],
       providers: [
         ChefSessionService,
         {
@@ -36,6 +49,16 @@ describe('HttpClientAuthInterceptor', () => {
 
     expect(httpRequest.request.headers.get('Authorization'))
       .toEqual(`Bearer ${chefSession.id_token}`);
+  });
+
+  it('includes projects in all requests', done => {
+    httpClient.get('/endpoint').subscribe(done);
+
+    const httpRequest = httpMock.expectOne('/endpoint');
+    httpRequest.flush('response');
+
+    expect(httpRequest.request.headers.get('projects'))
+      .toEqual(projects.join(', '));
   });
 
   describe('when a 401 response is intercepted', () => {
--- components/automate-ui/src/app/services/http/http-client-auth.interceptor.ts
+++ components/automate-ui/src/app/services/http/http-client-auth.interceptor.ts
@@ -1,26 +1,46 @@
-import { throwError as observableThrowError,  Observable } from 'rxjs';
-
-import { catchError } from 'rxjs/operators';
-import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { throwError as observableThrowError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { get } from 'lodash/fp';
-import { ChefSessionService } from '../chef-session/chef-session.service';
+
+import { NgrxStateAtom } from 'app/ngrx.reducers';
+import { ChefSessionService } from 'app/services/chef-session/chef-session.service';
+import * as selectors from 'app/services/projects-filter/projects-filter.selectors';
+import { ProjectsFilterOption } from '../projects-filter/projects-filter.reducer';
 
 @Injectable()
 export class HttpClientAuthInterceptor implements HttpInterceptor {
 
-  constructor(private chefSession: ChefSessionService) {}
+  private projects: string;
+
+  constructor(
+    private chefSession: ChefSessionService,
+    private store: Store<NgrxStateAtom>
+  ) {
+    this.store.select(selectors.options)
+      .subscribe((options: ProjectsFilterOption[]) => {
+        this.projects = options.map(p => p.value).join(', ') || 'proj1'; // TODO: empty string causes 403 errors ?!?
+      });
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next
       .handle(request.clone({
-        headers: request.headers.set('Authorization', `Bearer ${this.chefSession.id_token}`)
+        headers: request.headers
+          .set('Authorization', `Bearer ${this.chefSession.id_token}`)
+          .set('projects', this.getProjects())
       })).pipe(
-      catchError((response: HttpEvent<any>) => {
-        if (get('status', response) === 401) {
-          this.chefSession.logout();
-        }
-        return observableThrowError(response);
-      }));
+        catchError((response: HttpEvent<any>) => {
+          if (get('status', response) === 401) {
+            this.chefSession.logout();
+          }
+          return observableThrowError(response);
+        }));
+  }
+
+  private getProjects(): string {
+    return this.projects;
   }
 }
