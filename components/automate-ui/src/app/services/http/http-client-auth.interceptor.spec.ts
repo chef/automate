3cd7d053c4f55838ea997cbce7e3798bee41ac72
michael sorens <msorens@users.noreply.github.com>
2019-05-09 08:30:02 -0700

    Handle initial "no projects" case
    At login there will be no projects to send so need to suppress
    the header, because sending projects with an empty list
    violates the regex validation requiring each project sent to be non-empty.
    
    Signed-off-by: michael sorens <msorens@users.noreply.github.com>

--- components/automate-ui/src/app/services/http/http-client-auth.interceptor.spec.ts
+++ components/automate-ui/src/app/services/http/http-client-auth.interceptor.spec.ts
@@ -13,56 +13,27 @@ describe('HttpClientAuthInterceptor', () => {
   let httpClient: HttpClient;
   let httpMock: HttpTestingController;
   let chefSession: ChefSessionService;
-  const projects = ['proj1', 'proj2', 'proj3'];
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule,
-        StoreModule.forRoot({
-          projectsFilter: () => <ProjectsFilterState>{
-            options: projects.map(p => <ProjectsFilterOption>{ value: p })
-          }
-        })
-      ],
-      providers: [
-        ChefSessionService,
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: HttpClientAuthInterceptor,
-          multi: true
-        }
-      ]
+  describe('common functionality', () => {
+    beforeEach(() => {
+      configure();
+      httpClient = TestBed.get(HttpClient);
+      httpMock = TestBed.get(HttpTestingController);
+      chefSession = TestBed.get(ChefSessionService);
+      spyOnProperty(chefSession, 'id_token', 'get').and.returnValue('token');
     });
 
-    httpClient = TestBed.get(HttpClient);
-    httpMock = TestBed.get(HttpTestingController);
-    chefSession = TestBed.get(ChefSessionService);
-    spyOnProperty(chefSession, 'id_token', 'get').and.returnValue('token');
-  });
+    it('includes auth token in all requests', done => {
+      httpClient.get('/endpoint').subscribe(done);
 
-  it('includes auth token in all requests', done => {
-    httpClient.get('/endpoint').subscribe(done);
+      const httpRequest = httpMock.expectOne('/endpoint');
+      httpRequest.flush('response');
 
-    const httpRequest = httpMock.expectOne('/endpoint');
-    httpRequest.flush('response');
+      expect(httpRequest.request.headers.get('Authorization'))
+        .toEqual(`Bearer ${chefSession.id_token}`);
+    });
 
-    expect(httpRequest.request.headers.get('Authorization'))
-      .toEqual(`Bearer ${chefSession.id_token}`);
-  });
-
-  it('includes projects in all requests', done => {
-    httpClient.get('/endpoint').subscribe(done);
-
-    const httpRequest = httpMock.expectOne('/endpoint');
-    httpRequest.flush('response');
-
-    expect(httpRequest.request.headers.get('projects'))
-      .toEqual(projects.join(', '));
-  });
-
-  describe('when a 401 response is intercepted', () => {
-    it('logs out the session', done => {
+    it('when a 401 response is intercepted logs out the session', done => {
       spyOn(chefSession, 'logout');
       httpClient.get('/endpoint').subscribe(null, done);
 
@@ -72,4 +43,72 @@ describe('HttpClientAuthInterceptor', () => {
       expect(chefSession.logout).toHaveBeenCalled();
     });
   });
+
+  describe('project header', () => {
+
+    describe('with some projects', () => {
+      const projectsList = ['proj1', 'proj2', 'proj3'];
+
+      beforeEach(() => {
+        configure(projectsList.map(p => <ProjectsFilterOption>{ value: p }));
+        httpClient = TestBed.get(HttpClient);
+        httpMock = TestBed.get(HttpTestingController);
+        chefSession = TestBed.get(ChefSessionService);
+        spyOnProperty(chefSession, 'id_token', 'get').and.returnValue('token');
+      });
+
+      it('includes projects header in all requests', done => {
+        httpClient.get('/endpoint').subscribe(done);
+
+        const httpRequest = httpMock.expectOne('/endpoint');
+        httpRequest.flush('response');
+
+        expect(httpRequest.request.headers.keys()).toContain('projects');
+        expect(httpRequest.request.headers.get('projects')).toEqual(projectsList.join(', '));
+      });
+    });
+
+    describe('with no projects', () => {
+        const projectsList = [];
+
+      beforeEach(() => {
+        configure(projectsList.map(p => <ProjectsFilterOption>{ value: p }));
+        httpClient = TestBed.get(HttpClient);
+        httpMock = TestBed.get(HttpTestingController);
+        chefSession = TestBed.get(ChefSessionService);
+        spyOnProperty(chefSession, 'id_token', 'get').and.returnValue('token');
+      });
+
+      it('does not include projects header', done => {
+        httpClient.get('/endpoint').subscribe(done);
+
+        const httpRequest = httpMock.expectOne('/endpoint');
+        httpRequest.flush('response');
+
+        expect(httpRequest.request.headers.keys()).not.toContain('projects');
+      });
+    });
+  });
+
 });
+
+function configure(projects?: ProjectsFilterOption[]): void {
+  TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      StoreModule.forRoot({
+        projectsFilter: () => <ProjectsFilterState>{
+          options: projects || []
+        }
+      })
+    ],
+    providers: [
+      ChefSessionService,
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: HttpClientAuthInterceptor,
+        multi: true
+      }
+    ]
+  });
+}
