c46cbc17434e466a92b65be1a51b55428cdeefed
Victoria Jeffrey <vjeffrey@chef.io>
2019-05-10 10:13:45 -0600

    format ruby test so error msg will be better
    Signed-off-by: Victoria Jeffrey <vjeffrey@chef.io>

--- components/compliance-service/api/tests/test_ingest_to_mgr_conn.rb
+++ components/compliance-service/api/tests/test_ingest_to_mgr_conn.rb
@@ -20,17 +20,21 @@ describe File.basename(__FILE__) do
     nodes_list = MANAGER_GRPC nodes, :list, Nodes::Query.new()
     assert_equal(nodes_list.total, 9)
 
-    nodes_list['nodes'].each { |node|
-      assert_equal(node['scan_data'].id.length > 0, true)
-      assert_equal(node['scan_data'].status.length > 0, true)
-      assert_equal(node['scan_data'].end_time.length > 0, true)
-    }
-
     state_nodes = MANAGER_GRPC nodes, :list, Nodes::Query.new(filters:[Common::Filter.new(key: "state", values: ["RUNNING", "STOPPED", ""])])
     assert_equal(state_nodes.total, 9)
 
     # those nodes should not have been added to the manual node manager, as they were ingested nodes, not manually added nodes
     manually_managed_nodes = MANAGER_GRPC nodes, :list, Nodes::Query.new(filters: [Common::Filter.new(key: "manager_id", values: ["e69dc612-7e67-43f2-9b19-256afd385820"])])
     assert_equal(manually_managed_nodes.total, original_manually_managed_nodes.total)
+  end
+
+  it "nodes have scan data" do
+    nodes = Nodes::NodesService
+    nodes_list = MANAGER_GRPC nodes, :list, Nodes::Query.new()
+    assert nodes_list["nodes"].all? { |node|
+      node["scan_data"].id.length > 0 &&
+        node["scan_data"].status.length > 0 &&
+        node["scan_data"].end_time.length > 0
+    }, "Nodes did not have necessary scan_data: #{nodes_list["nodes"]}"
   end
 end
