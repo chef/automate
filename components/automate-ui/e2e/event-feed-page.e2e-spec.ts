9df502f38f08e64bd7c9d468145ba92d73a98eed
Lance Finfrock <lfinfrock@chef.io>
2019-05-14 13:06:37 -0700

    Fixing flaky test
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- components/automate-ui/e2e/event-feed-page.e2e-spec.ts
+++ components/automate-ui/e2e/event-feed-page.e2e-spec.ts
@@ -9,9 +9,18 @@ describe('Event Feed: Guitar String Graph Interaction', () => {
 
   it('should have a working dropdown filter', () => {
     const select = element(by.css('app-event-feed-select chef-select'));
-    select.click();
 
-    expect(select.getAttribute('class')).toContain('active');
+    // Click the event feed dropdown filter and wait until the filter turns 'active'
+    select.click().then(() => {
+      return new Promise(function(resolve) {
+        browser.wait(() => {
+          return select.getAttribute('class').then((values: string) => {
+            return values.indexOf('active') > 0;
+          });
+        // resolve the promise when the 'active' attribute is found or after one second
+        }, 1000).then( () => resolve());
+      });
+    });
   });
 
   xit('Event Feed page is accessible', (done) => {
