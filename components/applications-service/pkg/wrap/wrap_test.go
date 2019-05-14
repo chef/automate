b0b4d3721a269b7fb8b99440d0c7681573ecdbc6
danielsdeleo <dan@chef.io>
2019-05-14 14:50:04 -0700

    Write go code that shows how to use protobuf Any
    Signed-off-by: Daniel DeLeo <dan@chef.io>

---
+++ components/applications-service/pkg/wrap/wrap_test.go
@@ -0,0 +1,51 @@
+package wrap
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/stretchr/testify/require"
+
+	"github.com/chef/automate/api/external/applications"
+)
+
+func TestWrap(t *testing.T) {
+	ex1 := applications.ExampleOne{
+		IntOne: 23,
+		IntTwo: 42,
+	}
+
+	packedEx1, err := ptypes.MarshalAny(&ex1)
+	require.NoError(t, err)
+
+	wrap1 := applications.Wrapper{
+		Type:    "ExampleOne",
+		Message: packedEx1,
+	}
+
+	fmt.Printf("w: %+v\n", wrap1)
+
+	data, err := proto.Marshal(&wrap1)
+
+	require.NoError(t, err)
+
+	fmt.Printf("data: %+v\n", data)
+
+	var wrap1rt applications.Wrapper
+
+	err = proto.Unmarshal(data, &wrap1rt)
+
+	require.NoError(t, err)
+
+	fmt.Printf("w (rt): %+v\n", wrap1rt)
+
+	var extractedEx1 applications.ExampleOne
+
+	err = ptypes.UnmarshalAny(wrap1rt.Message, &extractedEx1)
+
+	require.NoError(t, err)
+
+	fmt.Printf("ex1 (rt): %+v\n", extractedEx1)
+}
