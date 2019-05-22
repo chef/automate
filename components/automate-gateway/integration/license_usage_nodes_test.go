f261238f8fa31c3c3c91b1f910512a2c9b2709a8
Stephan Renatus <srenatus@chef.io>
2019-05-22 10:15:25 +0200

    [integration] automate-gateway: fix tests to use GRPC port
    So apparently these HAD used the multiplexing code I've deleted. Well,
    they don't have to. Changed the hardcoded port to automate-gateway's
    default grpc port.
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- components/automate-gateway/integration/license_usage_nodes_test.go
+++ components/automate-gateway/integration/license_usage_nodes_test.go
@@ -22,7 +22,7 @@ import (
 const (
 	complianceEndpoint = "127.0.0.1:10121"
 	secretsEndpoint    = "127.0.0.1:10131"
-	gatewayEndpoint    = "127.0.0.1:2000"
+	gatewayEndpoint    = "127.0.0.1:2001"
 	nodesEndpoint      = "127.0.0.1:10120"
 )
 
