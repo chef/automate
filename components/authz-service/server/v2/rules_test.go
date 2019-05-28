db4c81445fca10f1a041802e906804e815385420
Brenna Hewer-Darroch <brenna@chef.io>
2019-05-28 16:04:48 -0700

    authz: add GetRules tests
    i hacked it to get the fromStorageConditions helper into my test,
    but i'm puzzled about why i can't access that helper automatically
    since it's in the same package (v2)
    
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- components/authz-service/server/v2/rules_test.go
+++ components/authz-service/server/v2/rules_test.go
@@ -6,10 +6,13 @@ import (
 	"testing"
 
 	cache "github.com/patrickmn/go-cache"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 
 	api "github.com/chef/automate/api/interservice/authz/v2"
+	v2 "github.com/chef/automate/components/authz-service/server/v2"
 	storage "github.com/chef/automate/components/authz-service/storage/v2"
 	"github.com/chef/automate/lib/grpc/grpctest"
 )
@@ -156,8 +159,62 @@ func TestCreateRule(t *testing.T) {
 	}
 }
 
+func TestGetRule(t *testing.T) {
+	ctx := context.Background()
+	cl, store, _, _ := setupRules(t)
+
+	storageConditions := []storage.Condition{
+		{
+			Type:      storage.Node,
+			Attribute: storage.Organization,
+			Operator:  storage.MemberOf,
+			Value:     []string{"opscode"},
+		},
+	}
+
+	cases := []struct {
+		desc string
+		f    func(*testing.T)
+	}{
+		{"if the rule id is empty, returns 'invalid argument'", func(t *testing.T) {
+			resp, err := cl.GetRule(ctx, &api.GetRuleReq{Id: ""})
+			grpctest.AssertCode(t, codes.InvalidArgument, err)
+			assert.Nil(t, resp)
+		}},
+		{"if the rule id is invalid, returns 'invalid argument'", func(t *testing.T) {
+			resp, err := cl.GetRule(ctx, &api.GetRuleReq{Id: "no_underscore_allowed"})
+			grpctest.AssertCode(t, codes.InvalidArgument, err)
+			assert.Nil(t, resp)
+		}},
+		{"if the rule does not exist, returns code 'not found'", func(t *testing.T) {
+			resp, err := cl.GetRule(ctx, &api.GetRuleReq{Id: "foo"})
+			grpctest.AssertCode(t, codes.NotFound, err)
+			assert.Nil(t, resp)
+		}},
+		{"if a rule with the requested id exists, returns the rule", func(t *testing.T) {
+			id := "foo-rule"
+			apiRule, err := addRuleToStore(t, store, id, "my coo foo rule", storage.Node, "foo-project", storageConditions)
+			require.NoError(t, err)
+
+			resp, err := cl.GetRule(ctx, &api.GetRuleReq{Id: id})
+
+			require.NoError(t, err)
+			assert.Equal(t, &apiRule, resp.Rule)
+		}},
+	}
+
+	rand.Shuffle(len(cases), func(i, j int) {
+		cases[i], cases[j] = cases[j], cases[i]
+	})
+
+	for _, test := range cases {
+		t.Run(test.desc, test.f)
+		store.Flush()
+	}
+}
+
 func addRuleToStore(t *testing.T, store *cache.Cache, id, name string, ruleType storage.RuleType, projectID string,
-	conditions []storage.Condition) {
+	conditions []storage.Condition) (api.ProjectRule, error) {
 	t.Helper()
 
 	rule := &storage.Rule{
@@ -168,6 +225,24 @@ func addRuleToStore(t *testing.T, store *cache.Cache, id, name string, ruleType 
 		Conditions: conditions,
 	}
 	store.Add(id, rule, 0)
+
+	returnType := api.ProjectRuleTypes_NODE
+	if ruleType == storage.Event {
+		returnType = api.ProjectRuleTypes_EVENT
+	}
+
+	returnConditions, err := v2.FromStorageConditions(conditions)
+	if err != nil {
+		return api.ProjectRule{}, errors.Errorf("failed to convert conditions: %s", err)
+	}
+
+	return api.ProjectRule{
+		Id:         id,
+		Name:       name,
+		Type:       returnType,
+		ProjectId:  projectID,
+		Conditions: returnConditions,
+	}, nil
 }
 
 func setupRules(t *testing.T) (api.ProjectsClient, *cache.Cache, *mockEventServiceClient, int64) {
