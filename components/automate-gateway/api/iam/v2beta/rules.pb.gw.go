02a8f8ae39d9cd27cfc2b18541a04001b97d00de
Brenna Hewer-Darroch <brenna@chef.io>
2019-05-30 13:44:23 -0700

    gateway: compile protobufs
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- components/automate-gateway/api/iam/v2beta/rules.pb.gw.go
+++ components/automate-gateway/api/iam/v2beta/rules.pb.gw.go
@@ -82,6 +82,33 @@ func request_Rules_ListRules_0(ctx context.Context, marshaler runtime.Marshaler,
 
 }
 
+func request_Rules_DeleteRule_0(ctx context.Context, marshaler runtime.Marshaler, client RulesClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq request.DeleteRuleReq
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
+	}
+
+	protoReq.Id, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	msg, err := client.DeleteRule(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterRulesHandlerFromEndpoint is same as RegisterRulesHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterRulesHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -180,6 +207,26 @@ func RegisterRulesHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("DELETE", pattern_Rules_DeleteRule_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Rules_DeleteRule_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Rules_DeleteRule_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -189,6 +236,8 @@ var (
 	pattern_Rules_GetRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"iam", "v2beta", "rules", "id"}, ""))
 
 	pattern_Rules_ListRules_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"iam", "v2beta", "rules"}, ""))
+
+	pattern_Rules_DeleteRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"iam", "v2beta", "rules", "id"}, ""))
 )
 
 var (
@@ -197,4 +246,6 @@ var (
 	forward_Rules_GetRule_0 = runtime.ForwardResponseMessage
 
 	forward_Rules_ListRules_0 = runtime.ForwardResponseMessage
+
+	forward_Rules_DeleteRule_0 = runtime.ForwardResponseMessage
 )
