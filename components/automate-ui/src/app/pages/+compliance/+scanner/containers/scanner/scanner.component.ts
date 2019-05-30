4525c86874c24fe0bdc768e5c64e246d8249aaa3
vinay sharma <vsharma@chef.io>
2019-05-30 20:11:56 +0530

    Adding loading pattern to profiles page + hide 0 until after page loads (#379)
    * added changes for don't show 0 count while loading
    
    Signed-off-by: vinay033 <vsharma@chef.io>
    
    * added loader image gif
    
    Signed-off-by: vinay033 <vsharma@chef.io>
    
    * added new chanegs for default spinner
    
    Signed-off-by: vinay033 <vsharma@chef.io>
    
    * remove unused spinner gif
    
    Signed-off-by: vinay033 <vsharma@chef.io>
    
    * fixed lint issue
    
    Signed-off-by: vinay033 <vsharma@chef.io>

--- components/automate-ui/src/app/pages/+compliance/+scanner/containers/scanner/scanner.component.ts
+++ components/automate-ui/src/app/pages/+compliance/+scanner/containers/scanner/scanner.component.ts
@@ -13,12 +13,22 @@ export class ScannerComponent implements OnInit {
   jobsCount$: Observable<number>;
   nodesCount$: Observable<number>;
 
+  // show spinner before loading data
+  countsLoading = true;
+  jobsCountLoaded = false;
+  nodesCountLoaded = false;
+
   constructor(private store: Store<any>) {}
 
   ngOnInit() {
     this.jobsCount$ = this.store.select(selectors.jobsList)
       .pipe(map(jobsList => jobsList.total));
+    this.jobsCountLoaded = true;
+
     this.nodesCount$ = this.store.select(selectors.nodeTotals)
       .pipe(map(nodeTotals => nodeTotals.all));
+    this.nodesCountLoaded = true;
+
+    this.countsLoading = false;
   }
 }
