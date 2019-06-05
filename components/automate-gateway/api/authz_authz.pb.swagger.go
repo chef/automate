c2156e17e6e69be2aa945ecbf4ba945063655a42
michael sorens <msorens@users.noreply.github.com>
2019-06-05 09:03:09 -0700

    Revert "Proto fix"
    This reverts commit 8dcc4e1661e10af4197b0d5c2154fac61c5dfaab.

--- components/automate-gateway/api/authz_authz.pb.swagger.go
+++ components/automate-gateway/api/authz_authz.pb.swagger.go
@@ -58,6 +58,22 @@ func init() {
         ]
       }
     },
+    "/auth/introspect_projects": {
+      "get": {
+        "operationId": "IntrospectAllProjects",
+        "responses": {
+          "200": {
+            "description": "A successful response.",
+            "schema": {
+              "$ref": "#/definitions/responseIntrospectProjectsResp"
+            }
+          }
+        },
+        "tags": [
+          "Authorization"
+        ]
+      }
+    },
     "/auth/introspect_some": {
       "post": {
         "operationId": "IntrospectSome",
@@ -268,6 +284,17 @@ func init() {
         "updated_at": {
           "type": "string",
           "format": "date-time"
+        }
+      }
+    },
+    "responseIntrospectProjectsResp": {
+      "type": "object",
+      "properties": {
+        "projects": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
         }
       }
     },
