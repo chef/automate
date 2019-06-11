7251b8adb349b3fec2b1ae15d284afb6babfbb93
Stephan Renatus <srenatus@chef.io>
2019-06-11 17:15:41 +0200

    e2e/cypress: switch to typescript
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- e2e/cypress/plugins/index.js
+++ e2e/cypress/plugins/index.js
@@ -10,8 +10,13 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const wp = require('@cypress/webpack-preprocessor')
 
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+// `on` is used to hook into various events Cypress emits
+// `config` is the resolved Cypress config
+module.exports = (on, _config) => {
+  const options = {
+    webpackOptions: require('../../webpack.config'),
+  }
+  on('file:preprocessor', wp(options))
 }
