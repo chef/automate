3556f621d335ca874c85622ab003e8b25ef0e8ad
Blake Johnson <bstier@chef.io>
2019-06-13 11:13:10 -0700

    repond to early feedback
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/authz-service/storage/postgres/migration/sql/47_staged_rules_tables.up.sql
+++ components/authz-service/storage/postgres/migration/sql/47_staged_rules_tables.up.sql
@@ -1,25 +1,12 @@
 BEGIN;
 
-CREATE TABLE iam_staged_projects (
-  db_id SERIAL,
-  id TEXT PRIMARY KEY,
-  name TEXT NOT NULL,
-  type iam_policy_type NOT NULL DEFAULT 'custom',
-  projects TEXT[],
-  deleted BOOLEAN,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-);
-
 CREATE TABLE iam_staged_project_rules (
   db_id SERIAL PRIMARY KEY,
   id TEXT NOT NULL UNIQUE,
-  project_id TEXT REFERENCES iam_staged_projects ON DELETE CASCADE,
+  project_id TEXT REFERENCES iam_projects ON DELETE CASCADE,
   name TEXT NOT NULL,
   type TEXT NOT NULL,
-  deleted BOOLEAN,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted BOOLEAN
 );
 
 CREATE TABLE iam_staged_rule_conditions (
@@ -28,21 +15,8 @@ CREATE TABLE iam_staged_rule_conditions (
   value TEXT[] NOT NULL,
   attribute TEXT NOT NULL,
   operator TEXT NOT NULL,
-  deleted BOOLEAN,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  deleted BOOLEAN
 );
-
-CREATE OR REPLACE FUNCTION
-  projects_match_for_staged_rule(_project_id TEXT, _projects_filter TEXT[])
-  RETURNS BOOLEAN AS $$
-    BEGIN
-      RETURN (
-        array_length(_projects_filter, 1) IS NULL
-        OR _project_id = ANY (_projects_filter)
-      );
-    END
-$$ LANGUAGE PLPGSQL;
 
 CREATE OR REPLACE FUNCTION
   query_staged_rule(_rule_db_id TEXT, _project_filter TEXT[])
@@ -54,17 +28,15 @@ CREATE OR REPLACE FUNCTION
       r.project_id,
       r.name,
       r.type,
-      r.deleted,
-      r.updated_at,
-      r.created_at
+      r.deleted
       -- A rule can't exist without conditions so we don't need to worry
       -- about null case here.
       json_agg(rc) AS conditions
     FROM iam_staged_project_rules AS r
     LEFT OUTER JOIN iam_staged_rule_conditions
     AS rc ON rc.rule_db_id=r.db_id
-    WHERE id=_rule_db_id AND projects_match_for_staged_rule(project_id, _project_filter)
-    GROUP BY r.id, r.project_id, r.name, r.type, r.deleted, r.updated_at, r.created_at
+    WHERE id=_rule_db_id AND projects_match_for_rule(project_id, _project_filter)
+    GROUP BY r.id, r.project_id, r.name, r.type, r.deleted
   )
   SELECT row_to_json(t) AS rule FROM t;
 
@@ -77,17 +49,6 @@ CREATE OR REPLACE FUNCTION
     UNION
     SELECT 'staged' as TableName from iam_staged_project_rules a where a.id=_rule_db_id
     );
-
-$$ LANGUAGE sql;
-
-CREATE OR REPLACE FUNCTION
-  query_staged_project(_project_id TEXT, _projects_filter TEXT[])
-  RETURNS json AS $$
-
-  WITH t AS
-    (SELECT p.id, p.name, p.type, p.projects p.deleted FROM iam_staged_projects p
-      WHERE p.id = _project_id AND (array_length(_projects_filter, 1) IS NULL OR p.projects && _projects_filter))
-  SELECT row_to_json(t) AS role FROM t;
 
 $$ LANGUAGE sql;
 
