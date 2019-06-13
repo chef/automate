9d4b6c3d6685d538848092c2fb0daa0fb5f964db
Jay Mundrawala <jmundrawala@chef.io>
2019-06-13 14:41:46 -0500

    simplify chef server only test
    Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

--- integration/tests/chef_server_only.sh
+++ integration/tests/chef_server_only.sh
@@ -4,7 +4,6 @@
 #shellcheck disable=SC1091
 
 test_name="chef-server-only"
-test_backup_restore=true
 
 source .studio/chef-server-collection
 
@@ -38,43 +37,4 @@ do_deploy() {
 do_test_deploy() {
     PATH="/hab/bin:/bin" chef-server-ctl test
     test_chef_server_ctl
-}
-
-do_backup() {
-    # Get ohai_time before backup
-    converge_chef_client
-    initial_ohai_time=$(ohai_time)
-
-    # Backup
-    chef-automate backup create
-
-    # Get the backup id
-    test_backup_id=$(chef-automate backup list | tail -1 | awk '{print $1}')
-
-    # Get ohai_time after backup
-    converge_chef_client
-    updated_ohai_time=$(ohai_time)
-
-    if [[ $initial_ohai_time = "$updated_ohai_time"  ]]
-        then
-	    log_error "Initial ohai_time ($initial_ohai_time) should not match updated ohai_time ($updated_ohai_time)."
-            return 1
-    fi
-    log_info "Initial ohai_time ($initial_ohai_time) correctly does not match updated ohai_time ($updated_ohai_time)."
-}
-
-do_restore() {
-   chef-automate backup restore --debug --override-origin "$HAB_ORIGIN" "$test_backup_id"
-   restored_ohai_time=$(ohai_time)
-
-   # Check ohai_time after restore matches ohai_time from backup
-   if [[ $restored_ohai_time != "$initial_ohai_time"  ]]
-
-       then
-	   log_error "Restored ohai_time ($restored_ohai_time) should match ohai_time ($initial_ohai_time) from backup."
-           return 1
-   fi
-   log_info "Restored ohai_time ($restored_ohai_time) correctly matches ohai_time ($initial_ohai_time) from backup."
-
-   delete_backup_and_assert_idempotent
 }
