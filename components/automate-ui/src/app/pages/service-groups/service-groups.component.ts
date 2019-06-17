b1ed1867bc2c4a59f7e6e8ee9bcdaa0b2fe49efd
Salim Afiune <afiune@chef.io>
2019-06-17 07:48:48 +0200

    Display an error message on failure
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- components/automate-ui/src/app/pages/service-groups/service-groups.component.ts
+++ components/automate-ui/src/app/pages/service-groups/service-groups.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subject, Observable, combineLatest } from 'rxjs';
 import { Store, createSelector } from '@ngrx/store';
 import { NgrxStateAtom } from 'app/ngrx.reducers';
+import { HttpErrorResponse } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Chicklet, RollupServiceStatus, SortDirection } from '../../types/types';
 import { EntityStatus } from '../../entities/entities';
@@ -13,7 +14,11 @@ import {
   ServiceGroup, ServiceGroupFilters, FieldDirection, HealthSummary, ServicesFilters
 } from '../../entities/service-groups/service-groups.model';
 import {
-  serviceGroupStatus, allServiceGroups, serviceGroupState, allServiceGroupHealth
+  serviceGroupStatus,
+  allServiceGroups,
+  serviceGroupState,
+  allServiceGroupHealth,
+  serviceGroupErrorResp
 } from '../../entities/service-groups/service-groups.selector';
 import { find, includes, get } from 'lodash/fp';
 
@@ -26,6 +31,7 @@ import { find, includes, get } from 'lodash/fp';
 export class ServiceGroupsComponent implements OnInit, OnDestroy {
   public serviceGroups$: Observable<ServiceGroup[]>;
   public serviceGroupStatus$: Observable<EntityStatus>;
+  public serviceGroupError$: Observable<HttpErrorResponse>;
   public sgHealthSummary: HealthSummary;
 
   // The selected service-group id that will be sent to the services-sidebar
@@ -99,6 +105,7 @@ export class ServiceGroupsComponent implements OnInit, OnDestroy {
     .subscribe(([queryParams]) => this.detailParamsChange(queryParams));
 
     this.serviceGroupStatus$ = this.store.select(serviceGroupStatus);
+    this.serviceGroupError$ = this.store.select(serviceGroupErrorResp);
     this.serviceGroups$ = this.store.select(allServiceGroups);
     this.serviceGroups$.pipe(
       withLatestFrom(this.route.queryParamMap),
