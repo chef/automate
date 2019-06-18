4ef7314b40f90f416d9de97c8e955855a56b7f56
Yvonne Lam <yvonne@chef.io>
2019-06-18 15:54:45 -0700

    Create and download a bootstrap bundle.
    Signed-off-by: Yvonne Lam <yvonne@chef.io>

---
+++ components/automate-cli/cmd/chef-automate/bootstrap.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	api "github.com/chef/automate/api/interservice/deployment"
+	"github.com/chef/automate/components/automate-cli/pkg/status"
+	"github.com/chef/automate/components/automate-deployment/pkg/client"
+	"github.com/spf13/cobra"
+)
+
+var bootstrapBundleCmd = &cobra.Command{
+	Use:    "bootstrap",
+	Short:  "Bundle bootstrap files.",
+	Long:   "Bundle files needed to bootstrap a new node.",
+	RunE:   runBootstrapBundleCmd,
+	Hidden: true,
+}
+
+func runBootstrapBundleCmd(cmd *cobra.Command, args []string) error {
+	connection, err := client.Connection(client.DefaultClientTimeout)
+	if err != nil {
+		fmt.Println("Connection failed")
+		return err
+	}
+
+	bundleInfo, err := connection.BootstrapBundle(context.Background(), &api.BootstrapBundleRequest{CollectionName: "chef-server"})
+	if err != nil {
+		fmt.Println("bundle creation failed")
+		return err
+	}
+
+	// Download the bundle via gRPC stream
+	stream, err := connection.BootstrapBundleDownload(
+		context.Background(),
+		&api.BootstrapBundleDownloadRequest{
+			BundleName: bundleInfo.BundleName,
+		},
+	)
+	if err != nil {
+		return status.WithRecovery(
+			status.Wrap(err, status.DeploymentServiceCallError, "Request to download bootstrap bundle failed"),
+			recoveryMsg,
+		)
+	}
+
+	downloadedBundlePath := bundleInfo.BundleName
+	downloadedBundleFile, err := os.OpenFile(downloadedBundlePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return status.Annotate(err, status.FileAccessError)
+	}
+
+	defer downloadedBundleFile.Close()
+
+	w := bufio.NewWriter(downloadedBundleFile)
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return status.WithRecovery(
+				status.Wrap(err, status.DeploymentServiceCallError, "Request to download bootstrap bundle failed"),
+				recoveryMsg,
+			)
+		}
+
+		data := resp.GetData()
+		if data == nil {
+			break
+		}
+		if _, err := w.Write(data); err != nil {
+			return status.Annotate(err, status.FileAccessError)
+		}
+	}
+
+	_ = w.Flush()
+	writer.Printf("Bootstrap bundle written to: %s\n", downloadedBundlePath)
+
+	return nil
+}
+
+func init() {
+	RootCmd.AddCommand(bootstrapBundleCmd)
+}
---
+++ components/automate-deployment/pkg/server/exporter_thing.go
@@ -0,0 +1,240 @@
+package server
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	api "github.com/chef/automate/api/interservice/deployment"
+	"github.com/chef/automate/components/automate-deployment/pkg/services"
+	"github.com/chef/automate/lib/io/chunks"
+	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
+)
+
+type bootstrapFile struct {
+	Filename string `json:"filename"`
+	Path     string `json:"path"`
+}
+
+type serviceForExport struct {
+	Service        string          `json:"service"`
+	BootstrapFiles []bootstrapFile `json:"bootstrap_files"`
+}
+
+type servicesForExport struct {
+	Services []serviceForExport `json:"services"`
+}
+
+// We want to be able to gather up files installed on one node and package them up so they can be made available when installing a second node.
+// Then we want to unpackage them and put them in the right place, with the right ownership.
+// LATER: We need to make sure a new version of the package gets generated when a file in that thing changes, but that is beyond
+// current scope.
+// PUT IT ELSEWHERE: For now I'm going to stuff the list of files in here, but we should probably put it elsewhere.
+// Also, we probably need to do it per service, which means we need to know who the running services in the installation are.
+//
+
+// BootstrapBundleCreate makes a bootstrap bundle
+func (s *server) BootstrapBundle(ctx context.Context,
+	req *api.BootstrapBundleRequest) (*api.BootstrapBundleResponse, error) {
+	collection := req.CollectionName
+	logrus.Infof("COLLECTION NAME is %s\n", collection)
+
+	// TODO: Check the collection is actually deployed.
+
+	// Create (and clean up) a staging area to copy files to
+	stagingDir := stagingDir(s.serverConfig)
+	logrus.Infof("STAGING DIR is %s\n", stagingDir)
+	archiveRoot, err := createTempDir(stagingDir, "bootstrap_bundle")
+	if err != nil {
+		return nil, err
+	}
+	logrus.Infof("ARCHIVE ROOT is %s\n", archiveRoot)
+	defer os.RemoveAll(archiveRoot)
+
+	// Jay will be providing a file where I can put package metadata; exportables should probably go in there.
+	// For now, we have a variable that lists exportables.
+	// (Yes, but not sure how yet.): Do you want to keep track of filepath and permissions?
+
+	var svcsForExport servicesForExport
+	err = json.Unmarshal([]byte(servicesJSON), &svcsForExport)
+	svcsInCollection, err := services.ServicesInCollection(collection)
+	logrus.Infof("SVCSINCOLLECTION is %+v", svcsInCollection)
+	if err != nil {
+		return nil, err
+	}
+	for _, svc := range svcsInCollection {
+		svcName := svc.Name()
+		logrus.Infof("SVCNAME is %s\n", svcName)
+		for _, s := range svcsForExport.Services {
+			if svcName == s.Service {
+				logrus.Infof("MATCHED ON service name: %s\n", svcName)
+				for _, b := range s.BootstrapFiles {
+
+					from, err := os.Open(b.Path)
+					if err != nil {
+						panic(err)
+					}
+					defer from.Close()
+
+					target := filepath.Join(archiveRoot, b.Filename)
+					to, err := os.OpenFile(target, os.O_RDWR|os.O_CREATE, 0666)
+					if err != nil {
+						panic(err)
+					}
+					defer to.Close()
+
+					_, err = io.Copy(to, from)
+					if err != nil {
+						panic(err)
+					}
+					logrus.Infof("COPIED FILE: %s\n", target)
+				}
+			} else {
+				continue
+			}
+		}
+	}
+
+	tgzFilepath := filepath.Join(stagingDir, "bootstrap-bundle.tgz")
+	fmt.Printf("TARBALL FILEPATH: %s\n", tgzFilepath)
+	tarAndGzipDirectory(archiveRoot, tgzFilepath)
+
+	tgzInfo, err := os.Stat(tgzFilepath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Is there a way to compute the checksum as we go instead of opening the file again? (not sure it matters, though)
+	f, err := os.Open(tgzFilepath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, f); err != nil {
+		return nil, err
+	}
+	cksum := hex.EncodeToString(hasher.Sum(nil))
+
+	return &api.BootstrapBundleResponse{
+		BundleName:     filepath.Base(tgzFilepath),
+		BundleChecksum: cksum,
+		BundleSize:     tgzInfo.Size(),
+	}, nil
+
+}
+
+func (s *server) BootstrapBundleDownload(req *api.BootstrapBundleDownloadRequest, stream api.Deployment_BootstrapBundleDownloadServer) error {
+	bundlePath := path.Join(stagingDir(s.serverConfig), req.BundleName)
+
+	defer func() {
+		err := os.Remove(bundlePath)
+		if err != nil {
+			log.WithError(err).Warn("Failed to remove support bundle file.")
+		}
+	}()
+
+	file, err := os.Open(bundlePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	buffer := make([]byte, defaultChunkSize)
+	writer := chunks.NewWriter(defaultChunkSize, func(p []byte) error {
+		return stream.Send(&api.BootstrapBundleDownloadResponse{Data: p})
+	})
+	_, err = io.CopyBuffer(writer, file, buffer)
+	return err
+}
+
+func tarAndGzipDirectory(inDir string, tgzFilepath string) error {
+	fmt.Println("MAKING THE TARBALL")
+	f, _ := os.Create(tgzFilepath)
+
+	zw := gzip.NewWriter(f)
+	defer zw.Close()
+
+	tw := tar.NewWriter(zw)
+	defer tw.Close()
+
+	// I need to self-review this more carefully.
+	return filepath.Walk(inDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		header, err := tar.FileInfoHeader(info, info.Name())
+		if err != nil {
+			return err
+		}
+
+		header.Name = filepath.Join(inDir, strings.TrimPrefix(path, inDir))
+
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		if _, err = io.Copy(tw, file); err != nil {
+			return err
+		}
+
+		// close each file as you deal with it
+		file.Close()
+		return nil
+	})
+
+}
+
+// TODO: move to products.json.
+// Not guaranteed to be complete/correct right now. The idea is to export *something*.
+const servicesJSON = `
+{
+  "services": [
+	{
+      "service": "automate-cs-oc-erchef",
+      "bootstrap_files": [
+		  {
+			"filename": "pivotal.pem",
+			"path": "/hab/svc/automate-cs-oc-erchef/data/pivotal.pem"
+		  },
+		  {
+			"filename": "pivotal.pub.pem",
+		  	"path": "/hab/svc/automate-cs-oc-erchef/data/pivotal.pub.pem"
+		  },
+		  {
+			"filename": "webui_priv.pem",
+			"path": "/hab/svc/automate-cs-oc-erchef/data/webui_priv.pem"
+		  },
+		  {
+			"filename": "webui_pub.pem",
+			"path": "/hab/svc/automate-cs-oc-erchef/data/webui_pub.pem"
+		  },
+		  {
+			"filename": "dark_launch_features.json",
+			"path": "/hab/svc/automate-cs-oc-erchef/data/dark_launch_features.json"
+		  }
+        ]
+    }
+  ]
+}
+`
