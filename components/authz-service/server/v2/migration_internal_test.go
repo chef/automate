db4c12afcba4e6f1880f83942585bbd591f80619
Stephan Renatus <srenatus@chef.io>
2019-06-18 17:13:08 +0200

    [A2-882] authz-service: fix v1 to v2 policy migration issues with applications (#612)
    * authz-service/migration: skip well-known "ApplicationServiceGroupsPolicy"
    
    This will resolve the following, irritating error:
    
        $ chef-automate iam upgrade-to-v2 --result-json output.json
    
        Upgrading to IAM v2
        Migrating v1 policies...
    
        Skipped policies:
          convert v1 policy "aee14d59-da0b-4974-ba6d-1a018b024874"
          unknown "well-known" policy
    
    ...by skipping this new default v1 policy in migration. Skipping is OK
    because the application access rights are implemented as additions to
    the viewer and editor roles in IAM v2 (v2.1).
    
    * [integration] iam v2: assert that no policy is skipped
    * authz-service/migration: remove comment -- it's only a warning
    * authz-service/migration: migrate custom v1 policies for app tab
    * authz-service/migration: bikeshed migration mapping function types
    * authz-service: add docs re: adding new default policies
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- components/authz-service/server/v2/migration_internal_test.go
+++ components/authz-service/server/v2/migration_internal_test.go
@@ -2239,6 +2239,21 @@ func TestV1PolicyMigration(t *testing.T) {
 				}),
 			),
 		},
+		"service_groups,list": {
+			storage_v1.Policy{
+				ID:       polID,
+				Subjects: []string{"user:local:albertine", "team:local:admins"},
+				Action:   "list",
+				Resource: "service_groups",
+			},
+			checks(
+				hasStatements(storage_v2.Statement{
+					Effect:    storage_v2.Allow,
+					Resources: []string{"applications:serviceGroups"},
+					Actions:   []string{"*:list"},
+				}),
+			),
+		},
 		"v1 default compliance token upload profile policy": {
 			wellknown(t, constants_v1.ComplianceTokenUploadProfilesPolicyID),
 			checks(hasID(constants_v2.ComplianceTokenPolicyID)),
@@ -2277,6 +2292,10 @@ func TestV1PolicyMigration(t *testing.T) {
 		},
 		"v1 default cn-nginx ingest events policy": {
 			wellknown(t, constants_v1.CSNginxComplianceDataCollectorPolicyID),
+			checks(isSkipped()),
+		},
+		"v1 default application policy": {
+			wellknown(t, constants_v1.ApplicationsServiceGroupsPolicyID),
 			checks(isSkipped()),
 		},
 	}
