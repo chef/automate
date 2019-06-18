8f1e71d940bfde8f2151cc31ce743f0f983221c2
michael sorens <msorens@chef.io>
2019-06-18 09:31:23 -0700

    Adjust apply rules API to single endpoint, different methods
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-gateway/api/iam/v2beta/rules.pb.policy-v2.go
+++ components/automate-gateway/api/iam/v2beta/rules.pb.policy-v2.go
@@ -85,13 +85,13 @@ func init() {
 		}
 		return ""
 	})
-	policyv2.MapMethodTo("/chef.automate.api.iam.v2beta.Rules/ApplyRulesStart", "iam:rules", "iam:rules:apply", "GET", "/iam/v2beta/apply-rules-start", func(unexpandedResource string, input interface{}) string {
+	policyv2.MapMethodTo("/chef.automate.api.iam.v2beta.Rules/ApplyRulesStart", "iam:rules", "iam:rules:apply", "POST", "/iam/v2beta/apply-rules", func(unexpandedResource string, input interface{}) string {
 		return unexpandedResource
 	})
-	policyv2.MapMethodTo("/chef.automate.api.iam.v2beta.Rules/ApplyRulesCancel", "iam:rules", "iam:rules:cancel", "GET", "/iam/v2beta/apply-rules-cancel", func(unexpandedResource string, input interface{}) string {
+	policyv2.MapMethodTo("/chef.automate.api.iam.v2beta.Rules/ApplyRulesCancel", "iam:rules", "iam:rules:cancel", "DELETE", "/iam/v2beta/apply-rules", func(unexpandedResource string, input interface{}) string {
 		return unexpandedResource
 	})
-	policyv2.MapMethodTo("/chef.automate.api.iam.v2beta.Rules/ApplyRulesStatus", "iam:rules", "iam:rules:status", "GET", "/iam/v2beta/apply-rules-status", func(unexpandedResource string, input interface{}) string {
+	policyv2.MapMethodTo("/chef.automate.api.iam.v2beta.Rules/ApplyRulesStatus", "iam:rules", "iam:rules:status", "GET", "/iam/v2beta/apply-rules", func(unexpandedResource string, input interface{}) string {
 		return unexpandedResource
 	})
 }
