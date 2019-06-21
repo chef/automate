1bb392e71dc25ecf1313b3dc66e1fdfd333c408f
Yvonne Lam <yvonne@chef.io>
2019-06-21 11:41:53 -0700

    Handing the chunks writer directly to the bundle creator results in the client receiving an empty file.
    Signed-off-by: Yvonne Lam <yvonne@chef.io>

--- components/automate-deployment/pkg/server/exporter_thing.go
+++ components/automate-deployment/pkg/server/exporter_thing.go
@@ -1,10 +1,6 @@
 package server
 
 import (
-	"io"
-	"os"
-	"path/filepath"
-
 	api "github.com/chef/automate/api/interservice/deployment"
 	"github.com/chef/automate/components/automate-deployment/pkg/bootstrap"
 	"github.com/chef/automate/lib/io/chunks"
@@ -27,9 +23,16 @@ type servicesForExport struct {
 // BootstrapBundle makes and downloads a bootstrap bundle
 func (s *server) BootstrapBundle(req *api.BootstrapBundleRequest, stream api.Deployment_BootstrapBundleServer) error {
 	// staging directory is where the tarball lands
-	stagingDir := stagingDir(s.serverConfig)
-	tarFilepath := filepath.Join(stagingDir, "bootstrap-bundle.tar")
-	f, _ := os.Create(tarFilepath)
+	/*
+		stagingDir := stagingDir(s.serverConfig)
+		tarFilepath := filepath.Join(stagingDir, "bootstrap-bundle.tar")
+		f, _ := os.Create(tarFilepath)
+
+		buffer := make([]byte, defaultChunkSize)
+	*/
+	writer := chunks.NewWriter(defaultChunkSize, func(p []byte) error {
+		return stream.Send(&api.BootstrapBundleResponse{Data: p})
+	})
 
 	bundleCreator := bootstrap.NewBundleCreator()
 
@@ -37,22 +40,11 @@ func (s *server) BootstrapBundle(req *api.BootstrapBundleRequest, stream api.Dep
 	for _, e := range s.deployment.ExpectedServices {
 		pkgs = append(pkgs, e.Name())
 	}
-	err := bundleCreator.Create(pkgs, f)
+	err := bundleCreator.Create(pkgs, writer)
 	if err != nil {
 		return err
 	}
-	f.Close()
 
-	file, err := os.Open(tarFilepath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	buffer := make([]byte, defaultChunkSize)
-	writer := chunks.NewWriter(defaultChunkSize, func(p []byte) error {
-		return stream.Send(&api.BootstrapBundleResponse{Data: p})
-	})
-	_, err = io.CopyBuffer(writer, file, buffer)
+	//_, err = io.CopyBuffer(writer, file, buffer)
 	return err
 }
