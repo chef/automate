1ca3143f1ca9415f6bd72a487ac32b3398ff090a
Salim Afiune <afiune@chef.io>
2019-06-22 00:54:54 +0200

    Move ingest logic to new ingest package
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- components/applications-service/pkg/ingest/ingest.go
+++ components/applications-service/pkg/ingest/ingest.go
@@ -1,34 +1,41 @@
-package server
+package ingest
 
 import (
 	"fmt"
 
-	"github.com/pkg/errors"
+	stan "github.com/nats-io/go-nats-streaming"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/chef/automate/api/external/habitat"
 	"github.com/chef/automate/components/applications-service/pkg/config"
 	"github.com/chef/automate/components/applications-service/pkg/nats"
 	"github.com/chef/automate/components/applications-service/pkg/storage/postgres"
+	"github.com/golang/protobuf/proto"
+	"github.com/pkg/errors"
 )
 
-const numWorkers = 50
+const (
+	numWorkers       = 50
+	eventsBufferSize = 100
+)
 
 type Ingester struct {
 	Cfg              *config.Applications
 	DBClient         *postgres.Postgres
 	natsClient       *nats.NatsClient
+	EventsCh         chan *stan.Msg
 	workerInputQueue chan<- *habitat.HealthCheckEvent
 	workerTaskQueue  <-chan *habitat.HealthCheckEvent
 }
 
-func NewIngester(c *config.Applications, db *postgres.Postgres) *Ingester {
+func New(c *config.Applications, db *postgres.Postgres) *Ingester {
 
 	q := make(chan *habitat.HealthCheckEvent)
 
 	return &Ingester{
 		Cfg:              c,
 		DBClient:         db,
+		EventsCh:         make(chan *stan.Msg, eventsBufferSize),
 		workerInputQueue: q,
 		workerTaskQueue:  q,
 	}
@@ -39,16 +46,14 @@ func NewIngester(c *config.Applications, db *postgres.Postgres) *Ingester {
 // Run() so connection errors can be handled easily while still running the
 // ingester in a goroutine.
 func (i *Ingester) Connect() error {
-	url := "nats://%s:%d"
-
 	natsClient := nats.NewDefaults(
-		fmt.Sprintf(url, i.Cfg.Events.Host, i.Cfg.Events.Port),
+		fmt.Sprintf("nats://%s:%d", i.Cfg.Events.Host, i.Cfg.Events.Port),
 		i.Cfg.Events.ClusterID,
 		*i.Cfg.TLSConfig,
 	)
 
 	// Trigger NATS Subscription
-	err := natsClient.ConnectAndSubscribe()
+	err := natsClient.ConnectAndSubscribe(i.EventsCh)
 	if err != nil {
 		return errors.Wrapf(err, "could not connect to nats-streaming")
 	}
@@ -65,8 +70,21 @@ func (i *Ingester) Run() {
 	// TODO @afiune Move this logic into an ingestion pipeline
 	for {
 		select {
-		case event := <-i.natsClient.EventsCh:
-			i.workerInputQueue <- event
+		case event := <-i.EventsCh:
+
+			// TODO: @afiune We should have a way to have multi-message-type ingestion
+			// Unmarshal the data and send the message to the channel
+			var habMsg habitat.HealthCheckEvent
+			err := proto.Unmarshal(event.Data, &habMsg)
+			if err != nil {
+				log.WithFields(log.Fields{
+					"data":    string(event.Data),
+					"subject": i.natsClient.Subject(),
+				}).Error("Unknown message, dropping")
+				continue
+			}
+
+			i.workerInputQueue <- &habMsg
 		}
 	}
 }
@@ -77,9 +95,7 @@ func (i *Ingester) runWorkerLoop() {
 		case event := <-i.workerTaskQueue:
 			err := i.DBClient.IngestHealthCheckEvent(event)
 			if err != nil {
-				log.WithFields(log.Fields{
-					"error": err.Error(),
-				}).Error("Unable to ingest habitat event")
+				log.WithError(err).Error("Unable to ingest habitat event")
 			}
 		}
 	}
--- components/applications-service/pkg/nats/subscriber.go
+++ components/applications-service/pkg/nats/subscriber.go
@@ -1,14 +1,12 @@
 package nats
 
 import (
-	"github.com/chef/automate/api/external/habitat"
-	"github.com/golang/protobuf/proto"
-
 	stan "github.com/nats-io/go-nats-streaming"
 	log "github.com/sirupsen/logrus"
 )
 
-func (nc *NatsClient) Subscribe() (stan.Subscription, error) {
+func (nc *NatsClient) Subscribe(eventsCh chan<- *stan.Msg) (stan.Subscription, error) {
+
 	log.WithFields(log.Fields{
 		"subject":    nc.subject,
 		"durable_id": nc.durableID,
@@ -24,19 +22,6 @@ func (nc *NatsClient) Subscribe() (stan.Subscription, error) {
 			"subject":  nc.subject,
 		}).Debug("Message received")
 
-		// TODO: @afiune We should have a way to have multi-message-type ingestion
-		// Unmarshal the data and send the message to the channel
-		var habMsg habitat.HealthCheckEvent
-		err := proto.Unmarshal(msg.Data, &habMsg)
-
-		if err != nil {
-			log.WithFields(log.Fields{
-				"data":    string(msg.Data),
-				"subject": nc.subject,
-			}).Error("Unknown message, dropping")
-		} else {
-			nc.EventsCh <- &habMsg
-		}
-
+		eventsCh <- msg
 	}, stan.DurableName(nc.durableID))
 }
