4b2d5c36072451ebdf7802a1695749f67ac728d1
Stephan Renatus <srenatus@chef.io>
2019-06-24 16:16:20 +0200

    authn-service/tokens: remove value uniqueness constraint again
    We're not ready for that yet -- although we should get an index for
    faster queries regardless, probably...
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- components/authn-service/tokens/pg/sql/13_add_db_id_column_to_tokens.up.sql
+++ components/authn-service/tokens/pg/sql/13_add_db_id_column_to_tokens.up.sql
@@ -3,6 +3,5 @@ BEGIN;
 ALTER TABLE chef_authn_tokens DROP CONSTRAINT chef_authn_clients_pkey;
 ALTER TABLE chef_authn_tokens ADD CONSTRAINT chef_authn_tokens_id_unique UNIQUE (id);
 ALTER TABLE chef_authn_tokens ADD COLUMN db_id SERIAL PRIMARY KEY;
-ALTER TABLE chef_authn_tokens ADD CONSTRAINT chef_authn_tokens_value_unique UNIQUE (value);
 
 COMMIT;
