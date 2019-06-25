9a8431ffd004aec734f98522cffb0b1f8d247d44
Jay Mundrawala <jmundrawala@chef.io>
2019-06-25 09:13:00 -0700

    Add top-level products.meta file (#627)
    * Add top-level products.meta file
    
    The products.meta file describes which products we can deploy, along
    with which packages belong to those products. This PR also adds a
    package.meta that can be added for any package in its component
    directory. With this file, we can describe things about the file like if
    it's a data service, what binlinks it should install, etc.

---
+++ lib/product/parser.go
@@ -0,0 +1,154 @@
+package product
+
+import (
+	"encoding/json"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+func parsePackageMetadata(packagePath string) (*PackageMetadata, error) {
+	f, err := os.Open(packagePath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Couldn't load %s", packagePath)
+	}
+	defer f.Close() // nolint: errcheck
+	decoder := json.NewDecoder(f)
+	decoder.DisallowUnknownFields()
+
+	metadata := PackageMetadata{}
+	if err := decoder.Decode(&metadata); err != nil {
+		return nil, errors.Wrapf(err, "Failed to read metadata for %s", packagePath)
+	}
+
+	if err := metadata.validate(); err != nil {
+		return nil, errors.Wrapf(err, "Failed to validate metadata for %s", packagePath)
+	}
+	return &metadata, nil
+}
+
+type productsmeta struct {
+	// Packages is a list of the root level packages/services that will
+	// make up a release of automate.
+	Packages []PackageName `json:"packages"`
+	// Collections is a list of collections that automate is broken down
+	// into. Some of these are deployable by the user.
+	Collections []*Collection `json:"collections"`
+}
+
+func (b *productsmeta) validate() error {
+	if len(b.Collections) <= 0 {
+		return errors.New("No collections found")
+	}
+
+	if len(b.Packages) <= 0 {
+		return errors.New("No packages found")
+	}
+
+	allowedPackageSet := make(map[PackageName]bool)
+	for _, p := range b.Packages {
+		if err := p.validate(); err != nil {
+			return err
+		}
+		allowedPackageSet[p] = true
+	}
+
+	collectionMap := make(map[string]*Collection)
+	for _, c := range b.Collections {
+		if err := c.validate(allowedPackageSet); err != nil {
+			return err
+		}
+		if _, found := collectionMap[c.Name]; found {
+			return errors.Errorf("collection name/alias %q was repeated", c.Name)
+
+		}
+		collectionMap[c.Name] = c
+		for _, alias := range c.Aliases {
+			if _, found := collectionMap[alias]; found {
+				return errors.Errorf("collection name/alias %q was repeated", c.Name)
+
+			}
+			collectionMap[alias] = c
+		}
+	}
+
+	for _, c := range b.Collections {
+		for _, dep := range c.Dependencies {
+			depCollection, found := collectionMap[dep]
+			if !found {
+				return errors.Errorf("%q is listed as a dependency for %q but was not found", dep, c.Name)
+			}
+			if c.Type == BaseType && depCollection.Type == ProductType {
+				return errors.Errorf("base collection %q may not depend on product collection (%q)",
+					c.Name, depCollection.Name)
+			}
+		}
+	}
+
+	return nil
+}
+
+func parseProductsMeta(filePath string) (*productsmeta, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to open %s", filePath)
+	}
+	defer f.Close() // nolint: errcheck
+
+	decoder := json.NewDecoder(f)
+	decoder.DisallowUnknownFields()
+
+	val := productsmeta{}
+	if err := decoder.Decode(&val); err != nil {
+		return nil, errors.Wrapf(err, "Failed to parse %s", filePath)
+	}
+
+	if err := val.validate(); err != nil {
+		return nil, err
+	}
+
+	return &val, nil
+}
+
+// Parse parses the root level product.meta along with the package.meta files that
+// may be provided by each component.
+func Parse(repoRootPath string) (*Metadata, error) {
+	componentsPath := path.Join(repoRootPath, "components")
+	packageMetadataMap := make(map[PackageName]*PackageMetadata)
+	packagesWithMetadata, err := filepath.Glob(path.Join(componentsPath, "*", "package.meta"))
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to glob packages")
+	}
+
+	if len(packagesWithMetadata) <= 0 {
+		return nil, errors.New("Found no package metadata")
+	}
+
+	for _, packagePath := range packagesWithMetadata {
+		metadata, err := parsePackageMetadata(packagePath)
+		if err != nil {
+			return nil, err
+		}
+		packageMetadataMap[metadata.Name] = metadata
+	}
+
+	metadata, err := parseProductsMeta(path.Join(repoRootPath, "products.meta"))
+	if err != nil {
+		return nil, err
+	}
+
+	packages := make([]*Package, len(metadata.Packages))
+	for i := range metadata.Packages {
+		packages[i] = &Package{
+			Name:     metadata.Packages[i],
+			Metadata: packageMetadataMap[metadata.Packages[i]],
+		}
+	}
+
+	return &Metadata{
+		Packages:    packages,
+		Collections: metadata.Collections,
+	}, nil
+}
---
+++ lib/product/types.go
@@ -0,0 +1,154 @@
+package product
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+type CollectionType string
+
+const (
+	// BaseType is a collection that may not be directly
+	// deployed by the user. It provides a way to group
+	// services which may be depended upon by other products.
+	BaseType CollectionType = "base"
+
+	// ProductType is a collection that can be deployed by the user.
+	ProductType CollectionType = "product"
+)
+
+func (c CollectionType) validate() error {
+	switch c {
+	case BaseType, ProductType:
+		return nil
+	default:
+		return errors.Errorf("%s is not a valid collection type", c)
+	}
+}
+
+// PackageName represents a package or service by origin/name
+type PackageName struct {
+	// Origin is the habitat origin this package belongs to
+	Origin string
+	// Name is the habitat name of the package
+	Name string
+}
+
+func (p *PackageName) UnmarshalText(text []byte) error {
+	parts := strings.Split(string(text), "/")
+	if len(parts) == 2 {
+		p.Origin = parts[0]
+		p.Name = parts[1]
+	} else {
+		return errors.Errorf("%s is not a valid package. Must be origin/name", parts)
+	}
+	return nil
+}
+
+func (p *PackageName) MarshalText() ([]byte, error) {
+	return []byte(fmt.Sprintf("%s/%s", p.Origin, p.Name)), nil
+}
+
+func (p *PackageName) validate() error {
+	if p.Origin == "" || p.Name == "" {
+		return errors.New("Invalid package")
+	}
+	return nil
+}
+
+func (p PackageName) String() string {
+	return fmt.Sprintf("%s/%s", p.Origin, p.Name)
+}
+
+// Collection is a group of services and packages that provide some
+// desired functionality. For example, postgres requires 3 services
+// to fully work in Automate: postgresql, a sidecar, and a tcp
+// gateway.
+// Collections can be of type base, meaning they are deployed as
+// part of other collections. The other type is product, which allows
+// them to be deployed by users (automate, chef-server, workflow, etc).
+type Collection struct {
+	Name string `json:"name"`
+
+	// Aliases is a list of alternative names that can be used for the
+	// collection. As an example, we might want to allow both chef-server
+	// and chef-infra-server
+	Aliases []string `json:"aliases"`
+
+	// Type is one of base or product. Products can be deployed, where as
+	// base collections can be included in other collections as deps
+	Type CollectionType `json:"type"`
+
+	// Services is the list of packages that will be deployed as services
+	// for the collection.
+	Services []PackageName `json:"services"`
+
+	// Packages is a list of packages that are needed for this collection.
+	// They will likely be providing binlinks for the user
+	Packages []PackageName `json:"packages"`
+
+	// Dependencies is a list of collections this collection depends on.
+	Dependencies []string `json:"dependencies"`
+
+	// Hidden allows adding collections that we don't want to present to the
+	// user
+	Hidden bool `json:"hidden"`
+}
+
+func (c *Collection) validate(allowedPackageSet map[PackageName]bool) error {
+	if err := c.Type.validate(); err != nil {
+		return err
+	}
+	for _, p := range c.Services {
+		if err := p.validate(); err != nil {
+			return err
+		}
+		if !allowedPackageSet[p] {
+			return errors.Errorf("Expected %s in packages list", p)
+		}
+	}
+
+	for _, p := range c.Packages {
+		if err := p.validate(); err != nil {
+			return err
+		}
+		if !allowedPackageSet[p] {
+			return errors.Errorf("Expected %s in packages list", p)
+		}
+	}
+	return nil
+}
+
+// PackageMetadata is a set of metadata that components may optionally provide.
+type PackageMetadata struct {
+	Name PackageName `json:"name"`
+
+	// DataService is set to true if this service is a a data service.
+	// For example, postgres, elasticsearch, s3
+	DataService bool `json:"data_service"`
+
+	// A list of binaries to be binlinked when the package is deployed
+	Binlinks []string `json:"binlinks"`
+}
+
+func (p *PackageMetadata) validate() error {
+	return nil
+}
+
+// Package represents a package/service along with its metadata
+type Package struct {
+	Name PackageName `json:"name"`
+	// Metadata is optional metadata a package may provide. This
+	// field is nullable
+
+	Metadata *PackageMetadata `json:"metadata"`
+}
+
+// Metadata is the top level metadata the describes the automate
+// collections and its packages.
+type Metadata struct {
+	Packages    []*Package    `json:"packages"`
+	Collections []*Collection `json:"collections"`
+}
