987bc7520d54d64b930bed8cde6998f12b8f3a4b
Stephan Renatus <srenatus@chef.io>
2019-06-25 08:08:35 +0200

    authz-service/postgres: dedup migration code
    The lib/db/migrator package features the same "hard reset migration"
    logic this code had for one version only.
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- lib/db/migrator/migrator.go
+++ lib/db/migrator/migrator.go
@@ -7,16 +7,37 @@ import (
 	_ "github.com/golang-migrate/migrate/database/postgres" // make driver available
 	_ "github.com/golang-migrate/migrate/source/file"       // make source available
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
+
+	"github.com/chef/automate/lib/logger"
 )
 
+type migrationLog struct {
+	logger.Logger
+	verbose bool
+}
+
+func (l migrationLog) Verbose() bool {
+	return l.verbose
+}
+
 // Migrate executes all migrations we have
-func Migrate(pgURL string, migrationsPath string) error {
-	logrus.Infof("Running db migrations from %q", migrationsPath)
-	m, err := migrate.New(addScheme(migrationsPath), pgURL)
+func Migrate(pgURL, migrationsPath string, l logger.Logger, verbose bool) error {
+	return MigrateWithMigrationsTable(pgURL, migrationsPath, "", l, verbose)
+}
+
+// MigrateWithMigrationsTable executes all migrations we have, using the
+// specified migrations table.
+func MigrateWithMigrationsTable(pgURL, migrationsPath, migrationsTable string, l logger.Logger, verbose bool) error {
+	l.Infof("Running db migrations from %q", migrationsPath)
+	purl, err := addMigrationsTable(pgURL, migrationsTable)
+	if err != nil {
+		return errors.Wrap(err, "parse PG URL")
+	}
+	m, err := migrate.New(addScheme(migrationsPath), purl)
 	if err != nil {
 		return errors.Wrap(err, "init migrator")
 	}
+	m.Log = migrationLog{Logger: l, verbose: verbose}
 	version, dirty, err := m.Version()
 	if err != nil && err != migrate.ErrNilVersion {
 		return errors.Wrap(err, "init migrator - error getting migration version")
@@ -28,9 +49,9 @@ func Migrate(pgURL string, migrationsPath string) error {
 		if err != nil {
 			return errors.Wrap(err, "force to working schema version")
 		}
-		logrus.Infof("Forced to previous version: %v to reattempt migration", int(version)-1)
+		l.Infof("Forced to previous version: %v to reattempt migration", int(version)-1)
 	} else {
-		logrus.Infof("Current schema version: %v", version)
+		l.Infof("Current schema version: %v", version)
 	}
 
 	err = m.Up()
@@ -38,7 +59,7 @@ func Migrate(pgURL string, migrationsPath string) error {
 		return errors.Wrap(err, "migration attempt failed")
 	}
 
-	logrus.Infof("Completed db migrations")
+	l.Infof("Completed db migrations")
 
 	// The first error is trying to Close() the source. For our file source,
 	// that's always nil
@@ -51,4 +72,17 @@ func addScheme(p string) string {
 	u.Scheme = "file"
 	u.Path = p
 	return u.String()
+}
+
+func addMigrationsTable(u, table string) (string, error) {
+	pgURL, err := url.Parse(u)
+	if err != nil {
+		return "", err
+	}
+	if table != "" {
+		q := pgURL.Query()
+		q.Set("x-migrations-table", table)
+		pgURL.RawQuery = q.Encode()
+	}
+	return pgURL.String(), nil
 }
