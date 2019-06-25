0545bd067a7e4cb99be9d967e95d0b36b21ea232
Stephan Renatus <srenatus@chef.io>
2019-06-25 08:09:18 +0200

    teams-service/postgres: deduplicate migration code
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- components/teams-service/storage/postgres/migration/migration.go
+++ components/teams-service/storage/postgres/migration/migration.go
@@ -3,11 +3,7 @@ package migration
 import (
 	"net/url"
 
-	"github.com/golang-migrate/migrate"
-	_ "github.com/golang-migrate/migrate/database/postgres" // make driver available
-	_ "github.com/golang-migrate/migrate/source/file"       // make source available
-	"github.com/pkg/errors"
-
+	"github.com/chef/automate/lib/db/migrator"
 	"github.com/chef/automate/lib/logger"
 )
 
@@ -19,37 +15,7 @@ type Config struct {
 	Logger logger.Logger
 }
 
-type migrationLog struct {
-	logger.Logger
-}
-
-func (migrationLog) Verbose() bool {
-	return false
-}
-
 // Migrate executes all migrations we have
 func (c *Config) Migrate() error {
-	m, err := migrate.New(addScheme(c.Path), c.PGURL.String())
-	if err != nil {
-		return errors.Wrap(err, "init migrate")
-	}
-
-	m.Log = migrationLog{c.Logger} // nolint: govet
-
-	err = m.Up()
-	if err != nil && err != migrate.ErrNoChange {
-		return errors.Wrap(err, "execute migrations")
-	}
-
-	// The first error is trying to Close() the source. For our file source,
-	// that's always nil
-	_, err = m.Close() // nolint: gas
-	return errors.Wrap(err, "close migrations connection")
-}
-
-func addScheme(p string) string {
-	u := url.URL{}
-	u.Scheme = "file"
-	u.Path = p
-	return u.String()
+	return migrator.Migrate(c.PGURL.String(), c.Path, c.Logger, false)
 }
