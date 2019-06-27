1b9f574352c91567805acd98b804524cb87d97b4
Salim Afiune <afiune@chef.io>
2019-06-27 07:56:26 +0200

    (fix) drop default value for package_ident column
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- components/applications-service/pkg/storage/postgres/schema/sql/05_previous_health_service.up.sql
+++ components/applications-service/pkg/storage/postgres/schema/sql/05_previous_health_service.up.sql
@@ -1,2 +1,6 @@
 -- Add previous_health column
 ALTER TABLE service ADD COLUMN previous_health TEXT NOT NULL DEFAULT '';
+
+-- (fix) drop the default value for the package_ident column
+-- @afiune: we do not want to allow this field to be empty
+ALTER TABLE service ALTER COLUMN package_ident DROP DEFAULT;
