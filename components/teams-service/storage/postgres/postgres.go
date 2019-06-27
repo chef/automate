868296bd641f5e6d5bf91d54ff648d038775d22c
Blake Johnson <bstier@chef.io>
2019-06-27 11:07:11 -0700

    Remove unneeded grouping
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/teams-service/storage/postgres/postgres.go
+++ components/teams-service/storage/postgres/postgres.go
@@ -407,8 +407,7 @@ func (p *postgres) GetTeamsForUser(ctx context.Context, userID string) ([]storag
 		`SELECT t.id, t.name, t.description, t.projects, t.created_at, t.updated_at
 		FROM teams t
 		LEFT JOIN teams_users_associations tu ON tu.team_db_id=t.db_id
-		WHERE tu.user_id=$1 AND projects_match(t.projects, $2::TEXT[])
-		GROUP BY t.id, t.name, t.description, t.projects, t.created_at, t.updated_at`,
+		WHERE tu.user_id=$1 AND projects_match(t.projects, $2::TEXT[])`,
 		userID, pq.Array(projectsFilter))
 	if err != nil {
 		return nil, p.processError(err)
