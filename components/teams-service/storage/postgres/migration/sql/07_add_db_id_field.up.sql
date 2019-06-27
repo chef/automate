2b0b7ca45021c76831c88d24a0dd765ed9603f82
Stephan Renatus <srenatus@chef.io>
2019-06-27 13:29:32 +0200

    teams-service/migrations: pg_format new migration
    To get `pg_format`, run `brew install pgformatter` (also `brew install
    moreutils` for `sponge`).
    
    Then use it:
    
      pg_format components/teams-service/storage/postgres/migration/sql/07_add_db_id_field.up.sql |\
        sponge components/teams-service/storage/postgres/migration/sql/07_add_db_id_field.up.sql
    
    See also
    
    - https://github.com/darold/pgFormatter
    - https://joeyh.name/code/moreutils/ (sponge)
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- components/teams-service/storage/postgres/migration/sql/07_add_db_id_field.up.sql
+++ components/teams-service/storage/postgres/migration/sql/07_add_db_id_field.up.sql
@@ -1,29 +1,44 @@
 BEGIN;
-
 -- break reference constraints
-ALTER TABLE teams_users_associations ADD COLUMN team_db_id SERIAL;
-ALTER TABLE teams_users_associations DROP CONSTRAINT teams_users_associations_pkey;
-ALTER TABLE teams_users_associations DROP CONSTRAINT teams_users_associations_team_id_fkey;
-
+ALTER TABLE teams_users_associations
+    ADD COLUMN team_db_id SERIAL;
+ALTER TABLE teams_users_associations
+    DROP CONSTRAINT teams_users_associations_pkey;
+ALTER TABLE teams_users_associations
+    DROP CONSTRAINT teams_users_associations_team_id_fkey;
 -- massage teams for new pkey
-ALTER TABLE teams DROP CONSTRAINT teams_pkey;
-ALTER TABLE teams ADD CONSTRAINT teams_id_unique UNIQUE (id);
-ALTER TABLE teams ADD COLUMN db_id SERIAL PRIMARY KEY;
-
+ALTER TABLE teams
+    DROP CONSTRAINT teams_pkey;
+ALTER TABLE teams
+    ADD CONSTRAINT teams_id_unique UNIQUE (id);
+ALTER TABLE teams
+    ADD COLUMN db_id SERIAL PRIMARY KEY;
 -- set the team_db_id fields in the assoc table
-UPDATE teams_users_associations SET team_db_id=db_id FROM teams WHERE teams.id=team_id;
-ALTER TABLE teams_users_associations DROP COLUMN team_id;
-
+UPDATE
+    teams_users_associations
+SET
+    team_db_id = db_id
+FROM
+    teams
+WHERE
+    teams.id = team_id;
+ALTER TABLE teams_users_associations
+    DROP COLUMN team_id;
 -- add new constraints
-ALTER TABLE teams_users_associations ADD CONSTRAINT teams_db_id_fkey
-  FOREIGN KEY (team_db_id) REFERENCES teams(db_id) ON DELETE CASCADE;
-ALTER TABLE teams_users_associations ADD CONSTRAINT teams_users_pkey
-  PRIMARY KEY (team_db_id, user_id);
-
+ALTER TABLE teams_users_associations
+    ADD CONSTRAINT teams_db_id_fkey FOREIGN KEY (team_db_id) REFERENCES teams (db_id) ON DELETE CASCADE;
+ALTER TABLE teams_users_associations
+    ADD CONSTRAINT teams_users_pkey PRIMARY KEY (team_db_id, user_id);
 -- add helper for team db_id lookup
-CREATE FUNCTION team_db_id(_id UUID) RETURNS INTEGER AS
+CREATE FUNCTION team_db_id (_id UUID)
+    RETURNS INTEGER
+    AS $$
+    SELECT
+        db_id
+    FROM
+        teams
+    WHERE
+        id = _id;
 $$
-  SELECT db_id FROM teams WHERE id=_id;
-$$ LANGUAGE SQL;
-
+LANGUAGE SQL;
 COMMIT;
