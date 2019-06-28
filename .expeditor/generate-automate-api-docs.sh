e73dcc1d8a270bb0973158eca3a78422b7433f1c
Pete Higgins <pete@peterhiggins.org>
2019-06-27 17:36:15 -0700

    Properly copy swagger files into docs component.
    Signed-off-by: Pete Higgins <pete@peterhiggins.org>

--- .expeditor/generate-automate-api-docs.sh
+++ .expeditor/generate-automate-api-docs.sh
@@ -5,6 +5,6 @@ set -eou pipefail
 mkdir -p /go/src/github.com/chef
 ln -s /workspace /go/src/github.com/chef/automate
 
-pushd /go/src/github.com/chef/automate/components/automate-gateway
-  make docs
+pushd /go/src/github.com/chef/automate/components/automate-chef-io
+  make sync_swagger_files
 popd
