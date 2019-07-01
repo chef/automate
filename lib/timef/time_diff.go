d7cc4cbb5a5e80264023a847151dbf5bc5fa00ac
Salim Afiune <afiune@chef.io>
2019-07-01 08:37:36 +0200

    [lib/timef] Move time calculation into a library
    New timef library: time format, time calculation, all time related things.
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

---
+++ lib/timef/time_diff.go
@@ -0,0 +1,96 @@
+// time format, time calculation, all time related things.
+package timef
+
+import (
+	"fmt"
+	"time"
+)
+
+// IntervalUntilNow is the short of 'DiffPretty(timeA, now)'
+func IntervalUntilNow(t time.Time) string {
+	return DiffPretty(t, time.Now())
+}
+
+// DiffPretty returns an interval between two times in a pretty format (single string)
+// e.g. 1 year, 4 months, 1 day, 7 hours, 40 minutes, 49 seconds
+func DiffPretty(a, b time.Time) string {
+	var (
+		pretty       = ""
+		separator    = ", "
+		addSeparator = func(str string) string {
+			if str != "" {
+				return str + separator
+			}
+			return str
+		}
+		addTimeParam = func(str, param string, value int) string {
+			if value > 0 {
+				str = addSeparator(str)
+				if value == 1 { // singular
+					return fmt.Sprintf("%s%d %s", str, value, param)
+				}
+				// plural
+				return fmt.Sprintf("%s%d %ss", str, value, param)
+			}
+			return str
+		}
+		year, month, day, hour, min, sec = Diff(a, b)
+	)
+
+	// @afiune order here matters!
+	pretty = addTimeParam(pretty, "year", year)
+	pretty = addTimeParam(pretty, "month", month)
+	pretty = addTimeParam(pretty, "day", day)
+	pretty = addTimeParam(pretty, "hour", hour)
+	pretty = addTimeParam(pretty, "minute", min)
+	return addTimeParam(pretty, "second", sec)
+}
+
+// Diff returns the specific interval between two times
+func Diff(a, b time.Time) (year, month, day, hour, min, sec int) {
+	if a.Location() != b.Location() {
+		b = b.In(a.Location())
+	}
+	if a.After(b) {
+		a, b = b, a
+	}
+	var (
+		y1, M1, d1 = a.Date()
+		y2, M2, d2 = b.Date()
+
+		h1, m1, s1 = a.Clock()
+		h2, m2, s2 = b.Clock()
+	)
+
+	year = y2 - y1
+	month = int(M2 - M1)
+	day = d2 - d1
+	hour = h2 - h1
+	min = m2 - m1
+	sec = s2 - s1
+
+	// Normalize negative values
+	if sec < 0 {
+		sec += 60
+		min--
+	}
+	if min < 0 {
+		min += 60
+		hour--
+	}
+	if hour < 0 {
+		hour += 24
+		day--
+	}
+	if day < 0 {
+		// days in month
+		t := time.Date(y1, M1, 32, 0, 0, 0, 0, time.UTC)
+		day += 32 - t.Day()
+		month--
+	}
+	if month < 0 {
+		month += 12
+		year--
+	}
+	return year, month, day, hour, min, sec
+}
---
+++ lib/timef/time_diff_test.go
@@ -0,0 +1,226 @@
+package timef_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/chef/automate/lib/timef"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntervalUntilNow(t *testing.T) {
+	// Lets get the time now
+	timeNow := time.Now()
+
+	// Wait for a second to test the interval until (the new) "now"
+	time.Sleep(1 * time.Second)
+
+	diff := timef.IntervalUntilNow(timeNow)
+	assert.Equal(t, "1 second", diff)
+}
+
+func TestDiffPrettyMatrix(t *testing.T) {
+	cases := []struct {
+		message  string
+		timeA    time.Time
+		timeB    time.Time
+		expected string
+	}{
+		{
+			message:  "11 seconds",
+			timeA:    time.Date(2018, 1, 1, 1, 1, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 1, 1, 1, 11, 0, time.UTC),
+			expected: "11 seconds",
+		},
+		{
+			message:  "2 minutes",
+			timeA:    time.Date(2018, 1, 1, 1, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 1, 1, 2, 0, 0, time.UTC),
+			expected: "2 minutes",
+		},
+		{
+			message:  "23 hours",
+			timeA:    time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 1, 23, 0, 0, 0, time.UTC),
+			expected: "23 hours",
+		},
+		{
+			message:  "10 days",
+			timeA:    time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 11, 0, 0, 0, 0, time.UTC),
+			expected: "10 days",
+		},
+		{
+			message:  "6 months",
+			timeA:    time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2017, 7, 1, 0, 0, 0, 0, time.UTC),
+			expected: "6 months",
+		},
+		{
+			message:  "40 years",
+			timeA:    time.Date(2010, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2050, 1, 1, 0, 0, 0, 0, time.UTC),
+			expected: "40 years",
+		},
+		{
+			message:  "a second",
+			timeA:    time.Date(2018, 1, 1, 1, 1, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 1, 1, 1, 1, 0, time.UTC),
+			expected: "1 second",
+		},
+		{
+			message:  "a minute",
+			timeA:    time.Date(2018, 1, 1, 1, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 1, 1, 1, 0, 0, time.UTC),
+			expected: "1 minute",
+		},
+		{
+			message:  "an hour",
+			timeA:    time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 1, 1, 0, 0, 0, time.UTC),
+			expected: "1 hour",
+		},
+		{
+			message:  "a day",
+			timeA:    time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 2, 0, 0, 0, 0, time.UTC),
+			expected: "1 day",
+		},
+		{
+			message:  "a month",
+			timeA:    time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2017, 2, 1, 0, 0, 0, 0, time.UTC),
+			expected: "1 month",
+		},
+		{
+			message:  "a year",
+			timeA:    time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC),
+			expected: "1 year",
+		},
+		{
+			message:  "all values must have a diff of 1",
+			timeA:    time.Date(2017, 5, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 6, 2, 1, 1, 1, 1, time.UTC),
+			expected: "1 year, 1 month, 1 day, 1 hour, 1 minute, 1 second",
+		},
+		{
+			message:  "singular and plurals",
+			timeA:    time.Date(2017, 5, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2028, 6, 21, 1, 30, 2, 1, time.UTC),
+			expected: "11 years, 1 month, 20 days, 1 hour, 30 minutes, 2 seconds",
+		},
+		{
+			message:  "the limit of one month is 30 days",
+			timeA:    time.Date(2017, 1, 2, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2017, 2, 1, 0, 0, 0, 0, time.UTC),
+			expected: "30 days",
+		},
+		{
+			message:  "feb has 28 days",
+			timeA:    time.Date(2016, 2, 2, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2016, 3, 1, 0, 0, 0, 0, time.UTC),
+			expected: "28 days",
+		},
+		{
+			message:  "11 months and one day",
+			timeA:    time.Date(2018, 2, 11, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2019, 1, 12, 0, 0, 0, 0, time.UTC),
+			expected: "11 months, 1 day",
+		},
+		{
+			message:  "same 11 months and one day in a different timezone UTC-8",
+			timeA:    time.Date(2018, 2, 11, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2019, 1, 12, 0, 0, 0, 0, time.FixedZone("UTC-8", -8*60*60)),
+			expected: "11 months, 1 day, 8 hours",
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.message, func(t *testing.T) {
+			actual := timef.DiffPretty(test.timeA, test.timeB)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestDiffMatrix(t *testing.T) {
+	cases := []struct {
+		message string
+		timeA   time.Time
+		timeB   time.Time
+		// one single string that contains the returned values from the
+		// timeDiff() func format: "y, mo, d, h, mi, s"
+		expected string
+	}{
+		{
+			message:  "a second",
+			timeA:    time.Date(2018, 1, 1, 1, 1, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 1, 1, 1, 1, 0, time.UTC),
+			expected: "0 0 0 0 0 1",
+		},
+		{
+			message:  "a minute",
+			timeA:    time.Date(2018, 1, 1, 1, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 1, 1, 1, 0, 0, time.UTC),
+			expected: "0 0 0 0 1 0",
+		},
+		{
+			message:  "an hour",
+			timeA:    time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 1, 1, 0, 0, 0, time.UTC),
+			expected: "0 0 0 1 0 0",
+		},
+		{
+			message:  "a day",
+			timeA:    time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 1, 2, 0, 0, 0, 0, time.UTC),
+			expected: "0 0 1 0 0 0",
+		},
+		{
+			message:  "a month",
+			timeA:    time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2017, 2, 1, 0, 0, 0, 0, time.UTC),
+			expected: "0 1 0 0 0 0",
+		},
+		{
+			message:  "a year",
+			timeA:    time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC),
+			expected: "1 0 0 0 0 0",
+		},
+		{
+			message:  "all values must have a diff of 1",
+			timeA:    time.Date(2017, 5, 1, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2018, 6, 2, 1, 1, 1, 1, time.UTC),
+			expected: "1 1 1 1 1 1",
+		},
+		{
+			message:  "the limit of one month is 30 days",
+			timeA:    time.Date(2017, 1, 2, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2017, 2, 1, 0, 0, 0, 0, time.UTC),
+			expected: "0 0 30 0 0 0",
+		},
+		{
+			message:  "feb has 28 days",
+			timeA:    time.Date(2016, 2, 2, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2016, 3, 1, 0, 0, 0, 0, time.UTC),
+			expected: "0 0 28 0 0 0",
+		},
+		{
+			message:  "a month and one day",
+			timeA:    time.Date(2018, 2, 11, 0, 0, 0, 0, time.UTC),
+			timeB:    time.Date(2019, 1, 12, 0, 0, 0, 0, time.UTC),
+			expected: "0 11 1 0 0 0",
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.message, func(t *testing.T) {
+			y, mo, d, h, mi, s := timef.Diff(test.timeA, test.timeB)
+			actual := fmt.Sprintf("%d %d %d %d %d %d", y, mo, d, h, mi, s)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
