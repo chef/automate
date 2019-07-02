37871e1cd75db5a8e4b78d8b7f00b9c04e93cc78
Salim Afiune <afiune@chef.io>
2019-07-02 10:11:45 +0200

    revert auto-formatting as per PR review
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- components/applications-service/pkg/server/server.go
+++ components/applications-service/pkg/server/server.go
@@ -262,13 +262,14 @@ func convertStorageServicesToApplicationsServices(svcs []*storage.Service) []*ap
 	services := make([]*applications.Service, len(svcs))
 	for i, svc := range svcs {
 		services[i] = &applications.Service{
-			SupervisorId: svc.SupMemberID,
-			Release:      svc.FullReleaseString(),
-			Group:        svc.Group,
-			HealthCheck:  convertHealthStatusToProto(svc.Health),
-			Status:       applications.ServiceStatus(applications.ServiceStatus_value[svc.Status]),
-			Application:  svc.Application,
-			Environment:  svc.Environment, Fqdn: svc.Fqdn,
+			SupervisorId:        svc.SupMemberID,
+			Release:             svc.FullReleaseString(),
+			Group:               svc.Group,
+			HealthCheck:         convertHealthStatusToProto(svc.Health),
+			Status:              applications.ServiceStatus(applications.ServiceStatus_value[svc.Status]),
+			Application:         svc.Application,
+			Environment:         svc.Environment,
+			Fqdn:                svc.Fqdn,
 			Channel:             svc.Channel,
 			Site:                svc.Site,
 			PreviousHealthCheck: convertHealthStatusToProto(svc.PreviousHealth),
