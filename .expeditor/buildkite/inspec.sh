0737fe9ceb90baf659a6495a391c19a34c6aa46b
Stephan Renatus <srenatus@chef.io>
2019-07-02 19:41:35 +0200

    quickfix: inspec and saml login (#745)
    * terraform/test-envs: that one isn't set up for SAML
    * ci/inspec: skip non-saml temporarily
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- .expeditor/buildkite/inspec.sh
+++ .expeditor/buildkite/inspec.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 
 echo -e "$CHEF_CI_SSH_PRIVATE_KEY" > chef-ci-ad-ssh
 
+# TODO(sr) Until we've fixed the inspec resource to deal with a single
+# login method properly, let's skip those machines for inspec tests.
 instances_to_test=$(curl --silent "https://a2-${CHANNEL}.cd.chef.co/assets/data.json" |\
-  jq --raw-output '.[] | select(.tags | any(. == "chef-automate-cli")) | .fqdn')
+  jq --raw-output '.[] | select(.tags | any(. == "chef-automate-cli")) | select(.tags | any(. == "saml")) | .fqdn')
 
 for instance in ${instances_to_test[*]}
 do
