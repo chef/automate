6c41deceea66be698a9f5fb93e750a7aa878f80c
Stephan Renatus <srenatus@chef.io>
2019-07-02 15:40:44 +0200

    inspec.sh: bikeshed jq script
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- .expeditor/buildkite/inspec.sh
+++ .expeditor/buildkite/inspec.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 echo -e "$CHEF_CI_SSH_PRIVATE_KEY" > chef-ci-ad-ssh
 
-instances_to_test=$(curl --silent "https://a2-${CHANNEL}.cd.chef.co/assets/data.json" | jq --raw-output 'map(select(.tags[] | contains ("chef-automate-cli"))) | .[] .fqdn')
+instances_to_test=$(curl --silent "https://a2-${CHANNEL}.cd.chef.co/assets/data.json" |\
+  jq --raw-output '.[] | select(.tags | any(. == "chef-automate-cli")) | .fqdn')
 
 for instance in ${instances_to_test[*]}
 do
