67ffacdc0080e281546f928f5b77407cef41798f
Blake Johnson <bstier@chef.io>
2019-07-03 10:35:58 -0700

    Format SQL file
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/authz-service/storage/postgres/migration/sql/53_add_db_id_to_iam_statements.up.sql
+++ components/authz-service/storage/postgres/migration/sql/53_add_db_id_to_iam_statements.up.sql
@@ -5,14 +5,12 @@ ALTER TABLE iam_policy_projects
     DROP CONSTRAINT iam_policy_projects_policy_id_fkey;
 ALTER TABLE iam_policy_statements
     DROP CONSTRAINT iam_policy_statements_policy_id_fkey;
-
 ALTER TABLE iam_policies
     DROP CONSTRAINT iam_policies_pkey;
 ALTER TABLE iam_policies
     ADD COLUMN db_id SERIAL PRIMARY KEY UNIQUE;
 ALTER TABLE iam_policies
     ADD UNIQUE (id);
-
 ALTER TABLE iam_policy_members RENAME COLUMN policy_id TO policy_temp_id;
 ALTER TABLE iam_policy_members
     ADD COLUMN policy_id INTEGER REFERENCES iam_policies (db_id) ON DELETE CASCADE DEFERRABLE;
@@ -30,7 +28,6 @@ ALTER TABLE iam_policy_members
     DROP COLUMN policy_temp_id;
 ALTER TABLE iam_policy_members
     ADD UNIQUE (member_id, policy_id);
-
 ALTER TABLE iam_policy_projects RENAME COLUMN policy_id TO policy_temp_id;
 ALTER TABLE iam_policy_projects
     ADD COLUMN policy_id INTEGER REFERENCES iam_policies (db_id) ON DELETE CASCADE;
@@ -48,10 +45,8 @@ ALTER TABLE iam_policy_projects
     DROP COLUMN policy_temp_id;
 ALTER TABLE iam_policy_projects
     ADD UNIQUE (project_id, policy_id);
-
 ALTER TABLE iam_policy_statements
     ADD CONSTRAINT iam_policy_statements_policy_id_fkey FOREIGN KEY (policy_id) REFERENCES iam_policies (id) ON DELETE CASCADE DEFERRABLE;
-
 -- add helper for policy db_id lookup
 CREATE FUNCTION policy_db_id (_id TEXT)
     RETURNS INTEGER
@@ -64,7 +59,6 @@ CREATE FUNCTION policy_db_id (_id TEXT)
         id = _id;
 $$
 LANGUAGE SQL;
-
 CREATE OR REPLACE FUNCTION query_policy (_policy_id TEXT, _projects_filter TEXT[])
     RETURNS json
     AS $$
@@ -85,7 +79,7 @@ CREATE OR REPLACE FUNCTION query_policy (_policy_id TEXT, _projects_filter TEXT[
                         -- get each statement's projects by cross-referencing iam_policy_statements and iam_statement_projects
                         (
                             SELECT
-                                COALESCE(Json_agg(proj.id) FILTER (WHERE proj.id IS NOT NULL),
+                                COALESCE(json_agg(proj.id) FILTER (WHERE proj.id IS NOT NULL),
                                     '[]')
                                 FROM iam_statement_projects AS stmt_projs
                             LEFT OUTER JOIN iam_projects AS proj ON stmt_projs.statement_id = stmt.id WHERE stmt_projs.project_id = proj.id) AS projects FROM iam_policy_statements AS pol_stmts
@@ -108,7 +102,10 @@ CREATE OR REPLACE FUNCTION query_policy (_policy_id TEXT, _projects_filter TEXT[
             FROM iam_policy_projects AS pol_projs
             LEFT OUTER JOIN iam_projects AS proj ON pol_projs.project_id = proj.id WHERE pol_projs.policy_id = pol.db_id) AS projects FROM iam_policies AS pol WHERE pol.id = _policy_id
         GROUP BY
-            pol.db_id, pol.id, pol.name, pol.type
+            pol.db_id,
+            pol.id,
+            pol.name,
+            pol.type
 )
 SELECT
     json_build_object('id', temp.id, 'name', temp.name, 'type', temp.type, 'statements', COALESCE(temp.statements, '{}'), 'members', COALESCE(temp.members, '{}'), 'projects', COALESCE(temp.projects, '{}')) AS POLICY
@@ -158,7 +155,10 @@ CREATE OR REPLACE FUNCTION query_policies (_projects_filter TEXT[])
         FROM iam_policy_projects AS pol_projs
         LEFT OUTER JOIN iam_projects AS proj ON pol_projs.project_id = proj.id WHERE pol_projs.policy_id = pol.db_id) AS projects FROM iam_policies AS pol
 GROUP BY
-    pol.db_id, pol.id, pol.name, pol.type
+    pol.db_id,
+    pol.id,
+    pol.name,
+    pol.type
 )
 SELECT
     json_build_object('id', temp.id, 'name', temp.name, 'type', temp.type, 'statements', COALESCE(temp.statements, '{}'), 'members', COALESCE(temp.members, '{}'), 'projects', COALESCE(temp.projects, '{}')) AS POLICY
@@ -168,5 +168,5 @@ WHERE
     projects_match (temp.projects::TEXT[], _projects_filter);
 $$
 LANGUAGE sql;
-
 COMMIT;
+
