84d7eec2dd63cea575dd6041410f823c802ab009
Lance Finfrock <lfinfrock@chef.io>
2019-07-08 16:52:09 -0700

    Migrating data from compliance index
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- components/event-feed-service/pkg/migration/migration.go
+++ components/event-feed-service/pkg/migration/migration.go
@@ -3,6 +3,13 @@ package migration
 import (
 	"context"
 	"github.com/chef/automate/components/event-feed-service/pkg/persistence"
+	log "github.com/sirupsen/logrus"
+	"time"
+)
+
+var (
+	eventFeedZeroIndexName  = "eventfeed-0-feeds"
+	complianceFeedIndexName = "comp-2-feeds"
 )
 
 // Migrator migration state struct
@@ -21,6 +28,11 @@ func New(ctx context.Context, feedStore persistence.FeedStore) *Migrator {
 
 // InitializeStore migrate or newly initialize the data store
 func (migrator *Migrator) InitializeStore() error {
+	err := migrator.removeZeroIndexIfNeeded()
+	if err != nil {
+		return err
+	}
+
 	migrated, err := migrator.complianceMigrationIfNeeded()
 	if err != nil {
 		return err
@@ -32,11 +44,73 @@ func (migrator *Migrator) InitializeStore() error {
 	return migrator.feedStore.InitializeStore(migrator.ctx)
 }
 
+func (migrator *Migrator) removeZeroIndexIfNeeded() error {
+	hasZeroIndex, err := migrator.feedStore.DoesIndexExists(migrator.ctx, eventFeedZeroIndexName)
+	if err != nil {
+		return err
+	}
+
+	if hasZeroIndex {
+		log.Infof("Deleting %s index", eventFeedZeroIndexName)
+		err = migrator.feedStore.DeleteIndex(migrator.ctx, eventFeedZeroIndexName)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (migrator *Migrator) complianceMigrationIfNeeded() (bool, error) {
-
-	// check if we need to migrate the data
-
-	// migrate the data
+	hasComplianceFeedData, err := migrator.hasComplianceFeedData()
+	if err != nil {
+		return false, err
+	}
+	if hasComplianceFeedData {
+		err = migrator.migrateComplianceFeedToCurrent()
+		return true, err
+	}
 
 	return false, nil
+}
+
+func (migrator *Migrator) hasComplianceFeedData() (bool, error) {
+	return migrator.feedStore.DoesIndexExists(migrator.ctx, complianceFeedIndexName)
+}
+
+func (migrator *Migrator) migrateComplianceFeedToCurrent() error {
+	err := migrator.feedStore.InitializeStore(migrator.ctx)
+	if err != nil {
+		return err
+	}
+
+	log.Infof("Reindexing %s index to current", complianceFeedIndexName)
+	reindexTaskID, err := migrator.feedStore.ReindexFeedsToLatest(migrator.ctx, complianceFeedIndexName)
+	if err != nil {
+		return err
+	}
+
+	// Wait for Reindex task to complete
+	for {
+		time.Sleep(time.Millisecond * 500)
+
+		status, err := migrator.feedStore.JobStatus(migrator.ctx, reindexTaskID)
+		if err != nil {
+			return err
+		}
+
+		if status.Completed {
+			log.Infof("Reindexing is %f complete", status.PercentageComplete)
+			break
+		}
+	}
+
+	log.Infof("Deleting %s index", complianceFeedIndexName)
+	err = migrator.feedStore.DeleteIndex(migrator.ctx, complianceFeedIndexName)
+	if err != nil {
+		return err
+	}
+
+	log.Info("Migration from compliance-service finished successfully")
+	return nil
 }
--- components/event-feed-service/pkg/persistence/store.go
+++ components/event-feed-service/pkg/persistence/store.go
@@ -2,11 +2,20 @@ package persistence
 
 import (
 	"github.com/chef/automate/components/event-feed-service/pkg/util"
+	project_update_lib "github.com/chef/automate/lib/authz"
 	olivere "github.com/olivere/elastic"
 	"golang.org/x/net/context"
 )
 
 type FeedStore interface {
+	// @param (context, indexName)
+	DeleteIndex(ctx context.Context, index string) error
+	// @param (context, jobID)
+	JobStatus(context.Context, string) (project_update_lib.JobStatus, error)
+	// @param (context, indexName)
+	DoesIndexExists(context.Context, string) (bool, error)
+	// @param (context, previousIndex)
+	ReindexFeedsToLatest(context.Context, string) (string, error)
 	// @param (context)
 	InitializeStore(context.Context) error
 	CreateFeedEntry(entry *util.FeedEntry) (bool, error)
