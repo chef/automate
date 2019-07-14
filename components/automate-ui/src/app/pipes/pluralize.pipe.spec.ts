06e9172a48802ebc4bd99911e79726fc3333fdec
michael sorens <msorens@chef.io>
2019-07-14 14:49:17 -0700

    Drive-by to enhance pluralizer pipe
    A general enhancement in the spirit of this PR though not quite related
    to the rest of them;
    I had this lying around for awhile and decided to include it.
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/pipes/pluralize.pipe.spec.ts
+++ components/automate-ui/src/app/pipes/pluralize.pipe.spec.ts
@@ -17,14 +17,28 @@ describe('PluralizePipe', () => {
       ['0', 'team', 's'],
       ['2', 'fox', 'es'],
       ['-5', 'color', 's']
-    ], (value, unit, suffix, _result: string) => {
-      it('pluralizes any NON-unitary value ', () => {
+    ], (value, unit, suffix: string) => {
+      it('pluralizes NON-unitary values with additive-suffix', () => {
         expect(pipe.transform(value, unit, suffix)).toBe(value + ' ' + unit + suffix);
       });
     });
 
-  it('providing UNITARY value returns singular form', () => {
+  using([
+      ['0', 'radius', '<<i', 'radii'],
+      ['4', 'goose', '<<<<eese',  'geese'],
+      ['-5', 'policy', '<ies', 'policies']
+    ], (value, unit, suffix, result: string) => {
+      it('pluralizes NON-unitary values with subtractive-suffix', () => {
+        expect(pipe.transform(value, unit, suffix)).toBe(value + ' ' + result);
+      });
+    });
+
+  it('providing UNITARY value with additive-suffix returns singular form', () => {
     expect(pipe.transform('1', 'team', 's' )).toBe('1 team');
+  });
+
+  it('providing UNITARY value with subtractive-suffix returns singular form', () => {
+    expect(pipe.transform('1', 'policy', '<ies' )).toBe('1 policy');
   });
 
 });
