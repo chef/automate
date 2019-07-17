f5988bcf50eed65112052f0f7d5ab254f6b4d587
Stephan Renatus <srenatus@chef.io>
2019-07-17 14:04:22 +0200

    iam_v2_only_diagnostics: remove cruft, enable upgrades
    Is it that easy? We'll see.
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- integration/tests/iam_v2_only_diagnostics.sh
+++ integration/tests/iam_v2_only_diagnostics.sh
@@ -4,38 +4,8 @@
 test_name="iam v2 only (diagnostics)"
 # Note: we can't run diagnostics AND inspec, so no inspec here
 #shellcheck disable=SC2034
-test_deploy_inspec_profiles=()
+test_upgrades=true
 test_diagnostics_filters="~iam-v1"
-
-do_setup() {
-    do_setup_default
-
-    # We are defaulting to a umask of 077 to test
-    # installations on systems that are super locked down.
-    # Briefly override that strict default so we can install
-    # packages that non-root users can use (like the hab user
-    # for health checks or this script).
-    local previous_umask
-    previous_umask=$(umask)
-    umask 022
-
-    hab pkg install core/curl
-    hab pkg install -b core/jo
-
-    umask "$previous_umask"
-}
-
-hab_curl() {
-    hab pkg exec core/curl curl "$@"
-}
-
-# Note: the inspec tests assert that a viewer or operator user is able
-# to retrieve the license status -- so we just add one.
-do_deploy() {
-    do_deploy_default
-    log_info "applying dev license"
-    chef-automate license apply "$A2_LICENSE"
-}
 
 do_test_deploy() {
     log_info "run chef-automate iam upgrade-to-v2 --skip-policy-migration"
