59fd441e07c433d5c83f7bf4569326f99a3b8ec1
Stephan Renatus <srenatus@chef.io>
2019-07-17 10:35:55 +0200

    integration: add IAM v2 diagnostics run
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

---
+++ integration/tests/iam_v2_only_diagnostics.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#shellcheck disable=SC2034
+test_name="iam v2 only (diagnostics)"
+# Note: we can't run diagnostics AND inspec, so no inspec here
+#shellcheck disable=SC2034
+test_deploy_inspec_profiles=()
+
+do_setup() {
+    do_setup_default
+
+    # We are defaulting to a umask of 077 to test
+    # installations on systems that are super locked down.
+    # Briefly override that strict default so we can install
+    # packages that non-root users can use (like the hab user
+    # for health checks or this script).
+    local previous_umask
+    previous_umask=$(umask)
+    umask 022
+
+    hab pkg install core/curl
+    hab pkg install -b core/jo
+
+    umask "$previous_umask"
+}
+
+hab_curl() {
+    hab pkg exec core/curl curl "$@"
+}
+
+# Note: the inspec tests assert that a viewer or operator user is able
+# to retrieve the license status -- so we just add one.
+do_deploy() {
+    do_deploy_default
+    log_info "applying dev license"
+    chef-automate license apply "$A2_LICENSE"
+}
+
+do_test_deploy() {
+    log_info "run chef-automate iam upgrade-to-v2 --skip-policy-migration"
+    chef-automate iam upgrade-to-v2 --skip-policy-migration || return 1
+
+    # ensure service startup works with IAM v2:
+    # - kill authz-service to force startup,
+    # - wait for service status to be healthy again
+    log_info "restarting authz-service, waiting 5s for it to come up again"
+    pkill -f authz-service && sleep 5 && chef-automate status -w || return 1
+
+    log_info "creating test users with automate-cli"
+    chef-automate dev create-iam-dev-users || return 1
+    do_test_deploy_default
+}
