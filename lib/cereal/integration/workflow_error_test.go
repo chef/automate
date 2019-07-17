3f068f1e273d37fe3418f9e9a044ffe9f6f59e7f
Steven Danna <steve@chef.io>
2019-07-17 23:58:05 +0100

    [lib/cereal] Handle a few more TODOs
    - Return, thus canceling the transaction and rolling back the workflow
      on Enqueue Task failures.
    
    - Fail rather than Complete workflow on jsonify error
    
    - Fail workflow when EnqueueTask and Complete have been called in the
      same workflow step.
    
    I wrote tests for 2 out of 3 of these cases. We really need a good way
    to inject postgresql failures.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- lib/cereal/integration/workflow_error_test.go
+++ lib/cereal/integration/workflow_error_test.go
@@ -41,7 +41,74 @@ func (suite *CerealTestSuite) TestWorkflowFail() {
 	suite.NoError(err)
 	suite.Error(w.Err())
 	suite.False(w.IsRunning())
-	suite.Equal(w.Err().Error(), "expected test error")
+	suite.Equal("expected test error", w.Err().Error())
+	err = m.Stop()
+	suite.NoError(err)
+}
+
+func (suite *CerealTestSuite) TestWorkflowFailOnBadEnqueue() {
+	workflowName := randName("failing")
+	instanceName := randName("instance")
+
+	doneChan := make(chan struct{})
+	m := suite.newManager(
+		WithWorkflowExecutor(
+			workflowName,
+			&workflowExecutorWrapper{
+				onStart: func(w cereal.WorkflowInstance, ev cereal.StartEvent) cereal.Decision {
+					close(doneChan)
+					w.EnqueueTask("foo", nil)
+					return w.Complete(nil)
+				},
+				onTaskComplete: func(w cereal.WorkflowInstance, ev cereal.TaskCompleteEvent) cereal.Decision {
+					return w.Complete()
+				},
+			},
+		),
+	)
+	defer m.Stop()
+	err := m.EnqueueWorkflow(context.Background(), workflowName, instanceName, nil)
+	suite.Require().NoError(err, "Failed to enqueue workflow")
+	<-doneChan
+	time.Sleep(20 * time.Millisecond)
+	w, err := m.GetWorkflowInstanceByName(context.Background(), instanceName, workflowName)
+	suite.NoError(err)
+	suite.Error(w.Err())
+	suite.False(w.IsRunning())
+	suite.Equal("EnqueueTask and Complete called in same workflow step", w.Err().Error())
+	err = m.Stop()
+	suite.NoError(err)
+}
+
+func (suite *CerealTestSuite) TestWorkflowFailOnUnmarshableJSON() {
+	workflowName := randName("failing")
+	instanceName := randName("instance")
+
+	doneChan := make(chan struct{})
+	m := suite.newManager(
+		WithWorkflowExecutor(
+			workflowName,
+			&workflowExecutorWrapper{
+				onStart: func(w cereal.WorkflowInstance, ev cereal.StartEvent) cereal.Decision {
+					close(doneChan)
+					return w.Continue(doneChan) // channels can't be marshalled
+				},
+				onTaskComplete: func(w cereal.WorkflowInstance, ev cereal.TaskCompleteEvent) cereal.Decision {
+					return w.Complete()
+				},
+			},
+		),
+	)
+	defer m.Stop()
+	err := m.EnqueueWorkflow(context.Background(), workflowName, instanceName, nil)
+	suite.Require().NoError(err, "Failed to enqueue workflow")
+	<-doneChan
+	time.Sleep(20 * time.Millisecond)
+	w, err := m.GetWorkflowInstanceByName(context.Background(), instanceName, workflowName)
+	suite.NoError(err)
+	suite.Error(w.Err())
+	suite.False(w.IsRunning())
+	suite.Equal("json: unsupported type: chan struct {}", w.Err().Error())
 	err = m.Stop()
 	suite.NoError(err)
 }
