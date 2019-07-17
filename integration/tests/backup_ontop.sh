a5a8b58b951926f4e5977cbe28b4f56635ccbe62
Stephan Renatus <srenatus@chef.io>
2019-07-17 11:04:32 +0200

    backup_ontop.sh: skip iam-v2, too
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- integration/tests/backup_ontop.sh
+++ integration/tests/backup_ontop.sh
@@ -8,7 +8,7 @@ test_diagnostics_filters="~iam-v2"
 do_prepare_restore() {
     # Run the diagnostics command again. We'll verify that this does not pass
     #shellcheck disable=SC2154
-    chef-automate diagnostics run ~remove-this-tag-after-merge --lb-url "$test_loadbalancer_url" --skip-cleanup --save-file "/tmp/context2"
+    chef-automate diagnostics run $test_diagnostics_filters ~remove-this-tag-after-merge --lb-url "$test_loadbalancer_url" --skip-cleanup --save-file "/tmp/context2"
 }
 
 do_restore() {
@@ -23,7 +23,7 @@ do_restore() {
 do_test_restore() {
     do_test_restore_default
 
-    if chef-automate diagnostics run ~remove-this-tag-after-merge --skip-generate --skip-cleanup --save-file "/tmp/context2" >/tmp/context2runlog 2>&1;
+    if chef-automate diagnostics run $test_diagnostics_filters ~remove-this-tag-after-merge --skip-generate --skip-cleanup --save-file "/tmp/context2" >/tmp/context2runlog 2>&1;
     then
         log_error "Expected post-restore diagnostics run to fail when using a context created before the backup was taken."
         log_error "diagnostics run output:"
