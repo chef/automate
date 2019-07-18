1c35943667654c3680366351deb800a128e187ba
michael sorens <msorens@chef.io>
2019-07-17 20:56:38 -0700

    Import fresh wallaby.js
    Commit 060eccd8d6e72997b769e7ddb8570d50e310d43b
    from https://github.com/wallabyjs/ngCliWebpackSample/o
    dated 2019-07-09
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/wallaby.js
+++ components/automate-ui/wallaby.js
@@ -1,93 +1,148 @@
-// This "config" file comes from https://github.com/wallabyjs/ngCliWebpackSample/ .
-// There are only a couple tweaks needed for Automate-UI, noted inline.
-
-var wallabyWebpack = require('wallaby-webpack');
-var path = require('path');
-
-var compilerOptions = Object.assign(
-  require('./tsconfig.json').compilerOptions,
-  require('./src/tsconfig.spec.json').compilerOptions);
-
-compilerOptions.module = 'CommonJs';
+// This "config" file comes from https://github.com/wallabyjs/ngCliWebpackSample/
 
 module.exports = function (wallaby) {
+  const wallabyWebpack = require('wallaby-webpack');
+  const path = require('path');
+  const fs = require('fs');
 
-  var webpackPostprocessor = wallabyWebpack({
-    entryPatterns: [
-      'src/wallabyTest.js',
-      'src/**/*spec.js'
-    ],
+  const specPattern = '/**/*spec.ts';
+  const angularConfig = require('./angular.json');
 
-    module: {
-      rules: [
-        {test: /\.css$/, loader: ['raw-loader']},
-        {test: /\.html$/, loader: 'raw-loader'},
-        {test: /\.ts$/, loader: '@ngtools/webpack', include: /node_modules/, query: {tsConfigPath: 'tsconfig.json'}},
-        {test: /\.js$/, loader: 'angular2-template-loader', exclude: /node_modules/},
-        {test: /\.styl$/, loaders: ['raw-loader', 'stylus-loader']},
-        {test: /\.less$/, loaders: ['raw-loader', {loader: 'less-loader', options: {paths: [__dirname]}}]},
-        {test: /\.scss$|\.sass$/, loaders: ['raw-loader', 'sass-loader']},
-        {test: /\.(jpg|png|svg)$/, loader: 'url-loader?limit=128000'}
-      ]
-    },
+  const projects = Object.keys(angularConfig.projects).map(key => {
+    return { name: key, ...angularConfig.projects[key] };
+  }).filter(project => project.sourceRoot);
 
-    resolve: {
-      extensions: ['.js', '.ts'],
-      modules: [
-        path.join(wallaby.projectCacheDir, 'src/app'),
-        path.join(wallaby.projectCacheDir, 'src'),
-        'node_modules'
-      ]
-    },
-    node: {
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty',
-      dns: 'empty'
-    }
-  });
+  const applications = projects.filter(project => project.projectType === 'application');
+  const libraries = projects.filter(project => project.projectType === 'library');
+
+  const tsConfigFile = projects
+    .map(project => path.join(__dirname, project.root, 'tsconfig.spec.json'))
+    .find(tsConfig => fs.existsSync(tsConfig));
+
+  const tsConfigSpec = tsConfigFile ? JSON.parse(fs.readFileSync(tsConfigFile)) : {};
+
+  const compilerOptions = Object.assign(require('./tsconfig.json').compilerOptions, tsConfigSpec.compilerOptions);
 
   return {
     files: [
-      {pattern: 'src/**/*.+(ts|css|less|scss|sass|styl|html|json|svg)', load: false},
-      // 2017.09.30 Automate-UI: Added json-tree item to this list
-      {pattern: 'src/app/page-components/json-tree/vendor/**/*.*', load: false, instrument: false},
-      {pattern: 'src/**/*.d.ts', ignore: true},
-      {pattern: 'src/**/*spec.ts', ignore: true}
+      { pattern: path.basename(__filename), load: false, instrument: false },
+      ...projects.map(project => ({
+        pattern: project.sourceRoot + '/**/*.+(ts|js|css|less|scss|sass|styl|html|json|svg)',
+        load: false
+      })),
+      ...projects.map(project => ({
+        pattern: project.sourceRoot + specPattern,
+        ignore: true
+      })),
+      { pattern: './**/*.d.ts', ignore: true }
     ],
 
     tests: [
-      {pattern: 'src/**/*spec.ts', load: false},
-      {pattern: 'src/**/*e2e-spec.ts', ignore: true}
+      ...projects.map(project => ({
+        pattern: project.sourceRoot + specPattern,
+        load: false
+      }))
     ],
 
     testFramework: 'jasmine',
 
     compilers: {
-      '**/*.ts': wallaby.compilers.typeScript(compilerOptions)
+      '**/*.ts': wallaby.compilers.typeScript({
+        ...compilerOptions,
+        getCustomTransformers: program => {
+          return {
+            before: [
+              require('@ngtools/webpack/src/transformers/replace_resources').replaceResources(
+                path => true,
+                () => program.getTypeChecker(),
+                false
+              )
+            ]
+          };
+        }
+      })
+    },
+
+    preprocessors: {
+      /* Initialize Test Environment for Wallaby */
+      [path.basename(__filename)]: file => `
+ import '@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills';
+ import 'zone.js/dist/zone-testing';
+ import { getTestBed } from '@angular/core/testing';
+ import { BrowserDynamicTestingModule,  platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
+
+ getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());`
     },
 
     middleware: function (app, express) {
-      var path = require('path');
-      app.use('/favicon.ico', express.static(path.join(__dirname, 'src/favicon.ico')));
-      app.use('/assets', express.static(path.join(__dirname, 'src/assets')));
+      const path = require('path');
+
+      applications.forEach(application => {
+        if (
+          !application.architect ||
+          !application.architect.test ||
+          !application.architect.test.options ||
+          !application.architect.test.options.assets
+        ) {
+          return;
+        }
+
+        application.architect.test.options.assets.forEach(asset => {
+          app.use(asset.slice(application.sourceRoot.length), express.static(path.join(__dirname, asset)));
+        });
+      });
     },
 
     env: {
-      // 2017.09.30 Automate-UI: Set the test-runner to use chrome headless runner
-      // instead of the default electron, since "ng test" also uses chrome headless.
-      // Note that it is specified as just "chrome" here unlike in karma.conf.js,
-      // and that wallaby passes in a --headless flag by itself.
       kind: 'chrome'
     },
 
-    postprocessor: webpackPostprocessor,
+    postprocessor: wallabyWebpack({
+      entryPatterns: [
+        ...applications
+          .map(project => project.sourceRoot + '/polyfills.js')
+          .filter(polyfills => fs.existsSync(path.join(__dirname, polyfills.replace(/js$/, 'ts')))),
+        path.basename(__filename),
+        ...projects.map(project => project.sourceRoot + specPattern.replace(/ts$/, 'js'))
+      ],
+
+      module: {
+        rules: [
+          { test: /\.css$/, loader: ['raw-loader'] },
+          { test: /\.html$/, loader: 'raw-loader' },
+          {
+            test: /\.ts$/,
+            loader: '@ngtools/webpack',
+            include: /node_modules/,
+            query: { tsConfigPath: 'tsconfig.json' }
+          },
+          { test: /\.styl$/, loaders: ['raw-loader', 'stylus-loader'] },
+          { test: /\.less$/, loaders: ['raw-loader', { loader: 'less-loader' }] },
+          {
+            test: /\.scss$|\.sass$/,
+            loaders: [{ loader: 'raw-loader' }, { loader: 'sass-loader', options: { implementation: require('sass') } }]
+          },
+          { test: /\.(jpg|png|svg)$/, loader: 'raw-loader' }
+        ]
+      },
+
+      resolve: {
+        extensions: ['.js', '.ts'],
+        modules: [
+          __dirname,
+          'node_modules',
+          ...(projects.length ? projects.filter(project => project.root).map(project => project.root) : []),
+          ...(projects.length ? projects.filter(project => project.sourceRoot).map(project => project.sourceRoot) : [])
+        ],
+        alias: libraries.reduce((result, project) => {
+          result[project.name] = path.join(wallaby.projectCacheDir, project.sourceRoot, 'public-api');
+          return result;
+        }, {})
+      }
+    }),
 
     setup: function () {
       window.__moduleBundler.loadTests();
-    },
-
-    // 2019.01.07 Automate-UI: turn down verbosity in Wallaby.js Console
-    debug: false
+    }
   };
 };
