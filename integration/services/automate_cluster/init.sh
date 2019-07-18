5c925b0ec2cdf45ad5f5c491e332198b95e840c0
Stephan Renatus <srenatus@chef.io>
2019-07-18 14:00:33 +0200

    integration/services/*/init.sh: fix shellcheck bits
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- integration/services/automate_cluster/init.sh
+++ integration/services/automate_cluster/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AUTOMATE_CLUSTER_DIR=$(dirname ${BASH_SOURCE[0]})
+AUTOMATE_CLUSTER_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 automate_cluster_container1=$(service_container_name "automate_cluster_1")
 automate_cluster_container2=$(service_container_name "automate_cluster_2")
@@ -11,17 +11,18 @@ automate_cluster_private=$(service_config_path "automate_cluster_private")
 automate_cluster_setup() {
     echo "Automate cluster directory: $automate_cluster_private"
 
-    mkdir -p $automate_cluster_private
+    mkdir -p "$automate_cluster_private"
 
     curl https://packages.chef.io/files/dev/automate/latest/chef-automate_linux_amd64.zip | gunzip - > \
         "$automate_cluster_private/chef-automate" && chmod +x "$automate_cluster_private/chef-automate"
 
-    docker_run $automate_cluster_container1
-    local automate_cluster_container1_ip=$(container_ip $automate_cluster_container1)
+    local automate_cluster_container1_ip automate_cluster_container2_ip
+    docker_run "$automate_cluster_container1"
+    automate_cluster_container1_ip=$(container_ip "$automate_cluster_container1")
     log_info "Launched $automate_cluster_container1 with ip ${automate_cluster_container1_ip}"
 
-    docker_run $automate_cluster_container2
-    local automate_cluster_container2_ip=$(container_ip $automate_cluster_container2)
+    docker_run "$automate_cluster_container2"
+    automate_cluster_container2_ip=$(container_ip "$automate_cluster_container2")
     log_info "Launched $automate_cluster_container2 with ip ${automate_cluster_container2_ip}"
 
     #hab pkg exec core/openssl openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
@@ -36,8 +37,8 @@ automate_cluster_setup() {
     docker cp "$AUTOMATE_CLUSTER_DIR/a2_setup.sh" "${automate_cluster_container2}:/a2_setup.sh"
     #docker cp "$AUTOMATE_CLUSTER_DIR/nginx_setup.sh" "${automate_cluster_container_nginx}:/nginx_setup.sh"
 
-    docker exec -t $automate_cluster_container1 /a2_setup.sh "$automate_cluster_container1_ip"
-    docker exec -t $automate_cluster_container2 /a2_setup.sh "$automate_cluster_container2_ip"
+    docker exec -t "$automate_cluster_container1" /a2_setup.sh "$automate_cluster_container1_ip"
+    docker exec -t "$automate_cluster_container2" /a2_setup.sh "$automate_cluster_container2_ip"
     #docker exec -t $automate_cluster_container_nginx /nginx_setup.sh "$automate_cluster_container_nginx_ip"
 }
 
--- integration/services/service_a/init.sh
+++ integration/services/service_a/init.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-SERVICE_A_DIR=$(dirname ${BASH_SOURCE[0]})
+SERVICE_A_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 service_a_container1=$(service_container_name "service_a_1")
 service_a_container2=$(service_container_name "service_a_2")
 service_a_config=$(service_config_path "service_a")
 
 service_a_setup() {
-    mkdir -p $(dirname $service_a_config)
-    docker_run $service_a_container1
+    mkdir -p "$(dirname "$service_a_config")"
+    docker_run "$service_a_container1"
     docker cp "$SERVICE_A_DIR/setup.sh" "${service_a_container1}:/setup.sh"
-    docker exec $service_a_container1 /setup.sh
-    log_info "Launched $service_a_container1 with ip $(container_ip $service_a_container1)"
-    docker_run $service_a_container2
-    log_info "Launched $service_a_container2 with ip $(container_ip $service_a_container2)"
-    cat <<DOC > $service_a_config
-hello
-DOC
-
+    docker exec "$service_a_container1" /setup.sh
+    log_info "Launched $service_a_container1 with ip $(container_ip "$service_a_container1")"
+    docker_run "$service_a_container2"
+    log_info "Launched $service_a_container2 with ip $(container_ip "$service_a_container2")"
+    echo "hello" > "$service_a_config"
 }
 
 service_a_dump_logs() {
