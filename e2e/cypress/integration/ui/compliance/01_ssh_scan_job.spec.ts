bee796a4d3c518bbc8c3b5ba344b2f9a41fd6848
Victoria Jeffrey <vjeffrey@chef.io>
2019-07-25 11:51:23 -0600

    decode key in cypress
    Signed-off-by: Victoria Jeffrey <vjeffrey@chef.io>

--- e2e/cypress/integration/ui/compliance/01_ssh_scan_job.spec.ts
+++ e2e/cypress/integration/ui/compliance/01_ssh_scan_job.spec.ts
@@ -1,3 +1,5 @@
+import { Base64 } from 'js-base64';
+
 describe('create a manual node ssh scan job and cleanup after', () => {
   before(() => {
     cy.adminLogin('/');
@@ -13,6 +15,8 @@ describe('create a manual node ssh scan job and cleanup after', () => {
   const credName = 'a test ' + nowTime;
   const nodePrefix = '0-' + nowTime;
   const jobName = 'job ' + nowTime;
+  const decoded = Base64.decode(Cypress.env('AUTOMATE_ACCEPTANCE_TARGET_KEY'))
+
   it('can create a credential with a username and key', () => {
     // navigate to credentials create page:
     // click on settings
@@ -38,7 +42,7 @@ describe('create a manual node ssh scan job and cleanup after', () => {
       cy.get('form input[formcontrolname="username"]').first()
         .type(Cypress.env('AUTOMATE_ACCEPTANCE_TARGET_USER'));
       cy.get('form textarea[formcontrolname="key"]')
-        .type(Cypress.env('AUTOMATE_ACCEPTANCE_TARGET_KEY'));
+        .type(decoded);
 
       // we save the route that will be called when we navigate to the page
       // in order to be able to wait for it later
