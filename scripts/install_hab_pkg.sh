5c41fed33fa73955944b5f29e97fce369ca2ab77
Stephan Renatus <srenatus@chef.io>
2019-07-30 11:32:57 +0200

    Update scripts/install_hab_pkg.sh
    Co-Authored-By: Steven Danna <steve@chef.io>
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- scripts/install_hab_pkg.sh
+++ scripts/install_hab_pkg.sh
@@ -5,5 +5,5 @@ export HAB_NONINTERACTIVE=true
 export HAB_NOCOLORING=true
 export HAB_LICENSE="accept-no-persist"
 
-ident="${1:-ident required}"
+ident="${1?ident required}"
 hab pkg install -b "$ident"
