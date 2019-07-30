47e368a74f3222fef6373b1b39346c0f3483228c
Blake Johnson <bstier@chef.io>
2019-07-30 10:14:22 -0700

    Rules are a nested resource under projects
    Updates endpoints to be more restful
    If a user has permission to update a project,
    that user should have permission to modify rules.

--- components/automate-gateway/api/iam/v2beta/rules.pb.gw.go
+++ components/automate-gateway/api/iam/v2beta/rules.pb.gw.go
@@ -41,6 +41,24 @@ func request_Rules_CreateRule_0(ctx context.Context, marshaler runtime.Marshaler
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["project_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "project_id")
+	}
+
+	protoReq.ProjectId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "project_id", err)
+	}
+
 	msg, err := client.CreateRule(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
@@ -64,6 +82,17 @@ func request_Rules_UpdateRule_0(ctx context.Context, marshaler runtime.Marshaler
 		err error
 		_   = err
 	)
+
+	val, ok = pathParams["project_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "project_id")
+	}
+
+	protoReq.ProjectId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "project_id", err)
+	}
 
 	val, ok = pathParams["id"]
 	if !ok {
@@ -92,6 +121,17 @@ func request_Rules_GetRule_0(ctx context.Context, marshaler runtime.Marshaler, c
 		_   = err
 	)
 
+	val, ok = pathParams["project_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "project_id")
+	}
+
+	protoReq.ProjectId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "project_id", err)
+	}
+
 	val, ok = pathParams["id"]
 	if !ok {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
@@ -104,15 +144,6 @@ func request_Rules_GetRule_0(ctx context.Context, marshaler runtime.Marshaler, c
 	}
 
 	msg, err := client.GetRule(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func request_Rules_ListRules_0(ctx context.Context, marshaler runtime.Marshaler, client RulesClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq request.ListRulesReq
-	var metadata runtime.ServerMetadata
-
-	msg, err := client.ListRules(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
@@ -154,6 +185,17 @@ func request_Rules_DeleteRule_0(ctx context.Context, marshaler runtime.Marshaler
 		err error
 		_   = err
 	)
+
+	val, ok = pathParams["project_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "project_id")
+	}
+
+	protoReq.ProjectId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "project_id", err)
+	}
 
 	val, ok = pathParams["id"]
 	if !ok {
@@ -296,26 +338,6 @@ func RegisterRulesHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
-	mux.Handle("GET", pattern_Rules_ListRules_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_Rules_ListRules_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_Rules_ListRules_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("GET", pattern_Rules_ListRulesForProject_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -420,17 +442,15 @@ func RegisterRulesHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Rules_CreateRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"iam", "v2beta", "rules"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Rules_CreateRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"iam", "v2beta", "projects", "project_id", "rules"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Rules_UpdateRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"iam", "v2beta", "rules", "id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Rules_UpdateRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"iam", "v2beta", "projects", "project_id", "rules", "id"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Rules_GetRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"iam", "v2beta", "rules", "id"}, "", runtime.AssumeColonVerbOpt(true)))
-
-	pattern_Rules_ListRules_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"iam", "v2beta", "rules"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Rules_GetRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"iam", "v2beta", "projects", "project_id", "rules", "id"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Rules_ListRulesForProject_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"iam", "v2beta", "projects", "id", "rules"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Rules_DeleteRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"iam", "v2beta", "rules", "id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Rules_DeleteRule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"iam", "v2beta", "projects", "project_id", "rules", "id"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Rules_ApplyRulesStart_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"iam", "v2beta", "apply-rules"}, "", runtime.AssumeColonVerbOpt(true)))
 
@@ -445,8 +465,6 @@ var (
 	forward_Rules_UpdateRule_0 = runtime.ForwardResponseMessage
 
 	forward_Rules_GetRule_0 = runtime.ForwardResponseMessage
-
-	forward_Rules_ListRules_0 = runtime.ForwardResponseMessage
 
 	forward_Rules_ListRulesForProject_0 = runtime.ForwardResponseMessage
 
