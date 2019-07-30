b27eb5b0406f8e37102358721279d3f151711160
Stephan Renatus <srenatus@chef.io>
2019-07-30 15:01:47 +0200

    authz-service/datamigrations: update for column removal
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- components/authz-service/storage/postgres/datamigration/sql/04_v2_set_projects_default.up.sql
+++ components/authz-service/storage/postgres/datamigration/sql/04_v2_set_projects_default.up.sql
@@ -1,15 +1,15 @@
 BEGIN;
 
-INSERT INTO iam_projects (id, name, type, projects)
-    VALUES ('(unassigned)', 'Unassigned', 'chef-managed', '{(unassigned)}')
+INSERT INTO iam_projects (id, name, type)
+    VALUES ('(unassigned)', 'Unassigned', 'chef-managed')
     ON CONFLICT DO NOTHING;
 
-/* Define the 'all projects' project */
-INSERT INTO iam_projects (id, name, type, projects)
-    VALUES ('~~ALL-PROJECTS~~', 'All Projects', 'chef-managed', '{~~ALL-PROJECTS~~}')
+-- Define the 'all projects' project
+INSERT INTO iam_projects (id, name, type)
+    VALUES ('~~ALL-PROJECTS~~', 'All Projects', 'chef-managed')
     ON CONFLICT DO NOTHING;
 
-/* For any statements lacking projects, provide the default of 'all projects' */
+-- For any statements lacking projects, provide the default of 'all projects'
 INSERT INTO iam_statement_projects (statement_id, project_id)
     SELECT db_id, project_db_id('~~ALL-PROJECTS~~') FROM iam_statements
     WHERE db_id NOT IN (SELECT statement_id FROM iam_statement_projects);
