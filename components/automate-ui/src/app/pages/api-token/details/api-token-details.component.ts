b7c8d3d8f8ace65f9d9014ecfbf66f0529671143
michael sorens <msorens@chef.io>
2019-07-31 15:02:27 -0700

    Reformat entire doc to align line indents
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/pages/api-token/details/api-token-details.component.ts
+++ components/automate-ui/src/app/pages/api-token/details/api-token-details.component.ts
@@ -27,71 +27,74 @@ export class ApiTokenDetailsComponent implements OnInit, OnDestroy {
   public saveInProgress = false;
   public firstLoad = true;
 
-  constructor(private store: Store<NgrxStateAtom>,
-              fb: FormBuilder
-              ) {
-      this.updateForm = fb.group({
-        // Must stay in sync with error checks in api-token-details.component.html
-        name: ['', [Validators.required, Validators.pattern(Regex.patterns.NON_BLANK)]],
-        status: ['active', Validators.required]
+  constructor(
+    private store: Store<NgrxStateAtom>,
+    fb: FormBuilder
+  ) {
+    this.updateForm = fb.group({
+      // Must stay in sync with error checks in api-token-details.component.html
+      name: ['', [Validators.required, Validators.pattern(Regex.patterns.NON_BLANK)]],
+      status: ['active', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.store.pipe(
+      select(apiTokenFromRoute),
+      filter(identity),
+      takeUntil(this.isDestroyed))
+      .subscribe((state) => {
+        this.token = { ...state };
+        this.updateForm.controls.name.setValue(this.token.name);
+        this.status = this.token.active ? 'active' : 'inactive';
+        this.updateForm.controls.status.setValue(this.status);
       });
+
+    this.store.pipe(
+      select(routeParams),
+      pluck('id'),
+      filter(identity),
+      takeUntil(this.isDestroyed))
+      .subscribe((id: string) => {
+        this.store.dispatch(new GetToken({ id }));
+      });
+  }
+
+  ngOnDestroy() {
+    this.isDestroyed.next(true);
+    this.isDestroyed.complete();
+  }
+
+  public handleChange(): void {
+    if (this.nameCtrl.value === this.token.name) {
+      this.nameCtrl.markAsPristine();
     }
+  }
 
-    ngOnInit(): void {
-      this.store.pipe(
-        select(apiTokenFromRoute),
-        filter(identity),
-        takeUntil(this.isDestroyed))
-        .subscribe((state) => {
-          this.token = { ...state };
-          this.updateForm.controls.name.setValue(this.token.name);
-          this.status = this.token.active ? 'active' : 'inactive';
-          this.updateForm.controls.status.setValue(this.status);
-        });
+  public saveChange(): void {
+    this.firstLoad = false;
+    this.saveInProgress = true;
+    const name: string = this.updateForm.controls.name.value.trim();
+    const active: boolean = this.updateForm.controls.status.value === 'active';
+    const token: ApiToken = { ...this.token, name, active };
+    this.store.dispatch(new UpdateToken({ token }));
 
-      this.store.pipe(
-        select(routeParams),
-        pluck('id'),
-        filter(identity),
-        takeUntil(this.isDestroyed))
-        .subscribe((id: string) => {
-          this.store.dispatch(new GetToken({ id }));
-        });
-    }
+    const pendingSave = new Subject<boolean>();
+    this.store.pipe(
+      select(updateStatus),
+      filter(identity),
+      takeUntil(pendingSave))
+      .subscribe((state) => {
+        if (!loading(state)) {
+          pendingSave.next(true);
+          pendingSave.complete();
+          this.saveInProgress = false;
+          this.updateForm.reset({ name, status: active ? 'active' : 'inactive' });
+        }
+      });
+  }
 
-    ngOnDestroy() {
-      this.isDestroyed.next(true);
-      this.isDestroyed.complete();
-    }
-
-    public handleChange(): void {
-      if (this.nameCtrl.value === this.token.name) { this.nameCtrl.markAsPristine(); }
-    }
-
-    public saveChange(): void {
-      this.firstLoad = false;
-      this.saveInProgress = true;
-      const name: string = this.updateForm.controls.name.value.trim();
-      const active: boolean = this.updateForm.controls.status.value === 'active';
-      const token: ApiToken = { ...this.token, name, active  };
-      this.store.dispatch(new UpdateToken({ token }));
-
-      const pendingSave = new Subject<boolean>();
-      this.store.pipe(
-        select(updateStatus),
-        filter(identity),
-        takeUntil(pendingSave))
-        .subscribe((state) => {
-          if (!loading(state)) {
-            pendingSave.next(true);
-            pendingSave.complete();
-            this.saveInProgress = false;
-            this.updateForm.reset({ name, status: active ? 'active' : 'inactive' });
-          }
-        });
-    }
-
-    get nameCtrl(): FormControl {
-      return <FormControl>this.updateForm.controls.name;
-    }
+  get nameCtrl(): FormControl {
+    return <FormControl>this.updateForm.controls.name;
+  }
 }
