272cbc9078c193ee229a7a4fbc7246d4cb6dce0c
michael sorens <msorens@chef.io>
2019-07-30 18:56:34 -0700

    Instrument chef-form-field to watch its input
    Rather than having to manually intstrument every input to watch for
    its value being edited so that it matches
    its original value, let's bake that in to the component.
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/components/form-field/form-field.component.spec.ts
+++ components/automate-ui/src/app/components/form-field/form-field.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormFieldComponent } from './form-field.component';
+import { FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
 
 describe('FormFieldComponent', () => {
   let component: FormFieldComponent;
@@ -22,4 +24,124 @@ describe('FormFieldComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+});
+
+@Component({
+  template: '<chef-form-field></chef-form-field>'
+})
+class TestEmptyHostComponent {
+  myWatch: string;
+  myControl: FormControl;
+}
+describe('FormFieldComponent without watched control', () => {
+  let component: TestEmptyHostComponent;
+  let fixture: ComponentFixture<TestEmptyHostComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [FormFieldComponent, TestEmptyHostComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestEmptyHostComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('can create chef-form-field without a watched control', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+@Component({
+  template: '<chef-form-field [watch]="myControl.value" [control]="myControl"></chef-form-field>'
+})
+class TestHostComponent {
+  myWatch: string;
+  myControl: FormControl;
+}
+
+describe('FormFieldComponent with watched control', () => {
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [FormFieldComponent, TestHostComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestHostComponent);
+    component = fixture.componentInstance;
+    component.myControl = new FormControl();
+    spyOn(component.myControl, 'markAsPristine');
+  });
+
+  it('can create chef-form-field with a watched control', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('setting watched form control to first value marks it pristine', () => {
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(0);
+    component.myControl.setValue('foo');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+  });
+
+   it('setting watched form control to different value does not mark it pristine', () => {
+    component.myControl.setValue('foo');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+
+    component.myControl.setValue('bar');
+    fixture.detectChanges();
+    // not called again, thus dirty
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+  });
+
+    it('setting watched form control to original value marks it pristine again', () => {
+    component.myControl.setValue('foo');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+
+    component.myControl.setValue('bar');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+
+    component.myControl.setValue('foo');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(2);
+  });
+
+  it('setting watched form control to empty does not mark it pristine', () => {
+    component.myControl.setValue('foo');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+
+    component.myControl.setValue('bar');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+
+    component.myControl.setValue('');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+  });
+
+  it('setting watched form control to non-empty after empty does not mark it pristine', () => {
+    component.myControl.setValue('foo');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+
+    component.myControl.setValue('');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+
+    component.myControl.setValue('non-empty');
+    fixture.detectChanges();
+    expect(component.myControl.markAsPristine).toHaveBeenCalledTimes(1);
+  });
+
 });
--- components/automate-ui/src/app/components/form-field/form-field.component.ts
+++ components/automate-ui/src/app/components/form-field/form-field.component.ts
@@ -3,8 +3,13 @@ import { Component,
          QueryList,
          AfterContentInit,
          ChangeDetectionStrategy,
-         ChangeDetectorRef
+         ChangeDetectorRef,
+         Input,
+         OnChanges,
+         SimpleChanges,
+         SimpleChange
        } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ErrorDirective } from '../error/error.directive';
 
 @Component({
@@ -15,8 +20,30 @@ import { ErrorDirective } from '../error/error.directive';
               '../input/input.directive.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FormFieldComponent implements AfterContentInit {
+export class FormFieldComponent implements AfterContentInit, OnChanges {
   @ContentChildren(ErrorDirective) errorChildren: QueryList<ErrorDirective>;
+
+  // A FormFieldComponent can optionally track whether an input field (FormControl)
+  // being edited is the same as, or different from, the field's initial value.
+  // That is, it knows whether the user has made a change or not,
+  // and marks the pristine/dirty state accordingly, so that information may be used
+  // in the parent form to e.g., disable the `Save` button, per our UX guidelines.
+  // Example:
+  // <form [formGroup]="updateForm">
+  //   <chef-form-field
+  //       [watch]="updateForm.controls.name.value"
+  //       [control]="updateForm.controls.name">
+  //     <label>Name <span aria-hidden="true">*</span></label>
+  //     <chef-input formControlName="name" type="text"></chef-input>
+  //   </chef-form-field>
+  // </form>
+  // <chef-button primary [disabled]="!updateForm.valid || !updateForm.dirty">
+  //
+  // You need to supply these two parameters:
+  @Input() watch: string; //  dynamic value to watch
+  @Input() control: FormControl; // control to set back to pristine when needed
+
+  private originalValue = '';
 
   constructor(public changeDetectorRef: ChangeDetectorRef) {}
 
@@ -28,4 +55,13 @@ export class FormFieldComponent implements AfterContentInit {
     });
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    const change: SimpleChange = changes.watch;
+    if (!change.previousValue && change.currentValue && !this.originalValue) {
+      this.originalValue = change.currentValue;
+    }
+    if (change.currentValue && (change.currentValue === this.originalValue)) {
+      this.control.markAsPristine();
+    }
+  }
 }
