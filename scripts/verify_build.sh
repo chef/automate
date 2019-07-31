1aa4c00e153816b98720b7d12547ce49246bffad
Stephan Renatus <srenatus@chef.io>
2019-07-31 08:48:44 +0200

    buildkite: ignore changes to datamigration SQL files
    These can be run multiple times, and must commute with any "real"
    migrations. So, they'll have to be adapted from time to time.
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- scripts/verify_build.sh
+++ scripts/verify_build.sh
@@ -45,7 +45,8 @@ else
     buildkite-agent annotate --style "info" "This change rebuilds no components."
 fi
 
-mapfile -t modified_sql_files < <(git diff --name-status "$(./scripts/git_difference_expression.rb)" | awk '/^[RMD][0-9]*.*\.sql/{ print $2 }')
+mapfile -t modified_sql_files < <(git diff --name-status "$(./scripts/git_difference_expression.rb)" |\
+                                    awk '!/datamigration/ && /^[RMD][0-9]*.*\.sql/ { print $2 }')
 if [[ ${#modified_sql_files[@]} -ne 0 ]]; then
     buildkite-agent annotate --context sql-check --style "warning" <<EOF
 This change modifies the following SQL files:
