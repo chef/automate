c3c7f568d4e037213b4f0189000ca395bc4f0daf
Jay Mundrawala <jmundrawala@chef.io>
2019-08-01 09:00:27 -0500

    viper config
    Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

--- components/cereal-service/cmd/cereal-service/cmd.go
+++ components/cereal-service/cmd/cereal-service/cmd.go
@@ -5,46 +5,176 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/chef/automate/lib/grpc/health"
+
+	"github.com/chef/automate/lib/grpc/secureconn"
+	"github.com/pkg/errors"
+
+	"github.com/chef/automate/lib/platform"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/spf13/viper"
 
 	grpcceral "github.com/chef/automate/api/interservice/cereal"
 	"github.com/chef/automate/components/cereal-service/pkg/server"
 	"github.com/chef/automate/lib/cereal/postgres"
-	"github.com/chef/automate/lib/platform/pg"
+	"github.com/chef/automate/lib/tls/certs"
 
 	"google.golang.org/grpc"
 )
 
-const defaultDatabaseName = "cereal_test"
+const megabyte = 1 << 20
 
-func defaultConnURIForDatabase(dbname string) string {
-	if os.Getenv("PG_URL") != "" {
-		return os.Getenv("PG_URL")
-	}
-	connInfo := pg.A2ConnInfo{
-		Host:  "localhost",
-		Port:  5432,
-		User:  "automate",
-		Certs: pg.A2SuperuserCerts,
-	}
-	return connInfo.ConnURI(dbname)
+type config struct {
+	Service struct {
+		Host             string `mapstructure:"host"`
+		Port             int    `mapstructure:"port"`
+		DisableTLS       bool   `mapstructure:"disable_tls"`
+		MaxRecvSizeBytes int    `mapstructure:"max_recv_size_bytes"`
+	} `mapstructure:"service"`
+	TLS certs.TLSConfig `mapstructure:"tls"`
+	Log struct {
+		Level string `mapstructure:"level"`
+	} `mapstructure:"log"`
+	Database struct {
+		URL string `mapstructure:"url"`
+	} `mapstructure:"database"`
 }
 
-func main() {
-	ctx := context.Background()
-	dbName := defaultDatabaseName
+var cfgFile string
+var C config
 
-	pgBackend := postgres.NewPostgresBackend(defaultConnURIForDatabase(dbName))
-	if err := pgBackend.Init(); err != nil {
-		panic(err)
+var rootCmd = &cobra.Command{
+	Use: "cereal-service",
+}
+
+var serveCmd = &cobra.Command{
+	Use:  "serve",
+	RunE: serve,
+}
+
+func initConfig() error {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+		if err := viper.ReadInConfig(); err != nil {
+			return err
+		}
 	}
 
-	svc := server.NewCerealService(ctx, pgBackend)
-	grpcServer := grpc.NewServer()
-	grpcceral.RegisterCerealServer(grpcServer, svc)
-	lis, err := net.Listen("tcp", fmt.Sprintf(":3210"))
+	if err := viper.Unmarshal(&C); err != nil {
+		return err
+	}
+
+	switch C.Log.Level {
+	case "trace":
+		logrus.SetLevel(logrus.TraceLevel)
+	case "debug":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "info":
+		logrus.SetLevel(logrus.InfoLevel)
+	case "warn":
+		logrus.SetLevel(logrus.WarnLevel)
+	case "error":
+		logrus.Error(logrus.ErrorLevel)
+	}
+
+	return nil
+}
+
+func must(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+func init() {
+	serveCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file")
+	serveCmd.PersistentFlags().String("host", "", "listen host")
+	serveCmd.PersistentFlags().Int("port", 0, "port to listen on")
+	serveCmd.PersistentFlags().Bool("no-tls", false, "disable tls")
+	must(viper.BindPFlag("service.host", serveCmd.PersistentFlags().Lookup("host")))
+	must(viper.BindPFlag("service.port", serveCmd.PersistentFlags().Lookup("port")))
+	must(viper.BindPFlag("service.disable_tls", serveCmd.PersistentFlags().Lookup("no-tls")))
+	must(viper.BindEnv("database.url", "PG_URL"))
+	viper.SetDefault("service.port", 10101)
+	viper.SetDefault("service.disable_tls", false)
+	viper.SetDefault("service.max_recv_size_bytes", 64*megabyte)
 
-	grpcServer.Serve(lis)
+	rootCmd.AddCommand(serveCmd)
+}
+
+func connURI() (string, error) {
+	if C.Database.URL != "" {
+		return C.Database.URL, nil
+	}
+	u, err := platform.PGURIFromEnvironment("cereal-service")
+	if err != nil {
+		return "", err
+	}
+	return u, nil
+}
+
+func serve(*cobra.Command, []string) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := initConfig(); err != nil {
+		return errors.Wrap(err, "failed to load config")
+	}
+
+	uri, err := connURI()
+	if err != nil {
+		return errors.Wrap(err, "failed to get database connection info")
+	}
+	pgBackend := postgres.NewPostgresBackend(uri)
+	if err := pgBackend.Init(); err != nil {
+		return errors.Wrap(err, "could not initialize database")
+	}
+
+	var grpcServer *grpc.Server
+	grpcServerOpts := []grpc.ServerOption{
+		grpc.MaxRecvMsgSize(C.Service.MaxRecvSizeBytes),
+	}
+
+	if C.Service.DisableTLS {
+		grpcServer = grpc.NewServer(grpcServerOpts...)
+	} else {
+		serviceCerts, err := C.TLS.ReadCerts()
+		if err != nil {
+			return errors.Wrap(err, "could not read tls certs")
+		}
+		f := secureconn.NewFactory(*serviceCerts)
+		grpcServer = f.NewServer(grpcServerOpts...)
+	}
+
+	svc := server.NewCerealService(ctx, pgBackend)
+	grpcceral.RegisterCerealServer(grpcServer, svc)
+
+	healthSvc := health.NewService()
+	health.RegisterHealthServer(grpcServer, healthSvc)
+
+	listenAddr := fmt.Sprintf("%s:%d", C.Service.Host, C.Service.Port)
+	lis, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return errors.Wrapf(err, "failed to listen on port %s", listenAddr)
+	}
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-ch
+		healthSvc.Shutdown()
+		grpcServer.GracefulStop()
+		logrus.WithField("signal", sig).Info("Exiting")
+	}()
+
+	return grpcServer.Serve(lis)
+}
+
+func main() {
+	rootCmd.Execute()
 }
