336be7bfc15b256ee6ae65b823560ea64c704a2e
Stephan Renatus <srenatus@chef.io>
2019-08-06 15:28:07 +0200

    automate-ui: adapt auth ID regular expression and error message
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- components/automate-ui/src/app/helpers/auth/regex.ts
+++ components/automate-ui/src/app/helpers/auth/regex.ts
@@ -3,7 +3,11 @@ export class Regex {
   public static readonly patterns = {
 
     // all auth IDs use this pattern
-    ID: '[0-9a-z-]+',
+    // Note: the restriction to <= 64 chars is implemented by another validator,
+    // so that we get a nicer error message. Here it's enough to restrict the
+    // char set.
+    // Interestingly, the regex validator doesn't seem to fire for empty strings.
+    ID: '[0-9a-z-_]+',
 
     // NB: neither \S nor ^\s work inside the brackets in this regex language.
     NON_BLANK: '.*[^ ].*'
