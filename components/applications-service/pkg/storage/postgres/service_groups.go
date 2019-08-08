fb5015c95062f39e6ffc466eb0723326c358e06e
kmacgugan <kmacgugan@chef.io>
2019-08-07 17:51:53 -0700

    Fix whitespace on sql
    Signed-off-by: kmacgugan <kmacgugan@chef.io>

--- components/applications-service/pkg/storage/postgres/service_groups.go
+++ components/applications-service/pkg/storage/postgres/service_groups.go
@@ -54,57 +54,57 @@ const (
 `
 
 	selectServiceGroupHealth = `
-	SELECT *
-				 ,(CASE WHEN health_critical > 0 THEN '1_CRITICAL'
-								WHEN health_unknown  > 0 THEN '2_UNKNOWN'
-								WHEN health_warning  > 0 THEN '3_WARNING'
-								ELSE '4_OK' END ) as health
-		FROM (SELECT  sg.id
-								 ,sg.deployment_id
-								 ,sg.name as name
-								 ,COUNT(s.health) FILTER (WHERE s.health = 'OK') AS health_ok
-								 ,COUNT(s.health) FILTER (WHERE s.health = 'CRITICAL') AS health_critical
-								 ,COUNT(s.health) FILTER (WHERE s.health = 'WARNING') AS health_warning
-								 ,COUNT(s.health) FILTER (WHERE s.health = 'UNKNOWN') AS health_unknown
-								 ,COUNT(s.health) AS health_total
-								 ,round((COUNT(s.health) FILTER (WHERE s.health = 'OK')
-											 / COUNT(s.health)::float) * 100) as percent_ok
-								 ,(SELECT array_agg(DISTINCT CONCAT (s.package_ident))
-										 FROM service AS s
-										WHERE s.group_id = sg.id) AS releases
-								 ,d.app_name as app_name
-								 ,d.environment as environment
-								 ,sg.name_suffix as name_suffix
-					FROM service_group AS sg
-					JOIN service AS s
-							 ON s.group_id = sg.id
-					JOIN deployment as d
-							 ON sg.deployment_id = d.id
+  SELECT *
+         ,(CASE WHEN health_critical > 0 THEN '1_CRITICAL'
+                WHEN health_unknown  > 0 THEN '2_UNKNOWN'
+                WHEN health_warning  > 0 THEN '3_WARNING'
+                ELSE '4_OK' END ) as health
+    FROM (SELECT  sg.id
+                 ,sg.deployment_id
+                 ,sg.name as name
+                 ,COUNT(s.health) FILTER (WHERE s.health = 'OK') AS health_ok
+                 ,COUNT(s.health) FILTER (WHERE s.health = 'CRITICAL') AS health_critical
+                 ,COUNT(s.health) FILTER (WHERE s.health = 'WARNING') AS health_warning
+                 ,COUNT(s.health) FILTER (WHERE s.health = 'UNKNOWN') AS health_unknown
+                 ,COUNT(s.health) AS health_total
+                 ,round((COUNT(s.health) FILTER (WHERE s.health = 'OK')
+                       / COUNT(s.health)::float) * 100) as percent_ok
+                 ,(SELECT array_agg(DISTINCT CONCAT (s.package_ident))
+                     FROM service AS s
+                    WHERE s.group_id = sg.id) AS releases
+                 ,d.app_name as app_name
+                 ,d.environment as environment
+                 ,sg.name_suffix as name_suffix
+          FROM service_group AS sg
+          JOIN service AS s
+               ON s.group_id = sg.id
+          JOIN deployment as d
+               ON sg.deployment_id = d.id
 `
 	groupByPart = `
-				 GROUP BY sg.id, sg.deployment_id, sg.name, d.app_name, d.environment, sg.name_suffix ) as sghc
+  GROUP BY sg.id, sg.deployment_id, sg.name, d.app_name, d.environment, sg.name_suffix ) as sghc
 `
 	joinSupervisor = `
   JOIN supervisor AS sup
     ON s.sup_id = sup.id
 `
 	selectServiceGroupHealthFilterCRITICAL = `
-	WHERE
+  WHERE
    health_critical > 0
 `
 	selectServiceGroupHealthFilterUNKNOWN = `
-	WHERE
+  WHERE
    health_unknown  > 0
    AND health_critical = 0
 `
 	selectServiceGroupHealthFilterWARNING = `
-	WHERE
+  WHERE
    health_warning  > 0
    AND health_critical = 0
    AND health_unknown  = 0
 `
 	selectServiceGroupHealthFilterOK = `
-	WHERE
+  WHERE
    health_ok > 0
    AND health_critical = 0
    AND health_warning  = 0
