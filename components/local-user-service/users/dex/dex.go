c96b57e8b0fa7261a4c79eba8dc6670c8a591316
Stephan Renatus <srenatus@chef.io>
2019-08-14 12:28:55 +0200

    local-user-service: use new method, cleanup oidc cruft
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- components/local-user-service/users/dex/dex.go
+++ components/local-user-service/users/dex/dex.go
@@ -2,11 +2,6 @@ package dex
 
 import (
 	"context"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"regexp"
-	"strings"
 
 	"github.com/dexidp/dex/api"
 	"github.com/pkg/errors"
@@ -14,7 +9,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/chef/automate/components/local-user-service/users"
-	"github.com/chef/automate/components/session-service/oidc"
 	"github.com/chef/automate/lib/grpc/secureconn"
 	"github.com/chef/automate/lib/tls/certs"
 )
@@ -22,23 +16,12 @@ import (
 // Config is used for configuring dex adapters
 type Config struct {
 	GRPCHost string `json:"grpcHost"`
-
-	// OIDC (public) client config
-	IssuerURL string `json:"issuerURL"`
-	DexURL    string `json:"dexURL"`
 }
 
 type state struct {
-	c      api.DexClient
-	oc     oidc.Client
-	dexURL *url.URL
-	l      *zap.Logger
+	c api.DexClient
+	l *zap.Logger
 }
-
-var (
-	localLoginRegexp = regexp.MustCompile(`(/dex/auth/local\?req=[^"]+)`)
-	oobCodeRegexp    = regexp.MustCompile(`Please copy this code, switch to your application and paste it there:`)
-)
 
 // Open initializes the dex adapter
 func (c *Config) Open(l *zap.Logger, cs *certs.ServiceCerts) (users.Adapter, error) {
@@ -49,47 +32,7 @@ func (c *Config) Open(l *zap.Logger, cs *certs.ServiceCerts) (users.Adapter, err
 	}
 	//defer conn.Close()
 
-	if c.IssuerURL == "" {
-		l.Warn("no OIDC config, not initializing Dex password validation client")
-		return &state{
-				c: api.NewDexClient(conn),
-				l: l},
-			nil
-	}
-
-	issuer, err := url.Parse(c.IssuerURL)
-	if err != nil {
-		return nil, errors.Wrapf(err, "parse issuer URL %q", c.IssuerURL)
-	}
-	dex, err := url.Parse(c.DexURL)
-	if err != nil {
-		return nil, errors.Wrapf(err, "parse dex URL %q", c.DexURL)
-	}
-	// good enough for our purposes here, and what a public client needs to use
-	redir, err := url.Parse("urn:ietf:wg:oauth:2.0:oob")
-	if err != nil {
-		return nil, errors.Wrap(err, "parse redirect URL")
-	}
-
-	cfg := oidc.Config{
-		ClientID:     "automate-api", // public client registered in dex' config.yml
-		ClientSecret: "",
-		IssuerURL:    issuer,
-		RedirectURL:  redir,
-		DexURL:       dex,
-	}
-
-	o, err := oidc.New(cfg, 2, cs, l.Sugar())
-	if err != nil {
-		return nil, errors.Wrap(err, "open OIDC client")
-	}
-
-	return &state{
-			c:      api.NewDexClient(conn),
-			oc:     o,
-			dexURL: issuer,
-			l:      l},
-		nil
+	return &state{c: api.NewDexClient(conn), l: l}, nil
 }
 
 func (s *state) GetUsers(ctx context.Context) (users.Users, error) {
@@ -265,76 +208,13 @@ func (s *state) HashPassword(password string) ([]byte, error) {
 	return hashedPass, nil
 }
 
-// TODO, brenna darroch 9/8/17
-// validate user function
-// need guidance from Product/UX on what to validate
-
 func (s *state) ValidatePassword(ctx context.Context, username, password string) (bool, error) {
-	// (tc) We don't need to worry about checking the OIDC state in a callback for the public
-	// clients we are using internally since all requests are happening within automate itself
-	// (in this instance, it's local user service talking directly to dex, not even going through ALB).
-	// Also, all of those requests are using HTTPS and mTLS. Therefore, there isn't a situation
-	// where these could be sniffed or a CSRF attack[1] could occur without automate being completely pwned already.
-	//
-	// [1] https://tools.ietf.org/html/rfc6749#section-10.12
-	oidcState := "thisShouldNotMatter"
-	authURL := s.oc.AuthCodeURL(oidcState)
-	req, err := http.NewRequest("GET", authURL, nil)
+	resp, err := s.c.VerifyPassword(ctx, &api.VerifyPasswordReq{Email: username, Password: password})
 	if err != nil {
-		return false, errors.Wrap(err, "create HTTP request")
+		return false, errors.Wrap(err, "verify password")
 	}
-	req.Header.Add("content-type", "form-www-urlencoded")
-	req = req.WithContext(ctx)
-	resp, err := s.oc.Client().Do(req)
-	if err != nil {
-		return false, errors.Wrap(err, "execute HTTP request")
+	if resp.NotFound {
+		return false, nil
 	}
-	defer resp.Body.Close() // nolint: errcheck
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return false, errors.Wrap(err, "read HTTP response body")
-	}
-
-	// Note: this works both for "only local connector" and "multiple connectors"
-	// Dex settings (e.g. local + saml), since our RE is loose enough to capture
-	// either the login form POST (single connector) or the link to the local
-	// connector (multiple connectors)
-
-	// find local login link with auth code
-	if fs := localLoginRegexp.Find(respBody); fs != nil {
-		vals := url.Values{}
-		vals.Add("login", username)
-		vals.Add("password", password)
-		body := strings.NewReader(vals.Encode())
-		loginURL, err := s.dexURL.Parse(string(fs))
-		if err != nil {
-			return false, errors.Wrap(err, "construct HTTP request path")
-		}
-		req, err := http.NewRequest("POST", loginURL.String(), body)
-		if err != nil {
-			return false, errors.Wrap(err, "create HTTP login request")
-		}
-		req.Header.Add("content-type", "application/x-www-form-urlencoded")
-		req = req.WithContext(ctx)
-		resp, err := s.oc.Client().Do(req)
-		if err != nil {
-			return false, errors.Wrap(err, "execute HTTP login request")
-		}
-		defer resp.Body.Close() // nolint: errcheck
-
-		respBody, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return false, errors.Wrap(err, "read HTTP login response body")
-		}
-
-		// Success means: we've been given a _code_ to redeem for a token.
-		// We don't care for the token, we only care for having been able to get
-		// one. So this stops here.
-		if code := oobCodeRegexp.Find(respBody); code != nil {
-			return true, nil
-		}
-	}
-
-	return false, nil
+	return resp.Verified, nil
 }
