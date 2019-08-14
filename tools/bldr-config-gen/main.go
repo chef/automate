2823db3b187e918d1cde5006a48d10297c105f2b
Steven Danna <steve@chef.io>
2019-08-14 13:16:52 +0100

    [bldr-config-gen] Individually track vendored deps
    Previously, any component that used a dependency simply tracked any
    changes to Gopkg.lock. As of this change, we specifically add globs
    for the things you depend on.
    
    The good thing about this change is that if you update Gopkg.lock you
    don't necessarily have to rebuild the entire world.
    
    The bad thing about this change is that you'll need to regenerate
    .bldr.toml anytime you add a dependency to your project.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- tools/bldr-config-gen/main.go
+++ tools/bldr-config-gen/main.go
@@ -28,8 +28,7 @@ type PackageSpec struct {
 }
 
 type GoDepInfo struct {
-	deps       []string
-	usesVendor bool
+	deps []string
 }
 
 var opts = struct {
@@ -167,10 +166,6 @@ func generateHabPackageConfig(outBuf io.Writer, habPackages []PackageSpec) error
 		for _, dep := range depInfo.deps {
 			fmt.Fprintf(outBuf, ",\n  \"%s/*\"", dep)
 		}
-
-		if depInfo.usesVendor {
-			fmt.Fprintf(outBuf, ",\n  \"Gopkg.lock\"")
-		}
 		fmt.Fprintf(outBuf, "\n]\n")
 	}
 	return nil
@@ -201,11 +196,6 @@ func getGoDepInfo(path string) (GoDepInfo, error) {
 				return info, errors.Errorf("unknown go dependency path for %s: %s", path, line)
 			}
 
-			if parts[3] == "vendor" {
-				info.usesVendor = true
-				continue
-			}
-
 			end := len(parts)
 			switch parts[3] {
 			case "api":
@@ -214,6 +204,8 @@ func getGoDepInfo(path string) (GoDepInfo, error) {
 				// We want to avoid rebuilds so we don't depend
 				// on the entire lib directory
 				end = 5
+			case "vendor":
+				end = 7
 			case "components":
 				if len(parts) > 4 && parts[4] == "automate-gateway" {
 					// Many services depend on
