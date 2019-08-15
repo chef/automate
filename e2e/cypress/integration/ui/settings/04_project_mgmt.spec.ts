cf29995c11cd9df9244223dce4a68af3dcf4f74b
Brenna Hewer-Darroch <brenna@chef.io>
2019-08-15 15:00:17 -0700

    e2e: add the rest of the tests
    list rules
    update rule
    delete rule
    delete project
    
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- e2e/cypress/integration/ui/settings/04_project_mgmt.spec.ts
+++ e2e/cypress/integration/ui/settings/04_project_mgmt.spec.ts
@@ -1,5 +1,3 @@
-import forEach = require("cypress/types/lodash/forEach");
-
 interface Project {
   id: string;
   name: string;
@@ -18,8 +16,8 @@ describeIAMV2P1('project management', () => {
   let adminToken = '';
   const now = Cypress.moment().format('MMDDYYhhmm');
   const cypressPrefix = 'cypress-test';
-  const project1ID = `${cypressPrefix}-project1-${now}`;
-  const project1Name = `${cypressPrefix} project1 ${now}`;
+  const projectID = `${cypressPrefix}-project1-${now}`;
+  const projectName = `${cypressPrefix} project1 ${now}`;
   const ruleID = `${cypressPrefix}-rule-${now}`;
   const ruleName = `${cypressPrefix} rule ${now}`;
 
@@ -60,29 +58,29 @@ describeIAMV2P1('project management', () => {
     cy.get('#create-button').contains('Create Project').click();
     cy.get('app-create-object-modal chef-modal').should('have.class', 'visible');
 
-    cy.get('[data-cy=create-name]').type(project1Name);
+    cy.get('[data-cy=create-name]').focus().type(projectName);
 
     cy.get('[data-cy=create-id]').should('not.be.visible');
     cy.get('[data-cy=edit-button]').contains('Edit ID').click();
     cy.get('[data-cy=id-label]').should('not.be.visible');
-    cy.get('#id-input').should('have.value', project1ID);
+    cy.get('#id-input').should('have.value', projectID);
 
     cy.get('[data-cy=save-button]').click();
     cy.get('app-create-object-modal chef-modal').should('not.be.visible');
     cy.get('chef-notification.info').should('be.visible');
-    cy.contains(project1Name).should('exist');
-    cy.contains(project1ID).should('exist');
+    cy.contains(projectName).should('exist');
+    cy.contains(projectID).should('exist');
 
-    cy.url().should('include', `/settings/projects/${project1ID}`);
+    cy.url().should('include', `/settings/projects/${projectID}`);
   });
 
   it('can create a rule for the new project', () => {
     cy.get('app-authorized button').contains('Create Rule').click();
 
-    cy.url().should('include', `/settings/projects/${project1ID}/rules`);
+    cy.url().should('include', `/settings/projects/${projectID}/rules`);
     cy.get('chef-page').should('be.visible');
 
-    cy.get('#create-name input').type(ruleName)
+    cy.get('#create-name input').focus().type(ruleName);
     cy.get('[data-cy=edit-id]').click();
 
     cy.get('#create-id input').should('have.value', ruleID);
@@ -98,7 +96,7 @@ describeIAMV2P1('project management', () => {
     // Node
     cy.get('#create-type-dropdown').select('Node');
     const nodeAttributes = ['Chef Organization', 'Chef Server', 'Environment',
-        'Chef Role', 'Chef Tag', 'Chef Policy Name', 'Chef Policy Group']
+        'Chef Role', 'Chef Tag', 'Chef Policy Name', 'Chef Policy Group'];
 
     nodeAttributes.forEach((att: string) => {
         cy.get('[data-cy=attribute-dropdown]').select(att);
@@ -109,24 +107,94 @@ describeIAMV2P1('project management', () => {
     cy.get('[data-cy=rule-value] input').type('Chef Policy Group Foo');
 
     cy.get('#right-buttons button').contains('Save Rule').click();
-    cy.get('chef-page').should('not.be.visible')
+    cy.get('chef-page').should('not.be.visible');
 
-    cy.url().should('include', `/settings/projects/${project1ID}`);
-    cy.get('chef-td').contains(ruleID)
+    cy.url().should('include', `/settings/projects/${projectID}`);
+    cy.get('chef-td').contains(ruleID);
   });
 
   it('can see a list of rules for a project', () => {
+    ['Name', 'ID', 'Resource Type', 'Conditions', 'Edits'].forEach((header) => {
+      cy.get('chef-th').contains(header);
+    });
+
+    cy.get('chef-td').contains(ruleID);
+    cy.get('chef-td').contains(ruleName);
+    cy.get('chef-td').contains('Node');
+    cy.get('chef-td').contains('1 condition');
+    cy.get('chef-td').contains('Edits pending');
   });
 
   it('can update a project rule', () => {
-  });
+    const updatedRuleName = `updated ${ruleName}`;
+    cy.get('a').contains(ruleName).click();
 
-  it('can delete a project rule', () => {
+    cy.url().should('include', `/settings/projects/${projectID}/rules/${ruleID}`);
+    cy.get('chef-page').should('be.visible');
+
+    cy.get('#create-name input').clear().focus().type(updatedRuleName);
+
+    cy.get('#create-id input').should('have.value', ruleID);
+    cy.get('#create-id input').should('be.disabled');
+
+    cy.get('#create-type-dropdown option:first').should('have.value', 'NODE');
+    cy.get('#create-type-dropdown option:first').should('be.selected');
+    cy.get('#create-type-dropdown').should('be.disabled');
+
+    cy.get('chef-button').contains('Add Condition').click();
+    cy.get('.end-row-items span').contains('AND').should('be.visible');
+
+    cy.get('[data-cy=attribute-dropdown]:last')
+      .select('Environment');
+
+    cy.get('[data-cy=operator-dropdown]:last')
+      .select('member of');
+
+    cy.get('[data-cy=rule-value]:last input')
+      .type('Dev, Prod, Test');
+
+    cy.get('#right-buttons button').contains('Save Rule').click();
+    cy.get('chef-page').should('not.be.visible');
+
+    cy.url().should('include', `/settings/projects/${projectID}`);
+    cy.get('chef-td').contains(updatedRuleName);
+    cy.get('chef-td').contains('2 conditions');
   });
 
   it('can update a project name', () => {
+    const updatedProjectName = `updated ${projectName}`;
+
+    cy.get('[data-cy=details-tab]').click();
+    cy.get('#update-name').find('input').clear().focus().type(updatedProjectName);
+    cy.get('chef-button').contains('Save').click();
+
+    cy.get('h1.page-title').contains(updatedProjectName);
+  });
+
+  it('can delete a project rule', () => {
+    cy.get('[data-cy=rules-tab]').click();
+
+    cy.get('chef-td').contains(ruleID).parent()
+      .find('chef-control-menu').as('controlMenu');
+    cy.get('@controlMenu').click();
+    cy.get('@controlMenu').find('[data-cy=delete-rule]').click({force: true});
+
+    cy.get('chef-button').contains('Delete Rule').click();
+
+    cy.get('chef-tbody chef-td').contains(ruleID).should('not.exist');
   });
 
   it('delete a project', () => {
+    cy.get('.breadcrumb').click();
+
+    cy.get('chef-td').contains(projectID).parent()
+      .find('chef-control-menu').as('controlMenu');
+    cy.get('@controlMenu').click().then(($menu) => {
+      $menu.find('[data-cy=delete-project]').click();
+    });
+
+    cy.get('chef-button').contains('Delete Project').click();
+
+    cy.get('chef-tbody chef-td').contains(projectID).should('not.exist');
   });
 });
