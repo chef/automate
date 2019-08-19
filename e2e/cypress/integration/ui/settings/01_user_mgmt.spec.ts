a0942a90d08af7acef54c93dea3e981584995463
Brenna Hewer-Darroch <brenna@chef.io>
2019-08-19 16:06:08 -0700

    e2e: address review
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- e2e/cypress/integration/ui/settings/01_user_mgmt.spec.ts
+++ e2e/cypress/integration/ui/settings/01_user_mgmt.spec.ts
@@ -16,6 +16,7 @@ describe('user management', () => {
   });
 
   const now = Cypress.moment().format('MMDDYYhhmm');
+  const typeDelay = 50;
   const name = `cypress test user ${now}`;
   const username = `testing${now}`;
   const password = 'testing!';
@@ -31,24 +32,24 @@ describe('user management', () => {
     // the `should` will wait until the actual input value matches the expected value
     // ref: https://github.com/cypress-io/cypress/issues/534
     cy.get('[formcontrolname=fullname]')
-      .type(name, { delay: 50 }).should('have.value', name);
+      .type(name, { delay: typeDelay }).should('have.value', name);
 
     cy.get('[formcontrolname=username]')
-      .type(username, { delay: 50 }).should('have.value', username);
+      .type(username, { delay: typeDelay }).should('have.value', username);
 
     cy.get('[formcontrolname=password]')
-      .type(password, { delay: 50 }).should('have.value', password);
+      .type(password, { delay: typeDelay }).should('have.value', password);
 
     cy.get('[formcontrolname=confirmPassword]')
-      .type(password, { delay: 50 }).should('have.value', password);
+      .type(password, { delay: typeDelay }).should('have.value', password);
 
     // save new user
     cy.get('[data-cy=save-user]').click();
     cy.get('app-user-management chef-modal').should('not.be.visible');
     cy.get('chef-notification.info').should('be.visible');
 
-    cy.get('chef-td').contains(username).should('exist');
-    cy.get('chef-td').contains(name).should('exist');
+    cy.get('app-user-table chef-td').contains(username).should('exist');
+    cy.get('app-user-table chef-td').contains(name).should('exist');
   });
 
   it('can view and edit user details', () => {
@@ -57,47 +58,46 @@ describe('user management', () => {
     const updated_name = `${name} updated`;
     const updated_password = 'testing?';
     cy.contains(name).click();
-    cy.get('app-user-details').should('exist');
-    cy.get('div.name-column').contains(name).should('exist');
-    cy.get('span').contains(username).should('exist');
-    cy.get('chef-form-field').contains('New Password').should('exist');
-    cy.get('chef-form-field').contains('Confirm New Password').should('exist');
+    cy.get('app-user-details div.name-column').contains(name).should('exist');
+    cy.get('app-user-details span').contains(username).should('exist');
+    cy.get('app-user-details chef-form-field').contains('New Password').should('exist');
+    cy.get('app-user-details chef-form-field').contains('Confirm New Password').should('exist');
 
-    cy.get('chef-button.edit-button').click();
+    cy.get('app-user-details chef-button.edit-button').click();
     cy.get('[formcontrolname=fullName]').find('input')
-      .clear().type(updated_name, { delay: 50 }).should('have.value', updated_name);
+      .clear().type(updated_name, { delay: typeDelay }).should('have.value', updated_name);
 
-    cy.get('chef-button.save-button').click();
+    cy.get('app-user-details chef-button.save-button').click();
     cy.wait('@updateUser');
     cy.contains(updated_name).should('exist');
 
     cy.get('[formcontrolname=newPassword]').find('input')
-      .type(updated_password, { delay: 50 }).should('have.value', updated_password);
+      .type(updated_password, { delay: typeDelay }).should('have.value', updated_password);
     cy.get('[formcontrolname=confirmPassword]').find('input')
-      .type(updated_password, { delay: 50 }).should('have.value', updated_password);
-    cy.get('chef-button').contains('Update Password').click({ force: true} );
+      .type(updated_password, { delay: typeDelay }).should('have.value', updated_password);
+    cy.get('app-user-details chef-button').contains('Update Password').click({ force: true} );
 
     // success alert displays
     cy.get('chef-notification.info').should('be.visible');
 
     // back to user list page
-    cy.get('.breadcrumb').contains('Users').click();
+    cy.get('app-user-details .breadcrumb').contains('Users').click();
   });
 
   it('can delete user', () => {
     cy.route('DELETE', '**/users/**').as('deleteUser');
 
-      cy.get('chef-td').contains(name).parent().parent()
+    cy.get('app-user-table chef-td').contains(name).parent().parent()
         .find('chef-control-menu').as('controlMenu');
-      // we throw in a should so cypress waits until introspection allows menu to be shown
-      cy.get('@controlMenu').should('be.visible')
-        .click();
-      cy.get('@controlMenu').find('[data-cy=delete]').click({ force: true });
+    // we throw in a should so cypress waits until introspection allows menu to be shown
+    cy.get('@controlMenu').should('be.visible')
+      .click();
+    cy.get('@controlMenu').find('[data-cy=delete]').click({ force: true });
 
-      // confirm in modal
-      cy.get('chef-button').contains('Delete User').click();
+    // confirm in modal
+    cy.get('app-user-management chef-button').contains('Delete User').click();
 
-      cy.wait('@deleteUser');
-      cy.get('chef-tbody chef-td').contains(username).should('not.exist');
+    cy.wait('@deleteUser');
+    cy.get('app-user-management chef-tbody chef-td').contains(username).should('not.exist');
   });
 });
