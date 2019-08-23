5048293417d3cc3d698cf28924bec9ec83827471
danielsdeleo <dan@chef.io>
2019-08-23 11:58:21 -0700

    Add basics for periodic jobs in applications
    Signed-off-by: Daniel DeLeo <dan@chef.io>

---
+++ components/applications-service/integration_test/disconnected_services_job_test.go
@@ -0,0 +1,107 @@
+package integration_test
+
+import (
+	"context"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/chef/automate/components/applications-service/pkg/config"
+	"github.com/chef/automate/components/applications-service/pkg/server"
+	certs "github.com/chef/automate/lib/tls/certs"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// CONFIGURE PERIODIC DELETION OF DISCONNECTED SERVICES:
+// * this config lives with the cereal service in the job config or something like that
+// The way it's updated is:
+// (m *Manager) UpdateWorkflowScheduleByName(ctx context.Context,
+// 	instanceName string, workflowName string, opts ...WorkflowScheduleUpdateOpts)
+// need to ensure it exists on startup I guess...
+
+const (
+	DisconnectedServicesJobName      = "disconnected_services"
+	DisconnectedServicesScheduleName = "periodic_disconnected_services"
+)
+
+const habConfigDir = "/hab/svc/applications-service/config"
+
+func TestPeriodicDisconnectedServices(t *testing.T) {
+	certs := studioCerts(t)
+
+	jobCfg := config.Jobs{
+		Host: "localhost",
+		Port: 10101,
+	}
+
+	mgr, err := server.NewJobManager(&jobCfg, certs)
+	require.NoError(t, err)
+	err = mgr.SetupScheduler()
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	sched, err := mgr.CerealSvc.GetWorkflowScheduleByName(ctx, DisconnectedServicesScheduleName, DisconnectedServicesJobName)
+
+	require.NoError(t, err)
+	assert.Equal(t, "disconnected_services", sched.WorkflowName)
+	assert.True(t, sched.Enabled)
+
+	// err = mgr.Start()
+	// require.NoError(t, err)
+
+	time.Sleep(2 * time.Second)
+
+	t.Run("disable and enable disconnected_services job", func(t *testing.T) {
+		err := mgr.DisableDisconnectedServicesJob(ctx)
+		require.NoError(t, err)
+
+		sched, err := mgr.CerealSvc.GetWorkflowScheduleByName(ctx, DisconnectedServicesScheduleName, DisconnectedServicesJobName)
+		require.NoError(t, err)
+		assert.False(t, sched.Enabled)
+
+		err = mgr.EnableDisconnectedServicesJob(ctx)
+		require.NoError(t, err)
+
+		sched, err = mgr.CerealSvc.GetWorkflowScheduleByName(ctx, DisconnectedServicesScheduleName, DisconnectedServicesJobName)
+		require.NoError(t, err)
+		assert.True(t, sched.Enabled)
+
+	})
+	t.Run("update disconnected_services job params", func(t *testing.T) {
+		newParams := server.DisconnectedServicesParamsV0{ThresholdSeconds: 23}
+		err := mgr.UpdateDisconnectedServicesJobParams(ctx, &newParams)
+		require.NoError(t, err)
+
+		sched, err := mgr.CerealSvc.GetWorkflowScheduleByName(ctx, DisconnectedServicesScheduleName, DisconnectedServicesJobName)
+		require.NoError(t, err)
+
+		var returnedParams server.DisconnectedServicesParamsV0
+		sched.GetParameters(&returnedParams)
+		assert.Equal(t, 23, returnedParams.ThresholdSeconds)
+
+		// Update the params again to ensure we didn't accidentally pass the test due to leftover state somewhere:
+		newParams = server.DisconnectedServicesParamsV0{ThresholdSeconds: 42}
+		err = mgr.UpdateDisconnectedServicesJobParams(ctx, &newParams)
+		require.NoError(t, err)
+
+		sched, err = mgr.CerealSvc.GetWorkflowScheduleByName(ctx, DisconnectedServicesScheduleName, DisconnectedServicesJobName)
+		require.NoError(t, err)
+
+		//9var returnedParams server.DisconnectedServicesParamsV0
+		sched.GetParameters(&returnedParams)
+		assert.Equal(t, 42, returnedParams.ThresholdSeconds)
+	})
+
+}
+
+func studioCerts(t *testing.T) *certs.ServiceCerts {
+	conf := certs.TLSConfig{
+		CertPath:       path.Join(habConfigDir, "service.crt"),
+		KeyPath:        path.Join(habConfigDir, "service.key"),
+		RootCACertPath: path.Join(habConfigDir, "root_ca.crt"),
+	}
+	certs, err := conf.ReadCerts()
+	require.NoError(t, err)
+	return certs
+}
---
+++ components/applications-service/pkg/server/periodic.go
@@ -0,0 +1,163 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/chef/automate/components/applications-service/pkg/config"
+	"github.com/chef/automate/lib/cereal"
+	cerealRPC "github.com/chef/automate/lib/cereal/grpc"
+	"github.com/chef/automate/lib/cereal/patterns"
+	"github.com/chef/automate/lib/grpc/secureconn"
+	certs "github.com/chef/automate/lib/tls/certs"
+	"github.com/chef/automate/lib/version"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	rrule "github.com/teambition/rrule-go"
+)
+
+type JobManager struct {
+	CerealSvc *cereal.Manager
+}
+
+type DisconnectedServicesParamsV0 struct {
+	ThresholdSeconds int `mapstructure:"threshold_seconds"`
+}
+
+const (
+	cerealServiceMutualTLSName = "cereal-service"
+
+	DisconnectedServicesJobIntervalSeconds = 60
+	DisconnectedServicesJobName            = "disconnected_services"
+	DisconnectedServicesScheduleName       = "periodic_disconnected_services"
+)
+
+func NewJobManager(jobCfg *config.Jobs, certs *certs.ServiceCerts) (*JobManager, error) {
+	svcURL := fmt.Sprintf("%s:%d", jobCfg.Host, jobCfg.Port)
+
+	connFactory := secureconn.NewFactory(*certs,
+		secureconn.WithVersionInfo(version.Version, version.GitSHA))
+	conn, err := connFactory.Dial(cerealServiceMutualTLSName, svcURL)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to connect to upstream job service %q at %q", cerealServiceMutualTLSName, svcURL)
+	}
+
+	be := cerealRPC.NewGrpcBackendFromConn("applications", conn)
+	cerealSvc, err := cereal.NewManager(be)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create cereal job manager from gRPC connection")
+	}
+
+	return &JobManager{CerealSvc: cerealSvc}, nil
+}
+
+// SetupScheduler ensures all our jobs exist in the cereal service backend.
+func (j *JobManager) SetupScheduler() error {
+	r, err := rrule.NewRRule(rrule.ROption{
+		Freq:     rrule.SECONDLY,
+		Interval: DisconnectedServicesJobIntervalSeconds,
+		Dtstart:  time.Now(),
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to create job scheduler configuration")
+	}
+
+	params := defaultDisconnectedServicesJobParams()
+
+	logCtx := log.WithFields(log.Fields(log.Fields{
+		"scheduleName": DisconnectedServicesScheduleName,
+		"jobName":      DisconnectedServicesJobName,
+		"jobParams":    params,
+		"recurrance":   r,
+	}))
+
+	err = j.CerealSvc.CreateWorkflowSchedule(
+		DisconnectedServicesScheduleName,
+		DisconnectedServicesJobName,
+		defaultDisconnectedServicesJobParams(),
+		true,
+		r,
+	)
+	if err == cereal.ErrWorkflowScheduleExists {
+		logCtx.Debug("workflow already created")
+	} else if err != nil {
+		logCtx.Error("failed to create workflow schedule")
+		return errors.Wrap(err, "failed to create workflow schedule")
+	} else {
+		logCtx.Info("created workflow schedule")
+	}
+
+	return nil
+}
+
+func (j *JobManager) Start() error {
+	err := j.CerealSvc.RegisterTaskExecutor(DisconnectedServicesJobName, &markDisconnectedServicesExecutor{}, cereal.TaskExecutorOpts{})
+	if err != nil {
+		return errors.Wrap(err, "failed to register as task exector to mark disconnected services")
+	}
+
+	wfX := patterns.NewSingleTaskWorkflowExecutor(DisconnectedServicesJobName, false)
+	err = j.CerealSvc.RegisterWorkflowExecutor(DisconnectedServicesJobName, wfX)
+	if err != nil {
+		return errors.Wrap(err, "failed to register as workflow exector to mark disconnected services")
+	}
+
+	// TODO: set a timeout
+	ctx := context.Background()
+
+	err = j.CerealSvc.Start(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to start workflow/job exector")
+	}
+
+	return nil
+}
+
+func (j *JobManager) UpdateDisconnectedServicesJobParams(ctx context.Context, params *DisconnectedServicesParamsV0) error {
+	err := j.CerealSvc.UpdateWorkflowScheduleByName(
+		ctx,
+		DisconnectedServicesScheduleName, DisconnectedServicesJobName,
+		cereal.UpdateParameters(params))
+	if err != nil {
+		return errors.Wrap(err, "failed to set disconnected_services job to enabled")
+	}
+	return nil
+}
+
+func (j *JobManager) EnableDisconnectedServicesJob(ctx context.Context) error {
+	err := j.CerealSvc.UpdateWorkflowScheduleByName(
+		ctx,
+		DisconnectedServicesScheduleName, DisconnectedServicesJobName,
+		cereal.UpdateEnabled(true))
+	if err != nil {
+		return errors.Wrap(err, "failed to set disconnected_services job to enabled")
+	}
+	return nil
+}
+
+func (j *JobManager) DisableDisconnectedServicesJob(ctx context.Context) error {
+	err := j.CerealSvc.UpdateWorkflowScheduleByName(
+		ctx,
+		DisconnectedServicesScheduleName, DisconnectedServicesJobName,
+		cereal.UpdateEnabled(false))
+	if err != nil {
+		return errors.Wrap(err, "failed to set disconnected_services job to disabled")
+	}
+	return nil
+
+}
+
+func defaultDisconnectedServicesJobParams() *DisconnectedServicesParamsV0 {
+	return &DisconnectedServicesParamsV0{ThresholdSeconds: 300}
+}
+
+type markDisconnectedServicesExecutor struct{}
+
+func (m *markDisconnectedServicesExecutor) Run(ctx context.Context, t cereal.Task) (interface{}, error) {
+	var params DisconnectedServicesParamsV0
+	t.GetParameters(&params)
+	fmt.Printf("markDisconnectedServicesExecutor Run() w/ %+v\n", params)
+	return nil, nil
+}
