a3e2e6893bf2a520020dbc5b5f02de9daadb1f7b
Stephan Renatus <srenatus@chef.io>
2019-08-26 11:39:57 +0200

    automate-ui: make auth interceptor request a re-signin with the same method
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- components/automate-ui/src/app/services/http/http-client-auth.interceptor.ts
+++ components/automate-ui/src/app/services/http/http-client-auth.interceptor.ts
@@ -1,9 +1,14 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpErrorResponse,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { throwError as observableThrowError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { get } from 'lodash/fp';
 
 import { NgrxStateAtom } from 'app/ngrx.reducers';
 import { ChefSessionService } from 'app/services/chef-session/chef-session.service';
@@ -29,20 +34,24 @@ export class HttpClientAuthInterceptor implements HttpInterceptor {
     let headers = request.headers.set('Authorization', `Bearer ${this.chefSession.id_token}`);
     const filtered = request.params.get('unfiltered') !== 'true';
     // Uncomment here and after to clone() arg list
-    // after https://github.com/angular/angular/issues/18812 is fixed.
+    // after we've upgraded to angular 7.2+ (for this issue:
+    // https://github.com/angular/angular/issues/18812).
     // const params = request.params.delete('unfiltered');
     if (this.projects && filtered) {
       headers = headers.set('projects', this.projects);
     }
     return next
-      .handle(request.clone({
-        headers
-      })).pipe(
-        catchError((response: HttpEvent<any>) => {
-          if (get('status', response) === 401) {
-            this.chefSession.logout();
+      .handle(request.clone({ headers })).pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) {
+            // TODO(sr) When refactoring the session handling in automate-ui and ngrx,
+            // this should be dealt with: right now, if any API request gets a 401,
+            // the user will be signed out. While we attempt to save the user from
+            // having to choose their login method again (that's the `true`), we don't
+            // know on which UI page this has occourred (that's why we pass "/").
+            this.chefSession.logout('/', true);
           }
-          return observableThrowError(response);
+          return observableThrowError(error);
         }));
   }
 }
