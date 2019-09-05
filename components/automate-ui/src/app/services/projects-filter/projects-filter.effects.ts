a124e92dd97ff1795dc2a705aa5fd0fc2cd396a5
michael sorens <msorens@chef.io>
2019-09-04 22:25:11 -0700

    Add a guard function based on IAM version
    Prevent the introspect_projects call unless V2p1 or greater.
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/services/projects-filter/projects-filter.effects.ts
+++ components/automate-ui/src/app/services/projects-filter/projects-filter.effects.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Store } from '@ngrx/store';
 import { Actions, Effect, ofType } from '@ngrx/effects';
+import { NgrxStateAtom } from 'app/ngrx.reducers';
 import { interval as observableInterval, of as observableOf, Observable } from 'rxjs';
-import { catchError, mergeMap, map, tap } from 'rxjs/operators';
+import { catchError, mergeMap, map, tap, withLatestFrom, filter } from 'rxjs/operators';
 
+import { atLeastV2p1 } from 'app/entities/policies/policy.selectors';
 import { Project } from 'app/entities/projects/project.model';
 import { ProjectsFilterOption, ProjectsFilterOptionTuple } from './projects-filter.reducer';
 import { ProjectsFilterService } from './projects-filter.service';
@@ -21,6 +24,7 @@ import {
 export class ProjectsFilterEffects {
   constructor(
     private actions$: Actions,
+    private store: Store<NgrxStateAtom>,
     private projectsFilter: ProjectsFilterService,
     private requests: ProjectsFilterRequests
   ) { }
@@ -28,8 +32,10 @@ export class ProjectsFilterEffects {
   private POLLING_INTERVAL_IN_SECONDS = 120; // 2 minutes
 
   @Effect()
-  latestOptions$ = observableInterval(1000 * this.POLLING_INTERVAL_IN_SECONDS)
-    .pipe(mergeMap(this.loadOptionsAction$()));
+  latestOptions$ = observableInterval(1000 * this.POLLING_INTERVAL_IN_SECONDS).pipe(
+    withLatestFrom(this.store.select(atLeastV2p1)),
+    filter(([_, projectsEnabled]) => projectsEnabled),
+    mergeMap(this.loadOptionsAction$()));
 
   @Effect()
   loadOptions$ = this.actions$.pipe(
