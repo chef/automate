9b66fba8831bb68565611b70fe5e1236fbc243c2
Steven Danna <steve@chef.io>
2019-09-07 12:02:47 +0100

    data-feed-service: naively apply cereal library
    This uses the cereal library in the data-feed-service in the most
    naive way. It now uses the cereal library to manage the periodic task
    management. The entire polling operation is a single task and we are
    not currently saving the last polled interval.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- components/data-feed-service/server/grpc.go
+++ components/data-feed-service/server/grpc.go
@@ -4,25 +4,28 @@ import (
 	"context"
 	"net"
 
-	"fmt"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	"github.com/chef/automate/components/data-feed-service/config"
 	"github.com/chef/automate/lib/grpc/health"
 	"github.com/chef/automate/lib/grpc/secureconn"
 	"github.com/chef/automate/lib/tracing"
-	log "github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
-// NewGRPC creates the gRPC server.
-func NewGRPC(ctx context.Context, config *config.DataFeedConfig) *grpc.Server {
-	// Set up our gRPC connection factory
-	connFactory := secureconn.NewFactory(*config.ServiceCerts)
+type Server struct {
+	cfg    *config.DataFeedConfig
+	health *health.Service
+}
 
-	// Register our API
+// NewGRPC creates the gRPC server.
+func NewGRPC(ctx context.Context, config *config.DataFeedConfig, connFactory *secureconn.Factory) *grpc.Server {
 	grpcServer := connFactory.NewServer(tracing.GlobalServerInterceptor())
-	srv := New(config)
+	srv := &Server{
+		cfg:    config,
+		health: health.NewService(),
+	}
 	health.RegisterHealthServer(grpcServer, srv.health)
 
 	// Register reflection service on gRPC server.
@@ -31,15 +34,13 @@ func NewGRPC(ctx context.Context, config *config.DataFeedConfig) *grpc.Server {
 }
 
 // StartGRPC starts the gRPC server
-func StartGRPC(ctx context.Context, config *config.DataFeedConfig) error {
-	g := NewGRPC(ctx, config)
+func StartGRPC(ctx context.Context, config *config.DataFeedConfig, connFactory *secureconn.Factory) error {
+	g := NewGRPC(ctx, config, connFactory)
 
 	// Create our listener channel
 	listener, err := net.Listen("tcp", config.ListenAddress())
 	if err != nil {
-		log.WithFields(log.Fields{
-			"err": err,
-		}).Fatal(fmt.Sprintf("could not listen on address: %s", config.ListenAddress()))
+		return errors.Wrapf(err, "could not listen on address: %s", config.ListenAddress())
 	}
 
 	return g.Serve(listener)
