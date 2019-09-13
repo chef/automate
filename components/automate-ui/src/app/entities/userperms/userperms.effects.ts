99646e81e09d449d244de29bc5f814333ab2c116
michael sorens <msorens@chef.io>
2019-09-13 14:51:56 -0700

    Make IntrospectAll/Some be mindful of request age
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/entities/userperms/userperms.effects.ts
+++ components/automate-ui/src/app/entities/userperms/userperms.effects.ts
@@ -1,9 +1,20 @@
-import { debounceTime, mergeMap, map, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Store } from '@ngrx/store';
 import { Actions, Effect, ofType } from '@ngrx/effects';
+import {
+  debounceTime,
+  mergeMap,
+  map,
+  catchError,
+  withLatestFrom,
+  switchMap,
+  filter
+} from 'rxjs/operators';
 import { of } from 'rxjs';
+import * as moment from 'moment';
 
+import { NgrxStateAtom } from 'app/ngrx.reducers';
 import {
   UserPermsTypes,
   GetAllUserPermsSuccess,
@@ -17,27 +28,35 @@ import {
   GetUserParamPermsFailure
 } from './userperms.actions';
 import { UserPermsRequests } from './userperms.requests';
+import { getlastFetchTime } from './userperms.selectors';
 
 @Injectable()
 export class UserPermEffects {
   constructor(
     private actions$: Actions,
-    private requests: UserPermsRequests
+    private requests: UserPermsRequests,
+    private store: Store<NgrxStateAtom>
   ) { }
 
-  // include debounce here because each request of this type is a true duplicate
   @Effect()
   fetchAllPerms$ = this.actions$.pipe(
     ofType(UserPermsTypes.GET_ALL),
-    debounceTime(20),
-    mergeMap(() => this.requests.fetchAll().pipe(
-      map((resp: UserPermsResponsePayload) => new GetAllUserPermsSuccess(resp.endpoints)),
-      catchError((error: HttpErrorResponse) => of(new GetAllUserPermsFailure(error))))));
+    debounceTime(20), // each request of this type is a true duplicate so debounce them
+    withLatestFrom(this.store.select(getlastFetchTime)),
+    filter(([_action, lastTime]) => this.stale(lastTime)),
+    switchMap(() => {
+      return this.requests.fetchAll().pipe(
+        map((resp: UserPermsResponsePayload) => new GetAllUserPermsSuccess(resp.endpoints)),
+        catchError((error: HttpErrorResponse) => of(new GetAllUserPermsFailure(error))));
+    }));
 
   @Effect()
   fetchSomePerms$ = this.actions$.pipe(
     ofType(UserPermsTypes.GET_SOME),
-    mergeMap((action: GetSomeUserPerms) => this.requests.fetchSome(action.payload).pipe(
+    withLatestFrom(this.store.select(getlastFetchTime)),
+    filter(([_action, lastTime]) => this.stale(lastTime)),
+    mergeMap(
+      ([action, _]: [GetSomeUserPerms, Date]) => this.requests.fetchSome(action.payload).pipe(
       map((resp: UserPermsResponsePayload) => new GetSomeUserPermsSuccess(resp.endpoints)),
       catchError((error: HttpErrorResponse) => of(new GetSomeUserPermsFailure(error))))));
 
@@ -47,4 +66,10 @@ export class UserPermEffects {
     mergeMap((action: GetUserParamPerms) => this.requests.fetchParameterized(action.payload).pipe(
       map((resp: UserPermsResponsePayload) => new GetUserParamPermsSuccess(resp.endpoints)),
       catchError((error: HttpErrorResponse) => of(new GetUserParamPermsFailure(error))))));
+
+  private freshLimitInSeconds = 30;
+
+  private stale(lastTime: Date): boolean {
+    return moment().diff(moment(lastTime), 'seconds') > this.freshLimitInSeconds;
+  }
 }
