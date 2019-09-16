906fce01df838e9a783540a4684ce69abb8a02fd
seajamied <jdegnan@chef.io>
2019-09-16 09:45:54 -0700

    Add failing test cases

--- components/automate-ui/src/app/components/projects-dropdown/projects-dropdown.component.spec.ts
+++ components/automate-ui/src/app/components/projects-dropdown/projects-dropdown.component.spec.ts
@@ -26,48 +26,69 @@ describe("ProjectsDropdownComponent", () => {
 
   describe("#projectsArray", () => {
     it("#projectsArray returns an object naturally ordered by name", () => {
-      // tslint:disable-next-line: no-unused-expression
-      let unsortedProjects: [
+      const unsortedProjects: {
+        checked: boolean;
+        id: string;
+        name: string;
+        status: string;
+        type: string;
+      }[] = [
         {
-          checked: false;
-          id: "123abc";
-          name: "123abc";
-          status: "NO_RULES";
-          type: "CUSTOM";
+          checked: false,
+          id: "123abc",
+          name: "123abc",
+          status: "NO_RULES",
+          type: "CUSTOM"
         },
         {
-          checked: false;
-          id: "project-6";
-          name: "project-6";
-          status: "NO_RULES";
-          type: "CUSTOM";
+          checked: false,
+          id: "project-6",
+          name: "project-6",
+          status: "NO_RULES",
+          type: "CUSTOM"
         },
         {
-          checked: false;
-          id: "12345zero";
-          name: "12345zero";
-          status: "NO_RULES";
-          type: "CUSTOM";
+          checked: false,
+          id: "12345zero",
+          name: "12345zero",
+          status: "NO_RULES",
+          type: "CUSTOM"
         },
         {
-          checked: false;
-          id: "123xyz";
-          name: "123xyz";
-          status: "NO_RULES";
-          type: "CUSTOM";
+          checked: false,
+          id: "123xyz",
+          name: "123xyz",
+          status: "NO_RULES",
+          type: "CUSTOM"
         },
         {
-          checked: false;
-          id: "abc123";
-          name: "abc123";
-          status: "NO_RULES";
-          type: "CUSTOM";
+          checked: false,
+          id: "abc123",
+          name: "abc123",
+          status: "NO_RULES",
+          type: "CUSTOM"
         }
       ];
       // usage and expects go here
       const dropdownComponent = new ProjectsDropdownComponent();
-      dropdownComponent.projects = { inputObj: unsortedProjects };
-      expect(dropdownComponent.projectsArray().length).toBe(1);
+      dropdownComponent.projects = unsortedProjects;
+
+      expect(dropdownComponent.projectsArray().length).toBe(5);
+      expect(dropdownComponent.projectsArray()[0]).toEqual(
+        jasmine.objectContaining({ name: "123abc" })
+      );
+      expect(dropdownComponent.projectsArray()[1]).toEqual(
+        jasmine.objectContaining({ name: "123xyz" })
+      );
+      expect(dropdownComponent.projectsArray()[2]).toEqual(
+        jasmine.objectContaining({ name: "123xyz" })
+      );
+      expect(dropdownComponent.projectsArray()[3]).toEqual(
+        jasmine.objectContaining({ name: "123xyz" })
+      );
+      expect(dropdownComponent.projectsArray()[4]).toEqual(
+        jasmine.objectContaining({ name: "123xyz" })
+      );
     });
   });
 });
