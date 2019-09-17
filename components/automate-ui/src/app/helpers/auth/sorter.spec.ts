32273336750f933bff8c02af49266db9fad7b1bc
seajamied <jdegnan@chef.io>
2019-09-17 12:47:32 -0700

    WIP - Function is visually working but failing tests
    Not sure why yet, It may have something to do with typescript declarations
    Visually, the expected output is matching what it is supposed to be.

--- components/automate-ui/src/app/helpers/auth/sorter.spec.ts
+++ components/automate-ui/src/app/helpers/auth/sorter.spec.ts
@@ -1,9 +1,9 @@
 
 import { ChefSorters } from './sorter';
 
-describe('normalSort', () => {
+fdescribe('normalSort', () => {
 
-    const inputObject: {} = [
+    const inputObject: object[] = [
         { checked: false, id: '123abc', name: '123abc', status: 'NO_RULES', type: 'CUSTOM'},
         { checked: false, id: 'project-6', name: 'project-6', status: 'NO_RULES', type: 'CUSTOM'},
         { checked: false, id: '12345zero', name: '12345zero', status: 'NO_RULES', type: 'CUSTOM'},
@@ -11,11 +11,35 @@ describe('normalSort', () => {
         { checked: false, id: 'abc123', name: 'abc123', status: 'NO_RULES', type: 'CUSTOM'}
       ];
 
-    const outputByName: {} = [
+    // const sortedOutput: object[] = [
+    //     { checked: false, id: '123abc', name: 'abc123', status: 'NO_RULES', type: 'CUSTOM' },
+    //     { checked: false, id: '123xyz', name: '123xyz', status: 'NO_RULES', type: 'CUSTOM' },
+    //     { checked: false, id: '12345zero', name: '12345zero', status: 'NO_RULES', type: 'CUSTOM' },
+    //     { checked: false, id: 'abc123', name: 'abc123', status: 'NO_RULES', type: 'CUSTOM' },
+    //     { checked: false, id: 'project-6', name: 'project-6', status: 'NO_RULES', type: 'CUSTOM' }
+    // ];
 
-    ]
+    const sortedNames: string[] = ['123abc', '123xyz', '12345zero', 'abc123', 'project-6'];
 
     it('returns a normally sorted array by chosen property', () => {
-        expect(ChefSorters.normalSort(inputObject)).toBe(someOutput);
+        expect(ChefSorters.normalSort(inputObject).map(p => p.name)).toBe(sortedNames);
     });
 });
+
+// Expected
+// [
+//     Object({ checked: false, id: '123abc', name: '123abc', status: 'NO_RULES', type: 'CUSTOM' }), 
+//     Object({ checked: false, id: '123xyz', name: '123xyz', status: 'NO_RULES', type: 'CUSTOM' }), 
+//     Object({ checked: false, id: '12345zero', name: '12345zero', status: 'NO_RULES', type: 'CUSTOM' }), 
+//     Object({ checked: false, id: 'abc123', name: 'abc123', status: 'NO_RULES', type: 'CUSTOM' }), 
+//     Object({ checked: false, id: 'project-6', name: 'project-6', status: 'NO_RULES', type: 'CUSTOM' })
+// ] 
+
+// to be
+// [
+//     Object({ checked: false, id: '123abc', name: 'abc123', status: 'NO_RULES', type: 'CUSTOM' }), 
+//     Object({ checked: false, id: '123xyz', name: '123xyz', status: 'NO_RULES', type: 'CUSTOM' }), 
+//     Object({ checked: false, id: '12345zero', name: '12345zero', status: 'NO_RULES', type: 'CUSTOM' }), 
+//     Object({ checked: false, id: 'abc123', name: 'abc123', status: 'NO_RULES', type: 'CUSTOM' }), 
+//     Object({ checked: false, id: 'project-6', name: 'project-6', status: 'NO_RULES', type: 'CUSTOM' })
+// ]
