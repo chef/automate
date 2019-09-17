443f798abe38d90ef4d3c4d959ee631090774126
Jay Mundrawala <jmundrawala@chef.io>
2019-09-17 06:36:49 -0700

    Verify the manifest signature (#1486)
    Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

--- components/automate-deployment/pkg/manifest/client/http_test.go
+++ components/automate-deployment/pkg/manifest/client/http_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -32,7 +33,7 @@ func TestGetCurrentManifest(t *testing.T) {
 	defer ts.Close()
 
 	testFmt := fmt.Sprintf("%s/%%s", ts.URL)
-	client := client.NewHTTPClient(client.LatestURLFormat(testFmt))
+	client := client.NewHTTPClient(client.LatestURLFormat(testFmt), client.NoVerify(true))
 	manifest, err := client.GetCurrentManifest(context.Background(), "current")
 	require.NoError(t, err)
 	assert.Equal(t, 1, len(manifest.Packages))
@@ -52,7 +53,7 @@ func TestGetManifest(t *testing.T) {
 	defer ts.Close()
 
 	testFmt := fmt.Sprintf("%s/%%s", ts.URL)
-	client := client.NewHTTPClient(client.URLFormat(testFmt))
+	client := client.NewHTTPClient(client.URLFormat(testFmt), client.NoVerify(true))
 	manifest, err := client.GetManifest(context.Background(), "20180207073355")
 	require.NoError(t, err)
 	assert.Equal(t, 1, len(manifest.Packages))
@@ -72,9 +73,115 @@ func TestGetManifestReturnNoSuchManifestOn404(t *testing.T) {
 	defer ts.Close()
 
 	testFmt := fmt.Sprintf("%s/%%s", ts.URL)
-	client := client.NewHTTPClient(client.URLFormat(testFmt))
+	client := client.NewHTTPClient(client.URLFormat(testFmt), client.NoVerify(true))
 	_, err := client.GetManifest(context.Background(), "20180207073355")
 	require.Error(t, err)
 	_, ok := err.(*manifest.NoSuchManifestError)
 	require.True(t, ok, "error should be a NoSuchManifestError")
+}
+
+func TestVerification(t *testing.T) {
+	chefSignedManifest :=
+		`{
+  "schema_version": "1",
+  "build": "20181207131525",
+  "hab": [
+    "core/hab/0.69.0/20181127182011",
+    "core/hab-sup/0.69.0/20181127183841",
+    "core/hab-launcher/9106/20181126205526"
+  ],
+  "hab_build": "core/hab/0.59.0/20180712155441",
+  "git_sha": "45e9604294d1b3908f9104e19a5538770ed8d4b2",
+  "packages": [
+    "chef/authn-service/0.1.0/20181206101503",
+    "chef/authz-service/0.1.0/20181206164837",
+    "chef/automate-chef-io/0.1.0/20181206205516",
+    "chef/automate-cli/0.1.0/20181207131102",
+    "chef/automate-cs-bookshelf/12.18.4/20181130132209",
+    "chef/automate-cs-nginx/12.18.3/20181130135245",
+    "chef/automate-cs-oc-bifrost/12.18.4/20181130132256",
+    "chef/automate-cs-oc-erchef/12.18.4/20181130132353",
+    "chef/automate-debug/0.1.0/20181011124837",
+    "chef/automate-dex/0.1.0/20181130131252",
+    "chef/automate-elasticsearch/6.2.2/20181130130730",
+    "chef/automate-gateway/0.1.0/20181207011834",
+    "chef/automate-load-balancer/0.1.0/20181130133337",
+    "chef/automate-postgresql/9.6.8/20180807041839",
+    "chef/automate-ui/2.0.0/20181206212345",
+    "chef/automate-workflow-nginx/2.8.26/20181130135402",
+    "chef/automate-workflow-server/2.8.30/20181207130859",
+    "chef/backup-gateway/0.1.0/20181204224819",
+    "chef/compliance-service/1.11.1/20181207011536",
+    "chef/config-mgmt-service/0.1.0/20181206103043",
+    "chef/data-lifecycle-service/0.0.1/20181206103221",
+    "chef/deployment-service/0.1.0/20181207131244",
+    "chef/es-sidecar-service/1.0.0/20181206103749",
+    "chef/event-service/0.1.0/20181206102132",
+    "chef/ingest-service/0.1.0/20181206103910",
+    "chef/license-control-service/1.0.0/20181206102259",
+    "chef/local-user-service/0.1.0/20181206104041",
+    "chef/notifications-service/1.0.0/20181130164345",
+    "chef/secrets-service/1.0.0/20181206102417",
+    "chef/session-service/0.1.0/20181206102548",
+    "chef/teams-service/0.1.0/20181206102727",
+    "core/rsync/3.1.2/20180608145950"
+  ]
+}`
+
+	manifestSignature := `-----BEGIN PGP SIGNATURE-----
+
+iF0EABEIAB0WIQQRaF25LwNkCi/+fKgpQKupg++CagUCXApzDgAKCRApQKupg++C
+aiqMAJ9eLFYKiyyXe0nJekBdvUPfVoxYGwCfcTkOHvDPo1UFsOeh0QppYDkTnjE=
+=KMyQ
+-----END PGP SIGNATURE-----`
+
+	t.Run("With valid signature and data", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasSuffix(r.RequestURI, ".asc") {
+				w.Write([]byte(manifestSignature))
+			} else {
+				w.Write([]byte(chefSignedManifest))
+			}
+		}))
+		defer ts.Close()
+
+		testFmt := fmt.Sprintf("%s/%%s", ts.URL)
+		client := client.NewHTTPClient(client.LatestURLFormat(testFmt))
+		_, err := client.GetCurrentManifest(context.Background(), "current")
+		require.NoError(t, err)
+	})
+
+	t.Run("With invalid data", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasSuffix(r.RequestURI, ".asc") {
+				w.Write([]byte(manifestSignature))
+			} else {
+				w.Write([]byte("not valid"))
+			}
+		}))
+		defer ts.Close()
+
+		testFmt := fmt.Sprintf("%s/%%s", ts.URL)
+		client := client.NewHTTPClient(client.LatestURLFormat(testFmt))
+		_, err := client.GetCurrentManifest(context.Background(), "current")
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "invalid signature")
+	})
+
+	t.Run("Cannot download signature", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasSuffix(r.RequestURI, ".asc") {
+				w.WriteHeader(404)
+			} else {
+				w.Write([]byte(chefSignedManifest))
+			}
+		}))
+		defer ts.Close()
+
+		testFmt := fmt.Sprintf("%s/%%s", ts.URL)
+		client := client.NewHTTPClient(client.LatestURLFormat(testFmt))
+		_, err := client.GetCurrentManifest(context.Background(), "current")
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "404 Not Found")
+	})
 }
