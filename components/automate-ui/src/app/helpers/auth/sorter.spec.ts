61683327e40c5fc85cf42f5c6056336f1955b6ed
seajamied <jdegnan@chef.io>
2019-09-17 10:24:49 -0700

    Build normalSort function testing scaffold
    Signed-off-by: seajamied <jdegnan@chef.io>

---
+++ components/automate-ui/src/app/helpers/auth/sorter.spec.ts
@@ -0,0 +1,9 @@
+
+
+
+describe('normalSort', () => {
+
+    it('returns a normally sorted array by chosen property', () => {
+        expect(someArrayOfObjects.normalSort(stuff)).toBe(someOutput);
+    });
+});
---
+++ components/automate-ui/src/app/helpers/auth/sorter.ts
@@ -0,0 +1,7 @@
+
+export class ChefSorters {
+
+    public static normalSort() {
+
+    }
+}
