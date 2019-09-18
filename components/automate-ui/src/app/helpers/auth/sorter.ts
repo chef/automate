ead4c105802f8caad4bfff5e2ad6c88bada931fa
seajamied <jdegnan@chef.io>
2019-09-18 14:51:22 -0700

    Refactor normalSort to accept input as an Array
    Previously, the normalSort function was intaking an object, and then using Object.values() to
    Pull out the values and convert to an Array.  After looking to use this across
    the codebase, I realized that there are different ways a user may want to present their
    input.  I've chosen to allow this up to the user to decide prior to using normalSort.

--- components/automate-ui/src/app/helpers/auth/sorter.ts
+++ components/automate-ui/src/app/helpers/auth/sorter.ts
@@ -1,14 +1,18 @@
 
 export class ChefSorters {
 
-    public static naturalSort(input: Array<any>, property: string): Array<any> {
+    // normalSort takes in an Object and returns an Array of Objects
+    // sorted by the property chosen.
+
+    public static normalSort(input: Array<any>, property: string): Array<any> {
         // per @msorens https://github.com/chef/a2/pull/4434
         // Stable sort so 'a' always comes before 'A'.
 
         // propComparator function idea from Dave Newton
         // https://stackoverflow.com/questions
         const propComparator = (propName) =>
-            (a, b) => a[propName].localeCompare(b[propName], undefined, opts) ||
+            (a, b) => a[propName].localeCompare(b[propName], undefined, { numeric: true,
+                                                                          sensitivity: 'base' }) ||
                       a[propName].localeCompare(b[propName], undefined, { numeric: true });
 
         const sortedArray = input.sort(propComparator(property));
