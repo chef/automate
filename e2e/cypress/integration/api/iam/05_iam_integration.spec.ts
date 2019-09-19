4b5a2f7f3a45c6dadcfd09c071999cdf7fb8bbe1
Blake Johnson <bstier@chef.io>
2019-09-19 10:32:14 -0700

    Iterate over all resources for test
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- e2e/cypress/integration/api/iam/05_iam_integration.spec.ts
+++ e2e/cypress/integration/api/iam/05_iam_integration.spec.ts
@@ -5,30 +5,27 @@ describeIfIAMV2p1('assigning projects', () => {
   let apiToken = '';
 
   const modifyError = 'cannot modify a project for this object';
-
-  // TODO iterate over these resources on each test pass
-  // const resources = ['tokens', 'teams', 'policies', 'roles'];
-
   const cypressPrefix = 'iam-integration';
   const resource = {
     id: `${cypressPrefix}-${Cypress.moment().format('MMDDYYhhmm')}`,
     name: 'test',
     projects: []
   };
-
   const notFoundProjectId = 'not-found-project';
   const authorizedProject1 = `${cypressPrefix}-authorized-project-1`;
   const authorizedProject2 = `${cypressPrefix}-authorized-project-2`;
   const unauthorizedProjectId = `${cypressPrefix}-unauthorized-project`;
   const policyId = `${cypressPrefix}-policy`;
   const tokenId = `${cypressPrefix}-token`;
+  const iamResourcesToTest = ['roles', 'tokens', 'teams'];
+  const objectsToCleanUp = iamResourcesToTest.concat(['projects']);
 
   before(() => {
     cy.adminLogin('/').then(() => {
       adminIdToken = JSON.parse(<string>localStorage.getItem('chef-automate-user')).id_token;
 
       // TODO cleanup everything in resources + projects
-      cy.cleanupV2IAMObjectsByIDPrefixes(adminIdToken, cypressPrefix, ['projects', 'tokens']);
+      cy.cleanupV2IAMObjectsByIDPrefixes(adminIdToken, cypressPrefix, objectsToCleanUp);
 
       for (const id of [unauthorizedProjectId, authorizedProject1, authorizedProject2]) {
         cy.request({
@@ -86,87 +83,115 @@ describeIfIAMV2p1('assigning projects', () => {
   });
 
   after(() => {
-    // TODO cleanup everything in resources + projects
-    cy.cleanupV2IAMObjectsByIDPrefixes(adminIdToken, cypressPrefix, ['projects', 'tokens']);
+    cy.cleanupV2IAMObjectsByIDPrefixes(adminIdToken, cypressPrefix, objectsToCleanUp);
   });
 
-  // iterate over resources
-  ['tokens'].forEach((token: string) => {
-    it(`when creating ${token}, successfully assigns an existing authorized project`, () => {
+  // iterate over iam resources
+  iamResourcesToTest.forEach((iamResources: string) => {
+    // get the singular form of the resource for json
+    const iamResource = `${iamResources}`.slice(0, -1);
+
+    it(`when creating ${iamResources}, successfully assigns an existing authorized project`, () => {
       cy.request({
         headers: { 'api-token': apiToken },
         method: 'POST',
-        url: `/apis/iam/v2beta/${token}`,
-        body: {id: resource.id, name: resource.name, projects: [authorizedProject1]}
-      }).then((resp) => {
-        assert.equal(resp.status, 200);
-        assert.isUndefined(resp.body.error);
-        assert.deepEqual(resp.body.token.projects, [authorizedProject1]);
-      });
-    });
-
-    it(`when updating ${token}, successfully assigns an existing authorized project`, () => {
-      cy.request({
-        headers: { 'api-token': apiToken },
-        method: 'PUT',
-        url: `/apis/iam/v2beta/${token}/${resource.id}`,
+        url: `/apis/iam/v2beta/${iamResources}`,
         body: {
-          name: `${resource.name} UpdatedName`,
-          projects: [authorizedProject1, authorizedProject2]
+          id: resource.id,
+          name: resource.name,
+          projects: [authorizedProject1],
+          actions: ['iam:users:create']
         }
       }).then((resp) => {
         assert.equal(resp.status, 200);
         assert.isUndefined(resp.body.error);
-        assert.deepEqual(resp.body.token.projects, [authorizedProject1, authorizedProject2]);
+        assert.deepEqual(resp.body[`${iamResource}`]['projects'], [authorizedProject1]);
       });
     });
 
-    it(`when creating ${token}, fails to assign a non-existing project`, () => {
+    it(`when updating ${iamResources}, successfully assigns an existing authorized project`, () => {
+      cy.request({
+        headers: { 'api-token': apiToken },
+        method: 'PUT',
+        url: `/apis/iam/v2beta/${iamResources}/${resource.id}`,
+        body: {
+          name: `${resource.name} UpdatedName`,
+          projects: [authorizedProject1, authorizedProject2],
+          actions: ['iam:users:create']
+        }
+      }).then((resp) => {
+        assert.equal(resp.status, 200);
+        assert.isUndefined(resp.body.error);
+        assert.deepEqual(
+            resp.body[`${iamResource}`]['projects'],
+            [authorizedProject1, authorizedProject2]);
+      });
+    });
+
+    it(`when creating ${iamResources}, fails to assign a non-existing project`, () => {
         cy.request({
           headers: { 'api-token': apiToken },
           method: 'POST',
-          url: `/apis/iam/v2beta/${token}`,
+          url: `/apis/iam/v2beta/${iamResources}`,
           failOnStatusCode: false,
-          body: { id: `${resource.id}-2`, name: resource.name, projects: [notFoundProjectId]}
+          body: {
+            id: `${resource.id}-2`,
+            name: resource.name,
+            projects: [notFoundProjectId],
+            actions: ['iam:users:create']
+          }
         }).then((resp) => {
           assert.equal(resp.status, 404);
           expect(resp.body.error).to.have.string(modifyError);
         });
     });
 
-    it(`when updating ${token}, fails to assign a non-existing project`, () => {
+    it(`when updating ${iamResources}, fails to assign a non-existing project`, () => {
       cy.request({
         headers: { 'api-token': apiToken },
         method: 'PUT',
-        url: `/apis/iam/v2beta/${token}/${resource.id}`,
+        url: `/apis/iam/v2beta/${iamResources}/${resource.id}`,
         failOnStatusCode: false,
-        body: { name: `${resource.name} UpdatedName`, projects: [notFoundProjectId]}
-      }).then((resp) => {
+        body: {
+          name: `${resource.name} UpdatedName`,
+          projects: [notFoundProjectId],
+          actions: ['iam:users:create']
+        }
+          }).then((resp) => {
         assert.equal(resp.status, 404);
         expect(resp.body.error).to.have.string(modifyError);
       });
     });
 
-    it(`when creating ${token}, fails to assign an unauthorized project`, () => {
+    it(`when creating ${iamResources}, fails to assign an unauthorized project`, () => {
       cy.request({
         headers: { 'api-token': apiToken },
         method: 'POST',
-        url: `/apis/iam/v2beta/${token}`,
+        url: `/apis/iam/v2beta/${iamResources}`,
         failOnStatusCode: false,
-        body: { id: `${resource.id}-unauth`, name: resource.name, projects: [unauthorizedProjectId]}
+        body: {
+          id: `${resource.id}-unauth`,
+          name: resource.name,
+          projects: [unauthorizedProjectId],
+          actions: ['iam:users:create']
+        }
       }).then((resp) => {
         assert.equal(resp.status, 403);
         expect(resp.body.error).to.have.string(modifyError);
       });
     });
 
-    it(`when updating ${token}, fails to assign an unauthorized project`, () => {
+    it(`when updating ${iamResources}, fails to assign an unauthorized project`, () => {
       cy.request({
         headers: { 'api-token': apiToken },
         method: 'PUT',
-        url: `/apis/iam/v2beta/${token}/${resource.id}`,
+        url: `/apis/iam/v2beta/${iamResources}/${resource.id}`,
         failOnStatusCode: false,
-        body: {name: `${resource.name} UpdatedName`, projects: [unauthorizedProjectId]}
+        body: {
+          name: `${resource.name} UpdatedName`,
+          projects: [unauthorizedProjectId],
+          actions: ['iam:users:create']
+        }
       }).then((resp) => {
         assert.equal(resp.status, 403);
         expect(resp.body.error).to.have.string(modifyError);
