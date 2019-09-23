1ac35e20654fb484f7f82a417b7e40eb2433f471
Steven Danna <steve@chef.io>
2019-09-23 13:02:10 +0100

    More github access token fixes (#1682)
    In the previous fix I was under the mistaken impression that only App
    tokens supported the x-access-token prefix, but I've now tested this
    with personal access tokens as well and it appears they both work with
    that as the username. Using this single method will hopefully keep us
    a bit more consistent.
    
    This hopefully also fixes the build issues as the core problem was
    that I forgot to rename the environment variable in that pipeline.
    
    > There may be times when we are powerless to prevent injustice, but
    > there must never be a time when we fail to protest.
    
                                                 -Elie Wiesel
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- components/automate-chef-io/scripts/clone_hugo.sh
+++ components/automate-chef-io/scripts/clone_hugo.sh
@@ -2,9 +2,7 @@
 
 clone_url="https://github.com/chef/chef-hugo-theme.git"
 if [[ -n "$GITHUB_TOKEN" ]];then
-    clone_url="https://${GITHUB_TOKEN}@github.com/chef/chef-hugo-theme.git"
-elif [[ -n "$GITHUB_APP_TOKEN" ]];then
-    clone_url="https://x-access-token:${GITHUB_APP_TOKEN}@github.com/chef/chef-hugo-theme.git"
+    clone_url="https://x-access-token:${GITHUB_TOKEN}@github.com/chef/chef-hugo-theme.git"
 fi
 
 git clone "$clone_url" themes/chef
