b1338a399d9dd3eae79c262b12953ea92f29494c
seajamied <jdegnan@chef.io>
2019-09-24 10:16:50 -0700

    Add comments and remove console.log
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/helpers/datetime/datetime.ts
+++ components/automate-ui/src/app/helpers/datetime/datetime.ts
@@ -1,3 +1,4 @@
 export class DateTime {
+    // formatted for use with moment.js -- https://momentjs.com/docs/#/displaying/format/
     public static readonly RFC2822: string = 'ddd, DD MMM YYYY HH:mm:ss [UTC]';
 }
