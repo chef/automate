b210cb44c060b871504bb12293cecd65cc361af3
Alex Pop <apop@chef.io>
2019-09-24 16:24:14 +0100

    Add additional control list integration tests with filters
    Signed-off-by: Alex Pop <apop@chef.io>

--- components/compliance-service/api/tests/010_controls_list_spec.rb
+++ components/compliance-service/api/tests/010_controls_list_spec.rb
@@ -370,4 +370,60 @@ describe File.basename(__FILE__) do
     assert_equal(2, control_item_array.size)
     assert_equal_json_content(expected_data, actual_data)
   end
+
+  it "control list items with a size of 4 filtered by control_tag" do
+    actual_data = GRPC reporting, :list_control_items, Reporting::ControlItemRequest.new(
+        filters: [
+            Reporting::ListFilter.new(type: 'start_time', values: ['2018-03-01T23:59:59Z']),
+            Reporting::ListFilter.new(type: 'end_time', values: ['2018-03-04T23:59:59Z']),
+            Reporting::ListFilter.new(type: 'control_tag:scoop', values: ['icecream'])
+        ],
+        size: 4
+    )
+    expected_data = {
+      "controlItem" =>
+        [
+          {
+            "id"=>"apache-01",
+            "title"=>"Apache should be running",
+            "profile"=> {
+              "title"=>"DevSec Apache Baseline",
+              "id"=>"41a02784bfea15592ba2748d55927d8d1f9da205816ef18d3bb2ebe4c5ce18a9",
+              "version"=>"2.0.1"
+            },
+            "impact" => 1,
+            "endTime" => "2018-03-04T09:18:41Z",
+            "controlSummary" => {
+              "total" => 1,
+              "passed" => {},
+              "skipped" => {
+                "total"=>1
+              },
+              "failed" => {}
+            },
+            "stringTags" => [
+              {
+                "key" => "scoop",
+                "values" => [ "icecream" ]
+              }
+            ]
+          }
+        ]
+      }
+    assert_equal_json_content(expected_data, actual_data)
+  end
+
+  it "control list items filtered by environment and platform" do
+    actual_data = GRPC reporting, :list_control_items, Reporting::ControlItemRequest.new(
+        filters: [
+            Reporting::ListFilter.new(type: 'start_time', values: ['2018-03-01T23:59:59Z']),
+            Reporting::ListFilter.new(type: 'end_time', values: ['2018-03-04T23:59:59Z']),
+            Reporting::ListFilter.new(type: 'environment', values: ['DevSec Prod Zeta', 'missing']),
+            Reporting::ListFilter.new(type: 'platform', values: ['windows', 'missing'])
+        ],
+        size: 1000
+    )
+    control_item_array = actual_data['control_item']
+    assert_equal(14, control_item_array.size)
+  end
 end
