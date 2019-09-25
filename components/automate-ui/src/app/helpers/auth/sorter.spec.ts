e40558d43507349dea8b6f1bc4da5832f566e512
seajamied <jdegnan@chef.io>
2019-09-25 10:04:46 -0700

    Copied over work for natural sort function
    I massively messed up a rebase on natural-sort-library-branch and
    decided it would be a quicker fix to just move the work over to a clean
    branch.
    
    Signed-off-by: seajamied <jdegnan@chef.io>

---
+++ components/automate-ui/src/app/helpers/auth/sorter.spec.ts
@@ -0,0 +1,45 @@
+import { ChefSorters } from './sorter';
+
+describe('naturalSort', () => {
+
+    const inputObject1: any[] = [
+        { checked: false, id: 'B', name: '123abc', status: 'NO_RULES', type: 'CUSTOM' },
+        { checked: false, id: '3', name: 'project-6', status: 'NO_RULES', type: 'CUSTOM' },
+        { checked: false, id: 'a', name: '12345zero', status: 'NO_RULES', type: 'CUSTOM' },
+        { checked: false, id: '1', name: '123xyz', status: 'NO_RULES', type: 'CUSTOM' },
+        { checked: false, id: 'abc123', name: 'abc123', status: 'NO_RULES', type: 'CUSTOM' }
+    ];
+
+    const inputObject2: any[] = [
+        { checked: false, id: 'a', name: 'one', status: 'NO_RULES', type: 'CUSTOM' },
+        { checked: false, id: 'A', name: 'two', status: 'NO_RULES', type: 'CUSTOM' },
+        { checked: false, id: '123', name: 'three', status: 'NO_RULES', type: 'CUSTOM' },
+        { checked: false, id: '1234', name: 'four', status: 'NO_RULES', type: 'CUSTOM' },
+        { checked: false, id: 'abc123', name: 'five', status: 'NO_RULES', type: 'CUSTOM' }
+    ];
+
+    const sortedNames: string[] = ['123abc', '123xyz', '12345zero', 'abc123', 'project-6'];
+    const sortedIds: string[] = ['1', '3', 'a', 'abc123', 'B'];
+    const sortedIds2: string[] = ['123', '1234', 'a', 'A', 'abc123'];
+
+
+    it('returns a normally sorted array by chosen property', () => {
+        // related to inputObject1
+        const nameOutput = ChefSorters.naturalSort(inputObject1, 'name')
+            .map(p => p.name);
+
+        const idOutput = ChefSorters.naturalSort(inputObject1, 'id')
+            .map(p => p.id);
+
+        expect(nameOutput).toEqual(sortedNames);
+        expect(idOutput).toEqual(sortedIds);
+
+        // related to inputObject2
+        const idOutput2 = ChefSorters.naturalSort(inputObject2, 'id')
+            .map(p => p.id);
+
+        // inputObject2
+        expect(idOutput2).toEqual(sortedIds2);
+
+    });
+});
---
+++ components/automate-ui/src/app/helpers/auth/sorter.ts
@@ -0,0 +1,35 @@
+export class ChefSorters {
+    // ***********
+    // naturalSort takes in an Array of Objects and returns an Array of Objects
+    // sorted 'naturally' by the property chosen.
+    // @input: Array of Objects
+    // @property: string or an Array of strings, max length is 2 properties
+    //            when using an Array, will sort first by string one, then by string 2;
+    //
+    // example use with single prop:  ChefSorters.naturalSort(users, 'name');
+    // example use with dual prop:    ChefSorters.naturalSort(users, ['name', 'id']);
+    // ***********
+
+    public static naturalSort(input: Array<any>, property: Array<string> | string): Array<any> {
+        // per @msorens https://github.com/chef/a2/pull/4434
+        // Stable sort so 'a' always comes before 'A'.
+        const opts = { numeric: true, sensitivity: 'base' };
+
+        // propComparator function idea from Dave Newton
+        // https://stackoverflow.com/questions
+        const propComparator = (propName) => {
+            return (a, b) => a[propName].localeCompare(b[propName], undefined, opts) ||
+                a[propName].localeCompare(b[propName], undefined, { numeric: true });
+        };
+
+        const dualPropComparator = (propOne, propTwo) => {
+            return (a, b) => a[propOne].localeCompare(b[propOne], undefined, opts) ||
+                a[propOne].localeCompare(b[propOne], undefined, { numeric: true }) ||
+                a[propTwo].localeCompare(b[propTwo], undefined, opts);
+        };
+
+        return property instanceof Array
+            ? input.sort(dualPropComparator(property[0], property[1]))
+            : input.sort(propComparator(property));
+    }
+}
