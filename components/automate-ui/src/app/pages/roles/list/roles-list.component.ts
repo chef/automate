3a3e2c8a840660bb6aaeb00f222194e1cbffb354
seajamied <jdegnan@chef.io>
2019-09-25 10:45:23 -0700

    Refactor roles-list component to use ChefSorters
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/pages/roles/list/roles-list.component.ts
+++ components/automate-ui/src/app/pages/roles/list/roles-list.component.ts
@@ -10,6 +10,7 @@ import { iamMajorVersion } from 'app/entities/policies/policy.selectors';
 import { IAMMajorVersion } from 'app/entities/policies/policy.model';
 import { allRoles, getAllStatus } from 'app/entities/roles/role.selectors';
 import { Role } from 'app/entities/roles/role.model';
+import { ChefSorters } from 'app/helpers/auth/sorter';
 
 @Component({
   selector: 'app-roles-list',
@@ -26,16 +27,7 @@ export class RolesListComponent implements OnInit {
   ) {
     this.loading$ = store.select(getAllStatus).pipe(map(loading));
     this.sortedRoles$ = store.select(allRoles).pipe(
-      map((roles: Role[]) => roles.sort(
-        (a, b) => {
-          // See https://stackoverflow.com/a/38641281 for these options
-          const opts = { numeric: true, sensitivity: 'base' };
-          // Sort by name then by cased-name, since no other field is useful as a secondary sort;
-          // this ensures stable sort with respect to case, so 'a' always comes before 'A'.
-          return a.name.localeCompare(b.name, undefined, opts)
-            || a.name.localeCompare(b.name, undefined, {numeric: true});
-        }
-      )));
+      map((roles: Role[]) => ChefSorters.naturalSort(roles, 'name')));
 
     this.iamMajorVersion$ = store.select(iamMajorVersion);
   }
