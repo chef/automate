683d736eabf32f491bdbd374535ea448a90384bd
seajamied <jdegnan@chef.io>
2019-09-26 11:08:30 -0700

    Clean up comments to be more accurate
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/helpers/auth/sorter.ts
+++ components/automate-ui/src/app/helpers/auth/sorter.ts
@@ -4,7 +4,7 @@ export class ChefSorters {
     // sorted 'naturally' by the property chosen.
     // @input: Array of Objects
     // @property: string or an Array of strings, max length is 2 properties
-    //            when using an Array, will sort first by string one, then by string 2;
+    //            when using an Array, sort by first property then by second property
     //
     // example use with single prop:  ChefSorters.naturalSort(users, 'name');
     // example use with dual prop:    ChefSorters.naturalSort(users, ['name', 'id']);
@@ -12,13 +12,13 @@ export class ChefSorters {
 
     // tslint:disable-next-line: max-line-length
     public static naturalSort(input: Array<any>, property: Array<string> | string): Array<any> {
-        // per @msorens https://github.com/chef/a2/pull/4434
-        // Stable sort so 'a' always comes before 'A'.
+        // See https://stackoverflow.com/a/38641281 for these options
+        // "sensitivity provides a stable sort so 'a' always comes before 'A'.
         const naturalAndCaseInsensitive = { numeric: true, sensitivity: 'base' };
         const naturalAndCaseSensitive = { numeric: true };
 
         // propComparator function idea from Dave Newton
-        // https://stackoverflow.com/questions/8537602
+        // https://stackoverflow.com/a/8537635
         const propComparator = (propName) => {
             return (a, b) => a[propName].localeCompare(b[propName],
                                                        undefined, naturalAndCaseInsensitive) ||
