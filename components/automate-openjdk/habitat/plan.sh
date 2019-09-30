d8a6e54055d38884f20486e111da2852268e136a
Ryan Cragun <ryan@chef.io>
2019-09-30 15:21:15 -0600

    [elasticsearch] Upgrade to es 6.8.3 and openjdk11
    * Add automate-openjdk which provides OpenJDK11 but doesn't depend on
      ALSA or FreeType
    * Update to ElasticSearch to 6.8.3
    * Update ingest integration test to be compatible with the ElasticSearch
      6.8.3 S3 snapshot repository plugin
    
    Signed-off-by: Ryan Cragun <ryan@chef.io>

---
+++ components/automate-openjdk/habitat/plan.sh
@@ -0,0 +1,68 @@
+#shellcheck disable=SC2034
+#shellcheck disable=SC2154
+
+# Instead of wrapping the core/openjdk11 package, we're using our own so that
+# we don't need to depend on ALSA or FreeType, which have non-comaptible licenses.
+
+pkg_origin=chef
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_name=automate-openjdk
+# Release archive https://jdk.java.net/archive/
+pkg_version=11.0.2
+pkg_source=https://download.java.net/java/GA/jdk11/9/GPL/openjdk-${pkg_version}_linux-x64_bin.tar.gz
+pkg_shasum=99be79935354f5c0df1ad293620ea36d13f48ec3ea870c838f20c504c9668b57
+pkg_filename=openjdk-${pkg_version}_linux-x64_bin.tar.gz
+pkg_dirname="jdk-${pkg_version}"
+pkg_license=("GPL-2.0-with-classpath-exception")
+pkg_description=('The official Reference Implementation for Java SE 11')
+pkg_upstream_url=https://openjdk.java.net/
+pkg_deps=(
+  core/gcc-libs
+  core/glibc
+  core/libxext
+  core/libxi
+  core/libxrender
+  core/libxtst
+  core/xlib
+  core/zlib
+)
+pkg_build_deps=(
+  core/patchelf
+  core/rsync
+)
+pkg_bin_dirs=(bin)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+
+source_dir=${HAB_CACHE_SRC_PATH}/${pkg_dirname}
+
+do_setup_environment() {
+ set_runtime_env JAVA_HOME "${pkg_prefix}"
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  pushd "${pkg_prefix}" || return 1
+  rsync -avz "${source_dir}/" .
+
+  export LD_RUN_PATH="${LD_RUN_PATH}:${pkg_prefix}/lib/jli:${pkg_prefix}/lib/server:${pkg_prefix}/lib"
+
+  build_line "Setting interpreter for all executables to '$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2'"
+  build_line "Setting rpath for all libraries to '$LD_RUN_PATH'"
+
+  find "$pkg_prefix"/{lib,bin} -type f -executable \
+    -exec sh -c 'file -i "$1" | grep -q "x-executable; charset=binary"' _ {} \; \
+    -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath "${LD_RUN_PATH}" {} \;
+
+  find "$pkg_prefix/lib" -type f -name "*.so" \
+    -exec patchelf --set-rpath "${LD_RUN_PATH}" {} \;
+
+  popd || return 1
+}
+
+do_strip() {
+  return 0
+}
