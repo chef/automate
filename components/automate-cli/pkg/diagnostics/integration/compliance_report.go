1fa03bff4f697b3b8e74819ed9f7ebc9a6e6a3bc
Lance Finfrock <lfinfrock@chef.io>
2019-09-30 11:14:25 -0700

    Fixing message
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- components/automate-cli/pkg/diagnostics/integration/compliance_report.go
+++ components/automate-cli/pkg/diagnostics/integration/compliance_report.go
@@ -228,7 +228,8 @@ func CreateComplianceReportDiagnostic() diagnostics.Diagnostic {
 
 					tries++
 					if tries >= maxTries {
-						assert.Failf(tstCtx, fmt.Sprintf("POST %s failed with status code %d", reportReqPath, resp.StatusCode))
+						assert.Fail(tstCtx, "POST Failed", "POST %s failed with status code %d",
+							reportReqPath, resp.StatusCode)
 						break
 					}
 					time.Sleep(2 * time.Duration(tries) * time.Second)
