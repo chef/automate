40fb76d03a78520817cb192fe3d5148796df6715
seajamied <jdegnan@chef.io>
2019-10-01 08:59:17 -0700

    Testing scaffolding is starting to come together
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/pages/policy/add-members/policy-add-members.spec.ts
+++ components/automate-ui/src/app/pages/policy/add-members/policy-add-members.spec.ts
@@ -1,29 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule, Store } from '@ngrx/store';
+// import { of as observableOf } from 'rxjs';
 import { MockComponent } from 'ng2-mock-component';
+
+// import { NgrxStateAtom } from 'app/ngrx.reducers';
+import { policyEntityReducer } from 'app/entities/policies/policy.reducer';
 
 import { ChefPipesModule } from 'app/pipes/chef-pipes.module';
 import { customMatchers } from 'app/testing/custom-matchers';
 import { PolicyAddMembersComponent } from './policy-add-members.component';
 
-describe('PolicyListComponent', () => {
+
+
+
+describe('PolicyAddMembersComponent', () => {
     let component: PolicyAddMembersComponent;
     let fixture: ComponentFixture<PolicyAddMembersComponent>;
     let element: HTMLElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [
-                ChefPipesModule,
-                FormsModule,
-                ReactiveFormsModule
-            ],
             declarations: [
                 MockComponent({ selector: 'chef-page' }),
-                MockComponent({ selector: 'chef-modal' }),
+                MockComponent({ selector: 'chef-modal', inputs: ['visible'] }),
                 MockComponent({ selector: 'chef-form-field' }),
                 MockComponent({ selector: 'chef-error'}),
-                MockComponent({ selector: 'chef-button'}),
+                MockComponent({ selector: 'chef-button', inputs: ['disabled'] }),
                 MockComponent({ selector: 'chef-input'}),
                 MockComponent({ selector: 'chef-table' }),
                 MockComponent({ selector: 'chef-thead' }),
@@ -31,7 +35,18 @@ describe('PolicyListComponent', () => {
                 MockComponent({ selector: 'chef-th' }),
                 MockComponent({ selector: 'chef-tbody' }),
                 MockComponent({ selector: 'chef-td' }),
+                MockComponent({ selector: 'chef-checkbox', inputs: ['checked'] }),
+                MockComponent({ selector: 'a', inputs: ['routerLink'] }),
                 PolicyAddMembersComponent
+            ],
+            imports: [
+                ChefPipesModule,
+                FormsModule,
+                ReactiveFormsModule,
+                RouterTestingModule,
+                StoreModule.forRoot({
+                    policies: policyEntityReducer
+                })
             ]
         }).compileComponents();
     }));
