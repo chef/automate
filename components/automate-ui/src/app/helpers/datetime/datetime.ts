bdfb19ea56e40882588efec413d77ea7a36bd400
seajamied <jdegnan@chef.io>
2019-10-02 12:31:41 -0700

    Update to 2 space indent
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/helpers/datetime/datetime.ts
+++ components/automate-ui/src/app/helpers/datetime/datetime.ts
@@ -1,5 +1,5 @@
 export class DateTime {
-    // formatted for use with moment.js -- https://momentjs.com/docs/#/displaying/format/
-    // RFC2822 format like: Wed, 03 Jul 2019 17:08:53 UTC
-    public static readonly RFC2822: string = 'ddd, DD MMM YYYY HH:mm:ss [UTC]';
+  // formatted for use with moment.js -- https://momentjs.com/docs/#/displaying/format/
+  // RFC2822 format like: Wed, 03 Jul 2019 17:08:53 UTC
+  public static readonly RFC2822: string = 'ddd, DD MMM YYYY HH:mm:ss [UTC]';
 }
