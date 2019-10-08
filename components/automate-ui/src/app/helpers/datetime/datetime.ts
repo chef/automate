cf8c3f83074868f8f7d73d73b60950fe3c8c8b51
seajamied <jdegnan@chef.io>
2019-10-08 08:52:04 -0700

    Add ChefDateTime string for use across application
    Currently named ChefDateTime (a WIP) to use across the app as a
    time format that will be regularly reused.  I also needed to
    import the custom chef pipes to be able to use in this module.
    
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/helpers/datetime/datetime.ts
+++ components/automate-ui/src/app/helpers/datetime/datetime.ts
@@ -2,4 +2,7 @@ export class DateTime {
   // formatted for use with moment.js -- https://momentjs.com/docs/#/displaying/format/
   // RFC2822 format like: Wed, 03 Jul 2019 17:08:53 UTC
   public static readonly RFC2822: string = 'ddd, DD MMM YYYY HH:mm:ss [UTC]';
+
+  // Tue, 24 Sept 2019
+  public static readonly CHEF_DATE_TIME: string = 'ddd, DD MMM YYYY';
 }
