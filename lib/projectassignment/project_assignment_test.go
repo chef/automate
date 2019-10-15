8e1b520ff2c284ab828982a0f3f509a2d2c37cac
Blake Johnson <bstier@chef.io>
2019-10-15 16:17:14 -0700

    Add tests for CalcDiff
    Signed-off-by: Blake Johnson <bstier@chef.io>
    Signed-off-by: Tyler Cloke <tylercloke@gmail.com>

--- lib/projectassignment/project_assignment_test.go
+++ lib/projectassignment/project_assignment_test.go
@@ -7,37 +7,82 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCalculateProjectDiff(t *testing.T) {
+func TestCalculateProjectDiffForUpdate(t *testing.T) {
+	isUpdate := true
 	cases := []struct {
 		desc string
 		f    func(*testing.T)
 	}{
-		{"unassigned case", func(t *testing.T) {
-			assert.ElementsMatch(t, []string{}, CalculateProjectDiff([]string{}, []string{}))
+		{"unassigned to unassigned", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{}, CalculateProjectDiff([]string{}, []string{}, isUpdate))
 		}},
 		{"there are only new projects", func(t *testing.T) {
-			assert.ElementsMatch(t, []string{"a", "b"}, CalculateProjectDiff([]string{}, []string{"a", "b"}))
+			assert.ElementsMatch(t, []string{"a", "b", "(unassigned)"}, CalculateProjectDiff([]string{}, []string{"a", "b"}, isUpdate))
 		}},
 		{"no projects were removed", func(t *testing.T) {
-			assert.ElementsMatch(t, []string{}, CalculateProjectDiff([]string{"a", "b"}, []string{"a", "b"}))
+			assert.ElementsMatch(t, []string{}, CalculateProjectDiff([]string{"a", "b"}, []string{"a", "b"}, isUpdate))
 		}},
 		{"there are only old projects", func(t *testing.T) {
-			assert.ElementsMatch(t, []string{"a", "b"}, CalculateProjectDiff([]string{"a", "b"}, []string{}))
+			assert.ElementsMatch(t, []string{"a", "b", "(unassigned)"}, CalculateProjectDiff([]string{"a", "b"}, []string{}, isUpdate))
 		}},
 		{"one project is removed", func(t *testing.T) {
-			assert.ElementsMatch(t, []string{"a"}, CalculateProjectDiff([]string{"a", "b"}, []string{"b"}))
+			assert.ElementsMatch(t, []string{"a"}, CalculateProjectDiff([]string{"a", "b"}, []string{"b"}, isUpdate))
 		}},
 		{"multiple projects are removed", func(t *testing.T) {
-			assert.ElementsMatch(t, []string{"a", "c"}, CalculateProjectDiff([]string{"a", "b", "c"}, []string{"b"}))
+			assert.ElementsMatch(t, []string{"a", "c"}, CalculateProjectDiff([]string{"a", "b", "c"}, []string{"b"}, isUpdate))
 		}},
 		{"one project is added", func(t *testing.T) {
-			assert.ElementsMatch(t, []string{"c"}, CalculateProjectDiff([]string{"a", "b"}, []string{"a", "b", "c"}))
+			assert.ElementsMatch(t, []string{"c"}, CalculateProjectDiff([]string{"a", "b"}, []string{"a", "b", "c"}, isUpdate))
 		}},
 		{"multiple projects are added", func(t *testing.T) {
-			assert.ElementsMatch(t, []string{"a", "d"}, CalculateProjectDiff([]string{"b", "c"}, []string{"a", "b", "c", "d"}))
+			assert.ElementsMatch(t, []string{"a", "d"}, CalculateProjectDiff([]string{"b", "c"}, []string{"a", "b", "c", "d"}, isUpdate))
 		}},
 		{"multiple projects are added and removed", func(t *testing.T) {
-			assert.ElementsMatch(t, []string{"a", "c", "d"}, CalculateProjectDiff([]string{"b", "c"}, []string{"a", "b", "d"}))
+			assert.ElementsMatch(t, []string{"a", "c", "d"}, CalculateProjectDiff([]string{"b", "c"}, []string{"a", "b", "d"}, isUpdate))
+		}},
+	}
+
+	rand.Shuffle(len(cases), func(i, j int) {
+		cases[i], cases[j] = cases[j], cases[i]
+	})
+
+	for _, test := range cases {
+		t.Run(test.desc, test.f)
+	}
+}
+
+func TestCalculateProjectDiffForCreate(t *testing.T) {
+	isUpdate := false
+	cases := []struct {
+		desc string
+		f    func(*testing.T)
+	}{
+		{"unassigned to unassigned", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{"(unassigned)"}, CalculateProjectDiff([]string{}, []string{}, isUpdate))
+		}},
+		{"there are only new projects", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{"a", "b", "(unassigned)"}, CalculateProjectDiff([]string{}, []string{"a", "b"}, isUpdate))
+		}},
+		{"no projects were removed", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{}, CalculateProjectDiff([]string{"a", "b"}, []string{"a", "b"}, isUpdate))
+		}},
+		{"there are only old projects", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{"a", "b", "(unassigned)"}, CalculateProjectDiff([]string{"a", "b"}, []string{}, isUpdate))
+		}},
+		{"one project is removed", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{"a"}, CalculateProjectDiff([]string{"a", "b"}, []string{"b"}, isUpdate))
+		}},
+		{"multiple projects are removed", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{"a", "c"}, CalculateProjectDiff([]string{"a", "b", "c"}, []string{"b"}, isUpdate))
+		}},
+		{"one project is added", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{"c"}, CalculateProjectDiff([]string{"a", "b"}, []string{"a", "b", "c"}, isUpdate))
+		}},
+		{"multiple projects are added", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{"a", "d"}, CalculateProjectDiff([]string{"b", "c"}, []string{"a", "b", "c", "d"}, isUpdate))
+		}},
+		{"multiple projects are added and removed", func(t *testing.T) {
+			assert.ElementsMatch(t, []string{"a", "c", "d"}, CalculateProjectDiff([]string{"b", "c"}, []string{"a", "b", "d"}, isUpdate))
 		}},
 	}
 
