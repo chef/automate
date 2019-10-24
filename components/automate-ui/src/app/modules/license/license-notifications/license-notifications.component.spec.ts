921a7b2e2a1521d428a877b23e6a2dcea8630114
Tara Black <tblack@chef.io>
2019-10-23 22:42:42 -0500

    Fix unit test errors Signed-Off-By: Tara Black <tblack@chef.io>

--- components/automate-ui/src/app/modules/license/license-notifications/license-notifications.component.spec.ts
+++ components/automate-ui/src/app/modules/license/license-notifications/license-notifications.component.spec.ts
@@ -1,17 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockComponent } from 'ng2-mock-component';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { StoreModule, Store } from '@ngrx/store';
+import { NgrxStateAtom } from 'app/ngrx.reducers';
 
 import { LicenseNotificationsComponent } from './license-notifications.component';
+import { LicenseFacadeService } from 'app/entities/license/license.facade';
+import { Notification } from 'app/entities/notifications/notification.model';
 
-describe('NotificationsComponent', () => {
+describe('LicenseNotificationsComponent', () => {
   let component: LicenseNotificationsComponent;
   let fixture: ComponentFixture<LicenseNotificationsComponent>;
+  let store: Store<NgrxStateAtom>;
 
+  class MockLicenseFacadeService extends LicenseFacadeService{
+    notifications$: Observable<Notification[]> = new BehaviorSubject([]);
+  }
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({})
+      ],
       declarations: [
         MockComponent({ selector: 'chef-notification', inputs: ['type', 'timeout'] }),
-        LicenseNotificationsComponent ]
+        LicenseNotificationsComponent],
+      providers: [{ provide: LicenseFacadeService, useClass:  MockLicenseFacadeService }]
     })
     .compileComponents();
   }));
@@ -19,6 +33,8 @@ describe('NotificationsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LicenseNotificationsComponent);
     component = fixture.componentInstance;
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
     fixture.detectChanges();
   });
 
