9cdc37a7b22c5a0123eb8056fb568687d52ec570
Jay Mundrawala <jmundrawala@chef.io>
2019-10-28 16:27:16 -0500

    Made admin e2e tests less flakey
    These tests were not waiting for the conditions they expected. Since we
    disable the automatic version of that, we need to do it manually.
    
    Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

---
+++ components/automate-ui/e2e/helpers/browser_expectations.ts
@@ -0,0 +1,25 @@
+import { browser, $, $$, ElementFinder } from 'protractor';
+function countElementsSelected(selector, count) {
+  return $$(selector).count().then(c => c === count);
+}
+
+export function expectCountElements(selector, count) {
+  expect(
+    browser.wait(() => countElementsSelected(selector, count),
+      5000,
+      `expected "${selector}" to select ${count} elements`)).toBeTruthy();
+}
+
+export function waitForElement(selector) {
+  return browser.wait(() => {
+    return $(selector).isPresent();
+  }, 5000).then<ElementFinder>((_) => {
+    return <ElementFinder>$(selector);
+  });
+}
+
+export function waitForSpinners() {
+  return browser.wait(() => {
+    return $$('chef-loading-spinner').count().then(c => c === 0);
+  }, 5000);
+}
