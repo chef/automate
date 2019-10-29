efa84ef87411993ae9ed475a80eb305dbf754ae3
victoria jeffrey <vjeffrey@chef.io>
2019-10-28 20:48:42 -0600

    clean up the 55_spec to make it less flaky (#2023)
    * clean up the 55_spec to make it less flaky
    
    Signed-off-by: Victoria Jeffrey <vjeffrey@chef.io>
    
    * fixups for reporting test job
    
    Signed-off-by: Victoria Jeffrey <vjeffrey@chef.io>

--- components/compliance-service/api/tests/55_ingest_spec.rb
+++ components/compliance-service/api/tests/55_ingest_spec.rb
@@ -13,13 +13,15 @@ describe File.basename(__FILE__) do
   Jobs = Chef::Automate::Domain::Compliance::Api::Jobs unless defined?(Jobs)
   Common = Chef::Automate::Domain::Compliance::Api::Common unless defined?(Common)
 
-  it "works" do
-    reporting = Reporting::ReportingService
-    nodes = Nodes::NodesService
-    jobs = Jobs::JobsService
+  reporting = Reporting::ReportingService
+  nodes = Nodes::NodesService
+  jobs = Jobs::JobsService
 
-    rand1000 = rand(1000)
+  @rand1000 = rand(1000)
+  @job_id = ''
 
+
+  before(:all) do
     # These ifs are used only when developing/troubleshooting the tests to avoid
     # having to create a node and wait for a job to complete
     job_id = ENV['JOB_ID'].to_s
@@ -27,7 +29,7 @@ describe File.basename(__FILE__) do
       if ENV['NODE_ID'].to_s == ''
         # Add a docker node to be able to reference it in the job
         node = MANAGER_GRPC nodes, :create, Nodes::Node.new(
-          name:"My cc_pg Docker Node #{rand1000}",
+          name:"My cc_pg Docker Node #{@rand1000}",
           manager:"automate",
           target_config: Nodes::TargetConfig.new(
             backend:"docker",
@@ -55,53 +57,30 @@ describe File.basename(__FILE__) do
         retries: 1,
         node_selectors: []
       )
-      job_id = job['id']
+      @job_id = job['id']
     end
-
+ 
     i = 0
     job_status = 'new'
     report_id = 'unknown'
     while job_status != "completed" do
-      job1 = GRPC jobs, :read, Jobs::Id.new(id: job_id)
+      job1 = GRPC jobs, :read, Jobs::Id.new(id: @job_id)
       job_status = job1['status']
-      if i > 30
+      if i > 40
         assert_equal('completed', job_status)
       end
+      puts "sleeping 5 sec, counter is: ", i
       sleep 5
       i += 1
     end
+    @report_id = job1['results'][0]['report_id']
+    @node_id = job1['results'][0]['node_id']
 
-    report_id = job1['results'][0]['report_id']
-    node_id = job1['results'][0]['node_id']
-
-    # Get job details
-    job1 = GRPC jobs, :read, Jobs::Id.new(id: job_id)
-
-    # Wait for a bit for the report to be pushed to ElasticSearched
-    sleep 3
-
-    # Filtering by job_id as this is leading to a search in the timeseries indices
-    act_rep_nodes = GRPC reporting, :list_nodes, Reporting::Query.new(filters: [
-      Reporting::ListFilter.new(type: 'job_id', values: [job_id])
-    ])
-
-    sleep_count = 0
-    while act_rep_nodes.nodes.size == 0 do
-      puts "waiting for node to land in elasticsearch"
-      sleep 1
-      sleep_count += 1
-      act_rep_nodes = GRPC reporting, :list_nodes, Reporting::Query.new(filters: [
-        Reporting::ListFilter.new(type: 'job_id', values: [job_id])])
-      break if sleep_count >= 30
-    end
-    assert_equal(1, act_rep_nodes.nodes.size)
-
-    act_rep_nodes['nodes'][0]['latest_report']['end_time']['seconds'] = 1
-    exp_rep_nodes = {
+    @exp_rep_nodes = {
       "nodes": [
         {
-          "id": node_id,
-          "name": "My cc_pg Docker Node #{rand1000}",
+          "id": @node_id,
+          "name": "My cc_pg Docker Node #{@rand1000}",
           "platform": {
             "name": "debian",
             "release": "9.7",
@@ -109,7 +88,7 @@ describe File.basename(__FILE__) do
           },
           "environment": "trouble",
           "latestReport": {
-            "id": report_id,
+            "id": @report_id,
             "endTime": "1970-01-01T00:00:01Z",
             "status": "passed",
             "controls": {
@@ -136,21 +115,46 @@ describe File.basename(__FILE__) do
       ],
       "total": 1,
       "totalPassed"=>1
-    }.to_json
-    assert_equal_json_sorted(exp_rep_nodes, act_rep_nodes.to_json)
+      }.to_json
+  end
 
+
+it "listing nodes with a job id filter returns the expected node" do
+
+    # Filtering by job_id as this is leading to a search in the timeseries indices
+    act_rep_nodes = GRPC reporting, :list_nodes, Reporting::Query.new(filters: [
+      Reporting::ListFilter.new(type: 'job_id', values: [@job_id])
+    ])
+
+    sleep_count = 0
+    while act_rep_nodes.nodes.size == 0 do
+      puts "sleeping 5, waiting for node to land in elasticsearch. count: ", sleep_count
+      sleep 5
+      sleep_count += 1
+      act_rep_nodes = GRPC reporting, :list_nodes, Reporting::Query.new(filters: [
+        Reporting::ListFilter.new(type: 'job_id', values: [@job_id])])
+      break if sleep_count >= 30
+    end
+    assert_equal(1, act_rep_nodes.nodes.size)
+
+    act_rep_nodes['nodes'][0]['latest_report']['end_time']['seconds'] = 1
+    assert_equal_json_sorted(@exp_rep_nodes, act_rep_nodes.to_json)
+  end
+
+  it "filtering the reporting nodes by node id works too" do
     # Filtering by node_id as this is leading to a search in the latest index
     act_rep_nodes = GRPC reporting, :list_nodes, Reporting::Query.new(filters: [
-      Reporting::ListFilter.new(type: 'node_id', values: [node_id])
+      Reporting::ListFilter.new(type: 'node_id', values: [@node_id])
     ])
     act_rep_nodes['nodes'][0]['latest_report']['end_time']['seconds'] = 1
     assert_equal(1, act_rep_nodes.nodes.size)
-    assert_equal_json_sorted(exp_rep_nodes, act_rep_nodes.to_json)
+    assert_equal_json_sorted(@exp_rep_nodes, act_rep_nodes.to_json)
+  end
 
-
-    # Get profiles used by the node with node_id. This will hit the profiles index
+  it "calling reporting profiles returns expected profile from scan job" do
+      # Get profiles used by the node with node_id. This will hit the profiles index
     act_rep_profiles = GRPC reporting, :list_profiles, Reporting::Query.new(filters: [
-      Reporting::ListFilter.new(type: 'node_id', values: [node_id])
+      Reporting::ListFilter.new(type: 'node_id', values: [@node_id])
     ])
     exp_rep_profiles = {
       "profiles": [
@@ -168,7 +172,9 @@ describe File.basename(__FILE__) do
       }
     }.to_json
     assert_equal_json_sorted(exp_rep_profiles, act_rep_profiles.to_json)
+  end
 
+  it "the nodemanager knows about the node too" do
     # that node should have recent last contact time, so we expect more than 0 nodes here
     formatted_one_hour_ago_utc = (Time.now.utc - 3600).iso8601
     formatted_now_time_utc = (Time.now.utc + 1).iso8601
