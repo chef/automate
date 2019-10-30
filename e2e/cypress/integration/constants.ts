4a809ba51177f1e50cc9c115358ad7a19cc1d65b
Brenna Hewer-Darroch <brenna@chef.io>
2019-10-30 15:50:41 -0700

    Revert "[automate-1877] clean up v2 in deployed envs and Cypress (#2042)" (#2052)
    This reverts commit 709584efc0c48f1e78e04f0b8909d024264119f9.

--- e2e/cypress/integration/constants.ts
+++ e2e/cypress/integration/constants.ts
@@ -1,5 +1,6 @@
-export type IAMVersion = 'v1' | 'v2.1';
-export const iamVersion: IAMVersion = Cypress.env('IAM_VERSION') || 'v2.1';
+export type IAMVersion = 'v1' | 'v2' | 'v2.1';
+export const iamVersion: IAMVersion = Cypress.env('IAM_VERSION') || 'v2';
+export const describeIfIAMV2 = iamVersion.match(/v2/) ? describe : describe.skip;
 export const describeIfIAMV2p1 = iamVersion === 'v2.1' ? describe : describe.skip;
 export const runFlaky: boolean = Cypress.env('RUN_FLAKY') || false;
 export const itFlaky = runFlaky ? it : it.skip;
