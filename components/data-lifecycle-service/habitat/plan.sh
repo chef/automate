7dc1384c992013a9c0f754170501eff3dedd1fc0
Jay Mundrawala <jmundrawala@chef.io>
2019-10-31 16:01:25 -0500

    Build empty data lifecycle service
    We create an empty build because deployment-service cannot continue if we
    remove a service from the manifest. The empty package is the least  bad of
    our options because it has no dependencies, so it wont cause us to install
    in data-lifecycle-service's old deps.
    
    This will be a 2 part PR. We'll pin data-lifecycle-service in
    products.meta once its built.
    
    Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

---
+++ components/data-lifecycle-service/habitat/plan.sh
@@ -0,0 +1,24 @@
+pkg_name="data-lifecycle-service"
+pkg_description="Automate Data Lifecycle Service"
+pkg_origin="chef"
+pkg_version="0.0.1"
+vendor_origin="chef"
+pkg_maintainer="Chef Software Inc. <support@chef.io>"
+pkg_license=("Chef-MLSA")
+pkg_upstream_url="https://www.chef.io/automate"
+
+do_prepare() {
+    :
+}
+
+do_build() {
+    :
+}
+
+do_install() {
+    :
+}
+
+do_strip() {
+    :
+}
