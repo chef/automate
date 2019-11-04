550578738a1df5530669fb7d9000ab682767690d
Blake Johnson <bstier@chef.io>
2019-11-04 10:37:45 -0800

    Sidebar -> isIAMv2
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/automate-ui/src/app/components/settings-sidebar/settings-sidebar.component.ts
+++ components/automate-ui/src/app/components/settings-sidebar/settings-sidebar.component.ts
@@ -4,11 +4,9 @@ import { Observable } from 'rxjs';
 
 import { NgrxStateAtom } from 'app/ngrx.reducers';
 import {
-  iamMajorVersion,
   isIAMv2
 } from 'app/entities/policies/policy.selectors';
 import { GetIamVersion } from 'app/entities/policies/policy.actions';
-import { IAMMajorVersion } from 'app/entities/policies/policy.model';
 import { FeatureFlagsService } from '../../services/feature-flags/feature-flags.service';
 
 @Component({
@@ -18,16 +16,14 @@ import { FeatureFlagsService } from '../../services/feature-flags/feature-flags.
 })
 
 export class SettingsSidebarComponent implements OnInit {
-  public iamMajorVersion$: Observable<IAMMajorVersion>;
-  public projectsEnabled$: Observable<boolean>;
+  public isIAMv2$: Observable<boolean>;
   featureFlagOn: boolean;
 
   constructor(
     private store: Store<NgrxStateAtom>,
     private featureFlags: FeatureFlagsService
     ) {
-    this.iamMajorVersion$ = store.select(iamMajorVersion);
-    this.projectsEnabled$ = store.select(isIAMv2);
+    this.isIAMv2$ = store.select(isIAMv2);
     this.featureFlagOn = this.featureFlags.getFeatureStatus('servicenow_cmdb');
   }
 
