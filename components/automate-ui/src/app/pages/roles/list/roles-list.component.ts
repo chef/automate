613d528a3a7796883a4d472e13b810545e9ee7e8
Blake Johnson <bstier@chef.io>
2019-11-04 14:46:23 -0800

    Roles
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/automate-ui/src/app/pages/roles/list/roles-list.component.ts
+++ components/automate-ui/src/app/pages/roles/list/roles-list.component.ts
@@ -7,8 +7,7 @@ import { ChefSorters } from 'app/helpers/auth/sorter';
 import { NgrxStateAtom } from 'app/ngrx.reducers';
 import { loading } from 'app/entities/entities';
 import { GetRoles } from 'app/entities/roles/role.actions';
-import { iamMajorVersion } from 'app/entities/policies/policy.selectors';
-import { IAMMajorVersion } from 'app/entities/policies/policy.model';
+import { isIAMv2 } from 'app/entities/policies/policy.selectors';
 import { allRoles, getAllStatus } from 'app/entities/roles/role.selectors';
 import { Role } from 'app/entities/roles/role.model';
 
@@ -20,7 +19,7 @@ import { Role } from 'app/entities/roles/role.model';
 export class RolesListComponent implements OnInit {
   public loading$: Observable<boolean>;
   public sortedRoles$: Observable<Role[]>;
-  public iamMajorVersion$: Observable<IAMMajorVersion>;
+  public isIAMv2$: Observable<boolean>;
 
   constructor(
     private store: Store<NgrxStateAtom>
@@ -29,7 +28,7 @@ export class RolesListComponent implements OnInit {
     this.sortedRoles$ = store.select(allRoles).pipe(
       map((roles: Role[]) => ChefSorters.naturalSort(roles, 'name')));
 
-    this.iamMajorVersion$ = store.select(iamMajorVersion);
+    this.isIAMv2$ = store.select(isIAMv2);
   }
 
   ngOnInit(): void {
