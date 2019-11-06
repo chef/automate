863b943ce1440de1d5a1b1ed3c8ab64d367babc6
seajamied <jdegnan@chef.io>
2019-11-06 13:51:22 -0800

    Add hostbindigns and stylings
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/components/chef-control-menu/chef-control-menu.component.ts
+++ components/automate-ui/src/app/components/chef-control-menu/chef-control-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-chef-control-menu',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChefControlMenuComponent implements OnInit {
 
+  public isFocused = false;
+  public isActive = false;
+  public isDisabled = false;
+
+  public selectedIndex = 0;
+  public focusedIndex = 0;
+
+  // can i use a ternarys here?
+  @HostBinding('attr.role') comboBox = 'combobox';
+  @HostBinding('attr.tabindex') isTabable: string = this.isDisabled === true ? '-1' : '0';
+  @HostBinding('class.focused') focused: string = this.isFocused ? 'focused' : '';
+  @HostBinding('class.active') active: string = this.isActive ? 'active' : '';
+  @HostBinding('class.disabled') disabled: string = this.isDisabled ? 'disabled' : '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleClickActivation(event) {
+    if (this.isDisabled) {
+      console.log('isDisabled')
+      return;
+    } else if (this.isActive) {
+      const option = event.target.closest('chef-option');
+      console.log(event.target.closest('chef-option'));
+      if (!option) {
+        return;
+      }
+      const optionId = option.optionId;
+      // this.makeSelection(findIndex(['optionId', optionId], this.options));
+      console.log('isActive')
+    } else {
+      // this.activate(this.selectedIndex);
+      console.log('Not isActive')
+    }
   }
 
 }
