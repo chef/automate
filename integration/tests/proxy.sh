ff2f53e12328fcaca958fb08d892725ab624e318
Jay Mundrawala <jmundrawala@chef.io>
2019-11-07 06:34:19 -0600

    actually deploy builder
    Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

--- integration/tests/proxy.sh
+++ integration/tests/proxy.sh
@@ -4,6 +4,23 @@
 test_name="proxy"
 test_proxy="true"
 
+do_deploy() {
+    #shellcheck disable=SC2154
+    chef-automate deploy config.toml \
+        --product automate \
+        --product builder \
+        --hartifacts "$test_hartifacts_path" \
+        --override-origin "$HAB_ORIGIN" \
+        --manifest-dir "$test_manifest_path" \
+        --admin-password chefautomate \
+        --accept-terms-and-mlsa
+
+    log_info "applying dev license"
+    chef-automate license apply "$A2_LICENSE"
+
+    echo "127.0.0.1 ${CONTAINER_HOSTNAME}" >> /etc/hosts
+}
+
 do_test_deploy() {
     #shellcheck disable=SC2154
     #shellcheck source=integration/helpers/bldr_tests.sh
