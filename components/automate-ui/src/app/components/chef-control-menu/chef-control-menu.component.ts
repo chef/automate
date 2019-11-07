debe741ca53df1ee358bf8b7255a808a9b48b5eb
seajamied <jdegnan@chef.io>
2019-11-07 11:13:31 -0800

    WIP - good progress, tabs are opening as they should and closing, need to figure out why options are rendering
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/components/chef-control-menu/chef-control-menu.component.ts
+++ components/automate-ui/src/app/components/chef-control-menu/chef-control-menu.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, HostBinding, HostListener } from '@angular/core';
+import { Component, OnInit, HostBinding, HostListener, ElementRef, Output } from '@angular/core';
+
+import { clamp, findIndex, getOr, find, lte } from 'lodash/fp';
+
 
 @Component({
   selector: 'app-chef-control-menu',
@@ -11,8 +14,10 @@ export class ChefControlMenuComponent implements OnInit {
   public isActive = false;
   public isDisabled = false;
 
+  public options: HTMLChefOptionElement[] = [];
   public selectedIndex = 0;
   public focusedIndex = 0;
+  public value: string;
 
   // can i use a ternarys here?
   @HostBinding('attr.role') comboBox = 'combobox';
@@ -36,6 +41,24 @@ export class ChefControlMenuComponent implements OnInit {
     }
   }
 
+  @HostListener('click', ['$event']) handleClickActivation(e) {
+    if (this.isDisabled) {
+      return;
+    } else if (this.isActive) {
+      const option = e.target.closest('chef-option');
+      if (!option || e.target.nodeName === 'CHEF-ICON') {
+        this.isActive = false;
+        return;
+      }
+      const optionId = option.optionId;
+      console.log('option: ' + optionId)
+      this.makeSelection(findIndex(['optionId', optionId], this.options));
+    } else {
+      console.log('click activate: ' + this.selectedIndex)
+      this.activate(this.selectedIndex);
+    }
+  }
+
   @HostListener('keydown', ['$event']) handleKeydown(e: KeyboardEvent) {
     switch (e.key) {
       case (' '): //fallthrough
@@ -43,54 +66,90 @@ export class ChefControlMenuComponent implements OnInit {
         return this.handleKeyActivation();
         break;
       case ('Escape'):
-        console.log('Escape');
-        // return this.handleEscape();
+        return this.handleEscape();
         break;
       case ('ArrowUp'):
-        console.log('ArrowUp');
-        // return this.handleUp(event);
+        return this.handleUp(e);
         break;
       case ('ArrowDown'):
-        console.log('ArrowDown');
-        // return this.handleDown(event);
+        return this.handleDown(e);
         break;
     }
   }
 
-  constructor() { }
+  constructor( private el: ElementRef) { }
 
   ngOnInit() {
+    this.options = Array.from(this.el.nativeElement.querySelectorAll('chef-option'));
+    // The default option is determined by first checking the value property, then
+    // looking for a selected attribute on the options list and finally defaulting
+    // to the first option if all else fails.
+    const defaultIndexes = [findIndex(['value', this.value], this.options),
+    findIndex('selected', this.options),
+      0];
+
+    const index = this.clamp(find(lte(0), defaultIndexes));
+
+    this.value = getOr('', [index, 'value'], this.options);
+    this.selectedIndex = index;
+    this.focusedIndex = index;
+    this.syncOptions(index);
   }
 
   handleKeyActivation() {
     if (this.isDisabled) {
       return;
     } else if (this.isActive) {
-      // this.makeSelection(this.focusedIndex);
-      console.log('makeSelection()');
+      this.makeSelection(this.focusedIndex);
     } else {
-      // this.activate(this.selectedIndex);
-      console.log('activate(selectedIndex)');
+      this.activate(this.selectedIndex);
     }
   }
 
-  handleClickActivation(event) {
-    if (this.isDisabled) {
-      console.log('isDisabled')
-      return;
-    } else if (this.isActive) {
-      const option = event.target.closest('chef-option');
-      console.log(event.target.closest('chef-option'));
-      if (!option) {
-        return;
-      }
-      const optionId = option.optionId;
-      // this.makeSelection(findIndex(['optionId', optionId], this.options));
-      console.log('isActive')
-    } else {
-      // this.activate(this.selectedIndex);
-      console.log('Not isActive')
+  handleEscape() {
+    this.isActive = false;
+    this.focusedIndex = this.selectedIndex;
+  }
+
+  handleUp(e: KeyboardEvent) {
+    e.preventDefault();
+    this.focusedIndex = this.clamp(this.focusedIndex - 1);
+  }
+
+  handleDown(e: KeyboardEvent) {
+    e.preventDefault();
+    this.focusedIndex = this.clamp(this.focusedIndex + 1);
+  }
+  private clamp(value: number) {
+    return clamp(0, this.options.length - 1, value);
+  }
+
+  private makeSelection(index) {
+    // const changeEvent = new CustomEvent('change');
+
+    this.selectedIndex = index;
+    this.isActive = false;
+    this.value = getOr('', [index, 'value'], this.options);
+    // this.change.emit(changeEvent);
+    this.syncOptions(index);
+  }
+
+  private activate(index) {
+    // check that this is an OK use of nativeElement, depending on our use case
+    // it may not work with webworkers https://alligator.io/angular/using-renderer2/
+    const dropdown: HTMLElement = this.el.nativeElement.querySelector('chef-dropdown');
+    console.log('activate');
+    this.isActive = true;
+    this.focusedIndex = this.clamp(index);
+    dropdown.focus();
+  }
+
+  private syncOptions(index) {
+    if (this.options[index]) {
+      this.options.forEach((opt) => opt.selected = false);
+      this.options[index].selected = true;
     }
   }
+
 
 }
