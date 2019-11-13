7105ecd1a2c598ae7a12bfc31f260283d5b755ff
Jay Mundrawala <jmundrawala@chef.io>
2019-11-13 13:52:39 -0600

    Add backup/restore test
    Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

--- integration/tests/bldr_smoke.sh
+++ integration/tests/bldr_smoke.sh
@@ -5,6 +5,7 @@ test_name="bldr_smoke"
 
 test_deploy_inspec_profiles=()
 test_skip_diagnostics=true
+test_backup=true
 
 do_deploy() {
     #shellcheck disable=SC2154
@@ -29,4 +30,20 @@ do_test_deploy() {
     source "${source_dir}/helpers/bldr_tests.sh"
 
     bldr_smoke_test
+}
+
+do_test_restore() {
+    hab pkg install core/hab
+    artifacts_dir=$(mktemp -d --suffix="bldr_smoke")
+    # TODO(jaym): remove when we upgrade hab to a version
+    # that supports hab pkg download
+    SSL_CERT_FILE="/hab/svc/automate-load-balancer/data/$CONTAINER_HOSTNAME.cert" \
+        hab pkg exec core/hab \
+        hab pkg download \
+        -u "https://$CONTAINER_HOSTNAME/bldr/v1" \
+        -c "unstable" \
+        --download-directory "$artifacts_dir" \
+        "${TEST_ORIGIN}/${TEST_PKG}"
+
+    rm -r "$artifacts_dir"
 }
