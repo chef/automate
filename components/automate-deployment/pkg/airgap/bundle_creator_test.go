2ff0ad129471a04e683ae9a003de1ba5b06226b9
Steven Danna <steve@chef.io>
2019-11-15 13:03:44 +0000

    airgap: download new hab binary from packages.chef.io
    The tarballs of the hab binary is now hosted on packages.chef.io
    rather than bintray.
    
    NB: Once we update to the newest version of hab, we can potentially
    remove bintray from our preflight checks and documentation.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- components/automate-deployment/pkg/airgap/bundle_creator_test.go
+++ components/automate-deployment/pkg/airgap/bundle_creator_test.go
@@ -10,9 +10,14 @@ import (
 )
 
 func TestHabBinaryDownload(t *testing.T) {
-	dl := bintrayHabDownloader{}
+	dl := netHabDownloader{}
 	buf := sha256.New()
 	err := dl.DownloadHabBinary("0.56.0", "20180530234036", buf)
 	require.NoError(t, err)
 	assert.Equal(t, "d829ee385a7ca3cca980f5c52671643df2ee4f80104f2336884d3c565f9a2e13", hex.EncodeToString(buf.Sum(nil)))
+
+	buf = sha256.New()
+	err = dl.DownloadHabBinary("0.90.6", "20191112141314", buf)
+	require.NoError(t, err)
+	assert.Equal(t, "1d4cdb2165e967e7421b671512c6588cdfeb7b218f18a594bcfaf52325f8a934", hex.EncodeToString(buf.Sum(nil)))
 }
