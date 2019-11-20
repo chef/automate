534eaa19ca071cbd3468cadfd93625f150d675b4
Steven Danna <steve@chef.io>
2019-11-20 08:50:07 +0000

    Add new automate-dev product (#2244)
    This product can be a home for in-development services.
    
    Signed-off-by: Steven Danna <steve@chef.io>
    Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

---
+++ integration/tests/product_dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#shellcheck disable=SC2034
+test_name="product-dev"
+test_deploy_inspec_profiles=(a2-deploy-integration)
+# The inspec tests don't pass if the diagnostics are run
+test_skip_diagnostics=true
+
+do_deploy() {
+    #shellcheck disable=SC2154
+    chef-automate deploy config.toml \
+        --product automate \
+        --product automate-dev \
+        --hartifacts "$test_hartifacts_path" \
+        --override-origin "$HAB_ORIGIN" \
+        --manifest-dir "$test_manifest_path" \
+        --admin-password chefautomate \
+        --accept-terms-and-mlsa \
+        --debug
+}
