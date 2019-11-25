5f7a4dfc2447574f4ede7bd8f7616b69eb5e312b
Steven Danna <steve@chef.io>
2019-11-25 14:49:04 +0000

    Add some basic assertions for EnqueuedAt
    Signed-off-by: Steven Danna <steve@chef.io>

--- lib/cereal/integration/simple_workflow.go
+++ lib/cereal/integration/simple_workflow.go
@@ -28,7 +28,8 @@ func (suite *CerealTestSuite) TestCompleteSimpleWorkflow() {
 	m := suite.newManager(
 		WithTaskExecutorF(
 			taskName,
-			func(context.Context, cereal.Task) (interface{}, error) {
+			func(_ context.Context, t cereal.Task) (interface{}, error) {
+				suite.Assert().False(t.GetMetadata().EnqueuedAt.IsZero())
 				wgTask.Done()
 				return nil, nil
 			}),
@@ -36,6 +37,7 @@ func (suite *CerealTestSuite) TestCompleteSimpleWorkflow() {
 			workflowName,
 			&workflowExecutorWrapper{
 				onStart: func(w cereal.WorkflowInstance, ev cereal.StartEvent) cereal.Decision {
+					suite.Assert().False(ev.EnqueuedAt.IsZero())
 					err := w.EnqueueTask(taskName, nil)
 					suite.Require().NoError(err, "failed to enqueue task")
 					return w.Continue(nil)
@@ -43,6 +45,7 @@ func (suite *CerealTestSuite) TestCompleteSimpleWorkflow() {
 				onTaskComplete: func(w cereal.WorkflowInstance, ev cereal.TaskCompleteEvent) cereal.Decision {
 					suite.Assert().Equal(1, w.TotalCompletedTasks())
 					suite.Assert().Equal(1, w.TotalEnqueuedTasks())
+					suite.Assert().False(ev.EnqueuedAt.IsZero())
 					wgTask.Done()
 					return w.Complete()
 				},
