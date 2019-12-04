5dacd2c3e05a6237e4d807ba54742c81cf3f2bcb
Brenna Hewer-Darroch <brenna@chef.io>
2019-12-04 14:20:59 -0800

    add test with many reports + summaries
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- components/compliance-service/integration_test/project_delete_test.go
+++ components/compliance-service/integration_test/project_delete_test.go
@@ -147,15 +147,88 @@ func TestSummaryProjectDeleteNoSummaries(t *testing.T) {
 }
 
 func TestProjectDelete(t *testing.T) {
-	t.Run("Deleting a project from summaries and reports", func(t *testing.T) {
-		// ingest both summary and report
+	t.Run("Deleting a project from multiple summaries and reports with different projects", func(t *testing.T) {
+		oneProjectReportNode := "one-proj-rep"
+		multiProjectReportNode := "multi-proj-rep"
+		noProjectReportNode := "no-proj-rep"
 
-		// call DeleteProjectTag(ctx, projectToDelete)
+		reports := []*relaxting.ESInSpecReport{
+			{
+				NodeID:   oneProjectReportNode,
+				Projects: []string{projectIDToDelete},
+			},
+			{
+				NodeID:   multiProjectReportNode,
+				Projects: []string{"project3", projectIDToDelete, "project9"},
+			},
+			{
+				NodeID:   noProjectReportNode,
+				Projects: []string{},
+			},
+		}
 
-		// wait
+		oneProjectSummaryNode := "one-proj-sum"
+		multiProjectSummaryNode := "multi-proj-sum"
+		noProjectSummaryNode := "no-proj-sum"
 
-		// refresh
+		summaries := []*relaxting.ESInSpecSummary{
+			{
+				NodeID:   oneProjectSummaryNode,
+				Projects: []string{projectIDToDelete},
+			},
+			{
+				NodeID:   multiProjectSummaryNode,
+				Projects: []string{"project4", projectIDToDelete, "project8"},
+			},
+			{
+				NodeID:   noProjectSummaryNode,
+				Projects: []string{},
+			},
+		}
 
-		// assert project is removed from both summary and report
+		_, err := suite.InsertInspecReports(reports)
+		require.NoError(t, err)
+		_, err = suite.InsertInspecSummaries(summaries)
+		require.NoError(t, err)
+
+		esJobIDs, err := suite.ingesticESClient.DeleteProjectTag(ctx, projectIDToDelete)
+		require.NoError(t, err)
+
+		waitForESJobsToComplete(esJobIDs)
+		suite.RefreshComplianceReportIndex()
+		suite.RefreshComplianceSummaryIndex()
+
+		updatedReports, err := suite.GetAllReportsESInSpecReport()
+		require.NoError(t, err)
+		require.Equal(t, 3, len(updatedReports))
+
+		reportMap := make(map[string]*relaxting.ESInSpecReport, len(updatedReports))
+		for _, report := range updatedReports {
+			reportMap[report.NodeID] = report
+		}
+
+		updatedSummaries, err := suite.GetAllSummaryESInSpecSummary()
+		require.NoError(t, err)
+		require.Equal(t, 3, len(updatedSummaries))
+
+		summaryMap := make(map[string]*relaxting.ESInSpecSummary, len(updatedSummaries))
+		for _, summary := range updatedSummaries {
+			summaryMap[summary.NodeID] = summary
+		}
+
+		assert.Equal(t, []string{}, reportMap[noProjectReportNode].Projects)
+		assert.Equal(t, []string{}, reportMap[oneProjectReportNode].Projects)
+		assert.Equal(t, []string{}, summaryMap[noProjectSummaryNode].Projects)
+		assert.Equal(t, []string{}, summaryMap[oneProjectSummaryNode].Projects)
+
+		assert.ElementsMatch(t, []string{"project3", "project9"}, reportMap[multiProjectReportNode].Projects)
+		assert.ElementsMatch(t, []string{"project4", "project8"}, summaryMap[multiProjectSummaryNode].Projects)
 	})
+	suite.DeleteAllDocuments()
+}
+
+func waitForESJobsToComplete(jobs []string) {
+	for _, job := range jobs {
+		suite.WaitForESJobToComplete(job)
+	}
 }
