dc8e9c0ab37054ea56cdda29eebf2ed20b39ebf9
Brenna Hewer-Darroch <brenna@chef.io>
2019-12-03 19:55:26 -0800

    compliance: add tests delete project from report test is mostly done the other ones are pseudocode
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

---
+++ components/compliance-service/integration_test/project_delete_test.go
@@ -0,0 +1,148 @@
+package integration_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/chef/automate/components/compliance-service/reporting/relaxting"
+)
+
+func TestReportProjectDelete(t *testing.T) {
+	var (
+		ctx               = context.Background()
+		projectIDToDelete = "target_project"
+	)
+
+	cases := []struct {
+		description        string
+		existingProjectIDs []string
+		projectIDToDelete  string
+		expectedProjectIDs []string
+	}{
+		{
+			description:        "Deleting the last project tag",
+			existingProjectIDs: []string{"target_project"},
+			expectedProjectIDs: []string{},
+		},
+		{
+			description:        "Deleting a project that does not exist",
+			existingProjectIDs: []string{},
+			expectedProjectIDs: []string{},
+		},
+		{
+			description:        "Deleting a project on a report with multiple projects",
+			existingProjectIDs: []string{"project3", "target_project", "project9"},
+			expectedProjectIDs: []string{"project3", "project9"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.description, func(t *testing.T) {
+
+			report := &relaxting.ESInSpecReport{
+				Projects: test.existingProjectIDs,
+			}
+
+			_, err := suite.InsertInspecReports([]*relaxting.ESInSpecReport{report})
+			require.NoError(t, err)
+
+			defer suite.DeleteAllDocuments()
+
+			esJobID, err := suite.ingesticESClient.DeleteReportProjectTag(ctx, projectIDToDelete)
+			require.NoError(t, err)
+
+			suite.WaitForESJobToComplete(esJobID)
+
+			suite.RefreshComplianceReportIndex()
+
+			// get the reports
+			reports, err := suite.GetAllReportsESInSpecReport()
+			require.NoError(t, err)
+			require.Equal(t, 1, len(reports))
+
+			updatedReport := reports[0]
+
+			assert.ElementsMatch(t, test.expectedProjectIDs, updatedReport.Projects)
+		})
+	}
+}
+
+func TestReportProjectDeleteNoReports(t *testing.T) {
+	t.Run("Deleting a project when there are no reports", func(t *testing.T) {
+		// Send a project rules update event
+
+		// wait
+
+		// refresh
+	})
+}
+
+func TestSummaryProjectDelete(t *testing.T) {
+
+	// cases := []struct {
+	// 	description        string
+	// summary             *relaxting.ESInSpecSummary
+	// 	projectIDToDelete  string
+	// 	expectedProjectIDs []string
+	// }{
+	// 	{
+	// 		description: "Deleting the last project tag",
+	// 		report: &relaxting.ESInSpecSummary{
+	// 			Projects: []string{"target_project"},
+	// 		},
+	// 		expectedProjectIDs: []string{},
+	// 	},
+	// 	{
+	// 		description: "Deleting a project that does not exist",
+	// 		report: &relaxting.ESInSpecSummary{
+	// 			Projects: []string{},
+	// 		},
+	// 		expectedProjectIDs: []string{},
+	// 	},
+	// 	{
+	// 		description: "Deleting a project on a report with multiple projects",
+	// 		report: &relaxting.ESInSpecSummary{
+	// 			Projects: []string{"project3", "target_project", "project9"},
+	// 		},
+	// 		expectedProjectIDs: []string{"project3", "project9"},
+	// 	},
+	// }
+
+	// ingest summary
+
+	// call DeleteSummaryProjectTag(ctx, projectToDelete)
+	// require no error
+
+	// wait
+
+	// refresh
+
+	// assert no nodes
+}
+
+func TestSummaryProjectDeleteNoSummaries(t *testing.T) {
+	t.Run("Deleting a project when there are no summaries", func(t *testing.T) {
+		// Send a project rules update event
+
+		// wait
+
+		// refresh
+	})
+}
+
+func TestProjectDelete(t *testing.T) {
+	t.Run("Deleting a project from summaries and reports", func(t *testing.T) {
+		// ingest both summary and report
+
+		// call DeleteProjectTag(ctx, projectToDelete)
+
+		// wait
+
+		// refresh
+
+		// assert project is removed from both summary and report
+	})
+}
