46d8b82b91526682d7084cf09778ce6f993efb74
Blake Johnson <bstier@chef.io>
2019-12-04 10:55:35 -0800

    Delete Summary test
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/compliance-service/integration_test/project_delete_test.go
+++ components/compliance-service/integration_test/project_delete_test.go
@@ -87,55 +87,72 @@ func TestReportProjectDeleteNoReports(t *testing.T) {
 }
 
 func TestSummaryProjectDelete(t *testing.T) {
+	cases := []struct {
+		description        string
+		existingProjectIDs []string
+		projectIDToDelete  string
+		expectedProjectIDs []string
+	}{
+		{
+			description:        "Deleting the last project tag",
+			existingProjectIDs: []string{"target_project"},
+			expectedProjectIDs: []string{},
+		},
+		{
+			description:        "Deleting a project that does not exist",
+			existingProjectIDs: []string{},
+			expectedProjectIDs: []string{},
+		},
+		{
+			description:        "Deleting a project on a summary with multiple projects",
+			existingProjectIDs: []string{"project3", "target_project", "project9"},
+			expectedProjectIDs: []string{"project3", "project9"},
+		},
+	}
 
-	// cases := []struct {
-	// 	description        string
-	// summary             *relaxting.ESInSpecSummary
-	// 	projectIDToDelete  string
-	// 	expectedProjectIDs []string
-	// }{
-	// 	{
-	// 		description: "Deleting the last project tag",
-	// 		report: &relaxting.ESInSpecSummary{
-	// 			Projects: []string{"target_project"},
-	// 		},
-	// 		expectedProjectIDs: []string{},
-	// 	},
-	// 	{
-	// 		description: "Deleting a project that does not exist",
-	// 		report: &relaxting.ESInSpecSummary{
-	// 			Projects: []string{},
-	// 		},
-	// 		expectedProjectIDs: []string{},
-	// 	},
-	// 	{
-	// 		description: "Deleting a project on a report with multiple projects",
-	// 		report: &relaxting.ESInSpecSummary{
-	// 			Projects: []string{"project3", "target_project", "project9"},
-	// 		},
-	// 		expectedProjectIDs: []string{"project3", "project9"},
-	// 	},
-	// }
+	for _, test := range cases {
+		t.Run(test.description, func(t *testing.T) {
 
-	// ingest summary
+			summary := &relaxting.ESInSpecSummary{
+				Projects: test.existingProjectIDs,
+			}
 
-	// call DeleteSummaryProjectTag(ctx, projectToDelete)
-	// require no error
+			_, err := suite.InsertInspecSummaries([]*relaxting.ESInSpecSummary{summary})
+			require.NoError(t, err)
 
-	// wait
+			defer suite.DeleteAllDocuments()
 
-	// refresh
+			esJobID, err := suite.ingesticESClient.DeleteSummaryProjectTag(ctx, projectIDToDelete)
+			require.NoError(t, err)
 
-	// assert no nodes
+			suite.WaitForESJobToComplete(esJobID)
+
+			suite.RefreshComplianceSummaryIndex()
+
+			// get the summaries
+			summaries, err := suite.GetAllSummaryESInSpecSummary()
+			require.NoError(t, err)
+			require.Equal(t, 1, len(summaries))
+
+			updatedSummary := summaries[0]
+
+			assert.ElementsMatch(t, test.expectedProjectIDs, updatedSummary.Projects)
+		})
+	}
 }
 
 func TestSummaryProjectDeleteNoSummaries(t *testing.T) {
 	t.Run("Deleting a project when there are no summaries", func(t *testing.T) {
-		// Send a project rules update event
+		esJobID, err := suite.ingesticESClient.DeleteSummaryProjectTag(ctx, projectIDToDelete)
+		assert.NoError(t, err)
 
-		// wait
+		suite.WaitForESJobToComplete(esJobID)
 
-		// refresh
+		suite.RefreshComplianceSummaryIndex()
+
+		summaries, err := suite.GetAllSummaryESInSpecSummary()
+		require.NoError(t, err)
+		require.Equal(t, 0, len(summaries))
 	})
 }
 
