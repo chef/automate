648dc47a4a2917857388a9cbc56b5133074c4e28
vinay033 <vsharma@chef.io>
2019-12-12 14:38:18 +0530

    added some minor changes
    Signed-off-by: vinay033 <vsharma@chef.io>

--- components/automate-ui/src/app/pages/infra-proxy/cookbook-list/cookbooks-list.component.ts
+++ components/automate-ui/src/app/pages/infra-proxy/cookbook-list/cookbooks-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Store, select } from '@ngrx/store';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, Subject, combineLatest } from 'rxjs';
