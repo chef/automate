c5c137da247da58b2e53fdce3db348d214f04645
michael sorens <msorens@chef.io>
2019-12-23 10:28:04 -0800

    Reverting reference back to main path
    Two previous PRs (#785 and #2267) updated plan.sh
    in a couple projects to be specific to a node.js major version.
    I now believe that was an unnecessary, and in fact, brittle change.
    A node.js package newly installed in core-plans makes its way
    to the "main branch" (https://bldr.habitat.sh/#/pkgs/core/node).
    References should remain invariant to that location
    so they do not need updating when node.js major versions change.
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-workflow-web/habitat/plan.sh
+++ components/automate-workflow-web/habitat/plan.sh
@@ -11,7 +11,7 @@ pkg_deps=(
 pkg_build_deps=(
   core/make
   core/gcc
-  core/node10/"$(cat "$PLAN_CONTEXT/../.nvmrc")"
+  core/node/"$(cat "$PLAN_CONTEXT/../.nvmrc")"
   core/phantomjs
 )
 pkg_description="Web interface for Chef Automate Workflow"
