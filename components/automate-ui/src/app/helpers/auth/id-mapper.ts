03d9718d90b8d7339d0c051137d88d025f5cd3b5
michael sorens <msorens@users.noreply.github.com>
2019-12-26 20:37:14 -0800

    Adds support for preserving underscore
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/helpers/auth/id-mapper.ts
+++ components/automate-ui/src/app/helpers/auth/id-mapper.ts
@@ -1,6 +1,6 @@
 export class IdMapper {
 
   public static transform(input: string): string {
-    return input.toLowerCase().replace(/[^a-z0-9]/g, '-');
+    return input.toLowerCase().replace(/[^a-z0-9_]/g, '-');
   }
 }
