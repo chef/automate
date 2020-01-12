831d0c5020f119f6bc9676a007a74488bb9c175d
michael sorens <msorens@users.noreply.github.com>
2020-01-11 16:10:06 -0800

    Post-upgrade compilation fix: ModuleWithProviders
    $ ng build
    
    ERROR in src/app/pages/+compliance/shared/shared.module.ts:28:21 - error TS2304: Cannot find name 'ModuleWithProviders'.
    
    28   static forRoot(): ModuleWithProviders<ComplianceSharedModule> {
                           ~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/pages/+compliance/shared/shared.module.ts
+++ components/automate-ui/src/app/pages/+compliance/shared/shared.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA , ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChefComponentsModule } from '../../../components/chef-components.module';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { ChefComponentsModule } from 'app/components/chef-components.module';
 import { StatsService } from './reporting';
 import { ReportQueryService } from './reporting/report-query.service';
 import { ReportDataService } from './reporting/report-data.service';
