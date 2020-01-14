daca5ce9794811c4cdc79abc84fb4c9f51809947
vinay033 <vsharma@chef.io>
2020-01-14 16:48:23 +0530

    added test cases for the components
    Signed-off-by: vinay033 <vsharma@chef.io>

--- components/automate-ui/src/app/modules/infra-proxy/chef-server-details/chef-server-details.component.spec.ts
+++ components/automate-ui/src/app/modules/infra-proxy/chef-server-details/chef-server-details.component.spec.ts
@@ -1,15 +1,58 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ChefServerDetailsComponent } from './chef-server-details.component';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CommonModule }  from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
+import { MockComponent } from 'ng2-mock-component';
+import { StoreModule, Store } from '@ngrx/store';
+import { Org } from 'app/entities/orgs/org.model';
+import { CreateOrgSuccess, CreateOrgFailure } from 'app/entities/orgs/org.actions';
+import { NgrxStateAtom, ngrxReducers, runtimeChecks } from 'app/ngrx.reducers';
+import { HttpStatus } from 'app/types/types';
+import { FeatureFlagsService } from 'app/services/feature-flags/feature-flags.service';
 
-xdescribe('ChefServerDetailsComponent', () => {
+describe('ChefServerDetailsComponent', () => {
   let component: ChefServerDetailsComponent;
   let fixture: ComponentFixture<ChefServerDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChefServerDetailsComponent ],
+      imports: [
+        CommonModule,
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterTestingModule,
+        StoreModule.forRoot(ngrxReducers, { runtimeChecks })
+      ],
+      declarations: [
+        MockComponent({ selector: 'chef-control-menu' }),
+        MockComponent({ selector: 'chef-th' }),
+        MockComponent({ selector: 'chef-td' }),
+        MockComponent({ selector: 'chef-error' }),
+        MockComponent({ selector: 'chef-form-field' }),
+        MockComponent({ selector: 'chef-heading' }),
+        MockComponent({ selector: 'chef-icon' }),
+        MockComponent({ selector: 'chef-loading-spinner' }),
+        MockComponent({ selector: 'chef-option' }),
+        MockComponent({ selector: 'chef-page-header' }),
+        MockComponent({ selector: 'chef-subheading' }),
+        MockComponent({ selector: 'chef-toolbar' }),
+        MockComponent({ selector: 'chef-table-new' }),
+        MockComponent({ selector: 'chef-table-header' }),
+        MockComponent({ selector: 'chef-table-body' }),
+        MockComponent({ selector: 'chef-table-row' }),
+        MockComponent({ selector: 'chef-table-header-cell' }),
+        MockComponent({ selector: 'chef-table-cell' }),
+        MockComponent({ selector: 'a', inputs: ['routerLink'] }),
+        ChefServerDetailsComponent
+      ],
+      providers: [
+        FeatureFlagsService
+      ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
@@ -19,9 +62,96 @@ xdescribe('ChefServerDetailsComponent', () => {
     fixture = TestBed.createComponent(ChefServerDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.updateServerForm = new FormBuilder().group({
+      name: ['', null],
+      description: ['', null],
+      fqdn: ['', null],
+      ip_address: ['', null]
+    });
+
+    component.orgForm = new FormBuilder().group({
+      name: ['', null],
+      admin_user: ['', null],
+      admin_key: ['', null]
+    });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('create org', () => {
+    let store: Store<NgrxStateAtom>;
+    const org = <Org> {
+        id: '1',
+        name: 'new org',
+        admin_user: 'new org user',
+        admin_key: 'new admin key'
+      };
+
+    beforeEach(() => {
+      store = TestBed.get(Store);
+    });
+
+    it('openCreateModal opens modal', () => {
+      expect(component.createModalVisible).toBe(false);
+      component.openCreateModal('create');
+      expect(component.createModalVisible).toBe(true);
+    });
+
+    it('opening create modal resets name, description, fqdn and ip_address to empty string', () => {
+      component.openCreateModal('create');
+      expect(component.orgForm.controls['name'].value).toEqual('');
+      expect(component.orgForm.controls['admin_user'].value).toEqual('')
+      expect(component.orgForm.controls['admin_key'].value).toEqual('')
+    });
+
+    it('on success, closes modal and adds new server', () => {
+      component.orgForm.controls['name'].setValue(org.name);
+      component.orgForm.controls['admin_user'].setValue(org.admin_user);
+      component.orgForm.controls['admin_key'].setValue(org.admin_key);
+      component.createServerOrg();
+
+      store.dispatch(new CreateOrgSuccess({'org': org}));
+
+      component.orgs.map(orgs => {
+        expect(orgs).toContain(org);
+      });
+    });
+
+    it('on conflict error, modal is open with conflict error', () => {
+      spyOn(component.conflictErrorEvent, 'emit');
+      component.openCreateModal('create');
+      component.orgForm.controls['name'].setValue(org.name);
+      component.orgForm.controls['admin_user'].setValue(org.admin_user);
+      component.orgForm.controls['admin_key'].setValue(org.admin_key);
+      component.createServerOrg();
+
+      const conflict = <HttpErrorResponse>{
+        status: HttpStatus.CONFLICT,
+        ok: false
+      };
+      store.dispatch(new CreateOrgFailure(conflict));
+
+      expect(component.createModalVisible).toBe(true);
+    });
+
+    it('on create error, modal is closed with failure banner', () => {
+      spyOn(component.conflictErrorEvent, 'emit');
+      component.openCreateModal('create');
+      component.orgForm.controls['name'].setValue(org.name);
+      component.orgForm.controls['admin_user'].setValue(org.admin_user);
+      component.orgForm.controls['admin_key'].setValue(org.admin_key);
+      component.createServerOrg();
+
+      const error = <HttpErrorResponse>{
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        ok: false
+      };
+      store.dispatch(new CreateOrgFailure(error));
+
+      expect(component.createModalVisible).toBe(true);
+    });
+
   });
 });
--- components/automate-ui/src/app/modules/infra-proxy/create-org-modal/create-org-modal.component.spec.ts
+++ components/automate-ui/src/app/modules/infra-proxy/create-org-modal/create-org-modal.component.spec.ts
@@ -1,7 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { CreateOrgModalComponent } from './create-org-modal.component';
+import { MockComponent } from 'ng2-mock-component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CommonModule }  from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 describe('CreateOrgModalComponent', () => {
   let component: CreateOrgModalComponent;
@@ -9,7 +13,22 @@ describe('CreateOrgModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateOrgModalComponent ],
+      declarations: [
+        CreateOrgModalComponent,
+        MockComponent({ selector: 'chef-button', inputs: ['disabled'] }),
+        MockComponent({ selector: 'chef-loading-spinner' }),
+        MockComponent({ selector: 'chef-form-field' }),
+        MockComponent({ selector: 'chef-checkbox' }),
+        MockComponent({ selector: 'chef-error' }),
+        MockComponent({ selector: 'chef-toolbar' }),
+        MockComponent({ selector: 'chef-modal', inputs: ['visible'] }),
+      ],
+      imports: [
+        CommonModule,
+        BrowserModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
@@ -18,7 +37,11 @@ describe('CreateOrgModalComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateOrgModalComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.createForm = new FormBuilder().group({
+      name: ['', null],
+      admin_user: ['', null],
+      admin_key: ['', null]
+    });
   });
 
   it('should create', () => {
