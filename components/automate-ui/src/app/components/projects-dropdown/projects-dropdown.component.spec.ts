00f11e25ef90e6c0664c5826b669f53b42890881
seajamied <jdegnan@chef.io>
2020-01-22 14:34:44 -0800

    Add basic test for dropdown
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/components/projects-dropdown/projects-dropdown.component.spec.ts
+++ components/automate-ui/src/app/components/projects-dropdown/projects-dropdown.component.spec.ts
@@ -27,4 +27,38 @@ describe('ProjectsDropdownComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('dropdown', () => {
+    beforeEach(() => {
+      component.active = true;
+      component.filteredProjects = [
+        {
+          name: 'Project 1',
+          id: 'project-1',
+          type: 'CUSTOM',
+          status: 'NO_RULES',
+          checked: false
+        },
+        {
+          name: 'Project 2',
+          id: 'project-2',
+          type: 'CUSTOM',
+          status: 'NO_RULES',
+          checked: true
+        }
+      ];
+      fixture.detectChanges();
+    });
+
+    it('displays a list of checkbox options', () => {
+      const options = Array.from(fixture.nativeElement.querySelectorAll('chef-checkbox'));
+      expect(options.length).toEqual(2);
+      options.forEach((option: HTMLInputElement, index: number) => {
+        const { name, checked } = component.filteredProjects[index];
+        expect(option.textContent).toEqual(name);
+        expect(option.checked).toEqual(checked);
+      });
+    });
+
+  });
 });
