e27b9ab9f3e33fc80a330df15b2fa4772461ea41
michael sorens <msorens@users.noreply.github.com>
2020-01-22 20:01:34 -0800

    Migrate tests over to create-user-modal
    All unit tests from user-management and user-form now belong
    with create-user-modal--in fact, just copy/pasted them verbatim
    and they worked perfectly!
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/page-components/create-user-modal/create-user-modal.component.spec.ts
+++ components/automate-ui/src/app/page-components/create-user-modal/create-user-modal.component.spec.ts
@@ -6,6 +6,7 @@ import { MockComponent } from 'ng2-mock-component';
 import { ngrxReducers, runtimeChecks } from 'app/ngrx.reducers';
 import { CreateUserModalComponent } from './create-user-modal.component';
 import { EventEmitter } from '@angular/core';
+import { using } from 'app/testing/spec-helpers';
 
 describe('CreateUserModalComponent', () => {
   let component: CreateUserModalComponent;
@@ -40,4 +41,52 @@ describe('CreateUserModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+ describe('#createUserForm', () => {
+    it('should be invalid when no fields are filled out', () => {
+      expect(component.createUserForm.valid).toBeFalsy();
+    });
+
+     it('should be valid when all fields are filled out and passwords match', () => {
+      component.createUserForm.controls['displayName'].setValue('Sam');
+      component.createUserForm.controls['username'].setValue('Mary');
+      component.createUserForm.controls['password'].setValue('imawitch');
+      component.createUserForm.controls['confirmPassword'].setValue('imawitch');
+      expect(component.createUserForm.valid).toBeTruthy();
+    });
+  });
+
+  describe('#matchFieldValidator', () => {
+    beforeEach(() => {
+      component.createUserForm.controls['password'].setValue('imawitch');
+    });
+
+    it('should be invalid when password and confirmPassword do not match', () => {
+      component.createUserForm.controls['confirmPassword'].setValue('notawitch');
+      expect(component.createUserForm.get('confirmPassword').hasError('noMatch')).toBeTruthy();
+    });
+
+    it('should be valid when password and confirmPassword match', () => {
+      component.createUserForm.controls['confirmPassword'].setValue('imawitch');
+      expect(component.createUserForm.get('confirmPassword').hasError('noMatch')).toBeFalsy();
+    });
+  });
+
+  using([
+    ['my-name', 'my-name', 'mirrors input with no leading/trailing whitespace'],
+    ['  my-name  ', 'my-name', 'trims leading whitespace'],
+    ['my-name  ', 'my-name', 'trims trailing whitespace']
+  ], function (inputVal: string, outputVal: string, desc: string) {
+
+    it('handleNameInput ' + desc, () => {
+      component.createUserForm.controls['displayName'].setValue(inputVal);
+      component.handleNameInput(new KeyboardEvent('keyup',
+        // Typescript bug requires us to work around with <any>
+        // https://github.com/Microsoft/TypeScript/issues/15228
+        <any>{bubbles : true, cancelable : true, key : inputVal, char : inputVal, shiftKey : true
+      }));
+      expect(component.createUserForm.controls['username'].value).toBe(outputVal);
+    });
+  });
+
 });
