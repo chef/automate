chef/automate

commit 8ad71b49b15a31b64c6564353fdb780e2b9aa424

[backup] allow deprecated config during restore (#2344)
* Fix a bug where existing config with deprecated settings 
  prevented restoring because it was re-validated.
  existing configuration which may have deprecated keys.
* When passing a configuration patch during restore we now generate a
  new user override config from the backup task, apply the patch to the
  task override config, then apply the aggregate override config to the
  existing deployment config.
* Validate patch config against redacted existing config
* Validate redacted set config
* Add unit testing for all of these scenarios

Co-Authored-by: Jay Mundrawala <jmundrawala@chef.io>
Signed-off-by: Ryan Cragun <ryan@chef.io>

api/interservice/deployment/override_config.go
@@ -1,32 +1,83 @@
 package deployment
 
 import (
+	"github.com/sirupsen/logrus"
+
 	api "github.com/chef/automate/api/config/deployment"
 	config "github.com/chef/automate/api/config/shared"
 	w "github.com/chef/automate/api/config/shared/wrappers"
-	"github.com/sirupsen/logrus"
 )
 
-// NewUserOverrideConfigFromBackupRestoreTask takes a BackupRestoreTask and
-// converts the restoration configuration contained within into a new user
-// override AutomateConfig.
-func NewUserOverrideConfigFromBackupRestoreTask(req *BackupRestoreTask) *api.AutomateConfig {
-	// Convert a BackupRestoreTask into a sparse AutomateConfig that is suitable
-	// to be merged into the UserOverrideConfig
+// MergeAndValidateNewUserOverrideConfig is responsible for taking an existing
+// user override AutomateConfig (from a backup), building a new user override
+// config from a BackupRestoreTask (args for the backup restore), and validating
+// the resulting user override config.
+//
+// Validation of the config is a bit tricky as there are a few corner cases we
+// need to worry about. It's possible that the user wants to pass in config set
+// or patch that includes deprecated values since it's possible that the version
+// of the software they are restoring could use those values. We'll handle this
+// case by validating the with a redacted copy of the config.
+func MergeAndValidateNewUserOverrideConfig(existing *api.AutomateConfig, req *BackupRestoreTask) error {
+	var err error
 	cfg := &api.AutomateConfig{}
+
+	// They gave us nothing, return the existing config.
 	if req == nil {
-		return cfg
-	}
-	if req.PatchConfig != nil {
-		cfg = req.PatchConfig
+		return nil
 	}
 
+	// If they passed a config set we'll only use it as that is their desired
+	// config.
+	if setConfig := req.GetSetConfig(); setConfig != nil {
+		redactedCopy, err := setConfig.RedactedCopy()
+		if err != nil {
+			return err
+		}
+
+		err = redactedCopy.ValidateWithGlobalAndDefaults()
+		if err != nil {
+			return err
+		}
+
+		*existing = *setConfig
+		return nil
+	}
+
+	// If they passed a patch we'll validate a redacted version. If it doesn't
+	// pass then they've passed in an invalid patch. Later we'll
+	// merge the patch config into the config that we'll generate from the
+	// restore task.
+	patchConfig := req.GetPatchConfig()
+	if patchConfig != nil {
+		existingRedactedCopy, err := existing.RedactedCopy()
+		if err != nil {
+			return err
+		}
+
+		redactedPatch, err := patchConfig.RedactedCopy()
+		if err != nil {
+			return err
+		}
+
+		err = existingRedactedCopy.OverrideConfigValues(redactedPatch)
+		if err != nil {
+			return err
+		}
+
+		err = existingRedactedCopy.ValidateWithGlobalAndDefaults()
+		if err != nil {
+			return err
+		}
+	}
+
+	// Merge the restore task options into the config
 	reqS3 := req.GetS3BackupLocation()
 	if reqS3.GetBucketName() == "" {
 		// Return the defaults because we're in filesystem mode and the request
 		// caries no meaningful backup info in that mode.
 		// NOTE: It would be nice to carry the endpoint type in the backup req
-		return cfg
+		return nil
 	}
 
 	// We're in S3 mode so we need to populate the config with any options
@@ -79,5 +130,17 @@ func NewUserOverrideConfigFromBackupRestoreTask(req *BackupRestoreTask) *api.Aut
 		creds.SessionToken = w.String(sessionToken)
 	}
 
-	return cfg
+	if patchConfig != nil {
+		err = cfg.OverrideConfigValues(patchConfig)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = existing.OverrideConfigValues(cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }

api/interservice/deployment/override_config_test.go
@@ -3,24 +3,79 @@ package deployment
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
+	"github.com/chef/automate/api/config/compliance"
 	dc "github.com/chef/automate/api/config/deployment"
+	"github.com/chef/automate/api/config/event_feed"
+	"github.com/chef/automate/api/config/ingest"
+	"github.com/chef/automate/api/config/load_balancer"
 	config "github.com/chef/automate/api/config/shared"
 	w "github.com/chef/automate/api/config/shared/wrappers"
-	"github.com/stretchr/testify/require"
 )
 
-func TestNewUserOverrideConfigFromBackupRestoreTask(t *testing.T) {
-	t.Run("nil task", func(t *testing.T) {
-		cfg := NewUserOverrideConfigFromBackupRestoreTask(nil)
-		require.Equal(t, &dc.AutomateConfig{}, cfg)
+// Return a valid user override config that a user must pass when configuring
+// Automate.
+func newValidOverrideConfig() *dc.AutomateConfig {
+	cfg := &dc.AutomateConfig{
+		Global: &config.GlobalConfig{
+			V1: &config.V1{
+				Fqdn: w.String("test-fqdn"),
+			},
+		},
+		LoadBalancer: &load_balancer.ConfigRequest{
+			V1: &load_balancer.ConfigRequest_V1{
+				Sys: &load_balancer.ConfigRequest_V1_System{
+					FrontendTls: []*config.FrontendTLSCredential{
+						&config.FrontendTLSCredential{
+							ServerName: "test",
+							Cert:       "cert",
+							Key:        "key",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return cfg
+}
+
+func newDeprecatedConfig() *dc.AutomateConfig {
+	cfg := newValidOverrideConfig()
+	cfg.Compliance = compliance.NewConfigRequest()
+	cfg.Compliance.V1.Sys.Retention = &compliance.ConfigRequest_V1_System_Retention{
+		ComplianceReportDays: w.Int32(30),
+	}
+	cfg.EventFeedService = event_feed.NewConfigRequest()
+	cfg.EventFeedService.V1.Sys.Service.PurgeEventFeedAfterDays = w.Int32(7)
+	cfg.Ingest = ingest.NewConfigRequest()
+	cfg.Ingest.V1.Sys.Service.PurgeActionsAfterDays = w.Int32(60)
+	cfg.Ingest.V1.Sys.Service.PurgeConvergeHistoryAfterDays = w.Int32(60)
+
+	return cfg
+}
+
+func TestMergeAndValidateNewUserOverrideConfig(t *testing.T) {
+	t.Run("nil restore task", func(t *testing.T) {
+		cfg := newValidOverrideConfig()
+		err := MergeAndValidateNewUserOverrideConfig(cfg, nil)
+		require.NoError(t, err)
+		require.Equal(t, newValidOverrideConfig(), cfg)
 	})
+
 	t.Run("filesystem restore task", func(t *testing.T) {
 		// In filesystem mode we don't yet care about information passed through
 		// the restore task so the override config should be blank
 		rt := NewBackupRestoreTask()
-		cfg := NewUserOverrideConfigFromBackupRestoreTask(rt)
-		require.Equal(t, &dc.AutomateConfig{}, cfg)
+		cfg := newValidOverrideConfig()
+		t.Log(cfg)
+		err := MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		t.Log(cfg)
+		require.NoError(t, err)
+		require.Equal(t, newValidOverrideConfig(), cfg)
 	})
+
 	t.Run("s3 restore task", func(t *testing.T) {
 		rt := NewBackupRestoreTask()
 		rt.S3BackupLocation = &S3BackupLocation{
@@ -31,7 +86,7 @@ func TestNewUserOverrideConfigFromBackupRestoreTask(t *testing.T) {
 			SecretKey:    "MYSUPERSECRETKEY",
 			SessionToken: "MYSESSIONTOKEN",
 		}
-		expectedCfg := &dc.AutomateConfig{Global: config.NewGlobalConfig()}
+		expectedCfg := newValidOverrideConfig()
 		expectedCfg.Global.V1.Backups = &config.Backups{
 			S3: &config.Backups_S3{
 				Bucket: &config.Backups_S3_Bucket{
@@ -46,10 +101,14 @@ func TestNewUserOverrideConfigFromBackupRestoreTask(t *testing.T) {
 				},
 			},
 		}
-		cfg := NewUserOverrideConfigFromBackupRestoreTask(rt)
+		cfg := newValidOverrideConfig()
+		err := MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		require.NoError(t, err)
 		require.Equal(t, expectedCfg, cfg)
 	})
-	t.Run("with patch config", func(t *testing.T) {
+
+	t.Run("config patch", func(t *testing.T) {
+		cfg := newValidOverrideConfig()
 		rt := NewBackupRestoreTask()
 		rt.S3BackupLocation = &S3BackupLocation{
 			BucketName:   "backup-bucket",
@@ -65,25 +124,165 @@ func TestNewUserOverrideConfigFromBackupRestoreTask(t *testing.T) {
 		rt.PatchConfig.Global.V1.Log = &config.Log{
 			Level: w.String("debug"),
 		}
-		expectedCfg := &dc.AutomateConfig{Global: config.NewGlobalConfig()}
+		rt.PatchConfig.Global.V1.Backups = &config.Backups{
+			S3: &config.Backups_S3{
+				Bucket: &config.Backups_S3_Bucket{
+					Name:     w.String("override-backup-bucket"),
+					BasePath: w.String("my-org/chef-automate-override"),
+					Endpoint: w.String("s3-us-east-1.amazonaws.com"),
+				},
+				Credentials: &config.Backups_S3_AWSCredentials{
+					AccessKey:    w.String("AKIMYACCESSKEYOVERRIDE"),
+					SecretKey:    w.String("MYSUPERSECRETKEYOVERRIDE"),
+					SessionToken: w.String("MYSESSIONTOKENOVERRIDE"),
+				},
+			},
+		}
+
+		// Make sure the patch config overrides the default backup config
+		// from the restore task
+		expectedCfg := newValidOverrideConfig()
 		expectedCfg.Global.V1.Backups = &config.Backups{
 			S3: &config.Backups_S3{
 				Bucket: &config.Backups_S3_Bucket{
-					Name:     w.String("backup-bucket"),
-					BasePath: w.String("my-org/chef-automate"),
-					Endpoint: w.String("s3-us-east-2.amazonaws.com"),
+					Name:     w.String("override-backup-bucket"),
+					BasePath: w.String("my-org/chef-automate-override"),
+					Endpoint: w.String("s3-us-east-1.amazonaws.com"),
 				},
 				Credentials: &config.Backups_S3_AWSCredentials{
-					AccessKey:    w.String("AKIMYACCESSKEY"),
-					SecretKey:    w.String("MYSUPERSECRETKEY"),
-					SessionToken: w.String("MYSESSIONTOKEN"),
+					AccessKey:    w.String("AKIMYACCESSKEYOVERRIDE"),
+					SecretKey:    w.String("MYSUPERSECRETKEYOVERRIDE"),
+					SessionToken: w.String("MYSESSIONTOKENOVERRIDE"),
 				},
 			},
 		}
 		expectedCfg.Global.V1.Log = &config.Log{
 			Level: w.String("debug"),
 		}
-		cfg := NewUserOverrideConfigFromBackupRestoreTask(rt)
+		err := MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		require.NoError(t, err)
 		require.Equal(t, expectedCfg, cfg)
+	})
+
+	t.Run("config set", func(t *testing.T) {
+		cfg := newValidOverrideConfig()
+		rt := NewBackupRestoreTask()
+		rt.S3BackupLocation = &S3BackupLocation{
+			BucketName:   "backup-bucket",
+			BasePath:     "my-org/chef-automate",
+			Endpoint:     "s3-us-east-2.amazonaws.com",
+			AccessKey:    "AKIMYACCESSKEY",
+			SecretKey:    "MYSUPERSECRETKEY",
+			SessionToken: "MYSESSIONTOKEN",
+		}
+		rt.SetConfig = newValidOverrideConfig()
+		rt.SetConfig.Global.V1.Log = &config.Log{
+			Level: w.String("debug"),
+		}
+		rt.SetConfig.Global.V1.Backups = &config.Backups{
+			S3: &config.Backups_S3{
+				Bucket: &config.Backups_S3_Bucket{
+					Name:     w.String("override-backup-bucket"),
+					BasePath: w.String("my-org/chef-automate-override"),
+					Endpoint: w.String("s3-us-east-1.amazonaws.com"),
+				},
+				Credentials: &config.Backups_S3_AWSCredentials{
+					AccessKey:    w.String("AKIMYACCESSKEYOVERRIDE"),
+					SecretKey:    w.String("MYSUPERSECRETKEYOVERRIDE"),
+					SessionToken: w.String("MYSESSIONTOKENOVERRIDE"),
+				},
+			},
+		}
+
+		// Make sure the set config is exactly what we've passed
+		expectedCfg := newValidOverrideConfig()
+		expectedCfg.Global.V1.Backups = &config.Backups{
+			S3: &config.Backups_S3{
+				Bucket: &config.Backups_S3_Bucket{
+					Name:     w.String("override-backup-bucket"),
+					BasePath: w.String("my-org/chef-automate-override"),
+					Endpoint: w.String("s3-us-east-1.amazonaws.com"),
+				},
+				Credentials: &config.Backups_S3_AWSCredentials{
+					AccessKey:    w.String("AKIMYACCESSKEYOVERRIDE"),
+					SecretKey:    w.String("MYSUPERSECRETKEYOVERRIDE"),
+					SessionToken: w.String("MYSESSIONTOKENOVERRIDE"),
+				},
+			},
+		}
+		expectedCfg.Global.V1.Log = &config.Log{
+			Level: w.String("debug"),
+		}
+		err := MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		require.NoError(t, err)
+		require.Equal(t, expectedCfg, cfg)
+	})
+
+	t.Run("existing deprecated config", func(t *testing.T) {
+		rt := NewBackupRestoreTask()
+		cfg := newValidOverrideConfig()
+		err := cfg.OverrideConfigValues(newDeprecatedConfig())
+		require.NoError(t, err)
+		err = MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		require.NoError(t, err)
+	})
+
+	t.Run("config set with deprecated config", func(t *testing.T) {
+		rt := NewBackupRestoreTask()
+		cfg := newValidOverrideConfig()
+		err := cfg.OverrideConfigValues(newDeprecatedConfig())
+		require.NoError(t, err)
+
+		rt.SetConfig = cfg
+		err = MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		require.NoError(t, err)
+	})
+
+	t.Run("config patch with deprecated config", func(t *testing.T) {
+		rt := NewBackupRestoreTask()
+		cfg := newValidOverrideConfig()
+		rt.PatchConfig = newDeprecatedConfig()
+		err := MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		require.NoError(t, err)
+	})
+
+	t.Run("config patch valid patch with existing deprecated config", func(t *testing.T) {
+		rt := NewBackupRestoreTask()
+		cfg := newValidOverrideConfig()
+		err := cfg.OverrideConfigValues(newDeprecatedConfig())
+		require.NoError(t, err)
+
+		rt.PatchConfig = &dc.AutomateConfig{
+			Global: config.NewGlobalConfig(),
+		}
+		rt.PatchConfig.Global.V1.Log = &config.Log{
+			Level: w.String("debug"),
+		}
+
+		expectedCfg := newValidOverrideConfig()
+		err = expectedCfg.OverrideConfigValues(newDeprecatedConfig())
+		require.NoError(t, err)
+		expectedCfg.Global.V1.Log = &config.Log{
+			Level: w.String("debug"),
+		}
+
+		err = MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		require.NoError(t, err)
+	})
+
+	t.Run("config patch deprecated patch with existing deprecated config", func(t *testing.T) {
+		rt := NewBackupRestoreTask()
+		cfg := newValidOverrideConfig()
+		err := cfg.OverrideConfigValues(newDeprecatedConfig())
+		require.NoError(t, err)
+
+		rt.PatchConfig = newDeprecatedConfig()
+		rt.PatchConfig.Compliance.V1.Sys.Retention.ComplianceReportDays = w.Int32(10)
+		rt.PatchConfig.EventFeedService.V1.Sys.Service.PurgeEventFeedAfterDays = w.Int32(7)
+		rt.PatchConfig.Ingest.V1.Sys.Service.PurgeActionsAfterDays = w.Int32(60)
+		rt.PatchConfig.Ingest.V1.Sys.Service.PurgeConvergeHistoryAfterDays = w.Int32(60)
+
+		err = MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		require.NoError(t, err)
 	})
 }

.expeditor/verify_private.pipeline.yml
@@ -731,6 +731,15 @@ steps:
           single-use: true
           privileged: true
 
+  - label: "deep upgrade backup restore"
+    command:
+      - integration/run_test integration/tests/deep_upgrade_backup_restore.sh
+    timeout_in_minutes: 25
+    expeditor:
+      executor:
+        linux:
+          privileged: true
+
   - label: "backups"
     command:
       - integration/run_test integration/tests/backup.sh

components/automate-deployment/pkg/server/automate_config.go
@@ -3,7 +3,7 @@ package server
 import (
 	"context"
 
-	"github.com/chef/automate/api/config/deployment"
+	"github.com/pkg/errors"
 
 	api "github.com/chef/automate/api/interservice/deployment"
 	"github.com/chef/automate/components/automate-deployment/pkg/converge"
@@ -212,37 +212,24 @@ func (s *server) updateUserOverrideConfigFromRestoreBackupRequest(req *api.Resto
 	// update them before we bootstrap backup-gateway. If we don't do that the
 	// bgw may be configured to use the wrong bucket or credentials.
 
-	var reqCfg *deployment.AutomateConfig
-	var curCfg *deployment.AutomateConfig
-	if req.GetRestore().GetSetConfig() != nil {
-		curCfg = req.GetRestore().GetSetConfig()
-	} else {
-		var err error
-		curCfg, err = s.deployment.GetUserOverrideConfigForPersistence().NewDeepCopy()
-		if err != nil {
-			return status.Error(codes.FailedPrecondition, "Failed to load configuration")
-		}
+	cfg, err := s.deployment.GetUserOverrideConfigForPersistence().NewDeepCopy()
+	if err != nil {
+		return status.Error(codes.FailedPrecondition, errors.Wrap(err, "copying existing config").Error())
 	}
 
-	reqCfg = api.NewUserOverrideConfigFromBackupRestoreTask(req.GetRestore())
-
-	if err := curCfg.OverrideConfigValues(reqCfg); err != nil {
-		return status.Error(codes.Internal, "Failed to set backup configuration")
+	if err = api.MergeAndValidateNewUserOverrideConfig(cfg, req.GetRestore()); err != nil {
+		return status.Error(codes.InvalidArgument, errors.Wrap(err, "updating config").Error())
 	}
 
-	if err := curCfg.ValidateWithGlobalAndDefaults(); err != nil {
-		return status.Error(codes.InvalidArgument, err.Error())
+	if err = s.deployment.ReplaceUserOverrideConfig(cfg); err != nil {
+		return status.Error(codes.Internal, errors.Wrap(err, "replacing config").Error())
 	}
 
-	if err := s.deployment.ReplaceUserOverrideConfig(curCfg); err != nil {
+	if err = s.updateExpectedServices(); err != nil {
 		return status.Error(codes.Internal, err.Error())
 	}
 
-	if err := s.updateExpectedServices(); err != nil {
-		return status.Error(codes.Internal, err.Error())
-	}
-
-	if err := s.persistDeployment(); err != nil {
+	if err = s.persistDeployment(); err != nil {
 		return status.Error(codes.Internal, err.Error())
 	}
 

integration/tests/deep_upgrade_backup_restore.sh
@@ -1,0 +1,83 @@
+#!/bin/bash
+
+#shellcheck disable=SC2034
+test_name="deep_upgrade_backup_restore"
+test_upgrades=true
+test_upgrade_strategy="none"
+test_loadbalancer_url="https://localhost:4443"
+upgrade_scaffold_pid_file="/tmp/upgrade-scaffold-pid"
+test_diagnostics_pre_upgrade_filters="~skip-for-deep-upgrade"
+test_diagnostics_filters="~purge"
+
+CURRENT_OLDEST_VERSION=20190501153509
+OLD_MANIFEST_DIR="${A2_ROOT_DIR}/components/automate-deployment/testdata/old_manifests/"
+DEEP_UPGRADE_PATH="${OLD_MANIFEST_DIR}/${CURRENT_OLDEST_VERSION}.json"
+
+do_create_config() {
+    log_info "Deferring configuration creation until do_deploy"
+}
+
+do_prepare_deploy() {
+    do_prepare_deploy_default
+
+    mkdir /etc/systemd/system/chef-automate.service.d
+    cat > /etc/systemd/system/chef-automate.service.d/custom.conf <<EOF
+[Service]
+Environment=CHEF_AUTOMATE_SKIP_MANIFEST_VERIFICATION=true
+EOF
+}
+
+do_deploy() {
+    #shellcheck disable=SC2154
+    cp "$DEEP_UPGRADE_PATH" "$test_manifest_path"
+    install_hab "0.54.0"
+    upgrade_scaffold_bin="$(a2_root_dir)/components/automate-deployment/bin/linux/upgrade-test-scaffold"
+    $upgrade_scaffold_bin setup "$test_manifest_path"
+    $upgrade_scaffold_bin serve "$test_manifest_path" "$upgrade_scaffold_pid_file" &
+    sleep 5
+
+    log_info "Generating Automate configuration"
+    #shellcheck disable=SC2154
+    /bin/chef-automate init-config \
+        --channel "$test_channel" \
+        --file "$test_config_path" \
+        --upgrade-strategy "$test_upgrade_strategy"
+    cat <<EOF >>"$test_config_path"
+[deployment.v1.sys.log]
+  level = "debug"
+[dex.v1.sys.expiry]
+  id_tokens = "5m"
+[postgresql.v1.sys.pg]
+  shared_buffers = "1GB"
+[load_balancer.v1.sys.service]
+  https_port = 4443
+[compliance.v1.sys.retention]
+compliance_report_days = 50
+EOF
+
+    #shellcheck disable=SC2154
+    /bin/chef-automate deploy "$test_config_path" \
+        --hartifacts "$test_hartifacts_path" \
+        --override-origin "$HAB_ORIGIN" \
+        --upgrade-strategy "$test_upgrade_strategy" \
+        --accept-terms-and-mlsa \
+        --admin-password chefautomate \
+        --skip-preflight \
+        --debug
+}
+
+do_upgrade() {
+    do_upgrade_default
+    do_backup_default
+
+    #shellcheck disable=SC2154
+    chef-automate backup restore \
+        --debug \
+        --override-origin "$HAB_ORIGIN" \
+        --yes \
+        "$test_backup_id"
+}
+
+do_cleanup() {
+    kill "$(cat $upgrade_scaffold_pid_file)"
+}

commit d032f028cfb5b75f0b269494d494a5336382c175

Fix broken patch config during restore
It seems to have broken in #2344
Basically, in most cases, the code ends up discarding the requested
patch config because it returns early. The only case when this doesn't
happen is if you're doing backups to S3

Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

api/interservice/deployment/override_config.go
@@ -73,61 +73,56 @@ func MergeAndValidateNewUserOverrideConfig(existing *api.AutomateConfig, req *Ba
 
 	// Merge the restore task options into the config
 	reqS3 := req.GetS3BackupLocation()
-	if reqS3.GetBucketName() == "" {
-		// Return the defaults because we're in filesystem mode and the request
-		// caries no meaningful backup info in that mode.
-		// NOTE: It would be nice to carry the endpoint type in the backup req
-		return nil
-	}
+	if reqS3.GetBucketName() != "" {
+		// We're in S3 mode so we need to populate the config with any options
+		// that are in the req.
+		if cfg.GetGlobal().GetV1() == nil {
+			cfg.Global = config.NewGlobalConfig()
+		}
 
-	// We're in S3 mode so we need to populate the config with any options
-	// that are in the req.
-	if cfg.GetGlobal().GetV1() == nil {
-		cfg.Global = config.NewGlobalConfig()
-	}
+		if cfg.Global.V1.Backups == nil {
+			cfg.Global.V1.Backups = &config.Backups{}
+		}
 
-	if cfg.Global.V1.Backups == nil {
-		cfg.Global.V1.Backups = &config.Backups{}
-	}
+		if cfg.Global.V1.Backups.GetS3() != nil {
+			// We can't use the config from the override config because it might
+			// be different than where you told us the backup lives. We need to
+			// configure automate to use the backup location where the backup lives
+			// in order to restore (mostly because of the way ES works)
+			logrus.Warn("Ignoring s3 config from restore override config in favor of user-specified backup location")
+		}
 
-	if cfg.Global.V1.Backups.GetS3() != nil {
-		// We can't use the config from the override config because it might
-		// be different than where you told us the backup lives. We need to
-		// configure automate to use the backup location where the backup lives
-		// in order to restore (mostly because of the way ES works)
-		logrus.Warn("Ignoring s3 config from restore override config in favor of user-specified backup location")
-	}
+		cfg.Global.V1.Backups.S3 = &config.Backups_S3{
+			Credentials: &config.Backups_S3_AWSCredentials{},
+			Bucket:      &config.Backups_S3_Bucket{},
+		}
 
-	cfg.Global.V1.Backups.S3 = &config.Backups_S3{
-		Credentials: &config.Backups_S3_AWSCredentials{},
-		Bucket:      &config.Backups_S3_Bucket{},
-	}
+		creds := cfg.GetGlobal().GetV1().GetBackups().GetS3().GetCredentials()
+		bucket := cfg.GetGlobal().GetV1().GetBackups().GetS3().GetBucket()
 
-	creds := cfg.GetGlobal().GetV1().GetBackups().GetS3().GetCredentials()
-	bucket := cfg.GetGlobal().GetV1().GetBackups().GetS3().GetBucket()
+		if bName := reqS3.GetBucketName(); bName != "" {
+			bucket.Name = w.String(bName)
+		}
 
-	if bName := reqS3.GetBucketName(); bName != "" {
-		bucket.Name = w.String(bName)
-	}
+		if endpoint := reqS3.GetEndpoint(); endpoint != "" {
+			bucket.Endpoint = w.String(endpoint)
+		}
 
-	if endpoint := reqS3.GetEndpoint(); endpoint != "" {
-		bucket.Endpoint = w.String(endpoint)
-	}
+		if basePath := reqS3.GetBasePath(); basePath != "" {
+			bucket.BasePath = w.String(basePath)
+		}
 
-	if basePath := reqS3.GetBasePath(); basePath != "" {
-		bucket.BasePath = w.String(basePath)
-	}
+		if accessKey := reqS3.GetAccessKey(); accessKey != "" {
+			creds.AccessKey = w.String(accessKey)
+		}
 
-	if accessKey := reqS3.GetAccessKey(); accessKey != "" {
-		creds.AccessKey = w.String(accessKey)
-	}
+		if secretKey := reqS3.GetSecretKey(); secretKey != "" {
+			creds.SecretKey = w.String(secretKey)
+		}
 
-	if secretKey := reqS3.GetSecretKey(); secretKey != "" {
-		creds.SecretKey = w.String(secretKey)
-	}
-
-	if sessionToken := reqS3.GetSessionToken(); sessionToken != "" {
-		creds.SessionToken = w.String(sessionToken)
+		if sessionToken := reqS3.GetSessionToken(); sessionToken != "" {
+			creds.SessionToken = w.String(sessionToken)
+		}
 	}
 
 	if patchConfig != nil {

api/interservice/deployment/override_config_test.go
@@ -107,7 +107,7 @@ func TestMergeAndValidateNewUserOverrideConfig(t *testing.T) {
 		require.Equal(t, expectedCfg, cfg)
 	})
 
-	t.Run("config patch", func(t *testing.T) {
+	t.Run("config patch 1", func(t *testing.T) {
 		cfg := newValidOverrideConfig()
 		rt := NewBackupRestoreTask()
 		rt.S3BackupLocation = &S3BackupLocation{
@@ -156,6 +156,29 @@ func TestMergeAndValidateNewUserOverrideConfig(t *testing.T) {
 				},
 			},
 		}
+		expectedCfg.Global.V1.Log = &config.Log{
+			Level: w.String("debug"),
+		}
+		err := MergeAndValidateNewUserOverrideConfig(cfg, rt)
+		require.NoError(t, err)
+		require.Equal(t, expectedCfg, cfg)
+	})
+
+	t.Run("config patch 2", func(t *testing.T) {
+		cfg := newValidOverrideConfig()
+		rt := NewBackupRestoreTask()
+
+		rt.PatchConfig = &dc.AutomateConfig{
+			Global: config.NewGlobalConfig(),
+		}
+		rt.PatchConfig.Global.V1.Log = &config.Log{
+			Level: w.String("debug"),
+		}
+
+		// Make sure the patch config overrides the default backup config
+		// from the restore task
+		expectedCfg := newValidOverrideConfig()
+
 		expectedCfg.Global.V1.Log = &config.Log{
 			Level: w.String("debug"),
 		}

integration/tests/ha_data_services_migrate.sh
@@ -65,3 +65,11 @@ do_upgrade() {
         "$test_backup_id"
 }
 
+do_test_upgrade() {
+    do_test_upgrade_default
+    # doing the patch again shouldn't change anything because
+    # the restore should have patched the config
+    log_info "Making sure patch config doesn't change anything"
+    chef-automate config patch /services/ha_backend.toml
+    do_test_upgrade_default
+}