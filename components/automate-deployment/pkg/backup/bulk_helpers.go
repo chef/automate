2ecee037590d6dd4f606abcb8378ed68b60c9ef8
Jay Mundrawala <jmundrawala@chef.io>
2020-01-27 10:07:24 -0600

    batch deletes
    Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

--- components/automate-deployment/pkg/backup/bulk_helpers.go
+++ components/automate-deployment/pkg/backup/bulk_helpers.go
@@ -114,8 +114,9 @@ func (m *simpleBulkDeleter) Delete(ctx context.Context, iterator BulkDeleteItera
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	batchIterator := newBulkDeleteBatcher(900, m.prefix, iterator)
 	for {
-		req, err := iterator.Next()
+		req, err := batchIterator.Next()
 		if err != nil {
 			if err == io.EOF {
 				break
@@ -130,8 +131,52 @@ func (m *simpleBulkDeleter) Delete(ctx context.Context, iterator BulkDeleteItera
 	return nil
 }
 
-func (m *simpleBulkDeleter) doDelete(ctx context.Context, keyToDelete string) error {
-	key := path.Join(m.prefix, keyToDelete)
-	logrus.Infof("Deleting Artifact %s", key)
-	return m.bkt.Delete(ctx, []string{key})
+func (m *simpleBulkDeleter) doDelete(ctx context.Context, keysToDelete []string) error {
+	for _, key := range keysToDelete {
+		logrus.Infof("Deleting Artifact %s", key)
+	}
+	return m.bkt.Delete(ctx, keysToDelete)
+}
+
+type bulkDeleteBatcher struct {
+	prefix   string
+	iterator BulkDeleteIterator
+	buffer   []string
+	err      error
+}
+
+func newBulkDeleteBatcher(batchSize int, prefix string, iterator BulkDeleteIterator) *bulkDeleteBatcher {
+	return &bulkDeleteBatcher{
+		iterator: iterator,
+		buffer:   make([]string, batchSize),
+	}
+}
+
+func (b *bulkDeleteBatcher) Next() ([]string, error) {
+	if b.err != nil {
+		return nil, b.err
+	}
+
+	items := b.buffer[:0]
+	i := 0
+	for len(items) < cap(items) {
+		item, err := b.iterator.Next()
+		if err != nil {
+			b.err = err
+			if len(items) > 0 {
+				return items, nil
+			}
+			return nil, err
+		}
+		if b.prefix != "" {
+			items = append(items, path.Join(b.prefix, item))
+		} else {
+			// minimize allocs if we don't need to prefix
+			items = append(items, item)
+
+		}
+		i++
+	}
+
+	return items, nil
 }
