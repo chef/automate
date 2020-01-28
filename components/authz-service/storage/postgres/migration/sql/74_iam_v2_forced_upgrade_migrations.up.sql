d1a4ddfbc9f1742b8e2a205cfd74c5965f1a871a
Blake Johnson <bstier@chef.io>
2020-01-28 14:11:03 -0800

    Move 09 Update project admin
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/authz-service/storage/postgres/migration/sql/74_iam_v2_forced_upgrade_migrations.up.sql
+++ components/authz-service/storage/postgres/migration/sql/74_iam_v2_forced_upgrade_migrations.up.sql
@@ -116,4 +116,31 @@ UPDATE iam_projects
     WHERE
         id = '(unassigned)';
 
+UPDATE iam_roles
+    SET
+        id = 'project-owner',
+        name = 'Project Owner',
+        actions = '{
+          infra:*,
+          compliance:*,
+          system:*,
+          event:*,
+          ingest:*,
+          secrets:*,
+          telemetry:*,
+          iam:projects:list,
+          iam:projects:get,
+          iam:projects:assign,
+          iam:policies:list,
+          iam:policies:get,
+          iam:policyMembers:*,
+          iam:teams:list,
+          iam:teams:get,
+          iam:teamUsers:*,
+          iam:users:get,
+          iam:users:list
+          }'
+    WHERE
+        id = 'project-admin';
+
 COMMMIT;
