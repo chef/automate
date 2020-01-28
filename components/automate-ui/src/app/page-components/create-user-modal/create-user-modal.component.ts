7ccdb8beb0e4888ea3393ac2e5b5a59bc4545b87
Brenna Hewer-Darroch <brenna@chef.io>
2020-01-28 11:38:07 -0800

    address review - updated comment - added comment explaining special case handling - updated required error to pop up for all-space values
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- components/automate-ui/src/app/page-components/create-user-modal/create-user-modal.component.ts
+++ components/automate-ui/src/app/page-components/create-user-modal/create-user-modal.component.ts
@@ -39,7 +39,7 @@ export class CreateUserModalComponent implements OnInit, OnDestroy {
     fb: FormBuilder
   ) {
     this.createUserForm = fb.group({
-      // Must stay in sync with error checks in user-form.component.html
+      // Must stay in sync with error checks in create-user-modal.component.html
       displayName: ['', [Validators.required, Validators.pattern(Regex.patterns.NON_BLANK)]],
       username: ['', [Validators.required, Validators.pattern(USERNAME_PATTERN)]],
       // length validator must be consistent with
@@ -126,6 +126,10 @@ export class CreateUserModalComponent implements OnInit, OnDestroy {
   }
 
   handlePasswordInput(event: KeyboardEvent): void {
+    // This handles the case where a user first enters the password and confirmPassword
+    // then goes back and changes the original password.
+    // Since the match validator is only activated by changes to confirmPassword,
+    // we have to manually revalidate confirmPassword here
     this.createUserForm.get('confirmPassword').updateValueAndValidity();
     if (!this.isNavigationKey(event)) {
       this.passwordError = false;
@@ -141,7 +145,7 @@ export class CreateUserModalComponent implements OnInit, OnDestroy {
       this.createUserForm.get(field).dirty);
   }
 
-  hasAttemptedInputWithError(field: string): boolean {
+  hasAttemptedInputWithRequiredError(field: string): boolean {
     return (this.createUserForm.get(field).hasError('required') &&
       (this.createUserForm.get(field).touched || this.createUserForm.get(field).dirty));
   }
