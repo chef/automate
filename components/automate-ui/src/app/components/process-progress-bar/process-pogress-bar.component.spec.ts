fd0da8ad8472bb9c35b2868722e96151e1197aff
Tara Black <tblack@chef.io>
2020-01-27 18:21:12 -0600

    Add spec files Signed-Off-By: Tara Black <tblack@chef.io>

---
+++ components/automate-ui/src/app/components/process-progress-bar/process-pogress-bar.component.spec.ts
@@ -0,0 +1,98 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule, Store } from '@ngrx/store';
+import { MockComponent } from 'ng2-mock-component';
+
+import { NgrxStateAtom, runtimeChecks } from 'app/ngrx.reducers';
+import { ChefPipesModule } from 'app/pipes/chef-pipes.module';
+import { customMatchers } from 'app/testing/custom-matchers';
+import { FeatureFlagsService } from 'app/services/feature-flags/feature-flags.service';
+import { notificationEntityReducer } from 'app/entities/notifications/notification.reducer';
+import { clientRunsEntityReducer } from 'app/entities/client-runs/client-runs.reducer';
+import { GetIamVersionSuccess } from 'app/entities/policies/policy.actions';
+import { policyEntityReducer } from 'app/entities/policies/policy.reducer';
+import { ProjectService } from 'app/entities/projects/project.service';
+import { ProcessProgressBarComponent } from './process-progress-bar.component';
+
+describe('PendingEditsBarComponent', () => {
+  let component: ProcessProgressBarComponent;
+  let fixture: ComponentFixture<ProcessProgressBarComponent>;
+  let store: Store<NgrxStateAtom>;
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [
+        MockComponent({
+          selector: 'chef-toolbar',
+          template: '<ng-content></ng-content>'
+        }),
+         MockComponent({
+          selector: 'app-authorized',
+          inputs: ['allOf', 'not'],
+          template: '<ng-content></ng-content>'
+        }),
+        MockComponent({
+          selector: 'app-message-modal',
+          inputs: ['visible' ],
+          outputs: ['close' ]
+        }),
+         MockComponent({
+          selector: 'app-confirm-apply-start-modal',
+          inputs: ['visible'],
+          outputs: ['confirm', 'cancel']
+        }),
+        MockComponent({
+          selector: 'app-confirm-apply-stop-modal',
+          inputs: ['visible', 'applyRulesStatus', 'stopRulesInProgress'],
+          outputs: ['confirm', 'cancel']
+        }),
+        MockComponent({ selector: 'mat-select' }),
+        MockComponent({ selector: 'mat-option' }),
+        MockComponent({ selector: 'chef-button', inputs: ['disabled'] }),
+        MockComponent({ selector: 'chef-heading' }),
+        MockComponent({ selector: 'chef-loading-spinner' }),
+        MockComponent({ selector: 'chef-page-header' }),
+        MockComponent({ selector: 'chef-subheading' }),
+        MockComponent({ selector: 'chef-table-new' }),
+        MockComponent({ selector: 'chef-table-header' }),
+        MockComponent({ selector: 'chef-table-body' }),
+        MockComponent({ selector: 'chef-table-row' }),
+        MockComponent({ selector: 'chef-table-header-cell' }),
+        MockComponent({ selector: 'chef-table-cell' }),
+        ProcessProgressBarComponent
+      ],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        ChefPipesModule,
+        StoreModule.forRoot(
+          {
+          policies: policyEntityReducer,
+          notifications: notificationEntityReducer, // not used here but needed to suppress warnings
+          clientRunsEntity: clientRunsEntityReducer // not used here but needed to suppress warnings
+        }, { runtimeChecks })
+      ],
+      providers: [
+        FeatureFlagsService,
+        ProjectService
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    jasmine.addMatchers(customMatchers);
+    fixture = TestBed.createComponent(ProcessProgressBarComponent);
+    component = fixture.componentInstance;
+    store = TestBed.get(Store);
+
+    store.dispatch(new GetIamVersionSuccess({ version: { major: 'v2' } }));
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
