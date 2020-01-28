a402c07f53a4e7d4c776775249031998bfd16d0a
Blake Johnson <bstier@chef.io>
2020-01-28 14:07:36 -0800

    Move 06 viewer actions
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/authz-service/storage/postgres/migration/sql/74_iam_v2_forced_upgrade_migrations.up.sql
+++ components/authz-service/storage/postgres/migration/sql/74_iam_v2_forced_upgrade_migrations.up.sql
@@ -77,4 +77,25 @@ INSERT INTO iam_statement_projects (statement_id, project_id)
 
 DELETE FROM iam_projects WHERE id='default';
 
+UPDATE iam_roles
+    SET
+        actions = '{
+            secrets:*:get,
+            secrets:*:list,
+            infra:*:get,
+            infra:*:list,
+            compliance:*:get,
+            compliance:*:list,
+            system:*:get,
+            system:*:list,
+            event:*:get,
+            event:*:list,
+            ingest:*:get,
+            ingest:*:list,
+            iam:projects:list,
+            iam:projects:get
+        }'
+    WHERE
+        id = 'viewer';
+
 COMMMIT;
