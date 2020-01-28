1b423dc03fe9bac8cd984ed840065571a68730ae
Blake Johnson <bstier@chef.io>
2020-01-28 14:06:26 -0800

    Move 04 projects updates
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/authz-service/storage/postgres/migration/sql/74_iam_v2_forced_upgrade_migrations.up.sql
+++ components/authz-service/storage/postgres/migration/sql/74_iam_v2_forced_upgrade_migrations.up.sql
@@ -61,4 +61,18 @@ UPDATE iam_roles
     WHERE
         id = 'all-actions';
 
+INSERT INTO iam_projects (id, name, type)
+    VALUES ('(unassigned)', 'Unassigned', 'chef-managed')
+    ON CONFLICT DO NOTHING;
+
+-- Define the 'all projects' project
+INSERT INTO iam_projects (id, name, type)
+    VALUES ('~~ALL-PROJECTS~~', 'All Projects', 'chef-managed')
+    ON CONFLICT DO NOTHING;
+
+-- For any statements lacking projects, provide the default of 'all projects'
+INSERT INTO iam_statement_projects (statement_id, project_id)
+    SELECT db_id, project_db_id('~~ALL-PROJECTS~~') FROM iam_statements
+    WHERE db_id NOT IN (SELECT statement_id FROM iam_statement_projects);
+
 COMMMIT;
