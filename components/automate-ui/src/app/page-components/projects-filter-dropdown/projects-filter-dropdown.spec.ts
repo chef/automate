7805ea178db1d671a2b069e806becfca8c6fd07f
seajamied <jdegnan@chef.io>
2020-01-30 22:18:26 -0800

    cleanup
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/page-components/projects-filter-dropdown/projects-filter-dropdown.spec.ts
+++ components/automate-ui/src/app/page-components/projects-filter-dropdown/projects-filter-dropdown.spec.ts
@@ -2,15 +2,9 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { StoreModule } from '@ngrx/store';
-// import { runtimeChecks } from 'app/ngrx.reducers';
 
 import { ProjectsFilterDropdownComponent } from './projects-filter-dropdown.component';
 import { ProjectsFilterService } from 'app/services/projects-filter/projects-filter.service';
-// import {
-//   projectsFilterInitialState,
-//   projectsFilterReducer
-// } from 'app/services/projects-filter/projects-filter.reducer';
-
 
 
 describe('ProjectsFilterDropdownComponent', () => {
