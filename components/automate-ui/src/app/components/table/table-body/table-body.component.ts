9cc861dcf14b292f31dc554c402bce09123b74cf
susanev <susan.ra.evans@gmail.com>
2020-02-02 08:06:03 -0800

    removing empty scss files
    Signed-off-by: susanev <susan.ra.evans@gmail.com>

--- components/automate-ui/src/app/components/table/table-body/table-body.component.ts
+++ components/automate-ui/src/app/components/table/table-body/table-body.component.ts
@@ -2,8 +2,7 @@ import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'chef-table-body',
-  templateUrl: './table-body.component.html',
-  styleUrls: ['./table-body.component.scss']
+  templateUrl: './table-body.component.html'
 })
 export class TableBodyComponent {
   @HostBinding('attr.role') role = 'rowgroup';
