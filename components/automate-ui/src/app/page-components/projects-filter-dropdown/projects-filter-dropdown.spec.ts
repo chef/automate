a71d3184940890ad9d9bb5c0134c0323273ff064
seajamied <jdegnan@chef.io>
2020-02-03 17:05:16 -0800

    Remove formModule in spec, no longer needed
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/page-components/projects-filter-dropdown/projects-filter-dropdown.spec.ts
+++ components/automate-ui/src/app/page-components/projects-filter-dropdown/projects-filter-dropdown.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ProjectsFilterDropdownComponent } from './projects-filter-dropdown.component';
 
@@ -12,9 +11,6 @@ describe('ProjectsFilterDropdownComponent', () => {
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       declarations: [
         ProjectsFilterDropdownComponent
-      ],
-      imports: [
-        FormsModule
       ]
     })
     .compileComponents();
