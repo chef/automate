2e28808aaea2156e22c05d3c26daa627d5317129
Lance Finfrock <lfinfrock@chef.io>
2020-02-07 15:51:42 -0800

    Adding run message validation to the ingest pipeline
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

---
+++ components/ingest-service/pipeline/processor/validator_run.go
@@ -0,0 +1,37 @@
+package processor
+
+import (
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/chef/automate/components/ingest-service/pipeline/message"
+)
+
+// MessageValidator - Validate the message before it moves further in the pipeline.
+func MessageValidator(in <-chan message.ChefRun) <-chan message.ChefRun {
+	out := make(chan message.ChefRun, 100)
+	go func() {
+		for msg := range in {
+			log.WithFields(log.Fields{
+				"message_id":  msg.ID,
+				"buffer_size": len(out),
+			}).Debug("MessageValidator ChefRun")
+
+			if msg.Run.EntityUuid == "" {
+				msg.FinishProcessing(status.Errorf(codes.InvalidArgument, "the entity_uuid is missing from the message"))
+				continue
+			}
+
+			if msg.Run.RunId == "" {
+				msg.FinishProcessing(status.Errorf(codes.InvalidArgument, "the run_id is missing from the message"))
+				continue
+			}
+
+			out <- msg
+		}
+		close(out)
+	}()
+
+	return out
+}
