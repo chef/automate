e403f12bb8abfae619d62c5a5cbc540ec9f08ed8
Brenna Hewer-Darroch <brenna@chef.io>
2020-02-11 14:57:55 -0800

    diagnostics: policies diagnostic can handle versions
        each step, generate, verify, and cleanup, confirms what IAM version
        Automate is using at that point in time.
        this allows the diagnostic to use the correct APIs
    
        also, dropped api.go since it was unclear what it was for
        based on the name
        and it was only used in auth_policies anyway
    
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- components/automate-cli/pkg/diagnostics/integration/auth_policies.go
+++ components/automate-cli/pkg/diagnostics/integration/auth_policies.go
@@ -1,6 +1,8 @@
 package integration
 
 import (
+	"fmt"
+
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/multierr"
@@ -9,10 +11,118 @@ import (
 	"github.com/chef/automate/components/automate-cli/pkg/diagnostics/lbrequest"
 )
 
+const createV1PolicyTemplateStr = `
+{
+	"action": "{{ .Action }}",
+	"subjects": ["token:{{ .TokenID }}"],
+	"resource": "{{ .Resource }}"
+}
+`
+
+const createV2PolicyTemplateStr = `
+{
+	"id": "{{ .ID }}",
+	"name": "{{ .ID }} test policy",
+	"members": ["token:{{ .TokenID }}"],
+	"statements": [
+		{
+			"effect": "ALLOW",
+			"actions": ["{{ .Action }}"],
+			"projects": ["*"]
+		}
+	]
+}
+`
+
+// PolicyInfo contains info about a created policy
+type PolicyInfo struct {
+	ID string `json:"id"`
+}
+
+// V2PolicyInfo represents the nested response from the v2 Policy API.
+type V2PolicyInfo struct {
+	Policy PolicyInfo
+}
+
+// CreatePolicyOnToken creates a policy on a given tokenID
+func CreatePolicyOnToken(tstCtx diagnostics.TestContext, tokenID string, action string, resource string) (*PolicyInfo, error) {
+	isV2, err := tstCtx.IsIAMV2()
+	if err != nil {
+		return nil, err
+	}
+
+	policyInfo := PolicyInfo{}
+	if isV2 {
+		v2PolicyInfo := V2PolicyInfo{Policy: policyInfo}
+		err = MustJSONDecodeSuccess(
+			tstCtx.DoLBRequest(
+				"/apis/iam/v2/policies",
+				lbrequest.WithMethod("POST"),
+				lbrequest.WithJSONStringTemplateBody(createV2PolicyTemplateStr, struct {
+					ID      string
+					Action  string
+					TokenID string
+				}{
+					ID:      TimestampName(),
+					Action:  action,
+					TokenID: tokenID,
+				}),
+			)).WithValue(&v2PolicyInfo)
+		policyInfo = v2PolicyInfo.Policy
+	} else {
+		err = MustJSONDecodeSuccess(
+			tstCtx.DoLBRequest(
+				"/api/v0/auth/policies",
+				lbrequest.WithMethod("POST"),
+				lbrequest.WithJSONStringTemplateBody(createV1PolicyTemplateStr, struct {
+					Action   string
+					TokenID  string
+					Resource string
+				}{
+					Action:   action,
+					TokenID:  tokenID,
+					Resource: resource,
+				}),
+			)).WithValue(&policyInfo)
+	}
+
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not create policy")
+	}
+
+	return &policyInfo, nil
+}
+
+// DeletePolicy deletes a policy with the given id
+func DeletePolicy(tstCtx diagnostics.TestContext, id string) error {
+	isV2, err := tstCtx.IsIAMV2()
+	if err != nil {
+		return err
+	}
+
+	var reqPath string
+	if isV2 {
+		reqPath = fmt.Sprintf("/apis/iam/v2/policies/%s", id)
+	} else {
+		reqPath = fmt.Sprintf("/api/v0/auth/policies/%s", id)
+	}
+
+	err = MustJSONDecodeSuccess(
+		tstCtx.DoLBRequest(
+			reqPath,
+			lbrequest.WithMethod("DELETE"),
+		)).Error()
+
+	if err != nil {
+		return errors.Wrap(err, "Could not delete policy")
+	}
+	return nil
+}
+
 type authPoliciesSave struct {
-	TokenID  string `json:"token_id"`
-	Token    string `json:"token"`
-	PolicyID string `json:"policy_id"`
+	TokenID    string `json:"token_id"`
+	TokenValue string `json:"token_value"`
+	PolicyID   string `json:"policy_id"`
 }
 
 // CreateAuthPoliciesDiagnostic create the diagnostic struct for auth tokens
@@ -20,22 +130,38 @@ type authPoliciesSave struct {
 func CreateAuthPoliciesDiagnostic() diagnostics.Diagnostic {
 	return diagnostics.Diagnostic{
 		Name: "auth-policies",
-		Tags: diagnostics.Tags{"auth", "iam-v1"},
+		Tags: diagnostics.Tags{"auth"},
 		Generate: func(tstCtx diagnostics.TestContext) error {
+			isV2, err := tstCtx.IsIAMV2()
+			if err != nil {
+				return err
+			}
+
 			tokenInfo, err := CreateRandomToken(tstCtx)
 			if err != nil {
 				return err
 			}
 
-			policyInfo, err := CreatePolicyOnToken(tstCtx, tokenInfo.ID, "read", "service_info:version")
+			var action string
+			var resource string
+			if isV2 {
+				action = "system:serviceVersion:get"
+				resource = "*"
+			} else {
+				action = "read"
+				resource = "service_info:version"
+			}
+			// this helper knows how to use the correct version of the policy API
+			// but we need to pass it the correctly versioned action and resource pair
+			policyInfo, err := CreatePolicyOnToken(tstCtx, tokenInfo.ID, action, resource)
 			if err != nil {
 				return err
 			}
 
 			tstCtx.SetValue("auth-policies", authPoliciesSave{
-				TokenID:  tokenInfo.ID,
-				PolicyID: policyInfo.ID,
-				Token:    tokenInfo.Token,
+				TokenID:    tokenInfo.ID,
+				PolicyID:   policyInfo.ID,
+				TokenValue: tokenInfo.Value,
 			})
 			return nil
 		},
@@ -46,7 +172,7 @@ func CreateAuthPoliciesDiagnostic() diagnostics.Diagnostic {
 			err = MustJSONDecodeSuccess(
 				tstCtx.DoLBRequest(
 					"/api/v0/gateway/version",
-					lbrequest.WithAuthToken(loaded.Token),
+					lbrequest.WithAuthToken(loaded.TokenValue),
 				)).Error()
 			require.NoError(tstCtx, err, "Expected to be able to read gateway version")
 		},
