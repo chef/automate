2482b6da4adc598fb82b4c5f54db8c06e88bbe2b
Sachin Bachhav <sbachhav@chef.io>
2020-02-25 14:20:23 +0530

    table alignment fix added
    Signed-off-by: Sachin Bachhav <sbachhav@chef.io>

--- components/automate-ui/src/app/modules/infra-proxy/chef-servers-list/chef-servers-list.component.ts
+++ components/automate-ui/src/app/modules/infra-proxy/chef-servers-list/chef-servers-list.component.ts
@@ -18,7 +18,8 @@ import { ChefSorters } from 'app/helpers/auth/sorter';
 
 @Component({
   selector: 'app-chef-servers-list',
-  templateUrl: './chef-servers-list.component.html'
+  templateUrl: './chef-servers-list.component.html',
+  styleUrls: ['./chef-servers-list.component.scss']
 })
 export class ChefServersListComponent implements OnInit, OnDestroy {
   public loading$: Observable<boolean>;
