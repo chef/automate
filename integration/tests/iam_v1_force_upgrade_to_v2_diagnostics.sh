1fd3a49919bcd0b7d5160d8e5796888c60fe6b47
Brenna Hewer-Darroch <brenna@chef.io>
2020-02-27 09:21:02 -0800

    integration: add v1_force_upgrade_v2 diagnostics script since we can't test both inspec and diagnostics in one script
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

---
+++ integration/tests/iam_v1_force_upgrade_to_v2_diagnostics.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#shellcheck disable=SC2034
+test_name="iam_v1_force_upgrade_to_v2_diagnostics"
+test_upgrades=true
+test_upgrade_strategy="none"
+
+# Note: we can't run diagnostics AND inspec, so skip diagnostics
+test_skip_diagnostics=false
+
+# on this version, Automate had upgrade-to-v2 and the first three v2 data migrations
+OLD_VERSION=20190501153509
+OLD_MANIFEST_DIR="${A2_ROOT_DIR}/components/automate-deployment/testdata/old_manifests/"
+DEEP_UPGRADE_PATH="${OLD_MANIFEST_DIR}/${OLD_VERSION}.json"
+
+
+do_deploy() {
+    #shellcheck disable=SC2154
+    cp "$DEEP_UPGRADE_PATH" "$test_manifest_path"
+
+    # we make sure to use the CLI for the old version of Automate we want to deploy
+    local cli_bin="/bin/chef-automate-${OLD_VERSION}"
+
+    download_cli "${OLD_VERSION}" "${cli_bin}"
+
+    #shellcheck disable=SC2154
+    "${cli_bin}" deploy "$test_config_path" \
+        --hartifacts "$test_hartifacts_path" \
+        --override-origin "$HAB_ORIGIN" \
+        --manifest-dir "$test_manifest_path" \
+        --admin-password chefautomate \
+        --accept-terms-and-mlsa \
+        --skip-preflight \
+        --debug
+}
