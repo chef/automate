06b0ef9300fdf919e17ff1480c43ef6f27dc275a
Brenna Hewer-Darroch <brenna@chef.io>
2020-03-02 15:38:16 -0800

    add force-upgrade v2 from v2 with legacy integration test
    we want to ensure that customers currently using IAM v2
    are not disrupted by the force-upgrade
    
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

---
+++ integration/tests/iam_v2_force_upgrade_to_v2_with_legacy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#shellcheck disable=SC2034
+test_name="iam_force_upgrade_to_v2_with_legacy"
+test_upgrades=true
+test_upgrade_strategy="none"
+test_deploy_inspec_profiles=(a2-iam-legacy-integration)
+test_upgrade_inspec_profiles=(a2-iam-legacy-integration a2-api-integration)
+# Note: we can't run diagnostics AND inspec, so skip diagnostics
+test_skip_diagnostics=true
+
+# on this version, Automate had upgrade-to-v2 and the first three v2 data migrations
+OLD_VERSION=20190501153509
+OLD_MANIFEST_DIR="${A2_ROOT_DIR}/components/automate-deployment/testdata/old_manifests/"
+DEEP_UPGRADE_PATH="${OLD_MANIFEST_DIR}/${OLD_VERSION}.json"
+
+do_deploy() {
+    #shellcheck disable=SC2154
+    cp "$DEEP_UPGRADE_PATH" "$test_manifest_path"
+
+    # we make sure to use the CLI for the old version of Automate we want to deploy
+    local cli_bin="/bin/chef-automate-${OLD_VERSION}"
+
+    download_cli "${OLD_VERSION}" "${cli_bin}"
+
+    #shellcheck disable=SC2154
+    "${cli_bin}" deploy "$test_config_path" \
+        --hartifacts "$test_hartifacts_path" \
+        --override-origin "$HAB_ORIGIN" \
+        --manifest-dir "$test_manifest_path" \
+        --admin-password chefautomate \
+        --accept-terms-and-mlsa \
+        --skip-preflight \
+        --debug
+
+    # one by-product of these tests is a custom v1 policy
+    # it should be:
+    # - migrated to v2 by the upgrade-to-v2 command below
+    # - preserved by the force-uprade
+    run_inspec_tests "${A2_ROOT_DIR}" "a2-iam-v1-integration"
+
+    # this migrates v1 legacy policies
+    "${cli_bin}" iam upgrade-to-v2 
+}
