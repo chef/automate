4ed5d2030da68769984e232940d27f631fdfcebd
Blake Johnson <bstier@chef.io>
2020-03-04 15:34:55 -0800

    Use ingest: in protos
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- components/automate-gateway/api/legacy/legacy.pb.policy-v2.go
+++ components/automate-gateway/api/legacy/legacy.pb.policy-v2.go
@@ -6,7 +6,7 @@ package legacy
 import policyv2 "github.com/chef/automate/components/automate-gateway/authz/policy_v2"
 
 func init() {
-	policyv2.MapMethodTo("/chef.automate.api.legacy.LegacyDataCollector/Status", "infra:status", "infra:ingest:get", "GET", "/events/data-collector", func(unexpandedResource string, input interface{}) string {
+	policyv2.MapMethodTo("/chef.automate.api.legacy.LegacyDataCollector/Status", "ingest:status", "ingest:status:get", "GET", "/events/data-collector", func(unexpandedResource string, input interface{}) string {
 		return unexpandedResource
 	})
 }
