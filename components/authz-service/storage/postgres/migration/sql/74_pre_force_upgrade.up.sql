958a13dfcf65a8ed14ab3e3128dc42d3dbc531ea
Brenna Hewer-Darroch <brenna@chef.io>
2020-03-05 10:54:09 -0800

    migration: any "{infra:ingest:*}" action is now "{ingest:*}"
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- components/authz-service/storage/postgres/migration/sql/74_pre_force_upgrade.up.sql
+++ components/authz-service/storage/postgres/migration/sql/74_pre_force_upgrade.up.sql
@@ -8,4 +8,8 @@ DROP FUNCTION migration_trigger_func();
 
 DROP FUNCTION valid_migration_state_change(migration_state, migration_state);
 
+-- we have updated the ingest endpoints so that their top-level namespace is `ingest`
+-- this line will update any custom or legacy policies using the old action name
+UPDATE iam_statements SET actions='{ingest:*}' WHERE actions='{infra:ingest:*}';
+
 COMMIT;
