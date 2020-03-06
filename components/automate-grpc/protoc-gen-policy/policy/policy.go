8c4def99912b59d4c663b2765e4b4416bb6398a6
michael sorens <msorens@chef.io>
2020-03-06 08:59:52 -0800

    Correct path

--- components/automate-grpc/protoc-gen-policy/policy/policy.go
+++ components/automate-grpc/protoc-gen-policy/policy/policy.go
@@ -47,7 +47,7 @@ type policyBundle struct {
 }
 
 var policyVersion = policyBundle{
-	pkg:       "github.com/chef/automate/components/automate-gateway/authz/policy",
+	pkg:       "github.com/chef/automate/components/automate-gateway/api/authz/policy",
 	extension: iam.E_Policy,
 	convert: func(x interface{}) *policyInfo {
 		if y, ok := x.(*iam.PolicyInfo); ok {
