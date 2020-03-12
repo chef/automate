b117744c0ed699382368afdf50b2eca87f5c1a76
Brenna Hewer-Darroch <brenna@chef.io>
2020-03-12 16:30:45 -0700

    merge all ingestion tests in one file project update takes a looong time now so we're trying to minimize the number of tests that need to run project update
    to this end, we combined the action and node project
    ingestion tests into one
    
    Signed-off-by: Brenna Hewer-Darroch <brenna@chef.io>

--- e2e/cypress/integration/api/iam/projects_api_ingestion.spec.ts
+++ e2e/cypress/integration/api/iam/projects_api_ingestion.spec.ts
@@ -1,21 +1,32 @@
 import { describeIfIAMV2p1 } from '../../../support/constants';
 import { uuidv4 } from '../../../support/helpers';
 
+const actionStart = Cypress.moment().utc().subtract(3, 'day').valueOf().toString();
+const actionEnd = Cypress.moment().utc().endOf('day').valueOf().toString();
+const nodeStart = Cypress.moment().utc().subtract(3, 'day').startOf('day').format();
+const nodeEnd = Cypress.moment().utc().endOf('day').format();
+const now = Cypress.moment().format('MMDDYYhhmm');
+
 describeIfIAMV2p1('Ingestion project tagging', () => {
-  const cypressPrefix = 'test-node-ingestion';
+  const cypressPrefix = 'test-ingestion-projects';
+  const complianceNodeId = uuidv4();
+  const clientRunsNodeId = uuidv4();
+  const reportId = uuidv4();
+  const nodeName = `${cypressPrefix}-${now}`;
+  const actionId = uuidv4();
+  const entityName = `ingest-action-${Cypress.moment().format('MMDDYYhhmmss')}`;
 
-
-  const projectsWithRule = [
+  const projectsWithNodeRules = [
     {
       project: {
-        id: `${cypressPrefix}-project-org-${Cypress.moment().format('MMDDYYhhmm')}`,
+        id: `${cypressPrefix}-project-org-${now}`,
         name: 'project org'
       },
       rule: {
         id: 'rule-org',
         name: 'rule CHEF_ORGANIZATION',
         type: 'NODE',
-        project_id: `${cypressPrefix}-project-org-${Cypress.moment().format('MMDDYYhhmm')}`,
+        project_id: `${cypressPrefix}-project-org-${now}`,
         conditions: [
           {
             attribute: 'CHEF_ORGANIZATION',
@@ -27,14 +38,14 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
     },
     {
       project: {
-        id: `${cypressPrefix}-project-chef-server-${Cypress.moment().format('MMDDYYhhmm')}`,
+        id: `${cypressPrefix}-project-chef-server-${now}`,
         name: 'project chef server'
       },
       rule: {
         id: 'rule-chef-server',
         name: 'rule CHEF_SERVER',
         type: 'NODE',
-        project_id: `${cypressPrefix}-project-chef-server-${Cypress.moment().format('MMDDYYhhmm')}`,
+        project_id: `${cypressPrefix}-project-chef-server-${now}`,
         conditions: [
           {
             attribute: 'CHEF_SERVER',
@@ -46,14 +57,14 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
     },
     {
       project: {
-        id: `${cypressPrefix}-project-environment-${Cypress.moment().format('MMDDYYhhmm')}`,
+        id: `${cypressPrefix}-project-environment-${now}`,
         name: 'project environment'
       },
       rule: {
         id: 'rule-environment',
         name: 'rule ENVIRONMENT',
         type: 'NODE',
-        project_id: `${cypressPrefix}-project-environment-${Cypress.moment().format('MMDDYYhhmm')}`,
+        project_id: `${cypressPrefix}-project-environment-${now}`,
         conditions: [
           {
             attribute: 'ENVIRONMENT',
@@ -65,7 +76,7 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
     },
     {
       project: {
-        id: `${cypressPrefix}-project-policy-group-${Cypress.moment().format('MMDDYYhhmm')}`,
+        id: `${cypressPrefix}-project-policy-group-${now}`,
         name: 'project policy group'
       },
       rule: {
@@ -73,7 +84,7 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
         name: 'rule CHEF_POLICY_GROUP',
         type: 'NODE',
         project_id:
-        `${cypressPrefix}-project-policy-group-${Cypress.moment().format('MMDDYYhhmm')}`,
+        `${cypressPrefix}-project-policy-group-${now}`,
         conditions: [
           {
             attribute: 'CHEF_POLICY_GROUP',
@@ -85,14 +96,14 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
     },
     {
       project: {
-        id: `${cypressPrefix}-project-policy-name-${Cypress.moment().format('MMDDYYhhmm')}`,
+        id: `${cypressPrefix}-project-policy-name-${now}`,
         name: 'project policy name'
       },
       rule: {
         id: 'rule-policy-name',
         name: 'rule CHEF_POLICY_NAME',
         type: 'NODE',
-        project_id: `${cypressPrefix}-project-policy-name-${Cypress.moment().format('MMDDYYhhmm')}`,
+        project_id: `${cypressPrefix}-project-policy-name-${now}`,
         conditions: [
           {
             attribute: 'CHEF_POLICY_NAME',
@@ -104,14 +115,14 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
     },
     {
       project: {
-        id: `${cypressPrefix}-project-role-${Cypress.moment().format('MMDDYYhhmm')}`,
+        id: `${cypressPrefix}-project-role-${now}`,
         name: 'project role'
       },
       rule: {
         id: 'rule-role',
         name: 'rule CHEF_ROLE',
         type: 'NODE',
-        project_id: `${cypressPrefix}-project-role-${Cypress.moment().format('MMDDYYhhmm')}`,
+        project_id: `${cypressPrefix}-project-role-${now}`,
         conditions: [
           {
             attribute: 'CHEF_ROLE',
@@ -123,14 +134,14 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
     },
     {
       project: {
-        id: `${cypressPrefix}-project-tag-${Cypress.moment().format('MMDDYYhhmm')}`,
+        id: `${cypressPrefix}-project-tag-${now}`,
         name: 'project tag'
       },
       rule: {
         id: 'rule-tag',
         name: 'rule CHEF_TAG',
         type: 'NODE',
-        project_id: `${cypressPrefix}-project-tag-${Cypress.moment().format('MMDDYYhhmm')}`,
+        project_id: `${cypressPrefix}-project-tag-${now}`,
         conditions: [
           {
             attribute: 'CHEF_TAG',
@@ -141,30 +152,82 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
       }
     }
   ];
-  const complianceNodeId = uuidv4();
-  const clientRunsNodeId = uuidv4();
-  const reportId = uuidv4();
-  const nodeName = `${cypressPrefix}-${Cypress.moment().format('MMDDYYhhmm')}`;
-  const start = Cypress.moment().utc().subtract(3, 'day').startOf('day').format();
-  const end = Cypress.moment().utc().endOf('day').format();
+
+  const projectsWithEventRules = [
+    {
+      project: {
+        id: `${cypressPrefix}-event-project-org-${now}`,
+        name: 'event project org'
+      },
+      rule: {
+        id: 'event-rule-org',
+        name: 'Chef Organization',
+        type: 'EVENT',
+        project_id: `${cypressPrefix}-event-project-org-${now}`,
+        conditions: [
+          {
+            attribute: 'CHEF_ORGANIZATION',
+            operator: 'EQUALS',
+            values: ['Team Pizza']
+          }
+        ]
+      }
+    },
+    {
+      project: {
+        id: `${cypressPrefix}-event-project-chef-server-${now}`,
+        name: 'event project chef server'
+      },
+      rule: {
+        id: 'event-rule-chef-server',
+        name: 'Chef Server',
+        type: 'EVENT',
+        project_id: `${cypressPrefix}-event-project-chef-server-${now}`,
+        conditions: [
+          {
+            attribute: 'CHEF_SERVER',
+            operator: 'EQUALS',
+            values: ['example.pizza']
+          }
+        ]
+      }
+    }
+  ];
 
   before(() => {
     cy.cleanupV2IAMObjectsByIDPrefixes(cypressPrefix, ['projects', 'policies']);
 
-    // create the projects with one rule
-    projectsWithRule.forEach(projectWithRule => {
+    // create the projects with one node rule each
+    projectsWithNodeRules.forEach(project => {
       cy.request({
         headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
         method: 'POST',
         url: '/apis/iam/v2/projects',
-        body: projectWithRule.project
+        body: project.project
       });
 
       cy.request({
         headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
         method: 'POST',
-        url: `/apis/iam/v2/projects/${projectWithRule.rule.project_id}/rules`,
-        body: projectWithRule.rule
+        url: `/apis/iam/v2/projects/${project.rule.project_id}/rules`,
+        body: project.rule
+      });
+    });
+
+    // create the projects with one event rule each
+    projectsWithEventRules.forEach(project => {
+      cy.request({
+        headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+        method: 'POST',
+        url: '/apis/iam/v2/projects',
+        body: project.project
+      });
+
+      cy.request({
+        headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+        method: 'POST',
+        url: `/apis/iam/v2/projects/${project.rule.project_id}/rules`,
+        body: project.rule
       });
     });
 
@@ -209,12 +272,32 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
       });
     });
 
+    // Ingest an action with attributes that match all the projects
+    cy.fixture('action/environment_create.json').then((action) => {
+      action.organization_name = 'Team Pizza';
+      action.remote_hostname = 'example.pizza';
+      action.id = actionId;
+      action.entity_name = entityName;
+      action.recorded_at = Cypress.moment().utc().subtract(1, 'day').format();
+
+      cy.request({
+        headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+        method: 'POST',
+        url: '/data-collector/v0',
+        body: action
+      });
+    });
+
     // wait for the report to be ingested
     cy.waitForNodemanagerNode(complianceNodeId, 30);
-    cy.waitForComplianceNode(complianceNodeId, start, end, 30);
+    cy.waitForComplianceNode(complianceNodeId, nodeStart, nodeEnd, 30);
+
     // wait for the client run report to be ingested
     cy.waitForNodemanagerNode(clientRunsNodeId, 30);
     cy.waitForClientRunsNode(clientRunsNodeId, 30);
+
+    // wait for the action to be ingested
+    waitForAction(entityName, 30);
   });
 
   after(() => {
@@ -235,7 +318,7 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
 
   describe('Node Manager', () => {
 
-    for ( const projectWithRule of projectsWithRule ) {
+    for (const projectWithRule of projectsWithNodeRules ) {
       it(`when a project has a rule that matches a node's ${projectWithRule.rule.name},
       successfully associates that node with the project`, () => {
         // Ensure that both the compliance and client run nodes' tags are updated
@@ -264,7 +347,7 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
   });
 
   describe('Client Runs', () => {
-    for ( const projectWithRule of projectsWithRule ) {
+    for (const projectWithRule of projectsWithNodeRules ) {
       it(`when a project has a rule that matches a node's ${projectWithRule.rule.name},
       successfully associates that node with the project`, () => {
 
@@ -286,7 +369,7 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
   });
 
   describe('Compliance Runs', () => {
-    for ( const projectWithRule of projectsWithRule ) {
+    for (const projectWithRule of projectsWithNodeRules ) {
       it(`when a project has a rule that matches a node's ${projectWithRule.rule.name},
       successfully associates that node with the project`, () => {
 
@@ -301,8 +384,8 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
           url: '/api/v0/compliance/reporting/nodes/search',
           body: {
             filters: [
-              { type: 'start_time', values: [start]},
-              { type: 'end_time', values: [end]},
+              { type: 'start_time', values: [nodeStart]},
+              { type: 'end_time', values: [nodeEnd]},
               { type: 'node_id', values: [complianceNodeId]}
             ],
             order: 'DESC',
@@ -316,4 +399,60 @@ describeIfIAMV2p1('Ingestion project tagging', () => {
       });
     }
   });
+
+  describe('Action', () => {
+    projectsWithEventRules.forEach((projectWithRule) => {
+      const attribute = projectWithRule.rule.name;
+      it(`when a project has a rule that matches an action's ${attribute},
+        successfully associates that action with the project`, () => {
+
+        // Ensure the action is tagged with the correct project
+        cy.request({
+          headers: {
+            'api-token': Cypress.env('ADMIN_TOKEN'),
+            projects: projectWithRule.project.id
+          },
+          method: 'GET',
+          url: `api/v0/eventfeed?collapse=false&page_size=100&start=${actionStart}&end=${actionEnd}`
+        }).then((response) => {
+          expect(response.body.events.length).to.be.greaterThan(0);
+          expect(eventExist(entityName, response.body.events)).to.equal(true);
+        });
+      });
+    });
+  });
 });
+
+function waitForAction(entityName: string, maxRetries: number) {
+  cy.request({
+    headers: {
+      projects: ['*'],
+      'api-token': Cypress.env('ADMIN_TOKEN')
+    },
+    method: 'GET',
+    url: `api/v0/eventfeed?collapse=false&page_size=100&start=${actionStart}&end=${actionEnd}`
+  })
+    .then((resp: Cypress.ObjectLike) => {
+      // to avoid getting stuck in an infinite loop
+      if (maxRetries === 0) {
+        expect(0).to.equal(1);
+        return;
+      }
+      if (resp.body.events && resp.body.events.length > 0 &&
+        eventExist(entityName, resp.body.events)) {
+        return;
+      }
+      cy.wait(1000);
+      waitForAction(entityName, maxRetries - 1);
+    });
+}
+
+function eventExist(entityName: string, events: any[]): boolean {
+  for (const event of events) {
+    if (event.entity_name === entityName) {
+      return true;
+    }
+  }
+
+  return false;
+}
