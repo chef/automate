7bf6da28c5c4938245a8f9cfb6fe28393a59fb73
IanMadd <imaddaus@chef.io>
2020-03-17 11:23:45 -0700

    Run automate site on old Hugo theme
    Signed-off-by: IanMadd <imaddaus@chef.io>

--- components/automate-chef-io/scripts/clone_hugo.sh
+++ components/automate-chef-io/scripts/clone_hugo.sh
@@ -5,4 +5,4 @@ if [[ -n "$GITHUB_TOKEN" ]];then
     clone_url="https://x-access-token:${GITHUB_TOKEN}@github.com/chef/chef-hugo-theme.git"
 fi
 
-git clone "$clone_url" themes/chef
+git clone -b old_hugo_theme "$clone_url" themes/chef
