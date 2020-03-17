3310af299bec157b7c53c4afa02fa37764572271
victoria jeffrey <vjeffrey@chef.io>
2020-03-17 15:52:35 -0600

    api docs: secrets service (#3113)
    * document the secrets service (proto)
    
    Signed-off-by: Victoria Jeffrey <vjeffrey@chef.io>
    
    * other changes that appeared after compiling protos
    
    Signed-off-by: Victoria Jeffrey <vjeffrey@chef.io>

--- components/automate-gateway/api/secrets.pb.swagger.go
+++ components/automate-gateway/api/secrets.pb.swagger.go
@@ -16,6 +16,8 @@ func init() {
   "paths": {
     "/secrets": {
       "post": {
+        "summary": "Create a secret",
+        "description": "Creates a secret. Requires values for name, type, and data.\n\nSupported types: ssh, winrm, sudo, aws, azure, gcp, service_now\nSupported keys by type: \nssh: username, password, key\nwinrm: username, password\nsudo: username, password\nservice_now: username, password\naws: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN\nazure: AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_TENANT_ID\ngcp: GOOGLE_CREDENTIALS_JSON\n\nExample:\n` + "`" + `` + "`" + `` + "`" + `\n{\n\"name\": \"my ssh secret\",\n\"type\": \"ssh\",\n\"data\": [\n{ \"key\": \"username\", \"value\": \"vagrant\" },\n{ \"key\": \"password\", \"value\": \"vagrant\"} \n]\n}\n` + "`" + `` + "`" + `` + "`" + `\n\nAuthorization Action:\n` + "`" + `` + "`" + `` + "`" + `\nsecrets:secrets:create\n` + "`" + `` + "`" + `` + "`" + `",
         "operationId": "Create",
         "responses": {
           "200": {
@@ -42,6 +44,8 @@ func init() {
     },
     "/secrets/id/{id}": {
       "get": {
+        "summary": "Read a secret",
+        "description": "Reads a secret given the ID of the secret.\nNote that the secret information (password and key values) will not be returned by the API, as a safety measure.\n\nAuthorization Action:\n` + "`" + `` + "`" + `` + "`" + `\nsecrets:secrets:get\n` + "`" + `` + "`" + `` + "`" + `",
         "operationId": "Read",
         "responses": {
           "200": {
@@ -54,6 +58,7 @@ func init() {
         "parameters": [
           {
             "name": "id",
+            "description": "Unique node ID (UUID).",
             "in": "path",
             "required": true,
             "type": "string"
@@ -64,6 +69,8 @@ func init() {
         ]
       },
       "delete": {
+        "summary": "Delete a secret",
+        "description": "Deletes a secret given the ID of the secret.\nNote that any nodes that were using the secret will no longer be associated with the deleted secret.\n\nAuthorization Action:\n` + "`" + `` + "`" + `` + "`" + `\nsecrets:secrets:delete\n` + "`" + `` + "`" + `` + "`" + `",
         "operationId": "Delete",
         "responses": {
           "200": {
@@ -76,6 +83,7 @@ func init() {
         "parameters": [
           {
             "name": "id",
+            "description": "Unique node ID (UUID).",
             "in": "path",
             "required": true,
             "type": "string"
@@ -86,6 +94,8 @@ func init() {
         ]
       },
       "patch": {
+        "summary": "Update a secret",
+        "description": "Updates a secret. \nThis is a PATCH operation, meaning the details sent in will override/replace those stored in the DB.\nSecret information that is not in the body of the request will persist.\n\nExample:\n` + "`" + `` + "`" + `` + "`" + `\ngiven a credential with a username and password, a user could update the password by passing in the following body, \nand the name of the secret as well as the username for the secret be unchanged:\n\n{\n\"id\": \"525c013a-2ab3-4e6f-9005-51bc620e9157\",\n\"data\": [\n{ \"key\": \"password\", \"value\": \"new-value\"} \n]\n}\n` + "`" + `` + "`" + `` + "`" + `\n\nAuthorization Action:\n` + "`" + `` + "`" + `` + "`" + `\nsecrets:secrets:update\n` + "`" + `` + "`" + `` + "`" + `",
         "operationId": "Update",
         "responses": {
           "200": {
@@ -98,6 +108,7 @@ func init() {
         "parameters": [
           {
             "name": "id",
+            "description": "Unique node ID (UUID).",
             "in": "path",
             "required": true,
             "type": "string"
@@ -118,6 +129,8 @@ func init() {
     },
     "/secrets/search": {
       "post": {
+        "summary": "List and filter secrets",
+        "description": "Makes a list of secrets.\nSupports filtering, pagination, and sorting.\nAdding a filter narrows the list of secrets to only those that match the filter or filters.\nSupported filters: type\nSupported sort types: name, type, last modified\n\nExample:\n` + "`" + `` + "`" + `` + "`" + `\n{\n\"sort\": \"type\",\n\"order\": \"ASC\",\n\"filters\": [\n{ \"key\": \"type\", \"values\": [\"ssh\",\"winrm\",\"sudo\"] }\n],\n\"page\":1,\n\"per_page\":100\n}\n` + "`" + `` + "`" + `` + "`" + `\n\nAuthorization Action:\n` + "`" + `` + "`" + `` + "`" + `\nsecrets:secrets:list\n` + "`" + `` + "`" + `` + "`" + `",
         "operationId": "List",
         "responses": {
           "200": {
@@ -185,7 +198,8 @@ func init() {
       "type": "object",
       "properties": {
         "id": {
-          "type": "string"
+          "type": "string",
+          "description": "Unique node ID (UUID)."
         }
       }
     },
@@ -196,21 +210,25 @@ func init() {
           "type": "array",
           "items": {
             "$ref": "#/definitions/chef.automate.api.common.query.Filter"
-          }
+          },
+          "description": "Use filters to limit the set of secrets."
         },
         "order": {
           "$ref": "#/definitions/chef.automate.api.secrets.Query.OrderType"
         },
         "sort": {
-          "type": "string"
+          "type": "string",
+          "description": "Sort the results on a specific field."
         },
         "page": {
           "type": "integer",
-          "format": "int32"
+          "format": "int32",
+          "description": "The number of result pages to return."
         },
         "per_page": {
           "type": "integer",
-          "format": "int32"
+          "format": "int32",
+          "description": "The number of results on each page."
         }
       }
     },
@@ -220,35 +238,42 @@ func init() {
         "ASC",
         "DESC"
       ],
-      "default": "ASC"
+      "default": "ASC",
+      "description": "Return the results in ascending or descending order."
     },
     "chef.automate.api.secrets.Secret": {
       "type": "object",
       "properties": {
         "id": {
-          "type": "string"
+          "type": "string",
+          "description": "Unique node ID (UUID)."
         },
         "name": {
-          "type": "string"
+          "type": "string",
+          "description": "User-specified name for the secret."
         },
         "type": {
-          "type": "string"
+          "type": "string",
+          "title": "Type of credential: ssh, winrm, sudo, aws, azure, gcp, service_now"
         },
         "last_modified": {
           "type": "string",
-          "format": "date-time"
+          "format": "date-time",
+          "description": "Timestamp denoting when the secret was last modified."
         },
         "tags": {
           "type": "array",
           "items": {
             "$ref": "#/definitions/chef.automate.api.common.query.Kv"
-          }
+          },
+          "description": "Tags to associate with the secret."
         },
         "data": {
           "type": "array",
           "items": {
             "$ref": "#/definitions/chef.automate.api.common.query.Kv"
-          }
+          },
+          "description": "Secret data, where the kv structs for the credential data live."
         }
       }
     },
@@ -259,11 +284,13 @@ func init() {
           "type": "array",
           "items": {
             "$ref": "#/definitions/chef.automate.api.secrets.Secret"
-          }
+          },
+          "description": "List of secrets."
         },
         "total": {
           "type": "integer",
-          "format": "int32"
+          "format": "int32",
+          "title": "Total count of secrets"
         }
       }
     },
