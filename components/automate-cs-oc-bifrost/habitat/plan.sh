bd843b159cba49afcfdc465e84586f3a35354c16
Ryan Cragun <ryan@chef.io>
2020-03-17 16:42:09 -0600

    bump to 13.1.13
    Signed-off-by: Ryan Cragun <ryan@chef.io>

--- components/automate-cs-oc-bifrost/habitat/plan.sh
+++ components/automate-cs-oc-bifrost/habitat/plan.sh
@@ -6,7 +6,7 @@ pkg_name="automate-cs-oc-bifrost"
 pkg_description="Wrapper package for chef/oc_bifrost"
 pkg_origin="chef"
 # WARNING: Version managed by .expeditor/update_chef_server.sh
-pkg_version="13.0.60"
+pkg_version="13.1.13"
 vendor_origin="chef"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
@@ -15,7 +15,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/oc_bifrost/13.0.60/20191025193934"
+  "${vendor_origin}/oc_bifrost/13.1.13/20191125202739"
 )
 
 pkg_binds=(
--- components/automate-cs-oc-erchef/habitat/plan.sh
+++ components/automate-cs-oc-erchef/habitat/plan.sh
@@ -6,7 +6,7 @@ pkg_name="automate-cs-oc-erchef"
 pkg_description="Wrapper package for chef/oc_erchef"
 pkg_origin="chef"
 # WARNING: Version managed by .expeditor/update_chef_server.sh
-pkg_version="13.0.60"
+pkg_version="13.1.13"
 vendor_origin="chef"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
@@ -16,7 +16,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/oc_erchef/13.0.60/20191025193934"
+  "${vendor_origin}/oc_erchef/13.1.13/20191125202739"
 )
 
 pkg_build_deps=(
