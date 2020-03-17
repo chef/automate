9c2fb6fc09e91a04801989d57e52698bda5005ee
vinay033 <vsharma@chef.io>
2020-03-17 20:44:16 +0530

    added changes for the get role entities
    Signed-off-by: vinay033 <vsharma@chef.io>

--- components/automate-ui/src/app/modules/infra-proxy/infra-role-details/infra-role-details.component.ts
+++ components/automate-ui/src/app/modules/infra-proxy/infra-role-details/infra-role-details.component.ts
@@ -8,19 +8,12 @@ import { NgrxStateAtom } from 'app/ngrx.reducers';
 import { LayoutFacadeService, Sidebar } from 'app/entities/layout/layout.facade';
 import { routeParams, routeURL } from 'app/route.selectors';
 import { filter, pluck, takeUntil } from 'rxjs/operators';
-import { EntityStatus, allLoaded } from 'app/entities/entities';
+import { EntityStatus } from 'app/entities/entities';
 import { identity, isNil } from 'lodash/fp';
 
-import {
-  allInfraRole, getStatusItem as getAllRoleItemForOrgStatus,
-      roleFromRoute, ruleFromRoute, getStatus
-    } from 'app/entities/infra-roles/infra-role.selectors';
-import { GetOrg } from 'app/entities/orgs/org.actions';
-import { GetRolesForOrg, GetRole } from 'app/entities/infra-roles/infra-role.action';
-// import { roleFromRoute } from 'app/entities/roles/role.selectors';
+import { getStatus, infaRoleFromRoute } from 'app/entities/infra-roles/infra-role.selectors';
+import { GetRole } from 'app/entities/infra-roles/infra-role.action';
 import { InfraRole } from 'app/entities/infra-roles/infra-role.model';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Org } from 'app/entities/orgs/org.model';
 
 export type InfraRoleTabName = 'runList' | 'attributes' | 'affectedNodes';
 
@@ -45,15 +38,10 @@ export interface Task {
 })
 export class InfraRoleDetailsComponent implements OnInit {
   public role: InfraRole;
-  public roles: InfraRole[] = [];
   public tabValue: InfraRoleTabName = 'runList';
   public url: string;
   public conflictErrorEvent = new EventEmitter<boolean>();
   public modalType: string;
-  // private id: string;
-  //private id: string;
-  public updateServerForm: FormGroup;
-  public org: Org;
   public serverId;
   public OrgId;
   public name;
@@ -131,7 +119,6 @@ export class InfraRoleDetailsComponent implements OnInit {
 
 
   constructor(
-    private fb: FormBuilder,
     private store: Store<NgrxStateAtom>,
     private router: Router,
     private layoutFacade: LayoutFacadeService
@@ -141,9 +128,6 @@ export class InfraRoleDetailsComponent implements OnInit {
   ngOnInit() {
     this.layoutFacade.showSidebar(Sidebar.Infrastructure);
     // Populate our tabValue from the fragment.
-    this.updateServerForm = this.fb.group({
-      name: [''],
-    });
 
     this.store.select(routeURL).pipe()
     .subscribe((url: string) => {
@@ -163,10 +147,6 @@ export class InfraRoleDetailsComponent implements OnInit {
       this.serverId = server_id;
       this.OrgId = org_id;
       this.name = name;
-      this.store.dispatch(new GetOrg({ server_id: server_id, id: org_id }));
-      this.store.dispatch(new GetRolesForOrg({
-        server_id: server_id, org_id: org_id
-      }));
       this.store.dispatch(new GetRole({
         server_id: server_id, org_id: org_id, name: name
       }));
@@ -174,38 +154,14 @@ export class InfraRoleDetailsComponent implements OnInit {
     
     combineLatest([
       this.store.select(getStatus),
-      this.store.select(getAllRoleItemForOrgStatus)
+      this.store.select(infaRoleFromRoute)
     ]).pipe(
-      takeUntil(this.isDestroyed)
-    ).subscribe(([getOrgSt, getRolesSt]) => {
-      this.isLoading =
-        !allLoaded([getOrgSt, getRolesSt]);
-    });
+      filter(([status, role]) => status === EntityStatus.loadingSuccess && !isNil(role)),
+      takeUntil(this.isDestroyed))
+      .subscribe(([_, role]) => {
+        this.role = { ...role };
+      });
 
-    combineLatest([
-      this.store.select(getStatus),
-      this.store.select(getAllRoleItemForOrgStatus),
-      this.store.select(roleFromRoute),
-        this.store.select(ruleFromRoute),
-      this.store.select(allInfraRole)
-    ]).pipe(
-      filter(([getOrgSt, getRolesSt, _roleState, _itemState, _allInfraRolesState]) =>
-      getOrgSt === EntityStatus.loadingSuccess &&
-      getRolesSt === EntityStatus.loadingSuccess &&
-        !isNil(_roleState) && !isNil(_itemState) &&
-        !isNil(_allInfraRolesState)),
-      takeUntil(this.isDestroyed)
-    ).subscribe(([_getOrgSt, _getRolesSt, roleState, itemState,
-       allInfraRolesState]) => {
-      // this.org = { ...orgState };
-      this.role = { ...roleState };
-      this.roles = allInfraRolesState;
-      console.log(itemState);
-      console.log(this.role);
-      console.log(allInfraRolesState);
-
-      //this.updateServerForm.controls['name'].setValue(this.role.name);
-    });
 
   }
     logNode(node: Node<Report>) {
