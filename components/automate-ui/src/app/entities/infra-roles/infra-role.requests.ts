24810c2ca1aac58ea6c621c56263aa73c1b1fa78
vinay033 <vsharma@chef.io>
2020-03-19 19:36:20 +0530

    fixed for roles duplicate issue + optimize some code part
    Signed-off-by: vinay033 <vsharma@chef.io>

--- components/automate-ui/src/app/entities/infra-roles/infra-role.requests.ts
+++ components/automate-ui/src/app/entities/infra-roles/infra-role.requests.ts
@@ -16,7 +16,6 @@ export class InfraRoleRequests {
       `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/roles`);
   }
 
-
   public getRole(server_id: string, org_id: string, name: string): Observable<InfraRole> {
     return this.http.get<InfraRole>(
         `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/roles/${name}`);
