c8b895654ff09e2fb114244f44945f690e02bd32
seajamied <jdegnan@chef.io>
2020-03-19 18:12:53 -0700

    Revert regex
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/helpers/auth/regex.ts
+++ components/automate-ui/src/app/helpers/auth/regex.ts
@@ -25,10 +25,7 @@ export class Regex {
     // Allows valid FQDN only
     VALID_FQDN: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-zA-Z0-9_]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
     // Allows valid IP Address only (ipv4)
-    VALID_IP_ADDRESS: '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$',
-
-    // Positive Integers Only including zero
-    POSITIVE_INT: '^[0-9][0-9]*$'
+    VALID_IP_ADDRESS: '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
   };
 
 }
