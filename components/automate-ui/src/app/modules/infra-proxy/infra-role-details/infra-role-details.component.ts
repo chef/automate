ea063d5b080bd74415b0be113f4fb84462d1e805
vinay033 <vsharma@chef.io>
2020-03-24 04:25:44 +0530

    added changes for load details data + minor code changes
    Signed-off-by: vinay033 <vsharma@chef.io>

--- components/automate-ui/src/app/modules/infra-proxy/infra-role-details/infra-role-details.component.ts
+++ components/automate-ui/src/app/modules/infra-proxy/infra-role-details/infra-role-details.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { combineLatest } from 'rxjs';
+import { combineLatest, Subject } from 'rxjs';
 import { NgrxStateAtom } from 'app/ngrx.reducers';
 import { LayoutFacadeService, Sidebar } from 'app/entities/layout/layout.facade';
 import { routeParams, routeURL } from 'app/route.selectors';
-import { filter, pluck } from 'rxjs/operators';
-import { EntityStatus } from 'app/entities/entities';
-import { identity, isNil } from 'lodash/fp';
-import { getStatus, infaRoleFromRoute } from 'app/entities/infra-roles/infra-role.selectors';
+import { filter, pluck, takeUntil } from 'rxjs/operators';
+import { identity } from 'lodash/fp';
+import { infaRoleFromRoute } from 'app/entities/infra-roles/infra-role.selectors';
 import { GetRole } from 'app/entities/infra-roles/infra-role.action';
 import { InfraRole } from 'app/entities/infra-roles/infra-role.model';
 
@@ -21,7 +20,7 @@ export type InfraRoleTabName = 'runList';
   styleUrls: ['./infra-role-details.component.scss']
 })
 
-export class InfraRoleDetailsComponent implements OnInit {
+export class InfraRoleDetailsComponent implements OnInit, OnDestroy {
   public role: InfraRole;
   public tabValue: InfraRoleTabName = 'runList';
   public url: string;
@@ -30,18 +29,20 @@ export class InfraRoleDetailsComponent implements OnInit {
   public serverId;
   public OrgId;
   public name;
-  // isLoading represents the initial load as well as subsequent updates in progress.
-  public isLoading = true;
   public runList: string[];
   public show = false;
   public data: any = [];
+
+  private isDestroyed = new Subject<boolean>();
+
   roleDetailsLoading = true;
 
   configs: any = {
     id_field: 'id',
     parent_id_field: 'parent',
     parent_display_field: 'name',
-    css: { // Optional
+    css: {
+      // Optional
       expand_class: 'fa fa-caret-right',
       collapse_class: 'fa fa-caret-down'
     },
@@ -64,7 +65,7 @@ export class InfraRoleDetailsComponent implements OnInit {
   ngOnInit() {
     this.data = [];
     this.layoutFacade.showSidebar(Sidebar.Infrastructure);
-    this.store.select(routeURL).pipe()
+    this.store.select(routeURL).pipe(takeUntil(this.isDestroyed))
     .subscribe((url: string) => {
       this.url = url;
       this.tabValue =  'runList';
@@ -74,7 +75,9 @@ export class InfraRoleDetailsComponent implements OnInit {
       this.store.select(routeParams).pipe(pluck('id'), filter(identity)),
       this.store.select(routeParams).pipe(pluck('orgid'), filter(identity)),
       this.store.select(routeParams).pipe(pluck('name'), filter(identity))
-    ]).pipe().subscribe(([server_id, org_id, name]: string[]) => {
+    ]).pipe(
+      takeUntil(this.isDestroyed)
+    ).subscribe(([server_id, org_id, name]: string[]) => {
       this.serverId = server_id;
       this.OrgId = org_id;
       this.name = name;
@@ -83,23 +86,24 @@ export class InfraRoleDetailsComponent implements OnInit {
       }));
     });
 
-    combineLatest([
-      this.store.select(getStatus),
-      this.store.select(infaRoleFromRoute)
-    ]).pipe(
-      filter(([status, role]) => status === EntityStatus.loadingSuccess && !isNil(role)))
-      .subscribe(([_, role]) => {
-        this.role = { ...role };
-        this.runList = this.role.run_list;
+    this.store.select(infaRoleFromRoute).pipe(
+      filter(identity),
+      takeUntil(this.isDestroyed)
+    ).subscribe(role => {
+      this.show = true;
+      if (this.role === undefined) {
+        this.role = role;
+      }
+
+      this.runList = this.role.run_list;
+      if (this.runList && this.runList.length) {
         this.show = true;
-        if (this.runList && this.runList.length) {
-          this.show = true;
-          this.treeNodes(this.runList);
-          this.roleDetailsLoading = false;
-        } else {
-          this.show = false;
-        }
-      });
+        this.treeNodes(this.runList);
+      } else {
+        this.show = false;
+      }
+      this.roleDetailsLoading = false;
+    });
   }
 
   treeNodes(runList: string[]) {
@@ -115,5 +119,10 @@ export class InfraRoleDetailsComponent implements OnInit {
   onSelectedTab(event: { target: { value: InfraRoleTabName } }) {
     this.tabValue = event.target.value;
     this.router.navigate([this.url.split('#')[0]], { fragment: event.target.value });
+  }
+
+  ngOnDestroy(): void {
+    this.isDestroyed.next(true);
+    this.isDestroyed.complete();
   }
 }
