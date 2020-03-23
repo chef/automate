302dc314541b8804656d9738b4bdf94cf3f353c0
Vivek Singh <vivek.singh@msystechnologies.com>
2020-03-23 20:33:07 +0530

    [infra-proxy-service] Add databags list & item detail APIs (#3040)
    * [infra-proxy-service] Add databags list & item detail APIs
    
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>
    
    * [infra-proxy-service] Add GetDataBagItem detail API stringify data
    
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>
    
    * [infra-proxy-service] handle dynamic data json marshal err
    
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>
    
    * Sync swagger files for Automate docs.
    
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- api/external/infra_proxy/infra_proxy.pb.policy-v2.go
+++ api/external/infra_proxy/infra_proxy.pb.policy-v2.go
@@ -326,6 +326,42 @@ func init() {
 		}
 		return ""
 	})
+	policyv2.MapMethodTo("/chef.automate.api.infra_proxy.InfraProxy/GetDataBags", "infra:chefServers:{server_id}:orgs:{org_id}:data_bags", "infra:chefServerOrgDataBags:list", "GET", "/infra/servers/{server_id}/orgs/{org_id}/data_bags/{name}", func(unexpandedResource string, input interface{}) string {
+		if m, ok := input.(*request.DataBags); ok {
+			return policyv2.ExpandParameterizedResource(unexpandedResource, func(want string) string {
+				switch want {
+				case "org_id":
+					return m.OrgId
+				case "server_id":
+					return m.ServerId
+				case "name":
+					return m.Name
+				default:
+					return ""
+				}
+			})
+		}
+		return ""
+	})
+	policyv2.MapMethodTo("/chef.automate.api.infra_proxy.InfraProxy/GetDataBagItem", "infra:chefServers:{server_id}:orgs:{org_id}:data_bags", "infra:chefServerOrgDataBags:get", "GET", "/infra/servers/{server_id}/orgs/{org_id}/data_bags/{name}/{item}", func(unexpandedResource string, input interface{}) string {
+		if m, ok := input.(*request.DataBag); ok {
+			return policyv2.ExpandParameterizedResource(unexpandedResource, func(want string) string {
+				switch want {
+				case "org_id":
+					return m.OrgId
+				case "server_id":
+					return m.ServerId
+				case "name":
+					return m.Name
+				case "item":
+					return m.Item
+				default:
+					return ""
+				}
+			})
+		}
+		return ""
+	})
 	policyv2.MapMethodTo("/chef.automate.api.infra_proxy.InfraProxy/GetEnvironments", "infra:chefServers:{server_id}:orgs:{org_id}:environments", "infra:chefServerOrgEnvironments:list", "GET", "/infra/servers/{server_id}/orgs/{org_id}/environments", func(unexpandedResource string, input interface{}) string {
 		if m, ok := input.(*request.Environments); ok {
 			return policyv2.ExpandParameterizedResource(unexpandedResource, func(want string) string {
