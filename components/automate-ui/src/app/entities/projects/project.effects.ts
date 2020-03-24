ef760496a3f905de2b0f184e092d0532be949a94
M Sorens <msorens@chef.io>
2020-03-24 16:06:59 -0700

    Make pending edits apply to multi-user environment (#3196)
    The prior fix (https://github.com/chef/automate/pull/3100)
    made "pending edits" status appear promptly for a newly logged in user,
    but neglected to handle if edits become pending while a user is logged in.
    
    This modifies the rules status polling to also include a project fetch,
    which is used to determine whether edits are pending.
    Also changing the interval from 1 to 2 minutes just to keep
    overhead to a minimum.
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/entities/projects/project.effects.ts
+++ components/automate-ui/src/app/entities/projects/project.effects.ts
@@ -49,7 +49,7 @@ import { applyRulesStatus } from './project.selectors';
 import { ApplyRulesStatusState } from './project.reducer';
 
 const ACTIVE_RULE_STATUS_INTERVAL = 5; // seconds between checks while update is in progress
-const DORMANT_RULE_STATUS_INTERVAL = 60; // seconds between checks while update is dormant
+const DORMANT_RULE_STATUS_INTERVAL = 120; // seconds between checks while update is dormant
 
 @Injectable()
 export class ProjectEffects {
@@ -219,7 +219,11 @@ export class ProjectEffects {
     filter(([[_, isV2], { state }]) =>
       isV2 && state === ApplyRulesStatusState.NotRunning
     ),
-    switchMap(this.getRulesStatus$()));
+    switchMap(() => [
+      new GetProjects(),
+      new GetApplyRulesStatus()
+    ]),
+    catchError((error: HttpErrorResponse) => observableOf(new GetApplyRulesStatusFailure(error))));
 
   private getRulesStatus$(): () => Observable<ProjectActions> {
     return () => this.requests.getApplyRulesStatus().pipe(
