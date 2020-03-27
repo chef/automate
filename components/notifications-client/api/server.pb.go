7ca3765bf18b6754ff3294282e5565c454d08f35
danielsdeleo <dan@chef.io>
2020-03-27 11:26:34 -0700

    Rebuild notifications-client protos
    Signed-off-by: Daniel DeLeo <dan@chef.io>

--- components/notifications-client/api/server.pb.go
+++ components/notifications-client/api/server.pb.go
@@ -6,12 +6,11 @@ package api
 import (
 	context "context"
 	fmt "fmt"
-	math "math"
-
 	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -52,11 +51,11 @@ var fileDescriptor_ad098daeda4239f7 = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // NotificationsClient is the client API for Notifications service.
 //
@@ -76,10 +75,10 @@ type NotificationsClient interface {
 }
 
 type notificationsClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewNotificationsClient(cc *grpc.ClientConn) NotificationsClient {
+func NewNotificationsClient(cc grpc.ClientConnInterface) NotificationsClient {
 	return &notificationsClient{cc}
 }
 
