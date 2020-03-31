17e823574bfd19124c495b75c7f2fb3cb551708d
vinay033 <vsharma@chef.io>
2020-03-31 22:56:21 +0530

    added some changes for the test cases
    Signed-off-by: vinay033 <vsharma@chef.io>

--- components/automate-ui/src/app/modules/infra-proxy/infra-roles-list/infra-roles-list.component.spec.ts
+++ components/automate-ui/src/app/modules/infra-proxy/infra-roles-list/infra-roles-list.component.spec.ts
@@ -10,51 +10,52 @@ import { ngrxReducers, runtimeChecks } from 'app/ngrx.reducers';
 import { FeatureFlagsService } from 'app/services/feature-flags/feature-flags.service';
 
 describe('InfraRolesListComponent', () => {
-    let component: InfraRolesListComponent;
-    let fixture: ComponentFixture<InfraRolesListComponent>;
-    beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        declarations: [
-          MockComponent({ selector: 'chef-th' }),
-          MockComponent({ selector: 'chef-td' }),
-          MockComponent({ selector: 'chef-error' }),
-          MockComponent({ selector: 'chef-form-field' }),
-          MockComponent({ selector: 'chef-heading' }),
-          MockComponent({ selector: 'chef-icon' }),
-          MockComponent({ selector: 'chef-loading-spinner' }),
-          MockComponent({ selector: 'mat-select' }),
-          MockComponent({ selector: 'mat-option' }),
-          MockComponent({ selector: 'chef-page-header' }),
-          MockComponent({ selector: 'chef-subheading' }),
-          MockComponent({ selector: 'chef-toolbar' }),
-          MockComponent({ selector: 'chef-table-new' }),
-          MockComponent({ selector: 'chef-table-header' }),
-          MockComponent({ selector: 'chef-table-body' }),
-          MockComponent({ selector: 'chef-table-row' }),
-          MockComponent({ selector: 'chef-table-header-cell' }),
-          MockComponent({ selector: 'chef-table-cell' }),
-          MockComponent({ selector: 'a', inputs: ['routerLink'] }),
-          InfraRolesListComponent
-        ],
-        providers: [
-          FeatureFlagsService
-        ],
-        imports: [
-          FormsModule,
-          ReactiveFormsModule,
-          RouterTestingModule,
-          StoreModule.forRoot(ngrxReducers, { runtimeChecks })
-        ],
-        schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
-      })
-      .compileComponents();
-    }));
-    beforeEach(() => {
-      fixture = TestBed.createComponent(InfraRolesListComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    });
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
+  let component: InfraRolesListComponent;
+  let fixture: ComponentFixture<InfraRolesListComponent>;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        MockComponent({ selector: 'chef-th' }),
+        MockComponent({ selector: 'chef-td' }),
+        MockComponent({ selector: 'chef-error' }),
+        MockComponent({ selector: 'chef-form-field' }),
+        MockComponent({ selector: 'chef-heading' }),
+        MockComponent({ selector: 'chef-icon' }),
+        MockComponent({ selector: 'chef-loading-spinner' }),
+        MockComponent({ selector: 'mat-select' }),
+        MockComponent({ selector: 'mat-option' }),
+        MockComponent({ selector: 'chef-page-header' }),
+        MockComponent({ selector: 'chef-subheading' }),
+        MockComponent({ selector: 'chef-toolbar' }),
+        MockComponent({ selector: 'chef-table-new' }),
+        MockComponent({ selector: 'chef-table-header' }),
+        MockComponent({ selector: 'chef-table-body' }),
+        MockComponent({ selector: 'chef-table-row' }),
+        MockComponent({ selector: 'chef-table-header-cell' }),
+        MockComponent({ selector: 'chef-table-cell' }),
+        MockComponent({ selector: 'a', inputs: ['routerLink'] }),
+        InfraRolesListComponent
+      ],
+      providers: [
+        FeatureFlagsService
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        StoreModule.forRoot(ngrxReducers, { runtimeChecks })
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    })
+    .compileComponents();
+  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InfraRolesListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
+
+  it('exists', () => {
+    expect(component).toBeTruthy();
+  });
+});
