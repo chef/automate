14487df95b7bb0ecf1a9303a570aad758b075a5b
vinay033 <vsharma@chef.io>
2020-04-03 23:11:56 +0530

    added changes for the expandable run list
    Signed-off-by: vinay033 <vsharma@chef.io>

--- components/automate-ui/src/app/entities/infra-roles/infra-role.model.ts
+++ components/automate-ui/src/app/entities/infra-roles/infra-role.model.ts
@@ -2,5 +2,20 @@ export interface InfraRole {
   name: string;
   chef_type: string;
   description: string;
+  json_class: string;
   run_list: string[];
+  expanded_run_list: ExpandList[];
+}
+
+export interface ExpandList {
+  id: string;
+  run_list: Lists[];
+}
+
+export interface Lists {
+  type: string;
+  name: string;
+  version: string;
+  skipped: string;
+  children: string[];
 }
