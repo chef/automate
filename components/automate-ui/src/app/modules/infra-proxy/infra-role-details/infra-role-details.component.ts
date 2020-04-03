835bc0df95262ebfd9b3f9fe4c0699e3bec18ce0
vinay033 <vsharma@chef.io>
2020-04-03 23:11:56 +0530

    added changes for the expandable run list
    Signed-off-by: vinay033 <vsharma@chef.io>

--- components/automate-ui/src/app/modules/infra-proxy/infra-role-details/infra-role-details.component.ts
+++ components/automate-ui/src/app/modules/infra-proxy/infra-role-details/infra-role-details.component.ts
@@ -9,7 +9,7 @@ import { filter, pluck, takeUntil } from 'rxjs/operators';
 import { identity } from 'lodash/fp';
 import { infaRoleFromRoute } from 'app/entities/infra-roles/infra-role-details.selectors';
 import { GetRole } from 'app/entities/infra-roles/infra-role.action';
-import { InfraRole } from 'app/entities/infra-roles/infra-role.model';
+import { InfraRole, ExpandList, Lists } from 'app/entities/infra-roles/infra-role.model';
 
 export type InfraRoleTabName = 'runList';
 
@@ -30,8 +30,12 @@ export class InfraRoleDetailsComponent implements OnInit, OnDestroy {
   public OrgId;
   public name;
   public runList: string[];
+  public expandedList: ExpandList[] = [];
+  public idList: any = [];
+  public expandRunList: Lists[] = [];
   public show = false;
   public data: any = [];
+  public id = '_default';
 
   private isDestroyed = new Subject<boolean>();
 
@@ -49,9 +53,24 @@ export class InfraRoleDetailsComponent implements OnInit, OnDestroy {
     columns: [
       {
         name: 'name',
-        header: 'Name',
+        header: '',
+        width: '50px'
+      },
+      {
+        name: 'version',
+        header: 'Version',
         width: '50px',
-        float: 'left'
+        renderer: function(value) {
+          return value ? value : 'N/A';
+        }
+      },
+      {
+        name: 'type',
+        header: 'Type',
+        width: '50px',
+        renderer: function(value) {
+          return value ? value : 'N/A';
+        }
       }
     ]
   };
@@ -64,6 +83,8 @@ export class InfraRoleDetailsComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.data = [];
+    this.idList = [];
+
     this.layoutFacade.showSidebar(Sidebar.Infrastructure);
     this.store.select(routeURL).pipe(takeUntil(this.isDestroyed))
     .subscribe((url: string) => {
@@ -92,11 +113,18 @@ export class InfraRoleDetailsComponent implements OnInit, OnDestroy {
     ).subscribe(role => {
       this.show = true;
       this.role = role;
+      this.expandedList = role.expanded_run_list;
 
       this.runList = this.role.run_list;
-      if (this.runList && this.runList.length) {
+      for ( let i = 0; i < this.expandedList.length; i++ ) {
+        this.idList.push({
+          id: this.expandedList[i].id
+        });
+      }
+
+      if (this.expandedList && this.expandedList.length) {
         this.show = true;
-        this.treeNodes(this.runList);
+        this.treeNodes( this.expandedList, this.id);
       } else {
         this.show = false;
       }
@@ -104,13 +132,36 @@ export class InfraRoleDetailsComponent implements OnInit, OnDestroy {
     });
   }
 
-  treeNodes(runList: string[]) {
-    for ( let i = 0; i < runList.length; i++) {
-      this.data.push({
-        id : i + 1,
-        name: runList[i],
-        parent: 0
-      });
+  selectChangeHandler (id: string) {
+    const selectedId = id;
+    this.treeNodes( this.expandedList, selectedId);
+  }
+
+  treeNodes( expandedList: ExpandList[], li: string) {
+    this.data = [];
+    for ( let i = -0; i < expandedList.length; i++ ) {
+      if ( expandedList[i].id === li ) {
+        this.expandRunList = expandedList[i].run_list;
+        if (this.expandRunList && this.expandRunList.length) {
+          for ( let j = 0; j < this.expandRunList.length; j++ ) {
+            this.data.push({
+              id: j + 1,
+              name: this.expandRunList[j].name,
+              version: this.expandRunList[j].version,
+              type: this.expandRunList[j].type,
+              parent: 0
+            });
+          }
+        } else {
+          this.data.push({
+            id: 1,
+            name: 'N/A',
+            version: 'N/A',
+            type: 'N/A',
+            parent: 0
+          });
+        }
+      }
     }
   }
 
