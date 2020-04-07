6957f8773d6d5bba92f704af4e1f0b27b6fd1d59
Jeremy J. Miller <jm@chef.io>
2020-04-07 13:24:23 -0400

    updating builder-api service pin to the release to SaaS Live builder on 04-07-2020
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- components/automate-builder-api/habitat/plan.sh
+++ components/automate-builder-api/habitat/plan.sh
@@ -14,7 +14,7 @@ pkg_deps=(
   core/bash
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # We need to pin here to get a build from unstable
-  "habitat/builder-api/8798/20200309134457"
+  "habitat/builder-api/8850/20200330144923"
 )
 
 pkg_binds=(
