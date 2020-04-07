a99d8ab66153e85313d5fd128c295394b295e3c7
Tyler Cloke <tylercloke@gmail.com>
2020-04-07 14:31:01 -0700

    Review comment
    Signed-off-by: Tyler Cloke <tylercloke@gmail.com>

--- components/automate-cli/cmd/chef-automate/deprecated.go
+++ components/automate-cli/cmd/chef-automate/deprecated.go
@@ -13,7 +13,6 @@ func newAdminTokenCommand() *cobra.Command {
 		Use:    "admin-token NAME",
 		Short:  "[deprecated] Chef Automate IAM v1 admin token create",
 		RunE:   runAdminTokenCmd,
-		Args:   cobra.ExactArgs(1),
 		Hidden: true,
 	}
 }
@@ -23,11 +22,8 @@ func init() {
 	RootCmd.AddCommand(adminCommand)
 }
 
-func runAdminTokenCmd(cmd *cobra.Command, args []string) error {
-	name := args[0]
-
-	writer.Failf("The admin-token command is deprecated. "+
-		"Use, run:\n\nchef-automate iam token create %s --admin", name)
+func runAdminTokenCmd(cmd *cobra.Command, _ []string) error {
+	writer.Failf("The admin-token command is deprecated. Use:\n\nchef-automate iam token create NAME --admin")
 	os.Exit(1)
 	return nil
 }
