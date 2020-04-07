a8998ddd12f48ab0566835e13c02126dd5fd07d7
Tyler Cloke <tylercloke@gmail.com>
2020-04-07 13:42:35 -0700

    Add admin-token back in as a hidden command and give deprecation error
    Signed-off-by: Tyler Cloke <tylercloke@gmail.com>

---
+++ components/automate-cli/cmd/chef-automate/deprecated.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// A place to put all of our deprecated commands
+
+func newAdminTokenCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:    "admin-token NAME",
+		Short:  "[deprecated] Chef Automate IAM v1 admin token create",
+		RunE:   runAdminTokenCmd,
+		Args:   cobra.ExactArgs(1),
+		Hidden: true,
+	}
+}
+
+func init() {
+	adminCommand := newAdminTokenCommand()
+	RootCmd.AddCommand(adminCommand)
+}
+
+func runAdminTokenCmd(cmd *cobra.Command, args []string) error {
+	name := args[0]
+
+	writer.Failf("The admin-token command was an IAM V1 command and is now deprecated. "+
+		"To make an admin token in IAM V2, run:\n\nchef-automate iam token create %s --admin", name)
+	os.Exit(1)
+	return nil
+}
