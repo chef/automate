30684a0c9dcd586e01ba04b2682e65506dfaaef2
Ryan Cragun <ryan@chef.io>
2020-04-07 17:01:57 -0600

    [core-plan-refresh] update pins for core plan refresh
    Signed-off-by: Ryan Cragun <ryan@chef.io>

--- scripts/install_grpcurl.sh
+++ scripts/install_grpcurl.sh
@@ -6,6 +6,8 @@ export HAB_NOCOLORING=true
 export HAB_LICENSE="accept-no-persist"
 
 install_grpcurl() {
+  #grpcurl implicity depends on latest glibc
+  hab pkg install core/glibc
   hab pkg install core/grpcurl -b -f
 }
 
