1ecf65dfd4d7e5109e22a56437686a562a84fb87
Lance Finfrock <lfinfrock@chef.io>
2020-04-13 19:07:48 -0700

    Adding filter tests
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- components/config-mgmt-service/integration_test/nodes_test.go
+++ components/config-mgmt-service/integration_test/nodes_test.go
@@ -380,6 +380,63 @@ func TestGetNodesStartEndDateTimeFilter(t *testing.T) {
 			},
 			expected: []string{"node1", "node2", "node3"},
 		},
+		{
+			description: "do not filter with date range filter with filters",
+			nodes: []iBackend.Node{
+				{
+					NodeInfo: iBackend.NodeInfo{
+						NodeName: "node1",
+					},
+					Checkin: time.Now().AddDate(0, 0, -5),
+				},
+				{
+					NodeInfo: iBackend.NodeInfo{
+						NodeName: "node2",
+					},
+					Checkin: time.Now().AddDate(0, 0, -2),
+				},
+				{
+					NodeInfo: iBackend.NodeInfo{
+						NodeName: "node3",
+					},
+					Checkin: time.Now().AddDate(0, 0, -1),
+				},
+			},
+			request: request.Nodes{
+				Filter: []string{"name:node2"},
+				Start:  time.Now().AddDate(0, 0, -6).Format(time.RFC3339),
+				End:    time.Now().Format(time.RFC3339),
+			},
+			expected: []string{"node2"},
+		},
+		{
+			description: "no start end filters out one node with date range another with filter",
+			nodes: []iBackend.Node{
+				{
+					NodeInfo: iBackend.NodeInfo{
+						NodeName: "node12",
+					},
+					Checkin: time.Now().AddDate(0, 0, -3),
+				},
+				{
+					NodeInfo: iBackend.NodeInfo{
+						NodeName: "node11",
+					},
+					Checkin: time.Now(),
+				},
+				{
+					NodeInfo: iBackend.NodeInfo{
+						NodeName: "node3",
+					},
+					Checkin: time.Now().AddDate(0, 0, -5),
+				},
+			},
+			request: request.Nodes{
+				Filter: []string{"name:node1*"},
+				End:    time.Now().AddDate(0, 0, -1).Format(time.RFC3339),
+			},
+			expected: []string{"node12"},
+		},
 	}
 
 	for _, test := range cases {
