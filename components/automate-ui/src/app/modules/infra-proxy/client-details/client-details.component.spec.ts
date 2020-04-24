8aa261f78622ade8bad85723269ec006e672caf4
vinay033 <vsharma@chef.io>
2020-04-24 13:04:44 +0530

    added client details component
    Signed-off-by: vinay033 <vsharma@chef.io>

---
+++ components/automate-ui/src/app/modules/infra-proxy/client-details/client-details.component.spec.ts
@@ -0,0 +1,70 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { ngrxReducers, runtimeChecks } from 'app/ngrx.reducers';
+import { FeatureFlagsService } from 'app/services/feature-flags/feature-flags.service';
+import { ClientDetailsComponent } from './client-details.component';
+import { MockComponent } from 'ng2-mock-component';
+
+describe('ClientDetailsComponent', () => {
+  let component: ClientDetailsComponent;
+  let fixture: ComponentFixture<ClientDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        MockComponent({ selector: 'input', inputs: ['resetOrigin'] }),
+        MockComponent({ selector: 'chef-button',
+          inputs: ['disabled', 'routerLink'] }),
+        MockComponent({ selector: 'mat-select' }),
+        MockComponent({ selector: 'chef-th' }),
+        MockComponent({ selector: 'chef-td' }),
+        MockComponent({ selector: 'chef-heading' }),
+        MockComponent({ selector: 'chef-icon' }),
+        MockComponent({ selector: 'chef-loading-spinner' }),
+        MockComponent({ selector: 'chef-option' }),
+        MockComponent({ selector: 'chef-page-header' }),
+        MockComponent({ selector: 'chef-subheading' }),
+        MockComponent({ selector: 'chef-toolbar' }),
+        MockComponent({ selector: 'chef-table' }),
+        MockComponent({ selector: 'chef-thead' }),
+        MockComponent({ selector: 'chef-tbody' }),
+        MockComponent({ selector: 'chef-tr' }),
+        MockComponent({ selector: 'chef-th' }),
+        MockComponent({ selector: 'chef-td' }),
+        MockComponent({ selector: 'a', inputs: ['routerLink'] }),
+        MockComponent({ selector: 'chef-tab-selector',
+          inputs: ['value', 'routerLink', 'fragment']
+        }),
+        ClientDetailsComponent
+      ],
+      providers: [
+        FeatureFlagsService
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        StoreModule.forRoot(ngrxReducers, { runtimeChecks })
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClientDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('defaults to showing details section', () => {
+    expect(component.tabValue).toBe('details');
+  });
+});
---
+++ components/automate-ui/src/app/modules/infra-proxy/client-details/client-details.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit, OnDestroy, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { combineLatest, Subject } from 'rxjs';
+import { NgrxStateAtom } from 'app/ngrx.reducers';
+import { LayoutFacadeService, Sidebar } from 'app/entities/layout/layout.facade';
+import { routeParams } from 'app/route.selectors';
+import { filter, pluck, takeUntil } from 'rxjs/operators';
+import { identity } from 'lodash/fp';
+import { clientFromRoute } from 'app/entities/clients/client-details.selectors';
+import { GetClient } from 'app/entities/clients/client.action';
+import { Client } from 'app/entities/clients/client.model';
+
+export type ClientTabName = 'details';
+
+@Component({
+  selector: 'app-client-details',
+  templateUrl: './client-details.component.html',
+  styleUrls: ['./client-details.component.scss']
+})
+
+export class ClientDetailsComponent implements OnInit, OnDestroy {
+  public client: Client;
+  public tabValue: ClientTabName = 'details';
+  public url: string;
+  public conflictErrorEvent = new EventEmitter<boolean>();
+  public modalType: string;
+  public serverId;
+  public orgId;
+  public name;
+  public show = false;
+  private isDestroyed = new Subject<boolean>();
+  clientDetailsLoading = true;
+
+  constructor(
+    private store: Store<NgrxStateAtom>,
+    private router: Router,
+    private layoutFacade: LayoutFacadeService
+  ) { }
+
+  ngOnInit() {
+    this.layoutFacade.showSidebar(Sidebar.Infrastructure);
+
+    combineLatest([
+      this.store.select(routeParams).pipe(pluck('id'), filter(identity)),
+      this.store.select(routeParams).pipe(pluck('orgid'), filter(identity)),
+      this.store.select(routeParams).pipe(pluck('name'), filter(identity))
+    ]).pipe(
+      takeUntil(this.isDestroyed)
+    ).subscribe(([server_id, org_id, name]: string[]) => {
+      this.serverId = server_id;
+      this.orgId = org_id;
+      this.name = name;
+      this.store.dispatch(new GetClient({
+        server_id: server_id, org_id: org_id, name: name
+      }));
+    });
+
+    this.store.select(clientFromRoute).pipe(
+      filter(identity),
+      takeUntil(this.isDestroyed)
+    ).subscribe(client => {
+      this.show = true;
+      this.client = client;
+      this.clientDetailsLoading = false;
+    });
+  }
+
+  onSelectedTab(event: { target: { value: ClientTabName } }) {
+    this.tabValue = event.target.value;
+    this.router.navigate([this.url.split('#')[0]], { fragment: event.target.value });
+  }
+
+  ngOnDestroy(): void {
+    this.isDestroyed.next(true);
+    this.isDestroyed.complete();
+  }
+}
