b52159ff558a7faf4b5f0dfee4e74c19f50a6072
Vivek Singh <vivek.singh@msystechnologies.com>
2020-04-24 21:13:26 +0530

    [infra-proxy-service] Add test resetter implementation (#3490)
    * [infra-proxy-service] Add test resetter implementation
     - Minor updates for make test.
     - DRY up the test code.
    
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>
    
    * [infra-proxy-service] Fixes buildkite
    
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- components/infra-proxy-service/server/server_test.go
+++ components/infra-proxy-service/server/server_test.go
@@ -7,24 +7,15 @@ import (
 	"github.com/stretchr/testify/require"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 
-	"github.com/chef/automate/lib/logger"
-
 	"github.com/chef/automate/components/infra-proxy-service/test"
 )
 
 func TestHealthGRPC(t *testing.T) {
 	ctx := context.Background()
-
-	l, err := logger.NewLogger("text", "debug")
-	require.NoError(t, err, "could not init logger", err)
-
-	migrationConfig, err := test.MigrationConfigIfPGTestsToBeRun(l, "../storage/postgres/migration/sql")
-	require.NoError(t, err)
-
-	_, _, conn, close, _ := test.SetupInfraProxyService(ctx, t, l, *migrationConfig)
-	defer close()
-
+	_, _, conn, close, _, _ := test.SetupInfraProxyService(ctx, t)
 	cl := healthpb.NewHealthClient(conn)
+
+	defer close()
 
 	t.Run("Check", func(t *testing.T) {
 		actual, err := cl.Check(ctx, &healthpb.HealthCheckRequest{})
