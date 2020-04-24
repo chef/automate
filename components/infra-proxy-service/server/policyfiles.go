8e3616ba19f122b5b770e648933c065d9a29a9af
Vivek Singh <vivek.singh@msystechnologies.com>
2020-04-24 21:22:36 +0530

    [infra-proxy-service] Add expanded run-list to policyfile detail API (#3460)
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- components/infra-proxy-service/server/policyfiles.go
+++ components/infra-proxy-service/server/policyfiles.go
@@ -53,11 +53,27 @@ func (s *Server) GetPolicyfile(ctx context.Context, req *request.Policyfile) (*r
 		return nil, status.Error(codes.Internal, err.Error())
 	}
 
+	result, err := c.GetRoleList()
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	runList, err := GetExpandRunlistFromRole(policy.RunList, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	eRunList := &response.ExpandedRunList{
+		Id:      "_default",
+		RunList: runList,
+	}
+
 	return &response.Policyfile{
 		RevisionId:          policy.RevisionID,
 		Name:                policy.Name,
 		RunList:             policy.RunList,
 		NamedRunList:        fromAPINamedRunList(policy.NamedRunList),
+		ExpandedRunList:     []*response.ExpandedRunList{eRunList},
 		IncludedPolicyLocks: fromAPIIncludedPolicyLocks(policy.IncludedPolicyLocks),
 		CookbookLocks:       fromAPICookbookLocks(policy.CookbookLocks),
 		DefaultAttributes:   string(defaultAttrs),
