d284792937163c55745be10196467ff25c9dea86
susanev <susan.ra.evans@gmail.com>
2020-04-25 10:43:40 -0700

    fix paths on landing components
    Signed-off-by: susanev <susan.ra.evans@gmail.com>

--- components/automate-ui/src/app/pages/settings-landing/settings-landing.component.ts
+++ components/automate-ui/src/app/pages/settings-landing/settings-landing.component.ts
@@ -10,11 +10,11 @@ export class SettingsLandingComponent {
 
   // order determined by settings-sidebar template and is vetted by settings-sidebar unit tests
   public routeList: RoutePerms[] = [
-    { anyOfCheck: [['/notifications/rules', 'get', '']], route: '/settings/notifications' },
-    { anyOfCheck: [['/datafeed/destinations', 'post', '']], route: '/settings/data-feeds' },
-    { anyOfCheck: [['/nodemanagers/search', 'post', '']], route: '/settings/node-integrations' },
-    { anyOfCheck: [['/secrets/search', 'post', '']], route: '/settings/node-credentials' },
-    { anyOfCheck: [['/retention/nodes/status', 'get', '']], route: '/settings/data-lifecycle' },
+    { anyOfCheck: [['/api/v0/notifications/rules', 'get', '']], route: '/settings/notifications' },
+    { anyOfCheck: [['/api/v0/datafeed/destinations', 'post', '']], route: '/settings/data-feeds' },
+    { anyOfCheck: [['/api/v0/nodemanagers/search', 'post', '']], route: '/settings/node-integrations' },
+    { anyOfCheck: [['/api/v0/secrets/search', 'post', '']], route: '/settings/node-credentials' },
+    { anyOfCheck: [['/api/v0/retention/nodes/status', 'get', '']], route: '/settings/data-lifecycle' },
     { allOfCheck: [['/apis/iam/v2/users', 'get', '']], route: '/settings/users' },
     { allOfCheck: [['/apis/iam/v2/teams', 'get', '']], route: '/settings/teams' },
     { allOfCheck: [['/apis/iam/v2/tokens', 'get', '']], route: '/settings/tokens' },
