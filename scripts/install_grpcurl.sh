014002fa56d0c55ec5e1a374030c472bbbeb0483
Ryan Cragun <ryan@chef.io>
2020-04-28 12:43:17 -0600

    install the old glibc
    Signed-off-by: Ryan Cragun <ryan@chef.io>

--- scripts/install_grpcurl.sh
+++ scripts/install_grpcurl.sh
@@ -7,7 +7,7 @@ export HAB_LICENSE="accept-no-persist"
 
 install_grpcurl() {
   #grpcurl implicity depends on latest glibc
-  hab pkg install core/glibc
+  hab pkg install core/glibc/2.27/20190115002733
   hab pkg install core/grpcurl -b -f
 }
 
