8cd1c233f3f15ed60ad2543eff8126cc43309ddd
Scott Christopherson <scott@chef.io>
2020-04-27 19:20:11 -0600

    Feedback edits
    Signed-off-by: Scott Christopherson <scott@chef.io>

--- components/automate-ui/src/app/components/chart-progress-bar/chart-progress-bar.model.ts
+++ components/automate-ui/src/app/components/chart-progress-bar/chart-progress-bar.model.ts
@@ -1,37 +1,17 @@
 export interface Styles {
   bar?: Bar;
-  container?: Container;
   progress?: Progress;
-  threshold?: any;
+  threshold?: Threshold;
 }
 
 export interface Bar {
-  background?: string;
-  borderRadius?: string;
-  boxShadow?: string;
-  height?: string;
   width?: string;
 }
 
-export interface Container {
-  height?: string;
-}
-
 export interface Progress {
-  background?: string;
-  borderRadius?: string;
-  bordeTopRightRadius?: string;
-  bordeBottomRightRadius?: string;
-  height?: string;
   width?: string;
 }
 
 export interface Threshold {
-  point1?: Point;
-  point2?: Point;
-}
-
-export interface Point {
-  height?: string;
-  marginLeft?: string;
+  [index:string]: { marginLeft?: string }
 }
