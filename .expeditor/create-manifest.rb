78222d3755673c48f99cf1b79bb91beca3756547
Ryan Cragun <ryan@chef.io>
2020-04-29 09:29:54 -0600

    bump automate-elasticsearch and automate-postgresql pins for core-plan refresh (#3536)
    Signed-off-by: Ryan Cragun <ryan@chef.io>

--- .expeditor/create-manifest.rb
+++ .expeditor/create-manifest.rb
@@ -313,8 +313,8 @@ pins = {
   # This will pin the services that we package and start with the deployment service,
   # while still allowing the clients of these databases to upgrade their client
   # libraries if any fixes are shipped there.
-  "automate-postgresql"    => {"origin" => "chef", "name" => "automate-postgresql",    "version" => "9.6.11", "release" => "20190409151101"},
-  "automate-elasticsearch" => {"origin" => "chef", "name" => "automate-elasticsearch", "version" => "6.8.3",  "release" => "20191217225932"},
+  "automate-postgresql"    => {"origin" => "chef", "name" => "automate-postgresql",    "version" => "9.6.11", "release" => "20200428204055"},
+  "automate-elasticsearch" => {"origin" => "chef", "name" => "automate-elasticsearch", "version" => "6.8.3",  "release" => "20200429011833"},
 }
 
 unless no_pin_hab
