a5d2d2f65f03c7cbcd91bfb673cdbcd8a4814662
Vivek Singh <vivek.singh@msystechnologies.com>
2020-05-01 11:48:07 +0530

    [infra-proxy-service] combine update query in single on
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- components/authz-service/storage/postgres/migration/sql/77_update_infra_service_policies.up.sql
+++ components/authz-service/storage/postgres/migration/sql/77_update_infra_service_policies.up.sql
@@ -1,15 +1,6 @@
-
--- append "infra:infraServers:list" & "infra:infraServers:get" to the editor role's actions
+-- append "infra:infraServers:list" & "infra:infraServers:get" to the editor and viewer role's actions
 UPDATE iam_roles
     SET actions = '{infra:infraServers:list,infra:infraServers:get}' || actions
     WHERE
-        id = 'editor' AND
-        NOT actions @> '{infra:infraServers:list,infra:infraServers:get}';
-
-
--- append "infra:infraServers:list" & "infra:infraServers:get" to the viewer role's actions
-UPDATE iam_roles
-    SET actions = '{infra:infraServers:list,infra:infraServers:get}' || actions
-    WHERE
-        id = 'viewer' AND
+        (id='editor' or id='viewer') AND
         NOT actions @> '{infra:infraServers:list,infra:infraServers:get}';
