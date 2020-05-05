25c55f92662ae20cd9e0c27c3dc6ce91acacccaa
Vivek Singh <vivek.singh@msystechnologies.com>
2020-05-06 02:22:50 +0530

    [infra-proxy-service] sql query & script load updates
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- components/infra-proxy-service/storage/postgres/migration/sql/04_convert_table_to_human_readable_ids.up.sql
+++ components/infra-proxy-service/storage/postgres/migration/sql/04_convert_table_to_human_readable_ids.up.sql
@@ -1,5 +1,5 @@
 -- drop foreign key constraint.
-ALTER TABLE orgs DROP CONSTRAINT orgs_server_id_fkey;
+ALTER TABLE orgs DROP CONSTRAINT IF EXISTS orgs_server_id_fkey;
 
 -- remove UUID column data type.
 ALTER TABLE servers ALTER COLUMN id TYPE TEXT;
@@ -11,11 +11,11 @@ ALTER TABLE orgs ADD CONSTRAINT orgs_server_id_fkey
 FOREIGN KEY (server_id) REFERENCES servers(id) ON DELETE RESTRICT;
 
 -- rename uniqueness constraint.
-ALTER TABLE servers DROP CONSTRAINT servers_name_key;
-ALTER TABLE orgs DROP CONSTRAINT orgs_name_server_id_key;
+ALTER TABLE servers DROP CONSTRAINT IF EXISTS servers_name_key;
+ALTER TABLE orgs DROP CONSTRAINT IF EXISTS orgs_name_server_id_key;
 
 -- drop description column.
-ALTER TABLE servers DROP COLUMN description;
+ALTER TABLE servers DROP COLUMN IF EXISTS description;
 
 -- add unique constraint.
 ALTER TABLE orgs ADD CONSTRAINT orgs_name_server_id_key
