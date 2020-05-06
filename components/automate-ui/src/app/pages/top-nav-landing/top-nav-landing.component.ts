b67172c88c07dd370a1334df2e79b36e70097eca
michael sorens <msorens@chef.io>
2020-05-06 10:48:00 -0700

    Wire up permission-based routing for top nav
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/pages/top-nav-landing/top-nav-landing.component.ts
+++ components/automate-ui/src/app/pages/top-nav-landing/top-nav-landing.component.ts
@@ -8,6 +8,53 @@ import { RoutePerms } from 'app/components/landing/landing.component';
 })
 export class TopNavLandingComponent {
 
+  // order must match layout in navbar.component.html
   public routeList: RoutePerms[] = [
+    {
+      allOfCheck: [
+        ['/api/v0/eventfeed', 'get', ''],
+        ['/api/v0/eventstrings', 'get', ''],
+        ['/api/v0/event_type_counts', 'get', ''],
+        ['/api/v0/event_task_counts', 'get', ''],
+        ['/api/v0/cfgmgmt/suggestions', 'get', '']
+      ], route: '/dashboards/event-feed'
+    },
+    {
+      anyOfCheck: [
+        ['/api/v0/applications/services', 'get', ''],
+        ['/api/v0/applications/service-groups', 'get', '']
+      ], route: '/applications/service-groups'
+    },
+    {
+      allOfCheck: [
+        ['/api/v0/cfgmgmt/nodes', 'get', ''],
+        ['/api/v0/cfgmgmt/stats/node_counts', 'get', '']
+      ], route: '/infrastructure'
+    },
+    {
+      anyOfCheck: [
+        ['/api/v0/compliance/reporting/stats/summary', 'post', ''],
+        ['/api/v0/compliance/reporting/stats/failures', 'post', ''],
+        ['/api/v0/compliance/reporting/stats/trend', 'post', ''],
+        ['/api/v0/compliance/scanner/jobs', 'post', ''],
+        ['/api/v0/compliance/scanner/jobs/search', 'post', ''],
+        ['/api/v0/compliance/profiles/search', 'post', '']
+      ], route: '/compliance'
+    },
+    {
+      anyOfCheck: [
+        ['/api/v0/datafeed/destinations', 'post', ''],
+        ['/api/v0/notifications/rules', 'get', ''],
+        ['/api/v0/secrets/search', 'post', ''],
+        ['/api/v0/nodemanagers/search', 'post', ''],
+        ['/api/v0/retention/nodes/status', 'get', ''],
+        ['/apis/iam/v2/users', 'get', ''],
+        ['/apis/iam/v2/teams', 'get', ''],
+        ['/apis/iam/v2/tokens', 'get', ''],
+        ['/apis/iam/v2/policies', 'get', ''],
+        ['/apis/iam/v2/roles', 'get', ''],
+        ['/apis/iam/v2/projects', 'get', '']
+      ], route: '/settings'
+    }
   ];
 }
