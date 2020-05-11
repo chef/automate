9e29de09e5c88c91e1752dc6e89b0cbab11aac75
michael sorens <msorens@chef.io>
2020-05-11 13:48:30 -0700

    Make rarely used parameters param optional
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/src/app/pages/compliance-landing/compliance-landing.component.ts
+++ components/automate-ui/src/app/pages/compliance-landing/compliance-landing.component.ts
@@ -11,18 +11,18 @@ export class ComplianceLandingComponent {
   // order determined by LayoutSidebarService and vetted by SidebarComponent unit tests
   public routeList: RoutePerms[] = [
     {
-      allOfCheck: [['/api/v0/compliance/reporting/stats/summary', 'post', ''],
-        ['/api/v0/compliance/reporting/stats/failures', 'post', ''],
-        ['/api/v0/compliance/reporting/stats/trend', 'post', '']],
+      allOfCheck: [['/api/v0/compliance/reporting/stats/summary', 'post'],
+        ['/api/v0/compliance/reporting/stats/failures', 'post'],
+        ['/api/v0/compliance/reporting/stats/trend', 'post']],
       route: '/compliance/reports'
     },
     {
-      allOfCheck: [['/api/v0/compliance/scanner/jobs', 'post', ''],
-      ['/compliance/scanner/jobs/search', 'post', '']],
+      allOfCheck: [['/api/v0/compliance/scanner/jobs', 'post'],
+      ['/compliance/scanner/jobs/search', 'post']],
       route: '/compliance/scan-jobs'
     },
     {
-      allOfCheck: [['/api/v0/compliance/profiles/search', 'post', '']],
+      allOfCheck: [['/api/v0/compliance/profiles/search', 'post']],
       route: '/compliance/compliance-profiles'
     }
   ];
--- components/automate-ui/src/app/pages/settings-landing/settings-landing.component.ts
+++ components/automate-ui/src/app/pages/settings-landing/settings-landing.component.ts
@@ -10,18 +10,18 @@ export class SettingsLandingComponent {
 
   // order determined by LayoutSidebarService and vetted by SidebarComponent unit tests
   public routeList: RoutePerms[] = [
-    { anyOfCheck: [['/api/v0/notifications/rules', 'get', '']], route: '/settings/notifications' },
-    { anyOfCheck: [['/api/v0/datafeed/destinations', 'post', '']], route: '/settings/data-feeds' },
-    { anyOfCheck: [['/api/v0/retention/nodes/status', 'get', '']],
+    { anyOfCheck: [['/api/v0/notifications/rules', 'get']], route: '/settings/notifications' },
+    { anyOfCheck: [['/api/v0/datafeed/destinations', 'post']], route: '/settings/data-feeds' },
+    { anyOfCheck: [['/api/v0/retention/nodes/status', 'get']],
       route: '/settings/data-lifecycle' },
-    { anyOfCheck: [['/api/v0/nodemanagers/search', 'post', '']], route: '/settings/node-integrations' },
-    { anyOfCheck: [['/api/v0/secrets/search', 'post', '']], route: '/settings/node-credentials' },
-    { allOfCheck: [['/apis/iam/v2/users', 'get', '']], route: '/settings/users' },
-    { allOfCheck: [['/apis/iam/v2/teams', 'get', '']], route: '/settings/teams' },
-    { allOfCheck: [['/apis/iam/v2/tokens', 'get', '']], route: '/settings/tokens' },
-    { allOfCheck: [['/apis/iam/v2/policies', 'get', '']], route: '/settings/policies' },
-    { allOfCheck: [['/apis/iam/v2/roles', 'get', '']], route: '/settings/roles' },
-    { allOfCheck: [['/apis/iam/v2/projects', 'get', '']], route: '/settings/projects' }
+    { anyOfCheck: [['/api/v0/nodemanagers/search', 'post']], route: '/settings/node-integrations' },
+    { anyOfCheck: [['/api/v0/secrets/search', 'post']], route: '/settings/node-credentials' },
+    { allOfCheck: [['/apis/iam/v2/users', 'get']], route: '/settings/users' },
+    { allOfCheck: [['/apis/iam/v2/teams', 'get']], route: '/settings/teams' },
+    { allOfCheck: [['/apis/iam/v2/tokens', 'get']], route: '/settings/tokens' },
+    { allOfCheck: [['/apis/iam/v2/policies', 'get']], route: '/settings/policies' },
+    { allOfCheck: [['/apis/iam/v2/roles', 'get']], route: '/settings/roles' },
+    { allOfCheck: [['/apis/iam/v2/projects', 'get']], route: '/settings/projects' }
   ];
 
 }
