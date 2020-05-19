bed82a654526c183b3eb2ad3c0a2d431ccc32ec2
seajamied <jdegnan@chef.io>
2020-05-19 15:29:18 -0700

    Update chef-phat-radio to take optional canDeselect attribute
    Adding the canDeselect attribute allows the user to deselect the group completely by
    clicking back on the same value
    
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/chef-ui-library/src/molecules/chef-phat-radio/chef-phat-radio.tsx
+++ components/chef-ui-library/src/molecules/chef-phat-radio/chef-phat-radio.tsx
@@ -22,6 +22,9 @@ export class ChefPhatRadio {
    * The value of the currently toggled option.
    */
   @Prop({ mutable: true }) value = '';
+  // canDeselect is an optional property that when true allows users
+  // to deselect all options by clicking on the currently selected option.
+  @Prop() canDeselect: boolean;
 
   @Element() el: HTMLElement;
 
@@ -32,17 +35,24 @@ export class ChefPhatRadio {
 
   @Listen('click') handleClick(event) {
     const option = event.target.closest('chef-option');
+    // If new click option is the same as current, we are deselecting
+    const isSameOption = this.value === option.value;
+
     if (option) {
-      this.value = option.value;
-      this.change.emit();
-      this.input.emit();
+    this.value = this.canDeselect && isSameOption ? '' : option.value;
+    this.change.emit();
+    this.input.emit();
     }
   }
 
   componentDidLoad() {
     const options = this.clearOptions();
     this.selected = find(['value', this.value], options);
-    if (!this.selected) {
+    // When option to deselect is true, we also do not want to make a default
+    // selection upon load, so we return early unless a value has been explicity set by prop
+    if (!this.selected && this.canDeselect) {
+      return;
+    } else if (!this.selected) {
       this.selected = this.el.querySelector('chef-option[selected]') || options[0];
       this.value = this.selected.value;
     }
@@ -51,6 +61,9 @@ export class ChefPhatRadio {
 
   componentDidUpdate() {
     this.clearOptions();
+    // Return early if the update is empty, because this means we are deselecting
+    if (this.canDeselect && this.value === '') { return; }
+
     this.selected = this.el.querySelector(`chef-option[value='${this.value}']`);
     this.selected.selected = true;
   }
