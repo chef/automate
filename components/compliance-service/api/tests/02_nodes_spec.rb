9d967b8e92d0aa587670f45cc57ee9669d78285b
Victoria Jeffrey <vjeffrey@chef.io>
2020-05-19 09:01:14 -0600

    move test comment
    Signed-off-by: Victoria Jeffrey <vjeffrey@chef.io>

--- components/compliance-service/api/tests/02_nodes_spec.rb
+++ components/compliance-service/api/tests/02_nodes_spec.rb
@@ -658,12 +658,13 @@ describe File.basename(__FILE__) do
     }.to_json
     assert_equal_json_sorted(expected_nodes, actual_nodes.to_json)
 
-    # Test filters by skipped status - expect 0 nodes, but expect counts to not be 0
+    # Test filters by skipped status
     actual_nodes = GRPC reporting, :list_nodes, Reporting::Query.new(filters: [
       Reporting::ListFilter.new(type: 'environment', values: ['DevSec Prod beta']),
       Reporting::ListFilter.new(type: 'status', values: ['skipped']),
       Reporting::ListFilter.new(type: 'end_time', values: ['2018-03-04T23:59:59Z'])
     ])
+    # expect 0 nodes, but expect counts to not be 0 
     expected_nodes = {
       "total" => 2,
       "totalFailed" => 1,
