f5595c37408adb8300f1eb7fc91128cd0fe6721a
seajamied <jdegnan@chef.io>
2020-05-20 12:31:42 -0700

    Add double column and begin basic styling of buttons - no actions
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/page-components/insight-attributes-dropdown/insight-attributes-dropdown.component.ts
+++ components/automate-ui/src/app/page-components/insight-attributes-dropdown/insight-attributes-dropdown.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterOptions, FilterableOptions } from './insight-attributes-dropdown.model';
+
 
 @Component({
   selector: 'app-insight-attributes-dropdown',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./insight-attributes-dropdown.component.scss']
 })
 export class InsightAttributesDropdownComponent implements OnInit {
+
+  public options: FilterOptions[] = FilterableOptions;
 
   ngOnInit() {
     console.log('on init');
