305519f003a6a282ad5700e8017074a54f94bdb8
seajamied <jdegnan@chef.io>
2020-05-21 17:18:14 -0700

    Add animation
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/page-components/insight-attributes-dropdown/insight-attributes-dropdown.component.ts
+++ components/automate-ui/src/app/page-components/insight-attributes-dropdown/insight-attributes-dropdown.component.ts
@@ -1,11 +1,36 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, HostBinding } from '@angular/core';
+import { trigger, transition, style, animate, query, group, animateChild } from '@angular/animations';
 import { FilterOption, FilterableOptions } from './insight-attributes-dropdown.model';
 import { isEqual } from 'lodash/fp';
+
+
+// Animations
+const menuOpenAnim = trigger('menuOpenAnim', [
+  transition(':enter', [
+    style({ opacity: 0, transform: 'scale(0)' }),
+    group([
+      query('@childAnim', animateChild()),
+      animate('220ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))
+    ])
+  ]),
+  transition(':leave', [
+    style({ opacity: 1 }),
+    animate('195ms ease-in', style({ opacity: 0 }))
+  ])
+]);
+
+const childAnim = trigger('childAnim', [
+  transition(':enter', [
+    style({ opacity: 0 }),
+    animate('220ms 170ms', style({ opacity: 1 }))
+  ])
+]);
 
 @Component({
   selector: 'app-insight-attributes-dropdown',
   templateUrl: './insight-attributes-dropdown.component.html',
-  styleUrls: ['./insight-attributes-dropdown.component.scss']
+  styleUrls: ['./insight-attributes-dropdown.component.scss'],
+  animations: [ menuOpenAnim, childAnim ]
 })
 export class InsightAttributesDropdownComponent implements OnInit {
 
@@ -13,6 +38,8 @@ export class InsightAttributesDropdownComponent implements OnInit {
   @Input() lastSelectedOptions: string[] = []; // these are filter ids
   @Output() onUpdateFilters: EventEmitter<any> = new EventEmitter();
   @Output() onToggleMenu: EventEmitter<any> = new EventEmitter();
+
+  @HostBinding('@menuOpenAnim') menuOpenAnim;
 
   public options: FilterOption[] = FilterableOptions;
   public selectedOptions: string[] = []; // these are filter ids
