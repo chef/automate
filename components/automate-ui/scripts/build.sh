b233ea48ea78320c22a6d9e9aa71a853d4b19842
michael sorens <msorens@chef.io>
2020-06-08 09:24:02 -0700

    Add some helpful in-code comments
    Signed-off-by: michael sorens <msorens@chef.io>

--- components/automate-ui/scripts/build.sh
+++ components/automate-ui/scripts/build.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 # The build machine is using sh; need to use bash to be able to use PIPESTATUS.
 
+# Purpose:
+# Treat warnings as errors for build commands that do not provide innate support to do that.
+
 CMD_STRING=$1
+# Examples:
+#    "ng lint"
+#    "ng lint | grep -v 'deprecated'"
+
 WARNING_LABEL=$2
+# Examples:
+#    "WARN:"         (as `ng test` uses)
+#    "WARNING:"      (as `ng lint` uses)
 
 # Setup an unused stream to redirect to stdout in the next command.
 exec 5>&1
 # We want to capture the target command's output to reuse it (rather than have to re-run the command),
 # so sending to an unused stream and redirecting that to stdout (stream 1) where it can be captured into the `out` variable.
 # In order to check the exit code of the target command itself, need to use PIPESTATUS
-# to grab the exit code of the first pipe component rather than the final one.
+# to grab the exit code of the first pipe component inside CMD_STRING rather than the final one.
 out=$(eval "$CMD_STRING | tee /dev/fd/5; exit \${PIPESTATUS[0]}")
 
 # If the unit test run reported non-zero--an actual unit test error--then exit with non-zero return code.
