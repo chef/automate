ee1f8ad89c1c1923b5e48e440a0fe2f6756c557d
Lance Finfrock <lfinfrock@chef.io>
2020-06-09 11:57:09 -0700

    removing unused validate
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- components/automate-cds/server/server.go
+++ components/automate-cds/server/server.go
@@ -1,14 +1,10 @@
 package server
 
 import (
-	"context"
 	"net"
 
-	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
 
 	grpc_s "github.com/chef/automate/api/interservice/cds/service"
 	"github.com/chef/automate/components/automate-cds/service"
@@ -18,18 +14,13 @@ import (
 
 // NewGRPCServer creates a grpc server that serves all Teams GRPC APIs
 func NewGRPCServer(s *service.Service) *grpc.Server {
-	g := s.ConnFactory.NewServer(
-		grpc.UnaryInterceptor(
-			grpc_middleware.ChainUnaryServer(
-				tracing.ServerInterceptor(tracing.GlobalTracer()),
-				InputValidationInterceptor(),
-			),
-		),
-	)
-	health.RegisterHealthServer(g, health.NewService())
-	grpc_s.RegisterAutomateCdsServer(g, NewServer(s))
-	reflection.Register(g)
-	return g
+	grpcServer := s.ConnFactory.NewServer(tracing.GlobalServerInterceptor())
+
+	health.RegisterHealthServer(grpcServer, health.NewService())
+	grpc_s.RegisterAutomateCdsServer(grpcServer, NewServer(s))
+	reflection.Register(grpcServer)
+
+	return grpcServer
 }
 
 // GRPC creates and listens on grpc server
@@ -41,21 +32,4 @@ func GRPC(addr string, s *service.Service) error {
 	serv := NewGRPCServer(s).Serve(list)
 	s.Logger.Debugf("Automate CDS GRPC API listening on %v", addr)
 	return serv
-}
-
-// InputValidationInterceptor is a middleware for running the protobuf validation.
-func InputValidationInterceptor() grpc.UnaryServerInterceptor {
-	return func(ctx context.Context,
-		req interface{},
-		_ *grpc.UnaryServerInfo,
-		handler grpc.UnaryHandler) (interface{}, error) {
-		if req, ok := req.(interface {
-			Validate() error
-		}); ok {
-			if err := req.Validate(); err != nil {
-				return nil, status.Error(codes.InvalidArgument, err.Error())
-			}
-		}
-		return handler(ctx, req)
-	}
 }
