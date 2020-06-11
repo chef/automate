e93eb2ae0227efdd13e48fac2b4d0c623529170c
Victoria Jeffrey <vjeffrey@chef.io>
2020-06-11 11:47:55 -0600

    cleanup
    Signed-off-by: Victoria Jeffrey <vjeffrey@chef.io>

--- components/compliance-service/reporting/relaxting/nodes.go
+++ components/compliance-service/reporting/relaxting/nodes.go
@@ -173,12 +173,8 @@ func (backend *ES2Backend) GetNodes(from int32, size int32, filters map[string][
 					// we are filtering by a profile or profile + control (deep filtering).
 					// so here we ensure we only add the node to the list object if the status matches the
 					// requested status.
-					if len(filters["status"]) == 0 {
+					if len(filters["status"]) == 0 || contains(filters["status"], latestReport.Status) {
 						nodes = append(nodes, &node)
-					} else {
-						if contains(filters["status"], latestReport.Status) {
-							nodes = append(nodes, &node)
-						}
 					}
 				} else {
 					return nil, emptyTotals, errors.Wrapf(err, "%s unmarshal error: ", myName)
