861ecf6545fdd19269837cde67c1a6d6447c88f2
seajamied <jdegnan@chef.io>
2020-06-16 15:13:31 -0700

    Updated reducer a little bit
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/entities/desktop/desktop.reducer.spec.ts
+++ components/automate-ui/src/app/entities/desktop/desktop.reducer.spec.ts
@@ -8,6 +8,9 @@ describe('desktopEntityReducer', () => {
     describe('UPDATE_DESKTOPS_FILTER_PAGE_SIZE_AND_CURRENT_PAGE', () => {
 
       it('updates the pageSize and pageNumber', () => {
+        expect(initialState.getDesktopsFilter.pageSize).toBe(10);
+        expect(initialState.getDesktopsFilter.currentPage).toBe(1);
+
         const action = new UpdateDesktopsFilterPageSizeAndCurrentPage({
           pageSize: 20, updatedPageNumber: 3
         });
