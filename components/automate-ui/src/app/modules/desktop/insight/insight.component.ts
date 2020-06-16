6a228f42876b652c9f5c113be674c1332305d88e
Tara Black <tblack@chef.io>
2020-06-16 09:21:17 -0500

    Desktop UI Download individual desktop historical data as JSON or CSV Signed-Off-By: Tara Black <tblack@chef.io>

--- components/automate-ui/src/app/modules/desktop/insight/insight.component.ts
+++ components/automate-ui/src/app/modules/desktop/insight/insight.component.ts
@@ -1,13 +1,22 @@
-import { Component, HostBinding, Input, Output, EventEmitter } from '@angular/core';
+import { Component, HostBinding, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { Store, createSelector } from '@ngrx/store';
+import { NgrxStateAtom } from 'app/ngrx.reducers';
 import { Desktop, TermFilter } from 'app/entities/desktop/desktop.model';
 import { FilterUpdate } from 'app/page-components/insight-attributes-dropdown/insight-attributes-dropdown.model';
+import { finalize } from 'rxjs/operators';
+import * as moment from 'moment/moment';
+import { saveAs } from 'file-saver';
+import { DateTime } from 'app/helpers/datetime/datetime';
+import { ClientRunsRequests } from 'app/entities/client-runs/client-runs.requests';
+import { clientRunsState } from 'app/entities/client-runs/client-runs.selectors';
+import { NodeFilter } from 'app/entities/client-runs/client-runs.model';
 
 @Component({
   selector: 'app-insight',
   templateUrl: './insight.component.html',
   styleUrls: ['./insight.component.scss']
 })
-export class InsightComponent {
+export class InsightComponent implements OnInit {
 
   @Input() desktops: Desktop[];
   @Input() selectedDesktop: Desktop;
@@ -26,6 +35,23 @@ export class InsightComponent {
   @Output() desktopSelected: EventEmitter<Desktop> = new EventEmitter();
 
   public attributesMenuOpen = false;
+  public downloadOptsVisible = false;
+  public downloadInProgress = false;
+  public downloadFailed = false;
+
+  private nodeFilter: NodeFilter;
+
+  constructor(
+    private store: Store<NgrxStateAtom>,
+    private requests: ClientRunsRequests
+  ) { }
+
+  ngOnInit() {
+    this.store.select(createSelector(clientRunsState, (state) => state.nodeFilter)).subscribe(
+      (nodeFilter: NodeFilter) => {
+        this.nodeFilter = nodeFilter;
+      });
+  }
 
   public close(): void {
     this.closed.emit();
@@ -49,5 +75,32 @@ export class InsightComponent {
 
   public updateFilters(event: FilterUpdate) {
     console.log(event);
+  }
+
+  toggleDownloadDropdown() {
+    this.downloadOptsVisible = !this.downloadOptsVisible;
+  }
+
+  hideDownloadDropdown() {
+    this.downloadOptsVisible = false;
+  }
+
+  onDownloadOptPressed(format) {
+    this.downloadOptsVisible = false;
+
+    const filename = `${moment().utc().format(DateTime.REPORT_DATE_TIME)}.${format}`;
+
+    const onComplete = () => this.downloadInProgress = false;
+    const onError = _e => this.downloadFailed = true;
+    const types = {'json': 'application/json', 'csv': 'text/csv'};
+    const onNext = data => {
+      const type = types[format];
+      const blob = new Blob([data], {type});
+      saveAs(blob, filename);
+    };
+
+    this.requests.downloadNodes(format, this.nodeFilter).pipe(
+      finalize(onComplete))
+      .subscribe(onNext, onError);
   }
 }
