57943604d2c2805472acaf696c990279de2160d3
Martin Scott <mscott@chef.io>
2020-06-28 11:47:53 +0100

    The data feed service breaks when used with chef-load and compliance only data #3954 (#3996)
    * addition of nil checks and additional unit tests on the data-feed-service functions
    
    Signed-off-by: Martin Scott <mscott@chef.io>

--- components/data-feed-service/server/server.go
+++ components/data-feed-service/server/server.go
@@ -62,7 +62,6 @@ func (datafeedServer *DatafeedServer) AddDestination(ctx context.Context, destin
 }
 
 func (datafeedServer *DatafeedServer) TestDestination(ctx context.Context, request *datafeed.URLValidationRequest) (*datafeed.TestDestinationResponse, error) {
-	log.Infof("TestDestination %s", request)
 	response := &datafeed.TestDestinationResponse{Success: false}
 	// http client to endpoint {text: "TEST: Successful validation completed by Automate"}
 	// if it's secret - get the credentials
