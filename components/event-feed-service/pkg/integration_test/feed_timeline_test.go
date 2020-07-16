f5e91d8f79f6fba43bc63c0f0a636eaf02684a06
Lance Finfrock <lancewf@gmail.com>
2020-07-16 11:43:43 -0700

    Event Feed timeline project filtering (#4072)
    Adding project filtering for the GetFeedTimeline endpoint for the event-feed-service. This is the last of the three endpoints that need project filtering. Project filtering is only used for chef-server events which are not added yet.
    
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- components/event-feed-service/pkg/integration_test/feed_timeline_test.go
+++ components/event-feed-service/pkg/integration_test/feed_timeline_test.go
@@ -8,12 +8,14 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 
 	"math/rand"
 	"strconv"
 
 	"github.com/chef/automate/api/interservice/event_feed"
+	authzConstants "github.com/chef/automate/components/authz-service/constants"
 	"github.com/chef/automate/components/event-feed-service/pkg/feed"
 	"github.com/chef/automate/components/event-feed-service/pkg/persistence"
 
@@ -224,6 +226,570 @@ func TestEventStringsFilterEventType(t *testing.T) {
 					}
 				}
 			})
+	}
+}
+
+func TestEventStringsProjectFilter(t *testing.T) {
+	var (
+		timezone  = "UTC"
+		startDate = time.Now().AddDate(0, 0, -3)
+		endDate   = time.Now()
+		request   = event_feed.FeedTimelineRequest{
+			Interval: 1,
+			Start:    startDate.Format("2006-01-02"),
+			End:      endDate.Format("2006-01-02"),
+			Timezone: timezone,
+		}
+	)
+
+	cases := []struct {
+		description string
+		entries     []feed.FeedEntry
+		ctx         context.Context
+		expected    map[string]int
+	}{
+		{
+			description: "No events with a request filting on a project",
+			entries:     []feed.FeedEntry{},
+			ctx:         contextWithProjects([]string{"project9"}),
+			expected:    map[string]int{},
+		},
+		{
+			description: "One event with a project matching the request's project filters",
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			ctx: contextWithProjects([]string{"project9"}),
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "One non-chef-server event with no projects; request's has a project",
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "profile",
+					Verb:               "update",
+					ProducerObjectType: "profile",
+				},
+			},
+			ctx: contextWithProjects([]string{"project9"}),
+			expected: map[string]int{
+				"profile": 1,
+			},
+		},
+		{
+			description: "Two events with the same project matching the request's project filter",
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "delete",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			ctx: contextWithProjects([]string{"project9"}),
+			expected: map[string]int{
+				"policyfile": 1,
+				"cookbook":   1,
+			},
+		},
+		{
+			description: "Two events one chef-server and the other not; The chef-server event has a matching project to the request's projects",
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "delete",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "profile",
+					Verb:               "update",
+					ProducerObjectType: "profile",
+				},
+			},
+			ctx: contextWithProjects([]string{"project9"}),
+			expected: map[string]int{
+				"profile":    1,
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Two events one chef-server and the other not; The chef-server event has a non-matching project to the request's projects",
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "delete",
+					Projects:           []string{"non-matching-project"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "profile",
+					Verb:               "update",
+					ProducerObjectType: "profile",
+				},
+			},
+			ctx: contextWithProjects([]string{"project9"}),
+			expected: map[string]int{
+				"profile": 1,
+			},
+		},
+		{
+			description: "Two Actions with only one's project matching requested projects",
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "delete",
+					Projects:           []string{"project3"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			ctx: contextWithProjects([]string{"project9"}),
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Action project not matching request projects",
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			ctx:      contextWithProjects([]string{"project3"}),
+			expected: map[string]int{},
+		},
+		{
+			description: "One Action has one project; request all projects allowed",
+			ctx:         contextWithProjects([]string{authzConstants.AllProjectsExternalID}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Three Actions; request all projects allowed",
+			ctx:         contextWithProjects([]string{authzConstants.AllProjectsExternalID}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "delete",
+					Projects:           []string{"project3"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "node",
+					Verb:               "create",
+					Projects:           []string{},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+				"node":       1,
+				"cookbook":   1,
+			},
+		},
+		{
+			description: "Action has no projects; request all projects allowed",
+			ctx:         contextWithProjects([]string{authzConstants.AllProjectsExternalID}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Action has no projects; request unassigned projects allowed",
+			ctx:         contextWithProjects([]string{authzConstants.UnassignedProjectID}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Action has a project; request only unassigned projects",
+			ctx:         contextWithProjects([]string{authzConstants.UnassignedProjectID}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{},
+		},
+		{
+			description: "Two Actions have and don't have projects; request only unassigned projects",
+			ctx:         contextWithProjects([]string{authzConstants.UnassignedProjectID}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "delete",
+					Projects:           []string{},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"cookbook": 1,
+			},
+		},
+		{
+			description: "Action has a project; request unassigned and matching project allowed",
+			ctx:         contextWithProjects([]string{authzConstants.UnassignedProjectID, "project9"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Action has no projects; request has no projects",
+			ctx:         contextWithProjects([]string{}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Two Actions have and don't have projects; request has no projects",
+			ctx:         contextWithProjects([]string{}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "delete",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+				"cookbook":   1,
+			},
+		},
+		{
+			description: "Action with one project matching one of several requested projects allowed",
+			ctx:         contextWithProjects([]string{"project3", "project9", "project7", "project6"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Two actions with one project matching different one of several requested projects allowed",
+			ctx:         contextWithProjects([]string{"project3", "project9", "project7", "project6"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "delete",
+					Projects:           []string{"project3"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+				"cookbook":   1,
+			},
+		},
+		{
+			description: "Action with one project not matching any of several requested projects allowed",
+			ctx:         contextWithProjects([]string{"project3", "project4", "project7", "project6"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{},
+		},
+		{
+			description: "Two Actions with one project not matching any of several requested projects allowed",
+			ctx:         contextWithProjects([]string{"project3", "project4", "project7", "project6"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project9"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "delete",
+					Projects:           []string{"project10"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{},
+		},
+		{
+			description: "Action with several projects where one matches a single requested project allowed",
+			ctx:         contextWithProjects([]string{"project3"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project3", "project4", "project7", "project6"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Two Actions with several projects where one matches a single requested project allowed",
+			ctx:         contextWithProjects([]string{"project3"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project3", "project4", "project7", "project6"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "delete",
+					Projects:           []string{"project12", "project10", "project11", "project3"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+				"cookbook":   1,
+			},
+		},
+		{
+			description: "Two Actions with several projects where only one action's project matches a single requested project allowed",
+			ctx:         contextWithProjects([]string{"project3"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project3", "project4", "project7", "project6"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "delete",
+					Projects:           []string{"project12", "project10", "project11", "project13"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Action with several projects where one matches one of several requested project allowed",
+			ctx:         contextWithProjects([]string{"project3", "project10", "project12", "project13"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project3", "project4", "project7", "project6"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+		{
+			description: "Two Actions with several projects where one matches one of several requested project allowed",
+			ctx:         contextWithProjects([]string{"project3", "project10", "project12", "project13"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project3", "project4", "project7", "project6"},
+					ProducerObjectType: "chef_server",
+				},
+				{
+					ObjectObjectType:   "cookbook",
+					Verb:               "delete",
+					Projects:           []string{"project13", "project14", "project17", "project16"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+				"cookbook":   1,
+			},
+		},
+		{
+			description: "Action with several projects where none matches several requested project allowed",
+			ctx:         contextWithProjects([]string{"project14", "project10", "project12", "project13"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project3", "project4", "project7", "project6"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{},
+		},
+		{
+			description: "Action with several projects where two matches two of several requested project allowed",
+			ctx:         contextWithProjects([]string{"project3", "project10", "project12", "project13"}),
+			entries: []feed.FeedEntry{
+				{
+					ObjectObjectType:   "policyfile",
+					Verb:               "update",
+					Projects:           []string{"project3", "project10", "project7", "project6"},
+					ProducerObjectType: "chef_server",
+				},
+			},
+			expected: map[string]int{
+				"policyfile": 1,
+			},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("Project filter: %s", test.description), func(t *testing.T) {
+			for index := range test.entries {
+				test.entries[index].ID = newUUID()
+				test.entries[index].Published = time.Now()
+				test.entries[index].ProducerName = "Fred"
+				test.entries[index].ProducerTags = []string{"mycompany", "engineering department", "compliance team"}
+				test.entries[index].FeedType = "event"
+				test.entries[index].EventType = event.ScanJobUpdatedEventName
+				test.entries[index].Tags = []string{"org_1", "compliance", "profile"}
+				test.entries[index].ActorID = "urn:mycompany:user:fred"
+				test.entries[index].ActorObjectType = "User"
+				test.entries[index].ActorName = "Fred"
+				test.entries[index].ObjectID = "urn:chef:compliance:scan-job"
+				test.entries[index].ObjectName = "Scan Job"
+				test.entries[index].TargetID = "urn:mycompany:environment:production"
+				test.entries[index].TargetObjectType = "Environment"
+				test.entries[index].TargetName = "Production"
+				test.entries[index].Created = time.Now().UTC()
+			}
+
+			for _, entry := range test.entries {
+				testSuite.feedBackend.CreateFeedEntry(&entry)
+			}
+
+			testSuite.RefreshIndices(persistence.IndexNameFeeds)
+			defer GetTestSuite().DeleteAllDocuments()
+
+			res, err := testSuite.feedClient.GetFeedTimeline(test.ctx, &request)
+			require.NoError(t, err)
+
+			itemsCount := map[string]int{}
+			// test response
+			for _, eventString := range res.ActionLines {
+
+				for _, item := range eventString.Slots {
+					if len(item.Counts) > 0 {
+						itemsCount[item.Counts[0].Category]++
+					}
+				}
+			}
+			for key, expectedValue := range test.expected {
+				value, ok := itemsCount[key]
+
+				assert.True(t, ok, "Event type '%s' was not found on string", key)
+				assert.Equal(t, expectedValue, value,
+					"Number of '%s' on string was %v should be %v", key, value, expectedValue)
+			}
+
+			for key := range itemsCount {
+				if key != "" {
+					_, ok := test.expected[key]
+					assert.True(t, ok, "Event type '%s' should not be on the string", key)
+				}
+			}
+		})
 	}
 }
 
