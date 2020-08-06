6f8b8332d62cc158471ec55ec2d212a79cbdd177
Dan DeLeo <dan@chef.io>
2020-08-06 10:59:48 -0700

    Test for Idempotent Schema Migration (#4187)
    In https://github.com/chef/automate/pull/4183 we fixed an error in the migrator that was re-running migrations in multiple front-end cluster topologies, but after that patch we still try to forcibly redo migrations if they fail and leave a dirty schema state. So it's best if our migrations are able to re-run. This PR adds a function to our migrator library which drops all the data in the database, then runs a service's migrations all the way through twice. In addition to checking that migrations are idempotent, this approach makes it convenient to develop new migrations, you can keep re-running the integration tests and any bad state created by mistakes gets cleaned up.
    
    I only modified the two services I know well in this pass, it should be pretty easy to add to others if we like.
    
    * Add test for migration idempotence to the migrator lib
    * Make config-mgmt's migrations idempotent and test it
    * Add idempotent migration checking to applications test suite

--- components/config-mgmt-service/backend/postgres/schema/sql/03_add_rollouts_author_initiator.up.sql
+++ components/config-mgmt-service/backend/postgres/schema/sql/03_add_rollouts_author_initiator.up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE rollouts ADD COLUMN scm_author_name TEXT NOT NULL DEFAULT '';
-ALTER TABLE rollouts ADD COLUMN scm_author_email TEXT NOT NULL DEFAULT '';
-ALTER TABLE rollouts ADD COLUMN policy_domain_username TEXT NOT NULL DEFAULT '';
+ALTER TABLE rollouts ADD COLUMN IF NOT EXISTS scm_author_name TEXT NOT NULL DEFAULT '';
+ALTER TABLE rollouts ADD COLUMN IF NOT EXISTS scm_author_email TEXT NOT NULL DEFAULT '';
+ALTER TABLE rollouts ADD COLUMN IF NOT EXISTS policy_domain_username TEXT NOT NULL DEFAULT '';
 
--- lib/db/migrator/migrator.go
+++ lib/db/migrator/migrator.go
@@ -31,7 +31,7 @@ func Migrate(pgURL, migrationsPath string, l logger.Logger, verbose bool) error 
 // MigrateWithMigrationsTable executes all migrations we have, using the
 // specified migrations table.
 func MigrateWithMigrationsTable(pgURL, migrationsPath, migrationsTable string, l logger.Logger, verbose bool) error {
-	l.Infof("Running db migrations from %q", migrationsPath)
+	l.Infof("running db migrations from %q", migrationsPath)
 	purl, err := addMigrationsTable(pgURL, migrationsTable)
 	if err != nil {
 		return errors.Wrap(err, "parse PG URL")
@@ -87,6 +87,41 @@ func MigrateWithMigrationsTable(pgURL, migrationsPath, migrationsTable string, l
 	// that's always nil
 	_, err = m.Close()
 	return errors.Wrap(err, "close migrations connection")
+}
+
+// DestructiveMigrateForTests will:
+// * Drop the database to give you a clean slate
+// * Run all your migrations
+// * Forcibly run all the migrations again to verify that they are idempotent.
+//
+// Obviously you don't want this for production, but you should use it instead
+// of the plain Migrate function in your tests if you can.
+func DestructiveMigrateForTests(pgURL, migrationsPath string, l logger.Logger, verbose bool) error {
+	l.Infof("running db migrations in destructo test mode for DB %q from %q", pgURL, migrationsPath)
+	purl, err := addMigrationsTable(pgURL, "")
+
+	m, err := migrate.New(addScheme(migrationsPath), purl)
+	if err != nil {
+		return errors.Wrapf(err, "migrator setup failed for %q", pgURL)
+	}
+	m.Log = migrationLog{Logger: l, verbose: verbose}
+	if err := m.Drop(); err != nil {
+		return errors.Wrapf(err, "drop database failed for %q", pgURL)
+	}
+	if err := m.Drop(); err != nil {
+		return errors.Wrapf(err, "drop database failed for %q", pgURL)
+	}
+	if err := m.Up(); err != nil {
+		return errors.Wrapf(err, "failed applying migrations to clean database %q", pgURL)
+	}
+	if err := m.Force(-1); err != nil {
+		return errors.Wrapf(err, "failed to force migration state to version 0 for db %q", pgURL)
+	}
+	if err := m.Up(); err != nil {
+		return errors.Wrapf(err, "failed to re-apply migration to migrated database %q - missing IF (NOT) EXISTS?", pgURL)
+	}
+
+	return nil
 }
 
 func addScheme(p string) string {
