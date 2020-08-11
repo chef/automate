8081c866cf06be3a552fedad4c36778ff9d98e7b
Vinay Sharma <vsharma@chef.io>
2020-08-11 12:36:37 +0530

    added changes for the test cases
    Signed-off-by: Vinay Sharma <vsharma@chef.io>

--- components/automate-ui/src/app/pages/create-notification-modal/create-notification-modal.component.spec.ts
+++ components/automate-ui/src/app/pages/create-notification-modal/create-notification-modal.component.spec.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockComponent } from 'ng2-mock-component';
 
@@ -14,10 +15,21 @@ describe('CreateNotificationModalComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         MockComponent({ selector: 'chef-button', inputs: ['disabled'] }),
-        MockComponent({ selector: 'chef-loading-spinner' }),
-        MockComponent({ selector: 'chef-form-field' }),
         MockComponent({ selector: 'chef-error' }),
+        MockComponent({ selector: 'chef-form-field' }),
+        MockComponent({ selector: 'chef-heading' }),
+        MockComponent({ selector: 'chef-icon' }),
+        MockComponent({ selector: 'chef-loading-spinner' }),
+        MockComponent({ selector: 'chef-page-header' }),
+        MockComponent({ selector: 'chef-subheading' }),
         MockComponent({ selector: 'chef-toolbar' }),
+        MockComponent({ selector: 'chef-table' }),
+        MockComponent({ selector: 'chef-thead' }),
+        MockComponent({ selector: 'chef-tbody' }),
+        MockComponent({ selector: 'chef-tr' }),
+        MockComponent({ selector: 'chef-th' }),
+        MockComponent({ selector: 'chef-td' }),
+        MockComponent({ selector: 'a', inputs: ['routerLink'] }),
         MockComponent({ selector: 'chef-modal',
           inputs: ['visible'],
           outputs: ['close']
@@ -25,6 +37,7 @@ describe('CreateNotificationModalComponent', () => {
         CreateNotificationModalComponent
       ],
       imports: [
+        MatSelectModule,
         ReactiveFormsModule
       ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
@@ -41,6 +54,7 @@ describe('CreateNotificationModalComponent', () => {
       url: ['', null],
       targetType: ['', null],
       ruleType: ['', null],
+      targetUrl: ['', null],
       username: ['', null],
       password: ['', null]
     });
--- components/automate-ui/src/app/pages/notifications/notifications.component.spec.ts
+++ components/automate-ui/src/app/pages/notifications/notifications.component.spec.ts
@@ -1,95 +1,82 @@
-import { TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { By } from '@angular/platform-browser';
-import { Observable, of as observableOf } from 'rxjs';
-import { Store, StoreModule } from '@ngrx/store';
-import { NgrxStateAtom, ngrxReducers, runtimeChecks } from 'app/ngrx.reducers';
-
-import { NotificationRule, ServiceActionType } from 'app/entities/notification_rules/notification_rule.model';
-import {
-  GetNotificationRulesSuccess
-} from 'app/entities/notification_rules/notification_rule.action';
-
-import { RulesService } from '../../services/rules/rules.service';
-import { TelemetryService } from '../../services/telemetry/telemetry.service';
-import { NotificationsComponent } from './notifications.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpErrorResponse } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
-import { FeatureFlagsService } from '../../services/feature-flags/feature-flags.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MockComponent } from 'ng2-mock-component';
+import { StoreModule, Store } from '@ngrx/store';
+import { NgrxStateAtom, ngrxReducers, runtimeChecks } from 'app/ngrx.reducers';
+import { NotificationsComponent } from './notifications.component';
+import { NotificationRuleRequests } from 'app/entities/notification_rules/notification_rule.requests';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { NotificationRule, ServiceActionType } from 'app/entities/notification_rules/notification_rule.model';
+import { CreateNotificationRuleSuccess, CreateNotificationRuleFailure } from 'app/entities/notification_rules/notification_rule.action';
+import { HttpStatus } from 'app/types/types';
+import { TelemetryService } from '../../services/telemetry/telemetry.service';
+import { FeatureFlagsService } from 'app/services/feature-flags/feature-flags.service';
+
+class MockTelemetryService {
+  track() { }
+}
 
 describe('NotificationsComponent', () => {
-  let store: Store<NgrxStateAtom>;
-  const rules: NotificationRule[] = [
-    new NotificationRule('id1', 'test rule1', 'ComplianceFailure',
-      'http://foo.com', ServiceActionType.SLACK, '', false),
-    new NotificationRule('id2', 'test rule2', 'CCRFailure',
-      'http://foo.com', ServiceActionType.WEBHOOK, '', false)
-  ];
-
-  // CSS identifiers
-  const cardId = '#notifications-cards';
-  const listId = '#notifications-list';
-
-  class MockTelemetryService {
-    track() { }
-  }
-
-  class MockRulesService {
-    fetchRules(): Observable<NotificationRule[]> {
-      return observableOf(rules);
-    }
-
-    deleteRule(rule) {
-    return observableOf(rule);
-    }
-  }
-
   let telemetryService: TelemetryService;
-  let fixture, component;
+  let component: NotificationsComponent;
+  let fixture: ComponentFixture<NotificationsComponent>;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        StoreModule.forRoot(ngrxReducers, { runtimeChecks })
-      ],
       declarations: [
-        NotificationsComponent
+        NotificationsComponent,
+        MockComponent({
+        selector: 'app-create-notification-modal',
+        inputs: ['visible', 'creating', 'conflictErrorEvent', 'createForm'],
+        outputs: ['close', 'createClicked']
+        }),
+        MockComponent({ selector: 'app-delete-object-modal',
+        inputs: ['default', 'visible', 'objectNoun', 'objectName'],
+        outputs: ['close', 'deleteClicked'] }),
+        MockComponent({ selector: 'chef-button',
+                inputs: ['disabled', 'routerLink'] }),
+        MockComponent({ selector: 'chef-error' }),
+        MockComponent({ selector: 'chef-form-field' }),
+        MockComponent({ selector: 'chef-heading' }),
+        MockComponent({ selector: 'chef-icon' }),
+        MockComponent({ selector: 'chef-loading-spinner' }),
+        MockComponent({ selector: 'chef-page-header' }),
+        MockComponent({ selector: 'chef-subheading' }),
+        MockComponent({ selector: 'chef-toolbar' }),
+        MockComponent({ selector: 'chef-table' }),
+        MockComponent({ selector: 'chef-thead' }),
+        MockComponent({ selector: 'chef-tbody' }),
+        MockComponent({ selector: 'chef-tr' }),
+        MockComponent({ selector: 'chef-th' }),
+        MockComponent({ selector: 'chef-td' }),
+        MockComponent({ selector: 'a', inputs: ['routerLink'] })
       ],
       providers: [
         { provide: TelemetryService, useClass: MockTelemetryService },
-        { provide: RulesService, useClass: MockRulesService },
-        FeatureFlagsService
+        FeatureFlagsService,
+        NotificationRuleRequests,
+        HttpClient,
+        HttpHandler
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        StoreModule.forRoot(ngrxReducers, { runtimeChecks })
       ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
-    }).compileComponents();
-    store = TestBed.inject(Store);
-    spyOn(store, 'dispatch').and.callThrough();
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(NotificationsComponent);
     component = fixture.componentInstance;
     telemetryService = TestBed.inject(TelemetryService);
     fixture.detectChanges();
-  });
-
-  describe ('in list view', () => {
-    beforeEach(() => {
-      store = TestBed.inject(Store);
-      store.dispatch(new GetNotificationRulesSuccess(rules));
-      component.cardView = false;
-      fixture.detectChanges();
-    });
-
-    it ('shows only the table', () => {
-      const listViewElement = fixture.debugElement.query(By.css(listId));
-      const cardViewElement = fixture.debugElement.query(By.css(cardId));
-      expect(listViewElement.name).toBe('chef-table');
-      expect(cardViewElement).toBeNull();
-    });
-
-    it('shows expected item count', () => {
-      const tableBody = getElementByCss(`${listId} chef-tr`);
-      expect(tableBody.children.length).toBe(rules.length + 3);
-    });
-
   });
 
   describe('sending telemetry', () => {
@@ -105,11 +92,121 @@ describe('NotificationsComponent', () => {
 
   });
 
-  function getElementByCss(cssString: string) {
-    return fixture
-      .debugElement
-      .query(By.css(cssString))
-      .nativeElement;
-  }
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
+  describe('create notification', () => {
+    let store: Store<NgrxStateAtom>;
+
+    const notification: NotificationRule =
+      new NotificationRule('id1', 'test rule1', 'ComplianceFailure',
+            'http://foo.com', ServiceActionType.SLACK, '', false);
+
+    beforeEach(() => {
+      store = TestBed.inject(Store);
+    });
+
+    it('openCreateModal opens modal', () => {
+      expect(component.createModalVisible).toBe(false);
+      component.openCreateModal();
+      expect(component.createModalVisible).toBe(true);
+    });
+
+    it('opening create modal resets name, targetType, RuleType and targetUrl to empty string',
+      () => {
+      component.createNotificationForm.controls['name'].setValue('any');
+      component.createNotificationForm.controls['targetType'].setValue('any');
+      component.createNotificationForm.controls['ruleType'].setValue('any');
+      component.createNotificationForm.controls['targetUrl'].setValue('any');
+      component.openCreateModal();
+      expect(component.createNotificationForm.controls['name'].value).toBe(null);
+      expect(component.createNotificationForm.controls['targetType'].value).toBe(null);
+      expect(component.createNotificationForm.controls['ruleType'].value).toBe(null);
+      expect(component.createNotificationForm.controls['targetUrl'].value).toBe(null);
+    });
+
+    it('on success, closes modal and adds new notification', () => {
+      component.createNotificationForm.controls['name'].setValue(notification.name);
+      component.createNotificationForm.controls['targetType'].setValue(notification.targetType);
+      component.createNotificationForm.controls['ruleType'].setValue(notification.ruleType);
+      component.createNotificationForm.controls['targetUrl'].setValue(notification.targetUrl);
+      component.createNotification();
+
+      store.dispatch(new CreateNotificationRuleSuccess(notification));
+      component.rules$.subscribe(rules => {
+        component.closeCreateModal();
+        expect(rules).toContain(notification);
+      });
+    });
+
+    it('on conflict error, modal is open with conflict error', () => {
+      spyOn(component.conflictErrorEvent, 'emit');
+      component.openCreateModal();
+      component.createNotificationForm.controls['name'].setValue(notification.name);
+      component.createNotificationForm.controls['targetType'].setValue(notification.targetType);
+      component.createNotificationForm.controls['ruleType'].setValue(notification.ruleType);
+      component.createNotificationForm.controls['targetUrl'].setValue(notification.targetUrl);
+      component.createNotification();
+
+      const conflict = <HttpErrorResponse>{
+        status: HttpStatus.CONFLICT,
+        ok: false
+      };
+      store.dispatch(new CreateNotificationRuleFailure(conflict));
+
+      expect(component.createModalVisible).toBe(true);
+      expect(component.conflictErrorEvent.emit).toHaveBeenCalled();
+    });
+
+    it('on create error, modal is closed with failure banner', () => {
+      spyOn(component.conflictErrorEvent, 'emit');
+      component.openCreateModal();
+      component.createNotificationForm.controls['name'].setValue(notification.name);
+      component.createNotificationForm.controls['targetType'].setValue(notification.targetType);
+      component.createNotificationForm.controls['ruleType'].setValue(notification.ruleType);
+      component.createNotificationForm.controls['targetUrl'].setValue(notification.targetUrl);
+      component.createNotification();
+
+      const error = <HttpErrorResponse>{
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        ok: false
+      };
+      store.dispatch(new CreateNotificationRuleFailure(error));
+
+      expect(component.createModalVisible).toBe(false);
+      expect(component.conflictErrorEvent.emit).toHaveBeenCalledWith(false);
+    });
+
+  });
+
+  describe('create notification form validation', () => {
+
+    it('- url field validity', () => {
+      component.openCreateModal();
+
+      let errors = {};
+      const url = component.createNotificationForm.controls['targetUrl'];
+      expect(url.valid).toBeFalsy();
+
+      // url field is required
+      errors = url.errors || {};
+      expect(errors['required']).toBeTruthy();
+
+      url.setValue('');
+      errors = url.errors || {};
+      expect(errors['required']).toBeTruthy();
+
+      // Set url to invalid inputs
+      url.setValue('  ');
+      errors = url.errors || {};
+      expect(errors['pattern']).toBeTruthy();
+      expect(errors['required']).toBeFalsy();
+
+      // Set url to valid inputs
+      url.setValue('any');
+      errors = url.errors || {};
+      expect(errors['pattern']).toBeFalsy();
+    });
+  });
 });
