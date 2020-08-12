684ed68ede9954486fe81f8f3741ba9f9bcfdd9e
Steven Danna <steve@chef.io>
2020-08-12 18:20:11 +0100

    automate-chef-server: Use new sqerl verify_ca feature
    This allows us to avoid a breaking changing by continuing to verify
    certificates but ignoring hostname mismatches.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- components/automate-cs-bookshelf/habitat/plan.sh
+++ components/automate-cs-bookshelf/habitat/plan.sh
@@ -6,7 +6,7 @@ pkg_name="automate-cs-bookshelf"
 pkg_description="Wrapper package for chef/bookshelf"
 pkg_origin="chef"
 # WARNING: Version managed by .expeditor/update_chef_server.sh
-pkg_version="14.0.21"
+pkg_version="14.0.22"
 vendor_origin="chef"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
@@ -15,7 +15,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/bookshelf/14.0.21/20200810215354"
+  "${vendor_origin}/bookshelf/14.0.22/20200812153852"
 )
 
 pkg_binds=(
