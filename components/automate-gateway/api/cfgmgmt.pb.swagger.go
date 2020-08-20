9da9876eb507dde1d415e868f8c245b2293ee4b5
danielsdeleo <dan@chef.io>
2020-08-20 15:17:34 -0700

    Enable ENUM_VALUE_PREFIX for new protos
    Signed-off-by: Daniel DeLeo <dan@chef.io>

--- components/automate-gateway/api/cfgmgmt.pb.swagger.go
+++ components/automate-gateway/api/cfgmgmt.pb.swagger.go
@@ -1038,18 +1038,18 @@ func init() {
     "chef.automate.api.cfgmgmt.request.SCMType": {
       "type": "string",
       "enum": [
-        "UNKNOWN_SCM",
-        "GIT"
+        "SCM_TYPE_UNKNOWN_SCM",
+        "SCM_TYPE_GIT"
       ],
-      "default": "UNKNOWN_SCM"
+      "default": "SCM_TYPE_UNKNOWN_SCM"
     },
     "chef.automate.api.cfgmgmt.request.SCMWebType": {
       "type": "string",
       "enum": [
-        "UNKNOWN_SCM_WEB",
-        "GITHUB"
+        "SCM_WEB_TYPE_UNKNOWN_SCM_WEB",
+        "SCM_WEB_TYPE_GITHUB"
       ],
-      "default": "UNKNOWN_SCM_WEB"
+      "default": "SCM_WEB_TYPE_UNKNOWN_SCM_WEB"
     },
     "chef.automate.api.cfgmgmt.response.CheckInCounts": {
       "type": "object",
@@ -1888,18 +1888,18 @@ func init() {
     "chef.automate.api.cfgmgmt.response.SCMType": {
       "type": "string",
       "enum": [
-        "UNKNOWN_SCM",
-        "GIT"
+        "SCM_TYPE_UNKNOWN_SCM",
+        "SCM_TYPE_GIT"
       ],
-      "default": "UNKNOWN_SCM"
+      "default": "SCM_TYPE_UNKNOWN_SCM"
     },
     "chef.automate.api.cfgmgmt.response.SCMWebType": {
       "type": "string",
       "enum": [
-        "UNKNOWN_SCM_WEB",
-        "GITHUB"
+        "SCM_WEB_TYPE_UNKNOWN_SCM_WEB",
+        "SCM_WEB_TYPE_GITHUB"
       ],
-      "default": "UNKNOWN_SCM_WEB"
+      "default": "SCM_WEB_TYPE_UNKNOWN_SCM_WEB"
     },
     "chef.automate.api.cfgmgmt.response.TypeCount": {
       "type": "object",
