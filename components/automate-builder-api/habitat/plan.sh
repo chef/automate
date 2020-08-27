ded2204e0ccd4976ad8960dab675eb2915e85539
Jeremy J. Miller <jm@chef.io>
2020-08-27 15:41:44 -0400

    updating the pin for builder-api to include a HTTP error message improvement
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- components/automate-builder-api/habitat/plan.sh
+++ components/automate-builder-api/habitat/plan.sh
@@ -14,7 +14,7 @@ pkg_deps=(
   core/bash
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # We need to pin here to get a build from unstable
-  "habitat/builder-api/9029/20200821134940"
+  "habitat/builder-api/9034/20200827185135"
 )
 
 pkg_binds=(
