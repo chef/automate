a6e8784a0715c0549a69231340f2daa9785c6641
Steven Danna <steve@chef.io>
2020-08-28 13:01:22 +0100

    Bump chef-server versions (#4279)
    This pulls in a workaround for TLS issues with external automate. The
    workaround requires some new configuration with restores the old
    behavior where we validate the certificate against the configured CA
    but ignore hostname mismatches.
    
    Our hope is to have full hostname checking support when Chef Server
    upgrades to Erlang 22.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- components/automate-cs-bookshelf/habitat/plan.sh
+++ components/automate-cs-bookshelf/habitat/plan.sh
@@ -6,7 +6,7 @@ pkg_name="automate-cs-bookshelf"
 pkg_description="Wrapper package for chef/bookshelf"
 pkg_origin="chef"
 # WARNING: Version managed by .expeditor/update_chef_server.sh
-pkg_version="14.0.22"
+pkg_version="14.0.25"
 vendor_origin="chef"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
@@ -15,7 +15,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/bookshelf/14.0.22/20200812153852"
+  "${vendor_origin}/bookshelf/14.0.25/20200828081447"
 )
 
 pkg_binds=(
