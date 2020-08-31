e755c54c9a4834de37ae3dabcf548220788eefb6
Steven Danna <steve@chef.io>
2020-08-31 09:08:59 +0100

    data-feed-service: move from port 14001 to 10501 (#4271)
    Any port over 13000 is problematic because of our recommended
    ephemeral port range for our A2 HA customers:
    
    ```
    cat /proc/sys/net/ipv4/ip_local_port_range
    10300	65024
    ```
    
    Thus, any port over 10300 might be in use as an ephemeral port and not
    available at service start-up or service restart.
    
    Port 10501 was the first port returned by
    
    ```
    chef-automate dev ports free
    ```
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- api/config/data_feed/config_request.pb.a2svc.go
+++ api/config/data_feed/config_request.pb.a2svc.go
@@ -40,7 +40,7 @@ func (m *ConfigRequest) BindPort(name string, value uint16) error {
 // ListPorts lists all the ports exposed by the config
 func (m *ConfigRequest) ListPorts() []a2conf.PortInfo {
 	return []a2conf.PortInfo{a2conf.PortInfo{
-		Default:  uint16(int32(14001)),
+		Default:  uint16(int32(10105)),
 		Name:     "service",
 		Protocol: "grpc",
 	}}
