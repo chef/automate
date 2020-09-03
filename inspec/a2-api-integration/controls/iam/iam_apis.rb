ac34523b59c97d8783a1a0664cba40b5b494ddba
Michael Sorens <msorens@chef.io>
2020-09-03 15:21:00 -0700

    Start at fixing integration tests
    Add relevant project passed in the header.
    Test: inspec exec inspec/a2-api-integration --controls=iam-api-1
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- inspec/a2-api-integration/controls/iam/iam_apis.rb
+++ inspec/a2-api-integration/controls/iam/iam_apis.rb
@@ -862,7 +862,8 @@ control 'iam-api-1' do
         it "creates a new rule" do
           resp = automate_api_request("/apis/iam/v2/projects/#{custom_project_id}/rules",
             http_method: 'POST',
-            request_body: CUSTOM_RULE.to_json
+            request_body: CUSTOM_RULE.to_json,
+            request_headers: { projects: [ custom_project_id ] }
           )
           expect(resp.http_status).to eq 200
           expect(resp.parsed_response_body[:rule]).to eq(staged_custom_rule)
@@ -947,19 +948,22 @@ control 'iam-api-1' do
 
         resp = automate_api_request("/apis/iam/v2/projects/#{custom_project_id}/rules",
           http_method: 'POST',
-          request_body: CUSTOM_RULE_1.to_json
+          request_body: CUSTOM_RULE_1.to_json,
+          request_headers: { projects: [ custom_project_id ] }
         )
         expect(resp.http_status).to eq 200
 
         resp = automate_api_request("/apis/iam/v2/projects/#{custom_project_id}/rules",
           http_method: 'POST',
-          request_body: CUSTOM_RULE_2.to_json
+          request_body: CUSTOM_RULE_2.to_json,
+          request_headers: { projects: [ custom_project_id ] }
         )
         expect(resp.http_status).to eq 200
 
         resp = automate_api_request("/apis/iam/v2/projects/#{custom_project_id_2}/rules",
           http_method: 'POST',
-          request_body: CUSTOM_RULE_3.to_json
+          request_body: CUSTOM_RULE_3.to_json,
+          request_headers: { projects: [ custom_project_id_2 ] }
         )
         expect(resp.http_status).to eq 200
       end
