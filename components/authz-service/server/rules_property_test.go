e516c66ee7e305ec8a36c6476edfc93f236a94aa
Michael Sorens <msorens@chef.io>
2020-09-03 19:34:04 -0700

    Revert "Re-sync property tests for CreateRule"
    This reverts commit 4f9c7958652707f5da73d13ae7e202e09d3a47b3.

--- components/authz-service/server/rules_property_test.go
+++ components/authz-service/server/rules_property_test.go
@@ -37,7 +37,8 @@ type projectAndRuleReq struct {
 var createRuleReqGen, createProjectReqGen, createProjectAndRulesGen, createProjectsAndRulesGen = getGenerators()
 
 func TestCreateRuleProperties(t *testing.T) {
-	ctx := genContext()
+	ctx := auth_context.NewOutgoingContext(auth_context.NewContext(context.Background(),
+		[]string{SuperuserSubject}, []string{}, "*", "*"))
 
 	projectClient, policyClient, testDB, store, seed := testhelpers.SetupProjectsAndRulesWithDB(t)
 	properties := getGopterParams(seed)
@@ -73,7 +74,6 @@ func TestCreateRuleProperties(t *testing.T) {
 				if err != nil {
 					return reportErrorAndYieldFalse(t, err)
 				}
-				ctx = insertProjectsIntoContext(ctx, []string{reqs.CreateProjectReq.Id})
 
 				if _, err := projectClient.CreateRule(ctx, &reqs.rules[0]); err != nil {
 					return reportErrorAndYieldFalse(t, err)
@@ -101,7 +101,8 @@ func TestCreateRuleProperties(t *testing.T) {
 }
 
 func TestGetRuleProperties(t *testing.T) {
-	ctx := genContext()
+	ctx := auth_context.NewOutgoingContext(auth_context.NewContext(context.Background(),
+		[]string{SuperuserSubject}, []string{}, "*", "*"))
 	projectClient, policyClient, testDB, store, seed := testhelpers.SetupProjectsAndRulesWithDB(t)
 	defer testDB.CloseDB(t)
 	defer store.Close()
@@ -711,15 +712,6 @@ func genStandardProjectAndRules(project api.CreateProjectReq, rules []api.Create
 	}
 }
 
-func genContext() context.Context {
-	return auth_context.NewOutgoingContext(auth_context.NewContext(context.Background(),
-		[]string{SuperuserSubject}, []string{}, "*", "*"))
-}
-
-func insertProjectsIntoContext(ctx context.Context, projects []string) context.Context {
-	return auth_context.NewOutgoingContext(auth_context.NewContext(ctx, []string{SuperuserSubject}, projects, "*", "*"))
-}
-
 func reportErrorAndYieldFalse(t *testing.T, err error) bool {
 	t.Helper()
 	t.Error(err.Error())
@@ -795,7 +787,6 @@ func createProjectAndRule(
 	if err != nil {
 		return nil, err
 	}
-	ctx = insertProjectsIntoContext(ctx, []string{reqs.CreateProjectReq.Id})
 
 	rule, err := projClient.CreateRule(ctx, &reqs.rules[0])
 	if err != nil {
