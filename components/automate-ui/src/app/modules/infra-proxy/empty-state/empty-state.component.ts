dde7a515a88475df101be1ef24061c8152dc90b3
sachin-msys <sbacchav@msystechnologies.com>
2020-09-23 18:55:14 +0530

    introduced empty state ui component
    Signed-off-by: sachin-msys <sbacchav@msystechnologies.com>

---
+++ components/automate-ui/src/app/modules/infra-proxy/empty-state/empty-state.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-empty-state',
+  templateUrl: './empty-state.component.html',
+  styleUrls: ['./empty-state.component.scss']
+})
+
+
+
+export class EmptyStateComponent {
+    @Input() moduleTitle: string;
+    @Output() resetKeyRedirection = new EventEmitter<boolean>();
+
+    ResetKeyTabRedirection() {
+        this.resetKeyRedirection.emit(true);
+    }
+}
