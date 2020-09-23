3d5bc8b9940edb61a7c7d8e7cf8bc657bc66386e
sachin-msys <sbacchav@msystechnologies.com>
2020-09-23 18:52:40 +0530

    add interceptor skip header
    Signed-off-by: sachin-msys <sbacchav@msystechnologies.com>

--- components/automate-ui/src/app/entities/data-bags/data-bags.requests.ts
+++ components/automate-ui/src/app/entities/data-bags/data-bags.requests.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment as env } from 'environments/environment';
 import { DataBagsSuccessPayload } from './data-bags.actions';
 import { DataBagDetailsSuccessPayload } from './data-bag-details.actions';
 import { DataBagsItemDetails } from './data-bags.model';
+import { InterceptorSkipHeader } from 'app/services/http/http-client-auth.interceptor';
+
+const headers = new HttpHeaders().set(InterceptorSkipHeader, '');
 
 @Injectable()
 export class DataBagsRequests {
@@ -13,18 +16,22 @@ export class DataBagsRequests {
 
   public getDataBags(server_id: string, org_id: string): Observable<DataBagsSuccessPayload> {
     return this.http.get<DataBagsSuccessPayload>(
-      `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/data_bags`);
+      `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/data_bags`, {headers});
   }
 
   public getDataBagDetails(server_id: string, org_id: string, name: string)
   : Observable<DataBagDetailsSuccessPayload> {
     return this.http.get<DataBagDetailsSuccessPayload>(
-      `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/data_bags?name=${name}`);
+      `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/data_bags?name=${name}`,
+      {headers}
+    );
   }
 
   public getDataBagItemDetails(server_id: string, org_id: string, name: string, item_name: string)
   : Observable<DataBagsItemDetails> {
     return this.http.get<DataBagsItemDetails>(
-      `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/data_bags/${name}/${item_name}`);
+      `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/data_bags/${name}/${item_name}`,
+      {headers}
+    );
   }
 }
--- components/automate-ui/src/app/entities/policy-files/policy-file.requests.ts
+++ components/automate-ui/src/app/entities/policy-files/policy-file.requests.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment as env } from 'environments/environment';
 import { PolicyFilesSuccessPayload } from './policy-file.action';
+import { InterceptorSkipHeader } from 'app/services/http/http-client-auth.interceptor';
+
+const headers = new HttpHeaders().set(InterceptorSkipHeader, '');
 
 @Injectable()
 export class PolicyFileRequests {
@@ -12,6 +15,6 @@ export class PolicyFileRequests {
   public getPolicyFiles(server_id: string, org_id: string):
     Observable<PolicyFilesSuccessPayload> {
     return this.http.get<PolicyFilesSuccessPayload>(
-      `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/policyfiles`);
+      `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/policyfiles`, {headers});
   }
 }
