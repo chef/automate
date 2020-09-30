12c2e4c4d79b7746629848fd5ef75d3f1f0f9972
Jamie D <jamie.degnan@gmail.com>
2020-09-30 06:56:46 -0700

    Add modal prompt to delete integration and associated nodes (#4348)
    * Modal added and deletion updated - might move modal into its own component
    * Simplify handleDelete
    * Add some simple tests at the component level
    * Updated modal to use a previously build component made for deleting things
    * Revert back to chef-modal in template since the internals needed to be changed
    * Lint fixes
    * Remove a bunch of .. notation
    * Update test to show baseline
    * Refactor modal to use already build delete-object-modal, add custom property, cleanup
    * html indentation
    
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/pages/integrations/list/integrations-list.component.spec.ts
+++ components/automate-ui/src/app/pages/integrations/list/integrations-list.component.spec.ts
@@ -1,16 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
 
-import { runtimeChecks, ngrxReducers } from 'app/ngrx.reducers';
-import { ChefPipesModule } from '../../../pipes/chef-pipes.module';
+import { runtimeChecks, ngrxReducers, NgrxStateAtom } from 'app/ngrx.reducers';
+import { ChefPipesModule } from 'app/pipes/chef-pipes.module';
 import { FeatureFlagsService } from 'app/services/feature-flags/feature-flags.service';
 import { IntegrationsListComponent } from './integrations-list.component';
+import { MatOptionSelectionChange } from '@angular/material/core/option';
+import { DeleteManager } from 'app/entities/managers/manager.actions';
 
 describe('IntegrationsListComponent', () => {
   let component: IntegrationsListComponent;
   let fixture: ComponentFixture<IntegrationsListComponent>;
+  let store: Store<NgrxStateAtom>;
+  const MockSelectionEvent: MatOptionSelectionChange = { isUserInput: true, source: null };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -43,6 +47,7 @@ describe('IntegrationsListComponent', () => {
       ]
     })
     .compileComponents();
+    store = TestBed.inject(Store);
   }));
 
   beforeEach(() => {
@@ -53,5 +58,29 @@ describe('IntegrationsListComponent', () => {
 
   it('exists', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('deleting a node manager', () => {
+    it('displays a modal prompt when user selects Delete Integration from the dropdown', () => {
+      expect(component.deletePromptVisible).toBe(false);
+      component.beginDelete(MockSelectionEvent, '12345');
+
+      expect(component.deletePromptVisible).toBe(true);
+    });
+
+    it('stores the id of the node manager to be deleted', () => {
+      expect(component.managerIdForDeletion).toBe('');
+
+      component.beginDelete(MockSelectionEvent, '12ebd34');
+      expect(component.managerIdForDeletion).toBe('12ebd34');
+    });
+
+    it('deletes the selected node manaager', () => {
+      spyOn(store, 'dispatch');
+      component.managerIdForDeletion = 'abcde';
+      component.handleDelete();
+
+      expect(store.dispatch).toHaveBeenCalledWith(new DeleteManager({ id: 'abcde' }));
+    });
   });
 });
