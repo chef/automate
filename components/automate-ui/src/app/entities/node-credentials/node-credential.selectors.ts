aeb328e3836d64447a52b7307036abf91b590aa2
samshinde <ashinde@chef.io>
2020-10-05 16:18:12 +0530

    Added consistency improvement in node credentials.
    Signed-off-by: samshinde <ashinde@chef.io>

---
+++ components/automate-ui/src/app/entities/node-credentials/node-credential.selectors.ts
@@ -0,0 +1,54 @@
+import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { NodeCredentialEntityState, nodeCredentialEntityAdapter } from './node-credential.reducer';
+import { routeParams } from 'app/route.selectors';
+import { find } from 'lodash/fp';
+// import { filter, includes } from 'lodash/fp';
+
+export const nodeCredentialState =
+createFeatureSelector<NodeCredentialEntityState>('nodeCredential');
+
+export const {
+  selectAll: allCredentials,
+  selectEntities: credentialEntities
+} = nodeCredentialEntityAdapter.getSelectors(nodeCredentialState);
+
+export const nodeCredentialStatus = createSelector(
+  nodeCredentialState,
+  (state) => state.nodeCredentialsStatus
+);
+
+export const getAllStatus = createSelector(
+  nodeCredentialState,
+  (state) => state.getAllStatus
+);
+
+export const saveStatus = createSelector(
+  nodeCredentialState,
+  (state) => state.saveStatus
+);
+
+export const saveError = createSelector(
+  nodeCredentialState,
+  (state) => state.saveError
+);
+
+export const getStatus = createSelector(
+  nodeCredentialState,
+  (state) => state.getStatus
+);
+
+export const nodeCredentialFromRoute = createSelector(
+  credentialEntities,
+  routeParams,
+  (state, { id }) => find({ id }, state)
+);
+
+export const updateStatus = createSelector(
+  nodeCredentialState,
+  (state) => state.updateStatus
+);
+
+export const deleteStatus = createSelector(
+  credentialEntities,
+  (state) => state.deleteStatus
+);
