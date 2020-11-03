b9e680d56a4b913a1f8619cf63915b80b7973075
Vivek Singh <vivek.singh@msystechnologies.com>
2020-11-03 12:24:12 +0530

    revert re-arrange proto attribute changes
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- components/automate-gateway/api/infra_proxy.pb.swagger.go
+++ components/automate-gateway/api/infra_proxy.pb.swagger.go
@@ -2688,14 +2688,6 @@ func init() {
           "type": "string",
           "description": "Node name."
         },
-        "fqdn": {
-          "type": "string",
-          "description": "Node FQDN."
-        },
-        "ip_address": {
-          "type": "string",
-          "description": "Node IP address."
-        },
         "check_in": {
           "type": "string",
           "description": "Node last checkin."
@@ -2715,6 +2707,14 @@ func init() {
         "policy_group": {
           "type": "string",
           "description": "Node policy group."
+        },
+        "fqdn": {
+          "type": "string",
+          "description": "Node FQDN."
+        },
+        "ip_address": {
+          "type": "string",
+          "description": "Node IP address."
         }
       }
     },
