9e64a71720f854dfab2b5ba738f12578e8606e02
Rick Marry <rick.marry@gmail.com>
2020-11-09 20:26:32 -0500

    temp-patch vagrant file

---
+++ ec2/temp-patch.rb
@@ -0,0 +1,11 @@
+# https://github.com/mitchellh/vagrant-aws/issues/566
+class Hash
+  def slice(*keep_keys)
+    h = {}
+    keep_keys.each { |key| h[key] = fetch(key) if has_key?(key) }
+    h
+  end unless Hash.method_defined?(:slice)
+  def except(*less_keys)
+    slice(*keys - less_keys)
+  end unless Hash.method_defined?(:except)
+end
