3d30fc330f0881f23a2df2fb0a5225fd33a623a7
Michael Sorens <msorens@chef.io>
2020-11-13 16:43:02 -0800

    Introduce rxjs-spy diagnostic tool to UI (#4434)

--- components/automate-ui/src/main.ts
+++ components/automate-ui/src/main.ts
@@ -1,5 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { create } from 'rxjs-spy';
+import { CyclePlugin } from 'rxjs-spy/plugin';
 
 import { applyPolyfills, defineCustomElements } from './assets/chef-ui-library/loader';
 
@@ -8,6 +10,22 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+} else {
+  // This provides rxjs-spy access in the browser console,
+  // which supports analyzing, debugging, and logging any tagged Observables.
+  // Just execute `enableRxjsSpy()` in the console, then apply commands from
+  // https://github.com/cartant/rxjs-spy#module-log
+  (window as any).enableRxjsSpy = () => {
+
+    const spy = create();
+
+    // suppress "cycle-plugin.js:49 Cyclic next detected" warning in browser console
+    // reference: https://github.com/cartant/rxjs-spy/issues/41
+    spy.unplug(spy.find(CyclePlugin));
+
+    // not strictly necessary, but let's you see what is in the object at runtime
+    return spy;
+  };
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule, { preserveWhitespaces: true });
