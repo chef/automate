715851cd70c9a3109924ecb71ef3aac91ae06dc5
Prajakta Purohit <prajakta@chef.io>
2020-12-10 10:59:09 -0800

    Update to the latest version of bookshelf
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- components/automate-cs-bookshelf/habitat/plan.sh
+++ components/automate-cs-bookshelf/habitat/plan.sh
@@ -15,7 +15,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/bookshelf/14.0.40/20201203131325"
+  "${vendor_origin}/bookshelf/14.0.72/20201210135534"
 )
 
 pkg_binds=(
