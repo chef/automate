bab940f795022575727b1ba88119e7310621dfed
chaitali-mane <cmane@msystechnologies.com>
2021-01-29 18:55:22 +0530

    added search bar to clients

--- components/automate-ui/src/app/entities/clients/client.requests.ts
+++ components/automate-ui/src/app/entities/clients/client.requests.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment as env } from 'environments/environment';
-import { ClientsSuccessPayload } from './client.action';
+import { ClientsSuccessPayload, ClientSearchPayload } from './client.action';
 import { Client } from './client.model';
 import { InterceptorSkipHeader } from 'app/services/http/http-client-auth.interceptor';
 
 const headers = new HttpHeaders().set(InterceptorSkipHeader, '');
+
+export interface ClientSearchResponse {
+  nodes: any[];
+  total: number;
+}
 
 @Injectable()
 export class ClientRequests {
@@ -22,5 +27,21 @@ export class ClientRequests {
   public getClient(server_id: string, org_id: string, name: string): Observable<Client> {
     return this.http.get<Client>(
         `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/clients/${name}`, {headers});
+  }
+
+  public getClientSearch(payload: ClientSearchPayload)
+  : Observable<ClientSearchResponse> {
+    return this.http.get<ClientSearchResponse>(
+      `${env.infra_proxy_url}/servers/${payload.server_id}/orgs/${payload.org_id}/clients?search_query.${payload.query}=name:${payload.clientId}`,
+      {headers}
+    );
+  }
+  // ?search_query.q=id:root
+  public getClientDetails(server_id: string, org_id: string, name: string, item_name: string)
+  : Observable<Client> {
+    return this.http.get<Client>(
+      `${env.infra_proxy_url}/servers/${server_id}/orgs/${org_id}/clients/${name}/${item_name}`,
+      {headers}
+    );
   }
 }
