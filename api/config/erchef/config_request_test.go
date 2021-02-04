ee25ff3292bf84e546302d22330bb48cf22e9850
Prajakta Purohit <prajakta@chef.io>
2021-02-04 06:00:27 -0800

    Set data_collector to true only if external automate or local automate is enabled.
    Co-authored-by: Jay Mundrawala <jmundrawala@chef.io>
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- api/config/erchef/config_request_test.go
+++ api/config/erchef/config_request_test.go
@@ -3,11 +3,42 @@ package erchef
 import (
 	"testing"
 
+	"github.com/chef/automate/api/config/shared"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	w "github.com/chef/automate/api/config/shared/wrappers"
 )
 
 func TestValidateConfigRequestValid(t *testing.T) {
 	c := NewConfigRequest()
 	err := c.Validate()
 	assert.Nil(t, err)
+}
+
+func TestDataCollector(t *testing.T) {
+	t.Run("enabled when External Automate is enabled and Internal Automate is disabled", func(t *testing.T) {
+		c := DefaultConfigRequest()
+		c.V1.Sys.ExternalAutomate = &shared.External_Automate{
+			Enable: w.Bool(true),
+		}
+		c.ConfigureProduct(&shared.ProductConfig{
+			Products: []string{"chef-server"},
+		})
+		require.True(t, c.V1.Sys.GetDataCollector().GetEnabled().GetValue())
+	})
+	t.Run("enabled when External Automate is disabled and Internal Automate is enabled", func(t *testing.T) {
+		c := DefaultConfigRequest()
+		c.ConfigureProduct(&shared.ProductConfig{
+			Products: []string{"automate", "chef-server"},
+		})
+		require.True(t, c.V1.Sys.GetDataCollector().GetEnabled().GetValue())
+	})
+	t.Run("disabled when External Automate is disabled and Internal Automate is disabled", func(t *testing.T) {
+		c := DefaultConfigRequest()
+		c.ConfigureProduct(&shared.ProductConfig{
+			Products: []string{"chef-server"},
+		})
+		require.False(t, c.V1.Sys.GetDataCollector().GetEnabled().GetValue())
+	})
 }
