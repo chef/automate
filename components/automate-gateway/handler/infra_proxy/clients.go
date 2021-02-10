2d638a17907de50fc0561ff03db40989e952ca02
Vivek Singh <vivek.singh@msystechnologies.com>
2021-02-11 05:25:08 +0530

    Rebase changes and comments update
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- components/automate-gateway/handler/infra_proxy/clients.go
+++ components/automate-gateway/handler/infra_proxy/clients.go
@@ -1,126 +1,127 @@
 package infra_proxy
 
 import (
-    "context"
+	"context"
 
-    gwreq "github.com/chef/automate/api/external/infra_proxy/request"
-    gwres "github.com/chef/automate/api/external/infra_proxy/response"
-    infra_req "github.com/chef/automate/api/interservice/infra_proxy/request"
-    infra_res "github.com/chef/automate/api/interservice/infra_proxy/response"
+	gwreq "github.com/chef/automate/api/external/infra_proxy/request"
+	gwres "github.com/chef/automate/api/external/infra_proxy/response"
+	infra_req "github.com/chef/automate/api/interservice/infra_proxy/request"
+	infra_res "github.com/chef/automate/api/interservice/infra_proxy/response"
 )
 
 // GetClients fetches an array of existing clients
 func (a *InfraProxyServer) GetClients(ctx context.Context, r *gwreq.Clients) (*gwres.Clients, error) {
-    req := &infra_req.Clients{
-        OrgId:    r.OrgId,
-        ServerId: r.ServerId,
-    }
-    res, err := a.client.GetClients(ctx, req)
-    if err != nil {
-        return nil, err
-    }
+	req := &infra_req.Clients{
+		OrgId:    r.OrgId,
+		ServerId: r.ServerId,
+	}
+	res, err := a.client.GetClients(ctx, req)
+	if err != nil {
+		return nil, err
+	}
 
-    return &gwres.Clients{
-        Clients: fromUpstreamClients(res.Clients),
-    }, nil
+	return &gwres.Clients{
+		Clients: fromUpstreamClients(res.Clients),
+	}, nil
 }
 
 // GetClient fetches an infra client details
 func (a *InfraProxyServer) GetClient(ctx context.Context, r *gwreq.Client) (*gwres.Client, error) {
-    req := &infra_req.Client{
-        OrgId:    r.OrgId,
-        ServerId: r.ServerId,
-        Name:     r.Name,
-    }
-    res, err := a.client.GetClient(ctx, req)
-    if err != nil {
-        return nil, err
-    }
+	req := &infra_req.Client{
+		OrgId:    r.OrgId,
+		ServerId: r.ServerId,
+		Name:     r.Name,
+	}
+	res, err := a.client.GetClient(ctx, req)
+	if err != nil {
+		return nil, err
+	}
 
-    return &gwres.Client{
-        Name:       res.GetName(),
-        ClientName: res.GetClientName(),
-        OrgName:    res.GetOrgName(),
-        Validator:  res.GetValidator(),
-        JsonClass:  res.GetJsonClass(),
-        ChefType:   res.GetChefType(),
-        ClientKey: &gwres.ClientAccessKey{
-            Name:           res.GetClientKey().GetName(),
-            ExpirationDate: res.GetClientKey().GetExpirationDate(),
-            PublicKey:      res.GetClientKey().GetPublicKey(),
-        },
-    }, nil
+	return &gwres.Client{
+		Name:       res.GetName(),
+		ClientName: res.GetClientName(),
+		OrgName:    res.GetOrgName(),
+		Validator:  res.GetValidator(),
+		JsonClass:  res.GetJsonClass(),
+		ChefType:   res.GetChefType(),
+		ClientKey: &gwres.ClientAccessKey{
+			Name:           res.GetClientKey().GetName(),
+			ExpirationDate: res.GetClientKey().GetExpirationDate(),
+			PublicKey:      res.GetClientKey().GetPublicKey(),
+		},
+	}, nil
 }
 
 // CreateClient creates an infra client
 func (a *InfraProxyServer) CreateClient(ctx context.Context, r *gwreq.CreateClient) (*gwres.CreateClient, error) {
-    req := &infra_req.CreateClient{
-        OrgId:     r.OrgId,
-        ServerId:  r.ServerId,
-        Name:      r.Name,
-        Validator: r.Validator,
-        CreateKey: r.CreateKey,
-    }
-    res, err := a.client.CreateClient(ctx, req)
-    if err != nil {
-        return nil, err
-    }
+	req := &infra_req.CreateClient{
+		OrgId:     r.OrgId,
+		ServerId:  r.ServerId,
+		Name:      r.Name,
+		Validator: r.Validator,
+		CreateKey: r.CreateKey,
+	}
+	res, err := a.client.CreateClient(ctx, req)
+	if err != nil {
+		return nil, err
+	}
 
-    return &gwres.CreateClient{
-        Name: res.GetName(),
-        ClientKey: &gwres.ClientKey{
-            Name:           res.GetClientKey().GetName(),
-            PublicKey:      res.GetClientKey().GetPublicKey(),
-            ExpirationDate: res.GetClientKey().GetExpirationDate(),
-            PrivateKey:     res.GetClientKey().GetPrivateKey(),
-        },
-    }, nil
+	return &gwres.CreateClient{
+		Name: res.GetName(),
+		ClientKey: &gwres.ClientKey{
+			Name:           res.GetClientKey().GetName(),
+			PublicKey:      res.GetClientKey().GetPublicKey(),
+			ExpirationDate: res.GetClientKey().GetExpirationDate(),
+			PrivateKey:     res.GetClientKey().GetPrivateKey(),
+		},
+	}, nil
 }
 
 // DeleteClient deletes an infra client
 func (a *InfraProxyServer) DeleteClient(ctx context.Context, r *gwreq.Client) (*gwres.Client, error) {
-    req := &infra_req.Client{
-        OrgId:    r.OrgId,
-        ServerId: r.ServerId,
-        Name:     r.Name,
-    }
-    res, err := a.client.DeleteClient(ctx, req)
-    if err != nil {
-        return nil, err
-    }
+	req := &infra_req.Client{
+		OrgId:    r.OrgId,
+		ServerId: r.ServerId,
+		Name:     r.Name,
+	}
+	res, err := a.client.DeleteClient(ctx, req)
+	if err != nil {
+		return nil, err
+	}
 
-    return &gwres.Client{
-        Name: res.GetName(),
-    }, nil
+	return &gwres.Client{
+		Name: res.GetName(),
+	}, nil
 }
 
 // ResetClientKey resets an infra client key
+// Deletes the associated key pair and generates new key pair again, and then attached it to the client.
 func (a *InfraProxyServer) ResetClientKey(ctx context.Context, r *gwreq.ClientKey) (*gwres.ClientKey, error) {
-    req := &infra_req.ClientKey{
-        OrgId:    r.OrgId,
-        ServerId: r.ServerId,
-        Name:     r.Name,
-        Key:      r.Key,
-    }
-    res, err := a.client.ResetClientKey(ctx, req)
-    if err != nil {
-        return nil, err
-    }
+	req := &infra_req.ClientKey{
+		OrgId:    r.OrgId,
+		ServerId: r.ServerId,
+		Name:     r.Name,
+		Key:      r.Key,
+	}
+	res, err := a.client.ResetClientKey(ctx, req)
+	if err != nil {
+		return nil, err
+	}
 
-    return &gwres.ClientKey{
-        Name:       res.GetName(),
-        PrivateKey: res.GetPrivateKey(),
-    }, nil
+	return &gwres.ClientKey{
+		Name:       res.GetName(),
+		PrivateKey: res.GetPrivateKey(),
+	}, nil
 }
 
 func fromUpstreamClients(clients []*infra_res.ClientListItem) []*gwres.ClientListItem {
-    ts := make([]*gwres.ClientListItem, len(clients))
+	ts := make([]*gwres.ClientListItem, len(clients))
 
-    for i, c := range clients {
-        ts[i] = &gwres.ClientListItem{
-            Name: c.GetName(),
-        }
-    }
+	for i, c := range clients {
+		ts[i] = &gwres.ClientListItem{
+			Name: c.GetName(),
+		}
+	}
 
-    return ts
+	return ts
 }
--- components/infra-proxy-service/server/clients.go
+++ components/infra-proxy-service/server/clients.go
@@ -14,210 +14,210 @@ import (
 
 // AccessKeyReq struct to add key.
 type AccessKeyReq struct {
-    Name           string `json:"name,omitempty"`
-    PublicKey      string `json:"public_key,omitempty"`
-    ExpirationDate string `json:"expiration_date,omitempty"`
-    CreateKey      bool   `json:"create_key,omitempty"`
+	Name           string `json:"name,omitempty"`
+	PublicKey      string `json:"public_key,omitempty"`
+	ExpirationDate string `json:"expiration_date,omitempty"`
+	CreateKey      bool   `json:"create_key,omitempty"`
 }
 
 // GetClients get clients list
 func (s *Server) GetClients(ctx context.Context, req *request.Clients) (*response.Clients, error) {
-    c, err := s.createClient(ctx, req.OrgId, req.ServerId)
-    if err != nil {
-        return nil, err
-    }
+	c, err := s.createClient(ctx, req.OrgId, req.ServerId)
+	if err != nil {
+		return nil, err
+	}
 
-    clients, err := c.client.Clients.List()
-    if err != nil {
-        return nil, ParseAPIError(err)
-    }
+	clients, err := c.client.Clients.List()
+	if err != nil {
+		return nil, ParseAPIError(err)
+	}
 
-    return &response.Clients{
-        Clients: fromAPIToListClients(clients),
-    }, nil
+	return &response.Clients{
+		Clients: fromAPIToListClients(clients),
+	}, nil
 }
 
 // GetClient gets the client
 func (s *Server) GetClient(ctx context.Context, req *request.Client) (*response.Client, error) {
-    c, err := s.createClient(ctx, req.OrgId, req.ServerId)
-    if err != nil {
-        return nil, err
-    }
+	c, err := s.createClient(ctx, req.OrgId, req.ServerId)
+	if err != nil {
+		return nil, err
+	}
 
-    ic, err := c.client.Clients.Get(req.Name)
-    if err != nil {
-        return nil, ParseAPIError(err)
-    }
+	ic, err := c.client.Clients.Get(req.Name)
+	if err != nil {
+		return nil, ParseAPIError(err)
+	}
 
-    // Fetch associated default client key detail
-    // Ignore if any error while fetching this.
-    var clientKey response.ClientAccessKey
-    chefKey, err := c.client.Clients.GetKey(req.Name, "default")
-    if err == nil {
-        clientKey = response.ClientAccessKey{
-            Name:           chefKey.Name,
-            ExpirationDate: chefKey.ExpirationDate,
-            PublicKey:      chefKey.PublicKey,
-        }
-    }
+	// Fetch associated default client key detail
+	// Ignore if any error while fetching this.
+	var clientKey response.ClientAccessKey
+	chefKey, err := c.client.Clients.GetKey(req.Name, "default")
+	if err == nil {
+		clientKey = response.ClientAccessKey{
+			Name:           chefKey.Name,
+			ExpirationDate: chefKey.ExpirationDate,
+			PublicKey:      chefKey.PublicKey,
+		}
+	}
 
-    return &response.Client{
-        Name:       ic.Name,
-        ClientName: ic.ClientName,
-        OrgName:    ic.OrgName,
-        Validator:  ic.Validator,
-        JsonClass:  ic.JsonClass,
-        ChefType:   ic.ChefType,
-        ClientKey:  &clientKey,
-    }, nil
+	return &response.Client{
+		Name:       ic.Name,
+		ClientName: ic.ClientName,
+		OrgName:    ic.OrgName,
+		Validator:  ic.Validator,
+		JsonClass:  ic.JsonClass,
+		ChefType:   ic.ChefType,
+		ClientKey:  &clientKey,
+	}, nil
 
 }
 
 // CreateClient creates the client
 func (s *Server) CreateClient(ctx context.Context, req *request.CreateClient) (*response.CreateClient, error) {
-    err := validation.New(validation.Options{
-        Target:  "client",
-        Request: *req,
-        Rules: validation.Rules{
-            "OrgId":    []string{"required"},
-            "ServerId": []string{"required"},
-            "Name":     []string{"required"},
-        },
-    }).Validate()
+	err := validation.New(validation.Options{
+		Target:  "client",
+		Request: *req,
+		Rules: validation.Rules{
+			"OrgId":    []string{"required"},
+			"ServerId": []string{"required"},
+			"Name":     []string{"required"},
+		},
+	}).Validate()
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    c, err := s.createClient(ctx, req.OrgId, req.ServerId)
-    if err != nil {
-        return nil, err
-    }
+	c, err := s.createClient(ctx, req.OrgId, req.ServerId)
+	if err != nil {
+		return nil, err
+	}
 
-    res, err := c.client.Clients.Create(chef.ApiNewClient{
-        Name:      req.Name,
-        Validator: req.Validator,
-        CreateKey: req.CreateKey,
-    })
-    if err != nil {
-        return nil, ParseAPIError(err)
-    }
+	res, err := c.client.Clients.Create(chef.ApiNewClient{
+		Name:      req.Name,
+		Validator: req.Validator,
+		CreateKey: req.CreateKey,
+	})
+	if err != nil {
+		return nil, ParseAPIError(err)
+	}
 
-    return &response.CreateClient{
-        Name: req.Name,
-        ClientKey: &response.ClientKey{
-            Name:           res.ChefKey.Name,
-            PublicKey:      res.ChefKey.PublicKey,
-            ExpirationDate: res.ChefKey.ExpirationDate,
-            PrivateKey:     res.ChefKey.PrivateKey,
-        },
-    }, nil
+	return &response.CreateClient{
+		Name: req.Name,
+		ClientKey: &response.ClientKey{
+			Name:           res.ChefKey.Name,
+			PublicKey:      res.ChefKey.PublicKey,
+			ExpirationDate: res.ChefKey.ExpirationDate,
+			PrivateKey:     res.ChefKey.PrivateKey,
+		},
+	}, nil
 
 }
 
 // DeleteClient deletes the client
 func (s *Server) DeleteClient(ctx context.Context, req *request.Client) (*response.Client, error) {
-    err := validation.New(validation.Options{
-        Target:          "client",
-        Request:         *req,
-        RequiredDefault: true,
-    }).Validate()
+	err := validation.New(validation.Options{
+		Target:          "client",
+		Request:         *req,
+		RequiredDefault: true,
+	}).Validate()
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    c, err := s.createClient(ctx, req.OrgId, req.ServerId)
-    if err != nil {
-        return nil, err
-    }
+	c, err := s.createClient(ctx, req.OrgId, req.ServerId)
+	if err != nil {
+		return nil, err
+	}
 
-    err = c.client.Clients.Delete(req.Name)
-    if err != nil {
-        return nil, ParseAPIError(err)
-    }
+	err = c.client.Clients.Delete(req.Name)
+	if err != nil {
+		return nil, ParseAPIError(err)
+	}
 
-    return &response.Client{
-        Name: req.Name,
-    }, nil
+	return &response.Client{
+		Name: req.Name,
+	}, nil
 
 }
 
 // ResetClientKey resets the client key
 // Deletes the associated key pair and generates new key pair again, and then attached it to the client.
 func (s *Server) ResetClientKey(ctx context.Context, req *request.ClientKey) (*response.ClientKey, error) {
-    err := validation.New(validation.Options{
-        Target:  "client",
-        Request: *req,
-        Rules: validation.Rules{
-            "OrgId":    []string{"required"},
-            "ServerId": []string{"required"},
-            "Name":     []string{"required"},
-        },
-    }).Validate()
+	err := validation.New(validation.Options{
+		Target:  "client",
+		Request: *req,
+		Rules: validation.Rules{
+			"OrgId":    []string{"required"},
+			"ServerId": []string{"required"},
+			"Name":     []string{"required"},
+		},
+	}).Validate()
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    c, err := s.createClient(ctx, req.OrgId, req.ServerId)
-    if err != nil {
-        return nil, err
-    }
+	c, err := s.createClient(ctx, req.OrgId, req.ServerId)
+	if err != nil {
+		return nil, err
+	}
 
-    key := req.Key
+	key := req.Key
 
-    if key == "" {
-        key = "default"
-    }
+	if key == "" {
+		key = "default"
+	}
 
-    // Deletes the existing key
-    _, err = c.client.Clients.DeleteKey(req.Name, key)
-    chefError, _ := chef.ChefError(err)
-    if err != nil && chefError.StatusCode() != 404 {
-        return nil, ParseAPIError(err)
-    }
+	// Deletes the existing key
+	_, err = c.client.Clients.DeleteKey(req.Name, key)
+	chefError, _ := chef.ChefError(err)
+	if err != nil && chefError.StatusCode() != 404 {
+		return nil, ParseAPIError(err)
+	}
 
-    // Add new key to existing client
-    body, err := chef.JSONReader(AccessKeyReq{
-        Name:           key,
-        ExpirationDate: "infinity",
-        CreateKey:      true,
-    })
+	// Add new key to existing client
+	body, err := chef.JSONReader(AccessKeyReq{
+		Name:           key,
+		ExpirationDate: "infinity",
+		CreateKey:      true,
+	})
 
-    var chefKey chef.ChefKey
-    addReq, err := c.client.NewRequest("POST", fmt.Sprintf("clients/%s/keys", req.Name), body)
-    res, err := c.client.Do(addReq, &chefKey)
-    if res != nil {
-        defer res.Body.Close()
-    }
+	var chefKey chef.ChefKey
+	addReq, err := c.client.NewRequest("POST", fmt.Sprintf("clients/%s/keys", req.Name), body)
+	res, err := c.client.Do(addReq, &chefKey)
+	if res != nil {
+		defer res.Body.Close()
+	}
 
-    if err != nil {
-        return nil, ParseAPIError(err)
-    }
+	if err != nil {
+		return nil, ParseAPIError(err)
+	}
 
-    return &response.ClientKey{
-        Name:       key,
-        PrivateKey: chefKey.PrivateKey,
-    }, nil
+	return &response.ClientKey{
+		Name:       key,
+		PrivateKey: chefKey.PrivateKey,
+	}, nil
 
 }
 
 // fromAPIToListClients a response.Clients from a struct of ClientList
 func fromAPIToListClients(al chef.ApiClientListResult) []*response.ClientListItem {
-    cl := make([]*response.ClientListItem, len(al))
+	cl := make([]*response.ClientListItem, len(al))
 
-    index := 0
-    for c := range al {
-        cl[index] = &response.ClientListItem{
-            Name: c,
-        }
-        index++
-    }
+	index := 0
+	for c := range al {
+		cl[index] = &response.ClientListItem{
+			Name: c,
+		}
+		index++
+	}
 
-    sort.Slice(cl, func(i, j int) bool {
-        return cl[i].Name < cl[j].Name
-    })
+	sort.Slice(cl, func(i, j int) bool {
+		return cl[i].Name < cl[j].Name
+	})
 
-    return cl
+	return cl
 }
