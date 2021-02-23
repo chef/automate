0fb189fa52a73e7e7b450dcc4f267d4268f2acc8
Kallol Roy <karoy@progress.com>
2021-02-23 22:15:43 +0530

    revert compliance profiles to 20210113170230 (#4762)
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- components/compliance-service/habitat/plan.sh
+++ components/compliance-service/habitat/plan.sh
@@ -46,7 +46,7 @@ else
   # WARNING: chef/automate-compliance-profiles is managed by Expeditor
   # See .expeditor/update-compliance-profiles.sh for details
   pkg_deps+=(
-      chef/automate-compliance-profiles/1.0.0/20210210220805
+      chef/automate-compliance-profiles/1.0.0/20210113170230
   )
 fi
 
