129b765f0237359141cb44027b93080a824ee61d
Vivek Singh <vivek.singh@msystechnologies.com>
2021-03-05 11:20:00 +0530

    more tests updates
    Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

--- components/infra-proxy-service/integration_test/environments_test.go
+++ components/infra-proxy-service/integration_test/environments_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/chef/automate/api/interservice/infra_proxy/request"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 func TestGetEnvironments(t *testing.T) {
@@ -99,6 +100,160 @@ func TestGetEnvironments(t *testing.T) {
 		assert.Equal(t, "_default", res.Environments[0].GetName())
 	})
 
+}
+
+func TestGetEnvironment(t *testing.T) {
+	// rpc GetEnvironment (request.Environment) returns (response.Environment)
+	ctx := context.Background()
+	t.Run("when the environment exists, return the environment successfully", func(t *testing.T) {
+		name := fmt.Sprintf("chef-environment-%d", time.Now().Nanosecond())
+		creReq := &request.CreateEnvironment{
+			ServerId:    autoDeployedChefServerID,
+			OrgId:       autoDeployedChefOrganizationID,
+			Name:        name,
+			Description: "auto generate chef environment",
+			CookbookVersions: map[string]string{
+				"audit":       ">= 9.5.0",
+				"chef-client": "= 12.3.3",
+			},
+			DefaultAttributes: &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"attribute1": &structpb.Value{Kind: &structpb.Value_StringValue{StringValue: "value"}},
+				},
+			},
+		}
+		envRes, err := infraProxy.CreateEnvironment(ctx, creReq)
+		assert.NoError(t, err)
+		assert.NotNil(t, envRes)
+
+		req := &request.Environment{
+			ServerId: autoDeployedChefServerID,
+			OrgId:    autoDeployedChefOrganizationID,
+			Name:     name,
+		}
+		res, err := infraProxy.GetEnvironment(ctx, req)
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, name, res.GetName())
+		assert.Equal(t, "auto generate chef environment", res.GetDescription())
+		assert.Equal(t, creReq.CookbookVersions, res.CookbookVersions)
+	})
+}
+
+func TestCreateEnvironment(t *testing.T) {
+	// rpc GetEnvironment (request.Environment) returns (response.Environment)
+	ctx := context.Background()
+	t.Run("when the valid request submit, creates the environment successfully", func(t *testing.T) {
+		name := fmt.Sprintf("chef-environment-%d", time.Now().Nanosecond())
+		req := &request.CreateEnvironment{
+			ServerId:    autoDeployedChefServerID,
+			OrgId:       autoDeployedChefOrganizationID,
+			Name:        name,
+			Description: "auto generate chef environment",
+			CookbookVersions: map[string]string{
+				"audit":       ">= 9.5.0",
+				"chef-client": "= 12.3.3",
+			},
+			DefaultAttributes: &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"attribute1": &structpb.Value{Kind: &structpb.Value_StringValue{StringValue: "value"}},
+				},
+			},
+		}
+		res, err := infraProxy.CreateEnvironment(ctx, req)
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, name, res.GetName())
+		assert.Equal(t, "auto generate chef environment", res.GetDescription())
+		assert.Equal(t, req.CookbookVersions, res.CookbookVersions)
+	})
+}
+
+func TestUpdateEnvironment(t *testing.T) {
+	// rpc UpdateEnvironment (request.UpdateEnvironment) returns (response.Environment)
+	ctx := context.Background()
+	t.Run("when the valid request submit, updates the environment successfully", func(t *testing.T) {
+		name := fmt.Sprintf("chef-environment-%d", time.Now().Nanosecond())
+		req := &request.CreateEnvironment{
+			ServerId:    autoDeployedChefServerID,
+			OrgId:       autoDeployedChefOrganizationID,
+			Name:        name,
+			Description: "auto generate chef environment",
+			CookbookVersions: map[string]string{
+				"audit":       ">= 9.5.0",
+				"chef-client": "= 12.3.3",
+			},
+			DefaultAttributes: &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"attribute1": &structpb.Value{Kind: &structpb.Value_StringValue{StringValue: "value"}},
+				},
+			},
+		}
+		res, err := infraProxy.CreateEnvironment(ctx, req)
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, name, res.GetName())
+		assert.Equal(t, "auto generate chef environment", res.GetDescription())
+		assert.Equal(t, req.CookbookVersions, res.CookbookVersions)
+
+		updateReq := &request.UpdateEnvironment{
+			ServerId:    autoDeployedChefServerID,
+			OrgId:       autoDeployedChefOrganizationID,
+			Name:        name,
+			Description: "updated chef environment",
+			CookbookVersions: map[string]string{
+				"audit":       ">= 9.5.0",
+				"chef-client": "= 12.3.3",
+				"aix":         "> 1.3.3",
+			},
+			DefaultAttributes: &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"attribute1": &structpb.Value{Kind: &structpb.Value_StringValue{StringValue: "value"}},
+				},
+			},
+		}
+
+		updateEnv, err := infraProxy.UpdateEnvironment(ctx, updateReq)
+		assert.NoError(t, err)
+		assert.NotNil(t, updateEnv)
+		assert.Equal(t, name, updateEnv.GetName())
+		assert.Equal(t, updateReq.Description, updateEnv.GetDescription())
+		assert.Equal(t, updateReq.CookbookVersions, updateEnv.CookbookVersions)
+	})
+}
+
+func TestDeleteEnvironment(t *testing.T) {
+	// rpc DeleteEnvironment (request.Environment) returns (response.Environment)
+	ctx := context.Background()
+	t.Run("when the valid request submit, deletes the environment successfully", func(t *testing.T) {
+		name := fmt.Sprintf("chef-environment-%d", time.Now().Nanosecond())
+		req := &request.CreateEnvironment{
+			ServerId:    autoDeployedChefServerID,
+			OrgId:       autoDeployedChefOrganizationID,
+			Name:        name,
+			Description: "auto generate chef environment",
+			CookbookVersions: map[string]string{
+				"audit":       ">= 9.5.0",
+				"chef-client": "= 12.3.3",
+			},
+			DefaultAttributes: &structpb.Struct{
+				Fields: map[string]*structpb.Value{
+					"attribute1": &structpb.Value{Kind: &structpb.Value_StringValue{StringValue: "value"}},
+				},
+			},
+		}
+		res, err := infraProxy.CreateEnvironment(ctx, req)
+		assert.NoError(t, err)
+		assert.NotNil(t, res)
+		// Deletes the created environment
+		delRes, err := infraProxy.DeleteEnvironment(ctx, &request.Environment{
+			ServerId: autoDeployedChefServerID,
+			OrgId:    autoDeployedChefOrganizationID,
+			Name:     name,
+		})
+		assert.NoError(t, err)
+		assert.NotNil(t, delRes)
+	})
 }
 
 // Adds environments records
