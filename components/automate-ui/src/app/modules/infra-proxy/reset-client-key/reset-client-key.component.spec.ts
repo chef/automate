f975731ad2a39962a1f511583d285dccd81e1862
chaitali-mane <cmane@progress.com>
2021-03-11 12:16:33 +0530

    unit test issue and style changes
    Signed-off-by: chaitali-mane <cmane@progress.com>

--- components/automate-ui/src/app/modules/infra-proxy/reset-client-key/reset-client-key.component.spec.ts
+++ components/automate-ui/src/app/modules/infra-proxy/reset-client-key/reset-client-key.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockComponent } from 'ng2-mock-component';
 import { EventEmitter } from '@angular/core';
 import { ResetClientKeyComponent } from './reset-client-key.component';
+import { StoreModule } from '@ngrx/store';
+import { ngrxReducers, runtimeChecks } from 'app/ngrx.reducers';
 
 describe('ResetClientKeyComponent', () => {
   let component: ResetClientKeyComponent;
@@ -13,7 +15,10 @@ describe('ResetClientKeyComponent', () => {
         MockComponent({ selector: 'chef-modal', inputs: ['visible'] }),
         MockComponent({ selector: 'chef-button', inputs: ['disabled'] }),
         MockComponent({ selector: 'chef-loading-spinner'}),
-        ResetClientKeyComponent ]
+        ResetClientKeyComponent ],
+        imports: [
+          StoreModule.forRoot(ngrxReducers, { runtimeChecks })
+        ]
     })
     .compileComponents();
   });
