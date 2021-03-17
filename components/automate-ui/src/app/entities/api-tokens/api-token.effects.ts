889cfcc38b52b4199a26f06b8eae58cf109d9dd6
seajamied <jdegnan@chef.io>
2021-03-17 13:41:58 -0700

    Manually patch deprecation for @effect
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/entities/api-tokens/api-token.effects.ts
+++ components/automate-ui/src/app/entities/api-tokens/api-token.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { catchError, mergeMap, map, filter } from 'rxjs/operators';
 
@@ -39,72 +39,80 @@ export class ApiTokenEffects {
     private requests: ApiTokenRequests
   ) { }
 
-  @Effect()
-  getAllTokens$ = this.actions$.pipe(ofType<GetAllTokens>(ApiTokenActionTypes.GET_ALL),
+  getAllTokens$ = createEffect(() => {
+    return this.actions$.pipe(ofType<GetAllTokens>(ApiTokenActionTypes.GET_ALL),
     mergeMap((_action) =>
       this.requests.getAll().pipe(
         map(resp => new GetAllTokensSuccess(resp)),
         catchError((error: HttpErrorResponse) => of(new GetAllTokensFailure(error))))
     ));
+  });
 
-  @Effect()
-  getAllTokensFailure$ = this.actions$.pipe(
+  getAllTokensFailure$ = createEffect(() => {
+    return this.actions$.pipe(
     ofType<GetAllTokensFailure>(ApiTokenActionTypes.GET_ALL_FAILURE),
     map(({ payload: { error } }) => new CreateNotification({
       type: Type.error,
       message: `Could not get tokens: ${error.error || error}.`
     })));
+  });
 
-  @Effect()
-  getToken$ = this.actions$.pipe(ofType<GetToken>(ApiTokenActionTypes.GET),
+  getToken$ = createEffect(() => {
+    return this.actions$.pipe(ofType<GetToken>(ApiTokenActionTypes.GET),
     mergeMap(({ payload: { id } }: GetToken) =>
       this.requests.get(id).pipe(
         map(resp => new GetTokenSuccess(resp.token)),
         catchError((error: HttpErrorResponse) => of(new GetTokenFailure(error))))
     ));
+  });
 
-  @Effect()
-  getTokenFailure$ = this.actions$.pipe(
+  getTokenFailure$ = createEffect(() => {
+    return this.actions$.pipe(
     ofType<GetTokenFailure>(ApiTokenActionTypes.GET_FAILURE),
     map(({ payload: { error } }) => new CreateNotification({
       type: Type.error,
       message: `Could not get token: ${error.error || error}.`
     })));
+  });
 
-  @Effect()
-  updateToken$ = this.actions$.pipe(ofType<UpdateToken>(ApiTokenActionTypes.UPDATE),
+  updateToken$ = createEffect(() => {
+    return this.actions$.pipe(ofType<UpdateToken>(ApiTokenActionTypes.UPDATE),
     mergeMap(({ payload }: UpdateToken) =>
       this.requests.update(payload).pipe(
         map(resp => new UpdateTokenSuccess(resp.token)),
         catchError((error: HttpErrorResponse) => of(new UpdateTokenFailure(error))))
     ));
+  });
 
-  @Effect()
-  updateTokenFailure$ = this.actions$.pipe(
+  updateTokenFailure$ = createEffect(() => {
+    return this.actions$.pipe(
     ofType<UpdateTokenFailure>(ApiTokenActionTypes.UPDATE_FAILURE),
     map(({ payload: { error } }) => new CreateNotification({
       type: Type.error,
       message: `Could not update token: ${error.error || error}.`
     })));
+  });
 
-  @Effect()
-  createToken$ = this.actions$.pipe(ofType<CreateToken>(ApiTokenActionTypes.CREATE),
+  createToken$ = createEffect(() => {
+    return this.actions$.pipe(ofType<CreateToken>(ApiTokenActionTypes.CREATE),
     mergeMap(({ payload }: CreateToken) =>
       this.requests.create(payload).pipe(
         map(resp => new CreateTokenSuccess(resp.token)),
         catchError((error: HttpErrorResponse) => of(new CreateTokenFailure(error))))
     ));
+  });
 
-  @Effect()
-  createTokenSuccess$ = this.actions$.pipe(
+  createTokenSuccess$ = createEffect(() => {
+    return this.actions$.pipe(
     ofType<CreateTokenSuccess>(ApiTokenActionTypes.CREATE_SUCCESS),
     map(({ payload: { name } }) => new CreateNotification({
       type: Type.info,
       message: `Created token ${name}.`
     })));
+  });
 
-  @Effect()
-  createTokenFailure$ = this.actions$.pipe(
+  createTokenFailure$ = createEffect(() => {
+    return this.actions$.pipe(
     ofType<CreateTokenFailure>(ApiTokenActionTypes.CREATE_FAILURE),
     // ID conflict handled in the modal, see api-token-list.component.ts
     filter(({ payload: { status } }) => status !== HttpStatus.CONFLICT),
@@ -112,52 +120,59 @@ export class ApiTokenEffects {
       type: Type.error,
       message: `Could not create token: ${error.error || error}.`
     })));
+  });
 
-  @Effect()
-  toggleToken$ = this.actions$.pipe(ofType<ToggleTokenActive>(ApiTokenActionTypes.TOGGLE),
+  toggleToken$ = createEffect(() => {
+    return this.actions$.pipe(ofType<ToggleTokenActive>(ApiTokenActionTypes.TOGGLE),
     mergeMap(({ payload: { id, name, active } }: ToggleTokenActive) =>
       this.requests.toggleActive(id, name, active).pipe(
         map(resp => new ToggleTokenActiveSuccess(resp.token)),
         catchError((error: HttpErrorResponse) => of(new ToggleTokenActiveFailure(error))))
     ));
+  });
 
-  @Effect()
-  toggleTokenSuccess$ = this.actions$.pipe(
+  toggleTokenSuccess$ = createEffect(() => {
+    return this.actions$.pipe(
     ofType<ToggleTokenActiveSuccess>(ApiTokenActionTypes.TOGGLE_SUCCESS),
     map(({ payload: { name, active } }) => new CreateNotification({
       type: Type.info,
       message: `Set token ${name} to ${active ? 'active' : 'inactive'}.`
     })));
+  });
 
-  @Effect()
-  toggleTokenFailure$ = this.actions$.pipe(
+  toggleTokenFailure$ = createEffect(() => {
+    return this.actions$.pipe(
     ofType<ToggleTokenActiveFailure>(ApiTokenActionTypes.TOGGLE_FAILURE),
     map(({ payload: { error } }) => new CreateNotification({
       type: Type.error,
       message: `Could not toggle token state: ${error.error || error}.`
     })));
+  });
 
-  @Effect()
-  deleteToken$ = this.actions$.pipe(ofType<DeleteToken>(ApiTokenActionTypes.DELETE),
+  deleteToken$ = createEffect(() => {
+    return this.actions$.pipe(ofType<DeleteToken>(ApiTokenActionTypes.DELETE),
     mergeMap(({ payload }: DeleteToken) =>
       this.requests.delete(payload.id).pipe(
         map(() => new DeleteTokenSuccess(payload)),
         catchError((error: HttpErrorResponse) => of(new DeleteTokenFailure(error))))
     ));
+  });
 
-  @Effect()
-  deleteTokenSuccess$ = this.actions$.pipe(
+  deleteTokenSuccess$ = createEffect(() => {
+    return this.actions$.pipe(
     ofType<DeleteTokenSuccess>(ApiTokenActionTypes.DELETE_SUCCESS),
     map(({ payload: { name } }) => new CreateNotification({
       type: Type.info,
       message: `Deleted token ${name}.`
     })));
+  });
 
-  @Effect()
-  deleteTokenFailure$ = this.actions$.pipe(
+  deleteTokenFailure$ = createEffect(() => {
+    return this.actions$.pipe(
     ofType<DeleteTokenFailure>(ApiTokenActionTypes.DELETE_FAILURE),
     map(({ payload: { error } }: DeleteTokenFailure) => new CreateNotification({
       type: Type.error,
       message: `Could not delete token: ${error.error || error}.`
     })));
+  });
 }
