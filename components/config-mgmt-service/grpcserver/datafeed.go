e492abfd38e761655a9ba73b17ccf10b284e3bf7
shaik80 <sh.mudassir98@gmail.com>
2021-03-22 12:41:29 +0530

    inventoryNodes data

--- components/config-mgmt-service/grpcserver/datafeed.go
+++ components/config-mgmt-service/grpcserver/datafeed.go
@@ -3,20 +3,49 @@ package grpcserver
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	pRequest "github.com/chef/automate/api/interservice/cfgmgmt/request"
 	interserviceResp "github.com/chef/automate/api/interservice/cfgmgmt/response"
+	"github.com/golang/protobuf/ptypes"
 )
+
+type Datafeed struct {
+	FeedStart time.Time
+	FeedEnd   time.Time
+}
 
 func (s *CfgMgmtServer) Getdata(ctx context.Context, req *pRequest.GetPaginationRequest) (*interserviceResp.GetPaginationResponse, error) {
 
+	params := Datafeed{}
+	feedStartString, err := ptypes.TimestampProto(params.FeedStart)
+	if err != nil {
+		return nil, err
+	}
+	feedEndString, err := ptypes.TimestampProto(params.FeedEnd)
+	if err != nil {
+		return nil, err
+	}
+
+	nodesRequest := &pRequest.InventoryNodes{
+		PageSize: 300,
+		Start:    feedStartString,
+		End:      feedEndString,
+		Sorting: &pRequest.Sorting{
+			Order: pRequest.Order_DESC,
+		},
+	}
+
+	inventoryNodes, err := s.GetInventoryNodes(ctx, nodesRequest)
+	if err != nil {
+		return nil, err
+	}
 	out := map[string]interface{}{}
 	// data := datafeedServer.paginationData.Data
 	out["offset"] = req.GetOffset()
 	out["size"] = req.GetSize()
-
+	out["inventoryNodes"] = inventoryNodes
 	outputJSON, _ := json.Marshal(out)
-
 	returnData := interserviceResp.GetPaginationResponse{
 		Data: string(outputJSON),
 	}
