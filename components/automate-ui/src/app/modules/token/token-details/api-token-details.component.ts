5690e66283dca725a5a94d596fbd4389258b574b
seajamied <jdegnan@chef.io>
2021-04-01 23:21:34 -0700

    Dropping async observables into template
    Moving along nicely now, able to toggle the timezone in initial state
    and adjust in three areas.  Will pause now until backend is in place
    incase adjustments need to be made
    
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/modules/token/token-details/api-token-details.component.ts
+++ components/automate-ui/src/app/modules/token/token-details/api-token-details.component.ts
@@ -17,6 +17,9 @@ import {
 import { ApiToken } from 'app/entities/api-tokens/api-token.model';
 import { ProjectConstants } from 'app/entities/projects/project.model';
 import { GetProjects } from 'app/entities/projects/project.actions';
+import { UserPreferencesService } from 'app/services/user-preferences/user-preferences.service';
+
+
 
 type TokenStatus = 'active' | 'inactive';
 type TokenTabName = 'details';
@@ -40,7 +43,8 @@ export class ApiTokenDetailsComponent implements OnInit, OnDestroy {
   constructor(
     private store: Store<NgrxStateAtom>,
     fb: FormBuilder,
-    private layoutFacade: LayoutFacadeService
+    private layoutFacade: LayoutFacadeService,
+    public userPrefsService: UserPreferencesService
   ) {
     this.updateForm = fb.group({
       // Must stay in sync with error checks in api-token-details.component.html
