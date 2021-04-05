fa1caa519bf089efac51dbf0cfd2263a3269c9f9
seajamied <jdegnan@chef.io>
2021-04-05 12:02:31 -0700

    Update compliance overview trend graph to allow timezone conversion.
    This absolutely will need a better set of data to test against
    so that we can be assured that the node statuses are actually
    shifting
    
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/pages/+compliance/+reporting/+reporting-overview/overview-trend/overview-trend.component.spec.ts
+++ components/automate-ui/src/app/pages/+compliance/+reporting/+reporting-overview/overview-trend/overview-trend.component.spec.ts
@@ -1,24 +1,29 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { UserPreferencesService } from 'app/services/user-preferences/user-preferences.service';
 import { OverviewTrendComponent } from './overview-trend.component';
+import { StoreModule } from '@ngrx/store';
 
 describe('OverviewTrendComponent', () => {
   let fixture: ComponentFixture<OverviewTrendComponent>;
   let component: OverviewTrendComponent;
+  let userPrefsService: UserPreferencesService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
+        StoreModule.forRoot({})
       ],
       declarations: [
-        OverviewTrendComponent
       ],
       providers: [
+        UserPreferencesService
       ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     });
 
     fixture = TestBed.createComponent(OverviewTrendComponent);
+    userPrefsService = TestBed.inject(UserPreferencesService);
     component = fixture.componentInstance;
   });
 
@@ -28,10 +33,11 @@ describe('OverviewTrendComponent', () => {
 
   it('create UTC date from string date with timezone', () => {
     for ( let hour = 0; hour < 10; hour++ ) {
-      expect(component.createUtcDate('2019-09-14T0' + hour + ':59:59Z').getDate()).toEqual(14);
+      console.log(component.createTimezoneDate('2019-09-14T0' + hour + ':59:59Z'));
+      expect(component.createTimezoneDate('2019-09-14T0' + hour + ':59:59Z').format('D')).toEqual('14');
     }
     for ( let hour = 10; hour < 24; hour++ ) {
-      expect(component.createUtcDate('2019-09-14T' + hour + ':59:59Z').getDate()).toEqual(14);
+      expect(component.createTimezoneDate('2019-09-14T' + hour + ':59:59Z').format('D')).toEqual('14');
     }
   });
 });
