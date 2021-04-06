b3738b804d91e1ec23863dbc4d4d6ee82386a3aa
seajamied <jdegnan@chef.io>
2021-04-06 12:44:55 -0700

    Add in a check to see that the firstFocus attribute element is focused
    Signed-off-by: seajamied <jdegnan@chef.io>

--- e2e/cypress/integration/ui/infra-proxy/chef-servers.spec.ts
+++ e2e/cypress/integration/ui/infra-proxy/chef-servers.spec.ts
@@ -27,6 +27,7 @@ describe('chef server', () => {
     it('can add a server', () => {
       cy.get('[data-cy=add-server-button]').contains('Add Chef Server').click();
       cy.get('app-chef-servers-list chef-modal').should('exist');
+      cy.get('[data-cy=add-name]').should('have.focus');
       cy.get('[data-cy=add-name]').type(serverName);
       cy.get('[data-cy=id-label]').contains(generatedServerID);
       cy.get('[data-cy=add-fqdn]').type(serverFQDN);
