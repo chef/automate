e0db1026ee5fdd9a16a1a1a00b2ac1f1ab62aa24
seajamied <jdegnan@chef.io>
2021-04-14 14:44:39 -0700

    Revert more timezone changes
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/pages/+compliance/+reporting/+reporting-overview/overview-trend/overview-trend.component.spec.ts
+++ components/automate-ui/src/app/pages/+compliance/+reporting/+reporting-overview/overview-trend/overview-trend.component.spec.ts
@@ -1,29 +1,24 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { UserPreferencesService } from 'app/services/user-preferences/user-preferences.service';
 import { OverviewTrendComponent } from './overview-trend.component';
-import { StoreModule } from '@ngrx/store';
 
 describe('OverviewTrendComponent', () => {
   let fixture: ComponentFixture<OverviewTrendComponent>;
   let component: OverviewTrendComponent;
-  let userPrefsService: UserPreferencesService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        StoreModule.forRoot({})
       ],
       declarations: [
+        OverviewTrendComponent
       ],
       providers: [
-        UserPreferencesService
       ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     });
 
     fixture = TestBed.createComponent(OverviewTrendComponent);
-    userPrefsService = TestBed.inject(UserPreferencesService);
     component = fixture.componentInstance;
   });
 
@@ -33,10 +28,10 @@ describe('OverviewTrendComponent', () => {
 
   it('create UTC date from string date with timezone', () => {
     for ( let hour = 0; hour < 10; hour++ ) {
-      expect(component.createTimezoneDate('2019-09-14T0' + hour + ':59:59Z').date()).toEqual(14);
+      expect(component.createUtcDate('2019-09-14T0' + hour + ':59:59Z').getDate()).toEqual(14);
     }
     for ( let hour = 10; hour < 24; hour++ ) {
-      expect(component.createTimezoneDate('2019-09-14T' + hour + ':59:59Z').date()).toEqual(14);
+      expect(component.createUtcDate('2019-09-14T' + hour + ':59:59Z').getDate()).toEqual(14);
     }
   });
 });
