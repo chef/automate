ba326f2d89b402f166c740b03e867a565b48dd4b
seajamied <jdegnan@chef.io>
2021-04-27 11:14:11 -0700

    Update to add link and reshape look
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-dex/web/theme/scripts.js
+++
@@ -1,8 +0,0 @@
-var popUpWrapper = document.querySelector('.popup-wrapper');
-var acceptButton = document.querySelector('.accept-button');
-
-acceptButton.addEventListener('click', closePopup);
-
-function closePopup() {
-  popUpWrapper.classList.remove('open');
-}
