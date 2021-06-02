cb46076a1b6c862836363a8f4d77f496c7d451be
chaitali-mane <cmane@progress.com>
2021-06-03 00:18:49 +0530

    Added unit test cases
    Signed-off-by: chaitali-mane <cmane@progress.com>

--- components/automate-ui/src/app/modules/infra-proxy/infra-nodes/infra-nodes.component.spec.ts
+++ components/automate-ui/src/app/modules/infra-proxy/infra-nodes/infra-nodes.component.spec.ts
@@ -7,8 +7,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MockComponent } from 'ng2-mock-component';
 import { By } from '@angular/platform-browser';
 import { FeatureFlagsService } from 'app/services/feature-flags/feature-flags.service';
-import { InfraNode } from 'app/entities/infra-nodes/infra-nodes.model';
-import { GetNodesSuccess, GetNodeSuccess } from 'app/entities/infra-nodes/infra-nodes.actions';
+import { InfraNode, InfraNodeAttribute } from 'app/entities/infra-nodes/infra-nodes.model';
+import { GetNodesSuccess, GetNodeSuccess, UpdateNodeAttributesSuccess } from 'app/entities/infra-nodes/infra-nodes.actions';
 import { GetNodeRunlistsSuccess } from 'app/entities/nodeRunlists/nodeRunlists.action';
 import { NodeRunlist } from 'app/entities/nodeRunlists/nodeRunlists.model';
 
@@ -50,6 +50,10 @@ describe('InfraNodesComponent', () => {
           inputs: ['label', 'openEvent', 'orgId', 'availableType',
             'node', 'serverId', 'selected'],
           outputs: ['closeRunlist']
+        }),
+        MockComponent({
+          selector: 'app-edit-infra-node-attribute-modal',
+          inputs: ['openEvent', 'serverId', 'orgId', 'node', 'jsonText']
         }),
         InfraNodesComponent
       ],
@@ -165,6 +169,58 @@ describe('InfraNodesComponent', () => {
       component.loadNodeRunlist(node);
       store.dispatch(new GetNodeRunlistsSuccess(runlist));
       expect(component.runlist.length).not.toBeNull();
+    });
+  });
+
+  describe('edit attributes', () => {
+    let store: Store<NgrxStateAtom>;
+    const node: InfraNode = {
+      server_id: 'chef-server-dev-test',
+      org_id: 'chef-org-dev',
+      id: 'node-692057300',
+      tags: ['tag2'],
+      check_in: '',
+      uptime: '',
+      platform: '',
+      automatic_attributes: '{}',
+      default_attributes: '{}',
+      environment: '_default',
+      name: 'node-692057300',
+      normal_attributes: '{}',
+      override_attributes: '{}',
+      policy_group: '',
+      policy_name: '',
+      run_list: ['recipe[centos-cookbook-file]', 'recipe[chef-client]'],
+      ip_address: '',
+      fqdn: ''
+    };
+
+    const nodeAttribute: InfraNodeAttribute = {
+      server_id: 'chef-server-dev-test',
+      org_id: 'chef-org-dev',
+      name: 'node-692057300',
+      attributes: []
+    };
+
+    beforeEach(() => {
+      store = TestBed.inject(Store);
+    });
+
+    it('open edit attributes modal ', () => {
+      component.editAttributes(node);
+      expect(component.editAttributesLoading).toEqual(true);
+    });
+
+    it('get infra node ', () => {
+      component.getNode(node);
+      store.dispatch(new GetNodeSuccess(node));
+      expect(component.nodeToEditRunlist).not.toBeNull();
+    });
+
+    it('load attributes ', () => {
+      component.loadAttributes(node);
+      store.dispatch(new UpdateNodeAttributesSuccess(nodeAttribute));
+      expect(component.attributes).not.toBeNull();
     });
   });
 });
