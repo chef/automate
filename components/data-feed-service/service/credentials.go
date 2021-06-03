211b41f86b92debec5163edd41ac1f9241f87ac3
Vivek Shankar <vshankar@progress.com>
2021-06-03 12:33:50 +0530

    WIP-webhook-backend

--- components/data-feed-service/service/credentials.go
+++ components/data-feed-service/service/credentials.go
@@ -10,9 +10,27 @@ import (
 
 const CredentialsError = "Could not create credentials"
 
-type Credentials interface {
-	GetAuthorizationHeaderValue() string
+type AwsCredentials struct {
+	region string
 }
+
+type AuthTypes struct {
+	AuthorizationHeader string
+	HeaderJSONString    string
+	AwsCreds            AwsCredentials
+}
+
+type Credentials interface {
+	GetValues() AuthTypes
+	GetAuthType() string
+}
+
+const (
+	BASIC_AUTH         = "basic_auth"
+	SPLUNK_AUTH        = "splunk_auth"
+	TOKEN_AUTH         = "token_auth"
+	CUSTOM_HEADER_AUTH = "custom_header"
+)
 
 type CredentialsFactory struct {
 	data map[string]string
@@ -30,6 +48,15 @@ func (c *CredentialsFactory) NewCredentials() (Credentials, error) {
 	if splunk, ok := c.data["Splunk"]; ok {
 		return NewSplunkAuthCredentials(splunk), nil
 	}
+	if c.data["auth_type"] == BASIC_AUTH {
+		return NewBasicAuthCredentials(c.data["username"], c.data["password"]), nil
+	}
+	if c.data["auth_type"] == TOKEN_AUTH {
+		return NewTokenAuthCredentials(c.data["token"]), nil
+	}
+	if c.data["auth_type"] == CUSTOM_HEADER_AUTH {
+		return NewCustomHeaderCredentials(c.data["headers"]), nil
+	}
 	return nil, errors.New(CredentialsError)
 }
 
@@ -42,8 +69,14 @@ func NewBasicAuthCredentials(username string, password string) BasicAuthCredenti
 	return BasicAuthCredentials{username: username, password: password}
 }
 
-func (c BasicAuthCredentials) GetAuthorizationHeaderValue() string {
-	return "Basic " + c.basicAuth()
+func (c BasicAuthCredentials) GetValues() AuthTypes {
+	return AuthTypes{
+		AuthorizationHeader: "Basic " + c.basicAuth(),
+	}
+}
+
+func (c BasicAuthCredentials) GetAuthType() string {
+	return BASIC_AUTH
 }
 
 func (c BasicAuthCredentials) basicAuth() string {
@@ -59,8 +92,50 @@ func NewSplunkAuthCredentials(splunkToken string) SplunkAuthCredentials {
 	return SplunkAuthCredentials{splunkToken: splunkToken}
 }
 
-func (c SplunkAuthCredentials) GetAuthorizationHeaderValue() string {
-	return "Splunk " + c.splunkToken
+func (c SplunkAuthCredentials) GetValues() AuthTypes {
+	return AuthTypes{
+		AuthorizationHeader: "Splunk " + c.splunkToken,
+	}
+}
+
+func (c SplunkAuthCredentials) GetAuthType() string {
+	return SPLUNK_AUTH
+}
+
+type TokenAuthCredentials struct {
+	token string
+}
+
+func NewTokenAuthCredentials(token string) TokenAuthCredentials {
+	return TokenAuthCredentials{token: token}
+}
+
+func (c TokenAuthCredentials) GetValues() AuthTypes {
+	return AuthTypes{
+		AuthorizationHeader: c.token,
+	}
+}
+
+func (c TokenAuthCredentials) GetAuthType() string {
+	return TOKEN_AUTH
+}
+
+type CustomHeaderCredentials struct {
+	headers string
+}
+
+func NewCustomHeaderCredentials(headers string) CustomHeaderCredentials {
+	return CustomHeaderCredentials{headers: headers}
+}
+
+func (c CustomHeaderCredentials) GetValues() AuthTypes {
+	return AuthTypes{
+		HeaderJSONString: c.headers,
+	}
+}
+
+func (c CustomHeaderCredentials) GetAuthType() string {
+	return CUSTOM_HEADER_AUTH
 }
 
 func GetCredentials(ctx context.Context, client secrets.SecretsServiceClient, secretID string) (Credentials, error) {
