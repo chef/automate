1be183db57c8c3c06a19061777316b5906d56e50
shaik80 <sh.mudassir98@gmail.com>
2021-06-08 18:43:37 +0530

    jobrunner with key

--- components/compliance-service/inspec-agent/runner/runner.go
+++ components/compliance-service/inspec-agent/runner/runner.go
@@ -441,7 +441,7 @@ func (t *InspecJobTask) Run(ctx context.Context, task cereal.Task) (interface{},
 
 	cleanupKeys(job.TargetConfig.KeyFiles)
 	logrus.Debugf("job '%s' finished", job.JobID)
-	fmt.Println("jobRunner ", job)
+	fmt.Printf("jobRunner %+v\n", job)
 
 	if job.NodeStatus == types.StatusRunning {
 		job.NodeStatus = types.StatusFailed
