528b84ec5c9b5564d3255c651d74e75baee91276
Himanshi Chhabra <61003053+himanshi-chhabra@users.noreply.github.com>
2021-06-24 18:29:56 +0530

    Added policyfile tab and list (#5277)
    * Added policyfile tab
    
    Signed-off-by: Himanshi Chhabra <hchhabra@progress.com>
    
    * removed checkbox
    
    Signed-off-by: Himanshi Chhabra <hchhabra@progress.com>
    
    * Added comment on pagination.
    
    Signed-off-by: Himanshi Chhabra <hchhabra@progress.com>

---
+++ e2e/cypress/integration/ui/infra-proxy/infra-policy-file-list.spec.ts
@@ -0,0 +1,134 @@
+describe('infra policy file', () => {
+  const now = Cypress.moment().format('MMDDYYhhmmss');
+  const cypressPrefix = 'infra';
+  let adminIdToken = '';
+  const serverID = 'chef-server-dev-test';
+  const serverName = 'chef server dev';
+  const orgID = 'chef-org-dev';
+  const orgName = '4thcoffee';
+  const serverFQDN = 'ec2-34-219-25-251.us-west-2.compute.amazonaws.com';
+  const serverIP = '34.219.25.251';
+  const adminUser = 'chefadmin';
+  const adminKey = Cypress.env('AUTOMATE_INFRA_ADMIN_KEY').replace(/\\n/g, '\n');
+
+  before(() => {
+    cy.adminLogin('/').then(() => {
+      const admin = JSON.parse(<string>localStorage.getItem('chef-automate-user'));
+      adminIdToken = admin.id_token;
+
+      cy.request({
+        auth: { bearer: adminIdToken },
+        failOnStatusCode: false,
+        method: 'POST',
+        url: '/api/v0/infra/servers',
+        body: {
+          id: serverID,
+          name: serverName,
+          fqdn: serverFQDN,
+          ip_address: serverIP
+        }
+      }).then((resp) => {
+        if (resp.status === 200 && resp.body.ok === true) {
+          return;
+        } else {
+          cy.request({
+            auth: { bearer: adminIdToken },
+            method: 'GET',
+            url: `/api/v0/infra/servers/${serverID}`,
+            body: {
+              id: serverID
+            }
+          });
+        }
+      });
+
+      cy.request({
+        auth: { bearer: adminIdToken },
+        failOnStatusCode: false,
+        method: 'POST',
+        url: `/api/v0/infra/servers/${serverID}/orgs`,
+        body: {
+          id: orgID,
+          server_id: serverID,
+          name: orgName,
+          admin_user: adminUser,
+          admin_key: adminKey
+        }
+      }).then((response) => {
+        if (response.status === 200 && response.body.ok === true) {
+          return;
+        } else {
+          cy.request({
+            auth: { bearer: adminIdToken },
+            method: 'GET',
+            url: `/api/v0/infra/servers/${serverID}/orgs/${orgID}`,
+            body: {
+              id: orgID,
+              server_id: serverID
+            }
+          });
+        }
+      });
+      cy.visit(`/infrastructure/chef-servers/${serverID}/organizations/${orgID}`);
+      cy.get('app-welcome-modal').invoke('hide');
+    });
+    cy.restoreStorage();
+  });
+
+  beforeEach(() => {
+    cy.restoreStorage();
+  });
+
+  afterEach(() => {
+    cy.saveStorage();
+  });
+
+  function getPolicyFile() {
+    return cy.request({
+      auth: { bearer: adminIdToken },
+      failOnStatusCode: false,
+      method: 'GET',
+      url: `/api/v0/infra/servers/${serverID}/orgs/${orgID}/policyfiles`
+    });
+  }
+
+  function checkResponse(response: any) {
+    if (response.body.policies.length === 0) {
+      cy.get('[data-cy=empty-list]').should('be.visible');
+    } else {
+      cy.get('[data-cy=policy-file-table-container] chef-th').contains('Name');
+      cy.get('[data-cy=policy-file-table-container] chef-th').contains('Revision ID');
+      return true;
+    }
+  }
+
+  describe('infra policy file list page', () => {
+    it('displays org details', () => {
+      cy.get('.page-title').contains(orgName);
+    });
+
+    // policy file tabs specs
+    it('can switch to policy file tab', () => {
+      cy.get('.nav-tab').contains('Policyfiles').click();
+    });
+
+    it('can check if policy file has list or not', () => {
+      getPolicyFile().then((response) => {
+        checkResponse(response);
+      });
+    });
+
+    context('can change page in policy file', () => {
+
+      it('can change page and load data according to page', () => {
+        getPolicyFile().then((response) => {
+          if (checkResponse(response)) {
+            if (cy.get('.policy-file-list-paging .page-picker-item').contains('3')) {
+              cy.get('.policy-file-paging .page-picker-item').contains('3').click();
+            }
+          }
+        });
+      });
+    });
+  });
+});
