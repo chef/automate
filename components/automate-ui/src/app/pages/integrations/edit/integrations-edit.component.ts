c4857d363f3cf7c0f9310b7fff36a5b949fe4e7b
shaik80 <sh.mudassir98@gmail.com>
2021-06-25 09:30:13 +0530

    lint changes
    Signed-off-by: shaik80 <sh.mudassir98@gmail.com>

--- components/automate-ui/src/app/pages/integrations/edit/integrations-edit.component.ts
+++ components/automate-ui/src/app/pages/integrations/edit/integrations-edit.component.ts
@@ -88,7 +88,7 @@ export class IntegrationsEditComponent implements OnDestroy {
           azure_client_id: '',
           azure_client_secret: '',
           azure_tenant_id: '',
-          azure_subscription_id:''
+          azure_subscription_id: ''
         })
       }),
       gcp: fb.group({
