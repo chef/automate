af9731b39d442ac285c98631241cbe4ba0c3dac6
Venkatesh-rengasamy <vrengasa@progress.com>
2021-07-25 23:34:32 +0530

    resolve conflicts
    Signed-off-by: Venkatesh-rengasamy <vrengasa@progress.com>

--- components/automate-ui/src/app/ngrx.reducers.ts
+++ components/automate-ui/src/app/ngrx.reducers.ts
@@ -45,6 +45,7 @@ import * as orgEntity from './entities/orgs/org.reducer';
 import * as permEntity from './entities/userperms/userperms.reducer';
 import * as policyEntity from './entities/policies/policy.reducer';
 import * as policyFileEntity from './entities/policy-files/policy-file.reducer';
+import * as policyFileDetailsEntity from './entities/policy-files/policy-file-details.reducer';
 import * as profileEntity from './entities/profiles/profile.reducer';
 import * as projectEntity from './entities/projects/project.reducer';
 import * as recipeEntity from './entities/recipes/recipe.reducer';
@@ -117,6 +118,7 @@ export interface NgrxStateAtom {
   orgs: orgEntity.OrgEntityState;
   policies: policyEntity.PolicyEntityState;
   policyFiles: policyFileEntity.PolicyFileEntityState;
+  policyFileDetails: policyFileDetailsEntity.PolicyFileDetailsEntityState;
   profiles: profileEntity.ProfileEntityState;
   projects: projectEntity.ProjectEntityState;
   recipes: recipeEntity.RecipeEntityState;
@@ -248,6 +250,7 @@ export const defaultInitialState = {
   notificationRules: NotificationRuleEntity.NotificationRuleEntityInitialState,
   policies: policyEntity.PolicyEntityInitialState,
   policyFiles: policyFileEntity.PolicyFileEntityInitialState,
+  policyFileDetails: policyFileDetailsEntity.PolicyFileDetailsEntityState,
   profiles: profileEntity.ProfileEntityInitialState,
   projects: projectEntity.ProjectEntityInitialState,
   recipes: recipeEntity.RecipeEntityInitialState,
@@ -317,6 +320,7 @@ export const ngrxReducers = {
   notificationRules: NotificationRuleEntity.notificationRuleEntityReducer,
   policies: policyEntity.policyEntityReducer,
   policyFiles: policyFileEntity.policyFileEntityReducer,
+  policyFileDetails: policyFileDetailsEntity.PolicyFileDetailsEntityState,
   profiles: profileEntity.profileEntityReducer,
   projects: projectEntity.projectEntityReducer,
   recipes: recipeEntity.recipeEntityReducer,
