0fcbac87f0a35e97d068e4ce1a0938a8ba6a2e54
Venkatesh-rengasamy <vrengasa@progress.com>
2021-07-25 23:15:44 +0530

    resolve ts-lint errors
    Signed-off-by: Venkatesh-rengasamy <vrengasa@progress.com>

--- components/automate-ui/src/app/services/user-preferences/user-preferences.model.ts
+++ components/automate-ui/src/app/services/user-preferences/user-preferences.model.ts
@@ -2,23 +2,23 @@ export interface UserPreferenceResponse {
   user: {
     name: string,
     connector: string
-  },
-  time_format: UserPreferenceTimeformat
+  };
+  time_format: UserPreferenceTimeformat;
 }
 
 export interface UserPreferencesPayload {
   user: {
     name: string,
     connector: string
-  },
+  };
   settings: {
     date_format: {
       value: string
     }
-  }
+  };
 }
 
 export interface UserPreferenceTimeformat {
-    value: string,
-    valid_values?: string[]
+    value: string;
+    valid_values?: string[];
 }
