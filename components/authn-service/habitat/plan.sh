ac3ebe8d89a7ea08df650f517d50ebbac74917b9
Kallol Roy <karoy@progress.com>
2021-07-26 18:48:07 +0530

    Made changes to import session-service grpc-port
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- components/authn-service/habitat/plan.sh
+++ components/authn-service/habitat/plan.sh
@@ -26,7 +26,7 @@ pkg_binds=(
   [teams-service]="port"
   [authz-service]="port"
   [cereal-service]="port"
-  [session-service]="port"
+  [session-service]="grpc-port"
 )
 pkg_bin_dirs=(bin)
 pkg_scaffolding="${local_scaffolding_origin:-chef}/automate-scaffolding-go"
