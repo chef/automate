e0f1aac61c946fddfce9f3777d23bc1098df8995
Venkatesh-rengasamy <vrengasa@progress.com>
2021-07-30 01:03:58 +0530

    resolve cypress test
    Signed-off-by: Venkatesh-rengasamy <vrengasa@progress.com>

--- e2e/cypress/integration/ui/common/user_settings.spec.ts
+++ e2e/cypress/integration/ui/common/user_settings.spec.ts
@@ -17,6 +17,45 @@ describe('login the app', () => {
             });
         });
 
+        it('change and save timeformat value', function () {
+            cy.get('[data-cy=timeformat-dropdown]').then((dropdown) => {
+                const timeformatList = ['YYYY-M-D', 'YYYY-MM-DD'];
+                let selectTimeformat = '';
+                for (let i = 0; i < timeformatList.length; i++) {
+                    if (dropdown[0].innerText.indexOf(timeformatList[i]) === -1) {
+                        selectTimeformat = timeformatList[i];
+                        break;
+                    }
+                }
+                const attrSelector = '[value=' + selectTimeformat + ']';
+                cy.get('[data-cy=timeformat-dropdown]').click().then(() => {
+                    cy.get(attrSelector).click({force: true}).then(() => {
+                        let count = 0;
+                        const innerText = dropdown[0].innerText;
+                        while (innerText.indexOf(selectTimeformat) !== 0
+                        && count < 10) {
+                            cy.get(attrSelector).click({force: true});
+                            count++;
+                        }
+                        cy.get('[data-cy=timeformat-dropdown]')
+                            .should('have.value', selectTimeformat)
+                            .then(() => {
+                                cy.server();
+                                cy.route({
+                                    method: 'PUT',
+                                    url: 'https://a2-dev.test/api/v0/user-settings/admin/local'
+                                }).as('updateUserPreference');
+                                cy.get('[data-cy=user-details-submit-button]')
+                                    .click({force: true}).then(() => {
+                                        cy.wait('@updateUserPreference').its('status')
+                                            .should('be', 200);
+                                });
+                        });
+                    });
+                });
+            });
+        });
+
     });
 
 });
