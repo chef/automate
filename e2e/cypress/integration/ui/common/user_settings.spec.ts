8725c9fa2213b10ffae627e2efa8c3f62230ea0f
Venkatesh-rengasamy <vrengasa@progress.com>
2021-07-30 14:26:48 +0530

    resolve cypress test
    Signed-off-by: Venkatesh-rengasamy <vrengasa@progress.com>

--- e2e/cypress/integration/ui/common/user_settings.spec.ts
+++ e2e/cypress/integration/ui/common/user_settings.spec.ts
@@ -1,5 +1,28 @@
 describe('login the app', () => {
     describe('load and update user preference', () => {
+        let adminIdToken = '';
+        let timeformat = 'ddd, DD MMM YYYY';
+        let username = '';
+        let connector = '';
+        before(() => {
+            cy.adminLogin('/').then(() => {
+              const admin = JSON.parse(<string>localStorage.getItem('chef-automate-user'));
+              adminIdToken = admin.id_token;
+              username = admin.username;
+              connector = admin.connector;
+              cy.request({
+                auth: { bearer: adminIdToken },
+                method: 'GET',
+                url: 'api/v0/user-settings/' + username + '/' + connector
+              }).then((resp) => {
+                if (resp.status === 200 && resp.statusText === 'OK') {
+                  timeformat = resp.body.settings.date_format.value;
+                  return;
+                }
+              });
+            });
+            cy.restoreStorage();
+        });
 
         it('open profile menu if exist', function () {
             cy.get('[data-cy=user-profile-button]').should('exist').then(() => {
@@ -13,7 +36,45 @@ describe('login the app', () => {
             cy.get('.dropdown-list-item .profile').click().then(() => {
                 cy.get('app-user-details').should('exist').then(() =>  {
                     cy.get('[data-cy=timeformat-dropdown]').should('exist');
+                    cy.get('[data-cy=timeformat-dropdown] .selected-value .option-content')
+                    .then(($element) => {
+                        cy.get('[data-cy=timeformat-dropdown] .selected-value .option-content')
+                        .contains(timeformat);
+                    });
+
                 });
+            });
+        });
+
+        it('change and save timeformat', function () {
+            const timeformatList = ['YYYY-M-D', 'YYYY-MM-DD'];
+            let selectTimeformat = '';
+            for (let i = 0; i < timeformatList.length; i++) {
+                if (timeformat.indexOf(timeformatList[i]) === -1) {
+                    selectTimeformat = timeformatList[i];
+                    break;
+                }
+            }
+            cy.request({
+                auth: { bearer: adminIdToken },
+                method: 'PUT',
+                url: 'api/v0/user-settings/' + username + '/' + connector,
+                body: {
+                    user: {
+                        name: username,
+                        connector: connector
+                    },
+                    settings: {
+                      date_format: {
+                        value: selectTimeformat
+                      }
+                    }
+                }
+              }).then((resp) => {
+                if (resp.status === 200 && resp.statusText === 'OK') {
+                  expect(resp.status).to.equal(200);
+                  return;
+                }
             });
         });
 
