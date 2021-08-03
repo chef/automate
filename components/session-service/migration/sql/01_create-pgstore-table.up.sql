5b0469fad2ca4c9ba6fad1ea05738c1f56356c3f
Kallol Roy <karoy@progress.com>
2021-08-03 20:58:06 +0530

    Added table for blacklisted id tokens
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- components/session-service/migration/sql/01_create-pgstore-table.up.sql
+++ components/session-service/migration/sql/01_create-pgstore-table.up.sql
@@ -4,3 +4,9 @@ CREATE TABLE sessions (
   expiry TIMESTAMPTZ NOT NULL
 );
 CREATE INDEX sessions_expiry_idx ON sessions (expiry);
+
+CREATE TABLE blacklisted_id_tokens (
+    id_token TEXT
+);
+CREATE INDEX blacklist_session_idx ON blacklisted_id_tokens (id_token);
+
