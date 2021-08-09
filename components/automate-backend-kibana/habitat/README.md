4385b42c9318f0b96d53cee93060c864e56287cf
FaizanSRE <ffulara@msystechnologies.com>
2021-08-09 23:23:41 +0530

    adding automate-backend-kibana
    Signed-off-by: FaizanSRE <ffulara@msystechnologies.com>

---
+++ components/automate-backend-kibana/habitat/README.md
@@ -0,0 +1,11 @@
+# Automate-Backend-Kibana
+
+This is a wrapper package of core/kibana for Chef Automate Backend.
+
+## Maintainers
+
+The Habitat Maintainers humans@habitat.sh
+
+## Type of Package
+
+This is a service package.
--- components/automate-deployment/pkg/services/internal/generated/gen.go
+++ components/automate-deployment/pkg/services/internal/generated/gen.go
@@ -445,6 +445,10 @@ var ProductMetadataJSON = `
       }
     },
     {
+      "name": "chef/automate-backend-kibana",
+      "metadata": null
+    },
+    {
       "name": "core/rsync",
       "metadata": null
     }
@@ -580,7 +584,8 @@ var ProductMetadataJSON = `
       "type": "product",
       "services": [
         "chef/automate-cds",
-        "chef/user-settings-service"
+        "chef/user-settings-service",
+        "chef/automate-backend-kibana"
       ],
       "packages": null,
       "dependencies": [
