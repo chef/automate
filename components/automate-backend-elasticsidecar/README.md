1447cf3989fdbf45f45f2605fc2c5b33b78d00a2
FaizanSRE <ffulara@msystechnologies.com>
2021-08-09 20:52:33 +0530

    Adding automate-backend-elasticsidecar
    Signed-off-by: FaizanSRE <ffulara@msystechnologies.com>

---
+++ components/automate-backend-elasticsidecar/README.md
@@ -0,0 +1,14 @@
+# Automate-Backend-Elasticsidecar
+
+This is a sidecar service for automate-backend-elasticsearch that reads users and passwords off the [elasticsearch] binding and applies them to Elasticsearch using the odfe tooling.
+## Maintainers
+
+The Habitat Maintainers humans@habitat.sh
+
+## Type of Package
+
+This is a service package.
+
+## Notes
+
+This package relies on strict bindings and will not start unless all credentials and ssl are available from Elasticsearch.
---
+++ components/automate-backend-elasticsidecar/habitat/plan.sh
@@ -0,0 +1,53 @@
+# shellcheck disable=SC2148
+pkg_name="automate-backend-elasticsidecar"
+pkg_description="Password manager for elasticsearch-odfe"
+pkg_origin="chef"
+vendor_origin="chef"
+pkg_maintainer="Chef Software Inc. <support@chef.io>"
+pkg_license=("Chef-MLSA")
+pkg_version="1.0.28"
+pkg_upstream_url="http://github.com/chef/a2-ha-backend/components/automate-backend-elasticsidecar"
+pkg_deps=(
+  core/bash
+  core/ruby26
+  chef/elasticsearch-odfe
+)
+pkg_build_deps=(
+  core/make
+  core/gcc
+)
+pkg_interpreters=(bin/ruby)
+
+
+# currently broken, these should be required binds, but they are never found
+pkg_binds=(
+  [elasticsearch]="http-port transport-port root-ca admin-pem admin-key admin_username admin_password dashboard_username dashboard_password"
+)
+
+pkg_lib_dirs=(lib)
+
+do_before() {
+  update_pkg_version
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  gem update --system
+  gem install bcrypt --no-document --install-dir "${pkg_prefix}/lib/gems"
+  gem install http --no-document --install-dir "${pkg_prefix}/lib/gems"
+  gem install json --no-document --install-dir "${pkg_prefix}/lib/gems"
+  gem install toml-rb --no-document --install-dir "${pkg_prefix}/lib/gems"
+  gem install mixlib-shellout --no-document --install-dir "${pkg_prefix}/lib/gems"
+  gem install pry --no-document --install-dir "${pkg_prefix}/lib/gems"
+  mkdir "${pkg_prefix}/bin"
+  install "$PLAN_CONTEXT/bin/elastic_sidecar.rb" "${pkg_prefix}/bin/elastic_sidecar.rb"
+  mkdir "${pkg_prefix}/data"
+  install "$PLAN_CONTEXT/data/dashboards.tar.gz" "${pkg_prefix}/data/dashboards.tar.gz"
+}
+
+do_end() {
+  return 0
+}
