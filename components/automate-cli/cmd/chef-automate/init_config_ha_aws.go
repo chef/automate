4e199a31db1101e97c12825d766a071bcc18a450
Jay Vikram Sharma <mailtome.jns@gmail.com>
2021-08-10 12:42:21 +0530

    simplifying the codes

--- components/automate-cli/cmd/chef-automate/init_config_ha_aws.go
+++ components/automate-cli/cmd/chef-automate/init_config_ha_aws.go
@@ -4,9 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/chef/automate/api/config/deployment"
 	"github.com/chef/automate/components/automate-cli/pkg/status"
-	"github.com/chef/automate/components/automate-deployment/pkg/toml"
 )
 
 func runInitConfigAwsHACmd() error {
@@ -16,25 +14,7 @@ func runInitConfigAwsHACmd() error {
 		return nil
 	}
 
-	cfg, err := deployment.GenerateInitHAConfig()
-
-	if err != nil {
-		return status.Wrap(err, status.ConfigError, "Generating initial configuration failed")
-	}
-
-	t, err := cfg.RenderHaSettings()
-	if err != nil {
-		return status.Wrap(err, status.ConfigError, "Rendering initial configuration failed")
-	}
-
-	// Make sure our user facing config is a valid AutomateConfig
-	ac := deployment.NewAutomateConfig()
-	err = toml.Unmarshal([]byte(t), ac)
-	if err != nil {
-		return status.Wrap(err, status.MarshalError, "Marshaling initial configuration failed")
-	}
-
-	err = ioutil.WriteFile(initConfigHAPath, []byte(t), 0600)
+	err := ioutil.WriteFile(initConfigHAPath, []byte(haAwsConfigTemplate), 0600)
 	if err != nil {
 		return status.Wrap(err, status.FileAccessError, "Writing initial configuration failed")
 	}
