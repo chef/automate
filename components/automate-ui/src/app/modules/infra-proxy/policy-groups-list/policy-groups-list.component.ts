3a3369d2ae3fa6618211153ac5b417938788f9da
Chaitali Mane <71449322+chaitali-mane@users.noreply.github.com>
2021-08-10 23:00:38 +0530

    Added pagination in policy groups list (#5457)
    * Added pagination
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Added minor changes
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * lint err
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * minor change
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * review comments
    
    Signed-off-by: chaitali-mane <cmane@progress.com>

--- components/automate-ui/src/app/modules/infra-proxy/policy-groups-list/policy-groups-list.component.ts
+++ components/automate-ui/src/app/modules/infra-proxy/policy-groups-list/policy-groups-list.component.ts
@@ -1,54 +1,24 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-policy-groups-list',
   templateUrl: './policy-groups-list.component.html'
 })
-export class PolicyGroupsListComponent implements OnInit, OnChanges {
-  @Input() policyFiles: [];
-  @Input() searchFlag: boolean;
-  public policyGroups = [];
-  public list = [];
+export class PolicyGroupsListComponent implements OnChanges {
+  @Input() policyGroups: [];
+  @Input() pageOfItems: [];
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.filterDataGroupWise();
-  }
-
-    ngOnChanges(changes: SimpleChanges) {
-    // update list if items array has changed
-    if (changes.policyFiles.currentValue !== changes.policyFiles.previousValue) {
-      this.list = this.policyFiles;
-      this.filterDataGroupWise();
-    }
-  }
-
-  filterDataGroupWise() {
-    const key = 'policy_group';
-    this.policyGroups = [];
-    if (this.list.length > 0) {
-      this.list.forEach((x) => {
-        // Checking if there is any object in this.policyGroupsList
-        // which contains the key value
-        if (this.policyGroups.some((val) => val[key] === x[key])) {
-          // If yes! then increase the occurrence by 1
-          this.policyGroups.forEach((k) => {
-            if (k[key] === x[key]) {
-              k['occurrence']++;
-            }
-          });
-        } else {
-          // If not! Then create a new object initialize
-          // it with the present iteration key's value and set the occurrence to 1
-          const a = {};
-          a[key] = x[key];
-          a['occurrence'] = 1;
-          this.policyGroups.push(a);
-        }
-      });
-    } else {
-      this.policyGroups = [];
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.pageOfItems) {
+      if (changes.pageOfItems.currentValue !== changes.pageOfItems.previousValue) {
+        this.policyGroups = this.pageOfItems;
+      }
     }
   }
 }
