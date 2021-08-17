b46a30dfc453ffea92c60408d7d21085384574d1
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2021-08-17 12:23:20 +0530

    test pipline
    Signed-off-by: Shaik Mudassir <47217471+shaik80@users.noreply.github.com>

--- integration/helpers/setup_minio.sh
+++ integration/helpers/setup_minio.sh
@@ -2,22 +2,11 @@
 
 export MINIO_LOG_FILE=/tmp/automate-MINIO.log
 
-document "start_minio_background" <<DOC
-  Start the minio in the background.
-
-  To show the logs run the command:
-    # minio_logs
-DOC
-function start_minio_background {
-  log_line "Launching the $(green minio) in the background."
+start_minio_background() {
   setup_minio >> $MINIO_LOG_FILE 2>&1 &
-  log_line "Run '$(yellow minio_logs)' to see how the automate-ui builds."
 }
 
-document "minio_logs" <<DOC
-  Display the UI logs stored at '$UI_LOG_FILE'.
-DOC
-function minio_logs {
+minio_logs() {
   tail -20f $MINIO_LOG_FILE
 }
 
