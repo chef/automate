68ec90e00d7e53abc8ef4937aaf6d4380dc4af22
Jay Vikram Sharma <mailtome.jns@gmail.com>
2021-08-17 18:32:39 +0530

    inital toml config

--- components/automate-cli/cmd/chef-automate/init_config_ha.go
+++ components/automate-cli/cmd/chef-automate/init_config_ha.go
@@ -4,14 +4,159 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
 
 	"github.com/chef/automate/components/automate-cli/pkg/status"
+	"github.com/chef/automate/components/automate-deployment/pkg/toml"
+	tomlBare "github.com/chef/toml"
 	"github.com/spf13/cobra"
 )
 
 var initConfigHAPathFlags = struct {
 	path string
 }{}
+
+type awsCommon = struct {
+	aws common `toml:"aws"`
+}
+
+type common = struct {
+	SecretsKeyFile   string `toml:"secrets_key_file"`
+	SecretsStoreFile string `toml:"secrets_store_file"`
+	Architecture     string `toml:"architecture"`
+	WorkspacePath    string `toml:"workspace_path"`
+	SshUser          string `toml:"ssh_user"`
+	SshKeyFile       string `toml:"ssh_key_file"`
+	SudoPassword     string `toml:"sudo_password"`
+	BackupMount      string `toml:"backup_mount"`
+}
+
+type automateConfig = struct {
+	automate automate `toml:"config"`
+}
+
+type automate = struct {
+	AutomateAdminPassword string
+	AutomateFQDN          string
+	AutomateTeamsPort     string
+	AutomateInstanceCount string
+	AutomateConfigFile    string
+}
+
+type chefServer = struct {
+	ChefServerInstanceCount string
+}
+
+type elasticSearch = struct {
+	ElasticSearchInstanceCount string
+}
+
+type postgress = struct {
+	PostgresqlInstanceCount string
+}
+
+type aws = struct {
+	Profile                        string
+	Region                         string
+	SshKeyPairName                 string
+	AutomateServerInstaceType      string
+	ChefServerInstanceType         string
+	ElasticSearchServerInstaceType string
+	PostgresqlServerInstanceType   string
+	AwsAutomateLBCertificateARN    string
+	ChefServerLBCertificateARN     string
+	AutomateEbsVolumeIops          string
+	AutomateEbsVolumeSize          string
+	AutomateEbsVolumeType          string
+	ChefEbsVolumeIops              string
+	ChefEbsVolumeSize              string
+	ChefEbsVolumeType              string
+	EsEbsVolumeIops                string
+	EsEbsVolumeSize                string
+	EsEbsVolumeType                string
+	PgsEbsVolumeIops               string
+	PgsEbsVolumeSize               string
+	PgsEbsVolumeType               string
+	TagContact                     string
+	TagDept                        string
+	TagProject                     string
+}
+
+var initConfigHAFlags = struct {
+	SecretsKeyFile                    string `toml:"architecture.aws.secrets_key_file"`
+	SecretsStoreFile                  string `toml:"architecture.aws.secrets_store_file"`
+	Architecture                      string `toml:"architecture.aws.architecture"`
+	WorkspacePath                     string `toml:"architecture.aws.workspace_path"`
+	SshUser                           string `toml:"architecture.aws.ssh_user"`
+	SshKeyFile                        string `toml:"architecture.aws.ssh_key_file"`
+	SudoPassword                      string `toml:"architecture.aws.sudo_password"`
+	BackupMount                       string `toml:"architecture.aws.backup_mount"`
+	AutomateAdminPassword             string `toml:"automate.config.admin_password"`
+	AutomateFQDN                      string `toml:"automate.config.fqdn"`
+	AutomateTeamsPort                 string `toml:"automate.config.AutomateTeamsPort"`
+	AutomateInstanceCount             string `toml:"automate.config.instance_count"`
+	AutomateConfigFile                string `toml:"automate.config.config_file"`
+	ChefServerInstanceCount           string `toml:"chef_server.config.instance_count"`
+	ElasticSearchInstanceCount        string `toml:"elasticsearch.config.instance_count"`
+	PostgresqlInstanceCount           string `toml:"postgresql.config.instance_count"`
+	AwsProfile                        string `toml:"aws.config.profile"`
+	AwsRegion                         string `toml:"aws.config.region"`
+	AwsSshKeyPairName                 string `toml:"aws.config.ssh_key_pair_name"`
+	AwsAutomateServerInstaceType      string `toml:"aws.config.automate_server_instance_type"`
+	AwsChefServerInstanceType         string `toml:"aws.config.chef_server_instance_type"`
+	AwsElasticSearchServerInstaceType string `toml:"aws.config.elasticsearch_server_instance_type"`
+	AwsPostgresqlServerInstanceType   string `toml:"aws.config.postgresql_server_instance_type"`
+	AwsAutomateLBCertificateARN       string `toml:"aws.config.automate_lb_certificate_arn"`
+	AwsChefServerLBCertificateARN     string `toml:"aws.config.chef_server_lb_certificate_arn"`
+	AwsAutomateEbsVolumeIops          string `toml:"aws.config.automate_ebs_volume_iops"`
+	AwsAutomateEbsVolumeSize          string `toml:"aws.config.automate_ebs_volume_size"`
+	AwsAutomateEbsVolumeType          string `toml:"aws.config.automate_ebs_volume_type"`
+	AwsChefEbsVolumeIops              string `toml:"aws.config.chef_ebs_volume_iops"`
+	AwsChefEbsVolumeSize              string `toml:"aws.config.chef_ebs_volume_size"`
+	AwsChefEbsVolumeType              string `toml:"aws.config.chef_ebs_volume_type"`
+	AwsEsEbsVolumeIops                string `toml:"aws.config.elasticsearch_ebs_volume_iops"`
+	AwsEsEbsVolumeSize                string `toml:"aws.config.elasticsearch_ebs_volume_size"`
+	AwsEsEbsVolumeType                string `toml:"aws.config.elasticsearch_ebs_volume_type"`
+	AwsPgsEbsVolumeIops               string `toml:"aws.config.postgresql_ebs_volume_iops"`
+	AwsPgsEbsVolumeSize               string `toml:"aws.config.postgresql_ebs_volume_size"`
+	AwsPgsEbsVolumeType               string `toml:"aws.config.postgresql_ebs_volume_type"`
+	AwsTagContact                     string `toml:"aws.config.X-Contact"`
+	AwsTagDept                        string `toml:"aws.config.X-Dept"`
+	AwsTagProject                     string `toml:"aws.config.X-Project"`
+}{}
+
+type tempStruct = struct {
+	Architecture awsCommon      `toml:"architecture"`
+	Automate     automateConfig `toml:"automate"`
+	ChefServer   chefServer     `toml:"chef_server"`
+}
+
+type initConfigHAExistingNodesFlags = struct {
+	SecretsKeyFile                       string `toml:"architecture.existing_nodes.secrets_key_file"`
+	SecretsStoreFile                     string `toml:"architecture.existing_nodes.secrets_store_file"`
+	Architecture                         string `toml:"architecture.existing_nodes.architecture"`
+	WorkspacePath                        string `toml:"architecture.existing_nodes.workspace_path"`
+	SshUser                              string `toml:"architecture.existing_nodes.ssh_user"`
+	SshKeyFile                           string `toml:"architecture.existing_nodes.ssh_key_file"`
+	SudoPassword                         string `toml:"architecture.existing_nodes.sudo_password"`
+	BackupMount                          string `toml:"architecture.existing_nodes.backup_mount"`
+	AutomateAdminPassword                string `toml:"automate.config.admin_password"`
+	AutomateFQDN                         string `toml:"automate.config.fqdn"`
+	AutomateTeamsPort                    string `toml:"automate.config.AutomateTeamsPort"`
+	AutomateInstanceCount                string `toml:"automate.config.instance_count"`
+	AutomateConfigFile                   string `toml:"automate.config.config_file"`
+	ChefServerInstanceCount              string `toml:"chef_server.config.instance_count"`
+	ElasticSearchInstanceCount           string `toml:"elasticsearch.config.instance_count"`
+	PostgresqlInstanceCount              string `toml:"postgresql.config.instance_count"`
+	ExistingNodesAutomateIPs             string `toml:"existing_nodes.config.automate_ips"`
+	ExistingNodesAutomatePrivateIPs      string `toml:"existing_nodes.config.chef_server_ips"`
+	ExistingNodesChefServerPrivateIPs    string `toml:"existing_nodes.config.chef_server_private_ips"`
+	ExistingNodesElasticsearchIPs        string `toml:"existing_nodes.config.elasticsearch_ips"`
+	ExistingNodesElasticsearchPrivateIPs string `toml:"existing_nodes.config.elasticsearch_private_ips"`
+	ExistingNodesPostgresqlIPs           string `toml:"existing_nodes.config.postgresql_ips"`
+	ExistingNodesPostgresqlPrivateIps    string `toml:"existing_nodes.config.postgresql_private_ips"`
+}
 
 func init() {
 	initConfigHACmd.PersistentFlags().StringVar(
@@ -44,9 +189,32 @@ func runInitConfigHACmd(cmd *cobra.Command, args []string) error {
 	} else if args[0] == "bare_metal" {
 		writer.Printf("Generating initial automate high availability configuration for existing infra nodes deployment")
 		return runInitConfigExistingNodeHACmd()
+	} else if args[0] == "deploy" {
+		return readConfigAndWriteToRile()
 	} else {
 		msg := "Incorrect argument expected is any of deplyment mode like aws or existing_node. "
 		writer.Printf(msg)
 		return status.Wrap(errors.New(msg), status.ConfigError, msg)
 	}
+}
+
+func readConfigAndWriteToRile() error {
+	//fmt.Printf("reading configs from toml file")
+	initConfigHAPath := initConfigHAPathFlags.path
+	content, err := ioutil.ReadFile(initConfigHAPath)
+	if err != nil {
+		return nil
+	} else {
+		//fmt.Println(string(content))
+	}
+	error := toml.Validate(content)
+	if error != nil {
+		fmt.Println("Invalid toml formate")
+	}
+	var conf tempStruct
+	if _, err := tomlBare.Decode(string(content), &conf); err != nil {
+		fmt.Printf(err.Error())
+	}
+	fmt.Printf("%+v\n", conf)
+	return nil
 }
---
+++ components/automate-cli/cmd/chef-automate/init_config_ha_a2ha_rb_template.go
@@ -0,0 +1,172 @@
+package main
+
+const existingNodesA2harbTemplate = `
+secrets_key_file "{{ .SecretsKeyFile }}"
+secrets_store_file "{{ .SecretsStoreFile }}"
+architecture "{{ .Architecture }}"
+workspace_path "{{ .WorkspacePath }}"
+ssh_user "{{ .SshUser }}"
+ssh_key_file "{{ .SshKeyFile }}"
+# sudo_password "{{ .SudoPassword }}"
+# logging_monitoring_management "true"
+# ew_elk "false"
+# existing_elk "false"
+# existing_elk_instance_ip ""
+# existing_elk_port ""
+# existing_elk_cert ""
+# existing_elk_username ""
+# existing_elk_password ""
+backup_mount "{{ .BackupMount }}"
+# habitat_uid_gid ""
+###############################################################
+### Automate frontend node related settings                 ###
+###############################################################
+automate do
+  # admin_password "{{ .AutomateAdminPassword }}"
+  ### Leave commented out if using AWS infrastructure
+  # fqdn "{{ .AutomateFQDN }}"
+  instance_count {{ .AutomateInstanceCount }}
+  ### Uncomment and set this value if the teams service
+  ### port (default: 10128) conflicts with another service.
+  # teams_port "{{ .AutomateTeamsPort }}"
+  config_file "{{ .AutomateConfigFile }}"
+end
+
+###############################################################
+### Chef Server frontend node related settings              ###
+###############################################################
+chef_server do
+  instance_count {{ .ChefServerInstanceCount }}
+end
+
+###############################################################
+### Elasticsearch related settings                          ###
+###############################################################
+elasticsearch do
+  instance_count {{ .ElasticSearchInstanceCount }}
+end
+
+###############################################################
+### PostgreSQL related settings                             ###
+###############################################################
+postgresql do
+  instance_count {{ .PostgresqlInstanceCount }}
+end
+
+###############################################################
+### Only applies when using an existing node architecture   ###
+###############################################################
+existing_nodes do
+  automate_ips []
+  automate_private_ips []
+  chef_server_ips []
+  chef_server_private_ips []
+  elasticsearch_ips []
+  elasticsearch_private_ips []
+  postgresql_ips []
+  postgresql_private_ips []
+end
+`
+
+const awsA2harbTemplate = `
+secrets_key_file "{{ .SecretsKeyFile }}"
+secrets_store_file "{{ .SecretsStoreFile }}"
+architecture "{{ .Architecture }}"
+workspace_path "{{ .WorkspacePath }}"
+ssh_user "{{ .SshUser }}"
+ssh_key_file "{{ .SshKeyFile }}"
+# logging_monitoring_management "true"
+# new_elk "false"
+# existing_elk "false"
+backup_mount "{{ .BackupMount }}"
+# sudo_password ""
+# existing_elk_instance_ip ""
+# existing_elk_port ""
+# existing_elk_cert ""
+# existing_elk_username ""
+# existing_elk_password ""
+# habitat_uid_gid ""
+###############################################################
+### Automate frontend node related settings                 ###
+###############################################################
+automate do
+  instance_count 1
+  config_file "/src/configs/automate.toml"
+  # admin_password "{{ .AutomateAdminPassword }}"
+  ### Leave commented out if using AWS infrastructure
+  # fqdn "{{ .AutomateFQDN }}"
+  ### Uncomment and set this value if the teams service
+  ### port (default: 10128) conflicts with another service.
+  # teams_port ""
+end
+
+###############################################################
+### Chef Server frontend node related settings              ###
+###############################################################
+chef_server do
+  instance_count 1
+end
+
+###############################################################
+### Elasticsearch related settings                          ###
+###############################################################
+elasticsearch do
+  instance_count 3
+end
+
+###############################################################
+### PostgreSQL related settings                             ###
+###############################################################
+postgresql do
+  instance_count 3
+end
+
+###############################################################
+### Only applies when using AWS architecture                ###
+###############################################################
+aws do
+  ### AWS Credentials profile to use when deploying AWS infrastructure
+  profile "{{ .AwsProfile }}"
+  region "{{ .AwsRegion }}"
+  # ssh_key_pair_name "{{ .AwsSshKeyPairName }}"
+  ### Filter settings default to CentOS if left blank
+  # ami_filter_name ""
+  ### Filter settings default to CentOS if left blank
+  # ami_filter_virt_type ""
+  ### Filter settings default to CentOS if left blank
+  # ami_filter_owner ""
+  ### Overrides ami filter search features
+  # ami_id ""
+  ### EC2 instance type to use for Automate frontends, minimum >2G of RAM for test, 8G for prod
+  automate_server_instance_type "{{ .AwsAutomateServerInstaceType }}"
+  ### EC2 instance type to use for Chef Server frontends, minimum >2G of RAM for test, 8G for prod
+  chef_server_instance_type "{{ .AwsChefServerInstanceType }}"
+  ### EC2 instance type to use for Elasticsearch backends, minimum 8G of RAM for test, 16G for prod
+  elasticsearch_server_instance_type "{{ .AwsElasticSearchServerInstaceType }}"
+  ### EC2 instance type to use for PostgreSQL backends, minimum 4G of RAM for test, 8G for prod
+  postgresql_server_instance_type "{{ .AwsPostgresqlServerInstanceType }}"
+  ### AWS Certificate is specific to the region and AWS account this is being deployed to.
+  automate_lb_certificate_arn "{{ .AwsAutomateLBCertificateARN }}"
+  ### AWS Certificate is specific to the region and AWS account this is being deployed to.
+  chef_server_lb_certificate_arn "{{ .AwsChefServerLBCertificateARN }}"
+  automate_ebs_volume_iops "{{ .AwsAutomateEbsVolumeIops }}"
+  automate_ebs_volume_size "{{ .AwsAutomateEbsVolumeSize }}"
+  automate_ebs_volume_type "{{ .AwsAutomateEbsVolumeType }}"
+  chef_ebs_volume_iops "{{ .AwsChefEbsVolumeIops }}"
+  chef_ebs_volume_size "{{ .AwsChefEbsVolumeSize }}"
+  chef_ebs_volume_type "{{ .AwsChefEbsVolumeType }}"
+  elasticsearch_ebs_volume_iops "{{ .AwsEsEbsVolumeIops }}"
+  elasticsearch_ebs_volume_size "{{ .AwsEsEbsVolumeSize }}"
+  elasticsearch_ebs_volume_type "{{ .AwsEsEbsVolumeType }}"
+  postgresql_ebs_volume_iops "{{ .AwsPgsEbsVolumeIops }}"
+  postgresql_ebs_volume_size "{{ .AwsPgsEbsVolumeSize }}"
+  postgresql_ebs_volume_type "{{ .AwsPgsEbsVolumeType }}"
+  ### DEPRECATED: AWS Tag: Contact email to apply to AWS insfrastructure tags
+  # contact "{{ .AwsTagContact }}"
+  ### DEPRECATED: AWS Tag: Department name to apply to AWS insfrastructure tags
+  # dept "{{ .AwsTagDept }}"
+  ### DEPRECATED: AWS Tag: Project name to apply to AWS insfrastructure tags
+  # project "{{ .AwsTagProject }}"
+  tags({"X-Contact"=>"{{ .AwsTagContact }}", "X-Dept"=>"{{ .AwsTagDept }}", "X-Project"=>"{{ .AwsTagProject }}"})
+end
+`
