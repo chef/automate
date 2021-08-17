b5e4aeb736d72e3b206fb25babb0544d5e9748e8
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2021-08-17 12:53:13 +0530

    wait for sucess in data-feed pipline
    Signed-off-by: Shaik Mudassir <47217471+shaik80@users.noreply.github.com>

--- integration/helpers/setup_minio.sh
+++ integration/helpers/setup_minio.sh
@@ -16,3 +16,20 @@ setup_minio(){
     chmod +x minio
     ./minio server --console-address ":9565" /data
 }
+
+wait_for_success() {
+  local SECONDS_WAITING=${TIMEOUT:-60}
+  local COUNTER=0
+
+  until "$@" &> /dev/null; do
+    sleep 1
+
+    echo "Waiting for '$*' to succeed. ("$COUNTER of $SECONDS_WAITING")"
+
+    if [[ $COUNTER -ge "$SECONDS_WAITING" ]]; then
+      echo "Command '$*' never succeeded in  "$SECONDS_WAITING" seconds."
+      return 1
+    fi
+    (( COUNTER=COUNTER+1 ))
+  done
+}
