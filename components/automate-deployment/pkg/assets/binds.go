cb4c4da55069b444e7a1a4d310ec47f16f990d12
FaizanSRE <ffulara@msystechnologies.com>
2021-08-23 17:04:13 +0530

    binds.go changes
    Signed-off-by: FaizanSRE <ffulara@msystechnologies.com>

--- components/automate-deployment/pkg/assets/binds.go
+++ components/automate-deployment/pkg/assets/binds.go
@@ -8,6 +8,7 @@ authn-service REQUIRED authz-service automate-dex automate-pg-gateway cereal-ser
 authn-service BINDING_MODE strict
 authz-service REQUIRED automate-pg-gateway cereal-service pg-sidecar-service
 authz-service BINDING_MODE strict
+<<<<<<< HEAD
 automate-backend-elasticsearch BINDING_MODE strict
 automate-backend-haproxy OPTIONAL database pgleaderchk
 automate-backend-haproxy BINDING_MODE strict
@@ -18,6 +19,10 @@ automate-backend-kibana BINDING_MODE strict
 automate-backend-metricbeat REQUIRED elasticsearch
 automate-backend-metricbeat OPTIONAL database
 automate-backend-metricbeat BINDING_MODE strict
+=======
+automate-backend-kibana REQUIRED elasticsearch
+automate-backend-kibana BINDING_MODE strict
+>>>>>>> binds.go changes
 automate-builder-api REQUIRED automate-builder-memcached automate-minio automate-pg-gateway pg-sidecar-service session-service
 automate-builder-api BINDING_MODE strict
 automate-builder-api-proxy REQUIRED automate-builder-api
