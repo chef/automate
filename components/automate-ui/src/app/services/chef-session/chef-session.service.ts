d828971543771faf20f736833311b45788e44c7b
Abdul-Az <aazeez@progress.com>
2021-08-23 17:29:37 +0530

    id_token user check
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- components/automate-ui/src/app/services/chef-session/chef-session.service.ts
+++ components/automate-ui/src/app/services/chef-session/chef-session.service.ts
@@ -4,7 +4,7 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angul
 import { Store } from '@ngrx/store';
 import { NgrxStateAtom } from 'app/ngrx.reducers';
 import { Observable, ReplaySubject, timer, throwError } from 'rxjs';
-import { map, mergeMap, filter, catchError, retryWhen, delay } from 'rxjs/operators';
+import { map, mergeMap, filter, catchError } from 'rxjs/operators';
 import { isNull, isNil } from 'lodash';
 import { BroadcastChannel } from 'broadcast-channel';
 
@@ -85,10 +85,6 @@ export class ChefSessionService implements CanActivate {
         mergeMap(() => {
           this.isRefreshing = true;
           return this.refresh();
-        }),
-        retryWhen(error => {
-          this.isRefreshing = false;
-          return error.pipe(delay(500));  // retry in 500ms if errored
         })
       ).subscribe(
         token => {
@@ -105,6 +101,10 @@ export class ChefSessionService implements CanActivate {
   }
 
   private refresh(): Observable<string> {
+    if (!this.user) {
+      this.isRefreshing = false;
+      return throwError('User is not set');
+    }
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
