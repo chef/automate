39af737c68f0a774717327c4af27d1781743e28b
punitmundra <punit.mundra@progress.com>
2021-08-30 16:09:25 +0530

    comp move debug 2

--- components/automate-backend-pgleaderchk/habitat/plan.sh
+++ components/automate-backend-pgleaderchk/habitat/plan.sh
@@ -2,12 +2,12 @@
 pkg_name=automate-backend-pgleaderchk
 binary_name=pgleaderchk
 pkg_origin=chef
-pkg_repo=a2-ha-backend
+pkg_repo=automate
 pkg_version="0.1.2"
 pkg_description="Automate Backend PostreSQL leader check"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=('Chef-MLSA')
-pkg_upstream_url="http://github.com/chef/pipelinefix/components/automate-backend-pgleaderchk"
+pkg_upstream_url="http://github.com/chef/automate/components/automate-backend-pgleaderchk"
 pkg_deps=(
   core/bash
   core/curl
@@ -30,7 +30,7 @@ pkg_exposes=(port)
 pkg_bin_dirs=(bin)
 pkg_scaffolding="${local_scaffolding_origin:-chef}/automate-scaffolding-go"
 scaffolding_go_base_path=github.com/chef
-scaffolding_go_repo_name=pipelinefix
+scaffolding_go_repo_name=automate
 scaffolding_go_import_path="${scaffolding_go_base_path}/${scaffolding_go_repo_name}/components/${pkg_name}"
 scaffolding_go_binary_list=(
   "${scaffolding_go_import_path}/cmd/${pkg_name}"
