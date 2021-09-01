0da89d2db52ae1dae13788d439dd80a1bc71e9d2
Faizan <60912283+FaizanSRE@users.noreply.github.com>
2021-09-01 19:45:43 +0530

    Added automate-backend-haproxy (#5451)
    * added automate-backend-haproxy
    
    Signed-off-by: FaizanSRE <ffulara@msystechnologies.com>
    
    * gen.go and binds.go changes
    
    Signed-off-by: FaizanSRE <ffulara@msystechnologies.com>
    
    * modified .bldr.toml
    
    Signed-off-by: FaizanSRE <ffulara@msystechnologies.com>
    
    * removing the haproxy entry from products.meta
    
    Signed-off-by: FaizanSRE <ffulara@msystechnologies.com>

---
+++ components/automate-backend-haproxy/habitat/plan.sh
@@ -0,0 +1,45 @@
+# shellcheck disable=SC2148
+UPSTREAM_PKG_IDENT="core/haproxy"
+pkg_name="automate-backend-haproxy"
+pkg_description="Wrapper package for core/haproxy"
+vendor_origin="chef"
+pkg_maintainer="Chef Software Inc. <support@chef.io>"
+pkg_license=("Chef-MLSA")
+pkg_upstream_url="https://www.chef.io/automate"
+pkg_version="1.0.28"
+pkg_build_deps=("${UPSTREAM_PKG_IDENT}")
+pkg_deps=(
+  chef/mlsa
+  core/bash
+  core/netcat-openbsd
+  "${UPSTREAM_PKG_IDENT}"
+)
+
+
+pkg_exports=(
+  [port]=front-end.port
+  [status-port]=status.port
+)
+
+pkg_exposes=(port status-port)
+
+pkg_binds_optional=(
+  [database]="port superuser_name"
+  [pgleaderchk]="port"
+)
+
+do_before() {
+  update_pkg_version
+}
+
+do_download() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
