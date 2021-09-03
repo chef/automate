9741da7cad49bf30c4929a9612579a30e3e87f74
Vivek Shankar <vshankar@progress.com>
2021-09-03 18:43:12 +0530

    minio test
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- integration/helpers/setup_minio.sh
+++ integration/helpers/setup_minio.sh
@@ -3,6 +3,9 @@
 export MINIO_LOG_FILE=/tmp/automate-MINIO.log
 
 start_minio_background() {
+  rm -f minio
+  wget https://dl.min.io/server/minio/release/linux-amd64/minio
+  chmod +x minio
   setup_minio >> $MINIO_LOG_FILE 2>&1 &
 }
 
@@ -11,9 +14,6 @@ minio_logs() {
 }
 
 setup_minio(){
-    rm -f minio
-    wget https://dl.min.io/server/minio/release/linux-amd64/minio
-    chmod +x minio
     ./minio server --console-address ":9565" /data
 }
 
