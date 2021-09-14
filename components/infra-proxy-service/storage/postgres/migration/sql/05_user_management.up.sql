e37ec35ff6285c0d4bd4199a8a00803d0ba6ea30
root <swale@msystechnologies.com>
2021-09-14 15:01:02 +0530

    updated up script
    Signed-off-by: root <swale@msystechnologies.com>

--- components/infra-proxy-service/storage/postgres/migration/sql/05_user_management.up.sql
+++ components/infra-proxy-service/storage/postgres/migration/sql/05_user_management.up.sql
@@ -10,6 +10,8 @@ CREATE TABLE IF NOT EXISTS users (
   connector                 TEXT NOT NULL DEFAULT 'local',
   automate_user_id          TEXT NOT NULL DEFAULT '',
   is_server_admin           BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at                TIMESTAMPTZ NOT NULL,
+  updated_at                TIMESTAMPTZ NOT NULL,
   CONSTRAINT infra_server_username_server_id_key
   UNIQUE(infra_server_username,server_id)
 );
