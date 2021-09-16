b5a2b93cc2de17d9305c2db93a5aa8b0efdef749
sonali523 <86949270+sonali523@users.noreply.github.com>
2021-09-16 23:30:03 +0530

    Script added for table 'org_users' (#5738)
    Signed-off-by: root <swale@msystechnologies.com>

--- components/infra-proxy-service/storage/postgres/migration/sql/05_user_management.down.sql
+++ components/infra-proxy-service/storage/postgres/migration/sql/05_user_management.down.sql
@@ -1,5 +1,8 @@
 -- drop column projects in servers table 
 ALTER TABLE servers DROP COLUMN projects;
 
+-- drop table org_users
+DROP TABLE IF EXISTS org_users;
+
 -- drop table users
 DROP TABLE IF EXISTS users;
