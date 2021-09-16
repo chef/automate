27e27b6140d1fd016b121135ce13323e9299a3e6
jay vikram sharma <jsharma@progress.com>
2021-09-16 13:15:56 +0530

    Signed-off-by: jay vikram sharma <jsharma@progress.com> printing multi-line output of automate-cluster-ctl deploy to console in chef-automate deploy command mappings

--- components/automate-cli/cmd/chef-automate/deploy_ha.go
+++ components/automate-cli/cmd/chef-automate/deploy_ha.go
@@ -3,9 +3,10 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"os"
 	"os/exec"
-	"strings"
 
 	"github.com/chef/automate/components/automate-cli/pkg/status"
 	ptoml "github.com/pelletier/go-toml"
@@ -37,15 +38,24 @@ func isA2HADeployment(configPath []string) bool {
 
 func deployA2HA() error {
 	writer.Printf("A2HA deployment started \n")
-	cmd, b := exec.Command("automate-cluster-ctl", "deploy"), new(strings.Builder)
-	cmd.Dir = "/hab/a2_deploy_workspace"
-	cmd.Stdout = b
-	err := cmd.Start()
+	//args := []string{"deploy"}
+	c := exec.Command("automate-cluster-ctl", "deploy")
+	c.Dir = "/hab/a2_deploy_workspace"
+	c.Stdin = os.Stdin
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	c.Stdout = io.MultiWriter(os.Stdout, &out)
+	c.Stderr = io.MultiWriter(os.Stderr, &stderr)
+	err := c.Run()
 	if err != nil {
-		return status.Annotate(err, status.CommandExecutionError)
+		writer.Printf(stderr.String())
+		return status.Wrap(err, status.CommandExecutionError, "deployment failed")
+	} else {
+		writer.Printf("No error in executing commands")
 	}
-	writer.Print(b.String())
-	writer.Printf("A2HA deployment done. %d, exiting\n", cmd.Process.Pid)
+	outStr, errStr := string(out.Bytes()), string(stderr.Bytes())
+	writer.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+	writer.Printf("A2HA deployment done. exiting\n")
 	return err
 }
 
