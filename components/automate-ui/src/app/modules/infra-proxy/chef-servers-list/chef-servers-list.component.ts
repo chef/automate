440cc0406c4ee9984ff2f6ca097fdfea13bbcfc8
vinay sharma <vsharma@chef.io>
2021-09-20 20:09:47 +0530

    Add changes for the create server slider (#5755)
    * added some changes for the create server slider
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>
    
    * added some minor changes
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>

--- components/automate-ui/src/app/modules/infra-proxy/chef-servers-list/chef-servers-list.component.ts
+++ components/automate-ui/src/app/modules/infra-proxy/chef-servers-list/chef-servers-list.component.ts
@@ -137,8 +137,7 @@ export class ChefServersListComponent implements OnInit, OnDestroy {
       ip_address: this.ipForm.controls['ip_address'].value?.trim() || ''
     };
     this.store.dispatch(new CreateServer(server));
-    this.creatingChefServer = false;
-    this.createChefServerForm.reset();
+    this.resetCreateModal();
   }
 
   private resetCreateModal(): void {
