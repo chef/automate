e778315ab0a4b4e40526fc15588575d510c73a51
Shivangi Singh <shisingh@progress.com>
2021-09-21 21:07:25 +0530

    Indentation changes
    Signed-off-by: Shivangi Singh <shisingh@progress.com>

--- e2e/cypress/integration/api/nodemanager/scan_job.spec.ts
+++ e2e/cypress/integration/api/nodemanager/scan_job.spec.ts
@@ -19,32 +19,31 @@ let x = 0;
   });
 
   it('add 110 nodes on Scan jobs page', () => {
-      cy.request({
-        headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
-        method: 'POST',
-        url: 'api/v0/nodes/bulk-create',
-        body: {
-          nodes: nodejson()
-        }
-      }).then((response) => {
-        expect(response.body.ids).to.have.length(110);
-      });
+    cy.request({
+      headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+      method: 'POST',
+      url: 'api/v0/nodes/bulk-create',
+      body: {
+        nodes: nodejson()
+      }
+    }).then((response) => {
+      expect(response.body.ids).to.have.length(110);
+    });
   });
 
-   it('check total node count displayed on automate manager', () => {
-      cy.request({
+  it('check total node count displayed on automate manager', () => {
+    cy.request({
       headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
       method: 'POST',
       url: 'api/v0/nodemanagers/id/e69dc612-7e67-43f2-9b19-256afd385820/search-nodes',
       body: {
-              query: {
-              filter_map: []
-          }
+        query: {
+          filter_map: []
+        }
       }
     }).then((response) => {
-          expect(response.body.nodes).to.have.length(x + 110);
-        });
-   });
-
+      expect(response.body.nodes).to.have.length(x + 110);
+    });
+  });
 });
 
