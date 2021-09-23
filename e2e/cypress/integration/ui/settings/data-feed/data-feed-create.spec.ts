0d42616ed070d9ec718b66571faa45b544c11e26
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2021-09-23 22:24:54 +0530

    S3 and Minio Details page (#5778)
    * data-feed-details page for s3 and minio
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * cypress and unit test cases
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * cypress test
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * css lint chages
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * added enums
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * login moved to ts file
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * dropdown size
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * dropdown
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * dropdown changes
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * drop-down option size
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * reslove indentation issue
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>
    
    * line height
    
    Signed-off-by: shaik80 <shaik.mudassir@progress.com>

--- e2e/cypress/integration/ui/settings/data-feed/data-feed-create.spec.ts
+++ e2e/cypress/integration/ui/settings/data-feed/data-feed-create.spec.ts
@@ -374,4 +374,142 @@ describe('chef datafeed', () => {
       cy.get('[data-cy=close-feed-button]').click();
     });
   });
+  describe ('chef data feed page', () => {
+    const reusableDate = Date.now();
+
+    it('check if clicking on new integration button opens up the slider', () => {
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=interation-menu]').should('be.visible');
+      cy.get('[data-cy=close-feed-button]').click();
+    });
+
+    it('check if clicking on a interation opens the form', () => {
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=ServiceNow]').click();
+      cy.get('[data-cy=data-feed-create-form]').should('be.visible');
+      cy.get('[data-cy=close-feed-button]').click();
+    });
+
+    it('create data feed service now', () => {
+      const date = Date.now();
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=ServiceNow]').click();
+      cy.get('[data-cy=add-name]').type(name + date);
+      cy.get('[data-cy=add-url]').type(url);
+      cy.get('[data-cy=select-auth-type]').click();
+      cy.get('[data-cy=select-username-password]').click();
+      cy.get('[data-cy=add-username]').type(username);
+      cy.get('[data-cy=add-password]').type(password);
+      cy.get('[data-cy=add-button]').click();
+      cy.get('app-notification.info').should('be.visible');
+      cy.get('app-notification.info chef-icon').click();
+      cy.contains('Data Feeds').click();
+      cy.get('chef-table chef-tbody chef-td').contains('cytest' + date).should('exist');
+    });
+
+    it('create data feed splunk', () => {
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=Splunk]').click();
+      cy.get('[data-cy=add-name]').type(name + reusableDate);
+      cy.get('[data-cy=add-url]').type(url);
+      cy.get('[data-cy=add-token]').type(token);
+      cy.get('[data-cy=add-button]').click();
+      cy.get('app-notification.info').should('be.visible');
+      cy.get('app-notification.info chef-icon').click();
+      cy.contains('Data Feeds').click();
+      cy.get('chef-table chef-tbody chef-td').contains('cytest' + reusableDate).should('exist');
+    });
+
+    it('create data feed error', () => {
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=Splunk]').click();
+      cy.get('[data-cy=add-name]').type(name + reusableDate);
+      cy.get('[data-cy=add-url]').type(url);
+      cy.get('[data-cy=add-token]').type(token);
+      cy.get('[data-cy=add-button]').click();
+      cy.get('app-data-feed-create').scrollTo('top');
+      cy.get('.data-feed-slider app-notification.error').should('be.visible');
+      cy.get('.data-feed-slider app-notification.error chef-icon').click();
+      cy.get('[data-cy=close-feed-button]').click();
+    });
+
+    it('create data feed with changed token type', () => {
+      const date = Date.now();
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=Splunk]').click();
+      cy.get('[data-cy=add-name]').type(name + date);
+      cy.get('[data-cy=add-url]').type(url);
+      cy.get('[data-cy=toggle-type]').click();
+      cy.get('[data-cy=add-token-type]').clear();
+      cy.get('[data-cy=add-token-type]').type(tokenType);
+      cy.get('[data-cy=add-token]').type(token);
+      cy.get('[data-cy=add-button]').click();
+      cy.get('app-notification.info').should('be.visible');
+      cy.get('app-notification.info chef-icon').click();
+      cy.contains('Data Feeds').click();
+      cy.get('chef-table chef-tbody chef-td').contains('cytest' + date).should('exist');
+    });
+
+    it('test error in data feed', () => {
+      const date = Date.now();
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=Splunk]').click();
+      cy.get('[data-cy=add-name]').type(name + date);
+      cy.get('[data-cy=add-url]').type(url);
+      cy.get('[data-cy=add-token]').type(token);
+      cy.get('[data-cy=test-button]').click();
+      cy.get('app-data-feed-create').scrollTo('top');
+      cy.get('.data-feed-slider app-notification.error').should('be.visible');
+      cy.get('.data-feed-slider app-notification.error chef-icon').click();
+      cy.get('[data-cy=close-feed-button]').click();
+    });
+
+    it('create data feed minio', () => {
+      const date = Date.now();
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=Minio]').click();
+      cy.get('[data-cy=add-name]').type(name + date);
+      cy.get('[data-cy=add-endpoint]').type(endpoint);
+      cy.get('[data-cy=add-bucket-name]').type(bucketName);
+      cy.get('[data-cy=add-access-key]').type(accessKey);
+      cy.get('[data-cy=add-secret-key]').type(secretKey);
+      cy.get('[data-cy=add-button]').click();
+      cy.get('app-notification.info').should('be.visible');
+      cy.get('app-notification.info chef-icon').click();
+      cy.contains('Data Feeds').click();
+      cy.get('chef-table chef-tbody chef-td').contains('cytest' + date).should('exist');
+    });
+
+    it('test error in data feed minio failure', () => {
+      const date = Date.now();
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=Minio]').click();
+      cy.get('[data-cy=add-name]').type(name + date);
+      cy.get('[data-cy=add-endpoint]').type(endpoint);
+      cy.get('[data-cy=add-bucket-name]').type(bucketName);
+      cy.get('[data-cy=add-access-key]').type(accessKey);
+      cy.get('[data-cy=add-secret-key]').type(secretKey);
+      cy.get('[data-cy=test-button]').click();
+      cy.get('app-data-feed-create').scrollTo('top');
+      cy.get('.data-feed-slider app-notification.error').should('be.visible');
+      cy.get('.data-feed-slider app-notification.error chef-icon').click();
+      cy.get('[data-cy=close-feed-button]').click();
+    });
+
+    it('test connection in data feed minio success', () => {
+      const date = Date.now();
+      cy.get('[data-cy=create-data-feed]').click();
+      cy.get('[data-cy=Minio]').click();
+      cy.get('[data-cy=add-name]').type(name + date);
+      cy.get('[data-cy=add-endpoint]').type(minioUrl);
+      cy.get('[data-cy=add-bucket-name]').type(minioBucket);
+      cy.get('[data-cy=add-access-key]').type(minioAccess);
+      cy.get('[data-cy=add-secret-key]').type(minioSecret);
+      cy.get('[data-cy=test-button]').click();
+      cy.get('app-data-feed-create').scrollTo('top');
+      cy.get('.data-feed-slider app-notification.info').should('be.visible');
+      cy.get('.data-feed-slider app-notification.info chef-icon').click();
+      cy.get('[data-cy=close-feed-button]').click();
+    });
+  });
 });
