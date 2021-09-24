7ad827086fd3cd81ad5203545ded92c38b945eed
root <swale@msystechnologies.com>
2021-09-24 18:19:42 +0530

    Updated up script for deployment table
    Signed-off-by: root <swale@msystechnologies.com>

--- components/license-control-service/migrations/001_deployment.up.sql
+++ components/license-control-service/migrations/001_deployment.up.sql
@@ -3,8 +3,7 @@ BEGIN;
 -- create table deployment
 CREATE TABLE IF NOT EXISTS deployment (
   id          TEXT PRIMARY KEY,
-  created_at  TIMESTAMPTZ NOT NULL,
-  updated_at  TIMESTAMPTZ NOT NULL
+  created_at  TIMESTAMPTZ NOT NULL
 );
 
 COMMIT;
