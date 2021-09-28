1a4c0d2d8790e3e4e5432175a8640dec5973aa49
Abdul-Az <aazeez@progress.com>
2021-09-28 15:41:44 +0530

    test cases fix
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- e2e/cypress/integration/api/iam/infra_server_actions_integration.spec.ts
+++ e2e/cypress/integration/api/iam/infra_server_actions_integration.spec.ts
@@ -1,48 +1,48 @@
-let withInfraServersListActionToken = '';
-let withoutInfraServersListActionToken = '';
-
-const cypressPrefix = 'infra-server-actions';
-const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
-const policyId2 = `${cypressPrefix}-pol-2-${Cypress.moment().format('MMDDYYhhmm')}`;
-const tokenId1 = `${cypressPrefix}-token-1-${Cypress.moment().format('MMDDYYhhmm')}`;
-const tokenId2 = `${cypressPrefix}-token-2-${Cypress.moment().format('MMDDYYhhmm')}`;
-const objectsToCleanUp = ['tokens', 'policies'];
-
-const withInfraServersListPolicy = {
-  id: policyId1,
-  name: tokenId1,
-  projects: [],
-  members: [`token:${tokenId1}`],
-  statements: [
-    {
-        effect: "ALLOW",
-        actions: [
-            "infra:infraServers:list",
-            "infra:nodes:list"
-        ],
-        projects: ["*"]
-    }]
-};
-
-
-const withoutInfraServersListPolicy = {
-    id: policyId2,
-    name: tokenId2,
-    projects: [],
-    members: [`token:${tokenId2}`],
-    statements: [
-      {
-          effect: "DENY",
-          actions: [
-            "infra:infraServers:list",
-            "infra:nodes:list"
-          ],
-          projects: ["*"]
-      }]
-  };
-
 // these tests are best read sequentially, as they share state
 describe('Infra servers list api', () => {
+    let withInfraServersListActionToken = '';
+    let withoutInfraServersListActionToken = '';
+
+    const cypressPrefix = 'infra-server-actions';
+    const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const policyId2 = `${cypressPrefix}-pol-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId1 = `${cypressPrefix}-token-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId2 = `${cypressPrefix}-token-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const objectsToCleanUp = ['tokens', 'policies'];
+
+    const withInfraServersListPolicy = {
+    id: policyId1,
+    name: tokenId1,
+    projects: [],
+    members: [`token:${tokenId1}`],
+    statements: [
+        {
+            effect: "ALLOW",
+            actions: [
+                "infra:infraServers:list",
+                "infra:nodes:list"
+            ],
+            projects: ["*"]
+        }]
+    };
+
+
+    const withoutInfraServersListPolicy = {
+        id: policyId2,
+        name: tokenId2,
+        projects: [],
+        members: [`token:${tokenId2}`],
+        statements: [
+        {
+            effect: "DENY",
+            actions: [
+                "infra:infraServers:list",
+                "infra:nodes:list"
+            ],
+            projects: ["*"]
+        }]
+    };
+
     before(() => {
         // TODO cleanup projects in before block (can't do now bc we have a project
         // limit and cereal runs async to delete policies)
@@ -93,28 +93,29 @@ describe('Infra servers list api', () => {
             expect(resp.status).to.equal(200);
           });
         })
+        after(() => {
+            cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
+          });
+    
+        it('infra servers list returns 200 when infraServers list actions is allowed', () => {
+        cy.request({
+            headers: { 'api-token': withInfraServersListActionToken, 'content-type': 'application/json+lax' },
+            method: 'GET',
+            url: '/api/v0/infra/servers'
+        }).then((resp) => {
+            assert.equal(resp.status, 200);
+            });
+        }); 
+    
+        it('infra servers list returns 403 when infraServers list actions is deneyed', () => {
+            cy.request({
+                headers: { 'api-token': withoutInfraServersListActionToken, 'content-type': 'application/json+lax' },
+                method: 'GET',
+                failOnStatusCode: false,
+                url: '/api/v0/infra/servers'
+            }).then((resp) => {
+                assert.equal(resp.status, 403);
+            });
+        }); 
     }) 
     
-    after(() => {
-        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
-      });
-
-    it(`infra servers list returns 200 when infraServers list actions is allowed`, () => {
-    cy.request({
-        headers: { 'api-token': withInfraServersListActionToken, 'content-type': 'application/json+lax' },
-        method: 'GET',
-        url: `/api/v0/infra/servers`
-    }).then((resp) => {
-        assert.equal(resp.status, 200);
-        });
-    }); 
-
-    it(`infra servers list returns 401 when infraServers list actions is deneyed`, () => {
-        cy.request({
-            headers: { 'api-token': withoutInfraServersListActionToken, 'content-type': 'application/json+lax' },
-            method: 'GET',
-            url: `/api/v0/infra/servers`
-        }).then((resp) => {
-            assert.equal(resp.status, 401);
-        });
-    }); 
