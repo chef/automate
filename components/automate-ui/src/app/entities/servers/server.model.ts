8fe9b8167c7bcedd91f8892b6490d56082b2f984
Chaitali Mane <71449322+chaitali-mane@users.noreply.github.com>
2021-09-28 11:35:05 +0530

    Added users tab in server details page (#5802)
    * Added Users tab UI
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Added enitites api integration
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * minor changes
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Updated minor change
    
    Signed-off-by: chaitali-mane <cmane@progress.com>

--- components/automate-ui/src/app/entities/servers/server.model.ts
+++ components/automate-ui/src/app/entities/servers/server.model.ts
@@ -5,3 +5,13 @@ export interface Server {
   ip_address: string;
   orgs_count?: number;
 }
+
+export interface User {
+  id: string;
+  server_id: string;
+  infra_server_username: string;
+  credential_id: string;
+  connector: string;
+  automate_user_id: string;
+  is_server_admin: boolean;
+}
