7a6ddf4723cfb8fe1a2f453185e5d5c4cf220939
Abdul-Az <aazeez@progress.com>
2021-09-28 15:46:15 +0530

    lint fixes
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- e2e/cypress/integration/api/iam/infra_server_actions_integration.spec.ts
+++ e2e/cypress/integration/api/iam/infra_server_actions_integration.spec.ts
@@ -1,5 +1,5 @@
 // these tests are best read sequentially, as they share state
-describe('Infra servers list api', () => {
+describe('Infra server list api', () => {
     let withInfraServersListActionToken = '';
     let withoutInfraServersListActionToken = '';
 
