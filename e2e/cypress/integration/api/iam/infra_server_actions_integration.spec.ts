d21186090689eb2770d4c2ea1c1f2e9beb4c5559
Abdul-Az <aazeez@progress.com>
2021-09-28 15:41:44 +0530

    test cases fix
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- e2e/cypress/integration/api/iam/infra_server_actions_integration.spec.ts
+++ e2e/cypress/integration/api/iam/infra_server_actions_integration.spec.ts
@@ -1,48 +1,48 @@
-let withInfraServersListActionToken = '';
-let withoutInfraServersListActionToken = '';
-
-const cypressPrefix = 'infra-server-actions';
-const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
-const policyId2 = `${cypressPrefix}-pol-2-${Cypress.moment().format('MMDDYYhhmm')}`;
-const tokenId1 = `${cypressPrefix}-token-1-${Cypress.moment().format('MMDDYYhhmm')}`;
-const tokenId2 = `${cypressPrefix}-token-2-${Cypress.moment().format('MMDDYYhhmm')}`;
-const objectsToCleanUp = ['tokens', 'policies'];
-
-const withInfraServersListPolicy = {
-  id: policyId1,
-  name: tokenId1,
-  projects: [],
-  members: [`token:${tokenId1}`],
-  statements: [
-    {
-        effect: "ALLOW",
-        actions: [
-            "infra:infraServers:list",
-            "infra:nodes:list"
-        ],
-        projects: ["*"]
-    }]
-};
-
-
-const withoutInfraServersListPolicy = {
-    id: policyId2,
-    name: tokenId2,
-    projects: [],
-    members: [`token:${tokenId2}`],
-    statements: [
-      {
-          effect: "DENY",
-          actions: [
-            "infra:infraServers:list",
-            "infra:nodes:list"
-          ],
-          projects: ["*"]
-      }]
-  };
-
 // these tests are best read sequentially, as they share state
 describe('Infra servers list api', () => {
+    let withInfraServersListActionToken = '';
+    let withoutInfraServersListActionToken = '';
+
+    const cypressPrefix = 'infra-server-actions';
+    const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const policyId2 = `${cypressPrefix}-pol-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId1 = `${cypressPrefix}-token-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId2 = `${cypressPrefix}-token-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const objectsToCleanUp = ['tokens', 'policies'];
+
+    const withInfraServersListPolicy = {
+    id: policyId1,
+    name: tokenId1,
+    projects: [],
+    members: [`token:${tokenId1}`],
+    statements: [
+        {
+            effect: "ALLOW",
+            actions: [
+                "infra:infraServers:list",
+                "infra:nodes:list"
+            ],
+            projects: ["*"]
+        }]
+    };
+
+
+    const withoutInfraServersListPolicy = {
+        id: policyId2,
+        name: tokenId2,
+        projects: [],
+        members: [`token:${tokenId2}`],
+        statements: [
+        {
+            effect: "DENY",
+            actions: [
+                "infra:infraServers:list",
+                "infra:nodes:list"
+            ],
+            projects: ["*"]
+        }]
+    };
+
     before(() => {
         // TODO cleanup projects in before block (can't do now bc we have a project
         // limit and cereal runs async to delete policies)
