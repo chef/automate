9a1795d9b3a0f2f2d7f8cd7125e8ea2d4f2335a2
Durga Sarat Chandra Maddu <44021874+Dmaddu@users.noreply.github.com>
2021-09-30 18:33:49 +0530

    Adding initial setup code for report manager service and report manager minio gateway (#5729)
    Signed-off-by: Durga Sarat Chandra Maddu <durgasaratchandra.maddu@progress.com>

---
+++ components/report-manager-minio-gateway/habitat/plan.sh
@@ -0,0 +1,34 @@
+pkg_name=report-manager-minio-gateway
+pkg_version="0.1.0"
+pkg_origin=chef
+pkg_maintainer="Chef Software Inc. <support@chef.io>"
+pkg_license=('Apache-2.0')
+pkg_deps=(
+  core/minio
+  core/bash
+  core/curl
+  core/cacerts
+  core/coreutils
+  chef/automate-platform-tools
+)
+
+pkg_exports=(
+  [port]=service.port
+)
+pkg_exposes=(port)
+
+do_unpack() {
+    return 0
+}
+
+do_build(){
+    return 0
+}
+
+do_install() {
+    return 0
+}
+
+do_strip() {
+    return 0
+}
