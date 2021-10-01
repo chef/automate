1a902eaffd546b0f4f0c8a749c1c4ae194906008
Abdul-Az <aazeez@progress.com>
2021-10-01 13:20:53 +0530

    wip
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- e2e/cypress/integration/api/iam/infra_server_orgs_actions.spec.ts
+++ e2e/cypress/integration/api/iam/infra_server_orgs_actions.spec.ts
@@ -1,7 +1,90 @@
+describe('chef orgs list', () => {
+    const serverID = 'chef-server-dev-test';
+    const serverName = 'chef server dev';
+    const orgID = 'chef-org-dev';
+    const orgName = '4thcoffee';
+    const serverFQDN = 'ec2-34-219-25-251.us-west-2.compute.amazonaws.com';
+    const serverIP = '34.219.25.251';
+    const adminUser = 'chefadmin';
+    const adminKey = Cypress.env('AUTOMATE_INFRA_ADMIN_KEY').replace(/\\n/g, '\n');
+  
+    before(() => {
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/api/v0/infra/servers',
+            body: {
+              id: serverID,
+              name: serverName,
+              fqdn: serverFQDN,
+              ip_address: serverIP
+            }
+          })
+
+        cy.request({
+          headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+          method: 'POST',
+          url: `/api/v0/infra/servers/${serverID}/orgs`,
+          body: {
+            id: orgID,
+            server_id: serverID,
+            name: orgName,
+            admin_user: adminUser,
+            admin_key: adminKey
+          }
+        })
+
+      });
+      cy.restoreStorage();
+    });
+  
+    beforeEach(() => {
+      cy.restoreStorage();
+    });
+  
+    afterEach(() => {
+      cy.saveStorage();
+    });
+  
+    describe('chef organizations details page', () => {
+      it('displays org details', () => {
+        cy.get('.page-title').contains(orgName);
+      });
+  
+      it('on page load Cookbook is in active state', () => {
+        cy.get('.nav-tab').contains('Cookbooks').should('have.class', 'active');
+      });
+  
+      tabNames.forEach((val) => {
+        it(`can switch to ${val} tab`, () => {
+            cy.get('.nav-tab').contains(val).click();
+        });
+      });
+  
+      it('lists of Cookbook', () => {
+        cy.get('.cookbooks').then(($cookbook) => {
+          if ($cookbook.hasClass('empty-section')) {
+            cy.get('[data-cy=cookbooks-table-container]').should('not.be.visible');
+              cy.get('.empty-section').should('be.visible');
+              cy.get('.empty-section p').contains('No cookbooks available');
+          } else {
+            cy.get('[data-cy=cookbooks-table-container] chef-th').contains('Name');
+            cy.get('[data-cy=cookbooks-table-container] chef-th').contains('Cookbook Version');
+          }
+        });
+      });
+  
+    });
+  });
+
 // these tests are best read sequentially, as they share state
 describe('Infra servers list api', () => {
     let withInfraServersListActionToken = '';
     let withoutInfraServersListActionToken = '';
+    const orgID = 'chef-org-dev';
+    const orgName = '4thcoffee';
+    const adminUser = 'chefadmin';
+    const adminKey = Cypress.env('AUTOMATE_INFRA_ADMIN_KEY').replace(/\\n/g, '\n');
 
     const cypressPrefix = 'infra-server-actions-list';
     const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
