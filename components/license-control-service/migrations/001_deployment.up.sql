5cf6e7037daebe137ffc650f05d1ca5d790eca71
sonali523 <86949270+sonali523@users.noreply.github.com>
2021-10-04 23:31:20 +0530

    Store the unique id for every deployment in DB (#5829)
    * Changes added to store deployment id in license-control-service
    
    Signed-off-by: root <swale@msystechnologies.com>
    
    * Deployment up script updated
    
    Signed-off-by: root <swale@msystechnologies.com>
    
    * Added storage function
    
    Signed-off-by: root <swale@msystechnologies.com>

--- components/license-control-service/migrations/001_deployment.up.sql
+++ components/license-control-service/migrations/001_deployment.up.sql
@@ -3,7 +3,8 @@ BEGIN;
 -- create table deployment
 CREATE TABLE IF NOT EXISTS deployment (
   id          TEXT PRIMARY KEY,
-  created_at  TIMESTAMPTZ NOT NULL
+  created_at  TIMESTAMPTZ NOT NULL,
+  updated_at  TIMESTAMPTZ NOT NULL
 );
 
 COMMIT;
