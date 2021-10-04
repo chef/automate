12bd61cf5a0a304e25281228b7f2de308d4fd72c
Kallol Roy <karoy@progress.com>
2021-10-04 15:38:47 +0530

    Remove the cacert for DST RootCA X3
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- components/automate-load-balancer/habitat/plan.sh
+++ components/automate-load-balancer/habitat/plan.sh
@@ -54,6 +54,7 @@ pkg_binds_optional=(
 )
 
 do_download() {
+  cp -f cacert.pem /hab/pkgs/core/cacerts/2020.01.01/20200306005234/ssl/certs/cacert.pem
   do_default_download
   pushd "${HAB_CACHE_SRC_PATH}" || return 1
   download_file "${headers_more_source}" "${headers_more_filename}" "${headers_more_shasum}"
