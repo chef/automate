8daabcf91e91c4b4a6e72dcec560b70af11b8436
Vivek Yadav <vivek.yadav@progress.com>
2021-10-04 19:38:05 +0530

    added postgresql: in regex
    Signed-off-by: Vivek Yadav <vivek.yadav@progress.com>

--- components/data-feed-service/config/mask.go
+++ components/data-feed-service/config/mask.go
@@ -5,8 +5,8 @@ import (
 )
 
 func MaskPGCredInURI(s string) string {
-	selector := regexp.MustCompile("^(.*?)//(.*):(.*)@(.*)$")
-	replaceRegex := "${1}//<USER>:<PASSWORD>@${4}"
+	selector := regexp.MustCompile("^(.*?)postgresql://(.*):(.*)@(.*)$")
+	replaceRegex := "${1}postgresql://<USER>:<PASSWORD>@${4}"
 	maskedPGCredInURI := selector.ReplaceAllString(s, replaceRegex)
 	return maskedPGCredInURI
 }
--- components/data-feed-service/config/mask_test.go
+++ components/data-feed-service/config/mask_test.go
@@ -9,19 +9,22 @@ import (
 )
 
 func TestMaskPGCredInURI(t *testing.T) {
-	configData := &config.DataFeedConfig{PostgresConfig: config.PostgresConfig{ConnectionString: "postgres://user:pass@127.0.0.1:10145"}}
+	configData := &config.DataFeedConfig{
+		CerealConfig:   config.CerealConfig{Target: "cereal://user:pass@127.0.01:3333"},
+		PostgresConfig: config.PostgresConfig{ConnectionString: "postgresql://user:pass@127.0.0.1:10145"},
+	}
 	fullConfig := fmt.Sprintf("DATA FEED SERVICE CONFIG: %+v", configData)
 	tests := []struct {
 		source   string
 		expected string
 	}{
-		{source: "postgres://user:pass@127.0.0.1:10145", expected: "postgres://<USER>:<PASSWORD>@127.0.0.1:10145"},
-		{source: "postgres://user:@127.0.0.1:10145", expected: "postgres://<USER>:<PASSWORD>@127.0.0.1:10145"},
-		{source: "postgres://:pass@127.0.0.1:10145", expected: "postgres://<USER>:<PASSWORD>@127.0.0.1:10145"},
-		{source: "postgres://127.0.0.1:10145", expected: "postgres://127.0.0.1:10145"},
-		{source: "postgres://:@127.0.0.1:10145", expected: "postgres://<USER>:<PASSWORD>@127.0.0.1:10145"},
-		{source: "postgres://user:pass127.0.0.1:10145", expected: "postgres://user:pass127.0.0.1:10145"},
-		{source: fullConfig, expected: "DATA FEED SERVICE CONFIG: &{ServiceConfig:{Host: Port:0 FeedInterval:0s AssetPageSize:0 ReportsPageSize:0 NodeBatchSize:0 UpdatedNodesOnly:false DisableCIDRFilter:false CIDRFilter: ExternalFqdn: AcceptedStatusCodes:[] ContentType:} LogConfig:{LogLevel: LogFormat:} TLSConfig:{CertPath: KeyPath: RootCACertPath:} SecretsConfig:{Target:} CfgmgmtConfig:{Target:} ComplianceConfig:{Target:} CerealConfig:{Target:} PostgresConfig:{ConnectionString:postgres://<USER>:<PASSWORD>@127.0.0.1:10145 Database: MigrationsPath:} ServiceCerts:<nil>}"},
+		{source: "postgresql://user:pass@127.0.0.1:10145", expected: "postgresql://<USER>:<PASSWORD>@127.0.0.1:10145"},
+		{source: "postgresql://user:@127.0.0.1:10145", expected: "postgresql://<USER>:<PASSWORD>@127.0.0.1:10145"},
+		{source: "postgresql://:pass@127.0.0.1:10145", expected: "postgresql://<USER>:<PASSWORD>@127.0.0.1:10145"},
+		{source: "postgresql://127.0.0.1:10145", expected: "postgresql://127.0.0.1:10145"},
+		{source: "postgresql://:@127.0.0.1:10145", expected: "postgresql://<USER>:<PASSWORD>@127.0.0.1:10145"},
+		{source: "postgresql://user:pass127.0.0.1:10145", expected: "postgresql://user:pass127.0.0.1:10145"},
+		{source: fullConfig, expected: "DATA FEED SERVICE CONFIG: &{ServiceConfig:{Host: Port:0 FeedInterval:0s AssetPageSize:0 ReportsPageSize:0 NodeBatchSize:0 UpdatedNodesOnly:false DisableCIDRFilter:false CIDRFilter: ExternalFqdn: AcceptedStatusCodes:[] ContentType:} LogConfig:{LogLevel: LogFormat:} TLSConfig:{CertPath: KeyPath: RootCACertPath:} SecretsConfig:{Target:} CfgmgmtConfig:{Target:} ComplianceConfig:{Target:} CerealConfig:{Target:cereal://user:pass@127.0.01:3333} PostgresConfig:{ConnectionString:postgresql://<USER>:<PASSWORD>@127.0.0.1:10145 Database: MigrationsPath:} ServiceCerts:<nil>}"},
 	}
 
 	for _, test := range tests {
