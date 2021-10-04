f597749600164abb35882fac95b54aee0adcc27f
Vivek Yadav <vivek.yadav@progress.com>
2021-10-04 18:07:25 +0530

    mask PG cred in URI
    Signed-off-by: Vivek Yadav <vivek.yadav@progress.com>

---
+++ lib/stringutils/mask.go
@@ -0,0 +1,12 @@
+package stringutils
+
+import (
+	"regexp"
+)
+
+func MaskPGCredInURI(s string) string {
+	selector := regexp.MustCompile("^(.*?)//(.*):(.*)@(.*)$")
+	replaceRegex := "${1}//<USER>:<PASSWORD>@${4}"
+	maskedPGCredInURI := selector.ReplaceAllString(s, replaceRegex)
+	return maskedPGCredInURI
+}
