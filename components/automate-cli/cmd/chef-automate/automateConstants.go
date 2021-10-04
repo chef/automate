6dcc452b30c1cdd224e7d9640f0c4aba7dfc7b66
jay vikram sharma <jsharma@progress.com>
2021-10-04 12:17:58 +0530

    Signed-off-by: jay vikram sharma <jsharma@progress.com> config patch-command mapping

--- components/automate-cli/cmd/chef-automate/automateConstants.go
+++ components/automate-cli/cmd/chef-automate/automateConstants.go
@@ -9,3 +9,4 @@ const (
 
 const AUTOMATE_HA_RUN_LOG_DIR = "/hab/a2_deploy_workspace/logs"
 const AUTOMATE_HA_WORKSPACE_DIR = "/hab/a2_deploy_workspace"
+const AUTOMATE_HA_AUTOMATE_CONFIG_FILE = "/hab/a2_deploy_workspace/configs/automate.toml"
