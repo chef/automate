497ae38377d014dc4bcbf1833a256cb1b0c5310f
shaik80 <smudassi@progress.com>
2021-10-04 20:41:01 +0530

    deep copy in simple way

--- components/automate-ui/src/app/page-components/selectbox/selectbox.component.ts
+++ components/automate-ui/src/app/page-components/selectbox/selectbox.component.ts
@@ -44,7 +44,7 @@ export class SelectboxComponent implements OnInit, OnChanges {
 
   ngOnChanges() {
     if (!this.copyDataListFlags && this.data === this.listData || this.data !== null ) {
-      this.listData = JSON.parse(JSON.stringify(this.data));
+      this.listData = [...this.data];
       this.selectData.emit(this.selectedListData);
       this.selectedListData.forEach((listDataValue) => {
         if (listDataValue.type === this.type) {
