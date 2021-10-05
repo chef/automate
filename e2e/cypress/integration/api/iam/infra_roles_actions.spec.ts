3b4559547ad48e775c875b583a31b68744fcb67e
Abdul-Az <aazeez@progress.com>
2021-10-05 15:40:38 +0530

    roles view  cy test cases
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- e2e/cypress/integration/api/iam/infra_roles_actions.spec.ts
+++ e2e/cypress/integration/api/iam/infra_roles_actions.spec.ts
@@ -1,34 +1,15 @@
-const serverID = `chef-server-dev-test-${Cypress.moment().format('MMDDYYhhmm')}`;
-const serverName = 'chef server dev role';
-const orgID = `chef-org-dev-${Cypress.moment().format('MMDDYYhhmm')}`;
-const orgName = 'chef-server-role';
-const serverFQDN = 'a2-dev.test';
-const serverIP = '127.0.0.1';
-const adminUser = 'chefadmin';
-// using dummy admin key value for creating the org
-const adminKey = 'Dummy--admin--key';
+describe('Roles list', () => {
+    let withInfraServersRolesListActionToken = '';
+    let withoutInfraServersRolesListActionToken = '';
 
-const now = Cypress.moment().format('MMDDYYhhmm');
-
-const cypressPrefix = 'infra-server-orgs-actions-list';
-const roleName = `${cypressPrefix}-role-${now}-1`;
-const roleDescription = 'role description';
-const roleRunlistName = `${cypressPrefix}-role-${now}-2`;
-const roleDefaultAttrName = `${cypressPrefix}-role-${now}-3`;
-const roleOverrideAttrName = `${cypressPrefix}-role-${now}-4`;
-
-describe('infra server roles list', () => {
-    let withInfraServersOrgsRolesListActionToken = '';
-    let withoutInfraServersOrgsRolesListActionToken = '';
-
+    const cypressPrefix = 'infra-server-roles-actions-list';
+    const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const policyId2 = `${cypressPrefix}-pol-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId1 = `${cypressPrefix}-token-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId2 = `${cypressPrefix}-token-2-${Cypress.moment().format('MMDDYYhhmm')}`;
     const objectsToCleanUp = ['tokens', 'policies'];
 
-    const policyId1 = `${cypressPrefix}-pol-orgs-1-${Cypress.moment().format('MMDDYYhhmm')}`;
-    const policyId2 = `${cypressPrefix}-pol-orgs-2-${Cypress.moment().format('MMDDYYhhmm')}`;
-    const tokenId1 = `${cypressPrefix}-token-orgs-1-${Cypress.moment().format('MMDDYYhhmm')}`;
-    const tokenId2 = `${cypressPrefix}-token-orgs-2-${Cypress.moment().format('MMDDYYhhmm')}`;
-
-    const withInfraServersOrgsRolesListPolicy = {
+    const withInfraServersRolesListPolicy = {
     id: policyId1,
     name: tokenId1,
     projects: [],
@@ -44,7 +25,7 @@ describe('infra server roles list', () => {
     };
 
 
-    const withoutInfraServersOrgsRolesListPolicy = {
+    const withoutInfraServersRolesListPolicy = {
         id: policyId2,
         name: tokenId2,
         projects: [],
@@ -67,127 +48,82 @@ describe('infra server roles list', () => {
             method: 'POST',
             url: '/apis/iam/v2/tokens',
             body: {
-            id: tokenId1,
-            name: tokenId1
-                }
+                id: tokenId1,
+                name: tokenId1
+            }
             }).then((resp) => {
-                withInfraServersOrgsRolesListActionToken = resp.body.token.value;
+                withInfraServersRolesListActionToken = resp.body.token.value;
             });
 
         cy.request({
             headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
             method: 'POST',
             url: '/apis/iam/v2/policies',
-            body: withInfraServersOrgsRolesListPolicy
-          }).then((resp) => {
-            expect(resp.status).to.equal(200);
-          });
+            body: withInfraServersRolesListPolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
+            });
 
         cy.request({
             headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
             method: 'POST',
             url: '/apis/iam/v2/tokens',
             body: {
-              id: tokenId2,
-              name: tokenId2
+                id: tokenId2,
+                name: tokenId2
             }
-          }).then((resp) => {
-            withoutInfraServersOrgsRolesListActionToken = resp.body.token.value;
-          });
+            }).then((resp) => {
+                withoutInfraServersRolesListActionToken = resp.body.token.value;
+            });
 
-          cy.request({
+        cy.request({
             headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
             method: 'POST',
             url: '/apis/iam/v2/policies',
-            body: withoutInfraServersOrgsRolesListPolicy
-          }).then((resp) => {
-            expect(resp.status).to.equal(200);
-          });
-
-        cy.request({
-            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
-            method: 'POST',
-            url: '/api/v0/infra/servers',
-            body: {
-              id: serverID,
-              name: serverName,
-              fqdn: serverFQDN,
-              ip_address: serverIP
-            }
-          });
-
-        cy.request({
-          headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
-          method: 'POST',
-          url: `/api/v0/infra/servers/${serverID}/orgs`,
-          body: {
-            id: orgID,
-            server_id: serverID,
-            name: orgName,
-            admin_user: adminUser,
-            admin_key: adminKey
-          }
-        });
-
-        cy.request({
-            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
-            method: 'POST',
-            url: `/api/v0/infra/servers/${serverID}/orgs/${orgID}/roles`,
-            body: {
-              org_id: orgID,
-              server_id: serverID,
-              name: orgName,
-              description: roleDescription,
-              default_attributes: roleDefaultAttrName,
-              override_attributes: roleOverrideAttrName,
-              run_list: roleRunlistName,
-            }
-          });
-    });
-
-
-
-    after(() => {
-        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
-      });
-
-    it('infra server roles list returns 200 when infraServers roles list action is allowed', () => {
-        cy.request({
-            headers: { 'api-token': withInfraServersOrgsRolesListActionToken,
-            'content-type': 'application/json+lax' },
-            method: 'GET',
-            url: `/api/v0/infra/servers/${serverID}/orgs/${orgID}/roles`
-        }).then((resp) => {
-            assert.equal(resp.status, 200);
+            body: withoutInfraServersRolesListPolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
             });
         });
 
-    it('infra server roles list returns 403 when infraServers roles list actions is deneyed', () => {
+    after(() => {
+        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
+    });
+
+    it('Roles get returns 200 when infraServersOrgsRoles list actions is allowed', () => {
         cy.request({
-            headers: { 'api-token': withoutInfraServersOrgsRolesListActionToken,
-            'content-type': 'application/json+lax' },
+            headers: { 'api-token': withInfraServersRolesListActionToken },
             method: 'GET',
-            failOnStatusCode: false,
-            url: `/api/v0/infra/servers/${serverID}/orgs/${orgID}/roles`
-        }).then((resp) => {
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/roles'
+            }).then((resp) => {
+                assert.equal(resp.status, 200);
+            });
+    });
+
+    it('Roles get returns 403 when infraServersOrgsRoles list actions is deneyed', () => {
+        cy.request({
+            headers: { 'api-token': withoutInfraServersRolesListActionToken },
+            method: 'GET',
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/roles',
+            failOnStatusCode: false
+            }).then((resp) => {
             assert.equal(resp.status, 403);
         });
     });
 });
 
+describe('Roles get', () => {
+    let withInfraServersRolesGetActionToken = '';
+    let withoutInfraServersRolesGetActionToken = '';
 
-describe('infra server roles get', () => {
-    let withInfraServersOrgsRolesGetActionToken = '';
-    let withoutInfraServersOrgsRolesGetActionToken = '';
-
+    const cypressPrefix = 'infra-server-roles-actions-get';
+    const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const policyId2 = `${cypressPrefix}-pol-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId1 = `${cypressPrefix}-token-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId2 = `${cypressPrefix}-token-2-${Cypress.moment().format('MMDDYYhhmm')}`;
     const objectsToCleanUp = ['tokens', 'policies'];
 
-    const policyId1 = `${cypressPrefix}-pol-orgs-1-${Cypress.moment().format('MMDDYYhhmm')}`;
-    const policyId2 = `${cypressPrefix}-pol-orgs-2-${Cypress.moment().format('MMDDYYhhmm')}`;
-    const tokenId1 = `${cypressPrefix}-token-orgs-1-${Cypress.moment().format('MMDDYYhhmm')}`;
-    const tokenId2 = `${cypressPrefix}-token-orgs-2-${Cypress.moment().format('MMDDYYhhmm')}`;
-
-    const withInfraServersOrgsRolesGetPolicy = {
+    const withInfraServersRolesGetPolicy = {
     id: policyId1,
     name: tokenId1,
     projects: [],
@@ -203,7 +139,7 @@ describe('infra server roles get', () => {
     };
 
 
-    const withoutInfraServersOrgsRolesGetPolicy = {
+    const withoutInfraServersRolesGetPolicy = {
         id: policyId2,
         name: tokenId2,
         projects: [],
@@ -226,71 +162,66 @@ describe('infra server roles get', () => {
             method: 'POST',
             url: '/apis/iam/v2/tokens',
             body: {
-            id: tokenId1,
-            name: tokenId1
-                }
+                id: tokenId1,
+                name: tokenId1
+            }
             }).then((resp) => {
-                withInfraServersOrgsRolesGetActionToken = resp.body.token.value;
+                withInfraServersRolesGetActionToken = resp.body.token.value;
             });
 
         cy.request({
             headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
             method: 'POST',
             url: '/apis/iam/v2/policies',
-            body: withInfraServersOrgsRolesGetPolicy
-          }).then((resp) => {
-            expect(resp.status).to.equal(200);
-          });
+            body: withInfraServersRolesGetPolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
+            });
 
         cy.request({
             headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
             method: 'POST',
             url: '/apis/iam/v2/tokens',
             body: {
-              id: tokenId2,
-              name: tokenId2
+                id: tokenId2,
+                name: tokenId2
             }
-          }).then((resp) => {
-            withoutInfraServersOrgsRolesGetActionToken = resp.body.token.value;
-          });
+            }).then((resp) => {
+                withoutInfraServersRolesGetActionToken = resp.body.token.value;
+            });
 
-          cy.request({
+        cy.request({
             headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
             method: 'POST',
             url: '/apis/iam/v2/policies',
-            body: withoutInfraServersOrgsRolesGetPolicy
-          }).then((resp) => {
-            expect(resp.status).to.equal(200);
-          });
-    });
-
-
-
-    after(() => {
-        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
-      });
-
-    it('infra server get list returns 200 when infraServers roles get action is allowed', () => {
-        cy.request({
-            headers: { 'api-token': withInfraServersOrgsRolesGetActionToken,
-            'content-type': 'application/json+lax' },
-            method: 'GET',
-            url: `/api/v0/infra/servers/${serverID}/orgs/${orgID}/roles/${roleName}`
-        }).then((resp) => {
-            assert.equal(resp.status, 200);
+            body: withoutInfraServersRolesGetPolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
             });
         });
 
-    it('infra server roles get returns 403 when infraServers roles get actions is deneyed', () => {
+    after(() => {
+        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
+    });
+
+    it('roles get returns 200 when infraServersOrgsRoles get actions is allowed', () => {
         cy.request({
-            headers: { 'api-token': withoutInfraServersOrgsRolesGetActionToken,
-            'content-type': 'application/json+lax' },
+            headers: { 'api-token': withInfraServersRolesGetActionToken },
             method: 'GET',
-            failOnStatusCode: false,
-            url: `/api/v0/infra/servers/${serverID}/orgs/${orgID}/roles/${roleName}`
-        }).then((resp) => {
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/roles/web'
+            }).then((resp) => {
+                assert.equal(resp.status, 200);
+            });
+    });
+
+    it('roles get returns 403 when infraServersOrgsRoles get actions is deneyed', () => {
+        cy.request({
+            headers: { 'api-token': withoutInfraServersRolesGetActionToken },
+            method: 'GET',
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/roles/web',
+            failOnStatusCode: false
+            }).then((resp) => {
             assert.equal(resp.status, 403);
         });
     });
 });
-
