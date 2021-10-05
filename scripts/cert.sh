68caaab939a4d843220c9ed1ed4541507e6ff2d9
meet.gadhiya <60958104+meet58@users.noreply.github.com>
2021-10-05 11:12:23 +0530

    fix: a2ha habitat cert issue (#5834)
    Signed-off-by: meet58 <mgadhiya@msystechnologies.com>

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -7,25 +7,21 @@ openssl genrsa -out MyRootCA.key 2048
 openssl req -x509 -new -key MyRootCA.key -sha256 -out MyRootCA.pem -subj '/C=US/ST=Washington/L=Seattle/O=Chef Software Inc/CN=chefrootca'
  
 #the rsa keys 
-openssl genrsa -out admin-pkcs12.key 2048
+openssl genrsa -out admin.key 2048
  
-#IMPORTANT: Convert these to PKCS#5 v1.5 to work correctly with the JDK.
-openssl pkcs8 -v1 "PBE-SHA1-3DES" -in "admin-pkcs12.key" -topk8 -out "admin.key" -nocrypt
-
 #Create the CSR and enter the organization and server details for the node key 
 openssl req -new -key admin.key -out admin.csr -subj '/C=US/ST=Washington/L=Seattle/O=Chef Software Inc/CN=chefadmin'
  
 #Use the CSR to generate the signed node Certificate:
 openssl x509 -req -in admin.csr -CA MyRootCA.pem -CAkey MyRootCA.key -CAcreateserial -out admin.pem -sha256
  
-
 # root pem cert that signed the below cert/key pairs below
 # Used for hab_sup_http_gateway_ca_cert 
 cat <<EOF >> ../terraform/variables_common.tf
 
 variable "hab_sup_http_gateway_ca_cert" {
   default = <<CERT
-$(cat MyRootCA.key)
+$(cat MyRootCA.pem)
 CERT
 
 
@@ -34,7 +30,7 @@ CERT
 
 variable "hab_sup_http_gateway_priv_key" {
   default = <<CERT
-$(cat admin.pem)
+$(cat admin.key)
 CERT
 
 
@@ -43,7 +39,7 @@ CERT
 
 variable "hab_sup_http_gateway_pub_cert" {
   default = <<CERT
-$(cat admin.key)
+$(cat admin.pem)
 CERT
 
 
@@ -52,4 +48,4 @@ CERT
 
 EOF
  
-rm MyRootCA.key MyRootCA.pem MyRootCA.srl admin-pkcs12.key admin.csr admin.key admin.pem
+rm MyRootCA.key MyRootCA.pem MyRootCA.srl admin.csr admin.key admin.pem
