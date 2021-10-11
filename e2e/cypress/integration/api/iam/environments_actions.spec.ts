ecb0fd39027b30b7c91426bd75579a5b61540e3f
Abdul-Az <aazeez@progress.com>
2021-10-11 19:14:10 +0530

    environments test cases added
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- e2e/cypress/integration/api/iam/environments_actions.spec.ts
+++ e2e/cypress/integration/api/iam/environments_actions.spec.ts
@@ -90,7 +90,7 @@ describe('Environments list', () => {
         cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
     });
 
-    it('Environments get returns 200 when infraServersOrgsEnvironments list actions is allowed', () => {
+    it('Env get returns 200 when infraServersOrgsEnvironments list actions is allowed', () => {
         cy.request({
             headers: { 'api-token': withInfraServersEnvironmentsListActionToken },
             method: 'GET',
@@ -100,7 +100,7 @@ describe('Environments list', () => {
             });
     });
 
-    it('Environments get returns 403 when infraServersOrgsEnvironments list actions is denied', () => {
+    it('Env get returns 403 when infraServersOrgsEnvironments list actions is denied', () => {
         cy.request({
             headers: { 'api-token': withoutInfraServersEnvironmentsListActionToken },
             method: 'GET',
@@ -204,7 +204,7 @@ describe('Environments get', () => {
         cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
     });
 
-    it('Environments get returns 200 when infraServersOrgsEnvironments get actions is allowed', () => {
+    it('Env get returns 200 when infraServersOrgsEnvironments get actions is allowed', () => {
         cy.request({
             headers: { 'api-token': withInfraServersEnvironmentsGetActionToken },
             method: 'GET',
@@ -214,7 +214,7 @@ describe('Environments get', () => {
             });
     });
 
-    it('Environments get returns 403 when infraServersOrgsEnvironments get actions is denied', () => {
+    it('Env get returns 403 when infraServersOrgsEnvironments get actions is denied', () => {
         cy.request({
             headers: { 'api-token': withoutInfraServersEnvironmentsGetActionToken },
             method: 'GET',
@@ -224,4 +224,372 @@ describe('Environments get', () => {
             assert.equal(resp.status, 403);
         });
     });
+});
+
+
+describe('Create Environments', () => {
+    let withInfraServersEnvironmentsCreateActionToken = '';
+    let withoutInfraServersEnvironmentsCreateActionToken = '';
+
+    const cypressPrefix = 'infra-server-environments-actions-create';
+    const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const policyId2 = `${cypressPrefix}-pol-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId1 = `${cypressPrefix}-token-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId2 = `${cypressPrefix}-token-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const objectsToCleanUp = ['tokens', 'policies'];
+
+    const withInfraServersEnvironmentsCreatePolicy = {
+    id: policyId1,
+    name: tokenId1,
+    projects: [],
+    members: [`token:${tokenId1}`],
+    statements: [
+        {
+            effect: 'ALLOW',
+            actions: [
+                'infra:infraServersOrgsEnvironments:create'
+            ],
+            projects: ['*']
+        }]
+    };
+
+
+    const withoutInfraServersEnvironmentsCreatePolicy = {
+        id: policyId2,
+        name: tokenId2,
+        projects: [],
+        members: [`token:${tokenId2}`],
+        statements: [
+        {
+            effect: 'DENY',
+            actions: [
+                'infra:infraServersOrgsEnvironments:create'
+            ],
+            projects: ['*']
+        }]
+    };
+
+    before(() => {
+        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/tokens',
+            body: {
+                id: tokenId1,
+                name: tokenId1
+            }
+            }).then((resp) => {
+                withInfraServersEnvironmentsCreateActionToken = resp.body.token.value;
+            });
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/policies',
+            body: withInfraServersEnvironmentsCreatePolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
+            });
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/tokens',
+            body: {
+                id: tokenId2,
+                name: tokenId2
+            }
+            }).then((resp) => {
+                withoutInfraServersEnvironmentsCreateActionToken = resp.body.token.value;
+            });
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/policies',
+            body: withoutInfraServersEnvironmentsCreatePolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
+            });
+        });
+
+    after(() => {
+        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
+    });
+
+    it('Env post returns 200 when infraServersOrgsEnvironments create actions is allowed', () => {
+        cy.request({
+            headers: { 'api-token': withInfraServersEnvironmentsCreateActionToken },
+            method: 'POST',
+            body: {
+                org_id: 'test-org',
+                server_id: 'local-dev',
+                name: 'test',
+                description: 'cypress testing'
+            },
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/environments'
+            }).then((resp) => {
+                assert.equal(resp.status, 200);
+            });
+    });
+
+    it('Env post returns 403 when infraServersOrgsEnvironments create actions is denied', () => {
+        cy.request({
+            headers: { 'api-token': withoutInfraServersEnvironmentsCreateActionToken },
+            method: 'POST',
+            body: {
+                org_id: 'test-org',
+                server_id: 'local-dev',
+                name: 'test',
+                description: 'cypress testing'
+            },
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/environments',
+            failOnStatusCode: false
+            }).then((resp) => {
+            assert.equal(resp.status, 403);
+        });
+    });
+});
+
+describe('Update Environments', () => {
+    let withInfraServersEnvironmentsUpdateActionToken = '';
+    let withoutInfraServersEnvironmentsUpdateActionToken = '';
+
+    const cypressPrefix = 'infra-server-environments-actions-update';
+    const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const policyId2 = `${cypressPrefix}-pol-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId1 = `${cypressPrefix}-token-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId2 = `${cypressPrefix}-token-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const objectsToCleanUp = ['tokens', 'policies'];
+
+    const withInfraServersEnvironmentsUpdatePolicy = {
+    id: policyId1,
+    name: tokenId1,
+    projects: [],
+    members: [`token:${tokenId1}`],
+    statements: [
+        {
+            effect: 'ALLOW',
+            actions: [
+                'infra:infraServersOrgsEnvironments:update'
+            ],
+            projects: ['*']
+        }]
+    };
+
+
+    const withoutInfraServersEnvironmentsUpdatePolicy = {
+        id: policyId2,
+        name: tokenId2,
+        projects: [],
+        members: [`token:${tokenId2}`],
+        statements: [
+        {
+            effect: 'DENY',
+            actions: [
+                'infra:infraServersOrgsEnvironments:update'
+            ],
+            projects: ['*']
+        }]
+    };
+
+    before(() => {
+        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/tokens',
+            body: {
+                id: tokenId1,
+                name: tokenId1
+            }
+            }).then((resp) => {
+                withInfraServersEnvironmentsUpdateActionToken = resp.body.token.value;
+            });
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/policies',
+            body: withInfraServersEnvironmentsUpdatePolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
+            });
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/tokens',
+            body: {
+                id: tokenId2,
+                name: tokenId2
+            }
+            }).then((resp) => {
+                withoutInfraServersEnvironmentsUpdateActionToken = resp.body.token.value;
+            });
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/policies',
+            body: withoutInfraServersEnvironmentsUpdatePolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
+            });
+        });
+
+    after(() => {
+        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
+    });
+
+    it('Env put returns 200 when infraServersOrgsEnvironments update actions is allowed', () => {
+        cy.request({
+            headers: { 'api-token': withInfraServersEnvironmentsUpdateActionToken },
+            method: 'PUT',
+            body: {
+                org_id: 'test-org',
+                server_id: 'local-dev',
+                description: 'cypress testing update'
+            },
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/environments/test'
+            }).then((resp) => {
+                assert.equal(resp.status, 200);
+            });
+    });
+
+    it('Env put returns 403 when infraServersOrgsEnvironments update actions is denied', () => {
+        cy.request({
+            headers: { 'api-token': withoutInfraServersEnvironmentsUpdateActionToken },
+            method: 'PUT',
+            body: {
+                org_id: 'test-org',
+                server_id: 'local-dev',
+                description: 'cypress testing update'
+            },
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/environments/test',
+            failOnStatusCode: false
+            }).then((resp) => {
+            assert.equal(resp.status, 403);
+        });
+    });
+});
+
+
+describe('Delete Environments', () => {
+    let withInfraServersEnvironmentsDeleteActionToken = '';
+    let withoutInfraServersEnvironmentsDeleteActionToken = '';
+
+    const cypressPrefix = 'infra-server-environments-actions-delete';
+    const policyId1 = `${cypressPrefix}-pol-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const policyId2 = `${cypressPrefix}-pol-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId1 = `${cypressPrefix}-token-1-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const tokenId2 = `${cypressPrefix}-token-2-${Cypress.moment().format('MMDDYYhhmm')}`;
+    const objectsToCleanUp = ['tokens', 'policies'];
+
+    const withInfraServersEnvironmentsDeletePolicy = {
+    id: policyId1,
+    name: tokenId1,
+    projects: [],
+    members: [`token:${tokenId1}`],
+    statements: [
+        {
+            effect: 'ALLOW',
+            actions: [
+                'infra:infraServersOrgsEnvironments:delete'
+            ],
+            projects: ['*']
+        }]
+    };
+
+
+    const withoutInfraServersEnvironmentsDeletePolicy = {
+        id: policyId2,
+        name: tokenId2,
+        projects: [],
+        members: [`token:${tokenId2}`],
+        statements: [
+        {
+            effect: 'DENY',
+            actions: [
+                'infra:infraServersOrgsEnvironments:delete'
+            ],
+            projects: ['*']
+        }]
+    };
+
+    before(() => {
+        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/tokens',
+            body: {
+                id: tokenId1,
+                name: tokenId1
+            }
+            }).then((resp) => {
+                withInfraServersEnvironmentsDeleteActionToken = resp.body.token.value;
+            });
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/policies',
+            body: withInfraServersEnvironmentsDeletePolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
+            });
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/tokens',
+            body: {
+                id: tokenId2,
+                name: tokenId2
+            }
+            }).then((resp) => {
+                withoutInfraServersEnvironmentsDeleteActionToken = resp.body.token.value;
+            });
+
+        cy.request({
+            headers: { 'api-token': Cypress.env('ADMIN_TOKEN') },
+            method: 'POST',
+            url: '/apis/iam/v2/policies',
+            body: withoutInfraServersEnvironmentsDeletePolicy
+            }).then((resp) => {
+                expect(resp.status).to.equal(200);
+            });
+        });
+
+    after(() => {
+        cy.cleanupIAMObjectsByIDPrefixes(cypressPrefix, objectsToCleanUp);
+    });
+
+
+    it('Env delete returns 403 when infraServersOrgsEnvironments delete actions is denied', () => {
+        cy.request({
+            headers: { 'api-token': withoutInfraServersEnvironmentsDeleteActionToken },
+            method: 'DELETE',
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/environments/test',
+            failOnStatusCode: false
+            }).then((resp) => {
+            assert.equal(resp.status, 403);
+        });
+    });
+
+    it('Env delete returns 200 when infraServersOrgsEnvironments delete actions is allowed', () => {
+        cy.request({
+            headers: { 'api-token': withInfraServersEnvironmentsDeleteActionToken },
+            method: 'DELETE',
+            url: '/api/v0/infra/servers/local-dev/orgs/test-org/environments/test'
+            }).then((resp) => {
+                assert.equal(resp.status, 200);
+            });
+    });
+
 });
