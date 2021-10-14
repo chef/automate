5d6da8c7263553068f960678fcc2abf065448f4a
chaitali-mane <cmane@progress.com>
2021-10-14 15:59:38 +0530

    update options
    Signed-off-by: chaitali-mane <cmane@progress.com>

--- components/automate-ui/src/app/modules/infra-proxy/paginator/paginator.component.ts
+++ components/automate-ui/src/app/modules/infra-proxy/paginator/paginator.component.ts
@@ -25,7 +25,7 @@ export class PaginatorComponent implements OnInit, OnChanges {
   @Output() changePage = new EventEmitter<any>(true);
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  pageSizeOptions: number[] = [3, 5, 10];
+  pageSizeOptions: number[] = [10, 50, 100];
   pageEvent: PageEvent;
   public isPageAvailable = false;
 
