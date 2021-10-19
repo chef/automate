511baf69e770c6faa6776f5580a3bbb40f6362e2
Durga Sarat Chandra Maddu <44021874+Dmaddu@users.noreply.github.com>
2021-10-19 20:22:41 +0530

    integrating report manager service with postgres and adding migration scripts (#5930)
    Signed-off-by: Durga Sarat Chandra Maddu <durgasaratchandra.maddu@progress.com>

--- components/automate-deployment/pkg/assets/binds.go
+++ components/automate-deployment/pkg/assets/binds.go
@@ -98,7 +98,7 @@ notifications-service BINDING_MODE strict
 pg-sidecar-service REQUIRED automate-pg-gateway
 pg-sidecar-service BINDING_MODE strict
 report-manager-minio-gateway BINDING_MODE strict
-report-manager-service REQUIRED cereal-service report-manager-minio-gateway
+report-manager-service REQUIRED automate-pg-gateway cereal-service pg-sidecar-service report-manager-minio-gateway
 report-manager-service BINDING_MODE strict
 sample-data-service BINDING_MODE strict
 secrets-service REQUIRED automate-pg-gateway pg-sidecar-service
