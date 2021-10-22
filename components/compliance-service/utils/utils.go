c6b2139e3bfce2e94c289df57f7db102f8d4d9e7
sonali523 <86949270+sonali523@users.noreply.github.com>
2021-10-22 20:30:02 +0530

    Compliance API to report the node usage data (#5931)
    * Complaince telemetry node usage API added
    
    Signed-off-by: root <swale@msystechnologies.com>
    
    * Changes added to get the compliance node usage data based on the last telemetry sent
    
    Signed-off-by: root <swale@msystechnologies.com>
    
    * Changes added to handle the last telemetry reported date is nil
    
    Signed-off-by: root <swale@msystechnologies.com>
    
    * First time telemetry data sent case handled
    
    Signed-off-by: root <swale@msystechnologies.com>
    
    * Changes added for the review comments
    
    Signed-off-by: root <swale@msystechnologies.com>
    
    * changes added for the storeCompliance function to call it in goroutine
    
    Signed-off-by: root <swale@msystechnologies.com>

--- components/compliance-service/utils/utils.go
+++ components/compliance-service/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/chef/automate/api/interservice/compliance/common"
 )
@@ -92,4 +93,19 @@ func DeDupFilters(filters map[string][]string) {
 			filters[key] = deDupSlice(value)
 		}
 	}
+}
+
+// DaysBetween get the calendar days between two timestamp
+func DaysBetween(fromTime, toTime time.Time) int {
+	if fromTime.After(toTime) {
+		fromTime, toTime = toTime, fromTime
+	}
+
+	days := -fromTime.YearDay()
+	for year := fromTime.Year(); year < toTime.Year(); year++ {
+		days += time.Date(year, time.December, 31, 0, 0, 0, 0, time.UTC).YearDay()
+	}
+	days += toTime.YearDay()
+
+	return days
 }
