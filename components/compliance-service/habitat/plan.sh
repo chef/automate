9141104badfb60b1748a2c52140a695546769b46
mchengat88 <88131604+mchengat88@users.noreply.github.com>
2021-10-25 20:22:52 +0530

    Invoke storereport api(#5711) (#5958)
    * Invoke storereport api(#5711)
    
    Signed-off-by: Mansoor <mchengat@msystechnologies.com>
    
    * Invoke storereport api(#5711)
    
    Signed-off-by: Mansoor <mchengat@msystechnologies.com>

--- components/compliance-service/habitat/plan.sh
+++ components/compliance-service/habitat/plan.sh
@@ -23,6 +23,7 @@ pkg_binds=(
   [authz-service]="port"
   [nodemanager-service]="port"
   [cereal-service]="port"
+  [report-manager-service]="port"
 )
 pkg_binds_optional=(
   [es-sidecar-service]="port"
