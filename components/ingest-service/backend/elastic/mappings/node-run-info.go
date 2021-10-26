dcad3ae27330b3b7c36e4b60c3157b82c265960b
Rick Marry <rmarry@chef.io>
2021-10-26 06:16:34 -0400

    add cfgmgmt node run info index into elasticsearch (#5985)
    * add cfgmgmt node run info index into elasticsearch
    
    Signed-off-by: Rick Marry <rmarry@chef.io>
    
    * rename node-run-info
    
    Signed-off-by: Rick Marry <rmarry@chef.io>

---
+++ components/ingest-service/backend/elastic/mappings/node-run-info.go
@@ -0,0 +1,32 @@
+package mappings
+
+// ConfigManagementRunInfo mapping used to create the `config-mgmt-run-info index
+var ConfigManagementRunInfo = Mapping{
+	Index:      IndexNameNodeRunInfo,
+	Type:       DocType,
+	Timeseries: false,
+	Mapping: `
+{
+  "settings": {
+    "index": {
+      "refresh_interval": "1s"
+    }
+  },
+  "mappings": {
+    "` + DocType + `": {
+      "properties": {
+        "node_uuid": {
+          "type": "keyword"
+        },
+        "first_run": {
+          "type": "date"
+        },
+        "last_run": {
+          "type": "date"
+        }
+      }
+    }
+  }
+}
+	`,
+}
