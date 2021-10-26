d8f3b1a5806f234b8aeb683829cf7f472ff33411
vivekshankar1 <59958706+vivekshankar1@users.noreply.github.com>
2021-10-26 16:54:44 +0530

    Go upgrade, Azure SDK upgrade with azure api scan filtering enabled (#5870)
    * scaffolding version and golang version
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * changes for azure sdk upgrade
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * license scout crypto
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * added filtering in backend api for api scan azure
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fixed duplicates
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * revert changes on go.sum
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * revert changes on go.sum
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fixed integration test cases for azure api
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * add go sum
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * certificate incompatibility issue fix for go 1.15
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * filter logic modified
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * added fget fields
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * removed logs and extra lines
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * added unit and integration tests
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * added unit and integration tests
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * print subs
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * check integration test
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * added go debug env
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * init method added for GODEBUG
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * added tests of env
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * set GODEBUG to PIPELINE
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * more tests added
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * refactored and timeout increase
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * revereted vm changes
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- components/nodemanager-service/tests/azure_api_nodes_integration_test.go
+++ components/nodemanager-service/tests/azure_api_nodes_integration_test.go
@@ -12,6 +12,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var AzureAPIManagerID = ""
+
 func TestAzureAPINodes(t *testing.T) {
 	if !mgrtesthelpers.CheckForCreds("azure") {
 		t.Log("azure credentials missing; aborting")
@@ -40,12 +42,12 @@ func TestAzureAPINodes(t *testing.T) {
 	require.NoError(t, err)
 	require.NotZero(t, len(mgrID.Ids))
 
-	mgrIDVal := mgrID.GetIds()[0].Id
+	AzureAPIManagerID = mgrID.GetIds()[0].Id
 
 	t.Log("ensure a node was added")
 	query := nodes.Query{
 		Filters: []*common.Filter{
-			{Key: "manager_id", Values: []string{mgrIDVal}},
+			{Key: "manager_id", Values: []string{AzureAPIManagerID}},
 		},
 	}
 	list, err := nodesClient.List(ctx, &query)
@@ -58,4 +60,106 @@ func TestAzureAPINodes(t *testing.T) {
 	node.Name = "updatedName"
 	_, err = nodesClient.Update(ctx, node)
 	assert.Equal(t, "rpc error: code = InvalidArgument desc = invalid option. unable to update azure-api node", err.Error())
+}
+
+func TestAzureApiSearchNodeFields(t *testing.T) {
+	if !mgrtesthelpers.CheckForCreds("azure") {
+		t.Log("azure credentials missing; aborting")
+		t.FailNow()
+	}
+	t.Log("Running Azure-API search node fields test.")
+	ctx := context.Background()
+
+	mgrConn, err := mgrtesthelpers.GetManagerConn()
+	require.NoError(t, err)
+	defer mgrConn.Close()
+
+	// setup clients
+	mgrClient := manager.NewNodeManagerServiceClient(mgrConn)
+
+	t.Log("search node fields for list of subscriptions")
+	query := manager.FieldQuery{
+		NodeManagerId: AzureAPIManagerID,
+		Query: &manager.Query{
+			FilterMap: []*common.Filter{},
+		},
+		Field: "subscriptions",
+	}
+
+	fields, err := mgrClient.SearchNodeFields(ctx, &query)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, len(fields.GetFields()))
+
+	t.Log("search node fields for list of names")
+	query.Field = "names"
+	fields, err = mgrClient.SearchNodeFields(ctx, &query)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, len(fields.GetFields()))
+
+	t.Log("search node fields for list of tags")
+	query.Field = "tags"
+	fields, err = mgrClient.SearchNodeFields(ctx, &query)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, len(fields.GetFields()))
+	t.Logf("fields found are %v", fields)
+	require.Equal(t, mgrtesthelpers.Contains(fields.GetFields(), "name"), true)
+
+	t.Log("search node fields for list of values for names")
+	query.Field = "tags:name"
+	fields, err = mgrClient.SearchNodeFields(ctx, &query)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, len(fields.GetFields()))
+}
+
+func TestAzureApiSearchNodes(t *testing.T) {
+	if !mgrtesthelpers.CheckForCreds("azure") {
+		t.Log("azure credentials missing; aborting")
+		t.FailNow()
+	}
+	t.Log("Running Azure-API search nodes test.")
+	ctx := context.Background()
+
+	mgrConn, err := mgrtesthelpers.GetManagerConn()
+	require.NoError(t, err)
+	defer mgrConn.Close()
+
+	// setup clients
+	mgrClient := manager.NewNodeManagerServiceClient(mgrConn)
+
+	t.Log("search all nodes for this manager")
+	query := manager.NodeQuery{
+		NodeManagerId: AzureAPIManagerID,
+		Query: &manager.Query{
+			FilterMap: []*common.Filter{},
+		},
+	}
+	nodes, err := mgrClient.SearchNodes(ctx, &query)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, len(nodes.GetNodes()))
+
+	t.Log("search all nodes for this manager, filtered by name")
+	query = manager.NodeQuery{
+		NodeManagerId: AzureAPIManagerID,
+		Query: &manager.Query{
+			FilterMap: []*common.Filter{
+				{Key: "name", Values: []string{"Inspec"}},
+			},
+		},
+	}
+	nodes, err = mgrClient.SearchNodes(ctx, &query)
+	require.NoError(t, err)
+	require.NotEqual(t, 0, len(nodes.GetNodes()))
+
+	t.Log("search all nodes for this manager, filtered by region")
+	query = manager.NodeQuery{
+		NodeManagerId: AzureAPIManagerID,
+		Query: &manager.Query{
+			FilterMap: []*common.Filter{
+				{Key: "name", Values: []string{"Inspec"}, Exclude: true},
+			},
+		},
+	}
+	nodes, err = mgrClient.SearchNodes(ctx, &query)
+	require.NoError(t, err)
+	require.Equal(t, 0, len(nodes.GetNodes()))
 }
