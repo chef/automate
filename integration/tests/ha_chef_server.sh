74880773b9772db404a56581f3aee75731e0b543
Kallol Roy <karoy@progress.com>
2021-11-07 21:02:04 +0530

    Checking a2-source-dir
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- integration/tests/ha_chef_server.sh
+++ integration/tests/ha_chef_server.sh
@@ -47,6 +47,9 @@ do_deploy() {
     docker_run "${_frontend2_container_name}"
     #shellcheck disable=SC2154
     docker exec -t "$_frontend1_container_name" \
+        "ls" "$(a2_root_dir)"
+
+    docker exec -t "$_frontend1_container_name" \
         "$(a2_root_dir)/scripts/copy_hartifacts.sh" "$test_hartifacts_path"
     docker exec -t "$_frontend2_container_name" \
         "$(a2_root_dir)/scripts/copy_hartifacts.sh" "$test_hartifacts_path"
