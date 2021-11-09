aba20968b581fe7948f602d016bbf935e9f0932b
Durga Sarat Chandra Maddu <44021874+Dmaddu@users.noreply.github.com>
2021-11-09 11:19:40 +0530

    exporting json custom report (#6052)
    Signed-off-by: Durga Sarat Chandra Maddu <durgasaratchandra.maddu@progress.com>

--- components/report-manager-service/objstore/objstore.go
+++ components/report-manager-service/objstore/objstore.go
@@ -3,7 +3,6 @@ package objstore
 import (
 	"context"
 	"io"
-	"io/ioutil"
 
 	"github.com/minio/minio-go/v7"
 )
@@ -12,7 +11,11 @@ type ObjectStore interface {
 	PutObject(ctx context.Context, bucketName, objectName string, reader io.Reader, objectSize int64,
 		opts minio.PutObjectOptions) (info minio.UploadInfo, err error)
 
-	GetObject(ctx context.Context, bucketName, objectName string, opts minio.GetObjectOptions) (*[]byte, error)
+	GetObject(ctx context.Context, bucketName, objectName string, opts minio.GetObjectOptions) (io.Reader, error)
+
+	BucketExists(ctx context.Context, bucketName string) (bool, error)
+
+	MakeBucket(ctx context.Context, bucketName string, opts minio.MakeBucketOptions) error
 }
 
 type ReportManagerObjStore struct {
@@ -24,11 +27,14 @@ func (rmc ReportManagerObjStore) PutObject(ctx context.Context, bucketName, obje
 	return rmc.ObjStoreClient.PutObject(ctx, bucketName, objectName, reader, objectSize, opts)
 }
 
-func (rmc ReportManagerObjStore) GetObject(ctx context.Context, bucketName, objectName string, opts minio.GetObjectOptions) (*[]byte, error) {
-	obj, err := rmc.ObjStoreClient.GetObject(ctx, bucketName, objectName, opts)
-	if err != nil {
-		return nil, err
-	}
-	bytes, err := ioutil.ReadAll(obj)
-	return &bytes, err
+func (rmc ReportManagerObjStore) GetObject(ctx context.Context, bucketName, objectName string, opts minio.GetObjectOptions) (io.Reader, error) {
+	return rmc.ObjStoreClient.GetObject(ctx, bucketName, objectName, opts)
+}
+
+func (rmc ReportManagerObjStore) BucketExists(ctx context.Context, bucketName string) (bool, error) {
+	return rmc.ObjStoreClient.BucketExists(ctx, bucketName)
+}
+
+func (rmc ReportManagerObjStore) MakeBucket(ctx context.Context, bucketName string, opts minio.MakeBucketOptions) (err error) {
+	return rmc.ObjStoreClient.MakeBucket(ctx, bucketName, opts)
 }
