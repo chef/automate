9981bedb1215f85cd33f5bdc2ffe53ec01dfdfbf
Abdul Azeez <aazeez@progress.com>
2021-11-16 19:23:22 +0530

    added policygroups actions (#6123)
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- api/external/infra_proxy/infra_proxy.pb.go
+++ api/external/infra_proxy/infra_proxy.pb.go
@@ -27,6 +27,8 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
+// This is a compile-time assertion that a sufficiently up-to-date version
+// of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
 var File_external_infra_proxy_infra_proxy_proto protoreflect.FileDescriptor
