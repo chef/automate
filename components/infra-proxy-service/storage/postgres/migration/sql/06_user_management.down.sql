7acb8a7551017269564b34bf34000ebfbfbacc46
Kallol Roy <karoy@progress.com>
2021-11-25 15:57:44 +0530

    Renaming the sql files and modifying to incorporate WebUI key
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- components/infra-proxy-service/storage/postgres/migration/sql/06_user_management.down.sql
+++ components/infra-proxy-service/storage/postgres/migration/sql/06_user_management.down.sql
@@ -1,5 +1,6 @@
 -- drop column projects in servers table 
 ALTER TABLE servers DROP COLUMN projects;
+ALTER TABLE servers DROP COLUMN credential_id;
 
 -- drop table org_users
 DROP TABLE IF EXISTS org_users;
