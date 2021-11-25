fedb1179ec94da4f0ad290852290f785342279e6
Kallol Roy <karoy@progress.com>
2021-11-25 15:57:44 +0530

    Renaming the sql files and modifying to incorporate WebUI key
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- components/infra-proxy-service/storage/postgres/migration/sql/06_user_management.up.sql
+++ components/infra-proxy-service/storage/postgres/migration/sql/06_user_management.up.sql
@@ -1,15 +1,14 @@
 -- add projects column
 ALTER TABLE servers ADD COLUMN projects TEXT[] NOT NULL DEFAULT '{}';
+ALTER TABLE servers ADD COLUMN credential_id TEXT NOT NULL DEFAULT '';
 
 -- create table users
 CREATE TABLE IF NOT EXISTS users (
   id                        TEXT PRIMARY KEY,
   server_id                 TEXT NOT NULL references servers(id) ON DELETE RESTRICT,
   infra_server_username     TEXT NOT NULL DEFAULT '',
-  credential_id             TEXT NOT NULL DEFAULT '',
   connector                 TEXT NOT NULL DEFAULT 'local',
   automate_user_id          TEXT NOT NULL DEFAULT '',
-  is_server_admin           BOOLEAN NOT NULL DEFAULT FALSE,
   created_at                TIMESTAMPTZ NOT NULL,
   updated_at                TIMESTAMPTZ NOT NULL,
   CONSTRAINT infra_server_username_server_id_key
