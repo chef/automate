08420ecf323c6bb4caec59b7b0f719180310f755
Durga Sarat Chandra Maddu <44021874+Dmaddu@users.noreply.github.com>
2021-11-30 11:24:51 +0530

    Add duration to get all status API (#6247)
    Signed-off-by: Durga Sarat Chandra Maddu <durgasaratchandra.maddu@progress.com>

--- components/report-manager-service/utils/utils.go
+++ components/report-manager-service/utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func GetObjName(name string) string {
 	return fmt.Sprintf("%s.json", name)
@@ -8,4 +11,29 @@ func GetObjName(name string) string {
 
 func GetCSVObjName(name string) string {
 	return fmt.Sprintf("%s.csv", name)
+}
+
+//ComputeDuration returns the time difference between startTime and endTime in user readable format.
+//if startTime is after endTime the method returns 0s
+func ComputeDuration(startTime, endTime time.Time) string {
+	if startTime.After(endTime) {
+		return "0s"
+	}
+	diff := endTime.Sub(startTime).Seconds()
+	if diff == 0 {
+		return "0s"
+	} else if diff > 0 && diff < 1 {
+		return "1s"
+	} else if diff < 60 {
+		return fmt.Sprintf("%ds", int(diff+0.5))
+	} else if diff >= 60 && diff < 3600 {
+		return fmt.Sprintf("%dm", int((diff/60)+0.5))
+	}
+	diffH := endTime.Sub(startTime).Hours()
+	hoursPart := int(diffH)
+	minPart := int((diffH - float64(int(diffH))) * 60)
+	if minPart == 0 {
+		return fmt.Sprintf("%dH", hoursPart)
+	}
+	return fmt.Sprintf("%dH%dm", hoursPart, minPart)
 }
