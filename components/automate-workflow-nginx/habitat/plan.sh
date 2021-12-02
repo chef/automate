f0f1f3026c75bcc21c42195d7808b0018ec61c12
shaik80 <smudassi@progress.com>
2021-12-02 16:21:06 +0530

    automate-workflow-nginx  pipline fix
    Signed-off-by: shaik80 <smudassi@progress.com>

--- components/automate-workflow-nginx/habitat/plan.sh
+++ components/automate-workflow-nginx/habitat/plan.sh
@@ -12,11 +12,11 @@ pkg_deps=(
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
   "${vendor_origin}/openresty-noroot/1.13.6.2/20210520120637"
   "${vendor_origin}/automate-workflow-web"
-  chef/mlsa
-  core/bash
-  core/curl
-  core/coreutils
-  core/libossp-uuid
+  chef/mlsa/1.0.1/20200421170200
+  core/bash/5.0.16
+  core/curl/7.68.0
+  core/coreutils/8.30
+  core/libossp-uuid/1.6.2/20200319193820
 )
 
 pkg_exposes=(port ssl-port)
