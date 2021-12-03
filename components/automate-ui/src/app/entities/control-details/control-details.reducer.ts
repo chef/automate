c106e46b7b3367dad0b10f10bcb5ac8112b23388
Chaitali Mane <71449322+chaitali-mane@users.noreply.github.com>
2021-12-03 19:48:20 +0530

    Control details ex-pander in compliance reporting (#6297)
    * Added pagination API for UI
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Added infinite scroll
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Updated code for scroll
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Updated scroll css
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * minor change
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Added API integration for controlDetails
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Added entity files
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * updated entity files
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Updated entity code
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Update control details code
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Update effects code
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Added single API call
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Added code for store
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Updated lint errors
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Removed unused code
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * Updated sonar cloud
    
    Signed-off-by: chaitali-mane <cmane@progress.com>
    
    * removed lint errors
    
    Signed-off-by: chaitali-mane <cmane@progress.com>

---
+++ components/automate-ui/src/app/entities/control-details/control-details.reducer.ts
@@ -0,0 +1,53 @@
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
+import { set, pipe } from 'lodash/fp';
+import { EntityStatus } from 'app/entities/entities';
+import { ControlDetailActionTypes, ControlDetailActions } from './control-details.action';
+import { ControlDetail } from './control-details.model';
+
+export interface ControlDetailEntityState extends EntityState<ControlDetail> {
+  controlsList: [];
+  controlDetailStatus: EntityStatus;
+  controlDetailList: [
+    ControlDetail[]
+  ];
+}
+
+const controlList = [];
+const GET_STATUS = 'controlDetailStatus';
+
+export const controlDetailEntityAdapter: EntityAdapter<ControlDetail> =
+  createEntityAdapter<ControlDetail>({
+    selectId: (controlDetail: ControlDetail) => controlDetail.id
+});
+
+export const ControlDetailEntityInitialState: ControlDetailEntityState =
+  controlDetailEntityAdapter.getInitialState(<ControlDetailEntityState>{
+    controlDetailStatus: EntityStatus.notLoaded
+  });
+
+export function controlDetailEntityReducer(
+  state: ControlDetailEntityState = ControlDetailEntityInitialState,
+  action: ControlDetailActions): ControlDetailEntityState {
+
+  switch (action.type) {
+    case ControlDetailActionTypes.GET:
+      return set(GET_STATUS, EntityStatus.loading, controlDetailEntityAdapter.removeAll(state));
+
+    case ControlDetailActionTypes.GET_SUCCESS:
+      controlList.push(action.payload);
+      return pipe(
+        set(GET_STATUS, EntityStatus.loadingSuccess),
+        set('controlDetailList', action.payload || []),
+        set('controlsList', controlList)
+      )(state) as ControlDetailEntityState;
+
+    case ControlDetailActionTypes.GET_FAILURE:
+      return set(GET_STATUS, EntityStatus.loadingFailure, state);
+
+    default:
+      return state;
+  }
+}
+
+export const getEntityById = (id: string) =>
+  (state: ControlDetailEntityState) => state.entities[id];
