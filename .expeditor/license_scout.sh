21e5dc271cfbaa447f34f4f17847873c92cbbab9
Kallol Roy <karoy@progress.com>
2021-12-11 23:14:36 +0530

    Remove debug from license_scout
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- .expeditor/license_scout.sh
+++ .expeditor/license_scout.sh
@@ -77,5 +77,5 @@ log "Finished installing Go dependencies"
 log_section_start "Running License Scout"
 # a bug requires the use of `--format csv` but the
 # format of the generated manifest is still json
-license_scout -l debug --format csv
+license_scout --format csv
 log "Done!"
