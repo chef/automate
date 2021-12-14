e2594071b320f77dac091d24ff2b0f6ce6b4048f
mimaslanka <85239869+mimaslanka@users.noreply.github.com>
2021-12-14 12:46:27 -0600

    Remove debug statement

--- integration/tests/ha_chef_server.sh
+++ integration/tests/ha_chef_server.sh
@@ -45,8 +45,8 @@ do_deploy() {
     docker_run_1 "${_frontend1_container_name}"
     docker_run_1 "${_frontend2_container_name}"
     #shellcheck disable=SC2154
-    docker exec -t "$_frontend1_container_name" \
-        "pwd && ls"
+    #docker exec -t "$_frontend1_container_name" \
+    #    "pwd && ls"
 
     docker exec -t "$_frontend1_container_name" \
         "$(a2_root_dir)/scripts/copy_hartifacts.sh" "$test_hartifacts_path"
