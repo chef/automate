514298a3ae36984141f0e7b20266e2e5a2ba2c34
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2021-12-22 13:19:15 +0530

    Infra proxy API to get the organisation of chef server (#6363)
    * Get organisation of infra server from chef server API added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * minor changes added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Test cases added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Changes added in test cases
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Added some changes in test cases
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Removed test cases to check the existing one
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * added test cases changes
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * incorporating review comments
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Minor changes added in test cases
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * added some logs
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * changes added in test cases
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * test cases changes added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Fixes added for repo health status
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Fix added for repo health failure
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Changes added in bash script of add users to remove credential id from users table
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Conflicts resolved
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>

--- api/interservice/infra_proxy/request/orgs.pb.go
+++ api/interservice/infra_proxy/request/orgs.pb.go
@@ -430,6 +430,54 @@ func (x *ResetOrgAdminKey) GetAdminKey() string {
 	return ""
 }
 
+type GetInfraServerOrgs struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Automate Infra Server ID
+	ServerId string `protobuf:"bytes,1,opt,name=server_id,json=serverId,proto3" json:"server_id,omitempty" toml:"server_id,omitempty" mapstructure:"server_id,omitempty"`
+}
+
+func (x *GetInfraServerOrgs) Reset() {
+	*x = GetInfraServerOrgs{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_interservice_infra_proxy_request_orgs_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GetInfraServerOrgs) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetInfraServerOrgs) ProtoMessage() {}
+
+func (x *GetInfraServerOrgs) ProtoReflect() protoreflect.Message {
+	mi := &file_interservice_infra_proxy_request_orgs_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetInfraServerOrgs.ProtoReflect.Descriptor instead.
+func (*GetInfraServerOrgs) Descriptor() ([]byte, []int) {
+	return file_interservice_infra_proxy_request_orgs_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *GetInfraServerOrgs) GetServerId() string {
+	if x != nil {
+		return x.ServerId
+	}
+	return ""
+}
+
 var File_interservice_infra_proxy_request_orgs_proto protoreflect.FileDescriptor
 
 var file_interservice_infra_proxy_request_orgs_proto_rawDesc = []byte{
@@ -472,12 +520,15 @@ var file_interservice_infra_proxy_request_orgs_proto_rawDesc = []byte{
 	0x09, 0x52, 0x02, 0x69, 0x64, 0x12, 0x1b, 0x0a, 0x09, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x5f,
 	0x69, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72,
 	0x49, 0x64, 0x12, 0x1b, 0x0a, 0x09, 0x61, 0x64, 0x6d, 0x69, 0x6e, 0x5f, 0x6b, 0x65, 0x79, 0x18,
-	0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x61, 0x64, 0x6d, 0x69, 0x6e, 0x4b, 0x65, 0x79, 0x42,
-	0x3f, 0x5a, 0x3d, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68,
-	0x65, 0x66, 0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d, 0x61, 0x74, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x2f,
-	0x69, 0x6e, 0x74, 0x65, 0x72, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2f, 0x69, 0x6e, 0x66,
-	0x72, 0x61, 0x5f, 0x70, 0x72, 0x6f, 0x78, 0x79, 0x2f, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x61, 0x64, 0x6d, 0x69, 0x6e, 0x4b, 0x65, 0x79, 0x22,
+	0x31, 0x0a, 0x12, 0x47, 0x65, 0x74, 0x49, 0x6e, 0x66, 0x72, 0x61, 0x53, 0x65, 0x72, 0x76, 0x65,
+	0x72, 0x4f, 0x72, 0x67, 0x73, 0x12, 0x1b, 0x0a, 0x09, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x5f,
+	0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72,
+	0x49, 0x64, 0x42, 0x3f, 0x5a, 0x3d, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
+	0x2f, 0x63, 0x68, 0x65, 0x66, 0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d, 0x61, 0x74, 0x65, 0x2f, 0x61,
+	0x70, 0x69, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2f,
+	0x69, 0x6e, 0x66, 0x72, 0x61, 0x5f, 0x70, 0x72, 0x6f, 0x78, 0x79, 0x2f, 0x72, 0x65, 0x71, 0x75,
+	0x65, 0x73, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -492,14 +543,15 @@ func file_interservice_infra_proxy_request_orgs_proto_rawDescGZIP() []byte {
 	return file_interservice_infra_proxy_request_orgs_proto_rawDescData
 }
 
-var file_interservice_infra_proxy_request_orgs_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_interservice_infra_proxy_request_orgs_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
 var file_interservice_infra_proxy_request_orgs_proto_goTypes = []interface{}{
-	(*CreateOrg)(nil),        // 0: chef.automate.domain.infra_proxy.request.CreateOrg
-	(*UpdateOrg)(nil),        // 1: chef.automate.domain.infra_proxy.request.UpdateOrg
-	(*DeleteOrg)(nil),        // 2: chef.automate.domain.infra_proxy.request.DeleteOrg
-	(*GetOrgs)(nil),          // 3: chef.automate.domain.infra_proxy.request.GetOrgs
-	(*GetOrg)(nil),           // 4: chef.automate.domain.infra_proxy.request.GetOrg
-	(*ResetOrgAdminKey)(nil), // 5: chef.automate.domain.infra_proxy.request.ResetOrgAdminKey
+	(*CreateOrg)(nil),          // 0: chef.automate.domain.infra_proxy.request.CreateOrg
+	(*UpdateOrg)(nil),          // 1: chef.automate.domain.infra_proxy.request.UpdateOrg
+	(*DeleteOrg)(nil),          // 2: chef.automate.domain.infra_proxy.request.DeleteOrg
+	(*GetOrgs)(nil),            // 3: chef.automate.domain.infra_proxy.request.GetOrgs
+	(*GetOrg)(nil),             // 4: chef.automate.domain.infra_proxy.request.GetOrg
+	(*ResetOrgAdminKey)(nil),   // 5: chef.automate.domain.infra_proxy.request.ResetOrgAdminKey
+	(*GetInfraServerOrgs)(nil), // 6: chef.automate.domain.infra_proxy.request.GetInfraServerOrgs
 }
 var file_interservice_infra_proxy_request_orgs_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -587,6 +639,18 @@ func file_interservice_infra_proxy_request_orgs_proto_init() {
 				return nil
 			}
 		}
+		file_interservice_infra_proxy_request_orgs_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GetInfraServerOrgs); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -594,7 +658,7 @@ func file_interservice_infra_proxy_request_orgs_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_interservice_infra_proxy_request_orgs_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   7,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
