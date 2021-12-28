a5a9c29fe72c4a46015691d600679106e8bd612d
jay vikram sharma <jsharma@progress.com>
2021-12-28 10:58:35 +0530

    Signed-off-by: jay vikram sharma <jsharma@progress.com> adding a2ha-manifest-tfvars templates

---
+++ components/automate-cli/cmd/chef-automate/airgabHATmpl.go
@@ -0,0 +1,13 @@
+package main
+
+const manifestTfVars = `
+pgleaderchk_pkg_ident = "            chef/automate-ha-pgleaderchk/0.1.0/20211122152431"
+postgresql_pkg_ident = "            chef/automate-ha-postgresql/11.2/20211118151626"
+proxy_pkg_ident = "            chef/automate-ha-haproxy/2.2.14/20211118151626"
+journalbeat_pkg_ident = "            chef/automate-ha-journalbeat/6.8.6/20211118151544"
+metricbeat_pkg_ident = "            chef/automate-ha-metricbeat/6.8.6/20211118151544"
+kibana_pkg_ident = "            chef/automate-ha-kibana/6.8.6/20211118151625"
+elasticsearch_pkg_ident = "            chef/automate-ha-elasticsearch/6.8.6/20211118151544"
+elasticsidecar_pkg_ident = "            chef/automate-ha-elasticsidecar/0.1.0/20211118151618"
+curator_pkg_ident = "            chef/automate-ha-curator/0.1.0/20211118151626"
+		`
