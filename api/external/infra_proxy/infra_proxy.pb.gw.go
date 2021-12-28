3fb595b9430adee8a45d64a8878e0830415b6628
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2021-12-28 07:39:08 +0530

    API to update the web ui key (#6433)
    * Update web ui key API added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Removed extra files
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Read secret changes added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Conflicts resolved
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Test cases added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Commented validate web ui key test cases
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Changes added in mock file
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * uncomment validate web ui key test cases
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>

--- api/external/infra_proxy/infra_proxy.pb.gw.go
+++ api/external/infra_proxy/infra_proxy.pb.gw.go
@@ -120,6 +120,40 @@ func local_request_InfraProxy_ValidateWebuiKey_0(ctx context.Context, marshaler 
 
 }
 
+func request_InfraProxy_UpdateWebuiKey_0(ctx context.Context, marshaler runtime.Marshaler, client InfraProxyClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq request.UpdateWebuiKey
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.UpdateWebuiKey(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_InfraProxy_UpdateWebuiKey_0(ctx context.Context, marshaler runtime.Marshaler, server InfraProxyServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq request.UpdateWebuiKey
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.UpdateWebuiKey(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_InfraProxy_GetServer_0(ctx context.Context, marshaler runtime.Marshaler, client InfraProxyClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq request.GetServer
 	var metadata runtime.ServerMetadata
@@ -5571,6 +5605,26 @@ func RegisterInfraProxyHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("POST", pattern_InfraProxy_UpdateWebuiKey_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_InfraProxy_UpdateWebuiKey_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_InfraProxy_UpdateWebuiKey_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_InfraProxy_GetServer_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -6812,6 +6866,26 @@ func RegisterInfraProxyHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("POST", pattern_InfraProxy_UpdateWebuiKey_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_InfraProxy_UpdateWebuiKey_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_InfraProxy_UpdateWebuiKey_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_InfraProxy_GetServer_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -7962,6 +8036,8 @@ var (
 
 	pattern_InfraProxy_ValidateWebuiKey_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "v0", "infra", "servers", "validate"}, "", runtime.AssumeColonVerbOpt(true)))
 
+	pattern_InfraProxy_UpdateWebuiKey_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "v0", "infra", "servers", "update"}, "", runtime.AssumeColonVerbOpt(true)))
+
 	pattern_InfraProxy_GetServer_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"api", "v0", "infra", "servers", "id"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_InfraProxy_CreateServer_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v0", "infra", "servers"}, "", runtime.AssumeColonVerbOpt(true)))
@@ -8083,6 +8159,8 @@ var (
 	forward_InfraProxy_GetServerStatus_0 = runtime.ForwardResponseMessage
 
 	forward_InfraProxy_ValidateWebuiKey_0 = runtime.ForwardResponseMessage
+
+	forward_InfraProxy_UpdateWebuiKey_0 = runtime.ForwardResponseMessage
 
 	forward_InfraProxy_GetServer_0 = runtime.ForwardResponseMessage
 
