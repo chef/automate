2c8011cfad7506569fbd662fd02333dae9ad6db9
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2021-12-28 07:39:08 +0530

    API to update the web ui key (#6433)
    * Update web ui key API added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Removed extra files
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Read secret changes added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Conflicts resolved
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Test cases added
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Commented validate web ui key test cases
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * Changes added in mock file
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>
    
    * uncomment validate web ui key test cases
    
    Signed-off-by: sonali wale <swale@msystechnologies.com>

--- api/external/infra_proxy/request/servers.pb.go
+++ api/external/infra_proxy/request/servers.pb.go
@@ -432,6 +432,63 @@ func (x *ValidateWebuiKey) GetWebuiKey() string {
 	return ""
 }
 
+type UpdateWebuiKey struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Chef Infra Server ID.
+	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	// Chef Server Webui Key
+	WebuiKey string `protobuf:"bytes,2,opt,name=webui_key,json=webuiKey,proto3" json:"webui_key,omitempty"`
+}
+
+func (x *UpdateWebuiKey) Reset() {
+	*x = UpdateWebuiKey{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_external_infra_proxy_request_servers_proto_msgTypes[7]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UpdateWebuiKey) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdateWebuiKey) ProtoMessage() {}
+
+func (x *UpdateWebuiKey) ProtoReflect() protoreflect.Message {
+	mi := &file_external_infra_proxy_request_servers_proto_msgTypes[7]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdateWebuiKey.ProtoReflect.Descriptor instead.
+func (*UpdateWebuiKey) Descriptor() ([]byte, []int) {
+	return file_external_infra_proxy_request_servers_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *UpdateWebuiKey) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *UpdateWebuiKey) GetWebuiKey() string {
+	if x != nil {
+		return x.WebuiKey
+	}
+	return ""
+}
+
 var File_external_infra_proxy_request_servers_proto protoreflect.FileDescriptor
 
 var file_external_infra_proxy_request_servers_proto_rawDesc = []byte{
@@ -467,11 +524,15 @@ var file_external_infra_proxy_request_servers_proto_rawDesc = []byte{
 	0x09, 0x52, 0x02, 0x69, 0x64, 0x12, 0x12, 0x0a, 0x04, 0x66, 0x71, 0x64, 0x6e, 0x18, 0x02, 0x20,
 	0x01, 0x28, 0x09, 0x52, 0x04, 0x66, 0x71, 0x64, 0x6e, 0x12, 0x1b, 0x0a, 0x09, 0x77, 0x65, 0x62,
 	0x75, 0x69, 0x5f, 0x6b, 0x65, 0x79, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x77, 0x65,
-	0x62, 0x75, 0x69, 0x4b, 0x65, 0x79, 0x42, 0x3b, 0x5a, 0x39, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62,
-	0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68, 0x65, 0x66, 0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d, 0x61,
-	0x74, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f,
-	0x69, 0x6e, 0x66, 0x72, 0x61, 0x5f, 0x70, 0x72, 0x6f, 0x78, 0x79, 0x2f, 0x72, 0x65, 0x71, 0x75,
-	0x65, 0x73, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x62, 0x75, 0x69, 0x4b, 0x65, 0x79, 0x22, 0x3d, 0x0a, 0x0e, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65,
+	0x57, 0x65, 0x62, 0x75, 0x69, 0x4b, 0x65, 0x79, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x12, 0x1b, 0x0a, 0x09, 0x77, 0x65, 0x62, 0x75,
+	0x69, 0x5f, 0x6b, 0x65, 0x79, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x77, 0x65, 0x62,
+	0x75, 0x69, 0x4b, 0x65, 0x79, 0x42, 0x3b, 0x5a, 0x39, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
+	0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68, 0x65, 0x66, 0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d, 0x61, 0x74,
+	0x65, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x69,
+	0x6e, 0x66, 0x72, 0x61, 0x5f, 0x70, 0x72, 0x6f, 0x78, 0x79, 0x2f, 0x72, 0x65, 0x71, 0x75, 0x65,
+	0x73, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -486,7 +547,7 @@ func file_external_infra_proxy_request_servers_proto_rawDescGZIP() []byte {
 	return file_external_infra_proxy_request_servers_proto_rawDescData
 }
 
-var file_external_infra_proxy_request_servers_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
+var file_external_infra_proxy_request_servers_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_external_infra_proxy_request_servers_proto_goTypes = []interface{}{
 	(*CreateServer)(nil),     // 0: chef.automate.api.infra_proxy.request.CreateServer
 	(*UpdateServer)(nil),     // 1: chef.automate.api.infra_proxy.request.UpdateServer
@@ -495,6 +556,7 @@ var file_external_infra_proxy_request_servers_proto_goTypes = []interface{}{
 	(*GetServer)(nil),        // 4: chef.automate.api.infra_proxy.request.GetServer
 	(*GetServerStatus)(nil),  // 5: chef.automate.api.infra_proxy.request.GetServerStatus
 	(*ValidateWebuiKey)(nil), // 6: chef.automate.api.infra_proxy.request.ValidateWebuiKey
+	(*UpdateWebuiKey)(nil),   // 7: chef.automate.api.infra_proxy.request.UpdateWebuiKey
 }
 var file_external_infra_proxy_request_servers_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -594,6 +656,18 @@ func file_external_infra_proxy_request_servers_proto_init() {
 				return nil
 			}
 		}
+		file_external_infra_proxy_request_servers_proto_msgTypes[7].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UpdateWebuiKey); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -601,7 +675,7 @@ func file_external_infra_proxy_request_servers_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_external_infra_proxy_request_servers_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   7,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
