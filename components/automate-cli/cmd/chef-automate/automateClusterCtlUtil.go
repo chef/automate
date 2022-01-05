06ad2093cbecc5623c8efa67ccadb36111f5464a
jay vikram sharma <jsharma@progress.com>
2022-01-05 19:01:41 +0530

    Signed-off-by: jay vikram sharma <jsharma@progress.com> using regex instead of string replace

--- components/automate-cli/cmd/chef-automate/automateClusterCtlUtil.go
+++ components/automate-cli/cmd/chef-automate/automateClusterCtlUtil.go
@@ -14,6 +14,7 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"time"
 
@@ -26,6 +27,7 @@ import (
 	mc "github.com/chef/automate/components/automate-deployment/pkg/manifest/client"
 	"github.com/chef/automate/lib/version"
 	"github.com/hpcloud/tail"
+	"github.com/sirupsen/logrus"
 )
 
 func executeAutomateClusterCtlCommand(command string, args []string, helpDocs string) error {
@@ -264,69 +266,49 @@ func getBytesFromTempalte(templateName string, templateContent string, placehold
 	}
 	return buf.Bytes()
 }
-func getPkgBldrFormate(pkgName string, actualPkgName string, replacePkgName string) string {
-	_packageName := pkgName
-	_packageName = strings.ReplaceAll(_packageName, "-", "/")
-	return strings.ReplaceAll(_packageName, replacePkgName, actualPkgName)
+func getBldrSupportedPkgName(h string) string {
+	packageName := extractPackageNameFromHartifactPath(h)
+	ver, rel := extarctVersionAndRelease(packageName)
+	origin := extractOrigin(packageName)
+	pkg := extractPackageName(packageName)
+	return origin + "/" + pkg + "/" + ver + "/" + rel
 }
 func generateA2HAManifestTfvars(airgapMetadata airgap.UnpackMetadata) error {
 	var deployablePackages []string
 	for _, h := range airgapMetadata.HartifactPaths {
 		if strings.Contains(h, AUTOMATE_HA_PKG_PG_LDR_CHK) {
-			packageName := extractPackageNameFromHartifactPath(h)
-			packageName = getPkgBldrFormate(packageName, AUTOMATE_HA_PKG_PG_LDR_CHK, _AUTOMATE_HA_PKG_PG_LDR_CHK)
-			deployablePackages = append(deployablePackages, "pgleaderchk_pkg_ident = \""+packageName+"\"")
+			deployablePackages = append(deployablePackages, "pgleaderchk_pkg_ident = \""+getBldrSupportedPkgName(h)+"\"")
 		}
 		if strings.Contains(h, AUTOMATE_HA_PKG_PG) {
-			packageName := extractPackageNameFromHartifactPath(h)
-			packageName = getPkgBldrFormate(packageName, AUTOMATE_HA_PKG_PG, _AUTOMATE_HA_PKG_PG)
-			deployablePackages = append(deployablePackages, "postgresql_pkg_ident = \""+packageName+"\"")
+			deployablePackages = append(deployablePackages, "postgresql_pkg_ident = \""+getBldrSupportedPkgName(h)+"\"")
 		}
 		if strings.Contains(h, AUTOMATE_HA_PKG_HA_PROXY) {
-			packageName := extractPackageNameFromHartifactPath(h)
-			packageName = getPkgBldrFormate(packageName, AUTOMATE_HA_PKG_HA_PROXY, _AUTOMATE_HA_PKG_HA_PROXY)
-			deployablePackages = append(deployablePackages, "proxy_pkg_ident = \""+packageName+"\"")
+			deployablePackages = append(deployablePackages, "proxy_pkg_ident = \""+getBldrSupportedPkgName(h)+"\"")
 		}
 		if strings.Contains(h, AUTOMATE_HA_PKG_JOURNALBEAT) {
-			packageName := extractPackageNameFromHartifactPath(h)
-			packageName = getPkgBldrFormate(packageName, AUTOMATE_HA_PKG_JOURNALBEAT, _AUTOMATE_HA_PKG_JOURNALBEAT)
-			deployablePackages = append(deployablePackages, "journalbeat_pkg_ident = \""+packageName+"\"")
+			deployablePackages = append(deployablePackages, "journalbeat_pkg_ident = \""+getBldrSupportedPkgName(h)+"\"")
 		}
 		if strings.Contains(h, AUTOMATE_HA_PKG_METRICBEAT) {
-			packageName := extractPackageNameFromHartifactPath(h)
-			packageName = getPkgBldrFormate(packageName, AUTOMATE_HA_PKG_METRICBEAT, _AUTOMATE_HA_PKG_METRICBEAT)
-			deployablePackages = append(deployablePackages, "metricbeat_pkg_ident = \""+packageName+"\"")
+			deployablePackages = append(deployablePackages, "metricbeat_pkg_ident = \""+getBldrSupportedPkgName(h)+"\"")
 		}
 		if strings.Contains(h, AUTOMATE_HA_PKG_KIBANA) {
-			packageName := extractPackageNameFromHartifactPath(h)
-			packageName = getPkgBldrFormate(packageName, AUTOMATE_HA_PKG_KIBANA, _AUTOMATE_HA_PKG_KIBANA)
-			deployablePackages = append(deployablePackages, "kibana_pkg_ident = \""+packageName+"\"")
+			deployablePackages = append(deployablePackages, "kibana_pkg_ident = \""+getBldrSupportedPkgName(h)+"\"")
 		}
 		if strings.Contains(h, AUTOMATE_HA_ES) {
-			packageName := extractPackageNameFromHartifactPath(h)
-			packageName = getPkgBldrFormate(packageName, AUTOMATE_HA_ES, _AUTOMATE_HA_ES)
-			deployablePackages = append(deployablePackages, "elasticsearch_pkg_ident = \""+packageName+"\"")
+			deployablePackages = append(deployablePackages, "elasticsearch_pkg_ident = \""+getBldrSupportedPkgName(h)+"\"")
 		}
 		if strings.Contains(h, AUTOMATE_HA_ES_CAR) {
-			packageName := extractPackageNameFromHartifactPath(h)
-			packageName = getPkgBldrFormate(packageName, AUTOMATE_HA_ES_CAR, _AUTOMATE_HA_ES_CAR)
-			deployablePackages = append(deployablePackages, "elasticsidecar_pkg_ident = \""+packageName+"\"")
+			deployablePackages = append(deployablePackages, "elasticsidecar_pkg_ident = \""+getBldrSupportedPkgName(h)+"\"")
 		}
 		if strings.Contains(h, AUTOMATE_HA_CURATOR) {
-			packageName := extractPackageNameFromHartifactPath(h)
-			packageName = getPkgBldrFormate(packageName, AUTOMATE_HA_CURATOR, _AUTOMATE_HA_CURATOR)
-			deployablePackages = append(deployablePackages, "curator_pkg_ident = \""+packageName+"\"")
+			deployablePackages = append(deployablePackages, "curator_pkg_ident = \""+getBldrSupportedPkgName(h)+"\"")
 		}
 	}
 	return ioutil.WriteFile(AUTOMATE_HA_TERRAFORM_DIR+"a2ha_manifest.auto.tfvars", []byte(strings.Join(deployablePackages[:], "\n")), AUTOMATE_HA_FILE_PERMISSION_0755) // nosemgrep
 }
 func extractPackageNameFromHartifactPath(path string) string {
 	path = strings.ReplaceAll(path, "/hab/cache/artifacts/", "")
-	path = strings.ReplaceAll(path, "-x86_64-linux.hart", "")
-	originIndex := strings.Index(path, "-")
-	orignName := path[0:originIndex]
-	packageName := (orignName + "/" + path[originIndex+1:])
-	return packageName
+	return path
 }
 
 func copyFileContents(src, dst string) (err error) {
@@ -477,4 +459,31 @@ func executeShellCommand(command string, args []string, workingDir string) error
 	}
 	writer.Printf("%s command execution done, exiting\n", command)
 	return err
+}
+
+func extarctVersionAndRelease(filename string) (string, string) {
+	r := regexp.MustCompile(`.*-(\d+\.\d+\.\d+)-(\d{14})-.*\.hart$`)
+	match := r.FindStringSubmatch(filename)
+	if match == nil {
+		logrus.Debug("failed to parse version of hart %s", filename)
+	}
+	return match[1], match[2]
+}
+
+func extractOrigin(filename string) string {
+	r := regexp.MustCompile(`[a-zA-z0-9]*`)
+	match := r.FindStringSubmatch(filename)
+	if match == nil {
+		logrus.Debug("failed to parse origin from of hart %s", filename)
+	}
+	return match[0]
+}
+
+func extractPackageName(filename string) string {
+	r := regexp.MustCompile(`-[a-zA-z0-9]*-.*-\d\.`)
+	match := r.FindStringSubmatch(filename)
+	if match == nil {
+		fmt.Printf("failed to parse package name of hart %s", filename)
+	}
+	return match[0][1 : len(match[0])-3]
 }
