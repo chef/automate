5d74732afc1823ccb0fbabe31512ba7f44dc8d2e
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2022-01-05 21:54:06 +0530

    Database script for migration tables (#6500)
    * Script added for migration tables
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Insert scripts added for migration type and status
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Server id column added in migration table
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Index added on server_id column in migration table
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>

---
+++ components/infra-proxy-service/storage/postgres/migration/sql/07_migration.down.sql
@@ -0,0 +1,8 @@
+-- drop table migration
+DROP TABLE IF EXISTS migration;
+
+-- drop table migration_type
+DROP TABLE IF EXISTS migration_type;
+
+-- drop table migration_status
+DROP TABLE IF EXISTS migration_status;
