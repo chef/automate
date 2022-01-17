485c5a2a3c39cf9850f502f66089f6faf30096f2
Kallol Roy <karoy@progress.com>
2022-01-17 12:19:29 +0530

    added Todo(milestone) for the servers
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- components/automate-deployment/pkg/manifest/provider.go
+++ components/automate-deployment/pkg/manifest/provider.go
@@ -97,6 +97,8 @@ func (c *cachingReleaseManifestProvider) RefreshManifest(ctx context.Context, ch
 		return nil, err
 	}
 
+	// Todo(milestone) -- Add a check if the next version is a major version.
+
 	c.cache[channel] = cacheEntry{
 		lastCheck:      c.nowProvider(),
 		cachedManifest: m,
