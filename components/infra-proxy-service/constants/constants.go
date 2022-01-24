fe2517f64596e7266fb9572e780f8a80746fc2e1
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2022-01-24 13:42:27 +0530

    Storage functions for the migration phases (#6529)
    * Get organisation API changes added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * minor changes added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Remove command to create an orgs
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Client Mock changes
    
    Signed-off-by: Yashvi Jain <Yashvi.jain@progress.com>
    
    * Storage function for all the phases of migration
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Minor changes added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Incorporated review comments
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Minor changes added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Changes for review comments
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Comments removed
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    Co-authored-by: Yashvi Jain <Yashvi.jain@progress.com>

--- components/infra-proxy-service/constants/constants.go
+++ components/infra-proxy-service/constants/constants.go
@@ -1,6 +1,48 @@
 package constants
 
+// MigrationPhase: enum of migration phases
+type MigrationPhase int64
+
+// Migration phases id should be between 100 & 5000
+const (
+	StartMigration    = 100
+	CompleteMigration = 5000
+)
+
+// MigrationStatus: enum of migration status
+type MigrationStatus int64
+
 // IAM default project ID
 const (
 	UnassignedProjectID = "(unassigned)"
+)
+
+const (
+	StartFileUpload MigrationPhase = iota + 1000
+	CompleteFileUpload
+	FailedFileUpload
+	StartUnzip
+	ComplteUnzip
+	FailedUnzip
+	StartZipParsing
+	CompleteZipParsing
+	FailedZipParsing
+	StartOrgMigration
+	CompleteOrgMigration
+	FailedOrgMigration
+	StartUserMigration
+	CompleteUserMigration
+	FailedUserMigration
+	StartAssciation
+	CompleteAssciation
+	FailedAssciation
+	StartPermissionMigration
+	CompletePermissionMigration
+	FailedPermissionMigration
+)
+
+const (
+	InProgress MigrationStatus = iota + 100
+	Completed
+	Failed
 )
