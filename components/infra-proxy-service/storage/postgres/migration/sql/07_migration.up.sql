81a8a7d225844d1014fa8ec87605ec49f79431f1
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2022-01-25 15:06:23 +0530

    Migration stage script and storage functions added (#6619)
    Signed-off-by: sonali wale <sonali.wale@progress.com>

--- components/infra-proxy-service/storage/postgres/migration/sql/07_migration.up.sql
+++ components/infra-proxy-service/storage/postgres/migration/sql/07_migration.up.sql
@@ -98,3 +98,77 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
+
+-- Create table migration_stage
+CREATE TABLE IF NOT EXISTS migration_stage (
+  id                               SERIAL PRIMARY KEY,
+  migration_id                     TEXT NOT NULL,
+  parsed_data                      json NOT NULL DEFAULT '{}',
+  created_at                       TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at                       TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+-- Created type migration_stage_records
+CREATE TYPE migration_stage_records AS (
+  id                               TEXT,
+  migration_id                     TEXT,
+  parsed_data                      json,
+  created_at                       TIMESTAMPTZ,
+  updated_at                       TIMESTAMPTZ
+);
+
+-- Insert entry into migration_stage table
+CREATE OR REPLACE FUNCTION insert_migration_stage(migrationId TEXT, parsedData json) 
+RETURNS json AS
+$$
+DECLARE result_record migration_stage_records;
+BEGIN
+	INSERT INTO migration_stage (
+		migration_id, parsed_data, 
+		created_at, updated_at)
+	VALUES (migrationId, parsedData, now(), now())
+  RETURNING id, migration_id, parsed_data ,created_at, updated_at
+  INTO 
+    result_record.id,result_record.migration_id,result_record.parsed_data,
+    result_record.created_at,result_record.updated_at;
+
+  RETURN row_to_json(result_record);
+
+END;
+$$
+LANGUAGE plpgsql;
+
+-- Get entry from migration_stage table
+CREATE OR REPLACE FUNCTION get_migration_stage(migrationId TEXT) 
+RETURNS json AS
+$$
+DECLARE result_record migration_stage_records;
+BEGIN
+	SELECT * FROM migration_stage where migration_id = migrationId
+  INTO 
+    result_record.id,result_record.migration_id,result_record.parsed_data,
+    result_record.created_at,result_record.updated_at;
+
+  RETURN row_to_json(result_record);
+
+END;
+$$
+LANGUAGE plpgsql;
+
+-- Delete entry from migration_stage table
+CREATE OR REPLACE FUNCTION delete_migration_stage(migrationId TEXT) 
+RETURNS json AS
+$$
+DECLARE result_record migration_stage_records;
+BEGIN
+	DELETE FROM migration_stage where migration_id = migrationId
+  RETURNING id, migration_id, parsed_data ,created_at, updated_at
+  INTO 
+    result_record.id,result_record.migration_id,result_record.parsed_data,
+    result_record.created_at,result_record.updated_at;
+
+  RETURN row_to_json(result_record);
+
+END;
+$$
+LANGUAGE plpgsql;
