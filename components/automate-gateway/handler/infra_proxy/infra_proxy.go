9240c4f235f9f1feaa173974ca1f175689a092d4
Yashvi Jain <91940132+YashviJain01@users.noreply.github.com>
2022-01-26 20:50:33 +0530

    Migration Data service creation and created a rpc method for upload zip file (#6612)
    * Changes for stream api for upload
    
    Signed-off-by: Yashvi Jain <Yashvi.jain@progress.com>
    
    * changes
    
    Signed-off-by: Yashvi Jain <Yashvi.jain@progress.com>
    
    * added some changes for the protofile
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>
    
    * added function changes
    
    * added changes for the proto
    
    * added changes to devide the chunks in bytes
    
    * added changes for the validation
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>
    
    * adding status in database
    
    Signed-off-by: Yashvi Jain <Yashvi.jain@progress.com>
    
    * adding the request for server id and filename
    
    Signed-off-by: Yashvi Jain <Yashvi.jain@progress.com>
    
    * Adding  loggers and saving the failed migration phase
    
    Signed-off-by: Yashvi Jain <Yashvi.jain@progress.com>
    
    * optimising the code
    
    Signed-off-by: Yashvi Jain <Yashvi.jain@progress.com>
    
    * Changes for content-type
    
    Signed-off-by: Yashvi Jain <Yashvi.jain@progress.com>
    
    * added changes for the validation
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>
    
    * Upload to the hab svc data folder
    
    Signed-off-by: Kallol Roy <karoy@progress.com>
    
    * Enhance log and modify migration phases
    
    Signed-off-by: Kallol Roy <karoy@progress.com>
    
    * Granularize the migration stages
    
    Signed-off-by: Kallol Roy <karoy@progress.com>
    
    * Granularize the migration stages
    
    Signed-off-by: Kallol Roy <karoy@progress.com>
    
    Co-authored-by: Vinay Sharma <vsharma@chef.io>
    Co-authored-by: Kallol Roy <karoy@progress.com>

--- components/automate-gateway/handler/infra_proxy/infra_proxy.go
+++ components/automate-gateway/handler/infra_proxy/infra_proxy.go
@@ -1,17 +1,20 @@
 package infra_proxy
 
 import (
+	migrations "github.com/chef/automate/api/interservice/infra_proxy/migrations/service"
 	infra_proxy "github.com/chef/automate/api/interservice/infra_proxy/service"
 )
 
 // InfraProxyServer stores client
 type InfraProxyServer struct {
-	client infra_proxy.InfraProxyServiceClient
+	client          infra_proxy.InfraProxyServiceClient
+	migrationClient migrations.MigrationDataServiceClient
 }
 
 // NewInfraProxyHandler initializes InfraProxyServer with client
-func NewInfraProxyHandler(client infra_proxy.InfraProxyServiceClient) *InfraProxyServer {
+func NewInfraProxyHandler(client infra_proxy.InfraProxyServiceClient, migrationClient migrations.MigrationDataServiceClient) *InfraProxyServer {
 	return &InfraProxyServer{
-		client: client,
+		client:          client,
+		migrationClient: migrationClient,
 	}
 }
