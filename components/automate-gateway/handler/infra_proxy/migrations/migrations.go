a24be8fd386cf2974ddbaabab39f3e59abf6ae86
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2022-01-31 12:43:26 +0530

    API to fetch the status of the migration (#6637)
    * Get migrations status API added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Added minor changes
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>

--- components/automate-gateway/handler/infra_proxy/migrations/migrations.go
+++ components/automate-gateway/handler/infra_proxy/migrations/migrations.go
@@ -1,1 +1,26 @@
 package migrations
+
+import (
+	"context"
+
+	gwreq "github.com/chef/automate/api/external/infra_proxy/migrations/request"
+	gwres "github.com/chef/automate/api/external/infra_proxy/migrations/response"
+	infra_req "github.com/chef/automate/api/interservice/infra_proxy/migrations/request"
+)
+
+// GetMigrationStatus fetches the latest migration status against migration id
+func (a *InfraProxyMigrationServer) GetMigrationStatus(ctx context.Context, r *gwreq.GetMigrationStatus) (*gwres.GetMigrationStatus, error) {
+	req := &infra_req.GetMigrationStatus{
+		MigrationId: r.MigrationId,
+	}
+	res, err := a.migrationClient.GetMigrationStatus(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &gwres.GetMigrationStatus{
+		MigrationId:     res.GetMigrationId(),
+		MigrationType:   res.GetMigrationType(),
+		MigrationStatus: res.GetMigrationStatus(),
+	}, nil
+}
