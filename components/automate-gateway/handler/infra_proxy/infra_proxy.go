8ec51e2c969a878419378faf83736b029d6f1d44
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2022-01-31 12:43:26 +0530

    API to fetch the status of the migration (#6637)
    * Get migrations status API added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Added minor changes
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>

--- components/automate-gateway/handler/infra_proxy/infra_proxy.go
+++ components/automate-gateway/handler/infra_proxy/infra_proxy.go
@@ -1,20 +1,17 @@
 package infra_proxy
 
 import (
-	migrations "github.com/chef/automate/api/interservice/infra_proxy/migrations/service"
 	infra_proxy "github.com/chef/automate/api/interservice/infra_proxy/service"
 )
 
 // InfraProxyServer stores client
 type InfraProxyServer struct {
-	client          infra_proxy.InfraProxyServiceClient
-	migrationClient migrations.MigrationDataServiceClient
+	client infra_proxy.InfraProxyServiceClient
 }
 
 // NewInfraProxyHandler initializes InfraProxyServer with client
-func NewInfraProxyHandler(client infra_proxy.InfraProxyServiceClient, migrationClient migrations.MigrationDataServiceClient) *InfraProxyServer {
+func NewInfraProxyHandler(client infra_proxy.InfraProxyServiceClient) *InfraProxyServer {
 	return &InfraProxyServer{
-		client:          client,
-		migrationClient: migrationClient,
+		client: client,
 	}
 }
---
+++ components/automate-gateway/handler/infra_proxy/migrations/infra_proxy_migrations.go
@@ -0,0 +1,15 @@
+package migrations
+
+import migrations "github.com/chef/automate/api/interservice/infra_proxy/migrations/service"
+
+// InfraProxyMigrationServer stores client
+type InfraProxyMigrationServer struct {
+	migrationClient migrations.MigrationDataServiceClient
+}
+
+// NewInfraProxyMigrationHandler initializes InfraProxyMigrationServer with client
+func NewInfraProxyMigrationHandler(migrationClient migrations.MigrationDataServiceClient) *InfraProxyMigrationServer {
+	return &InfraProxyMigrationServer{
+		migrationClient: migrationClient,
+	}
+}
