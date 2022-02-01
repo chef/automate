52a7a57945790895f43048b8749539d1454f8897
Chaitali Mane <71449322+chaitali-mane@users.noreply.github.com>
2022-02-01 14:12:51 +0530

    Stalwart 32 upload slider functionality (#6654)
    * Added upload slider
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Update upload functionality
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Added drag and drop function
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Added API integration code
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Added file upload changes
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Added cypress test cases
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Added unit tests
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Added loader
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Updated code
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * added header changes + added some type changes + fixed slider open and close after upload
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>
    
    * Updated for selector
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    Co-authored-by: Vinay Sharma <vsharma@chef.io>

---
+++ components/automate-ui/src/app/modules/infra-proxy/sync-org-users-slider/sync-org-users-slider.component.ts
@@ -0,0 +1,106 @@
+import { Component, EventEmitter, Input, Output, OnInit, HostBinding, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { IdMapper } from 'app/helpers/auth/id-mapper';
+import { Utilities } from 'app/helpers/utilities/utilities';
+
+@Component({
+  selector: 'app-sync-org-users-slider',
+  templateUrl: './sync-org-users-slider.component.html',
+  styleUrls: ['./sync-org-users-slider.component.scss']
+})
+export class SyncOrgUsersSliderComponent implements OnInit {
+  @Input() isUploaded = false;
+  @Input() uploadForm: FormGroup;
+  @Input() conflictErrorEvent: EventEmitter<boolean>;
+  @Output() uploadClicked = new EventEmitter();
+  @HostBinding('class.active') isSlideOpen = false;
+
+  public uploading = false;
+  public conflictError = false;
+  public migrationSliderVisible = false;
+  public migrationSlider = false;
+
+  @ViewChild('fileDropRef', { static: false }) fileDropEl: ElementRef;
+  public file: File = null;
+  public isFile = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.conflictErrorEvent.subscribe((isConflict: boolean) => {
+      this.conflictError = isConflict;
+      // Open the ID input on conflict so user can resolve it.
+    });
+  }
+
+  handleNameInput(event: KeyboardEvent): void {
+    if (!Utilities.isNavigationKey(event)) {
+      this.conflictError = false;
+      this.uploadForm.controls.id.setValue(
+        IdMapper.transform(this.uploadForm.controls.file.value.trim()));
+    }
+  }
+
+  uploadFile(): void {
+    this.uploading = true;
+    const formData = new FormData();
+    this.file = this.uploadForm.get('file').value;
+    formData.append('file', this.file);
+    this.uploadClicked.emit(this.file);
+    this.migrationSlider = true;
+    this.closeUploadSlider();
+  }
+
+  closeUploadSlider() {
+    this.uploading = false;
+    this.isUploaded = false;
+    this.toggleSlide();
+    this.file = null;
+    this.isFile = false;
+    this.isSlideOpen = false;
+  }
+
+  toggleSlide() {
+    this.isSlideOpen = !this.isSlideOpen;
+  }
+
+  slidePanel() {
+    this.isSlideOpen = true;
+  }
+
+  public openMigrationSlider(): void {
+    this.migrationSliderVisible = true;
+    this.resetMigrationSlider();
+  }
+
+  public closeMigrationSlider(): void {
+    this.migrationSliderVisible = false;
+    this.resetMigrationSlider();
+  }
+
+  private resetMigrationSlider(): void {
+    this.conflictErrorEvent.emit(false);
+  }
+
+  /*** on file drop handler */
+  onFileDropped($event: any[]) {
+    this.prepareFilesList($event);
+  }
+
+  /*** handle file from browsing */
+  fileBrowseHandler(file: any) {
+    this.prepareFilesList(file);
+  }
+
+  prepareFilesList(file: any) {
+    this.file = file[0];
+    this.uploadForm.get('file').setValue(this.file);
+    if ( this.file.type !== 'application/zip' ) {
+      this.isFile = false;
+      this.file = null;
+    } else {
+      this.isFile = true;
+    }
+    this.fileDropEl.nativeElement.value = '';
+  }
+}
