eacbe61ec1760b21e32e070b522c73d10582cd9f
vinay sharma <vsharma@chef.io>
2022-02-01 15:05:53 +0530

    Cancel migration API to cancel the running migration (#6653)
    * added changes for the cancel migration API
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>
    
    * Added changes to update the status and clear off data
    
    Signed-off-by: Kallol Roy <karoy@progress.com>
    
    * Fixed SQL query
    
    Signed-off-by: Kallol Roy <karoy@progress.com>
    
    Co-authored-by: Kallol Roy <karoy@progress.com>

--- components/infra-proxy-service/storage/postgres/migrations.go
+++ components/infra-proxy-service/storage/postgres/migrations.go
@@ -189,6 +189,16 @@ func (p *postgres) FailedMigration(ctx context.Context, migrationId, serverId, m
 	return p.insertMigration(ctx, migrationId, serverId, message, int64(constants.CompleteMigration), int64(constants.Failed), totalSucceeded, totalSkipped, totalFailed)
 }
 
+// CancelMigration Insert a migration entry to the DB for migration completed
+func (p *postgres) CancelMigration(ctx context.Context, migrationId, serverId string, totalSucceeded, totalSkipped, totalFailed int64) (storage.Migration, error) {
+	return p.insertMigration(ctx, migrationId, serverId, "", int64(constants.CancelMigration), int64(constants.Completed), totalSucceeded, totalSkipped, totalFailed)
+}
+
+// FailedCancelMigration Insert a migration entry to the DB for migration completed
+func (p *postgres) FailedCancelMigration(ctx context.Context, migrationId, serverId, message string, totalSucceeded, totalSkipped, totalFailed int64) (storage.Migration, error) {
+	return p.insertMigration(ctx, migrationId, serverId, message, int64(constants.CancelMigration), int64(constants.Failed), totalSucceeded, totalSkipped, totalFailed)
+}
+
 //TODO: Optimize the function parameters using variadic
 //insertMigration Inserts an entry to the DB
 func (p *postgres) insertMigration(ctx context.Context, migrationId, serverId, message string, migTypeId, migStatusId, totalSucceeded, totalSkipped, totalFailed int64) (storage.Migration, error) {
@@ -232,14 +242,14 @@ func (p *postgres) GetActiveMigration(ctx context.Context, serverId string) (sto
 // GetMigrationStatus Fetches migration status against migration id
 func (p *postgres) GetMigrationStatus(ctx context.Context, migrationId string) (storage.MigrationStatus, error) {
 	var m storage.MigrationStatus
-	query := `select m.migration_id,t.type,s.status_message
+	query := `select m.migration_id,t.type,s.status_message, t.id, s.id
 			  from migration m 
 			  join migration_type t on m.type_id=t.id 
 			  join migration_status s on m.status_id=s.id 
 			  and t.id <= 5000 and m.migration_id=$1 order by updated_timestamp desc FETCH FIRST ROW ONLY`
 	err := p.db.QueryRowContext(ctx,
 		query, migrationId).
-		Scan(&m.MigrationID, &m.MigrationType, &m.MigrationStatus)
+		Scan(&m.MigrationID, &m.MigrationType, &m.MigrationStatus, &m.MigrationTypeID, &m.MigrationStatusID)
 
 	if err != nil {
 		return storage.MigrationStatus{}, err
