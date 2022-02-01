116a61c791a03c7f44dc18f8004cfc90b4c295d3
Kallol Roy <karoy@progress.com>
2022-02-01 08:32:58 +0530

    Fixed SQL query
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- components/infra-proxy-service/storage/postgres/migration/sql/07_migration.up.sql
+++ components/infra-proxy-service/storage/postgres/migration/sql/07_migration.up.sql
@@ -40,7 +40,7 @@ INSERT INTO migration_type VALUES
       (1008,'Migration of users'),
       (1009,'Association of users to orgs'),
       (1010,'Migrating user permissions'),
-      (5000,'Migration Completed')
+      (5000,'Migration Completed'),
       (6000,'Migration Cancelled')
 ;
 
