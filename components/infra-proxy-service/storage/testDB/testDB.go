1bb60ae8523c646d051bab82a69975be303c9c5c
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2022-02-02 18:30:41 +0530

    Stalwart 24 Pipeline function to store the orgs to database (#6658)
    * Pipeline function to store the orgs
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Added minor changes
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Code formatting and setting up tests DB
    
    Signed-off-by: Kallol Roy <karoy@progress.com>
    
    * Added migration status changes
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Added minor changes
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Comment removed
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Logs added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Minor changes added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Removed message variable
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Incorporating review comments
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    Co-authored-by: Kallol Roy <karoy@progress.com>

---
+++ components/infra-proxy-service/storage/testDB/testDB.go
@@ -0,0 +1,104 @@
+package testDB
+
+import (
+	"context"
+	"errors"
+	"github.com/chef/automate/components/infra-proxy-service/storage"
+)
+
+type TestDB struct {
+	Orgs      map[string]storage.Org
+	Servers   map[string]storage.Server
+	Users     map[string]storage.User
+	NeedError bool
+}
+
+func (t *TestDB) GetServer(_ context.Context, id string) (storage.Server, error) {
+	if t.NeedError {
+		return storage.Server{}, errors.New("failed to fetch Server")
+	}
+	if _, ok := t.Servers[id]; !ok {
+		return storage.Server{}, errors.New("No such server found")
+	}
+	return t.Servers[id], nil
+}
+
+func (t *TestDB) GetServers(ctx context.Context) ([]storage.Server, error) {
+	if t.NeedError {
+		return []storage.Server{}, errors.New("failed to fetch Server")
+	}
+	var x []storage.Server
+	for _, v := range t.Servers {
+		x = append(x, v)
+	}
+	return x, nil
+}
+
+func (t *TestDB) StoreServer(ctx context.Context, id string, name string, fqdn string, ipAddress string, credentialId string) (storage.Server, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) DeleteServer(ctx context.Context, id string) (storage.Server, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) EditServer(ctx context.Context, id string, name string, fqdn string, ipAddress string) (storage.Server, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) EditServerWebuiKey(ctx context.Context, id, credentialId string) (storage.Server, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) GetOrg(ctx context.Context, orgID string, serverID string) (storage.Org, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) GetOrgs(ctx context.Context, serverID string) ([]storage.Org, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) StoreOrg(ctx context.Context, id string, name string, adminUser string, adminKey string, serverID string, projects []string) (storage.Org, error) {
+	if t.NeedError {
+		return storage.Org{}, errors.New("failed to store org")
+	}
+	return storage.Org{ID: id, Name: name, AdminUser: adminUser, CredentialID: adminKey, ServerID: serverID, Projects: projects}, nil
+}
+
+func (t *TestDB) DeleteOrg(ctx context.Context, orgID string, serverID string) (storage.Org, error) {
+	if t.NeedError {
+		return storage.Org{}, errors.New("failed to delete org")
+	}
+	return storage.Org{ID: orgID}, nil
+}
+
+func (t *TestDB) EditOrg(ctx context.Context, id string, name string, adminUser string, serverID string, projects []string) (storage.Org, error) {
+	if t.NeedError {
+		return storage.Org{}, errors.New("failed to edit org")
+	}
+	return storage.Org{ID: id, Name: name, AdminUser: adminUser, ServerID: serverID, Projects: projects}, nil
+}
+
+func (t *TestDB) TouchOrg(ctx context.Context, id string, serverID string) (storage.Org, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) InsertUser(ctx context.Context, id, serverID, infraServerUsername, credentialID, Connector, automateUserID string, IsServerAdmin bool) (storage.User, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) GetUser(ctx context.Context, id string) (storage.User, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) EditUser(ctx context.Context, id, serverID, infraServerUsername, credentialID, Connector, automateUserID string, IsServerAdmin bool) (storage.User, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) DeleteUser(ctx context.Context, id string) (storage.User, error) {
+	panic("implement me")
+}
+
+func (t *TestDB) GetAutomateInfraServerUsers(ctx context.Context, serverId string) ([]storage.User, error) {
+	panic("implement me")
+}
