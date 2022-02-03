0aa13212847ec5ef74175a01621a1c0d57751237
Chaitali Mane <cmane@progress.com>
2022-02-03 19:37:20 +0530

    Added metadata changes
    Signed-off-by: Chaitali Mane <cmane@progress.com>

--- components/automate-ui/src/app/modules/infra-proxy/migration-slider/migration-slider.component.ts
+++ components/automate-ui/src/app/modules/infra-proxy/migration-slider/migration-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { IdMapper } from 'app/helpers/auth/id-mapper';
 import { Utilities } from 'app/helpers/utilities/utilities';
@@ -9,8 +9,15 @@ import { Utilities } from 'app/helpers/utilities/utilities';
   styleUrls: ['./migration-slider.component.scss']
 })
 export class MigrationSliderComponent {
+  @Input() migrationID: string;
+  @Input() previewData;
+  @Output() migrationPreview = new EventEmitter();
+  @Output() cancelMigration = new EventEmitter();
+
   public migrating = false;
   public checkedUser = false;
+  public isLDPUser = true;
+  public isEditable = true;
   public migrationForm: FormGroup;
 
   @HostBinding('class.active') isSlideOpen1 = false;
@@ -27,17 +34,28 @@ export class MigrationSliderComponent {
     this.toggleSlide();
   }
 
+  cancelMigrationclicked() {
+    this.toggleSlide();
+    this.cancelMigration.emit(this.migrationID);
+  }
+
   toggleSlide() {
     this.isSlideOpen1 = !this.isSlideOpen1;
   }
 
   migrationFile() {
     this.toggleSlide();
+    this.migrationPreview.emit(this.migrationForm);
   }
 
   slidePanel() {
-    console.log('migration part');
     this.isSlideOpen1 = true;
+  }
+
+  selectedAllUsers(event: any) {
+    const checked = event.target.checked;
+    console.log(checked);
+    // this.users.forEach(item => item.selected = checked);
   }
 
   selectedUser(value: boolean) {
