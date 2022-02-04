5904facbeba15136674e764cae136a023d29b543
Dave Augustus <95467821+dave-augustus@users.noreply.github.com>
2022-02-04 22:48:19 +0530

    Feature/migration pipe (#6647)
    * Pipeline demo
    
    * Demo Pipeline
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Based on PR Review
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Models
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Updateed select statement
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Removed package-lock.json
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Pipeline to process and close the go routines
    
    Signed-off-by: Kallol Roy <karoy@progress.com>
    
    * Feature/models (#6655)
    
    * Pipeline demo
    
    * Demo Pipeline
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Based on PR Review
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Models
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Models done
    
    * Package JSON change
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Package JSON change
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Package JSON change
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Setup pipeline to process data
    
    Signed-off-by: Kallol Roy <karoy@progress.com>
    
    * Some comment corrections
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    Co-authored-by: Kallol Roy <karoy@progress.com>

--- components/infra-proxy-service/migrations/server.go
+++ components/infra-proxy-service/migrations/server.go
@@ -1,16 +1,20 @@
 package migrations
 
 import (
+	"github.com/chef/automate/components/infra-proxy-service/migrations/pipeline"
 	"github.com/chef/automate/components/infra-proxy-service/service"
 )
 
 type MigrationServer struct {
-	service *service.Service
+	service          *service.Service
+	phaseOnePipeline pipeline.PhaseOnePipleine
 }
 
 // NewMigrationServer returns an infra-proxy migration server
 func NewMigrationServer(service *service.Service) *MigrationServer {
+	c := pipeline.SetupPhaseOnePipeline()
 	return &MigrationServer{
-		service: service,
+		service:          service,
+		phaseOnePipeline: c,
 	}
 }
