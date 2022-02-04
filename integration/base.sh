cb9b8e2c39aa9930b5e5056b0c701d30b2c1ea38
Vivek Shankar <vshankar@progress.com>
2022-02-05 02:08:04 +0530

    add upgrade prepare
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- integration/base.sh
+++ integration/base.sh
@@ -150,13 +150,13 @@ do_build_default() {
         create_manifest "$test_manifest_dir/build.json"
     fi
 
-    if [ $test_upgrades = true ]; then
+    if [ $test_upgrades_ms = true ]; then
         log_info "Downloading channel manifests"
         download_manifest_version "dev" "$test_manifest_dir/dev.json" "20220131135806"
         download_manifest_version "acceptance" "$test_manifest_dir/acceptance.json" "20220121191356"
         download_manifest_version "current" "$test_manifest_dir/current.json" "20220121191356"
         log_info "Creating build.json"
-        create_manifest "$test_manifest_dir/build.json"
+        download_manifest_version "dev" "$test_manifest_dir/build.json" "20220131135806"
     fi
 
     log_info "Building Tools"
