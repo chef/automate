78689f93f78dac8a8f45189c931593c2a219bd76
sonali wale <sonali.wale@progress.com>
2022-02-08 10:44:06 +0530

    Test cases added
    Signed-off-by: sonali wale <sonali.wale@progress.com>

--- components/infra-proxy-service/migrations/migrations_test.go
+++ components/infra-proxy-service/migrations/migrations_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/chef/automate/api/external/infra_proxy/migrations/request"
+
 	"github.com/chef/automate/api/external/infra_proxy/migrations/response"
 
 	"github.com/golang/mock/gomock"
@@ -24,12 +25,12 @@ func TestMigrations(t *testing.T) {
 	t.Run("GetMigrationStatus", func(t *testing.T) {
 		test.ResetState(ctx, t, serviceRef)
 
-		res := &response.GetMigrationStatus{
+		res := &response.GetMigrationStatusResponse{
 			MigrationId:     "Fake id",
 			MigrationType:   "Migration Completed",
 			MigrationStatus: "Failed",
 		}
-		req := request.GetMigrationStatus{
+		req := request.GetMigrationStatusRequest{
 			MigrationId: "Fake id",
 		}
 		t.Run("when a migration id which exist in migration table is submitted, then return the latest status.", func(t *testing.T) {
@@ -40,6 +41,44 @@ func TestMigrations(t *testing.T) {
 		t.Run("when a migration id is not exist in migration table is submitted, then return an error", func(t *testing.T) {
 			infraMigrationMockClient.EXPECT().GetMigrationStatus(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, errors.New("sql: no rows in result set"))
 
+		})
+	})
+
+	t.Run("GetStagedData", func(t *testing.T) {
+		test.ResetState(ctx, t, serviceRef)
+
+		res := &response.GetStagedDataResponse{
+			MigrationId: "Fake id",
+			StagedData: &response.StagedData{
+				OrgsToMigrate: 2,
+				OrgsToSkip:    1,
+				OrgsToUpdate:  1,
+				OrgsToDelete:  1,
+				Users: []*response.User{
+					{
+						Username:         "test1",
+						Email:            "abc1@gmail.com",
+						DisplayName:      "test1",
+						FirstName:        "first1",
+						LastName:         "last1",
+						MiddleName:       "middle1",
+						AutomateUsername: "test1",
+						Connector:        "local",
+						IsConflicting:    true,
+						IsAdmin:          true,
+					},
+				},
+			},
+		}
+		req := request.GetStagedDataRequest{
+			MigrationId: "Fake id",
+		}
+		t.Run("when a migration id which exist in staged table is submitted, then return the staged data", func(t *testing.T) {
+			infraMigrationMockClient.EXPECT().GetStagedData(gomock.Any(), req, gomock.Any()).Return(res, nil)
+		})
+
+		t.Run("when a migration id is not exist in staged table is submitted, then return an error", func(t *testing.T) {
+			infraMigrationMockClient.EXPECT().GetStagedData(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, errors.New("sql: no rows in result set"))
 		})
 	})
 }
