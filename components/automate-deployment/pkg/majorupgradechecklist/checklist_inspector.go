adeaa769716bde7e3d7e0914e5b99ee3d50ee660
shaik80 <smudassi@progress.com>
2022-02-09 13:28:36 +0530

    postchecklist
    Signed-off-by: shaik80 <smudassi@progress.com>

--- components/automate-deployment/pkg/majorupgradechecklist/checklist_inspector.go
+++ components/automate-deployment/pkg/majorupgradechecklist/checklist_inspector.go
@@ -3,18 +3,41 @@ package majorupgradechecklist
 import (
 	"github.com/chef/automate/components/automate-cli/pkg/status"
 	"github.com/chef/automate/components/automate-deployment/pkg/cli"
+	"github.com/chef/automate/components/automate-deployment/pkg/manifest"
 )
 
-type ChecklistInspector interface {
+type ChecklistManager interface {
 	RunChecklist() error
-	GetPostChecklists() ([]string, error)
+	CreateJsonFile() error
 }
 
-//NewChecklistInspector returns the checklist inspector for given major release
-func NewChecklistInspector(writer cli.FormatWriter, version, major string) (ChecklistInspector, error) {
+type PerCheckList struct {
+	Msg      string `json:"msg"`
+	Response string `json:"-"`
+}
+
+type PostCheckList struct {
+	Msg        string `json:"msg"`
+	Cmd        string `json:"cmd"`
+	IsExecuted bool   `json:"is_executor"`
+}
+
+type PerPostChecklist struct {
+	PostChecklist []PostCheckList `json:"post_checklist"`
+}
+
+//NewChecklistManager returns the checklist inspector for given major release
+
+func NewChecklistManager(writer cli.FormatWriter, version, major string) (ChecklistManager, error) {
+
+	if major == "" {
+		resp, _ := manifest.IsSemVersionFmt(version)
+		major = resp
+	}
+
 	switch major {
-	case "22":
-		return NewV22ChecklistInspector(writer, version), nil
+	case "2":
+		return NewV22ChecklistManager(writer, version), nil
 	default:
 		return nil, status.Errorf(status.UpgradeError, "invalid major version")
 	}
