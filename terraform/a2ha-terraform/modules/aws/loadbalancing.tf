07daf45edf66e7fa4c038bbc9754c50d1eadd72f
meet58 <mgadhiya@msystechnologies.com>
2022-02-17 19:04:56 +0530

    chore: revert AutomateHA chef-server load balancer changes
    Signed-off-by: meet58 <mgadhiya@msystechnologies.com>

--- terraform/a2ha-terraform/modules/aws/loadbalancing.tf
+++ terraform/a2ha-terraform/modules/aws/loadbalancing.tf
@@ -97,7 +97,7 @@ resource "aws_alb" "chef_server_lb" {
   name               = "${var.tag_name}-${random_id.random.hex}-chef-server-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.base_linux.id, aws_security_group.chef_automate.id]
+  security_groups    = [aws_security_group.load_balancer.id]
   subnets            = aws_subnet.public.*.id
   tags               = var.tags
 }
