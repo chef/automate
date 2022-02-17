f2a0ea606c65bc8765c569fbc62ec9e3c62f5ade
shaik80 <smudassi@progress.com>
2022-02-17 12:16:23 +0530

    old manifests url
    Signed-off-by: shaik80 <smudassi@progress.com>

--- components/automate-deployment/pkg/manifest/provider.go
+++ components/automate-deployment/pkg/manifest/provider.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	automateVersionsURLFmt = "http://ec2-3-108-10-133.ap-south-1.compute.amazonaws.com:8080/manifests/%s/automate/versions.json" // mock url
+	automateVersionsURLFmt = "https://packages.chef.io/manifests/%s/automate/versions.json"
 )
 
 // A ReleaseManifestProvider is something that can return a manifest
