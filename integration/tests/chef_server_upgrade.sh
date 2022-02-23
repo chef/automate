32a2df38adcf70a875e75c53abb1dce494c1c9a7
Vinay Satish <vinay.satish@progress.com>
2022-02-23 10:10:30 +0530

    fixing chef-server-ctl test issue
    Signed-off-by: Vinay Satish <vinay.satish@progress.com>

--- integration/tests/chef_server_upgrade.sh
+++ integration/tests/chef_server_upgrade.sh
@@ -24,7 +24,7 @@ do_deploy() {
 do_test_deploy() {
     ## skipping status test because of the missing file in automate - /etc/opscode/chef-server-running.json 
     ## adding smoke tag or else all the test will be considered skipping only the status test
-    PATH="/hab/bin:/bin" chef-server-ctl test --smoke --skip-status --skip=response_headers
+    PATH="/hab/bin:/bin" chef-server-ctl test
     test_chef_server_ctl
     test_knife
     do_test_deploy_default
@@ -33,7 +33,7 @@ do_test_deploy() {
 do_test_upgrade() {
     ## skipping status test because of the missing file in automate - /etc/opscode/chef-server-running.json 
     ## adding smoke tag or else all the test will be considered skipping only the status test
-    PATH="/hab/bin:/bin" chef-server-ctl test --smoke --skip-status --skip=response_headers
+    PATH="/hab/bin:/bin" chef-server-ctl test
     test_chef_server_ctl
     test_knife
     do_test_upgrade_default
