9b8adb80632f447b46656107311a5ad84aff3d6f
Sonali Wale <86949270+sonali523@users.noreply.github.com>
2022-03-02 14:14:51 +0530

    Storage functions for org users association (#6741)
    * Storage functions for org users association
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Minor changes added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>
    
    * Minor changes added
    
    Signed-off-by: sonali wale <sonali.wale@progress.com>

--- components/infra-proxy-service/storage/postgres/automate_infra_server_users.go
+++ components/infra-proxy-service/storage/postgres/automate_infra_server_users.go
@@ -183,3 +183,66 @@ func (p *postgres) getUsers(ctx context.Context, q querier, serverID string) ([]
 	return users, nil
 }
 
+// StoreOrgUserAssociation: Inserts an entry of org users
+func (p *postgres) StoreOrgUserAssociation(ctx context.Context, serverID, orgID, username string, isAdmin bool) (storage.OrgUser, error) {
+	return p.insertOrgUserAssociation(ctx, serverID, orgID, username, isAdmin)
+}
+
+func (p *postgres) insertOrgUserAssociation(ctx context.Context, serverID, orgID, username string, isAdmin bool) (storage.OrgUser, error) {
+	var orgUser storage.OrgUser
+	nowTime := time.Now()
+	err := p.db.QueryRowContext(ctx,
+		`INSERT INTO org_users (
+		org_id, user_id, 
+		is_admin, 
+		created_at,updated_at)
+		VALUES ($1, SELECT id from users where infra_server_username=$2 and server_id=$3, $4, $5, $5)
+		RETURNING id, org_id, user_id, is_admin, created_at, updated_at`,
+		orgID, username, serverID, isAdmin, nowTime).
+		Scan(&orgUser.ID, &orgUser.OrgId, &orgUser.UserID, &orgUser.IsAdmin, &orgUser.CreatedAt, &orgUser.UpdatedAt)
+	if err != nil {
+		return storage.OrgUser{}, p.processError(err)
+	}
+	return orgUser, nil
+}
+
+// EditOrgUserAssociation: Updates an entry of org users
+func (p *postgres) EditOrgUserAssociation(ctx context.Context, serverID, orgID, username string, isAdmin bool) (storage.OrgUser, error) {
+	return p.updateOrgUserAssociation(ctx, serverID, orgID, username, isAdmin)
+}
+
+func (p *postgres) updateOrgUserAssociation(ctx context.Context, serverID, orgID, username string, isAdmin bool) (storage.OrgUser, error) {
+	var orgUser storage.OrgUser
+	nowTime := time.Now()
+	err := p.db.QueryRowContext(ctx,
+		`UPDATE org_users SET 
+		is_admin = $1, 
+		updated_at= $2
+		WHERE org_id=$3 and user_id = SELECT id FROM users u WHERE u.infra_server_username=$4 and u.server_id=$5
+		RETURNING id, org_id, user_id, is_admin, created_at, updated_at`,
+		isAdmin, nowTime, orgID, username, serverID).
+		Scan(&orgUser.ID, &orgUser.OrgId, &orgUser.UserID, &orgUser.IsAdmin, &orgUser.CreatedAt, &orgUser.UpdatedAt)
+	if err != nil {
+		return storage.OrgUser{}, p.processError(err)
+	}
+	return orgUser, nil
+}
+
+// DeleteOrgUserAssociation: Deletes an entry from org users
+func (p *postgres) DeleteOrgUserAssociation(ctx context.Context, serverID, orgID, username string, isAdmin bool) (storage.OrgUser, error) {
+	return p.deleteOrgUserAssociation(ctx, serverID, orgID, username, isAdmin)
+}
+
+func (p *postgres) deleteOrgUserAssociation(ctx context.Context, serverID, orgID, username string, isAdmin bool) (storage.OrgUser, error) {
+	var orgUser storage.OrgUser
+	err := p.db.QueryRowContext(ctx,
+		`DELETE FROM org_users 
+		WHERE org_id=$1 and user_id = SELECT id FROM users u WHERE u.infra_server_username=$2 and u.server_id=$3
+		RETURNING id, org_id, user_id, is_admin, created_at, updated_at`,
+		orgID, username, serverID).
+		Scan(&orgUser.ID, &orgUser.OrgId, &orgUser.UserID, &orgUser.IsAdmin, &orgUser.CreatedAt, &orgUser.UpdatedAt)
+	if err != nil {
+		return storage.OrgUser{}, p.processError(err)
+	}
+	return orgUser, nil
+}
