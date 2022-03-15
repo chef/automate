d1c48c6744da3b773f9dd38940bc101b58cfd294
Vinay Satish <vinay.satish@progress.com>
2022-03-15 21:12:33 +0530

    Updated Chef Server version to 14.13.42 with cookbook version cache

--- components/automate-cs-oc-erchef/habitat/plan.sh
+++ components/automate-cs-oc-erchef/habitat/plan.sh
@@ -13,8 +13,9 @@ pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
 pkg_upstream_url="https://www.chef.io/automate"
 pkg_deps=(
-  core/runit/2.1.2/20200403133549
-  chef/mlsa/1.0.1/20200421170200
+  #core/runit/2.1.2/20200403133549
+  core/runit
+  chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
   "${vendor_origin}/oc_erchef/14.13.42/20220228221324"
