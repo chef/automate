e6457569c7e3828716ce662ee425c2d7112d7eb3
Vinay Sharma <vsharma@chef.io>
2022-03-22 14:50:38 +0530

    added changes for the test cases
    Signed-off-by: Vinay Sharma <vsharma@chef.io>

--- components/infra-proxy-service/migrations/migrations_test.go
+++ components/infra-proxy-service/migrations/migrations_test.go
@@ -14,6 +14,38 @@ import (
 	"github.com/chef/automate/components/infra-proxy-service/storage/testDB"
 )
 
+func TestGetMigrationStatus(t *testing.T) {
+	type args struct {
+		ctx       context.Context
+		server    MigrationServer
+		req       *request.GetMigrationStatusRequest
+		NeedError bool
+	}
+
+	tests := []struct {
+		name      string
+		args      args
+		wantError error
+		want1     *response.GetMigrationStatusResponse
+	}{
+		{name: "Test Get migration status, should return the latest status of migration id", args: args{ctx: context.Background(), server: MigrationServer{service: &service.Service{Migration: &testDB.MigrationDB{}}}, req: &request.GetMigrationStatusRequest{MigrationId: "mig1"}}, wantError: nil, want1: &response.GetMigrationStatusResponse{MigrationId: "mig1", MigrationType: "Creating Preview", MigrationStatus: "Completed"}},
+		{name: "Test Error from Get migration status, if status is not present for migration id", args: args{ctx: context.Background(), server: MigrationServer{service: &service.Service{Migration: &testDB.MigrationDB{}}}, req: &request.GetMigrationStatusRequest{MigrationId: "mig1"}}, wantError: errors.New("Failed to fetch migration status"), want1: &response.GetMigrationStatusResponse{MigrationId: "mig1", MigrationType: "Creating Preview", MigrationStatus: "Completed"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := tt.args.server.GetMigrationStatus(tt.args.ctx, tt.args.req)
+			if err != nil && err.Error() != tt.wantError.Error() {
+				t.Errorf("GetMigrationStatus() err = %v, want %v", err, tt.wantError)
+			}
+			if !reflect.DeepEqual(got, tt.want1) {
+				t.Errorf("GetMigrationStatus() got = %v, want %v", got, tt.want1)
+			}
+		})
+	}
+}
+
 func TestConfirmPreview(t *testing.T) {
 	type args struct {
 		ctx       context.Context
