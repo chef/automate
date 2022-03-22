6cfa4a8e120a600355761814e44e19a1792b7cfd
vinay sharma <vsharma@chef.io>
2022-03-22 17:31:15 +0530

    Migrate users to DB After send the changed and selected user details from UI. (#6819)
    * added changes to migrate users into DB after selecting from UI
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>
    
    * added changes for the test cases
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>
    
    * added some minor changes
    
    Signed-off-by: Vinay Sharma <vsharma@chef.io>

--- components/infra-proxy-service/migrations/migrations_test.go
+++ components/infra-proxy-service/migrations/migrations_test.go
@@ -42,8 +42,38 @@ func TestGetMigrationStatus(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want1) {
 				t.Errorf("GetMigrationStatus() got = %v, want %v", got, tt.want1)
 			}
-
 		})
+	}
+}
 
+func TestConfirmPreview(t *testing.T) {
+	type args struct {
+		ctx       context.Context
+		server    MigrationServer
+		req       *request.ConfirmPreview
+		NeedError bool
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantError error
+		want1     *response.ConfirmPreview
+	}{
+		{name: "Test confrim preview, should return migration id", args: args{ctx: context.Background(), server: MigrationServer{service: &service.Service{Migration: &testDB.MigrationDB{}}}, req: &request.ConfirmPreview{ServerId: "server1", MigrationId: "mig1", StagedData: &request.StagedData{}}}, wantError: nil, want1: &response.ConfirmPreview{MigrationId: "mig1"}},
+
+		{name: "Test Error from confirm preview, if migration is failed", args: args{ctx: context.Background(), server: MigrationServer{service: &service.Service{Migration: &testDB.MigrationDB{NeedError: true}}}, req: &request.ConfirmPreview{ServerId: "server1", MigrationId: "mig1"}}, wantError: errors.New("Failed to migrate."), want1: nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := tt.args.server.ConfirmPreview(tt.args.ctx, tt.args.req)
+			if err != nil && err.Error() != tt.wantError.Error() {
+				t.Errorf("ConfirmPreview() err = %v, want %v", err, tt.wantError)
+			}
+			if !reflect.DeepEqual(got, tt.want1) {
+				t.Errorf("ConfirmPreview() got = %v, want %v", got, tt.want1)
+			}
+		})
 	}
 }
