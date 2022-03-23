3a1e5edbf80c5e807cef0b8f51e324cd423c104b
Chaitali Mane <71449322+chaitali-mane@users.noreply.github.com>
2022-03-23 15:09:28 +0530

    Added org user API integration (#6817)
    * Added APi integration
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Added code integration
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>
    
    * Added cypress test case
    
    Signed-off-by: Chaitali Mane <cmane@progress.com>

---
+++ components/automate-ui/src/app/entities/org-users/org-users.effects.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of as observableOf } from 'rxjs';
+import { catchError, mergeMap, map } from 'rxjs/operators';
+import { CreateNotification } from 'app/entities/notifications/notification.actions';
+import { Type } from 'app/entities/notifications/notification.model';
+
+import {
+  GetUsers,
+  GetUsersSuccess,
+  GetUsersFailure,
+  UsersSuccessPayload,
+  OrgUsersActionTypes
+} from './org-users.action';
+
+import { OrgUserRequests } from './org-users.requests';
+
+@Injectable()
+export class OrgUserEffects {
+  constructor(
+    private actions$: Actions,
+    private requests: OrgUserRequests
+  ) { }
+
+  getUsers$ = createEffect(() =>
+    this.actions$.pipe(
+    ofType(OrgUsersActionTypes.GET_ALL),
+    mergeMap(({ payload: { server_id, org_id } }: GetUsers) =>
+    this.requests.OrgUserRequests(server_id, org_id).pipe(
+      map((resp: UsersSuccessPayload) => new GetUsersSuccess(resp)),
+      catchError((error: HttpErrorResponse) =>
+      observableOf(new GetUsersFailure(error)))
+    ))));
+
+  getUsersFailure$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(OrgUsersActionTypes.GET_ALL_FAILURE),
+      map(({ payload }: GetUsersFailure) => {
+        const msg = payload.error.error;
+        return new CreateNotification({
+          type: Type.error,
+          message: `Could not get clients: ${msg || payload.error}`
+        });
+    })));
+}
