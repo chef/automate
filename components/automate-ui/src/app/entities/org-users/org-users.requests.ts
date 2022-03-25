a6493d309bc3cf821475ec9f9afc2dd5bef4f95a
Chaitali Mane <cmane@progress.com>
2022-03-25 19:40:57 +0530

    Added reset key modal code
    Signed-off-by: Chaitali Mane <cmane@progress.com>

--- components/automate-ui/src/app/entities/org-users/org-users.requests.ts
+++ components/automate-ui/src/app/entities/org-users/org-users.requests.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment as env } from 'environments/environment';
-import { UsersSuccessPayload } from './org-users.action';
+import { UsersSuccessPayload, ResetKeySuccessPayload } from './org-users.action';
 
 @Injectable()
 export class OrgUserRequests {
@@ -14,4 +14,11 @@ export class OrgUserRequests {
     const url = `${env.infra_proxy_url}/servers/${server_id}/org/${org_id}/users`;
     return this.http.get<UsersSuccessPayload>(url);
   }
+
+  public resetUserKeyRequests(server_id: string, org_id: string, name: string)
+  : Observable<ResetKeySuccessPayload> {
+    const url = `${env.infra_proxy_url}/servers/${server_id}/org/${org_id}/users/${name}`;
+    return this.http.get<ResetKeySuccessPayload>(url);
+  }
+  
 }
