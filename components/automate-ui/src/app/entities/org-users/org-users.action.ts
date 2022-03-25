56a973359bc049e4507430d3be1f7b668f00f963
Chaitali Mane <cmane@progress.com>
2022-03-25 19:40:57 +0530

    Added reset key modal code
    Signed-off-by: Chaitali Mane <cmane@progress.com>

--- components/automate-ui/src/app/entities/org-users/org-users.action.ts
+++ components/automate-ui/src/app/entities/org-users/org-users.action.ts
@@ -3,9 +3,12 @@ import { Action } from '@ngrx/store';
 import { OrgUser } from './org-users.model';
 
 export enum OrgUsersActionTypes {
-  GET_ALL         = 'USERS::GET_ALL',
-  GET_ALL_SUCCESS = 'USERS::GET_ALL::SUCCESS',
-  GET_ALL_FAILURE = 'USERS::GET_ALL::FAILURE'
+  GET_ALL           = 'USERS::GET_ALL',
+  GET_ALL_SUCCESS   = 'USERS::GET_ALL::SUCCESS',
+  GET_ALL_FAILURE   = 'USERS::GET_ALL::FAILURE',
+  RESETKEY          = 'USERS::RESETKEY',
+  RESETKEY_SUCCESS  = 'USERS::RESETKEY::SUCCESS',
+  RESETKEY_FAILURE  = 'USERS::RESETKEY::FAILURE'
 }
 
 export interface UsersSuccessPayload {
@@ -32,7 +35,35 @@ export class GetUsersFailure implements Action {
   constructor(public payload: HttpErrorResponse) { }
 }
 
+export class ResetUserKey implements Action {
+  readonly type = OrgUsersActionTypes.RESETKEY;
+  constructor(public payload: { server_id: string, org_id: string, name: string }) { }
+}
+
+export interface ResetKeySuccessPayload {
+  name: string;
+  key: {
+    name: string,
+    public_key: string,
+    expiration_date: string,
+    private_key: string
+  };
+}
+
+export class ResetUserKeySuccess implements Action {
+  readonly type = OrgUsersActionTypes.RESETKEY_SUCCESS;
+  constructor(public payload: ResetKeySuccessPayload ) { }
+}
+
+export class ResetUserKeyFailure implements Action {
+  readonly type = OrgUsersActionTypes.RESETKEY_FAILURE;
+  constructor(public payload: HttpErrorResponse) { }
+}
+
 export type OrgUsersActions =
   | GetUsers
   | GetUsersSuccess
-  | GetUsersFailure;
+  | GetUsersFailure
+  | ResetUserKey
+  | ResetUserKeySuccess
+  | ResetUserKeyFailure;
---
+++ components/automate-ui/src/app/modules/infra-proxy/reset-user-key/reset-user-key.component.ts
@@ -0,0 +1,116 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { takeUntil } from 'rxjs/operators';
+import { NgrxStateAtom } from 'app/ngrx.reducers';
+import { combineLatest, Subject } from 'rxjs';
+import { ResetUserKey } from 'app/entities/org-users/org-users.action';
+import { Utilities } from 'app/helpers/utilities/utilities';
+import { saveAs } from 'file-saver';
+import { ResetKey } from 'app/entities/clients/client.model';
+import { isNil } from 'lodash';
+import { EntityStatus } from 'app/entities/entities';
+import { resetError, resetStatus, resetUserKey } from 'app/entities/org-users/org-users.selectors';
+
+@Component({
+  selector: 'app-reset-user-key',
+  templateUrl: './reset-user-key.component.html',
+  styleUrls: ['./reset-user-key.component.scss']
+})
+
+export class ResetUserKeyComponent implements OnInit, OnDestroy {
+  @Input() openEvent: EventEmitter<boolean>;
+  @Input() serverId: string;
+  @Input() orgId: string;
+  @Input() name: string;
+
+  public isReset = false;
+  public resetting = false;
+  public error: string;
+  public publicKey: string;
+  public privateKey: string;
+  public org: string;
+  public server: string;
+  public visible = false;
+  public close = new EventEmitter();
+  private isDestroyed = new Subject<boolean>();
+
+  constructor(
+    private store: Store<NgrxStateAtom>
+  ) { }
+
+  ngOnInit(): void {
+    this.openEvent.pipe(takeUntil(this.isDestroyed))
+      .subscribe(() => {
+        this.visible = true;
+        this.resetting = false;
+        this.isReset = false;
+        this.server = this.serverId;
+        this.org = this.orgId;
+        this.name = this.name;
+        this.error = '';
+        this.privateKey = '';
+      });
+    
+      combineLatest([
+        this.store.select(resetStatus),
+        this.store.select(resetUserKey),
+        this.store.select(resetError)
+      ]).pipe(
+        takeUntil(this.isDestroyed))
+        .subscribe(([resetStatusSt, resetKeyState, errorSt]) => {
+          if (resetStatusSt === EntityStatus.loadingSuccess &&
+          !isNil(resetKeyState)) {
+              this.resetting = false;
+              this.isReset = true;
+              this.privateKey = resetKeyState?.user_key;
+          } else if (resetStatusSt === EntityStatus.loadingFailure) {
+              this.error = errorSt;
+              this.closeCreateModal();
+          }
+        });
+  }
+
+  ngOnDestroy(): void {
+    this.isDestroyed.next(true);
+    this.isDestroyed.complete();
+  }
+
+  handleInput(event: KeyboardEvent): void {
+    if (Utilities.isNavigationKey(event)) {
+      return;
+    }
+  }
+
+  closeCreateModal(): void {
+    this.resetCreateModal();
+    this.visible = false;
+  }
+
+  private resetCreateModal(): void {
+    this.resetting = false;
+    this.isReset = false;
+    this.error = '';
+    this.privateKey = '';
+  }
+
+  resetKeyUser(): void {
+    this.resetting = true;
+    const payload: ResetKey = {
+      'org_id': this.orgId,
+      'server_id': this.serverId,
+      'name': this.name
+    };
+    this.store.dispatch(new ResetUserKey(payload));
+  }
+
+  downloadKey(): void {
+    const template = `
+    Private RSA Key
+
+    ${this.privateKey}
+    `;
+
+    const blob = new Blob([template], { type: 'text/plain;charset=utf-8' });
+    saveAs(blob, this.name + '.pem');
+  }
+}
