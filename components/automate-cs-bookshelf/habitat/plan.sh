977b804e2b724bbe56ddc49e34cef1d04686b7f2
Vinay Satish <vinay.satish@progress.com>
2022-03-25 15:25:51 +0530

    Updating the sys config of erchef to have external opensearch changes
    Signed-off-by: Vinay Satish <vinay.satish@progress.com>

--- components/automate-cs-bookshelf/habitat/plan.sh
+++ components/automate-cs-bookshelf/habitat/plan.sh
@@ -7,7 +7,7 @@ pkg_name="automate-cs-bookshelf"
 pkg_description="Wrapper package for chef/bookshelf"
 pkg_origin="chef"
 # WARNING: Version managed by .expeditor/update_chef_server.sh
-pkg_version="14.13.42"
+pkg_version="14.13.64"
 vendor_origin="chef"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
@@ -16,7 +16,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/bookshelf/14.13.42/20220228221203"
+  "${vendor_origin}/bookshelf/14.14.1/20220405043525"
 )
 
 pkg_binds=(
