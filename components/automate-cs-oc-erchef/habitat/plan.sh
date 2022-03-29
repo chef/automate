74acb700205e552a57275bddcf1cd2b09b022e41
shanmugapriya-tr <shanmugapriya.tiruchengoderamanathan@progress.com>
2022-03-29 18:58:56 +0530

    Revert "Updating the chef-server to the latest version & CBV configs (#6726)"
    This reverts commit 3987d8dd5f1281d2beb354147660a4de020b86ea.

--- components/automate-cs-oc-erchef/habitat/plan.sh
+++ components/automate-cs-oc-erchef/habitat/plan.sh
@@ -7,18 +7,17 @@ pkg_name="automate-cs-oc-erchef"
 pkg_description="Wrapper package for chef/oc_erchef"
 pkg_origin="chef"
 # WARNING: Version managed by .expeditor/update_chef_server.sh
-pkg_version="14.13.42"
+pkg_version="14.11.36"
 vendor_origin="chef"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
 pkg_upstream_url="https://www.chef.io/automate"
 pkg_deps=(
-  #core/runit/2.1.2/20200403133549
-  core/runit
-  chef/mlsa
+  core/runit/2.1.2/20200403133549
+  chef/mlsa/1.0.1/20200421170200
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/oc_erchef/14.13.42/20220228221324"
+  "${vendor_origin}/oc_erchef/14.11.36/20211227114250"
 )
 
 pkg_build_deps=(
