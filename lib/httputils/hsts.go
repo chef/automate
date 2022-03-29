7a12e3bd9edd36ec902009c04048774f7f239243
Atul Krishna <Atul.Krishna@progress.com>
2022-03-29 15:07:22 +0530

    Created HSTS util and added to session-service
    Signed-off-by: Atul Krishna <Atul.Krishna@progress.com>

---
+++ lib/httputils/hsts.go
@@ -0,0 +1,88 @@
+package httputils
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type HSTSHandler struct {
+	// MaxAge sets the duration that the HSTS is valid for.
+	MaxAge time.Duration
+	// HostOverride provides a host to the redirection URL in the case that the system is behind a load balancer
+	// which doesn't provide the X-Forwarded-Host HTTP header (e.g. an Amazon ELB).
+	HostOverride string
+	// Decides whether to accept the X-Forwarded-Proto header as proof of SSL.
+	AcceptXForwardedProtoHeader bool
+	// SendPreloadDirective sets whether the preload directive should be set. The directive allows browsers to
+	// confirm that the site should be added to a preload list. (see https://hstspreload.appspot.com/)
+	SendPreloadDirective bool
+}
+
+func isHTTPS(r *http.Request, acceptXForwardedProtoHeader bool) bool {
+	// Added by common load balancers which do TLS offloading.
+	if acceptXForwardedProtoHeader && r.Header.Get("X-Forwarded-Proto") == "https" {
+		return true
+	}
+	// If the X-Forwarded-Proto was set upstream as HTTP, then the request came in without TLS.
+	if acceptXForwardedProtoHeader && r.Header.Get("X-Forwarded-Proto") == "http" {
+		return false
+	}
+	// Set by some middleware.
+	if r.URL.Scheme == "https" {
+		return true
+	}
+	// Set when the Go server is running HTTPS itself.
+	if r.TLS != nil && r.TLS.HandshakeComplete {
+		return true
+	}
+	return false
+}
+
+func createHeaderValue(maxAge time.Duration, sendPreloadDirective bool) string {
+	builder := strings.Builder{}
+	builder.WriteString("max-age=")
+	builder.WriteString(strconv.Itoa(int(maxAge.Seconds())))
+	builder.WriteString("; includeSubDomains")
+	if sendPreloadDirective {
+		builder.WriteString("; preload")
+	}
+	return builder.String()
+}
+
+func initHandler() *HSTSHandler {
+	return &HSTSHandler{
+		MaxAge:                      time.Hour * 24 * 730, //2 years; ie. 730 days; ie. 63072000 seconds
+		AcceptXForwardedProtoHeader: true,
+		SendPreloadDirective:        false,
+	}
+}
+
+//Add HSTS response header for https calls and redirect if call is over http
+func AddHSTSHeaderAndRedirect(next http.Handler) http.Handler {
+	h := initHandler()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if isHTTPS(r, h.AcceptXForwardedProtoHeader) {
+			w.Header().Add("Strict-Transport-Security", createHeaderValue(h.MaxAge, h.SendPreloadDirective))
+			next.ServeHTTP(w, r)
+		} else {
+			if h.HostOverride != "" {
+				r.URL.Host = h.HostOverride
+			} else if !r.URL.IsAbs() {
+				r.URL.Host = r.Host
+			}
+			r.URL.Scheme = "https"
+			http.Redirect(w, r, r.URL.String(), http.StatusMovedPermanently)
+		}
+	})
+}
+
+//Only add HSTS response header for https calls and doesn't redirect if call is over http
+func AddHSTSHeader(next http.Handler) http.Handler {
+	h := initHandler()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Strict-Transport-Security", createHeaderValue(h.MaxAge, h.SendPreloadDirective))
+		next.ServeHTTP(w, r)
+	})
+}
