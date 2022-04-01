8510852532a11e89dd2d633580d40cda5779ee88
sandhi <sagarwal@progress.com>
2022-04-01 10:40:03 +0530

    Added the changes for setting up Opensearch 1. Created new hab package for Opensearch in HA 2. Added a new terraform module to instal the package
    Signed-off-by: sandhi <sagarwal@progress.com>

---
+++ terraform/a2ha-terraform/modules/opensearch/inputs.tf
@@ -0,0 +1,74 @@
+variable "airgap_info" {
+}
+
+variable "backend_aib_dest_file" {
+}
+
+variable "backend_aib_local_file" {
+}
+
+variable "curator_pkg_ident" {
+}
+
+variable "opensearch_instance_count" {
+  default = 1
+}
+
+variable "opensearch_listen_port" {
+  default = 9200
+}
+
+variable "opensearch_pkg_ident" {
+  default = "chef/automate-ha-opensearch"
+}
+
+variable "opensearch_svc_load_args" {
+  default = "--topology standalone --strategy none"
+}
+
+variable "opensearchsidecar_pkg_ident" {
+}
+
+variable "opensearchsidecar_svc_load_args" {
+}
+
+variable "habitat_info" {
+}
+
+variable "journalbeat_pkg_ident" {
+  default = "chef/automate-ha-journalbeat"
+}
+
+variable "kibana_pkg_ident" {
+  default = "chef/automate-ha-kibana"
+}
+
+variable "metricbeat_pkg_ident" {
+  default = "chef/automate-ha-metricbeat"
+}
+
+variable "private_ips" {
+  default = []
+}
+
+variable "public_ips" {
+  default = []
+}
+
+variable "ssh_key_file" {
+}
+
+variable "ssh_user" {
+  default = "ubuntu"
+}
+
+variable "ssh_user_sudo_password" {
+}
+
+variable "sudo_cmd" {
+  default = "sudo"
+}
+
+variable "tmp_path" {
+  default = "/var/tmp"
+}
---
+++ terraform/a2ha-terraform/modules/opensearch/templates/opensearchsidecar_user.toml.tpl
@@ -0,0 +1,1 @@
+opensearch_ip = "${private_ip}"
