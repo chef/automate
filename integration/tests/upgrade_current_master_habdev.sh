99571f64d379b5718323789fd331cb8c12456359
Vivek Shankar <vshankar@progress.com>
2022-04-01 14:51:45 +0530

    creating versions.json properly
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- integration/tests/upgrade_current_master_habdev.sh
+++ integration/tests/upgrade_current_master_habdev.sh
@@ -16,8 +16,8 @@ EOF
 }
 
 do_prepare_upgrade() {
-    do_prepare_upgrade_default
     set_version_file
+    do_prepare_upgrade_default
     set_test_manifest "build-habdev.json"
     append_version_file
 }
