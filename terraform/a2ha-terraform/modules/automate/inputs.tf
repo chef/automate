b1f9446e84abe5abf205c97814be13040102fc4b
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2022-04-04 11:03:53 +0530

    sorting tf variables
    Signed-off-by: Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>

--- terraform/a2ha-terraform/modules/automate/inputs.tf
+++ terraform/a2ha-terraform/modules/automate/inputs.tf
@@ -38,6 +38,18 @@ variable "backend_aib_dest_file" {
 variable "backend_aib_local_file" {
 }
 
+variable "backup_config_efs" {
+  default = "false"
+}
+
+variable "backup_config_s3" {
+  default = "false"
+}
+
+variable "bucket_name" {
+  default = "chef-automate-ha"
+}
+
 variable "cluster_id" {
   default = ""
 }
@@ -118,6 +130,10 @@ variable "proxy_listen_port" {
 
 variable "public_ips" {
   default = []
+}
+
+variable "s3_endpoint" {
+  default = "https://s3.amazonaws.com"
 }
 
 variable "ssh_key_file" {
