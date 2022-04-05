602a382c2f3d096695c4dcfeaadb58c70035a9fe
Vivek Shankar <vshankar@progress.com>
2022-04-05 12:14:02 +0530

    mv version instead of manifest
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- scripts/verify_build.sh
+++ scripts/verify_build.sh
@@ -92,11 +92,11 @@ log_section_start "create manifest"
 .expeditor/create-manifest.rb
 echo "showing current dir files"
 ls
-mv manifest.json results/build.json
+mv "$VERSION".json results/build.json
 
 log_section_start "create manifest (latest hab)"
 HAB_PKG_CHANNEL=unstable NO_PIN_HAB=true .expeditor/create-manifest.rb
-mv manifest.json results/build-habdev.json
+mv "$VERSION".json results/build-habdev.json
 
 log_section_start "create buildkite artifact"
 # The integration test framework uses this file to decide whether or
