e52ae4cd8794d6b0474dd0f790f045194ce704b0
Vivek Shankar <vshankar@progress.com>
2022-04-06 17:41:09 +0530

    set versions file to timestamp version
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- integration/tests/ha_data_services_migrate.sh
+++ integration/tests/ha_data_services_migrate.sh
@@ -6,7 +6,7 @@ test_external_services=(ha_backend)
 test_diagnostics_filters="~purge"
 test_upgrades=true
 
-CURRENT_OLDEST_VERSION=20220329091442
+CURRENT_OLDEST_VERSION=20190501153509
 OLD_MANIFEST_DIR="${A2_ROOT_DIR}/components/automate-deployment/testdata/old_manifests/"
 DEEP_UPGRADE_PATH="${OLD_MANIFEST_DIR}/${CURRENT_OLDEST_VERSION}.json"
 
@@ -31,6 +31,10 @@ do_build() {
     log_info "Creating update airgap bundle"
 
     set_version_file
+
+    download_manifest_version "current" "20220329091442" "$test_manifest_dir/20220329091442.json"
+    set_test_manifest "20220329091442.json"
+    append_version_file
 
     #shellcheck disable=SC2154
     chef-automate airgap bundle create \
