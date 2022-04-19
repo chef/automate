27ce933c0cbd7dee10f6948342fce0e2e888c4cc
FaizanSRE <ffulara@msystechnologies.com>
2022-04-19 19:28:29 +0530

    dependencies changes
    Signed-off-by: FaizanSRE <ffulara@msystechnologies.com>

---
+++ terraform/a2ha-terraform/dependencies.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+   aws = {
+      source  = "hashicorp/aws"
+    }
+   null = {
+      source  = "hashicorp/null"
+    }
+   random = {
+      source = "hashicorp/random"
+   }
+   local = {
+      source = "hashicorp/local"
+   }
+
+  }
+}
