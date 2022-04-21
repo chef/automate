7b201b14d81b863abb22262b5c0b7fb939d7cf6e
Vivek Yadav <vivek.yadav@progress.com>
2022-04-21 18:30:11 +0530

    announcement and release notes changes (#6943)
    * announcement and release notes changes
    
    Signed-off-by: Vivek Yadav <vivek.yadav@progress.com>
    
    * fix semver changes in release management dashboard
    
    Signed-off-by: Vivek Yadav <vivek.yadav@progress.com>
    
    * fixed Announce post for discourse
    
    Signed-off-by: Vivek Yadav <vivek.yadav@progress.com>
    
    * latest.json is changed to latest_semver.json
    
    Signed-off-by: Vivek Yadav <vivek.yadav@progress.com>

--- .expeditor/publish-release-notes.sh
+++ .expeditor/publish-release-notes.sh
@@ -27,7 +27,7 @@ Chef lets you choose your **upgrade journey** based on your current version of C
 | Any version before 20220329091442| 20220329091442|
 | 20220329091442| 3.0.x|
 
-Click [here](/automate/major_upgrade/) to know more.
+Click [here](https://docs.chef.io/automate/major_upgrade/) to know more.
 
 ## Maintenance
 - 
