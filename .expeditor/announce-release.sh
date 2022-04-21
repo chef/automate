131ef4253efb831dcc21b55a56b1350c5d7276e8
Vivek Yadav <vivek.yadav@progress.com>
2022-04-21 16:41:27 +0530

    fixed Announce post for discourse
    Signed-off-by: Vivek Yadav <vivek.yadav@progress.com>

--- .expeditor/announce-release.sh
+++ .expeditor/announce-release.sh
@@ -9,7 +9,7 @@ build_version=$(jq -r -c ".version"  manifest.json)
 # Download the release-notes for our specific build
 curl -o release-notes.md "https://packages.chef.io/release-notes/automate/${build_version}.md"
 
-topic_title="Automate 2 version $build_version Released!"
+topic_title="Automate version $build_version Released!"
 topic_body=$(cat <<EOH
 We are delighted to announce the availability of version $build_version of Chef Automate.
 
