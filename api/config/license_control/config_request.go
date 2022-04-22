0f469d4549e790fcf0818f449ad7e6f4c42b8fc5
Kallol Roy <karoy@progress.com>
2022-04-22 23:35:59 +0530

    Update chef telemetry host name
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- api/config/license_control/config_request.go
+++ api/config/license_control/config_request.go
@@ -28,7 +28,7 @@ func DefaultConfigRequest() *ConfigRequest {
 	c.V1.Sys.Service.Port = w.Int32(10124)
 	// do not default the OptOut field because it is need to be blank for migration
 	// c.V1.Sys.Telemetry.OptOut = w.Bool(true)
-	c.V1.Sys.Telemetry.Url = w.String("https://telemetry.chef.io")
+	c.V1.Sys.Telemetry.Url = w.String("https://telemetry.chef.co")
 	c.V1.Sys.Log.Level = w.String("info")
 	c.V1.Sys.Log.Format = w.String("text")
 	return c
