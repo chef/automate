0b2bafc1f96065629da7d70ccf521661ad5301cb
Sahiba31 <sahibagoyal1999@gmail.com>
2022-04-25 17:58:05 +0530

    Creating airgap bundle
    Signed-off-by: Sahiba Goyal <sgoyal@progress.com>

--- .expeditor/create-airgap-bundle.sh
+++ .expeditor/create-airgap-bundle.sh
@@ -3,3 +3,11 @@
 curl https://packages.chef.io/files/current/latest/chef-automate-cli/chef-automate_linux_amd64.zip | gunzip - > chef-automate && chmod +x chef-automate
 
 ./chef-automate airgap bundle create -c dev
+
+#airgapbundle=$(ls | grep automate-[0-9.]*aib) 
+
+#aws s3 cp $airgapbundle "s3://sahiba-bucket1" --acl public-read --profile chef-cd
+
+gpg --armor --digest-algo sha256 --default-key 2940ABA983EF826A --output "automate-$VERSION.asc" --detach-sign "automate-$VERSION.aib"
+sha256sum "automate-$VERSION.aib" > "automate-$VERSION.sha256sum"
+aws s3 cp automate-$VERSION.aib "s3://chef-automate-artifacts/airgap_bundle/$VERSION/automate-$VERSION.aib" --acl public-read --profile chef-cd
