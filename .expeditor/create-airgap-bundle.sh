7be476bae0ecda2ac25b20ca24a8f2726206a245
Kallol Roy <karoy@progress.com>
2022-05-01 23:34:37 +0530

    Changes to test
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- .expeditor/create-airgap-bundle.sh
+++ .expeditor/create-airgap-bundle.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-curl https://packages.chef.io/files/current/latest/chef-automate-cli/chef-automate_linux_amd64.zip | gunzip - > chef-automate && chmod +x chef-automate
+set -eou pipefail
 
-./chef-automate airgap bundle create -c dev
+EXPEDITOR_TARGET_CHANNEL="acceptance"
+if [ "${EXPEDITOR_TARGET_CHANNEL}" = "unstable" ];
+then
+  echo "This file does not support actions for artifacts promoted to unstable"
+  exit 1
+fi
 
-airgapbundle=`ls | grep automate-[0-9.]*aib` 
-filename=$(echo $airgapbundle |  awk -F ".aib" '{print $1}')
-gpg --armor --digest-algo sha256 --default-key 2940ABA983EF826A --output "$filename.asc" --detach-sign $airgapbundle
-sha256sum $airgapbundle > "$filename.sha256sum"
-aws s3 cp $airgapbundle "s3://chef-automate-artifacts/airgap_bundle/$VERSION/$airgapbundle" --acl public-read --profile chef-cd
+aws s3 cp "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/automate/manifest_semver.json" manifest.json --profile chef-cd
+
+# Pull the version from the manifest
+version=$(jq -r -c ".version" manifest.json)
+
+# Get the current cli from the channel
+curl https://packages.chef.io/files/"${EXPEDITOR_TARGET_CHANNEL}"/latest/chef-automate-cli/chef-automate_linux_amd64.zip | gunzip - > chef-automate && chmod +x chef-automate
+
+./chef-automate airgap bundle create -c "${EXPEDITOR_TARGET_CHANNEL}" ./"${version}".aib
+
+gpg --armor --digest-algo sha256 --default-key 2940ABA983EF826A --output "$airgapbundle.asc" --detach-sign ./"${version}".aib
+sha256sum "$airgapbundle" > "${version}".aib.sha256sum
+
+aws s3 cp "${version}".aib "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/latest.aib" --acl public-read --profile chef-cd
+aws s3 cp "${version}".aib.asc "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/latest.aib.asc" --acl public-read --profile chef-cd
+aws s3 cp "${version}".aib.sha256sum "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/latest.aib.sha255sum" --acl public-read --profile chef-cd
+aws s3 cp "${version}".aib "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/${version}/${version}.aib" --acl public-read --profile chef-cd
+aws s3 cp "${version}".aib.asc "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/${version}/${version}.aib.asc" --acl public-read --profile chef-cd
+aws s3 cp "${version}".aib.sha256sum "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/${version}/${version}.aib.sha255sum" --acl public-read --profile chef-cd
