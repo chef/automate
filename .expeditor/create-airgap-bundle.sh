9d8373ed5d0a74d2bc9beb4114d50b656fdfa395
Kallol Roy <karoy@progress.com>
2022-05-01 23:53:03 +0530

    Fix script
    Signed-off-by: Kallol Roy <karoy@progress.com>

--- .expeditor/create-airgap-bundle.sh
+++ .expeditor/create-airgap-bundle.sh
@@ -19,8 +19,8 @@ curl https://packages.chef.io/files/"${EXPEDITOR_TARGET_CHANNEL}"/latest/chef-au
 
 ./chef-automate airgap bundle create -c "${EXPEDITOR_TARGET_CHANNEL}" ./"${version}".aib
 
-gpg --armor --digest-algo sha256 --default-key 2940ABA983EF826A --output "$airgapbundle.asc" --detach-sign ./"${version}".aib
-sha256sum "$airgapbundle" > "${version}".aib.sha256sum
+gpg --armor --digest-algo sha256 --default-key 2940ABA983EF826A --output "${version}".aib.asc --detach-sign ./"${version}".aib
+sha256sum "${version}".aib > "${version}".aib.sha256sum
 
 aws s3 cp "${version}".aib "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/latest.aib" --acl public-read --profile chef-cd
 aws s3 cp "${version}".aib.asc "s3://chef-automate-artifacts/${EXPEDITOR_TARGET_CHANNEL}/latest/latest.aib.asc" --acl public-read --profile chef-cd
