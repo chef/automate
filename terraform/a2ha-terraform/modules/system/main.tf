cbeb6ccd00610666a924851511b68915e9b30a6f
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2022-05-03 00:08:14 +0530

    Minor bug fixes
    Signed-off-by: Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>

--- terraform/a2ha-terraform/modules/system/main.tf
+++ terraform/a2ha-terraform/modules/system/main.tf
@@ -1,3 +1,13 @@
+locals {
+  tunables_sh = templatefile("${path.module}/templates/tunables.sh.tpl", {
+    tmp_path = var.tmp_path
+  })
+  archive_disk_info = templatefile("${path.module}/templates/archive_disk.info.tpl", {
+    automate_archive_disk_fs_path   = var.automate_archive_disk_fs_path,
+    opensearch_archive_disk_fs_path = var.opensearch_archive_disk_fs_path,
+    postgresql_archive_disk_fs_path = var.postgresql_archive_disk_fs_path
+  })
+}
 resource "null_resource" "create_temp_path" {
 
   count = var.instance_count
@@ -11,17 +21,6 @@ resource "null_resource" "create_temp_path" {
   provisioner "remote-exec" {
     inline = [ "sudo mkdir -p ${var.tmp_path}", "sudo chown -R ${var.ssh_user}:${var.ssh_user} ${var.tmp_path}" ]
    }
-}
-
-locals {
-  tunables_sh = templatefile("${path.module}/templates/tunables.sh.tpl", {
-    tmp_path = var.tmp_path
-  })
-  archive_disk_info = templatefile("${path.module}/templates/archive_disk.info.tpl", {
-    automate_archive_disk_fs_path   = var.automate_archive_disk_fs_path,
-    opensearch_archive_disk_fs_path = var.opensearch_archive_disk_fs_path,
-    postgresql_archive_disk_fs_path = var.postgresql_archive_disk_fs_path
-  })
 }
 
 resource "null_resource" "system_base_provisioning" {
