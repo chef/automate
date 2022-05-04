8837addb3086ebe04b35a9cee6df0a367140cf44
Vaibhav Gadre <vaibhavukandrao.gadre@progress.com>
2022-05-04 12:27:59 +0530

    updating chef server version to 14.15.8

--- components/automate-cs-oc-erchef/habitat/plan.sh
+++ components/automate-cs-oc-erchef/habitat/plan.sh
@@ -7,7 +7,7 @@ pkg_name="automate-cs-oc-erchef"
 pkg_description="Wrapper package for chef/oc_erchef"
 pkg_origin="chef"
 # WARNING: Version managed by .expeditor/update_chef_server.sh
-pkg_version="14.14.1"
+pkg_version="14.15.8"
 vendor_origin="chef"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
@@ -18,7 +18,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/oc_erchef/14.14.1/20220405043556"
+  "${vendor_origin}/oc_erchef/14.15.8/20220502135656"
 )
 
 pkg_build_deps=(
