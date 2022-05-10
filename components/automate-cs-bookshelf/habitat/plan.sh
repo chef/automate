c6effa0bd1bb47b33ede3003d35da659e3a5a9bc
jashaik <76031665+jashaik@users.noreply.github.com>
2022-05-10 16:58:21 +0530

    Updated chef version with chef 17.10 (#7011)
    Signed-off-by: jan shahid shaik <jashaik@progress.com>
    Signed-off-by: Vinay Satish <vinay.satish@progress.com>

--- components/automate-cs-bookshelf/habitat/plan.sh
+++ components/automate-cs-bookshelf/habitat/plan.sh
@@ -16,7 +16,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/bookshelf/14.15.9/20220508122943"
+  "${vendor_origin}/bookshelf/14.15.10/20220510065433"
 )
 
 pkg_binds=(
