6d3184ea5fdee7fe9c6e350467c6852f2f8bc793
Abdul-Az <aazeez@progress.com>
2022-05-12 12:14:08 +0530

    rebase successful
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- components/automate-deployment/pkg/services/internal/generated/gen.go
+++ components/automate-deployment/pkg/services/internal/generated/gen.go
@@ -505,30 +505,10 @@ var ProductMetadataJSON = `
         "uses_platform_scaffolding": false,
         "bootstrap": null
       }
-    },  
-    {
-      "name": "chef/report-manager-minio-gateway",
-      "metadata": {
-        "name": "chef/report-manager-minio-gateway",
-        "data_service": true,
-        "binlinks": null,
-        "uses_platform_scaffolding": false,
-        "bootstrap": null
-      }
     },
     {
       "name": "chef/automate-elasticsearch",
       "metadata": null
-    },
-    {
-      "name": "chef/report-manager-service",
-      "metadata": {
-        "name": "chef/report-manager-service",
-        "data_service": false,
-        "binlinks": null,
-        "uses_platform_scaffolding": true,
-        "bootstrap": null
-      }
     },
     {
       "name": "core/rsync",
