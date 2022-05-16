bb9ff49ac27170133dc0cfbddae66ab46b53277b
shaik80 <sh.mudassir98@gmail.com>
2022-05-16 13:05:01 +0530

    chef-server updated
    Signed-off-by: shaik80 <sh.mudassir98@gmail.com>

--- components/automate-cs-bookshelf/habitat/plan.sh
+++ components/automate-cs-bookshelf/habitat/plan.sh
@@ -16,7 +16,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/bookshelf/14.15.10/20220510065433"
+  "${vendor_origin}/bookshelf/14.15.10/20220511073223"
 )
 
 pkg_binds=(
