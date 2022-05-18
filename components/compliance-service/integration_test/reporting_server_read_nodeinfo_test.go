ff2662801dc2cbb9471c792a9c5e71f16933bba8
dmaddu <durgasaratchandra.maddu@progress.com>
2022-05-18 13:12:45 +0530

    updating to open search from elastic search
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>

--- components/compliance-service/integration_test/reporting_server_read_nodeinfo_test.go
+++ components/compliance-service/integration_test/reporting_server_read_nodeinfo_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestReadNodeInfo(t *testing.T) {
-	server := reportingServer.New(&relaxting.ES2Backend{ESUrl: elasticsearchUrl}, nil)
+	server := reportingServer.New(&relaxting.ES2Backend{ESUrl: opensearchUrl}, nil)
 	reports := []*relaxting.ESInSpecReport{
 		{
 			Projects: []string{},
