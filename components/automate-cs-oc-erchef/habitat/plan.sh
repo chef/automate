a28bb34636a0676f664351446d6e24efaeb0e7cc
shaik80 <sh.mudassir98@gmail.com>
2022-05-19 14:07:30 +0530

    update chef-server version timestamp
    Signed-off-by: shaik80 <sh.mudassir98@gmail.com>

--- components/automate-cs-oc-erchef/habitat/plan.sh
+++ components/automate-cs-oc-erchef/habitat/plan.sh
@@ -18,7 +18,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/oc_erchef/14.15.10/20220511073945"
+  "${vendor_origin}/oc_erchef/14.15.10/20220516074924"
 )
 
 pkg_build_deps=(
