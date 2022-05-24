5b8255c5f453bf2952b57f0068870046b7c7580e
Pappu Kumar <pappu.kumar@progress.com>
2022-05-24 18:15:29 +0530

    Testing old menifest
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- integration/tests/deep_upgrade.sh
+++ integration/tests/deep_upgrade.sh
@@ -21,7 +21,7 @@ test_detect_broken_cli=true
 # tests.
 test_detect_broken_packages=true
 
-CURRENT_OLDEST_VERSION=20180519154949
+CURRENT_OLDEST_VERSION=20200115001116
 OLD_MANIFEST_DIR="${A2_ROOT_DIR}/components/automate-deployment/testdata/old_manifests/"
 DEEP_UPGRADE_PATH="${OLD_MANIFEST_DIR}/${CURRENT_OLDEST_VERSION}.json"
 
@@ -47,7 +47,7 @@ do_upgrade() {
 do_deploy() {
     #shellcheck disable=SC2154
     cp "$DEEP_UPGRADE_PATH" "$test_manifest_path"
-    install_hab "0.54.0"
+    install_hab "0.90.6"
     upgrade_scaffold_bin="$(a2_root_dir)/components/automate-deployment/bin/linux/upgrade-test-scaffold"
     $upgrade_scaffold_bin setup "$test_manifest_path"
     $upgrade_scaffold_bin serve "$test_manifest_path" "$upgrade_scaffold_pid_file" &
