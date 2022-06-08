acabe1b1648af5eda2c3d2b6e6022b8911d0513d
punitmundra <punitmundra2008@gmail.com>
2022-06-08 14:20:24 +0530

    remove the echo logs from the cert.sh
    Signed-off-by: punitmundra <punitmundra2008@gmail.com>

--- components/automate-backend-postgresql/habitat/cert.sh
+++ components/automate-backend-postgresql/habitat/cert.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-basePath=$1
 echo " ================= Getting certs from vault using =============="
 echo $OPENSEARCH_ROOT_CA_PEM
-echo $OPENSEARCH_ADMIN_PEM
-echo $OPENSEARCH_ADMIN_KEY_PEM
 echo $OPENSEARCH_NODE1_PEM
 echo $OPENSEARCH_NODE1_KEY_PEM
 
