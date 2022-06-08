8538f9fda8735125bf02ebc05086a6ea36171f37
Sahiba31 <sahibagoyal1999@gmail.com>
2022-06-08 11:14:21 +0530

    Fixing UI Pipeline Issue
    Signed-off-by: Sahiba31 <sahibagoyal1999@gmail.com>

--- components/automate-ui/src/app/entities/download-reports/download-reports.service.spec.ts
+++ components/automate-ui/src/app/entities/download-reports/download-reports.service.spec.ts
@@ -119,7 +119,8 @@ describe('DownloadReportsService', () => {
   });
 
   it('should fetch the downloaded report', () => {
-    const expectedData = 'report';
+    const expectedData = new Blob();
+    // const expectedData = 'report';
     const ackId = '32f9a9c6-fe1f-456e-86de-6f5e308bab7e';
     const expectedUrl = `${REPORT_LIST_API_URL}/export/${ackId}`;
 
@@ -130,7 +131,7 @@ describe('DownloadReportsService', () => {
     const req = httpTestingController.expectOne(expectedUrl);
 
     expect(req.request.method).toEqual('GET');
-    expect(req.request.responseType).toEqual('text');
+    expect(req.request.responseType).toEqual('blob');
 
     req.flush(expectedData);
   });
