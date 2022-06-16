7a43f55a2a4ea98f1fd615e8157d172e1a24a9bf
Sahiba31 <64470245+Sahiba31@users.noreply.github.com>
2022-06-16 19:27:36 +0530

    Fixing UI Pipeline Issue (#7147)
    * Fixing UI Pipeline Issue
    
    Signed-off-by: Sahiba31 <sahibagoyal1999@gmail.com>
    
    * Fixing UI Pipeline
    
    Signed-off-by: Sahiba31 <sahibagoyal1999@gmail.com>
    
    * Fixing UI Pipeline
    
    Signed-off-by: Sahiba31 <sahibagoyal1999@gmail.com>
    
    * Fixing UI Pipeline
    
    Signed-off-by: Sahiba31 <sahibagoyal1999@gmail.com>
    
    * ui test fix
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>
    
    Co-authored-by: dmaddu <durgasaratchandra.maddu@progress.com>

--- components/automate-ui/src/app/pages/+compliance/+reporting/+reporting-node/reporting-node.component.spec.ts
+++ components/automate-ui/src/app/pages/+compliance/+reporting/+reporting-node/reporting-node.component.spec.ts
@@ -11,7 +11,11 @@ import { Observable, of as observableOf } from 'rxjs';
 import { FeatureFlagsService } from 'app/services/feature-flags/feature-flags.service';
 import { StatsService, ReportQueryService, ScanResultsService } from '../../shared/reporting';
 import { DatetimePipe } from 'app/pipes/datetime.pipe';
+import { TelemetryService } from '../../../../services/telemetry/telemetry.service';
 
+class MockTelemetryService {
+  track() { }
+}
 describe('ReportingNodeComponent', () => {
   let store: Store<NgrxStateAtom>;
   let fixture: ComponentFixture<ReportingNodeComponent>;
@@ -33,6 +37,7 @@ describe('ReportingNodeComponent', () => {
         DatetimePipe
       ],
       providers: [
+        { provide: TelemetryService, useClass: MockTelemetryService },
         ChefSessionService,
         StatsService,
         ReportQueryService,
