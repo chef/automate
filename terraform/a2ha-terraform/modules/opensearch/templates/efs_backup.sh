22896e601a5a4517b025fb9b3129c7d7af573595
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2022-06-17 15:00:29 +0530

    Fix for efs file path (#7181)
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>

--- terraform/a2ha-terraform/modules/opensearch/templates/efs_backup.sh
+++ terraform/a2ha-terraform/modules/opensearch/templates/efs_backup.sh
@@ -2,7 +2,7 @@
 
 source /hab/sup/default/SystemdEnvironmentFile.sh
 echo  '
-[es_yaml.path]      
+[path]      
   repo = "/mnt/automate_backups/opensearch" ' > es_config.toml
 
 hab config apply automate-ha-opensearch.default $(date '+%s') es_config.toml
