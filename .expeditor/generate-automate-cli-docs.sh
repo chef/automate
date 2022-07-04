de5cee6b9dd5151d87470d27859fc09a02d63bf1
Sunanda-Boorla <Sunanda.Boorla@progress.com>
2022-07-04 07:47:53 +0000

    go version upgrade to 1.18
    Signed-off-by: Sunanda-Boorla <Sunanda.Boorla@progress.com>

--- .expeditor/generate-automate-cli-docs.sh
+++ .expeditor/generate-automate-cli-docs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
-# bumping expeditor to go 1.13
-hab pkg install --binlink core/go/1.13.5 --force
+# bumping expeditor to go 1.18.3
+hab pkg install --binlink core/go/1.18.3 --force
 
 pushd components/automate-cli
   make docs
