667ba4f22d1f6036987cbc7314b5f54ec06a27d9
SahithiMy <98326242+SahithiMy@users.noreply.github.com>
2022-07-08 02:54:38 +0530

    Sahithi/stalwart 185 new (#7232)
    * Daily latest for control index
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Added function
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Done
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Testing the flow
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Testing
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Testing the PR
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Testing the PR
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Testing the PR
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Removing  errors in  the PR
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Removing extra code
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Fixed PR comments
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Fixed complication error
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Fixed complication error
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Fixed PR comments
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    Co-authored-by: SahithiMy <sahithi.mylangam@progress.com>
    Co-authored-by: Pappu Kumar <pappu.kumar@progress.com>

--- components/compliance-service/ingest/ingestic/ingestic.go
+++ components/compliance-service/ingest/ingestic/ingestic.go
@@ -955,9 +955,13 @@ func (backend *ESClient) UploadDataToControlIndex(ctx context.Context, reportuui
 	bulkRequest := backend.client.Bulk()
 	for _, control := range controls {
 		docId := GetDocIdByControlIdAndProfileID(control.ControlID, control.Profile.ProfileID)
-		err := backend.SetDayLatestToFalseForControlIndex(ctx, control.ControlID, control.Profile.ProfileID, mapping, index, control.Nodes[0].NodeUUID)
+		err := backend.SetDailyLatestToFalseForControlIndex(ctx, control.ControlID, control.Profile.ProfileID, mapping, index, control.Nodes[0].NodeUUID)
 		if err != nil {
-			return err
+			logrus.Errorf("Unable to SetDailyLatestToFalseForControlIndex %v", err)
+		}
+		err = backend.SetDayLatestToFalseForControlIndex(ctx, control.ControlID, control.Profile.ProfileID, mapping, index, control.Nodes[0].NodeUUID)
+		if err != nil {
+			logrus.Errorf("Unable to set Day Latest To false for control index %v", err)
 		}
 		found, err := backend.CheckIfControlIdExistsForToday(docId, index)
 		if err != nil {
@@ -1032,4 +1036,31 @@ func (backend *ESClient) SetDayLatestToFalseForControlIndex(ctx context.Context,
 		Refresh("false").
 		Do(ctx)
 	return errors.Wrap(err, "SetDayLatestsToFalseorControlIndex")
+}
+
+// Sets the 'daily_latest'  fields to 'false' for new control index
+// This targets only one ES UTC index
+func (backend *ESClient) SetDailyLatestToFalseForControlIndex(ctx context.Context, controlId string, profileId string, mapping mappings.Mapping, index string, nodeId string) error {
+	termQueryThisControl := elastic.NewTermsQuery("_id", GetDocIdByControlIdAndProfileID(controlId, profileId))
+
+	boolQueryDailyLatest := elastic.NewBoolQuery().
+		Must(termQueryThisControl)
+
+	// Script to find the nodes and making daily_latest as false
+	script := elastic.NewScript(`
+		 def targets = ctx._source.nodes.findAll(node -> node.node_uuid == params.node_uuid);
+		 for(node in targets) { 
+			 if(node.daily_latest==true) {
+				 node.daily_latest = false
+			 }
+		 }`).Param("node_uuid", nodeId)
+
+	// Updating in all the Indices
+	_, err := elastic.NewUpdateByQueryService(backend.client).
+		Index(index).
+		Query(boolQueryDailyLatest).
+		Script(script).
+		Refresh("false").
+		Do(ctx)
+	return errors.Wrap(err, "SetDailyLatestToFalseForControlIndex")
 }
