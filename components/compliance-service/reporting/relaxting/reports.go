489f2ba71bfbc6dd893eee50fd6eb7c1924fd657
Yashvi Jain <91940132+YashviJain01@users.noreply.github.com>
2022-07-21 16:05:51 +0530

    Control Summary From New Control Index (#7255)
    * Control Summary From New Control Index
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * Fixing code Smells
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * fixing test cases
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>

--- components/compliance-service/reporting/relaxting/reports.go
+++ components/compliance-service/reporting/relaxting/reports.go
@@ -2175,7 +2175,7 @@ func getControlSummaryFilters(controlId []string, filters map[string][]string) *
 
 func filterQueryChange(endTime string, startTime string) ([]string, error) {
 	if len(endTime) == 0 || len(startTime) == 0 {
-		return []string{"day_latest"}, nil
+		return []string{"day_latest", "daily_latest"}, nil
 	}
 	eTime, err := time.Parse(layout, endTime)
 	sTime, err := time.Parse(layout, startTime)
