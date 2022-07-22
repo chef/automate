fc1ad9536876fca8bbe9b788d7dc317f6134f7ce
Pappu Kumar <pappu.kumar@progress.com>
2022-07-22 14:26:29 +0530

    Test explained
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- components/compliance-service/ingest/ingestic/ingestic_test.go
+++ components/compliance-service/ingest/ingestic/ingestic_test.go
@@ -1,6 +1,7 @@
 package ingestic
 
 import (
+	"reflect"
 	"testing"
 	"time"
 
@@ -46,4 +47,43 @@ func TestScriptForUpdatingControlIndexStatusAndEndStatus(t *testing.T) {
 	script := scriptForUpdatingControlIndexStatusAndEndTime(controlStatus, nodeStatus, nodeEndTime)
 
 	assert.Equal(t, expectedScript, script)
+}
+
+func Test_scriptForUpdatingControlIndexStatusAndEndTime(t *testing.T) {
+	params := make(map[string]interface{})
+	params["node_end_time"] = time.Now().UTC()
+
+	expectedScript1 := elastic.NewScript(`ctx._source.end_time = params.node_end_time;
+	if(params.newStatus){
+		ctx._source.status = params.newStatus
+	}
+	`).Params(params)
+	
+	expectedScript2 := elastic.NewScript(`ctx._source.end_time = params.node_end_time;
+	if(params.newStatus){
+		ctx._source.status = params.newStatus
+	}
+	`).Params(params)
+
+	type args struct {
+		controlStatus string
+		nodeStatus    string
+		nodeEndtime   time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+		want *elastic.Script
+	}{
+		// TODO: Add test cases.
+		{name: "test 1", args: args{controlStatus: "", nodeStatus: "", nodeEndtime: time.Now()}, want: expectedScript1},
+		{name: "test 2", args: args{controlStatus: "", nodeStatus: "", nodeEndtime: time.Now()}, want: expectedScript2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := scriptForUpdatingControlIndexStatusAndEndTime(tt.args.controlStatus, tt.args.nodeStatus, tt.args.nodeEndtime); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("scriptForUpdatingControlIndexStatusAndEndTime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
