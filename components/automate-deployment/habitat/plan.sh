2b8291d056555aa63d896f676ae50b9fce047cf9
punitmundra <punitmundra2008@gmail.com>
2022-08-03 13:02:42 +0530

    hab upgrade to 20201030181202
    Signed-off-by: punitmundra <punitmundra2008@gmail.com>

--- components/automate-deployment/habitat/plan.sh
+++ components/automate-deployment/habitat/plan.sh
@@ -21,7 +21,7 @@ pkg_deps=(
   #core/hab/1.6.521/20220603154827
   #core/hab/1.6.543/20220721152654
   core/hab/1.6.181/20201030172917
-  core/hab-sup/1.6.181/20201030181202
+  #core/hab-sup/1.6.181/20201030181202
   core/net-tools
   core/procps-ng
   core/util-linux
