a354e1b6e449f8da09fb69c569619e261a014525
Yashvi Jain <91940132+YashviJain01@users.noreply.github.com>
2022-08-04 11:47:05 +0530

    adding queries for asset count api (#7296)
    * adding queries for asset count api
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * Adding collected query for asset
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * PR changes
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>

--- components/compliance-service/reporting/constants.go
+++ components/compliance-service/reporting/constants.go
@@ -1,3 +1,8 @@
 package reporting
 
 const ESize = 999999
+
+// These are filter types where we use ElasticSearch Term Queries
+var FilterType = []string{"environment", "organization", "chef_server", "chef_tags",
+	"policy_group", "policy_name", "status", "node_name", "platform", "platform_with_version",
+	"role", "recipe", "inspec_version", "ipaddress", "profile_id", "resource_type", "node_id"}
