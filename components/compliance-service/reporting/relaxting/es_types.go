5970871f924065e57ba9f41a477579922705f676
Dave Augustus <95467821+dave-augustus@users.noreply.github.com>
2022-08-09 15:05:45 +0530

    Populate info run index (#7302)
    * Populate info run index
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Done Run info populate
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Fixed some pr comments
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Changing the run info structure
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * Adding query for collected assests
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * Adding query for collected assests
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * Adding assets lists function for API
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * Resolving code smells
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * Adding go routines and channels
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * Fixing the channel error
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    * removing test loggers
    
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>
    
    Co-authored-by: Yashvi Jain <yashvi.jain@progress.com>

--- components/compliance-service/reporting/relaxting/es_types.go
+++ components/compliance-service/reporting/relaxting/es_types.go
@@ -329,3 +329,20 @@ type AssetSummary struct {
 	Failed  int32 `json:"failed"`
 	Waived  int32 `json:"waived"`
 }
+
+type ProfileRunInfo struct {
+	SHA256   string           `json:"sha256"`
+	Controls []ControlRunInfo `json:"controls"`
+	Name     string           `json:"name"`
+	Title    string           `json:"title"`
+	Full     string           `json:"full"`
+}
+
+type ControlRunInfo struct {
+	ID          string                            `json:"id"`
+	ControlTags []ESInSpecReportControlStringTags `json:"control_tags"`
+}
+
+type FirstRunInfo struct {
+	FirstRun string `json:"first_run""`
+}
