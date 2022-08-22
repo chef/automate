1bc51a0b3a86e3d703917f11ddb8877450ca0dc0
jay vikram sharma <jsharma@progress.com>
2022-08-22 13:00:19 +0530

    reading totalshards, total, indices-breaker, max_open_file, max_locked_mem
    Signed-off-by: jay vikram sharma <jsharma@progress.com>

--- components/automate-deployment/pkg/majorupgradechecklist/v3.go
+++ components/automate-deployment/pkg/majorupgradechecklist/v3.go
@@ -145,206 +145,12 @@ type ESClusterSetting struct {
 	Transient struct {
 	} `json:"transient"`
 	Defaults struct {
-		Cluster struct {
-			Routing struct {
-				Allocation struct {
-					NodeConcurrentIncomingRecoveries string `json:"node_concurrent_incoming_recoveries"`
-					NodeInitialPrimariesRecoveries   string `json:"node_initial_primaries_recoveries"`
-					SameShard                        struct {
-						Host string `json:"host"`
-					} `json:"same_shard"`
-					TotalShardsPerNode               string `json:"total_shards_per_node"`
-					Type                             string `json:"type"`
-					Enable                           string `json:"enable"`
-					NodeConcurrentOutgoingRecoveries string `json:"node_concurrent_outgoing_recoveries"`
-					AllowRebalance                   string `json:"allow_rebalance"`
-					ClusterConcurrentRebalance       string `json:"cluster_concurrent_rebalance"`
-					NodeConcurrentRecoveries         string `json:"node_concurrent_recoveries"`
-				} `json:"allocation"`
-			} `json:"routing"`
-			Name             string `json:"name"`
-			MaxShardsPerNode string `json:"max_shards_per_node"`
-		} `json:"cluster"`
-
-		Bootstrap struct {
-			MemoryLock       string `json:"memory_lock"`
-			SystemCallFilter string `json:"system_call_filter"`
-			Ctrlhandler      string `json:"ctrlhandler"`
-		} `json:"bootstrap"`
-		Processors string `json:"processors"`
-		Network    struct {
-			Host []interface{} `json:"host"`
-			TCP  struct {
-				ReuseAddress      string `json:"reuse_address"`
-				KeepAlive         string `json:"keep_alive"`
-				ConnectTimeout    string `json:"connect_timeout"`
-				ReceiveBufferSize string `json:"receive_buffer_size"`
-				NoDelay           string `json:"no_delay"`
-				SendBufferSize    string `json:"send_buffer_size"`
-			} `json:"tcp"`
-			BindHost []interface{} `json:"bind_host"`
-			Server   string        `json:"server"`
-			Breaker  struct {
-				InflightRequests struct {
-					Limit    string `json:"limit"`
-					Overhead string `json:"overhead"`
-				} `json:"inflight_requests"`
-			} `json:"breaker"`
-			PublishHost []interface{} `json:"publish_host"`
-		} `json:"network"`
-		Path struct {
-			Data       []string `json:"data"`
-			Logs       string   `json:"logs"`
-			SharedData string   `json:"shared_data"`
-			Home       string   `json:"home"`
-			Repo       []string `json:"repo"`
-		} `json:"path"`
-		Action struct {
-			AutoCreateIndex string `json:"auto_create_index"`
-			Search          struct {
-				ShardCount struct {
-					Limit string `json:"limit"`
-				} `json:"shard_count"`
-			} `json:"search"`
-			DestructiveRequiresName string `json:"destructive_requires_name"`
-			Master                  struct {
-				ForceLocal string `json:"force_local"`
-			} `json:"master"`
-		} `json:"action"`
-		Client struct {
-			Type      string `json:"type"`
-			Transport struct {
-				IgnoreClusterName    string `json:"ignore_cluster_name"`
-				NodesSamplerInterval string `json:"nodes_sampler_interval"`
-				Sniff                string `json:"sniff"`
-				PingTimeout          string `json:"ping_timeout"`
-			} `json:"transport"`
-		} `json:"client"`
-		Transport struct {
-			TCP struct {
-				ReuseAddress      string `json:"reuse_address"`
-				ConnectTimeout    string `json:"connect_timeout"`
-				Compress          string `json:"compress"`
-				Port              string `json:"port"`
-				NoDelay           string `json:"no_delay"`
-				KeepAlive         string `json:"keep_alive"`
-				ReceiveBufferSize string `json:"receive_buffer_size"`
-				SendBufferSize    string `json:"send_buffer_size"`
-			} `json:"tcp"`
-			BindHost           []interface{} `json:"bind_host"`
-			ConnectTimeout     string        `json:"connect_timeout"`
-			Compress           string        `json:"compress"`
-			PingSchedule       string        `json:"ping_schedule"`
-			ConnectionsPerNode struct {
-				Recovery string `json:"recovery"`
-				State    string `json:"state"`
-				Bulk     string `json:"bulk"`
-				Reg      string `json:"reg"`
-				Ping     string `json:"ping"`
-			} `json:"connections_per_node"`
-			Tracer struct {
-				Include []interface{} `json:"include"`
-				Exclude []string      `json:"exclude"`
-			} `json:"tracer"`
-			Type        string `json:"type"`
-			TypeDefault string `json:"type.default"`
-			Features    struct {
-				XPack string `json:"x-pack"`
-			} `json:"features"`
-			Port        string        `json:"port"`
-			Host        []interface{} `json:"host"`
-			PublishPort string        `json:"publish_port"`
-			TCPNoDelay  string        `json:"tcp_no_delay"`
-			PublishHost []interface{} `json:"publish_host"`
-			Netty       struct {
-				ReceivePredictorSize string `json:"receive_predictor_size"`
-				ReceivePredictorMax  string `json:"receive_predictor_max"`
-				WorkerCount          string `json:"worker_count"`
-				ReceivePredictorMin  string `json:"receive_predictor_min"`
-				BossCount            string `json:"boss_count"`
-			} `json:"netty"`
-		} `json:"transport"`
-		Script struct {
-			AllowedContexts     []interface{} `json:"allowed_contexts"`
-			MaxCompilationsRate string        `json:"max_compilations_rate"`
-			Cache               struct {
-				MaxSize string `json:"max_size"`
-				Expire  string `json:"expire"`
-			} `json:"cache"`
-			Painless struct {
-				Regex struct {
-					Enabled string `json:"enabled"`
-				} `json:"regex"`
-			} `json:"painless"`
-			MaxSizeInBytes string        `json:"max_size_in_bytes"`
-			AllowedTypes   []interface{} `json:"allowed_types"`
-		} `json:"script"`
-		Node struct {
-			Data                          string `json:"data"`
-			EnableLuceneSegmentInfosTrace string `json:"enable_lucene_segment_infos_trace"`
-			LocalStorage                  string `json:"local_storage"`
-			MaxLocalStorageNodes          string `json:"max_local_storage_nodes"`
-			Name                          string `json:"name"`
-			ID                            struct {
-				Seed string `json:"seed"`
-			} `json:"id"`
-			Store struct {
-				AllowMmap   string `json:"allow_mmap"`
-				AllowMmapfs string `json:"allow_mmapfs"`
-			} `json:"store"`
-			Attr struct {
-				Xpack struct {
-					Installed string `json:"installed"`
-				} `json:"xpack"`
-			} `json:"attr"`
-			Portsfile string `json:"portsfile"`
-			Ingest    string `json:"ingest"`
-			Master    string `json:"master"`
-			Ml        string `json:"ml"`
-		} `json:"node"`
 		Indices struct {
-			Cache struct {
-				CleanupInterval string `json:"cleanup_interval"`
-			} `json:"cache"`
-			Mapping struct {
-				DynamicTimeout string `json:"dynamic_timeout"`
-			} `json:"mapping"`
-			Memory struct {
-				Interval           string `json:"interval"`
-				MaxIndexBufferSize string `json:"max_index_buffer_size"`
-				ShardInactiveTime  string `json:"shard_inactive_time"`
-				IndexBufferSize    string `json:"index_buffer_size"`
-				MinIndexBufferSize string `json:"min_index_buffer_size"`
-			} `json:"memory"`
 			Breaker struct {
-				Request struct {
-					Limit    string `json:"limit"`
-					Type     string `json:"type"`
-					Overhead string `json:"overhead"`
-				} `json:"request"`
 				Total struct {
 					Limit string `json:"limit"`
 				} `json:"total"`
-				Accounting struct {
-					Limit    string `json:"limit"`
-					Overhead string `json:"overhead"`
-				} `json:"accounting"`
-				Fielddata struct {
-					Limit    string `json:"limit"`
-					Type     string `json:"type"`
-					Overhead string `json:"overhead"`
-				} `json:"fielddata"`
-				Type string `json:"type"`
 			} `json:"breaker"`
-			Query struct {
-				Bool struct {
-					MaxClauseCount string `json:"max_clause_count"`
-				} `json:"bool"`
-				QueryString struct {
-					AnalyzeWildcard      string `json:"analyze_wildcard"`
-					AllowLeadingWildcard string `json:"allowLeadingWildcard"`
-				} `json:"query_string"`
-			} `json:"query"`
 		} `json:"indices"`
 	} `json:"defaults"`
 }
@@ -581,19 +387,24 @@ func getElasticSearchClusterStas() (string, error) {
 	return string(indicesTotalShard), nil
 }
 
-func getElasticSearchCusterSetting() {
+func getElasticSearchCusterSetting() (*ESClusterSetting, error) {
 	basePath := getELasticSearchBasePath()
 	allClusterSettings, err := getDataFromUrl(basePath + "_cluster/settings?include_defaults=true")
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	clusterSetting := &ESClusterSetting{}
+	err = json.Unmarshal(allClusterSettings, clusterSetting)
+	if err != nil {
+		return nil, err
+	}
+	return clusterSetting, nil
 }
 
 func getElasticSearchRuntimeSettings() (string, error) {
 	var result = make(map[string]string)
 
-    var lines = data.Split(result,"\n")
+	var lines = data.Split(result, "\n")
 	pid, err := ioutil.ReadFile("/hab/svc/automate-elasticsearch/PID")
 	if err != nil {
 		return "", err
@@ -607,16 +418,5 @@ func getElasticSearchRuntimeSettings() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	json.Unmarshal(data, ESClusterSetting{})
-    
-	for i:= 1 ; i < len(lines); i++ {
-		s := strings.Split(lines[i], "\t")
-		if s[0]= "Max open files" || "Max locked memory" {
-		result[s[0]] = s[1]
-		}
-    	
-	}
-	
-
 	return string(data), nil
 }
