2cc68f090cd02ca142fc344e46bfbdb6a5362f5d
Pappu Kumar <pappu.kumar@progress.com>
2022-08-23 17:07:01 +0530

    Updated constants.go file
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- components/compliance-service/reporting/constants.go
+++ components/compliance-service/reporting/constants.go
@@ -3,16 +3,16 @@ package reporting
 const ESize = 999999
 
 const (
-	UNREPORTED = "unreported"
+	UNREPORTED  = "unreported"
 	UNREACHABLE = "unreachable"
 	UNCOLLECTED = "uncollected"
-	COLLECTED = "collected"
+	COLLECTED   = "collected"
 )
 
 // These are filter types where we use ElasticSearch Term Queries
 var FilterType = []string{"environment", "organization", "chef_server", "chef_tags",
 	"policy_group", "policy_name", "status", "node_name", "platform", "platform_with_version",
-	"role", "recipe", "inspec_version", "ipaddress", "profile_id", "resource_type"}
+	"role", "recipe", "inspec_version", "ipaddress", "resource_type"}
 
 var EndTime = "end_time"
 
