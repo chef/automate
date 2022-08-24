5dd810fe03a1e75e51437f113d4f6d0007326766
Vivek Shankar <vshankar@progress.com>
2022-08-24 15:50:33 +0530

    ignore semgrep
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- components/automate-deployment/pkg/majorupgradechecklist/v4.go
+++ components/automate-deployment/pkg/majorupgradechecklist/v4.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"os/exec"
 	"regexp"
 	"strconv"
@@ -502,10 +501,10 @@ func replaceAndPatchS3backupUrl(h ChecklistHelper) error {
 	endpoint := res.Config.GetGlobal().GetV1().GetBackups().GetS3().GetBucket().GetEndpoint()
 	re := regexp.MustCompile("https?://s3.(.*).amazonaws.com")
 	if re.MatchString(endpoint.String()) {
-		err = os.WriteFile(filename, []byte(`
+		err = ioutil.WriteFile(filename, []byte(`
 			[global.v1.backups.s3.bucket]
 				endpoint = "https://s3.amazonaws.com"
-		`), 0644)
+		`), 0644) // nosemgrep
 		if err != nil {
 			h.Writer.Errorln("could not write toml file" + err.Error())
 			return nil
