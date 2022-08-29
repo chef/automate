29ed6d1b7cefae749d291c7550de465469664c3d
Arvinth C <arvinth.chandrasekaran@progress.com>
2022-08-29 18:07:12 +0530

    Adding replace URL logic
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>

--- components/automate-deployment/pkg/client/automate_config.go
+++ components/automate-deployment/pkg/client/automate_config.go
@@ -11,7 +11,6 @@ import (
 	api "github.com/chef/automate/api/interservice/deployment"
 	"github.com/chef/automate/components/automate-cli/pkg/status"
 	"github.com/chef/automate/components/automate-deployment/pkg/cli"
-	"github.com/chef/automate/components/automate-deployment/pkg/majorupgradechecklist"
 )
 
 // GetAutomateConfig makes a gRPC request to the server and returns the
@@ -38,8 +37,6 @@ func PatchAutomateConfig(timeout int64, config *dc.AutomateConfig, writer cli.Fo
 	if err != nil {
 		return err
 	}
-
-	majorupgradechecklist.NewPostChecklistManager("NEXT_AUTOMATE_VERSION")
 
 	getRes, err := GetAutomateConfig(timeout)
 	if err != nil {
