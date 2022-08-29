91fb1ef679e71b18a27f374a2958805ccd41b308
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2022-08-29 22:05:47 +0530

    Added prompt to get acknowledgement from user for updating s3 url (#7364)
    * Added prompt to get acknowledgement from user for updating s3 url
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Adding replace URL logic
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Adding replace URL logic
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Renaming functions
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Validating s3 patch command
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Removing nested if else
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Patch changes
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Adding replace URL logic
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Adding replace URL logic
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Adding regexp logic to check valid AWS s3 url
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Validating s3 URL
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Adding replace URL logic
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Adding replace URL logic
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Adding replace URL logic
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Adding replace URL logic
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>

--- components/automate-deployment/pkg/client/automate_config.go
+++ components/automate-deployment/pkg/client/automate_config.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"time"
 
 	"github.com/pkg/errors"
@@ -43,6 +44,17 @@ func PatchAutomateConfig(timeout int64, config *dc.AutomateConfig, writer cli.Fo
 		return status.Wrap(err, status.DeploymentServiceCallError, "Failed attempting to get Chef Automate configuration from the deployment-service")
 	}
 
+	// Validating s3 url
+	if config.Global != nil && config.Global.String() != "" {
+		if config.Global.V1.Backups.Location.Value == "s3" {
+			isValid := validateS3Url(config)
+			if !isValid {
+				errMessage := "The endpoint value of s3 backup in provided patch configuration is having invalid pattern " + config.Global.V1.Backups.S3.Bucket.Endpoint.Value + ". Please ensure to replace it with https://s3.amazonaws.com"
+				return status.Wrap(nil, status.DeploymentServiceCallError, errMessage)
+			}
+		}
+	}
+
 	// TODO: Create merge configuration together, show it to the user and
 	// confirm that they wish to proceed with the patched merged config.
 
@@ -64,6 +76,19 @@ func PatchAutomateConfig(timeout int64, config *dc.AutomateConfig, writer cli.Fo
 	}
 
 	return nil
+}
+
+func validateS3Url(config *dc.AutomateConfig) bool {
+	endpoint := config.Global.V1.Backups.S3.Bucket.Endpoint.Value
+	s3regex := "https?://s3.(.*).amazonaws.com"
+	res1, err := regexp.MatchString(s3regex, endpoint)
+	if err != nil {
+		return false
+	}
+	if res1 {
+		return endpoint == "https://s3.amazonaws.com"
+	}
+	return true
 }
 
 // SetAutomateConfig makes a gRPC request to the server with a given
