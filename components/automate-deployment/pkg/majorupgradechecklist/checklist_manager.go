bf80d807ad97cf90a444bf6d0aaf22a248822572
Jay Vikram Sharma <36661596+jayvikramsharma1@users.noreply.github.com>
2022-08-30 15:25:53 +0530

    individual error message for each failed settings (#7372)
    * individual error message for each failed settings
    
    Signed-off-by: jayvikramsharma <jsharma@progress.com>
    
    * removing un-used variables
    
    Signed-off-by: jayvikramsharma <jsharma@progress.com>
    
    * error msg formatting
    
    Signed-off-by: jayvikramsharma <jsharma@progress.com>
    
    * using checklist for errors, and showing config before patch
    
    Signed-off-by: jayvikramsharma <jsharma@progress.com>
    
    * removing spaces from template
    
    Signed-off-by: jayvikramsharma <jsharma@progress.com>
    
    * resolving conflicts
    
    Signed-off-by: jayvikramsharma <jsharma@progress.com>
    
    * re-formating patch template
    
    Signed-off-by: jayvikramsharma <jsharma@progress.com>
    
    Signed-off-by: jayvikramsharma <jsharma@progress.com>
    Co-authored-by: jayvikramsharma <jsharma@progress.com>

--- components/automate-deployment/pkg/majorupgradechecklist/checklist_manager.go
+++ components/automate-deployment/pkg/majorupgradechecklist/checklist_manager.go
@@ -13,7 +13,6 @@ type ChecklistUpgradeFlags struct {
 type ChecklistManager interface {
 	RunChecklist(int64, ChecklistUpgradeFlags) error
 	GetPostChecklist() []PostCheckListItem
-	StoreSearchEngineSettings(writer cli.FormatWriter) (bool, error)
 }
 
 func NewChecklistManager(writer cli.FormatWriter, version string) (ChecklistManager, error) {
