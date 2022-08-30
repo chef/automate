5c80092748e6a984b3007c353928561a4d503da7
jayvikramsharma <jsharma@progress.com>
2022-08-30 15:23:55 +0530

    re-formating patch template
    Signed-off-by: jayvikramsharma <jsharma@progress.com>

--- components/automate-deployment/pkg/majorupgradechecklist/opensearch_settings_template.go
+++ components/automate-deployment/pkg/majorupgradechecklist/opensearch_settings_template.go
@@ -4,13 +4,13 @@ const patchOpensearchSettingsTemplate = `
 [opensearch]
   [opensearch.v1]
     [opensearch.v1.sys]
-    {{ if .TotalShardSettings }}[opensearch.v1.sys.cluster]
+      {{ if .TotalShardSettings }}[opensearch.v1.sys.cluster]
         max_shards_per_node = {{ .TotalShardSettings }} {{ end }}
-        {{ if .IndicesBreakerTotalLimit }} [opensearch.v1.sys.indices]
+      {{ if .IndicesBreakerTotalLimit }}[opensearch.v1.sys.indices]
         [opensearch.v1.sys.indices.breaker]
           total_limit = "{{ .IndicesBreakerTotalLimit }}%" {{ end }}
       [opensearch.v1.sys.runtime]
-      {{ if .RuntimeMaxOpenFile }} max_open_files = "{{ .RuntimeMaxOpenFile }}" {{ end }}
-      {{ if .RuntimeMaxLockedMem }} max_locked_memory = "{{ .RuntimeMaxLockedMem }}" {{ end }}
-      {{ if .HeapMemory }} heapsize = "{{ .HeapMemory }}g" {{ end }}
+        {{ if .RuntimeMaxOpenFile }}max_open_files = "{{ .RuntimeMaxOpenFile }}" {{ end }}
+        {{ if .RuntimeMaxLockedMem }}max_locked_memory = "{{ .RuntimeMaxLockedMem }}" {{ end }}
+        {{ if .HeapMemory }}heapsize = "{{ .HeapMemory }}g" {{ end }}
 `
