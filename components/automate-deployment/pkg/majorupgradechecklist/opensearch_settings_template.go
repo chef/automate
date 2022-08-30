700c6dcbcff5c5efb638348362e72a56c4f2d31b
jayvikramsharma <jsharma@progress.com>
2022-08-30 15:02:46 +0530

    removing spaces from template
    Signed-off-by: jayvikramsharma <jsharma@progress.com>

--- components/automate-deployment/pkg/majorupgradechecklist/opensearch_settings_template.go
+++ components/automate-deployment/pkg/majorupgradechecklist/opensearch_settings_template.go
@@ -4,23 +4,13 @@ const patchOpensearchSettingsTemplate = `
 [opensearch]
   [opensearch.v1]
     [opensearch.v1.sys]
-	{{ if .TotalShardSettings }}
-      [opensearch.v1.sys.cluster]
-        max_shards_per_node = {{ .TotalShardSettings }} # Refer the value from ElasticSearch Config, If this value is not there in elastic search config, then do not patch in openseaarch.
-	{{ end }}
-	{{ if .IndicesBreakerTotalLimit }}
-      [opensearch.v1.sys.indices]
+    {{ if .TotalShardSettings }}[opensearch.v1.sys.cluster]
+        max_shards_per_node = {{ .TotalShardSettings }} {{ end }}
+        {{ if .IndicesBreakerTotalLimit }} [opensearch.v1.sys.indices]
         [opensearch.v1.sys.indices.breaker]
-          total_limit = "{{ .IndicesBreakerTotalLimit }}%"
-	{{ end }}
+          total_limit = "{{ .IndicesBreakerTotalLimit }}%" {{ end }}
       [opensearch.v1.sys.runtime]
-	  {{ if .RuntimeMaxOpenFile }}
-        max_open_files = "{{ .RuntimeMaxOpenFile }}"
-	  {{ end }}
-	  {{ if .RuntimeMaxLockedMem }}
-        max_locked_memory = "{{ .RuntimeMaxLockedMem }}"
-	  {{ end }}
-	  {{ if .HeapMemory }}
-        heapsize = "{{ .HeapMemory }}g"
-	  {{ end }}
+      {{ if .RuntimeMaxOpenFile }} max_open_files = "{{ .RuntimeMaxOpenFile }}" {{ end }}
+      {{ if .RuntimeMaxLockedMem }} max_locked_memory = "{{ .RuntimeMaxLockedMem }}" {{ end }}
+      {{ if .HeapMemory }} heapsize = "{{ .HeapMemory }}g" {{ end }}
 `
