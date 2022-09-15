3ddbaca3124b478c6881b71f6fdb0bdf34766b89
Tejaswi Bondila <bondila.venkatatejaswi@progress.com>
2022-09-15 12:08:25 +0530

    tejaswi/modifying_output
    Signed-off-by: Tejaswi Bondila <bondila.venkatatejaswi@progress.com>

--- terraform/a2ha-terraform/reference_architectures/aws/outputs.tf
+++ terraform/a2ha-terraform/reference_architectures/aws/outputs.tf
@@ -6,6 +6,14 @@ output "ssh_key_file" {
   value = var.ssh_key_file
 }
 
+output "ssh_user_name" {
+  value = var.ssh_user_name
+}
+
+output "ssh_port" {
+  value = var.ssh_port
+}
+
 output "automate_private_ips" {
   value = formatlist("%s", module.aws.automate_private_ips)
 }
