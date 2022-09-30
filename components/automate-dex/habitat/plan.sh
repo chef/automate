0998b5966293d2d5d1af5d6532afaeb35eb8628f
Abdul-Az <aazeez@progress.com>
2022-09-30 15:43:48 +0530

    csp headers added
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- components/automate-dex/habitat/plan.sh
+++ components/automate-dex/habitat/plan.sh
@@ -39,7 +39,7 @@ do_before() {
 do_unpack() {
   git clone "https://github.com/chef/dex-1" "$GOPATH/src/github.com/chef/dex-1"
   ( cd "$GOPATH/src/github.com/chef/dex-1" || exit
-    git checkout add_invalid_attempts
+    git checkout abdul/csp_header_fix
   )
 }
 
