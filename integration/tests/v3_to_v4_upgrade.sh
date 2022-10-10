5349dbb83916d50045a7ab7eed9b885a6a256fa4
vivekshankar1 <59958706+vivekshankar1@users.noreply.github.com>
2022-10-10 18:27:46 +0530

    Fixing pipeline for new cli changes (#7472)
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * fix pipeline
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * pipeline fix
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * banner fix
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * banner fix
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- integration/tests/v3_to_v4_upgrade.sh
+++ integration/tests/v3_to_v4_upgrade.sh
@@ -57,12 +57,7 @@ run_upgrade() {
         echo "$ERROR"
         if echo "${ERROR}" | grep 'This is a Major upgrade'; then
             echo "major normal upgrade"
-            echo "y
-y
-y
-y
-y
-y" | chef-automate upgrade run --major --versions-file "$versionsFile"
+            echo "y" | chef-automate upgrade run --major --versions-file "$versionsFile"
             # NOTE: This is a hack
             # The hack above was no longer good enough because we have a thing that needs
             # to be updated that isn't a service
@@ -70,7 +65,9 @@ y" | chef-automate upgrade run --major --versions-file "$versionsFile"
 
             #shellcheck disable=SC2154
             wait_for_upgrade "$test_detect_broken_cli" "$test_detect_broken_packages"
-            echo 'y' | chef-automate post-major-upgrade migrate --data=es 
+            wait_for_healthy
+            echo 'y
+y' | chef-automate upgrade status --versions-file "$versionsFile"
         else
             echo "regular normal upgrade"
             sleep 45
