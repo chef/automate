6deb439da69b394950bd60d746da1fa2c133382b
vivekshankar1 <59958706+vivekshankar1@users.noreply.github.com>
2022-10-10 18:29:07 +0530

    Update .expeditor/publish-release-notes.sh
    Co-authored-by: Ian Maddaus <IanMadd@users.noreply.github.com>

--- .expeditor/publish-release-notes.sh
+++ .expeditor/publish-release-notes.sh
@@ -29,7 +29,7 @@ Chef lets you choose your **upgrade journey** based on your current version of C
 | 20220329091442| 3.0.x|
 | 3.0.49| 4.x|
 
-Click [here](https://docs.chef.io/automate/major_upgrade_4.x/) to know more.
+See the [Chef Automate 4.x upgrade documentation](https://docs.chef.io/automate/major_upgrade_4.x/) for more information. 
 
 ## New Features
 -
