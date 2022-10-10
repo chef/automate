c2b840f225d4d61d6ef48f8d3e364a2c080f2a10
Vivek Shankar <vshankar@progress.com>
2022-10-10 15:32:24 +0530

    banner fix
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- integration/tests/airgap_upgrade.sh
+++ integration/tests/airgap_upgrade.sh
@@ -18,14 +18,14 @@ do_build() {
 
     set_version_file
     #shellcheck disable=SC2154
-    newversion=$(jq -r -c ".build"  "$test_manifest_dir/3.0.49.json")
+    newversion=$(jq -r -c ".build"  "$test_manifest_dir/dev.json")
     #shellcheck disable=SC2154
     jq --arg val "$newversion" '. + [$val]' "$versionsFile" > tmp.$$.json && mv tmp.$$.json "$versionsFile"
 
     log_info "Creating initial airgap bundle"
     #shellcheck disable=SC2154
     chef-automate airgap bundle create \
-        --manifest "$test_manifest_dir/3.0.49.json" \
+        --manifest "$test_manifest_dir/dev.json" \
         --workspace workspace \
         --versions-file "$versionsFile" \
         bundle.aib
@@ -52,15 +52,5 @@ do_deploy() {
 }
 
 do_upgrade() {
-    echo "y" | chef-automate upgrade run --major --airgap-bundle update.aib --versions-file "$versionsFile"
-    # NOTE: This is a hack
-    # The hack above was no longer good enough because we have a thing that needs
-    # to be updated that isn't a service
-    sleep 45
-
-    #shellcheck disable=SC2154
-    wait_for_upgrade "$test_detect_broken_cli" "$test_detect_broken_packages"
-    wait_for_healthy
-    echo 'y
-y' | chef-automate upgrade status --versions-file "$versionsFile"
+    run_upgrade update.aib
 }
