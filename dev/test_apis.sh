ca3e6c70a639e4b4577ec967facae48199650c32
Pappu Kumar <pappu.kumar@progress.com>
2022-10-14 18:32:39 +0530

    changes concurrent fucntion
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- dev/test_apis.sh
+++ dev/test_apis.sh
@@ -5,14 +5,18 @@ token="1c92BssTcIGG3ClopuwooFXedQ4="
 newline=$'\n'
 
 overview() {
+	echo "${newline}****************** overview API ******************"
+
 	curl -k --location --request POST 'https://'$domain'/api/v0/compliance/reporting/stats/summary' \
---header 'accept: application/json, text/plain, */*' \
---header 'api-token: '$token'' \
---header 'Content-Type: text/plain' \
---data-raw '{"filters":[]}'
+	--header 'accept: application/json, text/plain, */*' \
+	--header 'api-token: '$token'' \
+	--header 'Content-Type: text/plain' \
+	--data-raw '{"filters":[]}'
 }
 
 list_nodes() {
+	echo "${newline}****************** List Nodes API ******************"
+
 	curl -k 'https://'$domain'/api/v0/compliance/reporting/nodes/search' \
 	--header 'accept: application/json, text/plain, */*' \
 	--header 'api-token: '$token'' \
@@ -22,16 +26,20 @@ list_nodes() {
 }
 
 get_report() {
+	echo "${newline}****************** Get Reports API ******************"
+
 	curl 'https://'$domain'/api/v0/compliance/reporting/reports' \
   	--header 'accept: application/json, text/plain, */*' \
 	--header 'api-token: '$token'' \
 	--header 'Content-Type: text/plain' \
-  --data-raw '{"filters":[{"type":"node_id","values":["9b9f4e51-b049-4b10-9555-10578916e666"]}],"page":1,"per_page":10,"sort":"latest_report.end_time","order":"DESC"}' \
-  --compressed \
-  --insecure
+	--data-raw '{"filters":[{"type":"node_id","values":["9b9f4e51-b049-4b10-9555-10578916e666"]}],"page":1,"per_page":10,"sort":"latest_report.end_time","order":"DESC"}' \
+	--compressed \
+	--insecure
 }
 
 suggestions() {
+	echo "${newline}****************** Suggestion API ******************"
+
 	curl 'https://'$domain'/api/v0/compliance/reporting/suggestions' \
 	--header 'accept: application/json, text/plain, */*' \
 	--header 'api-token: '$token'' \
@@ -42,6 +50,8 @@ suggestions() {
 }
 
 profiles() {
+	echo "${newline}****************** List Profiles API ******************"
+
 	curl 'https://'$domain'/api/v0/compliance/reporting/profiles' \
   	--header 'accept: application/json, text/plain, */*' \
 	--header 'api-token: '$token'' \
@@ -52,16 +62,20 @@ profiles() {
 }
 
 get_profile() {
+	echo "${newline}****************** Get profile API ******************"
+
 	curl 'https://'$domain'/api/v0/compliance/reporting/stats/profiles' \
    	--header 'accept: application/json, text/plain, */*' \
 	--header 'api-token: '$token'' \
 	--header 'Content-Type: text/plain' \
-  --data-raw '{"type":"summary","id":"b53ca05fbfe17a36363a40f3ad5bd70aa20057eaf15a9a9a8124a84d4ef08015","filters":[]}' \
-  --compressed \
-  --insecure
+	--data-raw '{"type":"summary","id":"b53ca05fbfe17a36363a40f3ad5bd70aa20057eaf15a9a9a8124a84d4ef08015","filters":[]}' \
+	--compressed \
+	--insecure
 }
 
 list_control_report() {
+	echo "${newline}****************** List Control Report API ******************"
+
 	curl 'https://'$domain'/api/v0/compliance/reporting/reportcontrols/id/44024b50-2e0d-42fa-a57c-dddddddddddd' \
 	--header 'accept: application/json, text/plain, */*' \
 	--header 'api-token: '$token'' \
@@ -72,6 +86,8 @@ list_control_report() {
 }
 
 list_control() {
+	echo "${newline}****************** Control API ******************"
+
 	curl 'https://'$domain'/api/v0/compliance/reporting/controls' \
 	--header 'accept: application/json, text/plain, */*' \
 	--header 'api-token: '$token'' \
@@ -82,22 +98,14 @@ list_control() {
 }
 
 for i in {1..1000} ; do
-    echo "${newline}****************** overview ******************"
-	overview
-	echo "${newline}****************** List node ******************"
-	list_nodes
-	echo "${newline}****************** Get Reports ******************"
-	get_report
-	echo "${newline}****************** Suggestion ******************"
-	suggestions
-	echo "${newline}****************** List profiles ******************"
-	profiles
-	echo "${newline}****************** Get profile ******************"
-	get_profile
-	echo "${newline}****************** List Control Report ******************"
-	list_control_report
-	echo "${newline}****************** Control ******************"
-	list_control
-	echo "${newline}Done!"
-   
+	overview &
+	list_nodes &
+	get_report &
+	suggestions &
+	profiles &
+	get_profile &
+	list_control_report &
+	list_control &
 done
+
+echo "${newline}Done executions!"
