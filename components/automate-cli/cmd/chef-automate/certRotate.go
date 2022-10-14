c898c00d3f0eee1be0eccffbb19590c7869c77bd
Sahiba3108 <sgoyal@progress.com>
2022-10-14 11:58:43 +0530

    add cert rotate

--- components/automate-cli/cmd/chef-automate/certRotate.go
+++ components/automate-cli/cmd/chef-automate/certRotate.go
@@ -6,31 +6,42 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var rootCaFlags = struct {
+	rootCa      string
+	privateCert string
+	publicCert  string
+}{}
+
+var sshFlag = struct {
+	automate bool
+	pg       bool
+}{}
+
 // certRotateCmd represents the certRotate command
 var certRotateCmd = &cobra.Command{
 	Use:   "certRotate",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("certRotate called")
-	},
+	Short: "Chef Automate rotate cert",
+	Long:  "Chef Automate CLI command to rotate certificates",
+	RunE:  certRotate,
 }
 
 func init() {
 	RootCmd.AddCommand(certRotateCmd)
 
-	// Here you will define your flags and configuration settings.
+	certRotateCmd.PersistentFlags().BoolVar(&sshFlag.automate, "automate", false, "Automate ha server name to ssh")
+	certRotateCmd.PersistentFlags().BoolVar(&sshFlag.pg, "pg", false, "Automate ha server name to ssh")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// certRotateCmd.PersistentFlags().String("foo", "", "A help for foo")
+	certRotateCmd.PersistentFlags().StringVar(&rootCaFlags.rootCa, "root-ca", "", "Automate Root CA value")
+	certRotateCmd.PersistentFlags().StringVar(&rootCaFlags.privateCert, "private-cert", "", "Automate ha private certificate")
+	certRotateCmd.PersistentFlags().StringVar(&rootCaFlags.publicCert, "public-cert", "", "Automate ha public certificate")
+}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// certRotateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func certRotate(cmd *cobra.Command, args []string) error {
+	fmt.Println(rootCaFlags.rootCa)
+	if sshFlag.automate {
+		fmt.Println("automate")
+	} else {
+		fmt.Println("pg")
+	}
+	return nil
 }
