b767082bbf9031f13a0c3f0e5d265feef6976a6a
VivekSharma2607 <sharmavishu760@gmail.com>
2022-10-17 15:52:19 +0530

    Set SSO Config Request
    Signed-off-by: VivekSharma2607 <sharmavishu760@gmail.com>

--- components/automate-gateway/handler/ssoconfig.go
+++ components/automate-gateway/handler/ssoconfig.go
@@ -1,11 +1,18 @@
 package handler
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 
 	deployment "github.com/chef/automate/api/interservice/deployment"
 	license_control "github.com/chef/automate/api/interservice/license_control"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	"github.com/chef/automate/api/external/sso"
@@ -16,6 +23,17 @@ import (
 type SsoConfig struct {
 	license_client license_control.LicenseControlServiceClient
 	client         deployment.DeploymentClient
+}
+
+type PostConfig struct {
+    Ca_contents           string  		`json:"ca_contents"`
+	Sso_url               string		`json:"sso_url"`
+	Email_attr            string		`json:"email_attr"`
+	Username_attr         string		`json:"username_attr"`
+	Groups_attr           string		`json:"groups_attr"`
+	Allowed_groups        []string	    `json:"allowed_groups"`
+	Entity_issuer         string		`json:"entity_issuer"`
+	Name_id_policy_format string	    `json:"name_id_policy_format"`
 }
 
 // NewSsoConfigHandler - create a new ssoconfig service handler
@@ -72,6 +90,19 @@ func (a *SsoConfig) GetSsoConfig(ctx context.Context, in *empty.Empty) (*sso.Get
 	}, nil
 }
 
+func(a *SsoConfig) validateDeploymentType(ctx context.Context) error {
+	deploymentType, err := a.getDeploymentDetails(ctx)
+	if err != nil {
+		return err
+	}
+
+	if deploymentType != "SAAS" {
+		msg := "Unauthorized: Deployment type is not SAAS"
+		return status.Error(codes.PermissionDenied, msg)
+	}
+	return nil
+}
+
 func (a *SsoConfig) getDeploymentDetails(ctx context.Context) (string, error) {
 	deployIDResponse, err := a.license_client.GetDeploymentID(ctx, &license_control.GetDeploymentIDRequest{})
 	if err != nil {
@@ -88,4 +119,78 @@ func (a *SsoConfig) getDeploymentDetails(ctx context.Context) (string, error) {
 	log.Debugf("deployIDResponse.DeploymentType: %s ", deployIDResponse.DeploymentType)
 
 	return deployIDResponse.DeploymentType, nil
+}
+
+func makeRequest(requestType string, url string, jsonData []byte, fileName string) {
+    req, err := http.NewRequest(requestType, url, bytes.NewBuffer(jsonData))
+	if err != nil {
+        log.Fatal("Error occurred", err)
+    }
+
+    req.Header.Set("Content-Type", "application/json")
+    client := &http.Client{}
+    resp, err := client.Do(req)
+
+    if err != nil {
+        log.Fatal("Error occurred", err)
+    }
+    defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		ioutil.WriteFile("/var/automate-ha/"+fileName, []byte("Success"), 0777)
+		return
+	}
+	ioutil.WriteFile("/var/automate-ha/"+fileName, []byte("Failure"), 0777)
+}
+
+func(a *SsoConfig) SetSsoConfig(ctx context.Context, in *sso.SetSsoConfigRequest) (*sso.SetSsoConfigResponse , error) {
+	err := a.validateDeploymentType(ctx)
+	if err != nil {
+		return nil , err
+	}
+
+	req := &sso.SetSsoConfigRequest{
+		CaContents: in.CaContents,
+		SsoUrl: in.SsoUrl,
+		EmailAttr: in.EmailAttr,
+		UsernameAttr: in.UsernameAttr,
+		GroupsAttr: in.GroupsAttr,
+		AllowedGroups: in.AllowedGroups,
+		EntityIssuer: in.EntityIssuer,
+		NameIdPolicyFormat: in.NameIdPolicyFormat,
+	}
+	bodyParams:= &PostConfig{
+		Ca_contents:         req.CaContents,
+		Sso_url:             req.SsoUrl,
+		Email_attr:          req.EmailAttr,
+		Username_attr:       req.UsernameAttr,
+		Groups_attr:         req.GroupsAttr,
+		Allowed_groups:      req.AllowedGroups,
+		Entity_issuer:       req.EntityIssuer,
+		Name_id_policy_format: req.NameIdPolicyFormat,
+	}
+	jsonValue, _ :=  json.Marshal(bodyParams)
+	url, err := getBastionUrl()
+	if err != nil {
+		log.Fatalln("Errror while creating bastion url = ", err)
+		return nil, err
+	}
+	fileName := "post-status.txt"
+	err = ioutil.WriteFile("/var/automate-ha/"+fileName , []byte("Pending"), 0777)
+	if err != nil {
+		fmt.Printf("Unable to write the file: %v" , err)
+	}
+	go makeRequest("POST", *url, jsonValue , fileName)
+	return &sso.SetSsoConfigResponse{
+		Response: "Config patch was successfull",
+	}, nil
+}
+
+func getBastionUrl() (*string, error) {
+	content, err := ioutil.ReadFile("/var/automate-ha/bastion_info.txt")
+	if err != nil {
+		log.Fatal("Error occurred while reading file: ", err)
+		return nil, err
+	}
+	url := "http://" + strings.TrimSpace(string(content))
+	return &url, nil
 }
