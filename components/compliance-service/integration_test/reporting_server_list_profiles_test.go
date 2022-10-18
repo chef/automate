aa6b0ed1c0206475572ed134680ab8c614d2c152
dmaddu <dmaddu@progress.com>
2022-10-18 19:13:18 +0530

    Serving Enhanced Reporting APIs based on configuration
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>

--- components/compliance-service/integration_test/reporting_server_list_profiles_test.go
+++ components/compliance-service/integration_test/reporting_server_list_profiles_test.go
@@ -19,7 +19,7 @@ func TestListProfiles(t *testing.T) {
 	suite.DeleteAllDocuments()
 
 	es2Backend := relaxting.ES2Backend{ESUrl: opensearchUrl}
-	server := reportingServer.New(&es2Backend, nil, 5, nil)
+	server := reportingServer.New(&es2Backend, nil, 5, nil, false)
 
 	reportFileName := "../ingest/examples/compliance-success-tiny-report.json"
 	everythingCtx := contextWithProjects([]string{authzConstants.AllProjectsExternalID})
--- components/compliance-service/integration_test/reporting_server_read_node_test.go
+++ components/compliance-service/integration_test/reporting_server_read_node_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestReadNode(t *testing.T) {
-	server := reportingServer.New(&relaxting.ES2Backend{ESUrl: opensearchUrl}, nil, 5, nil)
+	server := reportingServer.New(&relaxting.ES2Backend{ESUrl: opensearchUrl}, nil, 5, nil, false)
 
 	unassignedNodeId := newUUID()
 	assignedNodeId := newUUID()
