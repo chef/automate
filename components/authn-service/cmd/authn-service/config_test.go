2137d1afec704ac04ae9b51119b72b8899df93d8
“SanjuPal01” <sanju.sanju@progress.com>
2022-10-20 15:10:56 +0530

    Check 2
    Signed-off-by: “SanjuPal01” <sanju.sanju@progress.com>

--- components/authn-service/cmd/authn-service/config_test.go
+++ components/authn-service/cmd/authn-service/config_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/ghodss/yaml"
@@ -20,7 +19,6 @@ var _ = yaml.YAMLToJSON
 
 func TestUnmarshalConfig(t *testing.T) {
 	tokenID := uuid.Must(uuid.NewV4()).String()
-	fmt.Println(tokenID)
 	tests := map[string]struct {
 		rawConfig      []byte
 		expectedConfig Config
