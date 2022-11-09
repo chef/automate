b0499618b2df3b2b083f6cd525444dba138a6618
Sathvikreddy14 <creddy@progress.com>
2022-11-09 18:08:13 +0530

    cluster ctl pipeline
    Signed-off-by: Sathvikreddy14 <creddy@progress.com>

--- components/automate-cluster-ctl/spec/cluster/helpers_spec.rb
+++ components/automate-cluster-ctl/spec/cluster/helpers_spec.rb
@@ -43,7 +43,10 @@ describe AutomateCluster::Helpers do
       shellout = instance_double("Mixlib::ShellOut")
       expect(shellout).to receive(:stdout).and_return('Test Output')
 
-      expect(instance).to receive(:shellout!).with('make test', cwd: '/test/config_workspace').and_return(shellout)
+      opts = {
+        cwd: '/test/config_workspace'
+      }
+      expect(instance).to receive(:shellout!).with('make test', opts).and_return(shellout)
 
       instance.run_make_cmd('test')
     end
