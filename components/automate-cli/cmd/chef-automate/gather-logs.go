b9e30a1ef10270a5c3d2adca1942493a5d42b077
SahithiMy <sahithi.mylangam@progress.com>
2022-11-11 10:58:31 +0530

    Change for frontendnodes
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>

--- components/automate-cli/cmd/chef-automate/gather-logs.go
+++ components/automate-cli/cmd/chef-automate/gather-logs.go
@@ -638,13 +638,14 @@ func init() {
 func gatherLogsForFaultyNodes(outfileOverride string, logLines uint64) error {
 	_, err := client.Connection(client.DefaultClientTimeout)
 
-	if status.ErrorType(err) == "DeploymentServiceCallError" || status.ErrorType(err) == "DeploymentServiceUnreachableError" {
-		return runGatherLogsLocalCmd(outfileOverride, logLines)
-
-	}
 	cmd, err := exec.Command("hab", "svc", "status").CombinedOutput()
 	if err != nil {
 		return status.WithRecovery(errors.Wrapf(err, string(cmd)), "hab-sup is not working")
+	}
+
+	if status.ErrorType(err) == "DeploymentServiceCallError" || status.ErrorType(err) == "DeploymentServiceUnreachableError" {
+		return runGatherLogsLocalCmd(outfileOverride, logLines)
+
 	}
 
 	return gatherLogsFromServer(outfileOverride, logLines)
