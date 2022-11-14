171a9d9a138dfcedfbfb0a11d146e3d8094399aa
Sathvikreddy14 <creddy@progress.com>
2022-11-14 15:46:59 +0530

    Proxy Pipeline fix
    Signed-off-by: Sathvikreddy14 <creddy@progress.com>

--- integration/docker-squid/entrypoint.sh
+++ integration/docker-squid/entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 create_log_dir() {
-  mkdir -p ${SQUID_LOG_DIR}
-  chmod -R 755 ${SQUID_LOG_DIR}
-  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_LOG_DIR}
+  mkdir -p "${SQUID_LOG_DIR}"
+  chmod -R 755 "${SQUID_LOG_DIR}"
+  chown -R "${SQUID_USER}":"${SQUID_USER}" "${SQUID_LOG_DIR}"
 }
 
 create_cache_dir() {
-  mkdir -p ${SQUID_CACHE_DIR}
-  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
+  mkdir -p "${SQUID_CACHE_DIR}"
+  chown -R "${SQUID_USER}":"${SQUID_USER}" "${SQUID_CACHE_DIR}"
 }
 
 create_log_dir
@@ -17,10 +17,10 @@ create_cache_dir
 
 # Allow arguments to be passed to squid
 if [[ ${1:0:1} = '-' ]]; then
-  EXTRA_ARGS="$@"
+  EXTRA_ARGS="$*"
   set --
 elif [[ ${1} == squid || ${1} == $(which squid) ]]; then
-  EXTRA_ARGS="${@:2}"
+  EXTRA_ARGS="${*:2}"
   set --
 fi
 
@@ -31,7 +31,7 @@ if [[ -z ${1} ]]; then
     $(which squid) -N -f /etc/squid/squid.conf -z
   fi
   echo "Starting squid..."
-  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 "${EXTRA_ARGS}"
 else
   exec "$@"
 fi
