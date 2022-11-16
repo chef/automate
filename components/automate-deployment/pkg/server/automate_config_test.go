97d39789699c034446471d63a3e8baac18b686c9
SahithiMy <sahithi.mylangam@progress.com>
2022-11-16 16:46:00 +0530

    Added test cases
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>

--- components/automate-deployment/pkg/server/automate_config_test.go
+++ components/automate-deployment/pkg/server/automate_config_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/chef/automate/api/config/deployment"
 	"github.com/chef/automate/api/config/shared"
 	api "github.com/chef/automate/api/interservice/deployment"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
@@ -180,4 +181,114 @@ func TestUpdateByMergingStructs(t *testing.T) {
 			require.Equal(t, got.Config.Global.V1.Log.MaxNumberRotatedLogs.Value, tt.want.Config.Global.V1.Log.MaxNumberRotatedLogs.Value)
 		})
 	}
+}
+
+func TestSetConfigForRedirectLogs(t *testing.T) {
+	type args struct {
+		req          *api.PatchAutomateConfigRequest
+		existingCopy *deployment.AutomateConfig
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "RedirectSysLog is true for patch and existing",
+			args: args{
+				req: &api.PatchAutomateConfigRequest{
+					Config: &deployment.AutomateConfig{
+						Global: &shared.GlobalConfig{
+							V1: &shared.V1{
+								Log: &shared.Log{
+									RedirectSysLog:       &wrapperspb.BoolValue{Value: true},
+									RedirectLogFilePath:  &wrapperspb.StringValue{Value: filePath2},
+									CompressRotatedLogs:  &wrapperspb.BoolValue{Value: true},
+									MaxSizeRotateLogs:    &wrapperspb.StringValue{Value: "10"},
+									MaxNumberRotatedLogs: &wrapperspb.Int32Value{Value: 3},
+								},
+							},
+						},
+					},
+				},
+				existingCopy: &deployment.AutomateConfig{
+					Global: &shared.GlobalConfig{
+						V1: &shared.V1{
+							Log: &shared.Log{
+								RedirectSysLog: &wrapperspb.BoolValue{Value: true},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "If merge result and existing are equal",
+			args: args{
+				req: &api.PatchAutomateConfigRequest{
+					Config: &deployment.AutomateConfig{
+						Global: &shared.GlobalConfig{
+							V1: &shared.V1{
+								Log: &shared.Log{
+									RedirectSysLog: &wrapperspb.BoolValue{Value: true},
+								},
+							},
+						},
+					},
+				},
+				existingCopy: &deployment.AutomateConfig{
+					Global: &shared.GlobalConfig{
+						V1: &shared.V1{
+							Log: &shared.Log{
+								RedirectSysLog:       &wrapperspb.BoolValue{Value: true},
+								RedirectLogFilePath:  &wrapperspb.StringValue{Value: filePath1},
+								CompressRotatedLogs:  &wrapperspb.BoolValue{Value: true},
+								MaxSizeRotateLogs:    &wrapperspb.StringValue{Value: "30M"},
+								MaxNumberRotatedLogs: &wrapperspb.Int32Value{Value: 11},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "If requested RedirectLogFilePath is same as existing",
+			args: args{
+				req: &api.PatchAutomateConfigRequest{
+					Config: &deployment.AutomateConfig{
+						Global: &shared.GlobalConfig{
+							V1: &shared.V1{
+								Log: &shared.Log{
+									RedirectSysLog:      &wrapperspb.BoolValue{Value: true},
+									RedirectLogFilePath: &wrapperspb.StringValue{Value: filePath1},
+								},
+							},
+						},
+					},
+				},
+				existingCopy: &deployment.AutomateConfig{
+					Global: &shared.GlobalConfig{
+						V1: &shared.V1{
+							Log: &shared.Log{
+								RedirectSysLog:      &wrapperspb.BoolValue{Value: true},
+								RedirectLogFilePath: &wrapperspb.StringValue{Value: filePath1},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	{
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				err := setConfigForRedirectLogs(tt.args.req, tt.args.existingCopy)
+				if err == nil {
+					assert.NoError(t, err)
+				} else {
+					assert.Error(t, err)
+				}
+			})
+		}
+	}
+
 }
