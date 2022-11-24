ba6edd1c09d6aa1e6034beee79a1b8616722a1a6
Pappu Kumar <pappu.kumar@progress.com>
2022-11-24 21:47:30 +0530

    Done configuration changes one config key added
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- api/config/bifrost/config_request.go
+++ api/config/bifrost/config_request.go
@@ -30,12 +30,14 @@ func DefaultConfigRequest() *ConfigRequest {
 	c.V1.Sys.Log.RotationMaxBytes = w.Int64(104857600)
 	c.V1.Sys.Log.RotationMaxFiles = w.Int32(10)
 	c.V1.Sys.Log.MaxErrorLogsPerSecond = w.Int32(1000)
+	c.V1.Sys.Log.ExtendedPerfLog = w.Bool(true)
 
 	c.V1.Sys.Sql.Timeout = w.Int32(5000)
 	c.V1.Sys.Sql.PoolInitSize = w.Int32(10)
 	c.V1.Sys.Sql.PoolMaxSize = w.Int32(20)
 	c.V1.Sys.Sql.PoolQueueMax = w.Int32(50)
 	c.V1.Sys.Sql.PoolQueueTimeout = w.Int32(2000)
+
 	return c
 }
 
