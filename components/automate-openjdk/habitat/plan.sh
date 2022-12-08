413ce07f767ab049cf6ea8e3b5ecb4ba6ceb27b7
Durga Sarat Chandra Maddu <44021874+Dmaddu@users.noreply.github.com>
2022-12-08 12:39:17 +0530

    upgrading openJDK to 11.0.17 (#7605)
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>

--- components/automate-openjdk/habitat/plan.sh
+++ components/automate-openjdk/habitat/plan.sh
@@ -9,10 +9,10 @@ pkg_origin=chef
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_name=automate-openjdk
 # Release archive https://jdk.java.net/archive/
-pkg_version=11.0.15+10
-pkg_source=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.15%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz
-pkg_shasum=5fdb4d5a1662f0cca73fec30f99e67662350b1fa61460fa72e91eb9f66b54d0b
-pkg_filename=OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz
+pkg_version=11.0.17+8
+pkg_source=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.17%2B8/OpenJDK11U-jdk_x64_linux_hotspot_11.0.17_8.tar.gz
+pkg_shasum=b8d46ed08ef4859476fe6421a7690d899ed83dce63f13fd894f994043177ef3c
+pkg_filename=OpenJDK11U-jdk_x64_linux_hotspot_11.0.17_8.tar.gz
 pkg_dirname="jdk-${pkg_version}"
 pkg_license=("GPL-2.0-with-classpath-exception")
 pkg_description=('Eclipse Temurin is the open source Java SE build based upon OpenJDK.')
