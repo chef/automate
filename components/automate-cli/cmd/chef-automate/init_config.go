0f14265ca9705cc284a87045daac6920ae872321
Yashvi Jain <yashvi.jain@progress.com>
2022-12-22 15:20:00 +0530

    Adding yaml changes
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>

--- components/automate-cli/cmd/chef-automate/init_config.go
+++ components/automate-cli/cmd/chef-automate/init_config.go
@@ -82,7 +82,7 @@ var initConfigCmd = &cobra.Command{
 	Long:  "Initialize default configuration and save it to a file.",
 	Annotations: map[string]string{
 		NoCheckVersionAnnotation: NoCheckVersionAnnotation,
-		Compatiblity:             fmt.Sprintf("%s,%s", CompatiblewithHA, CompatiblewithStandalone),
+		Compatiblity:             "compatible",
 	},
 	RunE: runInitConfigCmd,
 }
