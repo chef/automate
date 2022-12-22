3efea35dbb07d3e574ea6ddaa8a8ba48a3e6fb93
Yashvi Jain <yashvi.jain@progress.com>
2022-12-22 10:32:29 +0530

    Adding integration test cases changes
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>

--- components/infra-proxy-service/integration_test/clients_test.go
+++ components/infra-proxy-service/integration_test/clients_test.go
@@ -94,7 +94,6 @@ func TestGetClients(t *testing.T) {
 	t.Run("Clients list with an invalid query search param", func(t *testing.T) {
 		req.SearchQuery = &request.SearchQuery{
 			Q:       "NO_KEY:NO_VALUE",
-			Page:    0,
 			PerPage: 5,
 		}
 		res, err := infraProxy.GetClients(ctx, req)
@@ -107,11 +106,14 @@ func TestGetClients(t *testing.T) {
 
 	t.Run("Clients list with an invalid query search param", func(t *testing.T) {
 		req.SearchQuery = &request.SearchQuery{
-			Q:       "INVALID_QUERY",
+			Q:       "INVALID_QUERY:INVALID_QUERY",
 			Page:    0,
 			PerPage: 5,
 		}
 		res, err := infraProxy.GetClients(ctx, req)
+		if err != nil {
+			fmt.Println("error we got is err", err)
+		}
 		require.NoError(t, err)
 		assert.Equal(t, 0, int(res.Page))
 		assert.Equal(t, 0, int(res.Total))
