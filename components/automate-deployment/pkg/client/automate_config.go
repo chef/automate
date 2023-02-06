eb27dced55632b1b11d92545ef8afa98cd6a9827
Sahiba3108 <sgoyal@progress.com>
2023-02-06 11:40:01 +0530

    reconfiguring opensearch backup location
    Signed-off-by: Sahiba3108 <sgoyal@progress.com>

--- components/automate-deployment/pkg/client/automate_config.go
+++ components/automate-deployment/pkg/client/automate_config.go
@@ -3,7 +3,9 @@ package client
 import (
 	"context"
 	"fmt"
+	"os/exec"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -13,6 +15,25 @@ import (
 	"github.com/chef/automate/components/automate-cli/pkg/status"
 	"github.com/chef/automate/components/automate-deployment/pkg/cli"
 )
+
+var backupPathFix = `sleep 5
+chef-automate status --wait-for-healthy > /dev/null
+indices=(
+chef-automate-es6-automate-cs-oc-erchef
+chef-automate-es6-compliance-service
+chef-automate-es6-event-feed-service
+chef-automate-es6-ingest-service
+)
+for index in ${indices[@]}; do
+curl -XPUT -k -H 'Content-Type: application/json' http://localhost:10144/_snapshot/$index --data-binary @- << EOF
+{
+	"type" : "fs",
+	"settings" : {
+	"location" : "%s/automate-elasticsearch-data/$index"
+	}
+}
+EOF
+done`
 
 // GetAutomateConfig makes a gRPC request to the server and returns the
 // userOverrideConfig.
@@ -75,6 +96,19 @@ func PatchAutomateConfig(timeout int64, config *dc.AutomateConfig, writer cli.Fo
 	h := configEventHandler{Writer: writer}
 	if err := con.StreamDeployEvents(patchRes.TaskId, &api.DeploymentID{}, &h); err != nil {
 		return status.Wrap(err, status.DeploymentServiceCallError, "Failed to stream configuration events")
+	}
+
+	if config.Global != nil && config.Global.String() != "" {
+		if config.Global.V1 != nil && config.Global.V1.Backups != nil && config.Global.V1.Backups.Filesystem != nil {
+			if config.Global.V1.Backups.Filesystem.Path.Value != "" {
+				fmt.Println("Waiting till all the services comes in healthy state...")
+				backupScript := fmt.Sprintf(backupPathFix, strings.TrimSuffix(config.Global.V1.Backups.Filesystem.Path.Value, "/"))
+				_, err := exec.Command("/bin/sh", "-c", backupScript).Output()
+				if err != nil {
+					return err
+				}
+			}
+		}
 	}
 
 	return nil
