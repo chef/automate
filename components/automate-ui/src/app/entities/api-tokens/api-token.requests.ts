1d44b5c3159cc0057d05ad3ada98e77d01ab97e3
Atul Krishna <Atul.Krishna@progress.com>
2023-02-10 18:48:38 +0530

    Added project as payload for toggle
    Signed-off-by: Atul Krishna <Atul.Krishna@progress.com>

--- components/automate-ui/src/app/entities/api-tokens/api-token.requests.ts
+++ components/automate-ui/src/app/entities/api-tokens/api-token.requests.ts
@@ -39,9 +39,9 @@ export class ApiTokenRequests {
 
   // Note: toggleActive takes the EXISTING value in `active`, and will take care
   // of flipping it.
-  public toggleActive(id: string, name: string, active: boolean): Observable<TokenPayloadResponse> {
+  public toggleActive(id: string, name: string, active: boolean, projects: string[]): Observable<TokenPayloadResponse> {
     return this.http.put<TokenPayloadResponse>(`${env.iam_url}/tokens/${id}`,
-      { name, active: !active });
+      { name, active: !active,projects: projects});
   }
 
   public delete(id: string): Observable<Object> {
