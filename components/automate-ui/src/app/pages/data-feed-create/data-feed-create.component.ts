572a29cc7cbd5d18e4409858e6aeb848dee1bf3a
prasad927 <Prasad.Chaudhari@progress.com>
2023-02-22 19:59:42 +0530

    fixed s3 invalid inputs error message
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>

--- components/automate-ui/src/app/pages/data-feed-create/data-feed-create.component.ts
+++ components/automate-ui/src/app/pages/data-feed-create/data-feed-create.component.ts
@@ -118,8 +118,9 @@ export class DataFeedCreateComponent {
   set testErrorSetter(val: boolean) {
     let errorString: string;
     if (this.integTitle === StorageIntegrationTypes.MINIO) {
-      errorString =
-        'Unable to connect: check endpoint, bucket name, access key and secret key.';
+      errorString = 'Unable to connect: check endpoint, bucket name, access key and secret key.';
+    } else if(this.integTitle===StorageIntegrationTypes.AMAZON_S3){
+      errorString = 'Unable to connect : check region, bucket name, access key and secret key.';
     } else if (this.authSelected === AuthTypes.USERNAMEANDPASSWORD) {
       errorString = 'Unable to connect: check URL, username and password.';
     } else if (this.authSelected === AuthTypes.ACCESSTOKEN) {
