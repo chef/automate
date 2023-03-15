ed496dd9175bf6808fb03e443a9c09d369f8ed47
ayush-billore <ayush.billore@progress.com>
2023-03-15 13:08:49 +0530

    replaced plan.sh of oc-id with plan.sh of erchef component
    Signed-off-by: ayush-billore <ayush.billore@progress.com>

--- components/automate-cs-oc-id/habitat/plan.sh
+++ components/automate-cs-oc-id/habitat/plan.sh
@@ -1,8 +1,10 @@
-#shellcheck disable=SC2148
+#shellcheck disable=SC2034
+#shellcheck disable=SC2039
+#shellcheck disable=SC2154
+#stable channel
 
-UPSTREAM_PKG_IDENT="chef/automate-cs-oc-id"
-pkg_name="automate-cs-oc-id"
-pkg_description="Wrapper package for chef/oc_id"
+pkg_name="automate-cs-oc-id-test"
+pkg_description="Wrapper package for chef/oc_id_test"
 pkg_origin="chef"
 # WARNING: Version managed by .expeditor/update_chef_server.sh
 pkg_version="15.4.0"
@@ -10,32 +12,49 @@ vendor_origin="chef"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
 pkg_upstream_url="https://www.chef.io/automate"
-pkg_build_deps=("${UPSTREAM_PKG_IDENT}")
-pkg_svc_user=root
-pkg_svc_group=root
 pkg_deps=(
+  #core/runit/2.1.2/20200403133549
+  core/runit
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
   "${vendor_origin}/oc_id/15.4.0/20230105061030"
 )
 
-do_before() {
-  update_pkg_version
-}
+pkg_build_deps=(
+  core/gcc
+)
 
-do_download() {
- return 0
-}
+pkg_binds=(
+  [automate-cs-oc-bifrost]="http-port"
+  [automate-es-gateway]="http-port"
+  [automate-cs-bookshelf]="http-port"
+  [automate-pg-gateway]="port"
+  [pg-sidecar-service]="port"
+)
 
-do_build() {
- return 0
-}
+pkg_binds_optional=(
+  [automate-gateway]="port"
+)
 
-do_install() {
- return 0
-}
+pkg_exports=(
+  [http-host]=network.host
+  [http-port]=network.port
+)
 
-do_end() {
- return 0
-}
+pkg_exposes=(http-port)
+
+pkg_bin_dirs=(bin)
+
+pkg_scaffolding="${local_scaffolding_origin:-chef}/automate-scaffolding-go"
+automate_scaffolding_include_templates=(sqerl.config)
+
+scaffolding_go_base_path=github.com/chef
+scaffolding_go_repo_name=automate
+scaffolding_go_import_path="${scaffolding_go_base_path}/${scaffolding_go_repo_name}/components/${pkg_name}"
+scaffolding_go_binary_list=(
+  "${scaffolding_go_import_path}/cmd/init-es"
+)
+
+chef_automate_hab_binding_mode="relaxed"
+
