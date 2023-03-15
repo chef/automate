ac9c4178e8bb0b46f21c6dd86d0a451d53a2a00b
ayush-billore <ayush.billore@progress.com>
2023-03-15 14:00:10 +0530

    follow bifrosh plan.sh in oc-id
    OCID is Building successfully

--- components/automate-cs-oc-id/habitat/plan.sh
+++ components/automate-cs-oc-id/habitat/plan.sh
@@ -3,8 +3,8 @@
 #shellcheck disable=SC2154
 #stable channel
 
-pkg_name="automate-cs-oc-id-test"
-pkg_description="Wrapper package for chef/oc_id_test"
+pkg_name="automate-cs-oc-id"
+pkg_description="Wrapper package for chef/oc_id"
 pkg_origin="chef"
 # WARNING: Version managed by .expeditor/update_chef_server.sh
 pkg_version="15.4.0"
@@ -13,30 +13,16 @@ pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
 pkg_upstream_url="https://www.chef.io/automate"
 pkg_deps=(
-  #core/runit/2.1.2/20200403133549
-  core/runit
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
   "${vendor_origin}/oc_id/15.4.0/20230105061030"
 )
 
-pkg_build_deps=(
-  core/gcc
-)
-
 pkg_binds=(
-  [automate-cs-oc-bifrost]="http-port"
-  [automate-es-gateway]="http-port"
-  [automate-cs-bookshelf]="http-port"
   [automate-pg-gateway]="port"
   [pg-sidecar-service]="port"
 )
-
-pkg_binds_optional=(
-  [automate-gateway]="port"
-)
-
 pkg_exports=(
   [http-host]=network.host
   [http-port]=network.port
@@ -44,17 +30,19 @@ pkg_exports=(
 
 pkg_exposes=(http-port)
 
-pkg_bin_dirs=(bin)
-
-pkg_scaffolding="${local_scaffolding_origin:-chef}/automate-scaffolding-go"
+pkg_scaffolding="${local_scaffolding_origin:-chef}/automate-scaffolding"
 automate_scaffolding_include_templates=(sqerl.config)
 
-scaffolding_go_base_path=github.com/chef
-scaffolding_go_repo_name=automate
-scaffolding_go_import_path="${scaffolding_go_base_path}/${scaffolding_go_repo_name}/components/${pkg_name}"
-scaffolding_go_binary_list=(
-  "${scaffolding_go_import_path}/cmd/init-es"
-)
+do_download() {
+  return 0
+}
 
-chef_automate_hab_binding_mode="relaxed"
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
+
 
