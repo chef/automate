65126461407045b43c77f5f7d4d53156dfb7cf46
ragarwal <ragarwal@progress.com>
2023-05-02 17:50:30 +0530

    [CHEF-2602]: Added trigger method for hardware resource
    Signed-off-by: ragarwal <ragarwal@progress.com>

--- components/automate-cli/pkg/verifyserver/server/server.go
+++ components/automate-cli/pkg/verifyserver/server/server.go
@@ -7,6 +7,7 @@ import (
 	v1 "github.com/chef/automate/components/automate-cli/pkg/verifyserver/server/api/v1"
 	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/services/batchcheckservice"
 	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/services/statusservice"
+	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/trigger"
 	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/utils/fiberutils"
 	"github.com/chef/automate/lib/logger"
 	"github.com/gofiber/cors"
@@ -43,7 +44,13 @@ func NewVerifyServer(port string, debug bool) (*VerifyServer, error) {
 	}
 	app := fiber.New(fconf)
 	handler := v1.NewHandler(l).
-		AddStatusService(statusservice.NewStatusService()).AddBatchCheckService(batchcheckservice.NewBatchCheckService())
+		AddStatusService(
+			statusservice.NewStatusService()).
+		AddBatchCheckService(
+			batchcheckservice.NewBatchCheckService().
+				AddHardwareResourceCountCheckTrigger(
+					trigger.NewHardwareResourceCountCheck(),
+				))
 	vs := &VerifyServer{
 		Port:    port,
 		Log:     l,
--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/batchcheckservice.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/batchcheckservice.go
@@ -4,20 +4,28 @@ import (
 	"fmt"
 
 	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
+	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/trigger"
 )
 
 type IBatchCheckService interface {
-	BatchCheck(checks []string, config models.Config)
+	BatchCheck(checks []string, config models.Config) bool
+	AddHardwareResourceCountCheckTrigger(h trigger.IHardwareResourceCountCheckTrigger) *BatchCheckService
 }
 
 type BatchCheckService struct {
+	hardwareResourceCountCheckTrigger trigger.IHardwareResourceCountCheckTrigger
 }
 
 func NewBatchCheckService() IBatchCheckService {
 	return &BatchCheckService{}
 }
 
-func (ss *BatchCheckService) BatchCheck(checks []string, config models.Config) {
+func (ss *BatchCheckService) AddHardwareResourceCountCheckTrigger(h trigger.IHardwareResourceCountCheckTrigger)  *BatchCheckService {
+	ss.hardwareResourceCountCheckTrigger = h
+	return ss
+}
+
+func (ss *BatchCheckService) BatchCheck(checks []string, config models.Config) bool{
 	bastionCheckResultChan := make(chan bool, 3)
 	go ss.hardwareResourceCountCheck(config, bastionCheckResultChan)
 	go ss.sshUserAccessCheck(config, bastionCheckResultChan)
@@ -27,13 +35,16 @@ func (ss *BatchCheckService) BatchCheck(checks []string, config models.Config) {
 		fmt.Println(result)
 	}
 	defer close(bastionCheckResultChan)
+	return true
 }
 
 func (ss *BatchCheckService) hardwareResourceCountCheck(config models.Config, resultChan chan bool) {
+	ss.hardwareResourceCountCheckTrigger.Run()
 	resultChan <- true
 }
 
 func (ss *BatchCheckService) sshUserAccessCheck(config models.Config, resultChan chan bool) {
+	
 	resultChan <- false
 }
 
