f9897c3d92dfbed47bc95a5f23a06b519ab3a140
Vivek Shankar <vshankar@progress.com>
2023-05-02 12:21:52 +0530

    added readme
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- components/automate-cli/pkg/verifyserver/services/statusservice/statusservice_test.go
+++ components/automate-cli/pkg/verifyserver/services/statusservice/statusservice_test.go
@@ -1,14 +1,15 @@
-package statusservice
+package statusservice_test
 
 import (
 	"testing"
 
 	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
+	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/services/statusservice"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStatusService(t *testing.T) {
-	ss := NewStatusService()
+	ss := statusservice.NewStatusService()
 	services := ss.GetServices()
 	assert.Equal(t, []models.ServiceDetails{}, services)
 }
