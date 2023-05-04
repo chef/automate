4bef7cf31797b8ee7d8953c5d4d525aa0aff9c13
Shanmugapriya <shramana@progress.com>
2023-05-04 11:10:09 +0530

    Updated unit tests
    Signed-off-by: Shanmugapriya <shramana@progress.com>

--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/hardwareresourcecountchecktrigger.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/hardwareresourcecountchecktrigger.go
@@ -7,28 +7,33 @@ import (
 	"net/http"
 
 	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
-	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/utils/restutils"
 )
 
 //HardwareResourceCountCheck - Calls the hardware-resource-count API and collates the response
-func (hrc *CheckTrigger) HardwareResourceCountCheck(config models.Config) (models.CheckTriggerResponse, error) {
+func (hrc *CheckTrigger) HardwareResourceCountCheck(config models.Config) (*models.CheckTriggerResponse, error) {
 	hrc.Logger.Info("Performing Hardware resource count check from batch Check API")
-	resp, err := restutils.Post("https://localhost/api/v1/checks/hardware-resource-count", config.Hardware)
+	reqUrl := "https://localhost/api/v1/checks/hardware-resource-count"
+	return hrc.TriggerHardwareResourceCountCheck(reqUrl, config.Hardware)
+
+}
+
+func (hrc *CheckTrigger) TriggerHardwareResourceCountCheck(url string, body interface{}) (*models.CheckTriggerResponse, error) {
+	resp, err := Post(url, body)
 	if err != nil {
 		hrc.Logger.Error("Error while Performing Hardware resource count check from batch Check API", err)
-		return models.CheckTriggerResponse{}, err
+		return &models.CheckTriggerResponse{}, err
 	}
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		hrc.Logger.Error("Error while reading response of Hardware resource count check from batch Check API", err)
-		return models.CheckTriggerResponse{}, err
+		return &models.CheckTriggerResponse{}, err
 	}
 	response := models.CheckTriggerResponse{}
 	json.Unmarshal(respBody, &response)
-	return response, nil
+	return &response, nil
 }
 
-func post(url string, body interface{}) (*http.Response, error) {
+func Post(url string, body interface{}) (*http.Response, error) {
 	requestBody, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
