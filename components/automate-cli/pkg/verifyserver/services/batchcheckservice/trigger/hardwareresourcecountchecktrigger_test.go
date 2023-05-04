f670ecada11615b6a9ae80d4bff2b150a7e70115
Shanmugapriya <shramana@progress.com>
2023-05-04 14:22:36 +0530

    Added test
    Signed-off-by: Shanmugapriya <shramana@progress.com>

--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/hardwareresourcecountchecktrigger_test.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/hardwareresourcecountchecktrigger_test.go
@@ -3,6 +3,7 @@ package trigger
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -288,4 +289,49 @@ func TestCheckTrigger_TriggerHardwareResourceCountCheck(t *testing.T) {
 		}
 	})
 
+}
+
+func TestPost(t *testing.T) {
+	// Create a test server to receive requests
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// Check that the request method is POST
+		if req.Method != http.MethodPost {
+			t.Errorf("Unexpected request method. Expected %v, got %v", http.MethodPost, req.Method)
+		}
+		// Check that the request body is correct
+		expectedBody := "{\"org\":\"chef\"}"
+		requestBody, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			t.Errorf("Unexpected error reading request body: %v", err)
+		}
+		if string(requestBody) != expectedBody {
+			t.Errorf("Unexpected request body. Expected %v, got %v", expectedBody, string(requestBody))
+		}
+		// Write a response to the client
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte("{\"status\":\"OK\"}"))
+	}))
+	defer server.Close()
+
+	// Call the Post function with a test URL and body
+	url := server.URL
+	body := map[string]string{"org": "chef"}
+	resp, err := Post(url, body)
+	if err != nil {
+		t.Errorf("Unexpected error from Post function: %v", err)
+	}
+
+	// Check that the response status code is correct
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected response status code. Expected %v, got %v", http.StatusOK, resp.StatusCode)
+	}
+	// Check that the response body is correct
+	expectedResponse := "{\"status\":\"OK\"}"
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Unexpected error reading response body: %v", err)
+	}
+	if string(responseBody) != expectedResponse {
+		t.Errorf("Unexpected response body. Expected %v, got %v", expectedResponse, string(responseBody))
+	}
 }
