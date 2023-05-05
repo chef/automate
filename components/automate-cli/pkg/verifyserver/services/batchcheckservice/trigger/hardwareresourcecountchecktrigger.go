4ea5d712994ece678d1d84384c8ef0a8b4918040
Shanmugapriya <shramana@progress.com>
2023-05-05 11:16:41 +0530

    CHEF-2869 : Hardware resurce Count check
    Signed-off-by: Shanmugapriya <shramana@progress.com>

--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/hardwareresourcecountchecktrigger.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/hardwareresourcecountchecktrigger.go
@@ -1,15 +1,87 @@
 package trigger
 
-import "github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/constants"
+	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
+	"github.com/gofiber/fiber"
+	"github.com/sirupsen/logrus"
+)
 
 type HardwareResourceCountCheck struct {
+	log logrus.Logger
 }
 
 func NewHardwareResourceCountCheck() *HardwareResourceCountCheck {
-	return &HardwareResourceCountCheck{}
+	return &HardwareResourceCountCheck{log: *logrus.New()}
 }
 
 func (ss *HardwareResourceCountCheck) Run(config models.Config) map[string]models.CheckTriggerResponse {
-	m := map[string]models.CheckTriggerResponse{}
-	return m
+	ss.log.Info("Performing Hardware Resource count check from batch check ")
+	reqUrl := "https://localhost/api/v1/checks/hardware-resource-count"
+	resp, err := ss.TriggerHardwareResourceCountCheck(reqUrl, config.Hardware)
+
+	//This map will hold the Response against each IP
+	finalResult := make(map[string]models.CheckTriggerResponse)
+
+	for _, result := range resp.Result {
+		checkResponse := models.CheckTriggerResponse{}
+		outputResult := models.ApiResult{}
+		outputResult.Check = constants.HARDWARE_RESOURCE_COUNT
+		outputResult.Message = constants.HARDWARE_RESOURCE_COUNT_MSG
+
+		if err != nil {
+			outputResult.Passed = false
+			checkResponse.Error = fiber.NewError(fiber.StatusServiceUnavailable, err.Error())
+
+		} else {
+			outputResult.Checks = result.Checks
+			outputResult.Passed = true
+		}
+		checkResponse.Result = outputResult
+
+		finalResult[result.IP] = checkResponse
+	}
+	return finalResult
+}
+
+//TriggerHardwareResourceCountCheck - Call the Hardware resource API and format response
+func (ss *HardwareResourceCountCheck) TriggerHardwareResourceCountCheck(url string, body interface{}) (*models.HardwareResourceCheckResponse, error) {
+	resp, err := Post(url, body)
+	if err != nil {
+		ss.log.Error("Error while Performing Hardware resource count check from batch Check API", err)
+		return &models.HardwareResourceCheckResponse{}, err
+	}
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		ss.log.Error("Error while reading response of Hardware resource count check from batch Check API", err)
+		return &models.HardwareResourceCheckResponse{}, err
+	}
+	response := models.HardwareResourceCheckResponse{}
+	json.Unmarshal(respBody, &response)
+	return &response, nil
+}
+
+//Post - This function performs HTTP Post request to the given endpoint with the provided request body
+func Post(url string, body interface{}) (*http.Response, error) {
+	requestBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
---
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/hardwareresourcecountchecktrigger_test.go
@@ -0,0 +1,353 @@
+package trigger
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	hardwareCheckResp = `[
+		{
+		  "ip": "172.154.0.1",
+		  "node_type": "automate",
+		  "checks": [
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "IP address is unique",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "IP address is of valid format",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "Not shared with backend nodes",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "<Node_type> Type has valid count as per Automate HA requirement",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			}
+		  ]
+		},
+		{
+		  "ip": "172.154.0.3",
+		  "node_type": "chef-infra-server",
+		  "checks": [
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "IP address is unique",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "IP address is of valid format",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "Not shared with backend nodes",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "<Node_type> Type has valid count as per Automate HA requirement",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			}
+		  ]
+		},
+		{
+		  "ip": "172.154.0.5",
+		  "node_type": "postgresql",
+		  "checks": [
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "IP address is unique",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "IP address is of valid format",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "Not shared with backend nodes",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "<Node_type> Type has valid count as per Automate HA requirement",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			}
+		  ]
+		},
+		{
+		  "ip": "172.154.0.8",
+		  "node_type": "opensearch",
+		  "checks": [
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "IP address is unique",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "IP address is of valid format",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "Not shared with backend nodes",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			},
+			{
+			  "title": "IP address",
+			  "passed": true,
+			  "success_msg": "<Node_type> Type has valid count as per Automate HA requirement",
+			  "error_msg": "",
+			  "resolution_msg": ""
+			}
+		  ]
+		}
+	  ]`
+)
+
+func GetRequestJson() models.Config {
+	ipConfig := models.Config{}
+
+	json.Unmarshal([]byte(`{
+		"checks": [
+		  "hardware-resource-count",
+		  "certificate",
+		  "ssh-user"
+		],
+		"config": {
+		  "ssh_user": {
+			"user_name": "ubuntu",
+			"private_key": "----- BEGIN PRIVATE RSA -----",
+			"sudo_password": "test@123"
+		  },
+		  "arch": "existing_nodes",
+		  "backup": {
+			"file_system": {
+			  "mount_location": "/mnt/automate_backups"
+			}
+		  },
+		  "hardware": {
+			"automate_node_count": 2,
+			"automate_node_ips": [
+			  "172.154.0.1",
+			  "172.154.0.2"
+			],
+			"chef_infra_server_node_count": 2,
+			"chef_infra_server_node_ips": [
+			  "172.154.0.3",
+			  "172.154.0.4"
+			],
+			"postgresql_node_count": 3,
+			"postgresql_node_ips": [
+			  "172.154.0.5",
+			  "172.154.0.6",
+			  "172.154.0.7"
+			],
+			"opensearch_node_count": 3,
+			"opensearch_node_ips": [
+			  "172.154.0.8",
+			  "172.154.0.9",
+			  "172.154.0.10"
+			]
+		  }
+		}
+	  }`), &ipConfig)
+	return ipConfig
+}
+
+// mockTransport is a mock implementation of the http.RoundTripper interface
+type mockTransport struct{}
+
+// RoundTrip returns an error for every request
+func (m *mockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	return nil, fmt.Errorf("mock error")
+}
+
+func TestCheckTrigger_TriggerHardwareResourceCountCheck(t *testing.T) {
+	t.Run("cannot reach", func(t *testing.T) {
+		// create the CheckTrigger instance to be tested
+		hrc := NewHardwareResourceCountCheck()
+
+		// make the HTTP request to an invalid URL
+		resp, err := hrc.TriggerHardwareResourceCountCheck("invalid-url", GetRequestJson())
+
+		if err != nil && !strings.Contains(err.Error(), "unsupported protocol scheme") {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if resp != nil {
+			t.Errorf("unexpected response: %v", resp)
+		}
+	})
+
+	t.Run("Bad request", func(t *testing.T) {
+		// create the CheckTrigger instance to be tested
+		hrc := NewHardwareResourceCountCheck()
+
+		// call the function being tested with an endpoint that returns an error status code
+		mockServer2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadRequest)
+		}))
+		defer mockServer2.Close()
+
+		resp, err := hrc.TriggerHardwareResourceCountCheck(mockServer2.URL, GetRequestJson())
+
+		if resp != nil {
+			t.Errorf("unexpected result: %v", resp)
+		}
+		if err != nil {
+			assert.EqualError(t, err, fmt.Sprintf("error triggering the API %s: status code %d ", mockServer2.URL, http.StatusBadRequest))
+		}
+	})
+
+	t.Run("Invalid JSON", func(t *testing.T) {
+		hrc := NewHardwareResourceCountCheck()
+
+		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`invalid JSON`))
+		}))
+		defer mockServer.Close()
+
+		resp, err := hrc.TriggerHardwareResourceCountCheck(mockServer.URL, GetRequestJson())
+
+		if resp != nil {
+			t.Errorf("unexpected result: %v", resp)
+		}
+		if err != nil {
+			assert.EqualError(t, err, fmt.Sprintf("error decoding response from the API %s: invalid character 'i' looking for beginning of value ", mockServer.URL))
+		}
+	})
+
+	t.Run("Returns OK", func(t *testing.T) {
+
+		hrc := NewHardwareResourceCountCheck()
+
+		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(hardwareCheckResp))
+		}))
+		defer mockServer.Close()
+
+		// call the function being tested
+		resp, err := hrc.TriggerHardwareResourceCountCheck(mockServer.URL, GetRequestJson())
+
+		if resp != nil {
+			t.Logf("Success response : %v", resp)
+		}
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+	})
+
+}
+
+func TestPost(t *testing.T) {
+	// Create a test server to receive requests
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// Check that the request method is POST
+		if req.Method != http.MethodPost {
+			t.Errorf("Unexpected request method. Expected %v, got %v", http.MethodPost, req.Method)
+		}
+		// Check that the request body is correct
+		expectedBody := "{\"org\":\"chef\"}"
+		requestBody, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			t.Errorf("Unexpected error reading request body: %v", err)
+		}
+		if string(requestBody) != expectedBody {
+			t.Errorf("Unexpected request body. Expected %v, got %v", expectedBody, string(requestBody))
+		}
+		// Write a response to the client
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte("{\"status\":\"OK\"}"))
+	}))
+	defer server.Close()
+
+	// Call the Post function with a test URL and body
+	url := server.URL
+	body := map[string]string{"org": "chef"}
+	resp, err := Post(url, body)
+	if err != nil {
+		t.Errorf("Unexpected error from Post function: %v", err)
+	}
+
+	// Check that the response status code is correct
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected response status code. Expected %v, got %v", http.StatusOK, resp.StatusCode)
+	}
+	// Check that the response body is correct
+	expectedResponse := "{\"status\":\"OK\"}"
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Unexpected error reading response body: %v", err)
+	}
+	if string(responseBody) != expectedResponse {
+		t.Errorf("Unexpected response body. Expected %v, got %v", expectedResponse, string(responseBody))
+	}
+}
+
+func TestHardwareResourceCountCheck_Run(t *testing.T) {
+
+	t.Run("Returns OK", func(t *testing.T) {
+		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(hardwareCheckResp))
+		}))
+		defer mockServer.Close()
+
+		hrc := NewHardwareResourceCountCheck()
+		request := GetRequestJson()
+		mapStruct := hrc.Run(request)
+		totalIps := request.Hardware.AutomateNodeCount + request.Hardware.ChefInfraServerNodeCount + request.Hardware.PostgresqlNodeCount + request.Hardware.OpensearchNodeCount
+		assert.Equal(t, totalIps, len(mapStruct))
+	})
+}
