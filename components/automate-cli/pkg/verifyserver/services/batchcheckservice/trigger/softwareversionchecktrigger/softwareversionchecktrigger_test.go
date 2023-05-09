b7152471ccc49e9023f84da1b822b095162f7647
Pappu Kumar <pappu.kumar@progress.com>
2023-05-10 03:09:28 +0530

    Done
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/softwareversionchecktrigger/softwareversionchecktrigger_test.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/softwareversionchecktrigger/softwareversionchecktrigger_test.go
@@ -1,70 +1,119 @@
 package softwareversionchecktrigger
 
 import (
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/constants"
 	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
 	"github.com/chef/automate/lib/logger"
-	"github.com/gofiber/fiber"
 	"github.com/stretchr/testify/require"
 )
 
 const (
 	softwareVersionResp = `{
-       "status": "success",
-       "result": {
-         "passed": true,
-         "msg": "API result message",
-         "check": "API check",
-         "checks": [
-           {
-             "title": "Title 1",
-             "passed": true,
-             "success_msg": "Title 1 passed",
-             "error_msg": "",
-             "resolution_msg": "not required"
-           },
-           {
-             "title": "Title 2",
-             "passed": false,
-             "success_msg": "",
-             "error_msg": "Title 2 failed",
-             "resolution_msg": "Check the log or conf"
-           }
-         ]
-       },
-       "host": ""
-     }`
+		"status": "success",
+		"result": {
+			"passed": true,
+			"msg": "API result message",
+			"check": "API check",
+			"checks": [
+				{
+					"title": "Check 1",
+					"passed": true,
+					"success_msg": "Check 1 passed",
+					"error_msg": "",
+					"resolution_msg": "No resolution required"
+				},
+				{
+					"title": "Check 2",
+					"passed": false,
+					"success_msg": "",
+					"error_msg": "Check 2 failed",
+					"resolution_msg": "Please check the configuration"
+				}
+			]
+		},
+		"host": ""
+	}`
 )
 
-func TestSystemUserCheck_Run(t *testing.T) {
-	t.Run("System User Check", func(t *testing.T) {
-		// Create a test server to mock the API endpoint
-		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Define the response based on the test case
-			w.WriteHeader(http.StatusOK)
-			err := json.NewEncoder(w).Encode(json.RawMessage(softwareVersionResp))
-			require.NoError(t, err)
-		}))
+func TestSoftwareVersionCheck_Run(t *testing.T) {
+	t.Run("Software Version Check", func(t *testing.T) {
+		// Create a dummy server
+		server, host, port := createDummyServer(http.StatusOK)
 		defer server.Close()
 
+		// Test data
 		config := models.Config{
 			Hardware: models.Hardware{
-				AutomateNodeCount:   1,
-				AutomateNodeIps:     []string{server.URL},
-				PostgresqlNodeCount: 1,
-				PostgresqlNodeIps:   []string{"127.0.0.1"},
+				AutomateNodeCount: 1,
+				AutomateNodeIps:   []string{host},
 			},
 		}
 
-		suc := NewSoftwareVersionCheck(logger.NewLogrusStandardLogger(), "8080")
+		suc := NewSoftwareVersionCheck(logger.NewLogrusStandardLogger(), port)
 		ctr := suc.Run(config)
+
 		// Assert the expected result
-		require.Len(t, ctr, 2) // Modify the count based on your configuration
-		require.Equal(t, ctr[server.URL].Error, &fiber.Error{Code: 500, Message: "error while connecting to the endpoint"})
-		require.Error(t, ctr["127.0.0.1"].Error)
+		require.Len(t, ctr, 1)
+		require.Nil(t, ctr[host].Error)
+		require.Len(t, ctr[host].Result.Checks, 2)
+
 	})
+
+	t.Run("Failed Software Version Check", func(t *testing.T) {
+		// Create a dummy server
+		server, host, port := createDummyServer(http.StatusInternalServerError)
+		defer server.Close()
+
+		// Test data
+		config := models.Config{
+			Hardware: models.Hardware{
+				AutomateNodeCount: 1,
+				AutomateNodeIps:   []string{host},
+			},
+		}
+
+		suc := NewSoftwareVersionCheck(logger.NewLogrusStandardLogger(), port)
+		ctr := suc.Run(config)
+
+		require.Len(t, ctr, 1)
+		require.NotNil(t, ctr[host].Error)
+	})
+}
+
+// Helper function to create a dummy server
+func createDummyServer(requiredStatusCode int) (*httptest.Server, string, string) {
+	if requiredStatusCode == http.StatusOK {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			if r.URL.Path == constants.SOFTWARE_VERSION_CHECK_API_PATH {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(softwareVersionResp))
+			}
+		}))
+
+		// Extract IP and port from the server's URL
+		address := server.URL[strings.Index(server.URL, "//")+2:]
+		colonIndex := strings.Index(address, ":")
+		ip := address[:colonIndex]
+		port := address[colonIndex+1:]
+
+		return server, ip, port
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(requiredStatusCode)
+	}))
+
+	// Extract IP and port from the server's URL
+	address := server.URL[strings.Index(server.URL, "//")+2:]
+	colonIndex := strings.Index(address, ":")
+	ip := address[:colonIndex]
+	port := address[colonIndex+1:]
+
+	return server, ip, port
 }
--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/systemuserchecktrigger/systemuserchecktrigger_test.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/systemuserchecktrigger/systemuserchecktrigger_test.go
@@ -1,70 +1,116 @@
 package systemuserchecktrigger
 
 import (
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/constants"
 	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
 	"github.com/chef/automate/lib/logger"
-	"github.com/gofiber/fiber"
 	"github.com/stretchr/testify/require"
 )
 
 const (
-	softwareVersionResp = `{
-       "status": "success",
-       "result": {
-         "passed": true,
-         "msg": "API result message",
-         "check": "API check",
-         "checks": [
-           {
-             "title": "Title 1",
-             "passed": true,
-             "success_msg": "Title 1 passed",
-             "error_msg": "",
-             "resolution_msg": "not required"
-           },
-           {
-             "title": "Title 2",
-             "passed": false,
-             "success_msg": "",
-             "error_msg": "Title 2 failed",
-             "resolution_msg": "Check the log or conf"
-           }
-         ]
-       },
-       "host": ""
-     }`
+	systemUser = `{
+		"status": "SUCCESS",
+		"result": {
+			"passed": true,
+			"checks": [
+				{
+					"title": "User creation/validation check",
+					"passed": true,
+					"success_msg": "User is created or found successfully",
+					"error_msg": "",
+					"resolution_msg": ""
+				},
+				{
+					"title": "User and group mapping successfully",
+					"passed": true,
+					"success_msg": "User and group mapping successful",
+					"error_msg": "",
+					"resolution_msg": ""
+				}
+			]
+		}
+	}`
 )
 
 func TestSystemUserCheck_Run(t *testing.T) {
 	t.Run("System User Check", func(t *testing.T) {
-		// Create a test server to mock the API endpoint
-		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Define the response based on the test case
-			w.WriteHeader(http.StatusOK)
-			err := json.NewEncoder(w).Encode(json.RawMessage(softwareVersionResp))
-			require.NoError(t, err)
-		}))
+		// Create a dummy server
+		server, host, port := createDummyServer(http.StatusOK)
 		defer server.Close()
 
+		// Test data
 		config := models.Config{
 			Hardware: models.Hardware{
-				AutomateNodeCount:   1,
-				AutomateNodeIps:     []string{server.URL},
-				PostgresqlNodeCount: 1,
-				PostgresqlNodeIps:   []string{"127.0.0.1"},
+				AutomateNodeCount: 1,
+				AutomateNodeIps:   []string{host},
 			},
 		}
 
-		suc := NewSystemUserCheck(logger.NewLogrusStandardLogger(), "8080")
+		suc := NewSystemUserCheck(logger.NewLogrusStandardLogger(), port)
 		ctr := suc.Run(config)
+
 		// Assert the expected result
-		require.Len(t, ctr, 2) // Modify the count based on your configuration
-		require.Equal(t, ctr[server.URL].Error, &fiber.Error{Code: 500, Message: "error while connecting to the endpoint"})
-		require.Error(t, ctr["127.0.0.1"].Error)
+		require.Len(t, ctr, 1)
+		require.Nil(t, ctr[host].Error)
+		require.Len(t, ctr[host].Result.Checks, 2)
+
 	})
+
+	t.Run("Failed User Check", func(t *testing.T) {
+		// Create a dummy server
+		server, host, port := createDummyServer(http.StatusInternalServerError)
+		defer server.Close()
+
+		// Test data
+		config := models.Config{
+			Hardware: models.Hardware{
+				AutomateNodeCount: 1,
+				AutomateNodeIps:   []string{host},
+			},
+		}
+
+		suc := NewSystemUserCheck(logger.NewLogrusStandardLogger(), port)
+		ctr := suc.Run(config)
+
+		require.Len(t, ctr, 1)
+		require.NotNil(t, ctr[host].Error)
+	})
+}
+
+// Helper function to create a dummy server
+func createDummyServer(requiredStatusCode int) (*httptest.Server, string, string) {
+	if requiredStatusCode == http.StatusOK {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			if r.URL.Path == constants.SYSTEM_USER_CHECK_API_PATH {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(systemUser))
+			}
+		}))
+
+		// Extract IP and port from the server's URL
+		address := server.URL[strings.Index(server.URL, "//")+2:]
+		colonIndex := strings.Index(address, ":")
+		ip := address[:colonIndex]
+		port := address[colonIndex+1:]
+
+		return server, ip, port
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(requiredStatusCode)
+	}))
+
+	// Extract IP and port from the server's URL
+	address := server.URL[strings.Index(server.URL, "//")+2:]
+	colonIndex := strings.Index(address, ":")
+	ip := address[:colonIndex]
+	port := address[colonIndex+1:]
+
+	return server, ip, port
 }
