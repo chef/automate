b3a2a70ce75966bd2b17e6d764a675190816b467
Shanmugapriya <shramana@progress.com>
2023-05-09 11:35:46 +0530

    CHEF-2869 : Minor refactor
    Signed-off-by: Shanmugapriya <shramana@progress.com>

--- components/automate-cli/pkg/verifyserver/utils/httputils/httputil.go
+++ components/automate-cli/pkg/verifyserver/utils/httputils/httputil.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"io"
 	"net/http"
 
 	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/constants"
@@ -11,11 +12,15 @@ import (
 
 //Post - This function performs HTTP Post request to the given endpoint with the provided request body
 func MakeRequest(requestMethod string, url string, body interface{}) (*http.Response, error) {
-	requestBody, err := json.Marshal(body)
-	if err != nil {
-		return nil, err
+	var reader io.Reader
+	if body != nil {
+		requestBody, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+		reader = bytes.NewBuffer(requestBody)
 	}
-	req, err := http.NewRequest(requestMethod, url, bytes.NewBuffer(requestBody))
+	req, err := http.NewRequest(requestMethod, url, reader)
 	if err != nil {
 		return nil, err
 	}
