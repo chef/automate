9412c3afd1ca35ea9c87beb54067dbfc36f0de88
vviveksharma <visharma@progress.com>
2023-05-12 16:03:31 +0530

    Added the Debian as the supported Device
    Signed-off-by: vviveksharma <visharma@progress.com>

--- components/automate-cli/pkg/verifyserver/services/softwareversionservice/mocksoftwareversionservice.go
+++ components/automate-cli/pkg/verifyserver/services/softwareversionservice/mocksoftwareversionservice.go
@@ -1,14 +1,13 @@
 package softwareversionservice
 
 import (
-    "github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
+	"github.com/chef/automate/components/automate-cli/pkg/verifyserver/models"
 )
 
-
 type MockSoftwareVersionService struct {
-    GetSoftwareServicesFunc func(string) (*models.SoftwareVersionDetails, error)
+	GetSoftwareServicesFunc func(string) (*models.SoftwareVersionDetails, error)
 }
 
 func (msv *MockSoftwareVersionService) GetSoftwareVersionServices(string) (*models.SoftwareVersionDetails, error) {
-    return msv.GetSoftwareServicesFunc("opensearch")
+	return msv.GetSoftwareServicesFunc("opensearch")
 }
