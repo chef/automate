049cd75acfa71e8441eed4dee52d9b9beae1d26c
RajeshPaul38 <rajesh.paul@progress.com>
2023-05-16 20:56:20 +0530

    added configuration for chef-server for ocid
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>

--- components/automate-cs-ocid/habitat/config/tasks/set_env_vars.rb
+++ components/automate-cs-ocid/habitat/config/tasks/set_env_vars.rb
@@ -1,8 +1,6 @@
 require 'yaml'
 env_config_file_path = ENV['ENV_CONFIG_FILE_PATH']
-puts "env_config_file_path: #{env_config_file_path}"
 env_config = YAML.load_file(env_config_file_path)
-puts "env_config: #{env_config}"
 override_configs = {
   'endpoint' => ENV['CHEF_SERVER_ENDPOINT'],
   'superuser' => ENV['CHEF_SERVER_SUPERUSER'],
@@ -11,5 +9,4 @@ override_configs = {
 env_config['chef'] ||= {}
 env_config['chef'].merge!(override_configs)
 env_config_yml = env_config.to_yaml
-puts "yaml type: #{env_config_yml.class.name}"
 File.write(env_config_file_path, env_config_yml)
