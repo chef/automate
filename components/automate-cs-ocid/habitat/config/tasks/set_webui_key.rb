7415dcbd9395ca8353915e900cde67f9aa3ae143
RajeshPaul38 <rajesh.paul@progress.com>
2023-05-17 13:08:32 +0530

    reading values from environment instead of arguments for setting webui key
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>

--- components/automate-cs-ocid/habitat/config/tasks/set_webui_key.rb
+++ components/automate-cs-ocid/habitat/config/tasks/set_webui_key.rb
@@ -1,7 +1,7 @@
 require 'json'
 
-webui_key = ARGV[0]
-private_chef_secrets_path = ARGV[1]
+webui_key = ENV['WEBUI_KEY']
+private_chef_secrets_path = ENV['PRIVATE_CHEF_SECRETS_PATH']
 
 private_chef_secrets_content = File.read(private_chef_secrets_path)
 private_chef_secrets_hash = JSON.parse(private_chef_secrets_content)
