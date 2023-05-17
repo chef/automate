a6eb55d4d491a94d599cadce36fc23bb53f752dc
Pappu Kumar <pappu.kumar@progress.com>
2023-05-18 04:20:26 +0530

    Creating request
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/firewallchecktrigger/firewallchecktrigger.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/firewallchecktrigger/firewallchecktrigger.go
@@ -17,9 +17,9 @@ type FirewallCheck struct {
 	log  logger.Logger
 }
 
-type Request struct {
-	Requests []ReqBody
-}
+// type Request struct {
+//  Requests []ReqBody
+// }
 
 type ReqBody struct {
 	SourceNodeIP               string `json:"source_node_ip"`
@@ -41,12 +41,89 @@ func NewFirewallCheck(log logger.Logger, port string) *FirewallCheck {
 
 func (fc *FirewallCheck) Run(config models.Config) []models.CheckTriggerResponse {
 	fc.log.Info("Performing Firewall check from batch check ")
-	portMap := constructPorts()
-
-	// Prepare request
-
-	bx, _ := json.MarshalIndent(portMap, "", "\t")
+	reqbody, _ := createReqBody(config)
+	bx, _ := json.MarshalIndent(reqbody, "", "\t")
 	ioutil.WriteFile("abc.json", bx, 0777)
 	return nil
 	return trigger.RunCheckAllInstance(config, fc.log, fc.port, constants.FIREWALL_API_PATH, "", http.MethodPost, nil)
+}
+
+func createReqBody(config models.Config) ([]ReqBody, error) {
+	var reqBodies []ReqBody
+
+	// Get the ports configuration
+	ports := constructPorts()
+
+	// source automate - dest postgres
+	for _, sourceNodeIP := range config.Hardware.AutomateNodeIps {
+		for _, destNodeIP := range config.Hardware.PostgresqlNodeIps {
+			reqBody := ReqBody{
+				SourceNodeIP:           sourceNodeIP,
+				DestinationNodeIP:      destNodeIP,
+				DestinationServicePort: "7432",
+			}
+			reqBodies = append(reqBodies, reqBody)
+		}
+	}
+
+	// source automate - dest postgres
+	for _, sourceNodeIP := range config.Hardware.AutomateNodeIps {
+		for _, destNodeIP := range config.Hardware.OpenSearchNodeIps {
+			reqBody := ReqBody{
+				SourceNodeIP:           sourceNodeIP,
+				DestinationNodeIP:      destNodeIP,
+				DestinationServicePort: "9200",
+			}
+			reqBodies = append(reqBodies, reqBody)
+		}
+	}
+
+	// TODO: correct the rest
+	for _, sourceNodeIP := range config.Hardware.ChefInfraServerNodeIps {
+		for _, destNodeIP := range config.Hardware.PostgresqlNodeIps {
+			reqBody, err := createReqBodyForNodes(sourceNodeIP, destNodeIP, ports.ChefInfraServer)
+			if err != nil {
+				return nil, err
+			}
+			reqBodies = append(reqBodies, reqBody)
+		}
+	}
+
+	for _, sourceNodeIP := range config.Hardware.PostgresqlNodeIps {
+		for _, destNodeIP := range config.Hardware.OpenSearchNodeIps {
+			reqBody, err := createReqBodyForNodes(sourceNodeIP, destNodeIP, ports.Postgresql)
+			if err != nil {
+				return nil, err
+			}
+			reqBodies = append(reqBodies, reqBody)
+		}
+	}
+
+	// Add other pairings as needed for different server types
+
+	return reqBodies, nil
+}
+
+func createReqBodyForNodes(sourceIP, destIP string, instances []Instance) (ReqBody, error) {
+	var reqBody ReqBody
+
+	// Find the matching instance configuration for the given server type
+	var instanceConfig Instance
+	for _, instance := range instances {
+		if instance.Name == constants.OPENSEARCH {
+			instanceConfig = instance
+			break
+		}
+	}
+
+	// Set the values in the request body
+	reqBody.SourceNodeIP = sourceIP
+	reqBody.DestinationNodeIP = destIP
+	reqBody.DestinationServicePort = instanceConfig.PortProtocolMap["tcp"][0]
+	reqBody.DestinationServiceProtocol = "tcp"
+	reqBody.Cert = ""
+	reqBody.Key = ""
+	reqBody.RootCert = ""
+
+	return reqBody, nil
 }
