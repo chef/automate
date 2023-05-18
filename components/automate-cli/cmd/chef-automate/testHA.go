dece5f501a1a2d2f44e942a1c433040094ee40c8
Vivek Shankar <vshankar@progress.com>
2023-05-18 18:21:54 +0530

    fix automate test command
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- components/automate-cli/cmd/chef-automate/testHA.go
+++ components/automate-cli/cmd/chef-automate/testHA.go
@@ -34,7 +34,7 @@ func runTestCmd(cmd *cobra.Command, args []string) error {
 		if testCommandFlags.full {
 			args = append(args, "--full")
 		}
-		return executeAutomateClusterCtlCommandAsync("test", args, testHAHelpDocs, true)
+		return executeAutomateClusterCtlCommandAsync("test", args, testHAHelpDocs, false)
 	} else {
 		return status.Wrap(errors.New(AUTOMATE_HA_INVALID_BASTION), status.ConfigError, testHAHelpDocs)
 	}
