bb24c229be8038218165d3e24d266cc5583ed1a5
“SanjuPal01” <sanju.sanju@progress.com>
2023-05-24 20:28:06 +0530

    Rebase with main
    Signed-off-by: “SanjuPal01” <sanju.sanju@progress.com>

--- components/automate-cli/pkg/verifyserver/server/api/v1/portreachable.go
+++ components/automate-cli/pkg/verifyserver/server/api/v1/portreachable.go
@@ -13,13 +13,11 @@ func (h *Handler) PortReachable(c *fiber.Ctx) error {
 	reqBody := models.PortReachableRequest{}
 	if err := c.BodyParser(&reqBody); err != nil {
 		h.Logger.Error(err.Error())
-		// c.Next(&fiber.Error{Code: http.StatusBadRequest, Message: "Invalid Body Request"})
 		return fiber.NewError(http.StatusBadRequest, "Invalid Body Request")
 	}
 	if reqBody.DestinationNodeIp == "" ||
 		reqBody.DestinationNodePort == 0 ||
 		reqBody.DestinationNodeServiceProtocol == "" {
-		// c.Next(&fiber.Error{Code: http.StatusBadRequest, Message: "DestinationNodeIp, DestinationNodePort, or DestinationNodeServiceProtocol cannot be empty"})
 		return fiber.NewError(http.StatusBadRequest, "DestinationNodeIp, DestinationNodePort, or DestinationNodeServiceProtocol cannot be empty")
 	}
 
@@ -27,16 +25,13 @@ func (h *Handler) PortReachable(c *fiber.Ctx) error {
 	err := validatePortRange(reqBody.DestinationNodePort, h)
 	if err != nil {
 		h.Logger.Error(err)
-		// c.Next(&fiber.Error{Code: http.StatusBadRequest, Message: "Invalid port number"})
 		return fiber.NewError(http.StatusBadRequest, "Invalid port number")
 	}
 
 	if reqBody.DestinationNodeServiceProtocol == constants.HTTPS && reqBody.RootCA == "" {
-		// c.Next(&fiber.Error{Code: http.StatusBadRequest, Message: "RootCA value is mandatory for protocol HTTPS"})
 		return fiber.NewError(http.StatusBadRequest, "RootCA value is mandatory for protocol HTTPS")
 	}
 	if reqBody.DestinationNodeServiceProtocol != constants.TCP && reqBody.DestinationNodeServiceProtocol != constants.UDP && reqBody.DestinationNodeServiceProtocol != constants.HTTPS {
-		// c.Next(&fiber.Error{Code: http.StatusBadRequest, Message: "Please Give Valid Protocol i.e tcp, udp or https"})
 		return fiber.NewError(http.StatusBadRequest, "Please Give Valid Protocol i.e tcp, udp or https")
 	}
 	portReachableResult := h.PortReachableService.GetPortReachableDetails(reqBody)
