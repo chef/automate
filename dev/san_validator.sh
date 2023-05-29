35ee058de706bde0c22e382f019e0fbcb3be7eaf
Durga Sarat Chandra Maddu <44021874+Dmaddu@users.noreply.github.com>
2023-05-29 16:35:58 +0530

    update to san_validator (#7920)
    * update to san_validator
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>
    
    * addressing review comments
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>
    
    ---------
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>

--- dev/san_validator.sh
+++ dev/san_validator.sh
@@ -2,26 +2,65 @@
 
 cert_path="$1"
 
-# Check for empty certificate path
-if [ -z "$cert_path" ]; then
-    echo "Error: Certificate path not provided"
-    echo "usage:"
-    echo "bash san_validator.sh <certificate path>"
+validate_cert() {
+    input_path="$1"
+    cert_info=$(openssl x509 -in "$input_path" -noout -text 2>/dev/null)
+    if echo "$cert_info" | grep -q "Subject Alternative Name"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_root_ca() {
+    input_path="$1"
+
+    #Check the certificate is RootCA is not
+    is_self_signed=$(openssl verify -CAfile "$cert" "$cert" 2>&1)
+
+    #enable case insensitive match for reg-ex
+    shopt -s nocasematch
+    if [[ ! -z $(cat "$cert") ]] && [[ $is_self_signed == *error* ]]; then
+            #disable case insensitive match for reg-ex
+            shopt -u nocasematch
+            return 1
+    else
+            #disable case insensitive match for reg-ex
+            shopt -u nocasematch
+            return 0
+    fi
+
+}
+
+# If the certificate path provide validate only that certificate
+if [ "$cert_path" ]; then
+    # Check for the certificate file exists
+    if [ ! -f "$cert_path" ]; then
+            echo "Error: Certificate file not found."
+            exit 1
+    fi
+
+    if validate_cert "$cert_path"; then
+        echo "Pass: your certificate is a valid SAN certificate"
+    else
+        echo "Fail: your certificate is not a valid SAN certificate"
+    fi
     exit 1
 fi
 
-# Check for the certificate file exists
-if [ ! -f "$cert_path" ]; then
-    echo "Error: Certificate file not found."
-    exit 1
-fi
 
-# Get the certificate information
-cert_info=$(openssl x509 -in "$cert_path" -noout -text 2>/dev/null)
+# Get all the internal certificates with extension(.crt), gets the RootCA and public certificates
+certs=$(find /hab/svc -type f -name "*.crt")
+invalid_certs=()
 
-# Check if the certificate has SAN or not
-if echo "$cert_info" | grep -q "Subject Alternative Name"; then
-    echo "Pass: your certificate is a valid SAN certificate"
-else
-    echo "Fail: your certificate is not a valid SAN certificate"
-fi
+for cert in $certs; do
+        if ! is_root_ca "$cert" && ! validate_cert "$cert"; then
+                invalid_certs+=("$cert")
+        fi
+done
+
+# Print the list of invalid certs
+echo "Invalid certificates:"
+for cert in "${invalid_certs[@]}"; do
+    echo "$cert"
+done
