68e16b53dd1bf04f7bebc8dc0d48218975c240a4
Rishabh Agarwal <10796521+rishabhjhs@users.noreply.github.com>
2023-06-14 16:18:00 +0530

    Command available from bastion doc fix (#7960)
    Signed-off-by: rishabhjhs <ragarwal@progress.com>
    Co-authored-by: rishabhjhs <ragarwal@progress.com>

--- components/automate-cli/cmd/chef-automate/pre_flight_check.go
+++ components/automate-cli/cmd/chef-automate/pre_flight_check.go
@@ -112,7 +112,7 @@ var preflightCheckCmd = &cobra.Command{
 	Long:  "Perform preflight check to verify host meets installation criteria.",
 	Annotations: map[string]string{
 		NoCheckVersionAnnotation: NoCheckVersionAnnotation,
-		docs.Tag:                 docs.FrontEnd,
+		docs.Tag:                 docs.BastionHost,
 	},
 	RunE: runPreflightCheckCmd,
 }
