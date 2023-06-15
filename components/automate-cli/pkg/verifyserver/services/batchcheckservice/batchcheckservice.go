f00836e3367e14679c4aa8e3989dcfdee336ce0f
rishabhjhs <ragarwal@progress.com>
2023-06-15 12:31:31 +0530

    [CHEF-3227]: Added close to stopmock channel
    Signed-off-by: rishabhjhs <ragarwal@progress.com>

--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/batchcheckservice.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/batchcheckservice.go
@@ -78,6 +78,7 @@ func (ss *BatchCheckService) BatchCheck(checks []string, config models.Config) (
 func (ss *BatchCheckService) stopAllMockServers(successfullyStartedMockServers []models.MockServerFromBatchServiceResponse) bool {
 	if len(successfullyStartedMockServers) > 0 {
 		stopMockServerChannel := make(chan models.MockServerFromBatchServiceResponse)
+		defer close(stopMockServerChannel)
 		totalReq := 0
 		mockServersStoppedSuccessfully := true
 		for _, successfullyStartedMockServer := range successfullyStartedMockServers {
@@ -370,8 +371,14 @@ func (ss *BatchCheckService) startMockServerOnHostAndPort(host, port string, sta
 	url := fmt.Sprintf("%s%s:%s%s", "http://", host, port, constants.START_MOCK_SERVER)
 	resp, err := ss.httpRequestClient.MakeRequest(http.MethodPost, url, startMockServerRequestBody)
 	if err != nil && resp == nil {
-		ss.log.Error(err.Error())
-		respChan <- models.MockServerFromBatchServiceResponse{}
+		ss.log.Error("Error occurred while starting mock server", err.Error())
+		respChan <- models.MockServerFromBatchServiceResponse{
+			Host:       host,
+			Error:      err,
+			StatusCode: http.StatusBadRequest,
+			Protocol:   startMockServerRequestBody.Protocol,
+			Port:       startMockServerRequestBody.Port,
+		}
 		return
 	}
 	if err != nil {
