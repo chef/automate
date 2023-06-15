0c2bce4c50706bbca11f4839e6aa4a2329ca9248
Sahiba3108 <sgoyal@progress.com>
2023-06-15 15:04:15 +0530

    add stop command
    Signed-off-by: Sahiba3108 <sgoyal@progress.com>

--- components/automate-cli/cmd/chef-automate/stop.go
+++ components/automate-cli/cmd/chef-automate/stop.go
@@ -3,58 +3,296 @@
 package main
 
 import (
+	"container/list"
 	"context"
+	"fmt"
+	"os"
+	"os/exec"
 
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	api "github.com/chef/automate/api/interservice/deployment"
 	"github.com/chef/automate/components/automate-cli/pkg/docs"
 	"github.com/chef/automate/components/automate-cli/pkg/status"
 	"github.com/chef/automate/components/automate-deployment/pkg/client"
-	"github.com/chef/automate/components/automate-deployment/pkg/target"
 )
 
-var stopCmd = &cobra.Command{
-	Use:   "stop",
-	Short: "Stop deployment",
-	Long:  "Stop a running deployment of Automate.",
-	RunE:  runStopCmd,
-	Annotations: map[string]string{
-		docs.Tag: docs.FrontEnd,
-	},
+const (
+	ERROR_ON_MANAGED_SERVICE_STOP = "Stopping the service for externally configured %s is not supported"
+)
+
+var stopCmdFlags = struct {
+	automate    bool
+	chef_server bool
+	opensearch  bool
+	postgresql  bool
+	node        string
+}{}
+
+func init() {
+	var stopCmd = &cobra.Command{
+		Use:   "stop",
+		Short: "Stop deployment",
+		Long:  "Stop a running deployment of Automate.",
+		RunE:  runStopCmd,
+		Annotations: map[string]string{
+			docs.Tag: docs.FrontEnd,
+		},
+	}
+
+	stopCmd.PersistentFlags().BoolVarP(&stopCmdFlags.automate, "automate", "a", false, "Stop services of automate node")
+	stopCmd.PersistentFlags().SetAnnotation("automate", docs.Compatibility, []string{docs.CompatiblewithHA})
+	stopCmd.PersistentFlags().BoolVar(&stopCmdFlags.automate, "a2", false, "Stop services of automate node")
+	stopCmd.PersistentFlags().SetAnnotation("a2", docs.Compatibility, []string{docs.CompatiblewithHA})
+	stopCmd.PersistentFlags().BoolVarP(&stopCmdFlags.chef_server, "chef_server", "c", false, "Stop services of chef_server node")
+	stopCmd.PersistentFlags().SetAnnotation("chef_server", docs.Compatibility, []string{docs.CompatiblewithHA})
+	stopCmd.PersistentFlags().BoolVar(&stopCmdFlags.chef_server, "cs", false, "Stop services of chef_server node")
+	stopCmd.PersistentFlags().SetAnnotation("cs", docs.Compatibility, []string{docs.CompatiblewithHA})
+	stopCmd.PersistentFlags().BoolVarP(&stopCmdFlags.opensearch, "opensearch", "o", false, "Stop services of opensearch node")
+	stopCmd.PersistentFlags().SetAnnotation("opensearch", docs.Compatibility, []string{docs.CompatiblewithHA})
+	stopCmd.PersistentFlags().BoolVar(&stopCmdFlags.opensearch, "os", false, "Stop services of opensearch node")
+	stopCmd.PersistentFlags().SetAnnotation("os", docs.Compatibility, []string{docs.CompatiblewithHA})
+	stopCmd.PersistentFlags().BoolVarP(&stopCmdFlags.postgresql, "postgresql", "p", false, "Stop services of postgresql node")
+	stopCmd.PersistentFlags().SetAnnotation("postgresql", docs.Compatibility, []string{docs.CompatiblewithHA})
+	stopCmd.PersistentFlags().BoolVar(&stopCmdFlags.postgresql, "pg", false, "Stop services of postgresql node")
+	stopCmd.PersistentFlags().SetAnnotation("pg", docs.Compatibility, []string{docs.CompatiblewithHA})
+
+	stopCmd.PersistentFlags().StringVar(&stopCmdFlags.node, "node", "", "Node Ip address")
+	RootCmd.AddCommand(stopCmd)
 }
 
 func runStopCmd(cmd *cobra.Command, args []string) error {
-	connection, err := client.Connection(client.DefaultClientTimeout)
-	if err != nil {
-		return err
+	if isA2HARBFileExist() {
+		if !stopCmdFlags.automate && !stopCmdFlags.chef_server && !stopCmdFlags.opensearch && !stopCmdFlags.postgresql {
+			if stopCmdFlags.node != "" {
+				writer.Println("Please Provide service flag")
+			}
+			writer.Println(cmd.UsageString())
+			return nil
+		}
+
+		infra, err := getAutomateHAInfraDetails()
+		if err != nil {
+			return err
+		}
+
+		if err = runStopCommandHA(infra, isManagedServicesOn()); err != nil {
+			return err
+		}
+
+	} else if isDevMode() {
+		if err := runStopDevMode(); err != nil {
+			return err
+		}
+
+	} else {
+		if err := runStopStandalone(); err != nil {
+			return err
+		}
 	}
 
-	if isDevMode() {
-		_, err = connection.Stop(context.Background(), &api.StopRequest{})
-		if err != nil {
-			return status.Wrap(
-				err,
-				status.DeploymentServiceCallError,
-				"Request to stop services failed",
-			)
-		}
-	} else {
-		// In the non studio case, it is important to ask systemd to perform the stop.
-		// There is currently a bug in habitat where it's possible for hab-launch to
-		// stay alive: https://github.com/habitat-sh/habitat/issues/5783
-		// The deployment-service converger does its best to work around this issue,
-		// but for extra safety, trigger the stop through systemctl.
-		t := target.NewLocalTarget(true)
-		if err := t.EnsureStopped(); err != nil {
-			return status.Wrap(err, status.UnknownError, "Failed to stop Automate")
-		}
+	return nil
+}
+
+func runStopStandalone() error {
+	writer.Title("Stopping Chef Automate")
+	systemctlCmd := exec.Command("systemctl", "stop", "chef-automate.service")
+	systemctlCmd.Stdout = os.Stdout
+	systemctlCmd.Stderr = os.Stderr
+	if err := systemctlCmd.Run(); err != nil {
+		return status.Annotate(err, status.ServiceUnloadError)
 	}
 
 	writer.Title("Chef Automate Stopped")
 	return nil
 }
 
-func init() {
-	RootCmd.AddCommand(stopCmd)
+func runStopDevMode() error {
+	writer.Title("Stopping Chef Automate")
+	connection, err := client.Connection(client.DefaultClientTimeout)
+	if err != nil {
+		return err
+	}
+
+	_, err = connection.Stop(context.Background(), &api.StopRequest{})
+	if err != nil {
+		return status.Wrap(
+			err,
+			status.DeploymentServiceCallError,
+			"Request to stop services failed",
+		)
+	}
+
+	writer.Title("Chef Automate Stopped")
+	return nil
+}
+
+func runStopCommandHA(infra *AutomateHAInfraDetails, isManagedServices bool) error {
+	errorList := list.New()
+	count := getFlagCount()
+
+	if count > 1 && stopCmdFlags.node != "" {
+		return errors.New("Please remove node flag if you have given multiple service flags.")
+	}
+	if stopCmdFlags.automate {
+		getAndStopHaNodes(AUTOMATE, infra, errorList)
+	}
+	if stopCmdFlags.chef_server {
+		getAndStopHaNodes(CHEF_SERVER, infra, errorList)
+	}
+	if stopCmdFlags.opensearch {
+		if isManagedServices {
+			return status.Errorf(status.InvalidCommandArgsError, ERROR_ON_MANAGED_SERVICE_STOP, OPENSEARCH)
+		}
+		getAndStopHaNodes(OPENSEARCH, infra, errorList)
+	}
+	if stopCmdFlags.postgresql {
+		if isManagedServices {
+			return status.Errorf(status.InvalidCommandArgsError, ERROR_ON_MANAGED_SERVICE_STOP, POSTGRESQL)
+		}
+		getAndStopHaNodes(POSTGRESQL, infra, errorList)
+	}
+
+	if errorList.Len() > 0 {
+		return status.New(status.ServiceUnloadError, getSingleErrorFromList(errorList).Error())
+	}
+	return nil
+}
+
+func getAndStopHaNodes(remoteService string, infra *AutomateHAInfraDetails, errorList *list.List) {
+	sshConfig := getSshDetails(infra)
+	var ips []string
+
+	if stopCmdFlags.node != "" {
+		isValid := validateEachIp(remoteService, infra, stopCmdFlags.node)
+		if !isValid {
+			errorList.PushBack(fmt.Sprintf("Please Enter Valid %s IP", remoteService))
+			return
+		}
+		ips = append(ips, stopCmdFlags.node)
+	} else {
+		ips = getIps(remoteService, infra)
+	}
+	stopCommandImplHA(*sshConfig, ips, remoteService, errorList)
+}
+
+func stopCommandImplHA(sshConfig SSHConfig, ips []string, remoteService string, errorList *list.List) {
+	sshUtilMap := getMapSSHUtils(ips, &sshConfig)
+	err := checkNodeType(sshUtilMap, ips, remoteService)
+	if err != nil {
+		errorList.PushBack(err.Error())
+	}
+}
+
+func checkNodeType(sshUtilMap map[string]SSHUtil, ips []string, remoteService string) error {
+	if len(ips) == 0 {
+		writer.Errorf("No %s IPs are found", remoteService)
+		return status.Errorf(1, "No %s IPs are found", remoteService)
+	}
+	if remoteService == OPENSEARCH || remoteService == POSTGRESQL {
+		return stopBackEndNodes(sshUtilMap, ips, remoteService)
+	}
+	return stopFrontEndNodes(sshUtilMap, ips, remoteService)
+}
+
+func stopFrontEndNodes(sshUtilMap map[string]SSHUtil, ips []string, remoteService string) error {
+	resultChan := make(chan Result, len(ips))
+	errorList := list.New()
+	scriptCommands := `sudo chef-automate stop`
+	for _, hostIP := range ips {
+		sshUtil := sshUtilMap[hostIP]
+		go commandExecuteFrontEnd(scriptCommands, sshUtil, resultChan)
+	}
+	printMessageForStopResultChan(resultChan, ips, remoteService, errorList)
+	close(resultChan)
+	if errorList != nil && errorList.Len() > 0 {
+		return status.Wrapf(getSingleErrorFromList(errorList), status.ServiceUnloadError, "Not able to stop one or more nodes in %s", remoteService)
+	}
+	return nil
+}
+
+func stopBackEndNodes(sshUtilMap map[string]SSHUtil, ips []string, remoteService string) error {
+	resultChan := make(chan Result, len(ips))
+	scriptCommands := "sudo systemctl stop hab-sup"
+	errorList := list.New()
+	for _, hostIP := range ips {
+		sshUtil := sshUtilMap[hostIP]
+		go commandExecuteBackendNode(scriptCommands, sshUtil, resultChan)
+	}
+	printMessageForStopResultChan(resultChan, ips, remoteService, errorList)
+	close(resultChan)
+	if errorList != nil && errorList.Len() > 0 {
+		return status.Wrapf(getSingleErrorFromList(errorList), status.ServiceUnloadError, "Not able to stop one or more nodes in %s", remoteService)
+	}
+	return nil
+}
+
+func printMessageForStopResultChan(resultChan chan Result, ips []string, remoteService string, errorList *list.List) {
+	for i := 0; i < len(ips); i++ {
+		result := <-resultChan
+		printStopConnectionMessage(remoteService, result.HostIP)
+		if result.Error != nil {
+			printStopErrorMessage(remoteService, result.HostIP, result.Error)
+			errorList.PushBack(result.Error.Error())
+		} else {
+			writer.Printf("Output for Host IP %s : %s", result.HostIP, result.Output+"\n")
+			printStopSuccessMessage(remoteService, result.HostIP)
+		}
+	}
+}
+
+func printStopConnectionMessage(remoteService string, hostIP string) {
+	writer.Println("Connecting to the " + remoteService + NODE + hostIP)
+	writer.BufferWriter().Flush()
+}
+
+func printStopErrorMessage(remoteService string, hostIP string, err error) {
+	writer.Failf("Stop Command failed on "+remoteService+NODE+hostIP+" with error:\n%v \n", err)
+	writer.BufferWriter().Flush()
+}
+
+func printStopSuccessMessage(remoteService string, hostIP string) {
+	writer.Success("Stop Command is completed on " + remoteService + NODE + hostIP + "\n")
+	writer.BufferWriter().Flush()
+}
+
+func validateEachIp(remoteService string, infra *AutomateHAInfraDetails, ip string) bool {
+	ips := getIps(remoteService, infra)
+	for i := 0; i < len(ips); i++ {
+		if ips[i] == ip {
+			return true
+		}
+	}
+	return false
+}
+
+func getIps(remoteService string, infra *AutomateHAInfraDetails) []string {
+	if remoteService == AUTOMATE {
+		return infra.Outputs.AutomatePrivateIps.Value
+	} else if remoteService == CHEF_SERVER {
+		return infra.Outputs.ChefServerPrivateIps.Value
+	} else if remoteService == POSTGRESQL {
+		return infra.Outputs.PostgresqlPrivateIps.Value
+	} else if remoteService == OPENSEARCH {
+		return infra.Outputs.OpensearchPrivateIps.Value
+	}
+	return []string{}
+}
+
+func getFlagCount() int {
+	count := 0
+	if stopCmdFlags.automate {
+		count++
+	}
+	if stopCmdFlags.chef_server {
+		count++
+	}
+	if stopCmdFlags.opensearch {
+		count++
+	}
+	if stopCmdFlags.postgresql {
+		count++
+	}
+	return count
 }
---
+++ components/automate-cli/cmd/chef-automate/stop_test.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckNodeType(t *testing.T) {
+	testCases := []struct {
+		testName      string
+		sshUtilMap    map[string]SSHUtil
+		Ips           []string
+		remoteService string
+		isError       bool
+		err           error
+	}{
+		{
+			"All the ips are reachable and stopped the services of automate",
+			createMockSSHUtilMap([]string{TEST_IP, TEST_IP2, TEST_IP3}, nil, "", nil),
+			[]string{TEST_IP, TEST_IP2, TEST_IP3},
+			"automate",
+			false,
+			nil,
+		},
+		{
+			"Some of the ips are not reachable which are provided for chef-server",
+			createMockSSHUtilMap([]string{TEST_IP}, nil, "", errors.New("ERROR")),
+			[]string{TEST_IP},
+			"chef_server",
+			true,
+			errors.New("Not able to stop one or more nodes in chef_server: \nERROR"),
+		},
+		{
+			"All th ips are reachable and stopped the services of opensearch",
+			createMockSSHUtilMap([]string{TEST_IP, TEST_IP2, TEST_IP3}, nil, "", nil),
+			[]string{TEST_IP, TEST_IP2, TEST_IP3},
+			"opensearch",
+			false,
+			nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			err := checkNodeType(testCase.sshUtilMap, testCase.Ips, testCase.remoteService)
+			if testCase.isError {
+				assert.EqualError(t, err, testCase.err.Error())
+			} else {
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
+
+func TestStopCommandHA(t *testing.T) {
+	tests := []struct {
+		name            string
+		isForAutomate   bool
+		isForChefServer bool
+		isForPG         bool
+		isForOS         bool
+		isManaged       bool
+		isInfraEmpty    bool
+		node            string
+		isErrorExpected bool
+		errorMessage    string
+	}{
+		{
+			name:            "test_Empty_Flags",
+			isForAutomate:   false,
+			isForChefServer: false,
+			isForPG:         false,
+			isForOS:         false,
+			isManaged:       false,
+			node:            "",
+			isErrorExpected: false,
+			errorMessage:    "",
+		},
+		{
+			name:            "test_Automate",
+			isForAutomate:   true,
+			isForChefServer: false,
+			isForPG:         false,
+			isForOS:         false,
+			isManaged:       false,
+			node:            "",
+			isErrorExpected: true,
+			errorMessage:    "\nNot able to stop one or more nodes in automate: \nopen : no such file or directory",
+		},
+		{
+			name:            "test_InfraServer",
+			isForAutomate:   false,
+			isForChefServer: true,
+			isForPG:         false,
+			isForOS:         false,
+			isManaged:       false,
+			node:            "",
+			isErrorExpected: true,
+			errorMessage:    "\nNot able to stop one or more nodes in chef_server: \nopen : no such file or directory",
+		},
+		{
+			name:            "test_Postgres",
+			isForAutomate:   false,
+			isForChefServer: false,
+			isForPG:         true,
+			isForOS:         false,
+			isManaged:       false,
+			node:            "",
+			isErrorExpected: true,
+			errorMessage:    "\nNot able to stop one or more nodes in postgresql: \nopen : no such file or directory",
+		},
+		{
+			name:            "test_Postgres_with_ManagedServices",
+			isForAutomate:   false,
+			isForChefServer: false,
+			isForPG:         true,
+			isForOS:         false,
+			isManaged:       true,
+			node:            "",
+			isErrorExpected: true,
+			errorMessage:    "Stopping the service for externally configured postgresql is not supported",
+		},
+		{
+			name:            "test_OpenSearch",
+			isForAutomate:   false,
+			isForChefServer: false,
+			isForPG:         false,
+			isForOS:         true,
+			isManaged:       false,
+			node:            "",
+			isErrorExpected: true,
+			errorMessage:    "\nNot able to stop one or more nodes in opensearch: \nopen : no such file or directory",
+		},
+		{
+			name:            "test_OpenSearch_with_ManagedServices",
+			isForAutomate:   false,
+			isForChefServer: false,
+			isForPG:         false,
+			isForOS:         true,
+			isManaged:       true,
+			node:            "",
+			isErrorExpected: true,
+			errorMessage:    "Stopping the service for externally configured opensearch is not supported",
+		},
+		{
+			name:            "test_node_with_MultipleServiceFlags",
+			isForAutomate:   true,
+			isForChefServer: true,
+			isForPG:         false,
+			isForOS:         false,
+			isManaged:       false,
+			node:            "1.2.3.4",
+			isErrorExpected: true,
+			errorMessage:    "Please remove node flag if you have given multiple service flags.",
+		},
+		{
+			name:            "test_valid_node_with_Automate",
+			isForAutomate:   true,
+			isForChefServer: false,
+			isForPG:         false,
+			isForOS:         false,
+			isManaged:       false,
+			node:            "127.0.0.0",
+			isErrorExpected: true,
+			errorMessage:    "\nNot able to stop one or more nodes in automate: \nopen : no such file or directory",
+		},
+		{
+			name:            "test_invalid_node_with_Automate",
+			isForAutomate:   true,
+			isForChefServer: false,
+			isForPG:         false,
+			isForOS:         false,
+			isManaged:       false,
+			node:            "127.0.0.5",
+			isErrorExpected: true,
+			errorMessage:    "\nPlease Enter Valid automate IP",
+		},
+		{
+			name:            "test_empty_infra",
+			isForAutomate:   true,
+			isForChefServer: false,
+			isForPG:         false,
+			isForOS:         false,
+			isManaged:       false,
+			isInfraEmpty:    true,
+			node:            "",
+			isErrorExpected: true,
+			errorMessage:    "\nNo automate IPs are found",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			var infra *AutomateHAInfraDetails
+			stopCmdFlags.automate = tc.isForAutomate
+			stopCmdFlags.chef_server = tc.isForChefServer
+			stopCmdFlags.opensearch = tc.isForOS
+			stopCmdFlags.postgresql = tc.isForPG
+			stopCmdFlags.node = tc.node
+			if tc.isInfraEmpty {
+				infra = &AutomateHAInfraDetails{}
+			} else {
+				infra = getMockInfra()
+			}
+			err := runStopCommandHA(infra, tc.isManaged)
+			if tc.isErrorExpected {
+				assert.Error(t, err)
+				assert.EqualError(t, err, tc.errorMessage)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestStopCmd(t *testing.T) {
+	tests := []struct {
+		testName        string
+		isHA            bool
+		isDevMode       bool
+		isStandalone    bool
+		isExpectedError bool
+		errorMessage    string
+	}{
+		{"Dev Mode", false, true, false, true, "Failed to read deployment-service TLS certificates: Could not read the service cert: open /hab/svc/deployment-service/data/deployment-service.crt: no such file or directory"},
+		{"Standalone Mode", false, false, true, true, "exec: \"systemctl\": executable file not found in $PATH"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+			if tc.isDevMode {
+				os.Setenv("CHEF_DEV_ENVIRONMENT", "true")
+			} else {
+				os.Setenv("CHEF_DEV_ENVIRONMENT", "false")
+			}
+			err := runStopCmd(&cobra.Command{}, []string{})
+			if tc.isExpectedError {
+				assert.Error(t, err)
+				assert.EqualError(t, err, tc.errorMessage)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
