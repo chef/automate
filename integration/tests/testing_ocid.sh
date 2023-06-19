0caa4cabc7e6c105186717a651973a6367105855
ayush-billore <ayush.billore@progress.com>
2023-06-19 19:32:01 +0530

    temp changes to run oc-id only block
    Signed-off-by: ayush-billore <ayush.billore@progress.com>

--- integration/tests/testing_ocid.sh
+++ integration/tests/testing_ocid.sh
@@ -1,19 +1,26 @@
-do_deploy() {
-    #shellcheck disable=SC2154
-    chef-automate deploy config.toml \
-        --hartifacts "$test_hartifacts_path" \
-        --override-origin "$HAB_ORIGIN" \
-        --manifest-dir "$test_manifest_path" \
-        --enable-chef-server \
-        --admin-password chefautomate \
-        --accept-terms-and-mlsa
-}
+#!/bin/bash
 
-do_test_deploy() {
-    do_test_deploy_default
-    test_knife
-}
+ #shellcheck disable=SC2034
+ test_name="ocid"
 
-do_create_config() {
-    do_test_deploy_default
-}
+ #shellcheck source=.studio/chef-server-collection
+ source .studio/chef-server-collection
+
+ do_deploy() {
+     #shellcheck disable=SC2154
+     chef-automate deploy config.toml \
+         --hartifacts "$test_hartifacts_path" \
+         --override-origin "$HAB_ORIGIN" \
+         --manifest-dir "$test_manifest_path" \
+         --enable-chef-server \
+         --admin-password chefautomate \
+         --accept-terms-and-mlsa
+ }
+
+ do_test_deploy() {
+     do_test_deploy_default
+     test_if_env_vars_are_configured
+     # test_if_webui_key_is_patched
+     test_if_login_working_with_correct_credentials
+     test_if_login_failing_with_incorrect_credentials
+ }
