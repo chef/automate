344dc68d3e3c8b3d22e22c9704fb3e427c3650af
ayush-billore <ayush.billore@progress.com>
2023-06-19 18:47:44 +0530

    add install_if_missing method
    Signed-off-by: ayush-billore <ayush.billore@progress.com>

--- integration/tests/testing_ocid.sh
+++ integration/tests/testing_ocid.sh
@@ -18,7 +18,6 @@ do_deploy() {
 }
 
 do_test_deploy() {
-    do_test_deploy_default
     test_if_env_vars_are_configured
     test_if_webui_key_is_patched
     test_if_login_working_with_correct_credentials
