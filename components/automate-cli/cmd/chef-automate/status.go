c4f9b97fd4c63cbb1dd7d679c47cc590b869f283
prasad <80198038+prasad927@users.noreply.github.com>
2023-06-22 13:40:41 +0530

    Added status cmd support from bastion (#7950)
    * added status cmd support from bastion
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * removed shorthand
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * changed writer
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * minor fix
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * created constants for commands
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * added test and error for node flag
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * removed extra args from function
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * added more test
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * minor fix
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * minor changes for flags
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * minor fix
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * updated the flags method
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * shifted mock struct
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * added shorthands
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * fixing code smell
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * removed wait-group
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * refactor code
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * improved code
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * minor fix due to latest changes
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * fixing code smells
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * added error msg
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * minor fix for goroutine
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * review comments fix
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * minor fix
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * review comment fixing
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * added docs support
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    * minor docs fix
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>
    
    ---------
    
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>

--- components/automate-cli/cmd/chef-automate/status.go
+++ components/automate-cli/cmd/chef-automate/status.go
@@ -5,23 +5,36 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	api "github.com/chef/automate/api/interservice/deployment"
 	"github.com/chef/automate/components/automate-cli/pkg/docs"
 	"github.com/chef/automate/components/automate-cli/pkg/status"
+	"github.com/chef/automate/components/automate-deployment/pkg/cli"
 	"github.com/chef/automate/components/automate-deployment/pkg/client"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
-var statusCmdFlags = struct {
+type StatusCmdFlags struct {
 	waitForHealthy      bool
 	waitTimeout         int64
 	waitRefreshInterval int64
-}{}
-
+	automate            bool
+	chefServer          bool
+	postgresql          bool
+	opensearch          bool
+	node                string
+}
+type StatusCmdResult struct {
+	cmdResult map[string][]*CmdResult
+	writer    *cli.Writer
+	nodeType  string
+	err       error
+}
 type StatusSummaryCmdFlags struct {
 	node         string
 	isAutomate   bool
@@ -29,7 +42,6 @@ type StatusSummaryCmdFlags struct {
 	isOpenSearch bool
 	isPostgresql bool
 }
-
 type FeStatusValue struct {
 	serviceName string
 	ipAddress   string
@@ -45,36 +57,75 @@ type BeStatusValue struct {
 	upTime      string
 	role        string
 }
+
 type FeStatus []FeStatusValue
 type BeStatus []BeStatusValue
 
-var statusCmd = &cobra.Command{
-	Use:   "status",
-	Short: "Retrieve Chef Automate status",
-	Long:  "Retrieve Chef Automate status. Includes status of Automate services.",
-	RunE:  runStatusCmd,
-}
+const (
+	STATUS_ERROR_ON_SELF_MANAGED = "Showing the status for externally configured %s is not supported."
+	CMD_FAIL_MSG                 = "Command failed on %s node : %s with error:\n %s\n"
+	BACKEND_STATUS_CMD           = "sudo HAB_LICENSE=accept-no-persist hab svc status"
+	FRONTEND_STATUS_CMD          = "sudo chef-automate status"
+)
 
 func newStatusCmd() *cobra.Command {
 
-	statusCmd.PersistentFlags().BoolVarP(
-		&statusCmdFlags.waitForHealthy, "wait-for-healthy", "w", false,
+	statusCmdFlag := StatusCmdFlags{}
+
+	var statusCmd = &cobra.Command{
+		Use:   "status",
+		Short: "Retrieve Chef Automate status",
+		Long:  "Retrieve Chef Automate status. Includes status of Automate services.",
+		RunE:  runStatusCmdGetter(&statusCmdFlag),
+		Annotations: map[string]string{
+			docs.Tag: docs.BastionHost,
+		},
+	}
+
+	statusCmd.Flags().BoolVarP(
+		&statusCmdFlag.waitForHealthy, "wait-for-healthy", "w", false,
 		"Wait until the status response is healthy or the timeout is reached",
 	)
-	statusCmd.PersistentFlags().SetAnnotation("wait-for-healthy", docs.Compatibility, []string{docs.CompatiblewithStandalone})
-	statusCmd.PersistentFlags().Int64VarP(
-		&statusCmdFlags.waitTimeout, "wait-timeout", "t", 600,
+
+	statusCmd.Flags().Int64VarP(
+		&statusCmdFlag.waitTimeout, "wait-timeout", "t", 600,
 		"How many seconds to wait for the status to be healthy before returning an error",
 	)
-	statusCmd.PersistentFlags().SetAnnotation("wait-timeout", docs.Compatibility, []string{docs.CompatiblewithStandalone})
-	statusCmd.PersistentFlags().Int64VarP(
-		&statusCmdFlags.waitRefreshInterval, "wait-refresh-interval", "r", 2,
+
+	statusCmd.Flags().Int64VarP(
+		&statusCmdFlag.waitRefreshInterval, "wait-refresh-interval", "r", 2,
 		"How many seconds to wait between polling for status updates",
 	)
-	statusCmd.PersistentFlags().SetAnnotation("wait-refresh-interval", docs.Compatibility, []string{docs.CompatiblewithStandalone})
 
+	statusCmd.Flags().BoolVarP(&statusCmdFlag.automate, "automate", "a", false, "Shows status from Automate nodes")
+	statusCmd.Flags().SetAnnotation("automate", docs.Compatibility, []string{docs.CompatiblewithHA})
+	statusCmd.Flags().BoolVarP(&statusCmdFlag.chefServer, "chef_server", "c", false, "Shows status from Chef-server nodes")
+	statusCmd.Flags().SetAnnotation("chef_server", docs.Compatibility, []string{docs.CompatiblewithHA})
+	statusCmd.Flags().BoolVarP(&statusCmdFlag.postgresql, "postgresql", "p", false, "Shows status from PostgresQL nodes")
+	statusCmd.Flags().SetAnnotation("postgresql", docs.Compatibility, []string{docs.CompatiblewithHA})
+	statusCmd.Flags().BoolVarP(&statusCmdFlag.opensearch, "opensearch", "o", false, "Shows status from OpenSearch nodes")
+	statusCmd.Flags().SetAnnotation("opensearch", docs.Compatibility, []string{docs.CompatiblewithHA})
+	statusCmd.Flags().BoolVar(&statusCmdFlag.automate, "a2", false, "Shows status from Automate nodes[DUPLICATE]")
+	statusCmd.Flags().SetAnnotation("a2", docs.Compatibility, []string{docs.CompatiblewithHA})
+	statusCmd.Flags().BoolVar(&statusCmdFlag.chefServer, "cs", false, "Shows status from Chef-server nodes[DUPLICATE]")
+	statusCmd.Flags().SetAnnotation("cs", docs.Compatibility, []string{docs.CompatiblewithHA})
+	statusCmd.Flags().BoolVar(&statusCmdFlag.postgresql, "pg", false, "Shows status from PostgresQL nodes[DUPLICATE]")
+	statusCmd.Flags().SetAnnotation("pg", docs.Compatibility, []string{docs.CompatiblewithHA})
+	statusCmd.Flags().BoolVar(&statusCmdFlag.opensearch, "os", false, "Shows status from OpenSearch nodes[DUPLICATE]")
+	statusCmd.Flags().SetAnnotation("os", docs.Compatibility, []string{docs.CompatiblewithHA})
+	statusCmd.Flags().StringVar(&statusCmdFlag.node, "node", "", "Pass this flag to check status of perticular node in the cluster")
+	statusCmd.Flags().SetAnnotation("node", docs.Compatibility, []string{docs.CompatiblewithHA})
+
+	statusCmd.AddCommand(newStatusSummaryCmd())
 	return statusCmd
 }
+
+func runStatusCmdGetter(flags *StatusCmdFlags) func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		return runStatusCmdFlowExecutor(cmd, args, flags)
+	}
+}
+
 func newStatusSummaryCmd() *cobra.Command {
 	var statusSummaryCmdFlags = StatusSummaryCmdFlags{}
 	var statusSummaryCmd = &cobra.Command{
@@ -165,10 +216,13 @@ func executeStatusSummary(cmd *cobra.Command, args []string, statusSummaryCmdFla
 	return nil
 }
 
-func runStatusCmd(cmd *cobra.Command, args []string) error {
+func runStatusCmdFlowExecutor(cmd *cobra.Command, args []string, flags *StatusCmdFlags) error {
 	if isA2HARBFileExist() {
-		return executeAutomateClusterCtlCommand("status", args, statusHAHelpDocs)
+		nodeOpUtils := &NodeUtilsImpl{}
+		remoteExecutor := NewRemoteCmdExecutorWithoutNodeMap(&SSHUtilImpl{}, cli.NewWriter(os.Stdout, os.Stderr, os.Stdin))
+		return runStatusFromBastion(flags, nodeOpUtils, remoteExecutor, printStatusOutput)
 	}
+
 	writeStatus := func(res *api.StatusResponse) {
 		writer.Titlef(
 			"Status from deployment with channel [%s] and type [%s]",
@@ -204,12 +258,12 @@ func runStatusCmd(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	if !statusCmdFlags.waitForHealthy {
+	if !flags.waitForHealthy {
 		return handleBadStatus(res, err)
 	}
 
-	timeout := startTime.Add(time.Second * time.Duration(statusCmdFlags.waitTimeout))
-	refresh := time.NewTicker(time.Second * time.Duration(statusCmdFlags.waitRefreshInterval)).C
+	timeout := startTime.Add(time.Second * time.Duration(flags.waitTimeout))
+	refresh := time.NewTicker(time.Second * time.Duration(flags.waitRefreshInterval)).C
 
 	// Listen to the refresh channel and query for the status. If the timeout
 	// elapses before a healthy status has been returned then write the status
@@ -234,7 +288,234 @@ func runStatusCmd(cmd *cobra.Command, args []string) error {
 	}
 }
 
+func runStatusFromBastion(flags *StatusCmdFlags, nodeOpUtils NodeOpUtils, remoteExe RemoteCmdExecutor, printStatusOutput func(map[string][]*CmdResult, string, *cli.Writer)) error {
+
+	infra, _, err := nodeOpUtils.getHaInfraDetails()
+	if err != nil {
+		return err
+	}
+
+	if !flags.automate && !flags.chefServer && !flags.opensearch && !flags.postgresql {
+		if len(flags.node) != 0 {
+			return status.Errorf(status.InvalidCommandArgsError, "Please provide service flag")
+		}
+
+		flags.automate = true
+		flags.chefServer = true
+
+		if !nodeOpUtils.isManagedServicesOn() {
+			flags.opensearch = true
+			flags.postgresql = true
+		}
+	}
+
+	statusCmdResults := make(chan StatusCmdResult, 4)
+
+	runStatusCmdForFrontEnd(infra, flags, statusCmdResults, remoteExe)
+
+	if nodeOpUtils.isManagedServicesOn() {
+		statusCmdResults <- StatusCmdResult{}
+		statusCmdResults <- StatusCmdResult{}
+		err = getValueFromChannel(statusCmdResults, printStatusOutput)
+		if err != nil {
+			return err
+		}
+		return handleManagedServiceErrorForStatusCmd(flags)
+	}
+
+	runStatusCmdForBackend(infra, flags, statusCmdResults, remoteExe)
+
+	return getValueFromChannel(statusCmdResults, printStatusOutput)
+}
+
+func runStatusCmdForFrontEnd(infra *AutomateHAInfraDetails, flags *StatusCmdFlags, statusCmdResults chan StatusCmdResult, remoteExe RemoteCmdExecutor) {
+
+	if flags.automate {
+		go nodeStatus(*flags, AUTOMATE, infra, statusCmdResults, remoteExe)
+	} else {
+		statusCmdResults <- StatusCmdResult{}
+	}
+
+	if flags.chefServer {
+		flags.automate = false
+		go nodeStatus(*flags, CHEF_SERVER, infra, statusCmdResults, remoteExe)
+	} else {
+		statusCmdResults <- StatusCmdResult{}
+	}
+}
+
+func runStatusCmdForBackend(infra *AutomateHAInfraDetails, flags *StatusCmdFlags, statusCmdResults chan StatusCmdResult, remoteExe RemoteCmdExecutor) {
+
+	if flags.postgresql {
+		flags.automate = false
+		flags.chefServer = false
+		go nodeStatus(*flags, POSTGRESQL, infra, statusCmdResults, remoteExe)
+	} else {
+		statusCmdResults <- StatusCmdResult{}
+	}
+
+	if flags.opensearch {
+		flags.automate = false
+		flags.chefServer = false
+		flags.postgresql = false
+		go nodeStatus(*flags, OPENSEARCH, infra, statusCmdResults, remoteExe)
+	} else {
+		statusCmdResults <- StatusCmdResult{}
+	}
+}
+
+func nodeStatus(flags StatusCmdFlags, nodeType string, infra *AutomateHAInfraDetails, statusCmdResults chan StatusCmdResult, remoteExe RemoteCmdExecutor) {
+	writer := cli.NewWriter(os.Stdout, os.Stderr, os.Stdin)
+	remoteExe.SetWriter(writer)
+	nodeMap := constructNodeMapForStatus(&flags, infra)
+	cmdResult, err := remoteExe.ExecuteWithNodeMap(nodeMap)
+	statusCmdResults <- StatusCmdResult{
+		cmdResult: cmdResult,
+		writer:    writer,
+		nodeType:  nodeType,
+		err:       err,
+	}
+}
+
+func printStatusOutput(cmdResult map[string][]*CmdResult, remoteService string, writer *cli.Writer) {
+	writer.Printf("\n=====================================================%s=======================================================\n", "Status on "+remoteService)
+
+	for _, value := range cmdResult {
+		for _, cmdResult := range value {
+			if cmdResult.Error != nil {
+				printStatusErrorOutput(cmdResult, remoteService, writer)
+			} else {
+				writer.Printf("Output for Host IP %s : \n%s", cmdResult.HostIP, cmdResult.Output+"\n")
+				writer.Success("Command is executed on " + remoteService + " node : " + cmdResult.HostIP + "\n")
+			}
+			writer.BufferWriter().Flush()
+		}
+	}
+}
+
+func printStatusErrorOutput(cmdResult *CmdResult, remoteService string, writer *cli.Writer) {
+	isOutputError := false
+	if strings.Contains(cmdResult.Output, "UnhealthyStatusError") {
+		isOutputError = true
+		writer.Failf("Output for Host IP %s : \n%s", cmdResult.HostIP, cmdResult.Output+"\n")
+		writer.Success("Command is executed on " + remoteService + " node : " + cmdResult.HostIP + "\n")
+	}
+
+	if strings.Contains(cmdResult.Output, "DeploymentServiceUnreachableError") {
+		isOutputError = true
+		writer.Failf(CMD_FAIL_MSG, remoteService, cmdResult.HostIP, cmdResult.Output)
+	}
+
+	if !isOutputError {
+		writer.Failf(CMD_FAIL_MSG, remoteService, cmdResult.HostIP, cmdResult.Error.Error())
+	}
+}
+
+func getValueFromChannel(statusCmdResults chan StatusCmdResult, printStatusOutput func(map[string][]*CmdResult, string, *cli.Writer)) error {
+	for i := 0; i < 4; i++ {
+		cmdResult := <-statusCmdResults
+		if cmdResult.err != nil {
+			return cmdResult.err
+		} else {
+			if cmdResult.writer != nil {
+				printStatusOutput(cmdResult.cmdResult, cmdResult.nodeType, cmdResult.writer)
+			}
+		}
+	}
+	return nil
+}
+
+func handleManagedServiceErrorForStatusCmd(flags *StatusCmdFlags) error {
+
+	if flags.postgresql && flags.opensearch {
+		return status.Errorf(status.InvalidCommandArgsError, STATUS_ERROR_ON_SELF_MANAGED, POSTGRESQL+" and "+OPENSEARCH)
+	}
+
+	if flags.postgresql {
+		return status.Errorf(status.InvalidCommandArgsError, STATUS_ERROR_ON_SELF_MANAGED, POSTGRESQL)
+	}
+
+	if flags.opensearch {
+		return status.Errorf(status.InvalidCommandArgsError, STATUS_ERROR_ON_SELF_MANAGED, OPENSEARCH)
+	}
+
+	return nil
+}
+
+func constructNodeMapForStatus(flags *StatusCmdFlags, infra *AutomateHAInfraDetails) *NodeTypeAndCmd {
+	commandFrontEnd := buildFrontEndStatusCmd(flags)
+	nodeMap := &NodeTypeAndCmd{
+		Frontend: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      commandFrontEnd,
+				WaitTimeout:              int(flags.waitTimeout),
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		Automate: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      commandFrontEnd,
+				WaitTimeout:              int(flags.waitTimeout),
+				ErrorCheckEnableInOutput: true,
+				NodeIps:                  []string{flags.node},
+				NodeType:                 flags.automate,
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		ChefServer: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      commandFrontEnd,
+				WaitTimeout:              int(flags.waitTimeout),
+				ErrorCheckEnableInOutput: true,
+				NodeIps:                  []string{flags.node},
+				NodeType:                 flags.chefServer,
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		Postgresql: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      BACKEND_STATUS_CMD,
+				NodeIps:                  []string{flags.node},
+				ErrorCheckEnableInOutput: true,
+				NodeType:                 flags.postgresql,
+				WaitTimeout:              int(flags.waitTimeout),
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		Opensearch: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      BACKEND_STATUS_CMD,
+				NodeIps:                  []string{flags.node},
+				ErrorCheckEnableInOutput: true,
+				NodeType:                 flags.opensearch,
+				WaitTimeout:              int(flags.waitTimeout),
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		Infra: infra,
+	}
+	return nodeMap
+}
+
+func buildFrontEndStatusCmd(flags *StatusCmdFlags) string {
+	command := FRONTEND_STATUS_CMD
+
+	if flags.waitForHealthy {
+		command += " -w"
+		command += " -t" + " " + fmt.Sprintf("%v", flags.waitTimeout)
+	}
+
+	command += " -r" + " " + fmt.Sprintf("%v", flags.waitRefreshInterval)
+
+	return command
+}
+
 func init() {
-	statusCmd.AddCommand(newStatusSummaryCmd())
-	RootCmd.AddCommand(newStatusCmd())
+	cmd := newStatusCmd()
+	RootCmd.AddCommand(cmd)
 }
---
+++ components/automate-cli/cmd/chef-automate/status_test.go
@@ -0,0 +1,289 @@
+package main
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/chef/automate/components/automate-cli/pkg/status"
+	"github.com/chef/automate/components/automate-deployment/pkg/cli"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildFrontEndStatusCmd(t *testing.T) {
+	type testCase struct {
+		flags           *StatusCmdFlags
+		expectedCommand string
+	}
+
+	testCases := []testCase{
+		{
+			flags: &StatusCmdFlags{
+				waitRefreshInterval: 2,
+				waitTimeout:         600,
+			},
+			expectedCommand: "sudo chef-automate status -r 2",
+		},
+		{
+			flags: &StatusCmdFlags{
+				waitRefreshInterval: 2,
+				waitTimeout:         600,
+				waitForHealthy:      true,
+			},
+			expectedCommand: "sudo chef-automate status -w -t 600 -r 2",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run("Checking command creation", func(t *testing.T) {
+			commandGet := buildFrontEndStatusCmd(testCase.flags)
+			assert.Equal(t, testCase.expectedCommand, commandGet)
+		})
+	}
+}
+
+func TestHandleManagedServiceForStatusCmd(t *testing.T) {
+
+	testCases := []struct {
+		flags          *StatusCmdFlags
+		errorExepected error
+	}{
+		{
+			flags: &StatusCmdFlags{
+				postgresql: true,
+			},
+			errorExepected: status.Errorf(status.InvalidCommandArgsError, STATUS_ERROR_ON_SELF_MANAGED, POSTGRESQL),
+		},
+		{
+			flags: &StatusCmdFlags{
+				opensearch: true,
+			},
+			errorExepected: status.Errorf(status.InvalidCommandArgsError, STATUS_ERROR_ON_SELF_MANAGED, OPENSEARCH),
+		},
+		{
+			flags:          &StatusCmdFlags{},
+			errorExepected: nil,
+		},
+		{
+			flags: &StatusCmdFlags{
+				postgresql: true,
+				opensearch: true,
+			},
+			errorExepected: status.Errorf(status.InvalidCommandArgsError, STATUS_ERROR_ON_SELF_MANAGED, POSTGRESQL+" and "+OPENSEARCH),
+		},
+	}
+
+	for _, testCase := range testCases {
+		err := handleManagedServiceErrorForStatusCmd(testCase.flags)
+
+		if testCase.errorExepected != nil {
+			assert.EqualError(t, err, testCase.errorExepected.Error())
+		} else {
+			assert.Nil(t, err)
+		}
+	}
+}
+
+func TestConstructNodeMapForStatus(t *testing.T) {
+	type testCase struct {
+		flags           *StatusCmdFlags
+		nodeMapExpected *NodeTypeAndCmd
+	}
+	infra := &AutomateHAInfraDetails{}
+
+	testCases := []testCase{
+		{
+			flags: &StatusCmdFlags{
+				waitForHealthy:      false,
+				waitTimeout:         600,
+				waitRefreshInterval: 10,
+				automate:            true,
+			},
+			nodeMapExpected: &NodeTypeAndCmd{
+				Frontend: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      "sudo chef-automate status -r 10",
+						WaitTimeout:              600,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				Automate: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      "sudo chef-automate status -r 10",
+						WaitTimeout:              600,
+						ErrorCheckEnableInOutput: true,
+						NodeIps:                  []string{""},
+						NodeType:                 true,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				ChefServer: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      "sudo chef-automate status -r 10",
+						WaitTimeout:              600,
+						ErrorCheckEnableInOutput: true,
+						NodeIps:                  []string{""},
+						NodeType:                 false,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				Postgresql: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      BACKEND_STATUS_CMD,
+						NodeIps:                  []string{""},
+						ErrorCheckEnableInOutput: true,
+						NodeType:                 false,
+						WaitTimeout:              600,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				Opensearch: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      BACKEND_STATUS_CMD,
+						NodeIps:                  []string{""},
+						ErrorCheckEnableInOutput: true,
+						NodeType:                 false,
+						WaitTimeout:              600,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				Infra: infra,
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		nodeMapGet := constructNodeMapForStatus(testCase.flags, infra)
+		assert.Equal(t, testCase.nodeMapExpected, nodeMapGet)
+	}
+}
+
+func TestRunStatusFromBastion(t *testing.T) {
+	type testCase struct {
+		description       string
+		flags             *StatusCmdFlags
+		mockNodeOpUtils   *MockNodeUtilsImpl
+		mockRemoteCmdExec *MockRemoteCmdExecutor
+		errorWant         error
+	}
+
+	printStatusOutput := func(m map[string][]*CmdResult, s string, w *cli.Writer) {}
+
+	testCases := []testCase{
+		{
+			description: "No service flag provided and node flag is provided",
+			flags: &StatusCmdFlags{
+				node: "1",
+			},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+			},
+			errorWant: status.Errorf(status.InvalidCommandArgsError, "Please provide service flag"),
+		},
+		{
+			description: "Error while reading infra details",
+			flags:       &StatusCmdFlags{},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return nil, nil, errors.New("Error occured while reading infra details")
+				},
+			},
+			errorWant: errors.New("Error occured while reading infra details"),
+		},
+		{
+			description: "Want status of all services",
+			flags:       &StatusCmdFlags{},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return false
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					return map[string][]*CmdResult{}, nil
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: nil,
+		},
+		{
+			description: "Want status of all services but error occured while remote execution",
+			flags:       &StatusCmdFlags{},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return false
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					return map[string][]*CmdResult{}, errors.New("Some error occured while remote execution")
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: errors.New("Some error occured while remote execution"),
+		},
+		{
+			description: "Want status of all services with managed services",
+			flags:       &StatusCmdFlags{},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return false
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					return map[string][]*CmdResult{}, nil
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: nil,
+		},
+		{
+			description: "Managed services and pg flag provided",
+			flags: &StatusCmdFlags{
+				postgresql: true,
+			},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return true
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					return map[string][]*CmdResult{}, nil
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: status.Errorf(status.InvalidCommandArgsError, STATUS_ERROR_ON_SELF_MANAGED, POSTGRESQL),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			err := runStatusFromBastion(testCase.flags, testCase.mockNodeOpUtils, testCase.mockRemoteCmdExec, printStatusOutput)
+			if err != nil {
+				assert.EqualError(t, testCase.errorWant, err.Error())
+			} else {
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
