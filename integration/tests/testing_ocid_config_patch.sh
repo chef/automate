98b98a5178819d23812070004d09cead4673ce46
ayush-billore <ayush.billore@progress.com>
2023-06-27 17:12:08 +0530

    test if deployment removal works
    Signed-off-by: ayush-billore <ayush.billore@progress.com>

--- integration/tests/testing_ocid_config_patch.sh
+++ integration/tests/testing_ocid_config_patch.sh
@@ -6,16 +6,16 @@ test_name="ocid-config-patch"
 #shellcheck source=.studio/chef-server-collection
 source .studio/chef-server-collection
 
-do_deploy() {
-    #shellcheck disable=SC2154
-    chef-automate deploy config.toml \
-        --hartifacts "$test_hartifacts_path" \
-        --override-origin "$HAB_ORIGIN" \
-        --manifest-dir "$test_manifest_path" \
-        --enable-chef-server \
-        --admin-password chefautomate \
-        --accept-terms-and-mlsa
-}
+# do_deploy() {
+#     #shellcheck disable=SC2154
+#     chef-automate deploy config.toml \
+#         --hartifacts "$test_hartifacts_path" \
+#         --override-origin "$HAB_ORIGIN" \
+#         --manifest-dir "$test_manifest_path" \
+#         --enable-chef-server \
+#         --admin-password chefautomate \
+#         --accept-terms-and-mlsa
+# }
 
 do_create_config() {
     do_create_config_default
