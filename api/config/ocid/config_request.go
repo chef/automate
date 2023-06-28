f181a814d228f7cda85fe5b9a29ceb3b899d7cdd
ayush-billore <ayush.billore@progress.com>
2023-06-28 12:58:02 +0530

    update config_request values by taking reference from erchef component
    Signed-off-by: ayush-billore <ayush.billore@progress.com>

--- api/config/ocid/config_request.go
+++ api/config/ocid/config_request.go
@@ -32,9 +32,9 @@ func DefaultConfigRequest() *ConfigRequest {
 	c.V1.Sys.Log.RotationMaxFiles = w.Int32(10)
 	c.V1.Sys.Log.MaxErrorLogsPerSecond = w.Int32(1000)
 
-	c.V1.Sys.Sql.Timeout = w.Int32(5000)
-	c.V1.Sys.Sql.PoolInitSize = w.Int32(10)
-	c.V1.Sys.Sql.PoolMaxSize = w.Int32(20)
+	c.V1.Sys.Sql.Timeout = w.Int32(30000)
+	c.V1.Sys.Sql.PoolInitSize = w.Int32(25)
+	c.V1.Sys.Sql.PoolMaxSize = w.Int32(100)
 	c.V1.Sys.Sql.PoolQueueMax = w.Int32(50)
 	c.V1.Sys.Sql.PoolQueueTimeout = w.Int32(2000)
 
