1f5049a1d4dea2a5bff8a2b73f4d3b3f75dbf7f3
prasad927 <Prasad.Chaudhari@progress.com>
2023-06-30 13:53:28 +0530

    removed bastion to dbs ports check
    Signed-off-by: prasad927 <Prasad.Chaudhari@progress.com>

--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/firewallchecktrigger/vars.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/firewallchecktrigger/vars.go
@@ -4,6 +4,6 @@ var (
 	postgresqlTCPPorts   = [4]string{"9631", "7432", "5432", "6432"}
 	opensearchTCPPorts   = [3]string{"9631", "9200", "9300"}
 	a2CsTCPPorts         = [1]string{"22"}
-	postgresBastionPorts = [2]string{"22", "7432"}
-	ossBastionPorts      = [2]string{"22", "9200"}
+	postgresBastionPorts = [1]string{"22"}
+	ossBastionPorts      = [1]string{"22"}
 )
