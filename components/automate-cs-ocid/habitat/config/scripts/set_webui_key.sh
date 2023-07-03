0832d9da8e933d91506604c6206c2a9602ec2c37
RajeshPaul38 <85236476+RajeshPaul38@users.noreply.github.com>
2023-07-03 16:47:54 +0530

    Generate secret_key_base for OCID rails app (#7932)
    * Generate secret_key_base for OCID rails app
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    file renamed
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    added provision for user to provide the secret_key_base for OCID rails application
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    doing a tidy up of the generated secret_key_base to remove all invalid characters
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    * code review changes
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    ---------
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>

--- components/automate-cs-ocid/habitat/config/scripts/set_webui_key.sh
+++ components/automate-cs-ocid/habitat/config/scripts/set_webui_key.sh
@@ -5,5 +5,5 @@ if [[ -z $WEBUI_KEY ]]
     echo "Could not find the webui key in erchef service. Pls wait for erchef to be running before OCID can be started..."
     exit 1
 fi
-export PRIVATE_CHEF_SECRETS_PATH="$OCID_CONFIG_FOLDER_PATH/private-chef-secrets.json"
+export PRIVATE_CHEF_SECRETS_PATH="$OCID_PKG_CONFIG_FOLDER_PATH/private-chef-secrets.json"
 ruby "{{pkg.svc_config_path}}/tasks/set_webui_key.rb"
