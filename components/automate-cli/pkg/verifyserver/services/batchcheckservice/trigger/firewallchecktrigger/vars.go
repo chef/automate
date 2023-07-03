2a65437ea1883c3d34e83dc83567aac268843727
Vipin Yadav <vipin.yadav@progress.com>
2023-07-03 18:51:48 +0530

    Changes for port 22 from bastion to cluster
    Signed-off-by: Vipin Yadav <vipin.yadav@progress.com>

--- components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/firewallchecktrigger/vars.go
+++ components/automate-cli/pkg/verifyserver/services/batchcheckservice/trigger/firewallchecktrigger/vars.go
@@ -1,9 +1,6 @@
 package firewallchecktrigger
 
 var (
-	postgresqlTCPPorts   = [4]string{"9631", "7432", "5432", "6432"}
-	opensearchTCPPorts   = [3]string{"9631", "9200", "9300"}
-	a2CsTCPPorts         = [1]string{"22"}
-	postgresBastionPorts = [1]string{"22"}
-	ossBastionPorts      = [1]string{"22"}
+	postgresqlTCPPorts = [4]string{"9631", "7432", "5432", "6432"}
+	opensearchTCPPorts = [3]string{"9631", "9200", "9300"}
 )
