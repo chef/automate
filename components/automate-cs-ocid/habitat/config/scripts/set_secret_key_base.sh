a5a8fedfbc7dfde5c32b3f62c88641642e0ac96c
RajeshPaul38 <85236476+RajeshPaul38@users.noreply.github.com>
2023-07-03 16:47:54 +0530

    Generate secret_key_base for OCID rails app (#7932)
    * Generate secret_key_base for OCID rails app
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    file renamed
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    added provision for user to provide the secret_key_base for OCID rails application
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    doing a tidy up of the generated secret_key_base to remove all invalid characters
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    * code review changes
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>
    
    ---------
    
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>

---
+++ components/automate-cs-ocid/habitat/config/scripts/set_secret_key_base.sh
@@ -0,0 +1,9 @@
+export WEBUI_SRC_PATH="/hab/svc/automate-cs-oc-erchef/data/webui_priv.pem"
+export SECRET_KEY_BASE_FILE_PATH="$OCID_PKG_CONFIG_FOLDER_PATH/secret_key_base.txt"
+export CUSTOM_SECRET_KEY_BASE="{{cfg.ocid.secret_key_base}}"
+
+ruby "{{pkg.svc_config_path}}/tasks/save_secret_key_base_file.rb"
+
+# Using `2>/dev/null` along with `cat` command suppresses the error
+# in case file doesn't exist and it returns empty string.
+export SECRET_KEY_BASE=`cat $SECRET_KEY_BASE_FILE_PATH 2>/dev/null`
