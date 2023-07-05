9b152d16f0dd05dff2648b5acf216ba4c26723dc
Jay Vikram Sharma <36661596+jayvikramsharma1@users.noreply.github.com>
2023-07-05 13:23:22 +0530

    checking for output.auto.tfvars in terraform dir in case of not available in referance arch dir (#8035)
    * checking for output.auto.tfvars in terraform dir in case of not available in referance arch dir
    
    Signed-off-by: Jay Sharma <jsharma@progress.com>
    
    * adding comments in code for better understanding of logic
    
    Signed-off-by: Jay Sharma <jsharma@progress.com>
    
    ---------
    
    Signed-off-by: Jay Sharma <jsharma@progress.com>

--- components/automate-cli/cmd/chef-automate/fetchAwsConfigDetails.go
+++ components/automate-cli/cmd/chef-automate/fetchAwsConfigDetails.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"os"
 	"path/filepath"
+
+	"github.com/pkg/errors"
 )
 
 type configDetails struct {
@@ -21,7 +24,13 @@ type configDetails struct {
 var ConvTfvarToJsonFunc func(string) string = convTfvarToJson
 
 func fetchAwsConfigFromTerraform() (*configDetails, error) {
-	AwsConfigJsonString := ConvTfvarToJsonFunc(filepath.Join(initConfigHabA2HAPathFlag.a2haDirPath, "terraform", "reference_architectures", "deployment", "output.auto.tfvars"))
+	// Fetching output.auto.tfvars from reference_architectures directory for pre-deployment state
+	outputAutoTfVarsFilePath := filepath.Join(initConfigHabA2HAPathFlag.a2haDirPath, "terraform", "reference_architectures", "deployment", "output.auto.tfvars")
+	if _, err := os.Stat(outputAutoTfVarsFilePath); errors.Is(err, os.ErrNotExist) {
+		// In case of post-deployment after workspace upgrade output.auto.tfvars might not be available in reference_architectures directory it will be available in terraform directory
+		outputAutoTfVarsFilePath = filepath.Join(initConfigHabA2HAPathFlag.a2haDirPath, "terraform", "output.auto.tfvars")
+	}
+	AwsConfigJsonString := ConvTfvarToJsonFunc(outputAutoTfVarsFilePath)
 	awsAutoTfvarConfig, err := getJsonFromTerraformOutputAutoTfVarsFile(AwsConfigJsonString)
 	if err != nil {
 		return nil, err
