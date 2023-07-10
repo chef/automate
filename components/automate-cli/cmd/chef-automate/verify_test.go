02cba07d808afd2ced469b947f27ff4c3d6e51b4
Abdul-Az <aazeez@progress.com>
2023-07-10 20:49:14 +0530

    teest cases fix
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- components/automate-cli/cmd/chef-automate/verify_test.go
+++ components/automate-cli/cmd/chef-automate/verify_test.go
@@ -27,7 +27,9 @@ const (
 	DARWIN              = "darwin"
 )
 
-var AwsAutoTfvarsJsonStringEmpty = `{}`
+var AwsAutoTfvarsJsonStringEmpty = `
+{"automate_private_ips":["10.0.130.162", "10.0.153.152"], "chef_server_private_ips":["10.0.136.84", "10.0.149.79"], "postgresql_private_ips":["10.0.135.82", "10.0.159.64", "10.0.161.255"], "opensearch_private_ips":["10.0.133.254", "10.0.144.64", "10.0.163.250"], "automate_lb_fqdn":"A2-df60949d-automate-lb-1317318119.eu-west-2.elb.amazonaws.com", "automate_frontend_url":"https://A2-df60949d-automate-lb-1317318119.eu-west-2.elb.amazonaws.com", "bucket_name":"test", "aws_os_snapshot_role_arn":" ", "os_snapshot_user_access_key_id":" ", "os_snapshot_user_access_key_secret":" "}
+`
 
 func TestRunVerifyCmd(t *testing.T) {
 	tests := []struct {
