10c266e1e8020cb07d4138f3b9d4360b9a43c56e
Vivek Shankar <vshankar@progress.com>
2023-07-17 15:36:19 +0530

    updated message
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- components/automate-cli/pkg/verifyserver/constants/skipmessages.go
+++ components/automate-cli/pkg/verifyserver/constants/skipmessages.go
@@ -7,6 +7,6 @@ const (
 	SKIP_BACKUP_TEST_MESSAGE                 = "- No Backup configuration was found\n- Backup settings wont be set"
 	SKIP_OS_BUCKET_TEST_MESSAGE              = "- No External OpenSearch configuration provided\n- No S3 Backup configuration was provided"
 	SKIP_OS_BUCKET_TEST_SELF_MANAGED_MESSAGE = "- External OpenSearch is self managed"
-	SKIP_MISSING_HARDWARE_MESSAGE            = "- Instance counts and Instance IPs are missing"
+	SKIP_MISSING_HARDWARE_MESSAGE            = "- Instance count and Instance IPs are missing"
 	SKIP_SSH_USER_TEST_MESSAGE               = "- SSH credentials are missing"
 )
