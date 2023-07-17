0ad0b6f79a1af2bd09488fb66e47f551f48e76ad
Vivek Shankar <vshankar@progress.com>
2023-07-17 13:38:01 +0530

    added more elaborate messsages
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- components/automate-cli/pkg/verifyserver/constants/skipmessages.go
+++ components/automate-cli/pkg/verifyserver/constants/skipmessages.go
@@ -1,13 +1,13 @@
 package constants
 
 const (
-	SKIP_CERT_TEST_MESSAGE                   = "Using default certificates"
-	SKIP_NODE_CERT_TEST_MESSAGE              = "Using default certificates for nodes"
-	SKIP_MANAGED_OS_TEST_MESSAGE             = "Using Chef Managed OpenSearch"
-	SKIP_MANAGED_PG_TEST_MESSAGE             = "Using Chef Managed PostgreSQL"
-	SKIP_BACKUP_TEST_MESSAGE                 = "Using Chef Managed PostgreSQL"
-	SKIP_OS_BUCKET_TEST_MESSAGE              = "External OpenSearch configuration or Backup config not provided"
-	SKIP_OS_BUCKET_TEST_SELF_MANAGED_MESSAGE = "External OpenSearch is self managed"
-	SKIP_MISSING_HARDWARE_MESSAGE            = "Missing instance counts and instance IPs"
-	SKIP_SSH_USER_TEST_MESSAGE               = "SSH credentials missing"
+	SKIP_CERT_TEST_MESSAGE                   = "Using Default certificates since no Custom certificates have been provided"
+	SKIP_NODE_CERT_TEST_MESSAGE              = "Using Default certificates for this node since no Custom certificates have been provided"
+	SKIP_MANAGED_OS_TEST_MESSAGE             = "Using Chef Managed OpenSearch since no External OpenSearch configuration found"
+	SKIP_MANAGED_PG_TEST_MESSAGE             = "Using Chef Managed PostgreSQL since no External OpenSearch configuration found"
+	SKIP_BACKUP_TEST_MESSAGE                 = "Backup settings wont be set since no Backup configuration was found"
+	SKIP_OS_BUCKET_TEST_MESSAGE              = "Check skipped since no External OpenSearch configuration or no S3 Backup configuration was provided"
+	SKIP_OS_BUCKET_TEST_SELF_MANAGED_MESSAGE = "Check skipped since External OpenSearch is self managed"
+	SKIP_MISSING_HARDWARE_MESSAGE            = "Check skipped since Instance counts and Instance IPs are missing"
+	SKIP_SSH_USER_TEST_MESSAGE               = "Check skipped since SSH credentials are missing"
 )
