7dd9d90bac12635d2bb47b22512e6d0ca6ee381b
Vivek Shankar <vshankar@progress.com>
2023-07-17 19:46:44 +0530

    fqdn message
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- components/automate-cli/pkg/verifyserver/constants/skipmessages.go
+++ components/automate-cli/pkg/verifyserver/constants/skipmessages.go
@@ -2,7 +2,7 @@ package constants
 
 const (
 	SKIP_CERT_TEST_MESSAGE                   = "- No Custom certificates provided\n- Using Default certificates"
-	SKIP_CS_FQDN_TEST_MESSAGE                = "- FQDN and LoadBalancer Root CA missing\n- Public and private keys missing"
+	SKIP_CS_FQDN_TEST_MESSAGE                = "- FQDN and LoadBalancer Root CA missing"
 	SKIP_MANAGED_OS_TEST_MESSAGE             = "- No External OpenSearch configuration found\n- Using Chef Managed OpenSearch"
 	SKIP_MANAGED_PG_TEST_MESSAGE             = "- No External OpenSearch configuration found\n- Using Chef Managed PostgreSQL"
 	SKIP_BACKUP_TEST_MESSAGE_NFS             = "- Backup configuration not set to file_system/efs"
