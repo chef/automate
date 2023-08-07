c02be8bf39b93376fac94edee9804a433b4cbe22
Jay Sharma <jsharma@progress.com>
2023-08-07 15:52:45 +0530

    verify-command
    Signed-off-by: Jay Sharma <jsharma@progress.com>

--- components/automate-cli/cmd/chef-automate/deployHa.go
+++ components/automate-cli/cmd/chef-automate/deployHa.go
@@ -5,16 +5,47 @@ package main
 import (
 	"strings"
 
+	"github.com/chef/automate/components/automate-cli/pkg/verifysystemdcreate"
+	"github.com/chef/automate/lib/config"
+	"github.com/chef/automate/lib/httputils"
+	"github.com/chef/automate/lib/logger"
+	"github.com/chef/automate/lib/platform/command"
+	"github.com/chef/automate/lib/sshutils"
 	"github.com/pkg/errors"
 )
 
 func executeDeployment(args []string) error {
 	var indexOfConfig = 0
+	var configFile = ""
 	for i, a := range args {
 		if strings.Contains(a, ".toml") {
+			configFile = a
 			indexOfConfig = i
 			break
 		}
+	}
+	if len(configFile) > 1 {
+		log, err := logger.NewLogger("text", "info")
+		if err != nil {
+			return err
+		}
+
+		createSystemdServiceWithBinary, err := verifysystemdcreate.NewCreateSystemdService(
+			verifysystemdcreate.NewSystemdCreateUtilsImpl(),
+			BINARY_DESTINATION_FOLDER,
+			SYSTEMD_PATH,
+			false,
+			writer,
+		)
+		if err != nil {
+			return err
+		}
+		deps := &verifyCmdDeps{
+			getAutomateHAInfraDetails: getAutomateHAInfraDetails,
+			PopulateHaCommonConfig:    PopulateHaCommonConfig,
+		}
+		c := NewVerifyCmdFlow(httputils.NewClient(log), createSystemdServiceWithBinary, verifysystemdcreate.NewSystemdCreateUtilsImpl(), config.NewHaDeployConfig(), sshutils.NewSSHUtilWithCommandExecutor(sshutils.NewSshClient(), log, command.NewExecExecutor()), writer, deps)
+		err = c.RunVerify(configFile)
 	}
 	args = append(args[:indexOfConfig], args[indexOfConfig+1:]...)
 	args = append(args, "-y")
