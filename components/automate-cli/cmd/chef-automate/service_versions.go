94d8c6fbc8218e0725896e215fae47e81309d92a
SahithiMy <sahithi.mylangam@progress.com>
2023-08-09 15:48:28 +0530

    Service_versions command
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>

--- components/automate-cli/cmd/chef-automate/service_versions.go
+++ components/automate-cli/cmd/chef-automate/service_versions.go
@@ -4,26 +4,287 @@ package main
 
 import (
 	"context"
+	"os"
+	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	api "github.com/chef/automate/api/interservice/deployment"
 	"github.com/chef/automate/components/automate-cli/pkg/docs"
 	"github.com/chef/automate/components/automate-cli/pkg/status"
+	"github.com/chef/automate/components/automate-deployment/pkg/cli"
 	"github.com/chef/automate/components/automate-deployment/pkg/client"
 )
 
-var serviceVersionsCmd = &cobra.Command{
-	Use:   "service-versions",
-	Short: "Retrieve the versions of the individual Chef Automate services",
-	Long:  "Retrieve the versions of the individual Chef Automate services",
-	RunE:  runServiceVersionsCmd,
-	Annotations: map[string]string{
-		docs.Tag: docs.FrontEnd,
-	},
+const (
+	SERVICE_VERSIONS_ERROR_ON_SELF_MANAGED = "Showing the service-versions for externally configured %s is not supported."
+	BACKEND_SERVICE_VERSIONS_CMD           = `sudo HAB_LICENSE=accept-no-persist hab svc status | awk 'NR>1 {split($1,a,"/"); printf "%s/%s %s %s\n", a[1], a[2], a[3], a[4]}'`
+	FRONTEND_SERVICE_VERSIONS_CMD          = "sudo chef-automate service-versions"
+)
+
+type ServiceVersionsCmdFlags struct {
+	automate   bool
+	chefServer bool
+	postgresql bool
+	opensearch bool
+	node       string
+}
+
+var serviceVersionsCmdFlag = ServiceVersionsCmdFlags{}
+
+type ServiceVersionsCmdResult struct {
+	cmdResult map[string][]*CmdResult
+	writer    *cli.Writer
+	nodeType  string
+	err       error
+}
+
+func init() {
+	var serviceVersionsCmd = &cobra.Command{
+		Use:   "service-versions",
+		Short: "Retrieve the versions of the individual Chef Automate services",
+		Long:  "Retrieve the versions of the individual Chef Automate services",
+		RunE:  runServiceVersionsCmd,
+		Annotations: map[string]string{
+			docs.Tag: docs.BastionHost,
+		},
+	}
+	serviceVersionsCmd.PersistentFlags().BoolVarP(&serviceVersionsCmdFlag.automate, "automate", "a", false, "Shows service-versions for Automate nodes")
+	serviceVersionsCmd.PersistentFlags().SetAnnotation("automate", docs.Compatibility, []string{docs.CompatiblewithHA})
+	serviceVersionsCmd.PersistentFlags().BoolVarP(&serviceVersionsCmdFlag.chefServer, "chef_server", "c", false, "Shows service-versions for Chef-server nodes")
+	serviceVersionsCmd.PersistentFlags().SetAnnotation("chef_server", docs.Compatibility, []string{docs.CompatiblewithHA})
+	serviceVersionsCmd.PersistentFlags().BoolVarP(&serviceVersionsCmdFlag.postgresql, "postgresql", "p", false, "Shows service-versions for PostgresQL nodes")
+	serviceVersionsCmd.PersistentFlags().SetAnnotation("postgresql", docs.Compatibility, []string{docs.CompatiblewithHA})
+	serviceVersionsCmd.PersistentFlags().BoolVarP(&serviceVersionsCmdFlag.opensearch, "opensearch", "o", false, "Shows service-versions for OpenSearch nodes")
+	serviceVersionsCmd.PersistentFlags().SetAnnotation("opensearch", docs.Compatibility, []string{docs.CompatiblewithHA})
+	serviceVersionsCmd.PersistentFlags().BoolVar(&serviceVersionsCmdFlag.automate, "a2", false, "Shows service-versions for Automate nodes[DUPLICATE]")
+	serviceVersionsCmd.PersistentFlags().SetAnnotation("a2", docs.Compatibility, []string{docs.CompatiblewithHA})
+	serviceVersionsCmd.PersistentFlags().BoolVar(&serviceVersionsCmdFlag.chefServer, "cs", false, "Shows service-versions for Chef-server nodes[DUPLICATE]")
+	serviceVersionsCmd.PersistentFlags().SetAnnotation("cs", docs.Compatibility, []string{docs.CompatiblewithHA})
+	serviceVersionsCmd.PersistentFlags().BoolVar(&serviceVersionsCmdFlag.postgresql, "pg", false, "Shows service-versions for PostgresQL nodes[DUPLICATE]")
+	serviceVersionsCmd.PersistentFlags().SetAnnotation("pg", docs.Compatibility, []string{docs.CompatiblewithHA})
+	serviceVersionsCmd.PersistentFlags().BoolVar(&serviceVersionsCmdFlag.opensearch, "os", false, "Shows service-versions for OpenSearch nodes[DUPLICATE]")
+	serviceVersionsCmd.PersistentFlags().SetAnnotation("os", docs.Compatibility, []string{docs.CompatiblewithHA})
+	serviceVersionsCmd.PersistentFlags().StringVar(&serviceVersionsCmdFlag.node, "node", "", "Pass this flag to check service-versions of perticular node in the cluster")
+	serviceVersionsCmd.PersistentFlags().SetAnnotation("node", docs.Compatibility, []string{docs.CompatiblewithHA})
+	RootCmd.AddCommand(serviceVersionsCmd)
 }
 
 func runServiceVersionsCmd(cmd *cobra.Command, args []string) error {
+	if isA2HARBFileExist() {
+		nodeOpUtils := &NodeUtilsImpl{}
+		remoteExecutor := NewRemoteCmdExecutorWithoutNodeMap(&SSHUtilImpl{}, cli.NewWriter(os.Stdout, os.Stderr, os.Stdin))
+		if err := isFlagSet(cmd); err != nil {
+			return err
+		}
+		return runServiceVersionsFromBastion(&serviceVersionsCmdFlag, nodeOpUtils, remoteExecutor, printServiceVersionsOutput)
+	}
+	if err := runServiceVersionsOnStandalone(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func runServiceVersionsFromBastion(flags *ServiceVersionsCmdFlags, nodeOpUtils NodeOpUtils, remoteExe RemoteCmdExecutor, printServiceVersionsOutput func(map[string][]*CmdResult, string, *cli.Writer)) error {
+
+	infra, _, err := nodeOpUtils.getHaInfraDetails()
+	if err != nil {
+		return err
+	}
+	serviceVersionsCmdResults := make(chan ServiceVersionsCmdResult, 4)
+
+	runServiceVersionsCmdForFrontEnd(flags, infra, serviceVersionsCmdResults, remoteExe)
+
+	if nodeOpUtils.isManagedServicesOn() {
+		serviceVersionsCmdResults <- ServiceVersionsCmdResult{}
+		serviceVersionsCmdResults <- ServiceVersionsCmdResult{}
+		err = getValueFromResultChannel(serviceVersionsCmdResults, printServiceVersionsOutput)
+		if err != nil {
+			return err
+		}
+		return handleManagedServiceErrorForServiceVersionsCmd(flags)
+	}
+
+	runServiceVersionsCmdForBackend(flags, infra, serviceVersionsCmdResults, remoteExe)
+
+	return getValueFromResultChannel(serviceVersionsCmdResults, printServiceVersionsOutput)
+}
+
+func runServiceVersionsCmdForFrontEnd(flags *ServiceVersionsCmdFlags, infra *AutomateHAInfraDetails, serviceVersionsCmdResults chan ServiceVersionsCmdResult, remoteExe RemoteCmdExecutor) {
+
+	if flags.automate {
+		go nodeServiceVersions(*flags, AUTOMATE, infra, serviceVersionsCmdResults, remoteExe)
+	} else {
+		serviceVersionsCmdResults <- ServiceVersionsCmdResult{}
+	}
+	if flags.chefServer {
+		flags.automate = false
+		go nodeServiceVersions(*flags, CHEF_SERVER, infra, serviceVersionsCmdResults, remoteExe)
+	} else {
+		serviceVersionsCmdResults <- ServiceVersionsCmdResult{}
+	}
+}
+func getValueFromResultChannel(serviceVersionsCmdResults chan ServiceVersionsCmdResult, printServiceVersionsOutput func(map[string][]*CmdResult, string, *cli.Writer)) error {
+	for i := 0; i < 4; i++ {
+		cmdResult := <-serviceVersionsCmdResults
+		if cmdResult.err != nil {
+			return cmdResult.err
+		} else {
+			if cmdResult.writer != nil {
+				printServiceVersionsOutput(cmdResult.cmdResult, cmdResult.nodeType, cmdResult.writer)
+			}
+		}
+	}
+	return nil
+}
+
+func handleManagedServiceErrorForServiceVersionsCmd(flags *ServiceVersionsCmdFlags) error {
+
+	if flags.postgresql && flags.opensearch {
+		return status.Errorf(status.InvalidCommandArgsError, SERVICE_VERSIONS_ERROR_ON_SELF_MANAGED, POSTGRESQL+" and "+OPENSEARCH)
+	}
+
+	if flags.postgresql {
+		return status.Errorf(status.InvalidCommandArgsError, SERVICE_VERSIONS_ERROR_ON_SELF_MANAGED, POSTGRESQL)
+	}
+
+	if flags.opensearch {
+		return status.Errorf(status.InvalidCommandArgsError, SERVICE_VERSIONS_ERROR_ON_SELF_MANAGED, OPENSEARCH)
+	}
+
+	return nil
+}
+func runServiceVersionsCmdForBackend(flags *ServiceVersionsCmdFlags, infra *AutomateHAInfraDetails, serviceVersionsCmdResults chan ServiceVersionsCmdResult, remoteExe RemoteCmdExecutor) {
+
+	if flags.postgresql {
+		flags.automate = false
+		flags.chefServer = false
+		go nodeServiceVersions(*flags, POSTGRESQL, infra, serviceVersionsCmdResults, remoteExe)
+	} else {
+		serviceVersionsCmdResults <- ServiceVersionsCmdResult{}
+	}
+
+	if flags.opensearch {
+		flags.automate = false
+		flags.chefServer = false
+		flags.postgresql = false
+		go nodeServiceVersions(*flags, OPENSEARCH, infra, serviceVersionsCmdResults, remoteExe)
+	} else {
+		serviceVersionsCmdResults <- ServiceVersionsCmdResult{}
+	}
+
+}
+
+func nodeServiceVersions(flags ServiceVersionsCmdFlags, nodeType string, infra *AutomateHAInfraDetails, serviceVersionsCmdResults chan ServiceVersionsCmdResult, remoteExe RemoteCmdExecutor) {
+	writer := cli.NewWriter(os.Stdout, os.Stderr, os.Stdin)
+	remoteExe.SetWriter(writer)
+	nodeMap := constructNodeMapForServiceVersions(infra, &flags)
+	cmdResult, err := remoteExe.ExecuteWithNodeMap(nodeMap)
+	serviceVersionsCmdResults <- ServiceVersionsCmdResult{
+		cmdResult: cmdResult,
+		writer:    writer,
+		nodeType:  nodeType,
+		err:       err,
+	}
+}
+
+func constructNodeMapForServiceVersions(infra *AutomateHAInfraDetails, flags *ServiceVersionsCmdFlags) *NodeTypeAndCmd {
+	nodeMap := &NodeTypeAndCmd{
+		Frontend: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      FRONTEND_SERVICE_VERSIONS_CMD,
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		Automate: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      FRONTEND_SERVICE_VERSIONS_CMD,
+				ErrorCheckEnableInOutput: true,
+				NodeIps:                  []string{flags.node},
+				NodeType:                 flags.automate,
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		ChefServer: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      FRONTEND_SERVICE_VERSIONS_CMD,
+				ErrorCheckEnableInOutput: true,
+				NodeIps:                  []string{flags.node},
+				NodeType:                 flags.chefServer,
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		Postgresql: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      BACKEND_SERVICE_VERSIONS_CMD,
+				NodeIps:                  []string{flags.node},
+				ErrorCheckEnableInOutput: true,
+				NodeType:                 flags.postgresql,
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		Opensearch: &Cmd{
+			CmdInputs: &CmdInputs{
+				Cmd:                      BACKEND_SERVICE_VERSIONS_CMD,
+				NodeIps:                  []string{flags.node},
+				ErrorCheckEnableInOutput: true,
+				NodeType:                 flags.opensearch,
+				SkipPrintOutput:          true,
+				HideSSHConnectionMessage: true,
+			},
+		},
+		Infra: infra,
+	}
+	return nodeMap
+}
+
+func printServiceVersionsOutput(cmdResult map[string][]*CmdResult, remoteService string, writer *cli.Writer) {
+	writer.Printf("\n=====================================================%s =======================================================\n", " Service-Versions on "+remoteService)
+
+	for _, value := range cmdResult {
+		for _, cmdResult := range value {
+			if cmdResult.Error != nil {
+				printServiceVersionsErrorOutput(cmdResult, remoteService, writer)
+			} else {
+				writer.Printf("Output for Host IP %s : \n%s", cmdResult.HostIP, cmdResult.Output+"\n")
+				writer.Success("Command is executed on " + remoteService + " node : " + cmdResult.HostIP + "\n")
+			}
+			writer.BufferWriter().Flush()
+		}
+	}
+}
+
+func printServiceVersionsErrorOutput(cmdResult *CmdResult, remoteService string, writer *cli.Writer) {
+	isOutputError := false
+
+	if strings.Contains(cmdResult.Output, "DeploymentServiceUnreachableError") {
+		isOutputError = true
+		writer.Failf(CMD_FAIL_MSG, remoteService, cmdResult.HostIP, cmdResult.Output)
+	}
+
+	if !isOutputError {
+		writer.Failf(CMD_FAIL_MSG, remoteService, cmdResult.HostIP, cmdResult.Error.Error())
+	}
+}
+
+func isFlagSet(cmd *cobra.Command) error {
+	if !serviceVersionsCmdFlag.automate && !serviceVersionsCmdFlag.chefServer && !serviceVersionsCmdFlag.opensearch && !serviceVersionsCmdFlag.postgresql {
+		if len(serviceVersionsCmdFlag.node) != 0 {
+			return status.Errorf(status.InvalidCommandArgsError, "Please provide service flag")
+		}
+		writer.Println(cmd.UsageString())
+		return errors.New("No flag is enabled. Please provide any flag")
+	}
+	return nil
+}
+
+func runServiceVersionsOnStandalone() error {
 	connection, err := client.Connection(client.DefaultClientTimeout)
 	if err != nil {
 		return err
@@ -40,10 +301,5 @@ func runServiceVersionsCmd(cmd *cobra.Command, args []string) error {
 	for _, s := range response.Services {
 		writer.Printf("%s/%s %s %s\n", s.Origin, s.Name, s.Version, s.Release)
 	}
-
 	return nil
-}
-
-func init() {
-	RootCmd.AddCommand(serviceVersionsCmd)
 }
---
+++ components/automate-cli/cmd/chef-automate/service_versions_test.go
@@ -0,0 +1,386 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/chef/automate/components/automate-cli/pkg/status"
+	"github.com/chef/automate/components/automate-deployment/pkg/cli"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunServiceVersionsFromBastion(t *testing.T) {
+	type testCase struct {
+		description       string
+		flags             *ServiceVersionsCmdFlags
+		mockNodeOpUtils   *MockNodeUtilsImpl
+		mockRemoteCmdExec *MockRemoteCmdExecutor
+		errorWant         error
+	}
+
+	printServiceVersionsOutput := func(m map[string][]*CmdResult, s string, w *cli.Writer) {}
+	testCases := []testCase{
+
+		{
+			description: "Error while reading infra details",
+			flags:       &ServiceVersionsCmdFlags{},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return nil, nil, errors.New("Error occured while reading infra details")
+				},
+			},
+			errorWant: errors.New("Error occured while reading infra details"),
+		},
+		{
+			description: "Want service-versions of all services",
+			flags: &ServiceVersionsCmdFlags{
+				automate:   true,
+				chefServer: true,
+				opensearch: true,
+				postgresql: true,
+			},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return false
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					return map[string][]*CmdResult{}, nil
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: nil,
+		},
+		{
+			description: "Want service_versions of backend services",
+			flags: &ServiceVersionsCmdFlags{
+				automate:   false,
+				chefServer: false,
+				opensearch: true,
+				postgresql: true,
+			},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return false
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					return map[string][]*CmdResult{}, nil
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: nil,
+		},
+		{
+			description: "Want service_versions of frontend services",
+			flags: &ServiceVersionsCmdFlags{
+				automate:   true,
+				chefServer: true,
+				opensearch: false,
+				postgresql: false,
+			},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return false
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					return map[string][]*CmdResult{}, nil
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: nil,
+		},
+		{
+			description: "Want service-versions of all services but error occured while remote execution",
+			flags: &ServiceVersionsCmdFlags{
+				automate:   true,
+				chefServer: true,
+				opensearch: true,
+				postgresql: true,
+			},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return false
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					return map[string][]*CmdResult{}, errors.New("Some error occured while remote execution")
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: errors.New("Some error occured while remote execution"),
+		},
+		{
+			description: "Managed services and pg flag provided",
+
+			flags: &ServiceVersionsCmdFlags{
+				postgresql: true,
+			},
+
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return true
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					return map[string][]*CmdResult{}, nil
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: status.Errorf(status.InvalidCommandArgsError, SERVICE_VERSIONS_ERROR_ON_SELF_MANAGED, POSTGRESQL),
+		},
+		{
+			description: "Want service-versions of all services but error occured while remote execution",
+			flags: &ServiceVersionsCmdFlags{
+				automate: true,
+			},
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				getHaInfraDetailsfunc: func() (*AutomateHAInfraDetails, *SSHConfig, error) {
+					return &AutomateHAInfraDetails{}, &SSHConfig{}, nil
+				},
+				isManagedServicesOnFunc: func() bool {
+					return true
+				},
+			},
+			mockRemoteCmdExec: &MockRemoteCmdExecutor{
+				ExecuteWithNodeMapFunc: func(nodeMap *NodeTypeAndCmd) (map[string][]*CmdResult, error) {
+					cmdResultWithError := &CmdResult{
+						ScriptName: "script_1",
+						HostIP:     "192.168.1.1",
+						Error:      errors.New("Error occurred"),
+					}
+					return map[string][]*CmdResult{
+						"1": {cmdResultWithError},
+					}, errors.New("Some error occured while remote execution")
+				},
+				SetWriterFunc: func(cli *cli.Writer) {},
+			},
+			errorWant: errors.New("Some error occured while remote execution"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			err := runServiceVersionsFromBastion(testCase.flags, testCase.mockNodeOpUtils, testCase.mockRemoteCmdExec, printServiceVersionsOutput)
+			if err != nil {
+				assert.EqualError(t, testCase.errorWant, err.Error())
+			} else {
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
+
+func TestHandleManagedServiceForServiceVersionCmd(t *testing.T) {
+
+	testCases := []struct {
+		flags          *ServiceVersionsCmdFlags
+		errorExepected error
+	}{
+		{
+			flags: &ServiceVersionsCmdFlags{
+
+				postgresql: true,
+			},
+			errorExepected: status.Errorf(status.InvalidCommandArgsError, SERVICE_VERSIONS_ERROR_ON_SELF_MANAGED, POSTGRESQL),
+		},
+		{
+			flags: &ServiceVersionsCmdFlags{
+
+				opensearch: true,
+			},
+			errorExepected: status.Errorf(status.InvalidCommandArgsError, SERVICE_VERSIONS_ERROR_ON_SELF_MANAGED, OPENSEARCH),
+		},
+		{
+			flags: &ServiceVersionsCmdFlags{},
+
+			errorExepected: nil,
+		},
+		{
+			flags: &ServiceVersionsCmdFlags{
+				opensearch: true,
+				postgresql: true,
+			},
+			errorExepected: status.Errorf(status.InvalidCommandArgsError, SERVICE_VERSIONS_ERROR_ON_SELF_MANAGED, POSTGRESQL+" and "+OPENSEARCH),
+		},
+	}
+
+	for _, testCase := range testCases {
+		err := handleManagedServiceErrorForServiceVersionsCmd(testCase.flags)
+
+		if testCase.errorExepected != nil {
+			assert.EqualError(t, err, testCase.errorExepected.Error())
+		} else {
+			assert.Nil(t, err)
+		}
+	}
+}
+
+func TestConstructNodeMapForServiceVersions(t *testing.T) {
+	type testCase struct {
+		flags           *ServiceVersionsCmdFlags
+		nodeMapExpected *NodeTypeAndCmd
+	}
+	infra := &AutomateHAInfraDetails{}
+
+	testCases := []testCase{
+		{
+			flags: &ServiceVersionsCmdFlags{
+				automate:   true,
+				chefServer: true,
+			},
+			nodeMapExpected: &NodeTypeAndCmd{
+				Frontend: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      FRONTEND_SERVICE_VERSIONS_CMD,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				Automate: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      FRONTEND_SERVICE_VERSIONS_CMD,
+						ErrorCheckEnableInOutput: true,
+						NodeIps:                  []string{""},
+						NodeType:                 true,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				ChefServer: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      FRONTEND_SERVICE_VERSIONS_CMD,
+						ErrorCheckEnableInOutput: true,
+						NodeIps:                  []string{""},
+						NodeType:                 true,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				Postgresql: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      BACKEND_SERVICE_VERSIONS_CMD,
+						NodeIps:                  []string{""},
+						ErrorCheckEnableInOutput: true,
+						NodeType:                 false,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				Opensearch: &Cmd{
+					CmdInputs: &CmdInputs{
+						Cmd:                      BACKEND_SERVICE_VERSIONS_CMD,
+						NodeIps:                  []string{""},
+						ErrorCheckEnableInOutput: true,
+						NodeType:                 false,
+						SkipPrintOutput:          true,
+						HideSSHConnectionMessage: true,
+					},
+				},
+				Infra: infra,
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		nodeMapGet := constructNodeMapForServiceVersions(infra, testCase.flags)
+		assert.Equal(t, testCase.nodeMapExpected, nodeMapGet)
+	}
+}
+
+func TestRunServiceVersionsCmd(t *testing.T) {
+	tests := []struct {
+		testName        string
+		isHA            bool
+		isDevMode       bool
+		isStandalone    bool
+		isExpectedError bool
+		errorMessage    string
+		mockNodeOpUtils *MockNodeUtilsImpl
+	}{
+		{
+			mockNodeOpUtils: &MockNodeUtilsImpl{
+				isA2HARBFileExistFunc: func() bool {
+					return true
+				},
+			},
+			isExpectedError: true,
+			errorMessage:    "Failed to read deployment-service TLS certificates: Could not read the service cert: open /hab/svc/deployment-service/data/deployment-service.crt: no such file or directory",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+			err := runServiceVersionsCmd(&cobra.Command{}, []string{})
+			if tc.isExpectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestIsFalgSet(t *testing.T) {
+
+	testCases := []struct {
+		isForAutomate   bool
+		isForChefServer bool
+		isForPG         bool
+		isForOS         bool
+		node            string
+		errorExepected  error
+	}{
+		{
+			errorExepected: errors.New("No flag is enabled. Please provide any flag"),
+		},
+		{
+			node: "12",
+
+			errorExepected: status.Errorf(status.InvalidCommandArgsError, "Please provide service flag"),
+		},
+		{
+
+			node:           "12",
+			isForAutomate:  true,
+			errorExepected: nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		serviceVersionsCmdFlag.automate = testCase.isForAutomate
+		serviceVersionsCmdFlag.chefServer = testCase.isForChefServer
+		serviceVersionsCmdFlag.opensearch = testCase.isForOS
+		serviceVersionsCmdFlag.postgresql = testCase.isForPG
+		serviceVersionsCmdFlag.node = testCase.node
+		err := isFlagSet(&cobra.Command{})
+
+		if testCase.errorExepected != nil {
+			assert.EqualError(t, err, testCase.errorExepected.Error())
+		} else {
+			assert.Nil(t, err)
+		}
+	}
+}
