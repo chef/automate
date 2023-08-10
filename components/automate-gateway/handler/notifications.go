6d3545f704385a9670e01074f4f7e8670aaf4b15
Abdul-Az <aazeez@progress.com>
2023-08-10 19:00:26 +0530

    response msg update
    Signed-off-by: Abdul-Az <aazeez@progress.com>

--- components/automate-gateway/handler/notifications.go
+++ components/automate-gateway/handler/notifications.go
@@ -149,7 +149,7 @@ func (s *notificationsServer) ValidateWebhook(ctx context.Context,
 
 	resp, err := s.notificationsClient.ValidateWebhook(ctx, &transformUVR)
 	if err != nil {
-		return nil, errors.New("error validating webhook")
+		return nil, errors.New("unable to connect: check URL, username and password.")
 	}
 
 	switch resp.GetCode() {
