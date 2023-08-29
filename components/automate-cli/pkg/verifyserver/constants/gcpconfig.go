e7a56928ded3bd87303e82325b5d10b6c1f20fa4
Pappu Kumar <pappu.kumar@progress.com>
2023-08-29 14:28:04 +0530

    Resolution msg correction and rebased
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- components/automate-cli/pkg/verifyserver/constants/gcpconfig.go
+++ components/automate-cli/pkg/verifyserver/constants/gcpconfig.go
@@ -8,7 +8,7 @@ const (
 	GCP_CONNECTION_RESOLUTION_MSG       = "Provide the correct GCP credentials"
 	GCP_CONNECTION_SUCCESS_MSG          = "Machine is able to connect with GCP using the provided credentials"
 	GCP_BUCKET_ACCESS_TITLE             = "GCP bucket access test"
-	GCP_BUCKET_ACCESS_ERROR_MSG         = "Machine is not able to access the GCP bucket using the provided access key and secret key"
-	GCP_BUCKET_ACCESS_RESOLUTION_MSG    = "Please check if the provided GCP bucket exists or not. If it exists then provide the bucket access to the snapshot user."
-	GCP_BUCKET_ACCESS_SUCCESS_MSG       = "Machine is able to access the GCP bucket using the provided access key and secret key"
+	GCP_BUCKET_ACCESS_ERROR_MSG         = "Machine is not able to access the GCP bucket using the provided credentials"
+	GCP_BUCKET_ACCESS_RESOLUTION_MSG    = "Please check if the provided GCP bucket exists or not. If it exists then provide the correct credentials."
+	GCP_BUCKET_ACCESS_SUCCESS_MSG       = "Machine is able to access the GCP bucket using the provided credentials"
 )
