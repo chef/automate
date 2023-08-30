6f1bd749321f50a9fa72d4e765e1ada237a5be9d
Pappu Kumar <pappu.kumar@progress.com>
2023-08-31 02:05:21 +0530

    Improvised code
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- components/automate-cli/pkg/verifyserver/utils/gcputils/gcp.go
+++ components/automate-cli/pkg/verifyserver/utils/gcputils/gcp.go
@@ -34,13 +34,11 @@ func (au *GCPUtilsImpl) NewSessionWithOptions(ctx context.Context, gsa *models.G
 
 func (au *GCPUtilsImpl) NewUploader(ctx context.Context, uploadObject *storage.ObjectHandle) (*models.Checks, error) {
 	wc := uploadObject.NewWriter(ctx)
-	if _, err := fmt.Fprintf(wc, "Heute ist ein schöner Tag."); err != nil {
+	_, err := fmt.Fprintf(wc, "Heute ist ein schöner Tag.")
+	if err != nil {
 		return nil, err
 	}
-	if err := wc.Close(); err != nil {
-		return nil, err
-	}
-	return nil, nil
+	return nil, wc.Close()
 }
 
 func (au *GCPUtilsImpl) DeleteObject(ctx context.Context, obj *storage.ObjectHandle) error {
@@ -49,25 +47,18 @@ func (au *GCPUtilsImpl) DeleteObject(ctx context.Context, obj *storage.ObjectHan
 
 func (au *GCPUtilsImpl) ListObjects(ctx context.Context, bucket *storage.BucketHandle, query *storage.Query) error {
 	it := bucket.Objects(ctx, query)
-	for {
-		attrs, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-
+	for attrs, err := it.Next(); err != iterator.Done; attrs, err = it.Next() {
 		if err != nil {
 			logrus.Errorf("Error listing the objects")
 			return err
 		}
-
 		fmt.Println("The bucket", attrs.Name)
 	}
 	return nil
 }
 
 func (au *GCPUtilsImpl) BucketAttributes(ctx context.Context, bucket *storage.BucketHandle) error {
-	_, err := bucket.Attrs(ctx)
-	if err != nil {
+	if _, err := bucket.Attrs(ctx); err != nil {
 		return err
 	}
 	return nil
