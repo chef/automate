0ab989038b4df930098363db43298649ded77c1a
Dave Augustus <36985548+daveaugustus@users.noreply.github.com>
2023-09-02 18:39:23 +0530

    Create The API for checking the GCS config (#8159)
    * added values for config parser
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * modified gcs values in config parser
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Added test cases
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Pr comments
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Modification based on the credential file path
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Pipeline trigger
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * moving changes in json file
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * moving changes in json file
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * moving rebase conflict changes
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * added values for config parser
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * modified gcs values in config parser
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Pr comments
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    
    * Initial commit for GCP APi
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Added GetGCPConnection test
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Removed rebased not required changes
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Just a save
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * API check connection success
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * API done, test cases remain
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Removed copied changes
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Resolution msg correction and rebased
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Unit test cases for the handler
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Mocked tests for the service
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Added UUID and gcpUtils
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Modified mock
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Added changes based on mock
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Done with API changes
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Improvised test cases
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Removed unused variables
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Improvised code
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Moved utils to services
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Contex added, modified the return type of NewGCPUtils() func
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * updated mod file
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * updating the code
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>
    
    * updating to internal logger
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>
    
    * If connection fails than dont check for access
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    ---------
    
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>
    Co-authored-by: SahithiMy <sahithi.mylangam@progress.com>
    Co-authored-by: dmaddu <dmaddu@progress.com>

---
+++ components/automate-cli/pkg/verifyserver/models/gcpconfig.go
@@ -0,0 +1,11 @@
+package models
+
+type GCPCloudStorageConfigRequest struct {
+	BucketName               string             `json:"bucket_name"`
+	GoogleServiceAccountFile string             `json:"google_service_account_file"`
+	GcpServiceAccount        *GcpServiceAccount `json:"gcp_service_account"`
+}
+type GCPCloudStorageResponse struct {
+	Passed bool     `json:"passed"`
+	Checks []Checks `json:"checks"`
+}
