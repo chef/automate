cab1ff41555059af85eb9e4e945740b0e4835156
Arvinth C <arvinth.chandrasekaran@progress.com>
2023-10-10 17:13:26 +0530

    Updating utils without dependency
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>

--- lib/arrayutils/remove_duplicate.go
+++ lib/arrayutils/remove_duplicate.go
@@ -1,15 +1,21 @@
 package arrayutils
 
-import (
-	"golang.org/x/exp/slices"
-)
-
 func RemoveStringDuplicates(inputArray []string) []string {
 	arrayWithoutDuplicate := []string{}
 	for _, item := range inputArray {
-		if !slices.Contains(arrayWithoutDuplicate, item) {
+		if !contains(arrayWithoutDuplicate, item) {
 			arrayWithoutDuplicate = append(arrayWithoutDuplicate, item)
 		}
 	}
 	return arrayWithoutDuplicate
+}
+
+func contains(arrayWithoutDuplicate []string, item string) bool {
+	index := -1
+	for i := range arrayWithoutDuplicate {
+		if item == arrayWithoutDuplicate[i] {
+			index = i
+		}
+	}
+	return index >= 0
 }
