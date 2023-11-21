26d682535b76c1fc39144398af058619bc036840
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2023-11-21 11:16:55 +0530

    Adding check in system-user verify flow to verify HAB uid (#8290)
    * Adding hab uid check in system-user verify flow
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Removing logconfig changes
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * code coveragr
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * refactoring for code smell and removing nil pointers
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Test coverage
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    * Review comments
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>
    
    ---------
    
    Signed-off-by: Arvinth C <arvinth.chandrasekaran@progress.com>

--- components/automate-cli/pkg/verifyserver/models/systemuser.go
+++ components/automate-cli/pkg/verifyserver/models/systemuser.go
@@ -1,6 +1,12 @@
 package models
 
 type SystemUserResponse struct {
-	Passed bool     `json:"passed"`
-	Checks []*Checks `json:"checks"`
+	Passed bool          `json:"passed"`
+	Checks []*Checks     `json:"checks"`
+	Id     *SystemUserID `json:"id,omitempty"`
+}
+
+type SystemUserID struct {
+	UserID  string `json:"user_id,omitempty"`
+	GroupID string `json:"group_id,omitempty"`
 }
--- lib/arrayutils/remove_duplicate.go
+++ lib/arrayutils/remove_duplicate.go
@@ -3,14 +3,14 @@ package arrayutils
 func RemoveStringDuplicates(inputArray []string) []string {
 	arrayWithoutDuplicate := []string{}
 	for _, item := range inputArray {
-		if !contains(arrayWithoutDuplicate, item) {
+		if !Contains(arrayWithoutDuplicate, item) {
 			arrayWithoutDuplicate = append(arrayWithoutDuplicate, item)
 		}
 	}
 	return arrayWithoutDuplicate
 }
 
-func contains(arrayWithoutDuplicate []string, item string) bool {
+func Contains(arrayWithoutDuplicate []string, item string) bool {
 	index := -1
 	for i := range arrayWithoutDuplicate {
 		if item == arrayWithoutDuplicate[i] {
