51d189a60d338b6634e9e0fcd0075f63f69faa94
Dave Augustus <36985548+daveaugustus@users.noreply.github.com>
2023-11-22 17:48:21 +0530

    CHEF-6969 Added minio into the backup gateway  (#8292)
    * Added minio into the backup gateway
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    * Corrected checkout branch
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>
    
    ---------
    
    Signed-off-by: Pappu Kumar <pappu.kumar@progress.com>

--- components/backup-gateway/habitat/plan.sh
+++ components/backup-gateway/habitat/plan.sh
@@ -5,8 +5,10 @@ pkg_version="0.1.0"
 pkg_origin=chef
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=('Apache-2.0')
+pkg_build_deps=(core/go19 core/git core/gcc)
+pkg_bin_dirs=(bin)
+
 pkg_deps=(
-  core/minio
   core/cacerts
   chef/automate-platform-tools
 )
@@ -16,16 +18,27 @@ pkg_exports=(
 )
 pkg_exposes=(port)
 
-do_unpack() {
-    return 0
+do_before() {
+ GOPATH=$HAB_CACHE_SRC_PATH/$pkg_dirname
+ export GOPATH
 }
 
+do_unpack() {
+ git clone "https://github.com/chef/minio" "$GOPATH/src/github.com/chef/minio"
+ ( cd "$GOPATH/src/github.com/chef/minio" || exit
+   git checkout automate_minio
+ )
+}
+
+
 do_build(){
-    return 0
+  build_line "build minio"
+  cd "$GOPATH/src/github.com/chef/minio"
+  go build --ldflags "${GO_LDFLAGS}" -o "$pkg_prefix/bin/"
 }
 
 do_install() {
-    return 0
+  return 0
 }
 
 do_strip() {
