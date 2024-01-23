273cd107b3bb6d8df9458ee2c086222996545a48
Aishwarya2001A <145524448+Aishwarya2001A@users.noreply.github.com>
2024-01-23 13:40:21 +0530

    upgraded gopkg.in/yaml version (#8344)
    * upgraded gopkg.in/yaml version
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * upgraded gopkg.in/yaml version
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * upgraded gopkg.in/yaml version
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * upgraded gopkg.in/yaml version
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * upgraded gopkg.in/yaml version
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * upgraded gopkg.in/yaml version
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * changes in yaml
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * changes in yaml
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * changes in yaml
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * changes in backup-gateway
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * changes in backup-gateway
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * changes in backup-gateway
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * changed yaml version
    
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>
    
    * changed yaml version
    
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>
    
    * changed yaml version
    
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>
    
    * changed yaml version
    
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>
    
    * changed yaml version
    
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>
    
    * changes in yaml version
    
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>
    
    ---------
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>

--- components/automate-cli/pkg/docs/yaml_docs.go
+++ components/automate-cli/pkg/docs/yaml_docs.go
@@ -13,7 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 package docs
 
 import (
@@ -25,7 +24,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 
 	"github.com/chef/automate/components/automate-cli/pkg/status"
 )
