d6f6694391b2f435a44c8e568992862a8ca63335
karan parmar <kparmar@progress.com>
2024-01-30 13:11:47 +0530

    changed pinned version for cs components
    Signed-off-by: karan parmar <kparmar@progress.com>

--- components/automate-cs-ocid/habitat/plan.sh
+++ components/automate-cs-ocid/habitat/plan.sh
@@ -21,7 +21,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/oc_id/15.8.0/20240129214653"
+  "${vendor_origin}/oc_id/15.4.0/20240129214653"
 )
 
 pkg_binds=(
