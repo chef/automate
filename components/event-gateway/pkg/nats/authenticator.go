6e9adf4a044d9da05c3e8999732caa7f75e2c64f
karan parmar <kparmar@progress.com>
2024-03-20 14:16:27 +0530

    nats server version changed to 2.9.23
    Signed-off-by: karan parmar <kparmar@progress.com>

--- components/event-gateway/pkg/nats/authenticator.go
+++ components/event-gateway/pkg/nats/authenticator.go
@@ -113,7 +113,7 @@ func newAutomateAuthenticator(c *config.EventGatewayConfig) (*automateAuthentica
 //     publish/subscribe to.
 func (a *automateAuthenticator) Check(client natsd.ClientAuthentication) bool {
 	log.WithFields(log.Fields{"client": client.RemoteAddress()}).Debug("authenticating NATS connection request")
-	token := client.GetOpts().Authorization
+	token := client.GetOpts().Token
 
 	// use ConstantTimeCompare so as not to leak the healthCheckToken via timing attacks.
 	if subtle.ConstantTimeCompare([]byte(token), []byte(a.healthCheckToken)) == 1 {
