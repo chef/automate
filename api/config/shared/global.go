0b743f486e075b5081f29a117f26b1247d48191e
Aishwarya Aishwarya <aaishwar@progress.com>
2024-03-21 20:14:46 +0530

    Nginx config for X-Forwarded-For Header
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>

--- api/config/shared/global.go
+++ api/config/shared/global.go
@@ -18,13 +18,28 @@ import (
 const (
 	habPkgPlatformToolsPath = "hab pkg path chef/automate-platform-tools"
 )
-
-// NewGlobalConfig returns a new GlobalConfig instance with zero values.
-func NewGlobalConfig() *GlobalConfig {
-	return &GlobalConfig{
-		V1: &V1{},
-	}
+type NgxSettings struct {
+    Http *NgxHttpSettings `protobuf:"bytes,15,opt,name=http,proto3" json:"http,omitempty"`
 }
+type NgxHttpSettings struct {
+    IncludeXForwardedFor *gw.BoolValue `protobuf:"bytes,1,opt,name=include_x_forwarded_for,json=includeXForwardedFor,proto3" json:"include_x_forwarded_for,omitempty"`
+}
+type SysSettings struct {
+    Ngx *NgxSettings `protobuf:"bytes,10,opt,name=ngx,proto3" json:"ngx,omitempty"`
+}
+type V1Settings struct {
+    Sys *SysSettings `protobuf:"bytes,1,opt,name=sys,proto3" json:"sys,omitempty"`
+}
+type GlobalConfig1 struct {
+    V1 *V1Settings `protobuf:"bytes,1,opt,name=v1,proto3" json:"v1,omitempty"`
+}
+func SetIncludeXForwardedForToFalse(config *GlobalConfig1) error {
+    if config.V1 == nil || config.V1.Sys == nil || config.V1.Sys.Ngx == nil || config.V1.Sys.Ngx.Http == nil {
+        return errors.New("NGINX HTTP configuration settings not found or nil")
+    }
+    config.V1.Sys.Ngx.Http.IncludeXForwardedFor = w.Bool(false)
+    return nil
+} 
 // DefaultGlobalConfig returns a new GlobalConfig instance with default values.
 func DefaultGlobalConfig() *GlobalConfig {
 	return &GlobalConfig{
@@ -55,17 +70,17 @@ func DefaultGlobalConfig() *GlobalConfig {
 			LargeReporting: &LargeReporting{
 				EnableLargeReporting: w.Bool(false),
 			},
-			Sys: &SysSettings{
-				Ngx: &NgxSettings{
-					Http: &NgxHttpSettings{
-						IncludeXForwardedFor: &gw.BoolValue{Value: false},
-					},
-				},
-			},
+			
 		},
 	}
-}
+	err := SetIncludeXForwardedForToFalse(config)
+    if err != nil {
+        // Handle the error if necessary
+        fmt.Println("Error:", err)
+    }
 
+    return config
+}
 // Validate validates that the config is valid. If validation succeeds it will
 // return nil, if it fails it will return a new instance of config.InvalidConfigError
 // that has the missing keys and invalid fields populated.
