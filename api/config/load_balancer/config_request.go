dd6c30c6dc8d6b4080bbe038456951db4d35e027
Aishwarya Aishwarya <aaishwar@progress.com>
2024-03-26 16:51:29 +0530

    corrections
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>

--- api/config/load_balancer/config_request.go
+++ api/config/load_balancer/config_request.go
@@ -169,9 +169,9 @@ func (c *ConfigRequest) SetGlobalConfig(g *config.GlobalConfig) {
 	if logLevel := g.GetV1().GetLog().GetLevel().GetValue(); logLevel != "" {
 		c.V1.Sys.Log.Level.Value = config.GlobalLogLevelToNginxLevel(logLevel)
 	}
-	if xForwarded := g.GetV1().GetNginx().GetHttpIncludeXForwardedFor(); xForwarded != nil {
-		c.V1.Sys.Ngx.IncludeXForwardedFor = xForwarded.GetValue()
-	}	
+	if xForwarded := g.GetV1().GetNginx().GetIncludeXForwardedFor(); xForwarded != nil {
+		c.V1.Nginx.HttpIncludeXForwardedFor = xForwarded
+	}		
 }
 
 // PrepareSystemConfig returns a system configuration that can be used
