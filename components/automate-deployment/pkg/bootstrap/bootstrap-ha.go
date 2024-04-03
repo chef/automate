fca580aadb32a4fb0de7bb3590284aa9b3c19528
deenchev <deenchev@progress.com>
2024-04-03 13:49:00 +0300

    Moving the creation of the '/hab/tmp' dir inside the automate-deployment bootsraping

--- components/automate-deployment/pkg/bootstrap/bootstrap-ha.go
+++ components/automate-deployment/pkg/bootstrap/bootstrap-ha.go
@@ -2,6 +2,8 @@ package bootstrap
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	dc "github.com/chef/automate/api/config/deployment"
 	"github.com/chef/automate/components/automate-deployment/pkg/cli"
@@ -14,6 +16,7 @@ func FullBootstrapHA(ctx context.Context,
 	config *dc.ConfigRequest,
 	currentM manifest.ReleaseManifest,
 	writer cli.FormatWriter, saas bool) error {
+	const HAB_TMP_DIR = "/hab/tmp"
 
 	writer.Body("Installing Habitat")
 	err := b.InstallHabitat(ctx, currentM, writer)
@@ -27,5 +30,31 @@ func FullBootstrapHA(ctx context.Context,
 		writer.Printf("Some error occurred %s\n", err.Error())
 		return err
 	}
+
+	writer.Body("Creating a temporary folder " + HAB_TMP_DIR)
+	err = createHabTmpDir(writer, HAB_TMP_DIR)
+	if err != nil {
+		writer.Printf("Error occured while creating a temporary directory: %s\n", err.Error())
+		return err
+	}
+
 	return nil
+}
+
+func createHabTmpDir(writer cli.FormatWriter, tmpDir string) error {
+	var err error
+	if _, err = os.Stat(tmpDir); os.IsNotExist(err) {
+		err = os.MkdirAll(tmpDir, os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("error while creating %s dir: %w", tmpDir, err)
+		}
+
+		// Change the permissions from 0777 to 1777
+		err = os.Chmod(tmpDir, 0777|os.ModeSticky)
+		if err != nil {
+			return fmt.Errorf("error while modifying the permissions of %s dir: %w", tmpDir, err)
+		}
+		return nil
+	}
+	return err
 }
