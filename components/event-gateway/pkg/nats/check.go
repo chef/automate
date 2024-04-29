70bb3897d1d197e7fdc019e8c4114ca0f19214c3
Aishwarya Aishwarya <aaishwar@progress.com>
2024-04-29 11:48:45 +0530

    alignment
    Signed-off-by: Aishwarya Aishwarya <aaishwar@progress.com>

--- components/event-gateway/pkg/nats/check.go
+++ components/event-gateway/pkg/nats/check.go
@@ -1,16 +1,16 @@
 package nats
 
 import (
-	 "context"
-	 "crypto/rand"
-	 "crypto/tls"
-	 "encoding/hex"
-	 "fmt"
-	 "time"
+	"context"
+	"crypto/rand"
+	"crypto/tls"
+	"encoding/hex"
+	"fmt"
+	"time"
 
-	 natsc "github.com/nats-io/nats.go"
-	 "github.com/pkg/errors"
-	 log "github.com/sirupsen/logrus"
+	natsc "github.com/nats-io/nats.go"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/chef/automate/components/event-gateway/pkg/config"
 )
@@ -19,64 +19,64 @@ func ConnectivityCheck(c *config.EventGatewayConfig) error {
 
 	token, err := ReadHealthCheckCredentials(c)
 	if err != nil {
-	 	return errors.Wrap(err, "reading credentials for healthcheck")
-	 }
+		return errors.Wrap(err, "reading credentials for healthcheck")
+	}
 
-	 natsURL := fmt.Sprintf("nats://%s@localhost:4222", token)
+	natsURL := fmt.Sprintf("nats://%s@localhost:4222", token)
 
-	 opts := []natsc.Option{}
+	opts := []natsc.Option{}
 
 	if !c.Service.DisableFrontendTLS {
-	 	t := &tls.Config{
+		t := &tls.Config{
 			MinVersion:         tls.VersionTLS12,
 			InsecureSkipVerify: true,
-	 	}
-	 	opts = append(opts, natsc.Secure(t))
-	 }
+		}
+		opts = append(opts, natsc.Secure(t))
+	}
 
-	 log.WithFields(log.Fields{"url": natsURL}).Debug("connecting to event gateway")
-	 conn, err := natsc.Connect(natsURL, opts...)
-	 if err != nil {
-	 	return errors.Wrapf(err, "connecting to NATS server %q", natsURL)
-	 }
-	 defer conn.Close()
+	log.WithFields(log.Fields{"url": natsURL}).Debug("connecting to event gateway")
+	conn, err := natsc.Connect(natsURL, opts...)
+	if err != nil {
+		return errors.Wrapf(err, "connecting to NATS server %q", natsURL)
+	}
+	defer conn.Close()
 
-	 // Generate a random hex string to use as a sub-topic. This prevents two
+	// Generate a random hex string to use as a sub-topic. This prevents two
 	// healthchecks from interacting if they run concurrently.
-	 b := make([]byte, 20)
-	 _, err = rand.Read(b)
-	 if err != nil {
+	b := make([]byte, 20)
+	_, err = rand.Read(b)
+	if err != nil {
 		return errors.Wrap(err, "generating random topic name for healthcheck")
-	 }
+	}
 
-	 slug := hex.EncodeToString(b)
-	 topicName := fmt.Sprintf("healthcheck.%s", slug)
+	slug := hex.EncodeToString(b)
+	topicName := fmt.Sprintf("healthcheck.%s", slug)
 
-	 // subscriber that will reply
-	 sub, err := conn.Subscribe(topicName, func(m *natsc.Msg) {
-	 	log.WithFields(log.Fields{"data": string(m.Data), "topic_name": topicName}).Debug("received healthcheck message")
-	 	conn.Publish(m.Reply, m.Data) // nolint errcheck
-	 })
-      if err != nil {
-	 	return errors.Wrapf(err, "subscription to topic %q failed", topicName)
-	  }
-	  defer sub.Drain() // nolint errcheck
+	// subscriber that will reply
+	sub, err := conn.Subscribe(topicName, func(m *natsc.Msg) {
+		log.WithFields(log.Fields{"data": string(m.Data), "topic_name": topicName}).Debug("received healthcheck message")
+		conn.Publish(m.Reply, m.Data) // nolint errcheck
+	})
+	if err != nil {
+		return errors.Wrapf(err, "subscription to topic %q failed", topicName)
+	}
+	defer sub.Drain() // nolint errcheck
 
-	 ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	 defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
 
-	 log.WithFields(log.Fields{"data": slug, "topic_name": topicName}).Debug("sending reply request to event gateway")
-	 reply, err := conn.RequestWithContext(ctx, topicName, []byte(slug))
+	log.WithFields(log.Fields{"data": slug, "topic_name": topicName}).Debug("sending reply request to event gateway")
+	reply, err := conn.RequestWithContext(ctx, topicName, []byte(slug))
 
 	if err != nil {
-	 	return errors.Wrap(err, "publishing message and receive reply within timeout")
-	 }
+		return errors.Wrap(err, "publishing message and receive reply within timeout")
+	}
 
-	 log.WithFields(log.Fields{"data": string(reply.Data), "expected": slug}).Debug("received reply from event gateway")
+	log.WithFields(log.Fields{"data": string(reply.Data), "expected": slug}).Debug("received reply from event gateway")
 
 	if string(reply.Data) != slug {
-	 	return errors.Errorf("received unexpected reply. Expected %q, got %q", slug, string(reply.Data))
-	 }
+		return errors.Errorf("received unexpected reply. Expected %q, got %q", slug, string(reply.Data))
+	}
 
 	return nil
 }
