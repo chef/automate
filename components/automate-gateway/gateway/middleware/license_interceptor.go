e175013291d7bcb99a2c9e9b4a4c3bc4c11ac8d0
Yashvi Jain <yashvi.jain@progress.com>
2024-04-30 15:54:19 +0530

    Adding interceptor layer for license
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>

--- components/automate-gateway/gateway/middleware/license_interceptor.go
+++ components/automate-gateway/gateway/middleware/license_interceptor.go
@@ -3,9 +3,13 @@ package middleware
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/chef/automate/api/interservice/license_control"
+	"github.com/pkg/errors"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type licenseInterceptor struct {
@@ -20,6 +24,21 @@ func NewLicenseInterceptor(license_client license_control.LicenseControlServiceC
 	}
 }
 
+const (
+	commercial = "commercial"
+)
+
+type LicenseStatus struct {
+	LicenseDetails  *LicenseDetails
+	DetailsValidity time.Time
+}
+type LicenseDetails struct {
+	licenseType string
+	expiryDate  time.Time
+}
+
+var licenseStatus *LicenseStatus
+
 // UnaryInterceptor returns a grpc UnaryServerInterceptor that performs license check
 func (l *licenseInterceptor) UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(
@@ -28,20 +47,23 @@ func (l *licenseInterceptor) UnaryServerInterceptor() grpc.UnaryServerIntercepto
 		info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler) (interface{}, error) {
 
-		// licenseDetails, err := l.license_client.Status(ctx, &license_control.StatusRequest{})
-		// if err != nil {
-		// 	return nil, errors.Wrap(err, "unable to get the status of the license")
-		// }
+		//if the global variable license status containing details is nil or If its not nil checking the details validity.
+		//If the details validity is before the current time refersh the license details
+		if (licenseStatus != nil && licenseStatus.DetailsValidity.Before(time.Now())) || licenseStatus == nil {
+			fmt.Println("Getting details again----------------")
+			err := l.getLicenseDetails(ctx)
+			if err != nil {
+				return nil, errors.Wrap(err, "Unable to fetch license details")
+			}
+		}
 
-		// expireTime := time.Unix(licenseDetails.LicensedPeriod.End.GetSeconds(), 0).UTC()
-		// currentTime := time.Now()
+		fmt.Println("-------------- licene we got " + licenseStatus.LicenseDetails.licenseType)
+		if !l.isValidLicense() {
+			return nil, status.Errorf(codes.PermissionDenied, "License is expired, please apply a new license")
 
-		// if expireTime.Before(currentTime) {
-		// 	return nil, status.Errorf(codes.FailedPrecondition, "License expired, please apply the new license")
+		}
 
-		// }
-
-		return nil, fmt.Errorf("unable to perform action license failed")
+		return handler(ctx, req)
 
 	}
 }
@@ -49,7 +71,52 @@ func (l *licenseInterceptor) UnaryServerInterceptor() grpc.UnaryServerIntercepto
 func (l *licenseInterceptor) StreamServerInterceptor() grpc.StreamServerInterceptor {
 	return func(req interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 
-		return fmt.Errorf("unable to perform action license failed")
+		i := interceptedServerStream{ServerStream: ss}
+		return handler(req, &i)
 
 	}
+}
+
+func (l *licenseInterceptor) getLicenseDetails(ctx context.Context) error {
+	//Getting the license details for license service
+	licenseDetailsResponse, err := l.license_client.Status(ctx, &license_control.StatusRequest{})
+	if err != nil {
+		return errors.Wrap(err, "unable to get the status of the license")
+	}
+
+	licenseDetails := &LicenseDetails{
+		licenseType: licenseDetailsResponse.LicenseType,
+		expiryDate:  time.Unix(licenseDetailsResponse.LicensedPeriod.End.GetSeconds(), 0).UTC(),
+	}
+
+	//Marking the validity of the license details for next 6 hours of time
+	validity := time.Now().Add(time.Minute * time.Duration(2))
+
+	licenseStatus = &LicenseStatus{
+		LicenseDetails:  licenseDetails,
+		DetailsValidity: validity,
+	}
+
+	return nil
+
+}
+
+func (l *licenseInterceptor) isValidLicense() bool {
+
+	if licenseStatus.LicenseDetails == nil {
+		return false
+	}
+	licenseValidDate := licenseStatus.LicenseDetails.expiryDate
+
+	//If the license type is commercial, adding grace period of 1 week
+	if licenseStatus.LicenseDetails.licenseType == commercial {
+		//Adding grace period for 7 days i.e. one week
+		licenseValidDate = licenseValidDate.AddDate(0, 0, 7)
+	}
+
+	if licenseValidDate.Before(time.Now()) {
+		return false
+	}
+
+	return true
 }
