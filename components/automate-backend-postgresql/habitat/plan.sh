b00e5d3b13b8d15b6519ea0449e933fe5bc0f782
swatiganesh <145528613+swatiganesh@users.noreply.github.com>
2024-05-20 15:48:01 +0530

    updated postgresql version in automate-backed-postgresql (#8434)
    * updated postgresql version in automate-backed-postgresql
    
    Signed-off-by: swatign <SwatiGanesh.Naik@Progress.com>
    
    * dummy comment
    
    Signed-off-by: swatign <SwatiGanesh.Naik@Progress.com>
    
    * removing dummy comment
    
    Signed-off-by: swatign <SwatiGanesh.Naik@Progress.com>
    
    * removing the  version to check pipeline
    
    Signed-off-by: swatign <SwatiGanesh.Naik@Progress.com>
    
    * changing  version to 13.4.0
    
    Signed-off-by: swatign <SwatiGanesh.Naik@Progress.com>
    
    * changing  version to 13.4.0  in automate-postgresql component
    
    Signed-off-by: swatign <SwatiGanesh.Naik@Progress.com>
    
    * changing  version to 13.14.0  in automate-postgresql and backend-postgresql  component
    
    Signed-off-by: swatign <SwatiGanesh.Naik@Progress.com>
    
    * updating pg version in docs
    
    Signed-off-by: Jay Sharma <jsharma@progress.com>
    
    ---------
    
    Signed-off-by: swatign <SwatiGanesh.Naik@Progress.com>
    Signed-off-by: Jay Sharma <jsharma@progress.com>
    Co-authored-by: Jay Sharma <jsharma@progress.com>

--- components/automate-backend-postgresql/habitat/plan.sh
+++ components/automate-backend-postgresql/habitat/plan.sh
@@ -1,12 +1,12 @@
 # shellcheck disable=SC2148
 # stable channel
 
-UPSTREAM_PKG_IDENT="core/postgresql13/13.5"
+UPSTREAM_PKG_IDENT="core/postgresql13/13.14"
 pkg_name="automate-ha-postgresql"
 pkg_description="Wrapper package for core/postgresql13"
 pkg_origin="chef"
 vendor_origin="chef"
-pkg_version="13.5.0"
+pkg_version="13.14.0"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Chef-MLSA")
 pkg_upstream_url="https://www.chef.io/automate"
