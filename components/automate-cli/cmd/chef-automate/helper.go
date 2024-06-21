f8568a8c1a4b41f799b2dac5d47b554d891cea95
Yashvi Jain <yashvi.jain@progress.com>
2024-06-21 17:44:03 +0530

    Adding new statements for cli
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>

--- components/automate-cli/cmd/chef-automate/helper.go
+++ components/automate-cli/cmd/chef-automate/helper.go
@@ -105,48 +105,69 @@ func checkLicenseExpiry(licenseResult *LicenseResult) error {
 	}
 	licenseValidDate := time.Unix(licenseResult.Result.ExpirationDate.Seconds, 0) // gives unix time stamp in utc
 
+	gracePeriodDuration := 60
+
+	daysAgo := int(time.Since(licenseValidDate).Hours() / 24)
+
+	gracePeriodDate := licenseValidDate.AddDate(0, 0, gracePeriodDuration)
+
+	licenseDate := licenseValidDate.Format("02-01-2006")
 	// If the license type is commercial, adding grace period of 1 month
-	if licenseValidDate.Before(time.Now()) {
-		if licenseResult.Result.LicenseType == commercial {
-			// Check if the license (including the grace period) is expired
-			if !licenseResult.Result.GracePeriod {
-				return status.New(
-					status.LicenseError,
-					"This license and grace period have expired. Please contact sales@chef.io to renew your Chef Automate license.",
-				)
+	if licenseResult.Result.LicenseType == commercial {
+
+		graceDate := gracePeriodDate.Format("02-01-2006")
+		if !licenseResult.Result.GracePeriod {
+
+			if daysAgo > 0 && daysAgo <= gracePeriodDuration {
+				status.New(
+					status.LicenseError, fmt.Sprintf("Chef Automate license is expiring on %s. Your grace period will be %d days. Please update your license before %s to avoid any service disruption", licenseDate, gracePeriodDuration, graceDate))
 			} else {
-				cli.NewWriter(os.Stdout, os.Stderr, os.Stdin).Warn(fmt.Sprintf("Your license expired is expired,Please apply a new license.\n"))
+				status.New(
+					status.LicenseError, fmt.Sprintf(`Your Chef Automate license has expired.
+
+				Contact us to get a license. If you already have one, enter it now.`))
 			}
-
 		} else {
-			return status.New(
-				status.LicenseError,
-				"This license have expired. Please contact sales@chef.io to renew your Chef Automate license.",
-			)
-		}
 
+			status.New(
+				status.LicenseError, fmt.Sprintf("Chef Automate license has expired on %s. We request you to get a new license by connecting with your account team. To avoid service disruption, please update a valid license by %s.", licenseDate, graceDate))
+
+		}
+	} else {
+		if daysAgo > 0 {
+			status.New(
+				status.LicenseError, fmt.Sprintf("Chef Automate license has expired on %s. We request you to get a new license by connecting with your account team. To avoid service disruption, please update a valid license by %s.", licenseDate, licenseDate))
+
+		}
 	}
+
 	return nil
 }
 
 func warnIfLicenseNearExpiry(licenseResult *LicenseResult) {
 	// Calculate the license valid date
 	licenseValidDate := time.Unix(licenseResult.Result.ExpirationDate.Seconds, 0)
-	// Check if the license is expired
-	if licenseValidDate.Before(time.Now()) {
-		daysAgo := int(time.Since(licenseValidDate).Hours() / 24)
-		if licenseResult.Result.LicenseType == commercial {
-			// If the license is expired, check if it's within the grace period
-			if licenseResult.Result.GracePeriod {
-				// Warning during the grace period
-				cli.NewWriter(os.Stdout, os.Stderr, os.Stdin).Warn(fmt.Sprintf("Your license expired %d days ago,Please apply a license.Please contact sales@chef.io to have your Chef Automate license.\n", daysAgo))
 
+	gracePeriodDuration := 60
+
+	gracePeriodDate := licenseValidDate.AddDate(0, 0, gracePeriodDuration)
+
+	if licenseResult.Result.LicenseType == commercial {
+		licenseDate := licenseValidDate.Format("02-01-2006")
+		graceDate := gracePeriodDate.Format("02-01-2006")
+		if !licenseResult.Result.GracePeriod {
+			daysAgo := int(time.Since(licenseValidDate).Hours() / 24)
+			if daysAgo > 0 && daysAgo <= gracePeriodDuration {
+				cli.NewWriter(os.Stdout, os.Stderr, os.Stdin).Warn(fmt.Sprintf("Chef Automate license is expiring on %s. Your grace period will be %d days. Please update your license before %s to avoid any service disruption", licenseDate, gracePeriodDuration, graceDate))
 			} else {
-				// Warning if the grace period has ended
-				cli.NewWriter(os.Stdout, os.Stderr, os.Stdin).Warn(fmt.Sprintf("Your license expired %d ago and you are out of 30 days of grace period . Please apply a new license to continue using the software.", daysAgo))
+				cli.NewWriter(os.Stdout, os.Stderr, os.Stdin).Warn(fmt.Sprintf(`Your Chef Automate license has expired.
+
+				Contact us to get a license. If you already have one, enter it now.`))
 			}
 		} else {
-			cli.NewWriter(os.Stdout, os.Stderr, os.Stdin).Warn(fmt.Sprintf("Your license expired %d days ago,Please apply a license.Please contact sales@chef.io to have your Chef Automate license.\n", daysAgo))
+
+			cli.NewWriter(os.Stdout, os.Stderr, os.Stdin).Warn(fmt.Sprintf("Chef Automate license has expired on %s. We request you to get a new license by connecting with your account team. To avoid service disruption, please update a valid license by %s.", licenseDate, graceDate))
+
 		}
 	}
 }
