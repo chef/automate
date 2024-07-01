1402794888cfa023fc5c18fce6f90545802faf67
vinay sharma <vsharma@chef.io>
2024-07-01 17:32:27 +0530

    remove workflow web whole component inside the automate
    Signed-off-by: vinay sharma <vsharma@chef.io>

--- components/automate-workflow-web/src/routes/project/dependencies/dependencies_controller.js
+++
@@ -1,9 +0,0 @@
-import ng from 'angular';
-import sref from '../../../helpers/sref';
-
-function dependenciesController() {}
-
-export default ng
-  .module('cd.routes.project.dependencies.controller', [])
-  .controller('dependenciesController', dependenciesController)
-  .name;
--- components/automate-workflow-web/test/e2e/helpers/scroll_to_top.js
+++
@@ -1,7 +0,0 @@
-import scrollTo from './scroll_to';
-
-export default function scrollToTop() {
-  return scrollTo(0, 0);
-}
-
-global.scrollToTop = scrollToTop;
