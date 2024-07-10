94a3b05914ca74a1fa7ecd59f412451c44c2af7c
iamazzeez <aazeez@progress.com>
2024-07-10 16:03:33 +0530

    go veresion upgrade to 1.22.4
    Signed-off-by: iamazzeez <aazeez@progress.com>

--- scripts/repo_health.sh
+++ scripts/repo_health.sh
@@ -9,7 +9,7 @@ desired_golang_version() {
 }
 
 hab pkg install -b core/git core/ruby core/jq-static core/shellcheck core/cacerts
-hab pkg install -b "core/go19/$(desired_golang_version)"
+hab pkg install -b "core/go22/1.22.4/20240625061400"
 
 echo "Checking Go Dependencies And Vendored Protos"
 go mod verify
