276f5a81ac5a853e926c3e0aaaf0a555b649c806
Yashvi Jain <yashvi.jain@progress.com>
2024-07-12 17:54:23 +0530

    Removing nil check
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>

--- components/automate-cli/cmd/chef-automate/helper.go
+++ components/automate-cli/cmd/chef-automate/helper.go
@@ -159,18 +159,18 @@ func readFileAndMarshal(fileName string) (*LicenseResult, error) {
 }
 
 func checkLicenseExpiry(licenseResult *LicenseResult) error {
-	if licenseResult.Result.LicenseId == "" {
-		if licenseResult.ErrorType != "" {
-			return status.New(
-				status.DeploymentServiceCallError,
-				licenseResult.ErrorDescription,
-			)
-		}
-		return status.New(
-			status.LicenseError,
-			"Please apply a license.Please contact sales@chef.io to have your Chef Automate license.",
-		)
-	}
+	// if licenseResult.Result.LicenseId == "" {
+	// 	if licenseResult.ErrorType != "" {
+	// 		return status.New(
+	// 			status.DeploymentServiceCallError,
+	// 			licenseResult.ErrorDescription,
+	// 		)
+	// 	}
+	// 	return status.New(
+	// 		status.LicenseError,
+	// 		"Please apply a license.Please contact sales@chef.io to have your Chef Automate license.",
+	// 	)
+	// }
 
 	licenseValidDate := time.Unix(licenseResult.Result.ExpirationDate.Seconds, 0) // gives unix time stamp in utc
 	gracePeriodDuration := 60
