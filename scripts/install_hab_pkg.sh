1e1a5d3d54e5acd8f29d61121ce196be68d58645
iamazzeez <aazeez@progress.com>
2024-07-15 11:47:39 +0530

    go version upgrade pipeline fix
    Signed-off-by: iamazzeez <aazeez@progress.com>

--- scripts/install_hab_pkg.sh
+++ scripts/install_hab_pkg.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 export HAB_NONINTERACTIVE=true
 export HAB_NOCOLORING=true
 export HAB_LICENSE="accept-no-persist"
+export GOPROXY="https://proxy.golang.org,direct"
 
 ident="${1?ident required}"
 hab pkg install -b "$ident"
