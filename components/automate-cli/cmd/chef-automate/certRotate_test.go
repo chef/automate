082e4529e3284fce153026ae37c71db6be9261a3
Vivek Shankar <vshankar@progress.com>
2024-07-27 10:23:44 +0530

    fix pipeline
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- components/automate-cli/cmd/chef-automate/certRotate_test.go
+++ components/automate-cli/cmd/chef-automate/certRotate_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 	"testing"
 	"time"
 
@@ -3115,11 +3116,15 @@ func TestCertRotateFromTemplate(t *testing.T) {
 		t.Run(testCase.description, func(t *testing.T) {
 			c := certRotateFlow{fileUtils: &fileutils.MockFileSystemUtils{
 				ReadFileFunc: func(filepath string) ([]byte, error) {
-					return []byte(`elasticsidecar_pkg_ident = "chef/automate-ha-elasticsidecar/0.1.0/20240725174312"
-proxy_pkg_ident = "chef/automate-ha-haproxy/2.2.14/20240725171322"
-opensearch_pkg_ident = "chef/automate-ha-opensearch/1.3.14/20240725173732"
-pgleaderchk_pkg_ident = "chef/automate-ha-pgleaderchk/0.1.0/20240725172920"
-postgresql_pkg_ident = "chef/automate-ha-postgresql/13.14.0/20240725171322"`), nil
+					if strings.Contains(filepath, "a2ha_manifest.auto.tfvars") {
+						return []byte(`elasticsidecar_pkg_ident = "chef/automate-ha-elasticsidecar/0.1.0/20240725174312"
+	proxy_pkg_ident = "chef/automate-ha-haproxy/2.2.14/20240725171322"
+	opensearch_pkg_ident = "chef/automate-ha-opensearch/1.3.14/20240725173732"
+	pgleaderchk_pkg_ident = "chef/automate-ha-pgleaderchk/0.1.0/20240725172920"
+	postgresql_pkg_ident = "chef/automate-ha-postgresql/13.14.0/20240725171322"`), nil
+					}
+					fu := fileutils.FileSystemUtils{}
+					return fu.ReadFile(filepath)
 				},
 			},
 				sshUtil: testCase.MockSSHUtil,
