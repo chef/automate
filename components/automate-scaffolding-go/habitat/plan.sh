a369bc3e148d9c57b99ace43319d9d6a75200db9
SahithiMy <sahithi.mylangam@progress.com>
2024-08-02 12:13:58 +0530

    updated scaffolding
    Signed-off-by: SahithiMy <sahithi.mylangam@progress.com>

--- components/automate-scaffolding-go/habitat/plan.sh
+++ components/automate-scaffolding-go/habitat/plan.sh
@@ -12,7 +12,7 @@ pkg_license=('Chef-MLSA')
 pkg_source=nosuchfile.tar.gz
 pkg_deps=(
  core/go19/1.19.3/20240106044515 # This is only pinned to force a scaffolding rebuild to this version
- core/git/2.33.1/20240106041223
+ core/git
  
 )
 
