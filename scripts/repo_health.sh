10234c526d820fdac9ae574f3946082ec84acf6d
iamazzeez <aazeez@progress.com>
2024-08-21 16:16:44 +0530

    proto buf error fix
    Signed-off-by: iamazzeez <aazeez@progress.com>

--- scripts/repo_health.sh
+++ scripts/repo_health.sh
@@ -11,7 +11,7 @@ desired_golang_version() {
 export GOPROXY="https://proxy.golang.org,direct"
 
 hab pkg install -b core/git core/ruby core/jq-static core/shellcheck core/cacerts
-hab pkg install -b "core/go22/1.22.5/20240712112719"
+hab pkg install -b "core/go22/$(desired_golang_version)"
 
 echo "Checking Go Dependencies And Vendored Protos"
 go mod verify
