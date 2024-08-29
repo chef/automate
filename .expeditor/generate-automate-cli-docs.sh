369cebf474feb586316227deb25ef121d865e57b
daveaugustus <pappu.kumar@progress.com>
2024-08-29 15:31:59 +0530

    Modified Go version in all the files
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>

--- .expeditor/generate-automate-cli-docs.sh
+++ .expeditor/generate-automate-cli-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eou pipefail
 
-# bumping expeditor to go 1.22
+# bumping expeditor to go 1.22.5
 hab pkg install --binlink core/go1_22/1.22.5/20240805184444 --force
 
 pushd components/automate-cli
