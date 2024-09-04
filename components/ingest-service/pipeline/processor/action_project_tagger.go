98224ae9813d48136d03c1452b1686a3aa978160
Augusutus <36985548+daveaugustus@users.noreply.github.com>
2024-09-04 16:59:52 +0530

    Dave/pipeline fix (#8564)
    * downgraded packages
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * upgraded linter
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * downgraded elastic search package
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * downgraded elastic search package
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * increased timeout
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * Added timeout for channels
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * modified the test case
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * Added timeout for channels
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * Fixed pipeline closing issue ingest service
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * fixed testcases ingest service
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * Changes in for range
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * Changes in for range
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * Modified for loops
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * error corrections
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    * reverted timeout
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    
    ---------
    
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>
    Co-authored-by: daveaugustus <pappu.kumar@progress.com>

--- components/ingest-service/pipeline/processor/action_project_tagger.go
+++ components/ingest-service/pipeline/processor/action_project_tagger.go
@@ -71,7 +71,7 @@ func actionBundleProjectTagger(in <-chan message.ChefAction,
 func dropChefActionMessages(in <-chan message.ChefAction, err error, numToDrop int) {
 	var numDropped int
 	err = errors.Wrap(err, "bulk dropping message")
-	for numDropped = 0; numDropped < numToDrop; numDropped++ {
+	for numDropped = range make([]struct{}, numToDrop) {
 		select {
 		case m := <-in:
 			m.FinishProcessing(err)
