044fc2db5db4b5873aab3f14c8bb195d53ad04f2
Augusutus <36985548+daveaugustus@users.noreply.github.com>
2024-09-11 11:54:23 +0530

    Update storage.go

--- components/license-control-service/pkg/storage/storage.go
+++ components/license-control-service/pkg/storage/storage.go
@@ -129,7 +129,11 @@ func (p *PGBackend) Init(ctx context.Context, l *keys.LicenseParser) error {
 
 	err = migrator.Migrate(p.pgURL, p.migrationPath, logger.NewLogrusStandardLogger(), false)
 	if err != nil {
-		return errors.Wrap(err, "failed to apply database schema")
+	    if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+	        log.Warn("Skipping migration as it has already been applied")
+	    } else {
+	        return errors.Wrap(err, "failed to apply database schema")
+	    }
 	}
 
 	_, _, err = p.GetLicense(ctx)
