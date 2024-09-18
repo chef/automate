bdaf3b0f3b49fbf11b7ae1802b047a457b26fead
daveaugustus <pappu.kumar@progress.com>
2024-09-18 16:29:12 +0530

    Pipeline for LTS Channel and go_1_22.4
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>

--- components/automate-scaffolding-go/habitat/plan.sh
+++ components/automate-scaffolding-go/habitat/plan.sh
@@ -11,7 +11,7 @@ pkg_version="0.1.0"
 pkg_license=('Chef-MLSA')
 pkg_source=nosuchfile.tar.gz
 pkg_deps=(
- core/go22/1.22.5/20240712112719 # This is only pinned to force a scaffolding rebuild to this version
+ core/go1_22/1.22.4 # This is only pinned to force a scaffolding rebuild to this version
  core/git
  
 )
