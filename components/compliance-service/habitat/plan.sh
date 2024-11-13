cb799175972bd2914871e61bdd725b0873923e5b
Chef Expeditor <chef-ci@chef.io>
2024-11-13 17:54:16 +0000

    Bump automate-compliance-profiles
    This pull request was triggered automatically via Expeditor.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- components/compliance-service/habitat/plan.sh
+++ components/compliance-service/habitat/plan.sh
@@ -51,7 +51,7 @@ else
   # WARNING: chef/automate-compliance-profiles is managed by Expeditor
   # See .expeditor/update-compliance-profiles.sh for details
   pkg_deps+=(
-      chef/automate-compliance-profiles/1.0.0/20240823105414
+      chef/automate-compliance-profiles/1.0.0/20241113101648
   )
 fi
 
