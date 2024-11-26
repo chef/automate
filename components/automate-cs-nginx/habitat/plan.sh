e6d78b2878fe07d63853d3c193542ca731ce7f58
jan shahid shaik <jashaik@progress.com>
2024-11-26 16:13:15 +0530

    Updated 15.10.21 chef-server release in automate
    Signed-off-by: jan shahid shaik <jashaik@progress.com>

--- components/automate-cs-nginx/habitat/plan.sh
+++ components/automate-cs-nginx/habitat/plan.sh
@@ -9,7 +9,7 @@ pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=('Chef-MLSA')
 
 # WARNING: Version managed by .expeditor/update_chef_server.sh
-pkg_version="15.10.12"
+pkg_version="15.10.21"
 pkg_deps=(
   core/coreutils
   chef/mlsa
@@ -22,8 +22,8 @@ pkg_deps=(
   core/curl
   core/ruby30
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/chef-server-nginx/15.10.12/20240910074923"
-  "${vendor_origin}/chef-server-ctl/15.10.12/20240910074330"
+  "${vendor_origin}/chef-server-nginx/15.10.21/20241126094216"
+  "${vendor_origin}/chef-server-ctl/15.10.21/20241126093701"
 )
 
 
