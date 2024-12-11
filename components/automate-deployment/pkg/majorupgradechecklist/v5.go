a05dc828f321f0c708c2a6484c4914d0c061d137
Sahiba3108 <sgoyal@progress.com>
2024-12-11 14:23:47 +0530

    add checklist for v5 for postgres
    Signed-off-by: Sahiba3108 <sgoyal@progress.com>

--- components/automate-deployment/pkg/majorupgradechecklist/v5.go
+++ components/automate-deployment/pkg/majorupgradechecklist/v5.go
@@ -7,6 +7,61 @@ import (
 	"github.com/pkg/errors"
 )
 
+var postChecklistEmbeddedV5 = []PostCheckListItem{
+	{
+		Id:         "upgrade_status",
+		Msg:        run_chef_automate_upgrade_status,
+		Cmd:        run_chef_automate_upgrade_status_cmd,
+		Optional:   true,
+		IsExecuted: false,
+	}, {
+		Id:         "migrate_pg",
+		Msg:        fmt.Sprintf(run_pg_data_migrate, 13.5, 17.0),
+		Cmd:        run_pg_data_migrate_cmd,
+		IsExecuted: false,
+	}, {
+		Id:         "check_ui",
+		Msg:        ui_check,
+		Cmd:        "",
+		Optional:   true,
+		IsExecuted: false,
+	}, {
+		Id:         "clean_up",
+		Msg:        fmt.Sprintf(run_pg_data_cleanup, 13.5),
+		Cmd:        run_pg_data_cleanup_cmd,
+		Optional:   true,
+		IsExecuted: false,
+	},
+}
+
+var postChecklistExternalV5 = []PostCheckListItem{
+	{
+		Id:         "patch_new_config",
+		Msg:        patch_new_conf,
+		Cmd:        patch_new_conf_cmd,
+		Optional:   true,
+		IsExecuted: false,
+	}, {
+		Id:         "upgrade_status",
+		Msg:        run_chef_automate_upgrade_status,
+		Cmd:        run_chef_automate_upgrade_status_cmd,
+		Optional:   true,
+		IsExecuted: false,
+	}, {
+		Id:         "status",
+		Msg:        run_chef_automate_status,
+		Cmd:        run_chef_automate_status_cmd,
+		Optional:   true,
+		IsExecuted: false,
+	}, {
+		Id:         "check_ui",
+		Msg:        ui_check,
+		Cmd:        "",
+		Optional:   true,
+		IsExecuted: false,
+	},
+}
+
 type V5ChecklistManager struct {
 	writer       cli.FormatWriter
 	version      string
@@ -24,9 +79,9 @@ func NewV5ChecklistManager(writer cli.FormatWriter, version string) *V5Checklist
 func (ci *V5ChecklistManager) GetPostChecklist() []PostCheckListItem {
 	var postChecklist []PostCheckListItem
 	if ci.isExternalPG {
-		postChecklist = postChecklistExternal
+		postChecklist = postChecklistExternalV5
 	} else {
-		postChecklist = postChecklistEmbedded
+		postChecklist = postChecklistEmbeddedV5
 	}
 	return postChecklist
 }
@@ -39,11 +94,11 @@ func (ci *V5ChecklistManager) RunChecklist(timeout int64, flags ChecklistUpgrade
 
 	if ci.isExternalPG {
 		dbType = "External"
-		postcheck = postChecklistExternal
-		checklists = append(checklists, []Checklist{downTimeCheck(), backupCheck(), externalPGUpgradeCheck(), postChecklistIntimationCheck()}...)
+		postcheck = postChecklistExternalV5
+		checklists = append(checklists, []Checklist{downTimeCheck(), backupCheck(), externalPGUpgradeCheck(13.5, 17.0), postChecklistIntimationCheck()}...)
 	} else {
 		dbType = "Embedded"
-		postcheck = postChecklistEmbedded
+		postcheck = postChecklistEmbeddedV5
 		checklists = append(checklists, []Checklist{diskSpaceCheck(ci.version, flags.SkipStorageCheck, flags.OsDestDataDir), downTimeCheck(), backupCheck(), postChecklistIntimationCheck()}...)
 	}
 	checklists = append(checklists, showPostChecklist(&postcheck), promptUpgradeContinue())
@@ -52,7 +107,7 @@ func (ci *V5ChecklistManager) RunChecklist(timeout int64, flags ChecklistUpgrade
 		Writer: ci.writer,
 	}
 
-	ci.writer.Println(fmt.Sprintf(initMsg, dbType, ci.version)) //display the init message
+	ci.writer.Println(fmt.Sprintf(initMsg, 17.0, dbType, ci.version)) //display the init message
 
 	for _, item := range checklists {
 		if item.TestFunc == nil {
