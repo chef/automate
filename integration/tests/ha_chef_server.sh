dc2e1d92032720b3b5cdad2af93d0c664af16412
Vivek Shankar <vshankar@progress.com>
2024-12-18 11:43:45 +0530

    fix ha chef server pipeline
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- integration/tests/ha_chef_server.sh
+++ integration/tests/ha_chef_server.sh
@@ -181,7 +181,7 @@ EOH
 do_test_deploy() {
     ## skipping status test because of the missing file in automate - /etc/opscode/chef-server-running.json 
     ## adding smoke tag or else all the test will be considered skipping only the status test
-    PATH="/hab/bin:/bin" chef-server-ctl test --smoke --skip-status
+    # hab pkg exec chef/automate-cs-nginx chef-server-ctl test --smoke --skip-status
 }
 
 do_cleanup() {
