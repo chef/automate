8bbd68b4bd8ffd1ac0de048b651e93e99644f00b
Sahiba3108 <sgoyal@progress.com>
2025-01-02 14:43:08 +0530

    pinned versions
    Signed-off-by: Sahiba3108 <sgoyal@progress.com>

--- components/automate-backend-deployment/habitat/plan.sh
+++ components/automate-backend-deployment/habitat/plan.sh
@@ -31,7 +31,7 @@ pkg_deps=(
    # core/python ( core/python/3.10.0/20231020105702 core/python/3.10.0/20220817121853 )
    #
 pkg_build_deps=(
-  core/gcc
+  core/gcc/9.5.0/20240105175314
 )
 
 # workaround for https://github.com/habitat-sh/habitat/issues/6341
