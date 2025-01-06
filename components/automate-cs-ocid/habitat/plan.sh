8f92cd354b5d491e6e669b08b35bf0cc23b43b6a
sreepuramsudheer <ssudheer@progress.com>
2025-01-06 11:50:31 +0530

    Testing sqitch changes.
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- components/automate-cs-ocid/habitat/plan.sh
+++ components/automate-cs-ocid/habitat/plan.sh
@@ -22,7 +22,7 @@ pkg_deps=(
   chef/mlsa
   "${local_platform_tools_origin:-chef}/automate-platform-tools"
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
-  "${vendor_origin}/oc_id/15.10.27/20250102025130"
+  "${vendor_origin}/oc_id/15.10.27/20250106052143"
 )
 
 pkg_binds=(
