102cef692be745282d261fe7d08bb32f0f2252c1
Yashvi Jain <yashvi.jain@progress.com>
2025-01-06 12:57:10 +0530

    Checking the builds
    Signed-off-by: Yashvi Jain <yashvi.jain@progress.com>

--- .expeditor/generate-automate-cli-docs.sh
+++ .expeditor/generate-automate-cli-docs.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 
 # bumping expeditor to go 1.22
-hab pkg install --binlink core/go1_22/1.22.5 --force
+hab pkg install --binlink core/go1_22 --force
 
 pushd components/automate-cli
   make docs
