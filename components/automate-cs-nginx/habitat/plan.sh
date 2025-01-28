3c7e2ce28ac8453b826b06dbd1d49a63c8f7d95d
talktovikas <vikas.yadav@progress.com>
2025-01-28 19:14:55 +0530

    fixing nginx dependencies.
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

--- components/automate-cs-nginx/habitat/plan.sh
+++ components/automate-cs-nginx/habitat/plan.sh
@@ -11,15 +11,15 @@ pkg_license=('Chef-MLSA')
 # WARNING: Version managed by .expeditor/update_chef_server.sh
 pkg_version="15.10.27"
 pkg_deps=(
-  core/coreutils/8.32/20240105213308
-  chef/mlsa/1.0.1/20240125084021
+  core/coreutils/9.4/20241017111752
+  chef/mlsa/1.0.1/20241018111404
   # TODO 2020-05-12: PIN PIN PIN
   #
   # All dependencies that are shared between this package and the
   # chef-server-* packages are pinned to the versions required by the
   # chef-server-* packages.
   #
-  core/curl/8.7.1/20240614090648
+  core/curl/8.7.1/20241017131032
   core/ruby31/3.1.6/20240912144513
   # WARNING: Version pin managed by .expeditor/update_chef_server.sh
   "${vendor_origin}/chef-server-nginx/15.10.27/20250102025712"
