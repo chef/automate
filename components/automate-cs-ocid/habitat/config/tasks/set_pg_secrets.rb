bb13ed38e3adb48cef9039969c9172fa37a97236
RoyShravani <shravani.roy@progress.com>
2025-02-08 12:50:32 +0530

    adding pg secrets to ocid
    Signed-off-by: RoyShravani <shravani.roy@progress.com>

---
+++ components/automate-cs-ocid/habitat/config/tasks/set_pg_secrets.rb
@@ -0,0 +1,28 @@
+require 'json'
+
+class PGDatabaseConfig
+
+  def pg_superuser_id_from_env
+    fd = ENV['CHEF_SECRETS_FD']
+    if fd
+      f = IO.for_fd(fd.to_i)
+      secrets = JSON.parse(f.read())
+      secrets['userconfig']['pg_superuser_password']
+    else
+        raise "No PG secrets data found in environment"
+    end
+  end
+
+  def get_pg_database_uri()
+    dbURL = ENV['DATABASE_URL']
+    if dbURL.match('redacted') != nil
+      userpass = pg_superuser_id_from_env
+      puts dbURL.dup.sub!('<redacted>',userpass)
+    else
+      dbURL
+    end
+  end
+
+end
+
+PGDatabaseConfig.new.get_pg_database_uri
