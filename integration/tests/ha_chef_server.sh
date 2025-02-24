a7d7bfca94bafda3c6aa1b7b7afeccf70c4ec023
Durga Sarat Chandra Maddu <44021874+Dmaddu@users.noreply.github.com>
2025-02-24 13:27:45 +0000

    fixing the repo health pipeline (#8797)
    * fixing the repo health pipeline
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>
    
    * fixing protobuf
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>
    
    ---------
    
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>

--- integration/tests/ha_chef_server.sh
+++ integration/tests/ha_chef_server.sh
@@ -188,19 +188,3 @@ do_cleanup() {
     docker stop "$_frontend1_container_name"
     docker stop "$_frontend2_container_name"
 }
-
-do_dump_logs() {
-  do_dump_logs_default
-    
-    docker exec -t "$_frontend1_container_name" journalctl --no-pager -u chef-automate > "logs/_frontend1_container_name"
-    docker exec -t "$_frontend2_container_name" journalctl --no-pager -u chef-automate > "logs/_frontend2_container_name"
-
- 
-    if command -v buildkite-agent; then
-        if ! buildkite-agent artifact upload "logs/*"
-        then
-            echo "Failed to frontend conatiner logs"
-        fi
-    fi
-    rm -r "$tmpdir"
-}
