095673404808228c12df1ee71b2494a67a1368e0
vivekshankar1 <59958706+vivekshankar1@users.noreply.github.com>
2025-02-27 15:58:11 +0530

    pinned ha proxy untill hab team fix glib c issue (#8802)
    * pinned ha proxy untill hab team fix glib c issue
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * version bump manual
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    * pin all deps in ha proxy
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>
    
    ---------
    
    Signed-off-by: Vivek Shankar <vshankar@progress.com>

--- components/automate-backend-haproxy/habitat/plan.sh
+++ components/automate-backend-haproxy/habitat/plan.sh
@@ -2,7 +2,7 @@
 # stable channel
 
 
-UPSTREAM_PKG_IDENT="core/haproxy"
+UPSTREAM_PKG_IDENT="core/haproxy/2.2.29/20240106203532"
 pkg_name="automate-ha-haproxy"
 pkg_description="Wrapper package for core/haproxy"
 vendor_origin="chef"
@@ -12,9 +12,9 @@ pkg_upstream_url="https://www.chef.io/automate"
 pkg_version="2.2.14"
 pkg_build_deps=("${UPSTREAM_PKG_IDENT}")
 pkg_deps=(
-  chef/mlsa
-  core/bash
-  core/netcat-openbsd
+  chef/mlsa/1.0.1/20240125084021
+  core/bash/5.1/20240105214248
+  core/netcat-openbsd/1.226/20240106172343
   "${UPSTREAM_PKG_IDENT}"
 )
 
