733787aebb2c678980f5cd1de76ace5a268921c9
daveaugustus <pappu.kumar@progress.com>
2025-03-24 16:38:16 +0530

    removed the print statements
    Signed-off-by: daveaugustus <pappu.kumar@progress.com>

--- components/ingest-service/backend/elastic/migration.go
+++ components/ingest-service/backend/elastic/migration.go
@@ -558,9 +558,7 @@ func (es *Backend) GetAliases(ctx context.Context, index string) ([]string, bool
 	return aliases, hasAliases, nil
 }
 
-// Example settings: map[analysis:map[analyzer:map[autocomplete:map[filter:[lowercase] tokenizer:autocomplete_tokenizer]] normalizer:map[case_insensitive:map[char_filter:[] filter:[lowercase asciifolding] type:custom]] tokenizer:map[autocomplete_tokenizer:map[max_gram:20 min_gram:2 token_chars:[letter digit] type:edge_ngram]]] creation_date:1742381505210 number_of_replicas:1 number_of_shards:5 provided_name:comp-3-profiles refresh_interval:1s uuid:LxDd9TZUQtqL-DSLrdR_FQ version:map[created:6082399 upgraded:135249827]]
 func (es *Backend) FetchIndexSettings(index string) (map[string]any, error) {
-	fmt.Println("Fetching settings for index:", index)
 	resp, err := es.client.IndexGetSettings(index).Do(context.Background())
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch settings for index %s: %w", index, err)
@@ -593,12 +591,10 @@ func (es *Backend) FetchIndexSettings(index string) (map[string]any, error) {
 	delete(settingsMap, "query_string")
 	delete(settingsMap, "verified_before_close")
 
-	fmt.Printf("settingsMap: %+v\n", settingsMap)
 	return settingsMap, nil
 }
 
 func (es *Backend) FetchIndexMappings(index string) (map[string]interface{}, error) {
-	fmt.Println("Fetching mappings for index:", index)
 	resp, err := es.client.GetMapping().Index(index).Do(context.Background())
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch mappings for index %s: %w", index, err)
@@ -630,80 +626,13 @@ func (es *Backend) FetchIndexMappings(index string) (map[string]interface{}, err
 	// Remove unsupported root parameters
 	delete(mappingsMap, "mappings")
 
-	fmt.Printf("mappingsMap: %+v\n", mappingsMap)
-
 	return mappingsMap, nil
 }
-
-// // Example settings: map[analysis:map[analyzer:map[autocomplete:map[filter:[lowercase] tokenizer:autocomplete_tokenizer]] normalizer:map[case_insensitive:map[char_filter:[] filter:[lowercase asciifolding] type:custom]] tokenizer:map[autocomplete_tokenizer:map[max_gram:20 min_gram:2 token_chars:[letter digit] type:edge_ngram]]] creation_date:1742381505210 number_of_replicas:1 number_of_shards:5 provided_name:comp-3-profiles refresh_interval:1s uuid:LxDd9TZUQtqL-DSLrdR_FQ version:map[created:6082399 upgraded:135249827]]
-// func (es *Backend) SanitizeSettings(settings map[string]interface{}, indexName string) (map[string]interface{}, error) {
-// 	fmt.Printf("Sanitizing settings for index: %s\n", indexName)
-// 	fmt.Printf("Source index settings: %v\n", settings)
-
-// 	indexData, ok := settings[indexName]
-// 	if !ok {
-// 		return nil, fmt.Errorf("settings for index %s not found", indexName)
-// 	}
-
-// 	fmt.Println("Debug 1")
-
-// 	indexDataMap, ok := indexData.(map[string]interface{})
-// 	if !ok {
-// 		return nil, fmt.Errorf("invalid format for settings of index %s", indexName)
-// 	}
-
-// 	fmt.Println("Debug 2")
-
-// 	settingsMap, ok := indexDataMap["settings"].(map[string]interface{})
-// 	if !ok {
-// 		return nil, fmt.Errorf("settings key not found in index data for %s", indexName)
-// 	}
-
-// 	fmt.Println("Debug 3")
-
-// 	indexSettings, ok := settingsMap["index"].(map[string]interface{})
-// 	if !ok {
-// 		return nil, fmt.Errorf("index key not found in settings for index %s", indexName)
-// 	}
-
-// 	fmt.Println("Debug 4")
-
-// 	sanitizedIndexSettings := make(map[string]interface{})
-
-// 	for key, value := range indexSettings {
-// 		// Remove only settings that are not allowed during index creation
-// 		switch key {
-// 		case "creation_date", "creation_date_string", "uuid", "version", "provided_name", "resize", "routing", "store", "warmer", "flush", "merge", "sync", "translog", "query_string", "verified_before_close":
-// 			// Skip these keys
-// 			fmt.Println("Skipping key:", key)
-// 			continue
-// 		default:
-// 			fmt.Println("Adding key:", key)
-// 			sanitizedIndexSettings[key] = value
-// 		}
-// 	}
-
-// 	fmt.Println("Debug 5")
-
-// 	settingsMap["index"] = sanitizedIndexSettings
-// 	indexDataMap["settings"] = settingsMap
-// 	settings[indexName] = indexDataMap
-
-// 	fmt.Printf("sanitizedIndexSettings: %+v\n", sanitizedIndexSettings)
-
-// 	return sanitizedIndexSettings, nil
-// }
 
 func (es *Backend) CreateIndex(destIndex string, sourceIndex string, sourceIndexSettings map[string]any, sourceIndexMappings map[string]any) error {
 	fmt.Printf("Creating index %s from index %s\n", destIndex, sourceIndex)
 	fmt.Printf("Source index settings: %+v\n", sourceIndexSettings)
 	fmt.Printf("Source index mappings: %+v\n", sourceIndexMappings)
-
-	// // Sanitize settings before creating the index
-	// sanitizedSettings, err := es.SanitizeSettings(sourceIndexSettings, sourceIndex)
-	// if err != nil {
-	// 	return fmt.Errorf("failed to sanitize settings for index %s: %w", sourceIndex, err)
-	// }
 
 	createIndexService := es.client.CreateIndex(destIndex).
 		BodyJson(map[string]any{
@@ -716,6 +645,5 @@ func (es *Backend) CreateIndex(destIndex string, sourceIndex string, sourceIndex
 		return fmt.Errorf("failed to create index %s: %w", destIndex, err)
 	}
 
-	fmt.Printf("Index %s created successfully.\n", destIndex)
 	return nil
 }
