c3e1572694722a5dbbe017f94b798c4ee014c348
Yashvi Jain <Yashvi.jain@progress.com>
2025-07-14 20:11:34 +0530

    Added the hab token in every component
    Signed-off-by: Yashvi Jain <Yashvi.jain@progress.com>

--- components/automate-deployment/pkg/target/hab_cmd.go
+++ components/automate-deployment/pkg/target/hab_cmd.go
@@ -22,8 +22,6 @@ var stdHabOptions = []command.Opt{
 	command.Envvar("HAB_NONINTERACTIVE", "true"),
 	// Don't prompt for license acceptance
 	command.Envvar("HAB_LICENSE", "accept-no-persist"),
-
-	command.Envvar("HAB_BLDR_URL", "https://bldr.acceptance.habitat.sh"),
 }
 
 const (
