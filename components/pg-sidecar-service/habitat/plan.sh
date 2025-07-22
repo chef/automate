bf369188fec7633ef30177a0332bee67ddc17d9a
Aishwarya2001A <145524448+Aishwarya2001A@users.noreply.github.com>
2025-07-22 20:25:05 +0530

    pg update (#8932)
    * pg update
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * modification
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * license scout changes
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * changed core/sqitch/ version
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * sqitch pkg version change
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * sqitch pkg version change 2
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * updated regex (#8927)
    
    Signed-off-by: vinay sharma <vsharma@chef.io>
    
    * Bump version to 4.13.214 by Chef Expeditor
    
    Obvious fix; these changes are the result of automation not creative thinking.
    
    * Bump version to 4.13.215 by Chef Expeditor
    
    Obvious fix; these changes are the result of automation not creative thinking.
    
    * unpinned versions
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * a
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * a
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * a
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * a
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * a
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    * sqitch LTS version pinned
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    
    ---------
    
    Signed-off-by: Aishwarya2001A <aaishwar@progress.com>
    Signed-off-by: vinay sharma <vsharma@chef.io>
    Co-authored-by: vinay sharma <vsharma@chef.io>
    Co-authored-by: Chef Expeditor <chef-ci@chef.io>

--- components/pg-sidecar-service/habitat/plan.sh
+++ components/pg-sidecar-service/habitat/plan.sh
@@ -20,11 +20,11 @@ pkg_exposes=(port)
 pkg_deps=(
   chef/mlsa
   chef/automate-platform-tools
-  # Pin postgresql, sqitch_pg, and postgresql client until automate-postgresql
+  # Pin postgresql, sqitch, and postgresql client until automate-postgresql
   # is updated.
-  core/postgresql13/13.18/20241203070217 # for psql and pg_dump
-  core/postgresql13-client/13.18/20241203070721
-  core/sqitch_pg/3.15.0/20240614112821
+  core/postgresql13/13.21/20250711150030 # for psql and pg_dump
+  core/postgresql13-client/13.21/20250711152541
+  core/sqitch/1.4.1/20250722100509
 )
 pkg_bin_dirs=(bin)
 pkg_scaffolding="${local_scaffolding_origin:-chef}/automate-scaffolding-go"
